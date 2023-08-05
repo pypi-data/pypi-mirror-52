7f7e1b14e7e84c23c41807e41c025e84ddcce53e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:23 +0000

    Add package pystrict/1.0/pystrict-1.0-py3-none-any.whl

---
+++ packages/pystrict/pystrict-1.0-py3-none-any.whl/pystrict.py
@@ -0,0 +1,53 @@
+# pylint: disable=protected-access
+
+"""
+## strict
+
+Using @strict on classes can prevent serious errors by raising an
+exception when an instance has a variable created outside of init.
+
+"""
+
+__version__ = "1.0"
+
+import inspect, itertools, functools
+
+__all__ = ['strict', 'StrictError']
+
+class StrictError(TypeError):
+    pass
+
+def _check_args(func):
+    info = inspect.getfullargspec(func)
+
+    for k in itertools.chain(info.args, info.kwonlyargs):
+        if k != "self" and k not in func.__annotations__:
+            raise StrictError("%s missing type specifier in __init__" % k)
+
+def _init_decorator(func):
+    _check_args(func)
+
+    @functools.wraps(func)
+    def wrapper(self, *args, **kwargs):
+        func(self, *args, **kwargs)
+        self._x_frozen = True
+    return wrapper
+
+def strict(thing):
+    if inspect.isfunction(thing):
+        func = thing
+        _check_args(func)
+        return func
+    else:
+        cls = thing
+
+        def frozen_setattr(self, key, value):
+            if self._x_frozen and not hasattr(self, key):
+                raise StrictError("Class %s is frozen. Cannot set '%s'." % (cls.__name__, key))
+            cls._x_setter(self, key, value)
+
+        cls._x_frozen = False
+        cls._x_setter = getattr(cls, "__setattr__", object.__setattr__)
+        cls.__setattr__ = frozen_setattr
+        cls.__init__ = _init_decorator(cls.__init__)
+    return cls
