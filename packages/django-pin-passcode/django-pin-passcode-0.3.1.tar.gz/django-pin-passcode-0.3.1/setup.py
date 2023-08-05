e64c04dcd6559eed0b7957dc3f6e458ddd383aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:19 +0000

    Add package django-pin-passcode/0.3.1/django-pin-passcode-0.3.1.tar.gz

---
+++ packages/django-pin-passcode/django-pin-passcode-0.3.1.tar.gz/django-pin-passcode-0.3.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setup(
+    name="django-pin-passcode",
+    packages=find_packages(),
+    include_package_data=True,  # declarations in MANIFEST.in
+    version="0.3.1",
+    author="Eric Carmichael",
+    author_email="eric@ckcollab.com",
+    description="A simple django app that provides site-wide easy password authentication for 1 user",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/ckcollab/django-pin-passcode",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
