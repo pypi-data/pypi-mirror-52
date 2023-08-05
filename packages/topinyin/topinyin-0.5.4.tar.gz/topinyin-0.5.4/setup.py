9d81fa78dc822ff4b43739b805b5a9afdaf2c33f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:18:29 +0000

    Add package topinyin/0.5.4/topinyin-0.5.4.tar.gz

---
+++ packages/topinyin/topinyin-0.5.4.tar.gz/topinyin-0.5.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name = 'topinyin',      
+    version = '0.5.4',
+    author = 'yanwydxf',        
+    author_email = 'yanwydxf@163.com',
+    url = 'https://github.com/yanwydxf/topinyin',
+    description = 'translate chinese hanzi to pinyin by python'  ,
+    packages=["topinyin"] 
+ )
