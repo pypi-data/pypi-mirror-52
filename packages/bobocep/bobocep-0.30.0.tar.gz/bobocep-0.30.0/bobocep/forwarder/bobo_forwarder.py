4a3e1aad006b1d1c4b7c208bdbd9a3063bc34ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:18 +0000

    Add package bobocep/0.30.0/bobocep-0.30.0.tar.gz

---
+++ packages/bobocep/bobocep-0.30.0.tar.gz/bobocep-0.30.0/bobocep/forwarder/bobo_forwarder.py
@@ -0,0 +1,101 @@
+from abc import ABC, abstractmethod
+from queue import Queue
+
+from bobocep.forwarder.abstract_forwarder import AbstractForwarder
+from bobocep.forwarder.forwarder_subscriber import IForwarderSubscriber
+from bobocep.producer.producer_subscriber import IProducerSubscriber
+from bobocep.rules.actions.action_subscriber import IActionSubscriber
+from bobocep.rules.events.action_event import ActionEvent
+from bobocep.rules.events.bobo_event import BoboEvent
+from bobocep.rules.events.composite_event import CompositeEvent
+from bobocep.setup.task.bobo_task import BoboTask
+
+
+class BoboForwarder(AbstractForwarder,
+                    BoboTask,
+                    IProducerSubscriber,
+                    IActionSubscriber,
+                    ABC):
+    """A :code:`bobocep` event forwarder that forwards CompositeEvent
+    instances.
+
+    :param max_queue_size: The maximum data queue size,
+                           defaults to 0 (infinite).
+    :type max_queue_size: int, optional
+    """
+
+    def __init__(self, max_queue_size: int = 0) -> None:
+        super().__init__()
+
+        self._event_queue = Queue(maxsize=max_queue_size)
+        self._subs = []
+
+    @abstractmethod
+    def _handle_forwarder_event(self, event: BoboEvent) -> bool:
+        """
+        Enables the forwarder to perform some action with the BoboEvent
+        instance before it is sent to the subscribers of the forwarder.
+        If the method returns True, the BoboEvent instance will be
+        forwarded to subscribers.
+
+        :param event: The event.
+        :type event: BoboEvent
+
+        :return: True if event was successfully handled, False otherwise.
+        """
+
+    def _loop(self) -> None:
+        while not self._event_queue.empty():
+            event = self._event_queue.get_nowait()
+
+            if event is not None:
+                if self._handle_forwarder_event(event):
+                    self._notify_success(event)
+                else:
+                    self._notify_failure(event)
+
+    def on_accepted_producer_event(self, event: CompositeEvent):
+        if not self._cancelled:
+            self._event_queue.put_nowait(event)
+
+    def on_action_attempt(self, event: ActionEvent) -> None:
+        if not self._cancelled:
+            self._event_queue.put_nowait(event)
+
+    def subscribe(self, subscriber: IForwarderSubscriber) -> None:
+        """
+        :param subscriber: Subscribes to events from Forwarder.
+        :type subscriber: IForwarderSubscriber
+        """
+
+        with self._lock:
+            if not self._cancelled:
+                if subscriber not in self._subs:
+                    self._subs.append(subscriber)
+
+    def unsubscribe(self, unsubscriber: IForwarderSubscriber) -> None:
+        """
+        :param unsubscriber: Unsubscribes to events from Forwarder.
+        :type unsubscriber: IForwarderSubscriber
+        """
+
+        with self._lock:
+            if unsubscriber in self._subs:
+                self._subs.remove(unsubscriber)
+
+    def _notify_success(self, event: CompositeEvent):
+        for subscriber in self._subs:
+            subscriber.on_forwarder_success_event(event)
+
+    def _notify_failure(self, event: CompositeEvent):
+        for subscriber in self._subs:
+            subscriber.on_forwarder_failure_event(event)
+
+    def on_rejected_producer_event(self, event: CompositeEvent):
+        """"""
+
+    def _setup(self) -> None:
+        """"""
+
+    def _cancel(self) -> None:
+        """"""
