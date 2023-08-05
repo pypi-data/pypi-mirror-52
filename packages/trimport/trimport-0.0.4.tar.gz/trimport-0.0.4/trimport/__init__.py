9fef2e218a94bdea38f55b44b02027fcc4021232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:14 +0000

    Add package trimport/0.0.4/trimport-0.0.4.tar.gz

---
+++ packages/trimport/trimport-0.0.4.tar.gz/trimport-0.0.4/trimport/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    init
+    ~~~~~
+    init
+    :copyright: 2019 Chris Zimmerman
+    :license: BSD-3-Clause
+"""
+from .core import FunctionPathFactory
+from .core import FunctionPath
+
+__version__ = "0.0.4"
