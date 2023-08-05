037cc865a44a5ff11794d448245fbd082eedefaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:23 +0000

    Add package peewee-generic-filters/0.2.2/peewee_generic_filters-0.2.2-py3-none-any.whl

---
+++ packages/peewee-generic-filters/peewee_generic_filters-0.2.2-py3-none-any.whl/peewee_filters/__init__.py
@@ -0,0 +1,23 @@
+from . filterset import FilterSet
+from . filters import (
+    Filter,
+    MethodFilter,
+    CharFilter,
+    NumberFilter,
+    DateTimeFilter,
+    TimeFilter,
+    DateFilter,
+    BooleanFilter,
+    UUIDFilter,
+    SearchingFilter,
+    LimitFilter,
+    OffsetFilter,
+    OrderingFilter
+)
+
+__version__ = '0.2.2'
+
+__all__ = [
+    'FilterSet', 'Filter', 'MethodFilter', 'CharFilter', 'NumberFilter', 'DateTimeFilter', 'TimeFilter',
+    'DateFilter', 'BooleanFilter', 'UUIDFilter', 'SearchingFilter', 'LimitFilter', 'OffsetFilter', 'OrderingFilter'
+]
