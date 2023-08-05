2c6b3f31df806617b41962fe3bcc6fcd5b4b4686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:37 +0000

    Add package mc-sprite/0.0.10/mc_sprite-0.0.10-py3-none-any.whl

---
+++ packages/mc-sprite/mc_sprite-0.0.10-py3-none-any.whl/Engine/Engine.py
@@ -0,0 +1,66 @@
+import _thread
+from Timer.Time import Time
+import pygame
+import sys
+
+pygame.init()
+
+class Engine(object):
+    drawlist={}
+    screen=None
+    time=None
+    __isMouseDown_=False
+    __isMouseUp_=True
+    __isMouse=False
+    @staticmethod
+    def Start(screen=None):
+        Engine.time=Time()
+        if not screen==None:
+            Engine.screen=screen
+        pass
+    @staticmethod
+    def isMouseDown():
+        return Engine.__isMouseDown_
+        pass
+    @staticmethod
+    def isMouseClicked():
+        if Engine.__isMouseDown_:
+            Engine.__isMouse=True
+        if Engine.__isMouseUp_:
+            if Engine.__isMouse:
+                Engine.__isMouse=False
+                return True
+        pass
+    @staticmethod
+    def isMouseUp():
+        return Engine.__isMouseUp_
+        pass
+    @staticmethod
+    def A():
+        pass
+    @staticmethod
+    def SetMode(resolution=(0,0),flags=0,depth=0):
+        Engine.screen=pygame.display.set_mode(resolution,flags,depth)
+    @staticmethod
+    def SetCaption(title: str, icontitle = None):
+        if icontitle==None:
+            pygame.display.set_caption(title)
+        else:
+            pygame.display.set_caption(title,icontitle)
+    @staticmethod
+    def Draw():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                Engine.__isMouseDown_=True
+                Engine.__isMouseUp_=False
+            elif event.type == pygame.MOUSEBUTTONUP:
+                Engine.__isMouseDown_=False 
+                Engine.__isMouseUp_=True
+        Engine.time.CalculationDeltaTime()
+        for x in sorted(Engine.drawlist):
+            temp=Engine.drawlist[x]
+            if temp.active:
+                Engine.screen.blit(temp.transform.image,temp.transform.position.value)
+        pygame.display.update()
