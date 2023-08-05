11294c7205708621d9623ad2e9bce3b295d3dea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:53 +0000

    Add package uhid-freebsd/1.2.0/uhid-freebsd-1.2.0.tar.gz

---
+++ packages/uhid-freebsd/uhid-freebsd-1.2.0.tar.gz/uhid-freebsd-1.2.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup, Extension
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='uhid-freebsd',
+    version='1.2.0',
+    author='Michael Gmelin',
+    author_email='freebsd@grem.de',
+    ext_modules=[Extension('uhid_freebsd', ['uhid_freebsd.cpp'])],
+    maintainer='Michael Gmelin',
+    maintainer_email='freebsd@grem.de',
+    url='https://github.com/grembo/uhid-freebsd',
+    description='Get information on FreeBSD uhid devices.',
+    long_description=readme(),
+    long_description_content_type='text/x-rst',
+    license='BSD 2-Clause',
+    platforms='FreeBSD',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: BSD :: FreeBSD',
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development'
+    ]
+)
