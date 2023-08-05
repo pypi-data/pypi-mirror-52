31c9c66e9060ce7fdeb71931874855f8be47ed57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:01 +0000

    Add package smoothnlp-api/0.1.4/smoothnlp_api-0.1.4.tar.gz

---
+++ packages/smoothnlp-api/smoothnlp_api-0.1.4.tar.gz/smoothnlp_api-0.1.4/setup.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+
+"""
+    Investment
+
+    投资事件(主要来源ITjuzi)  * 默认域名： service-m5j3awiv-1259459016.ap-shanghai.apigateway.myqcloud.com/release * 自定义域名： data.service.invest.smoothnlp.com/   # noqa: E501
+"""
+
+
+from setuptools import setup, find_packages  # noqa: H301
+import os
+
+rootdir = os.path.abspath(os.path.dirname(__file__))
+long_description = open(os.path.join(rootdir, 'README.md')).read()
+
+NAME = "smoothnlp_api"
+VERSION = "0.1.4"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="Investment",
+    author_email="zhangruinan@smoothnlp.com",
+    url="https://github.com/smoothnlp/smoothnlp_api",
+    keywords=["Swagger", "data","api"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description_content_type="text/markdown",
+    long_description=long_description
+)
