6d1cdca8be27a5e4bde4e36b0b21302918451d24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:39 +0000

    Add package quintoandar-eb-deployer/3.1/quintoandar_eb_deployer-3.1.tar.gz

---
+++ packages/quintoandar-eb-deployer/quintoandar_eb_deployer-3.1.tar.gz/quintoandar_eb_deployer-3.1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(
+    name='quintoandar_eb_deployer',
+    description='Make your project publishes itself to AWS',
+    version='3.1',
+    author='Diego Queiroz',
+    author_email='diego.queiroz@gmail.com',
+    license='MIT',
+    url='https://github.com/quintoandar/eb_deployer',
+    long_description=open('README.rst').read(),
+    packages=[
+        'quintoandar_eb_deployer',
+        'quintoandar_eb_deployer.management',
+        'quintoandar_eb_deployer.management.commands',
+        'quintoandar_eb_deployer.management.commands.aws'
+    ],
+    install_requires=[
+        'Django',
+        'python_magic'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: System :: Installation/Setup'
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'ebdeployer=quintoandar_eb_deployer.management.commands._awsElasticbeanstalkPush:main',
+        ],
+    },
+)
