34ee9720ba9247aed7d2ff46b402cd7c6c237a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:58 +0000

    Add package brightwind/0.2.10/brightwind-0.2.10.tar.gz

---
+++ packages/brightwind/brightwind-0.2.10.tar.gz/brightwind-0.2.10/brightwind/__init__.py
@@ -0,0 +1,12 @@
+from .load.load import *
+from .analyse import correlation as Correl
+from .analyse.shear import *
+from .analyse.analyse import *
+from .analyse.plot import *
+from .transform.transform import *
+from .export.export import *
+from . import datasets
+# from .utils.utils import *
+
+__all__ = ['analyse', 'transform', 'export', 'load', 'datasets']
+
---
+++ packages/brightwind/brightwind-0.2.10.tar.gz/brightwind-0.2.10/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='brightwind',
+    # Update version number here:
+    version='0.2.10',
+    packages=['brightwind', 'brightwind.load', 'brightwind.utils', 'brightwind.export', 'brightwind.analyse',
+              'brightwind.transform'],  # , 'brightwind.datasets'],
+    package_data={
+        # If any package contains *.mplstyle or *.txt files, include them:
+        '': ['*.mplstyle'],  # , 'datasets/demo/*.csv', 'datasets/demo/*.txt'],
+    },
+    url='https://github.com/brightwind-dev/brightwind.git',
+    # Update version number here:
+    download_url = 'https://github.com/brightwind-dev/brightwind/archive/v0.2.10.tar.gz',
+    license='GNU Lesser General Public License v3 or later (LGPLv3+)',
+    author='Stephen Holleran and Inder Preet of BrightWind Ltd',
+    author_email='stephen@brightwindanalysis.com',
+    description='Scripts for wind resource data processing.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=['BRIGHT', 'WIND', 'RESOURCE', 'DATA', 'ANALYSTS', 'PROCESSING', 'WASP', 'ROSE', 'WINDFARMER', 'OPENWIND',
+              'WIND PRO', 'WINDOGRAPHER'],
+    install_requires=[
+        'pandas>=0.24.0',
+        'numpy>=1.16.4',
+        'scikit-learn>=0.19.1',
+        'matplotlib>=3.0.3',
+        'requests>=2.20.0',
+        'scipy>=0.19.1',
+        'pytest>= 4.1.0',
+        'six>= 1.12.0',
+        'python-dateutil>=2.8.0',
+        'ipywidgets>=7.4.2',
+        'ipython>=7.4.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+    ],
+)
