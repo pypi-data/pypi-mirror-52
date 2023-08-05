82d9559f0f24e7db92ba3efd4495cae812e78ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:46 +0000

    Add package plover-space-config/0.0.8/plover_space_config-0.0.8-py3-none-any.whl

---
+++ packages/plover-space-config/plover_space_config-0.0.8-py3-none-any.whl/plover_space_config.py
@@ -0,0 +1,37 @@
+'''
+Functionality to switch spacing styles in Plover without having to open your config.
+'''
+
+BEFORE='Before Output'
+AFTER='After Output'
+
+from plover.engine import StenoEngine
+
+def space_placement_toggle(engine: StenoEngine, _) -> None:
+	'''
+	Toggle the spacing type.
+	'''
+
+	print("toggling")
+	print("current: " + engine.config['space_placement'])
+
+	if engine.config['space_placement'] == BEFORE:
+		engine.config = { 'space_placement' : AFTER }
+	else:
+		engine.config = { 'space_placement' : BEFORE }
+
+	print("after: " + engine.config['space_placement'])
+
+def space_placement_before(engine: StenoEngine, _) -> None:
+	'''
+	Set spacing to before.
+	'''
+
+	engine.config = { 'space_placement' : BEFORE }
+
+def space_placement_after(engine: StenoEngine, _) -> None:
+	'''
+	Set spacing to after.
+	'''
+
+	engine.config = { 'space_placement' : AFTER }
