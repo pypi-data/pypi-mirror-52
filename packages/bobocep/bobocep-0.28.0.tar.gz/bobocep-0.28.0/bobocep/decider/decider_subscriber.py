d2c5d1d3e31189240ff9135706266fdbc422cef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:57 +0000

    Add package bobocep/0.28.0/bobocep-0.28.0.tar.gz

---
+++ packages/bobocep/bobocep-0.28.0.tar.gz/bobocep-0.28.0/bobocep/decider/decider_subscriber.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+from bobocep.rules.events.composite_event import CompositeEvent
+
+
+class IDeciderSubscriber(ABC):
+    """An interface to subscribe to Decider events."""
+
+    @abstractmethod
+    def on_decider_complex_event(self,
+                                 nfa_name: str,
+                                 event: CompositeEvent) -> None:
+        """
+        When a Decider identifies a complex event in the event stream.
+
+        :param nfa_name: The NFA from which the event was generated.
+        :type nfa_name: str
+
+        :param event: The complex event.
+        :type event: CompositeEvent
+        """
