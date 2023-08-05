34b78967d82bd425755fb163132796407408f271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:12 +0000

    Add package nti.site/2.0.0/nti.site-2.0.0.tar.gz

---
+++ packages/nti.site/nti.site-2.0.0.tar.gz/nti.site-2.0.0/setup.py
@@ -0,0 +1,73 @@
+import codecs
+from setuptools import setup, find_packages
+
+
+TESTS_REQUIRE = [
+    'fudge',
+    'nti.testing',
+    'pyhamcrest',
+    'z3c.baseregistry',
+    'zope.testrunner',
+]
+
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='nti.site',
+    version=_read('version.txt').strip(),
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="Opinionated ZODB persistent site implementations",
+    long_description=_read('README.rst'),
+    url="https://github.com/NextThought/nti.site",
+    license='Apache',
+    keywords='Site management',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'BTrees >= 4.3.2',  # permissive get()
+        'ZODB',
+        'nti.schema',
+        'nti.transactions >= 3.0.0',
+        'persistent',
+        'setuptools',
+        'six',
+        'transaction >= 2.4.0',  # for looser text/byte handling
+        'zope.component',
+        'zope.container',
+        'zope.interface >= 4.4.2',
+        'zope.location',
+        'zope.proxy',
+        'zope.site',
+        'zope.traversing',
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+        'docs': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+            'sphinx_rtd_theme',
+        ]
+    },
+)
