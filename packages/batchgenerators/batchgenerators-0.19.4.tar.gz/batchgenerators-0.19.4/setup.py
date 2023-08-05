ed6f7b2de0e075ef95e74c5776420f45b32eca7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:08 +0000

    Add package batchgenerators/0.19.4/batchgenerators-0.19.4.tar.gz

---
+++ packages/batchgenerators/batchgenerators-0.19.4.tar.gz/batchgenerators-0.19.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(name='batchgenerators',
+      version='0.19.4',
+      description='Data augmentation toolkit',
+      url='https://github.com/MIC-DKFZ/batchgenerators',
+      author='Division of Medical Image Computing, German Cancer Research Center',
+      author_email='f.isensee@dkfz-heidelberg.de',
+      license='Apache License Version 2.0, January 2004',
+      packages=['batchgenerators', 'batchgenerators.augmentations',
+      'batchgenerators.examples', 'batchgenerators.transforms', 'batchgenerators.dataloading',
+                'batchgenerators.utilities'],
+      install_requires=[
+            "numpy>=1.10.2",
+            "scipy",
+            "scikit-image",
+            "scikit-learn",
+            "future",
+            "unittest2"
+      ],
+      keywords=['deep learning', 'image segmentation', 'image classification', 'medical image analysis',
+                  'medical image segmentation', 'data augmentation'],
+      download_url="https://github.com/MIC-DKFZ/batchgenerators/archive/v0.19.3.tar.gz"
+      )
