3d6f872759b23b34ff604b3e614c41747611125e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:50 +0000

    Add package kuber/1.9.1/kuber-1.9.1.tar.gz

---
+++ packages/kuber/kuber-1.9.1.tar.gz/kuber-1.9.1/kuber/latest/__init__.py
@@ -0,0 +1,18 @@
+import datetime as _datetime
+
+from kuber import versioning
+
+# Information about the kubernetes API library version associated with this
+# package and kuber metadata around its creation and labeling within the
+# kuber library.
+KUBERNETES_VERSION = versioning.KubernetesVersion(
+    label='latest',
+    version='v1.15.3',
+    major='1',
+    minor='15',
+    patch='3',
+    pre_release='',
+    build='',
+    created_at=_datetime.datetime(2019, 9, 10),
+    commit_sha='2d3c76f9091b6bec110a5e63777c332469e0cba2'
+)
---
+++ packages/kuber/kuber-1.9.1.tar.gz/kuber-1.9.1/kuber/v1_14/__init__.py
@@ -0,0 +1,18 @@
+import datetime as _datetime
+
+from kuber import versioning
+
+# Information about the kubernetes API library version associated with this
+# package and kuber metadata around its creation and labeling within the
+# kuber library.
+KUBERNETES_VERSION = versioning.KubernetesVersion(
+    label='v1.14',
+    version='v1.14.6',
+    major='1',
+    minor='14',
+    patch='6',
+    pre_release='',
+    build='',
+    created_at=_datetime.datetime(2019, 9, 10),
+    commit_sha='96fac5cd13a5dc064f7d9f4f23030a6aeface6cc'
+)
