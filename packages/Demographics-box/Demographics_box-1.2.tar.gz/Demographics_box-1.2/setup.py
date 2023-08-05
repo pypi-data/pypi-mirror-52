9087814b5bd0433bbcd7fcd12d97d8d6f9eb0a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:18:41 +0000

    Add package Demographics-box/1.2/Demographics_box-1.2.tar.gz

---
+++ packages/Demographics-box/Demographics_box-1.2.tar.gz/Demographics_box-1.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="Demographics_box",
+    version="1.2",
+    author="Emmanuel Sevrin",
+    description="A package to generate list of people",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
