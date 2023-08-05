12ddbef8ab097a35eb24d6c630bbbba1199a72d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:10 +0000

    Add package py-hopscotch-dict/1.0.0/py_hopscotch_dict-1.0.0-py2.py3-none-any.whl

---
+++ packages/py-hopscotch-dict/py_hopscotch_dict-1.0.0-py2.py3-none-any.whl/py_hopscotch_dict/__init__.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+################################################################################
+#                              py-hopscotch-dict                               #
+#    Full-featured `dict` replacement with guaranteed constant-time lookups    #
+#                               (C) 2019 Mischif                               #
+#       Released under version 3.0 of the Non-Profit Open Source License       #
+################################################################################
+
+from io import open
+from os.path import abspath, dirname, join
+
+from py_hopscotch_dict.hopscotchdict import HopscotchDict
+
+module_root = dirname(abspath(__file__))
+package_root = dirname(module_root)
+
+with open(join(package_root, 'VERSION'), encoding="utf-8") as version_file:
+	__version__ = version_file.read().strip()
