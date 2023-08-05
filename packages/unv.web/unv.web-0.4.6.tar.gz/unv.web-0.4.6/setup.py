61120d7582d812071c8312bdb54daa5215b5195c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:26 +0000

    Add package unv.web/0.4.6/unv.web-0.4.6.tar.gz

---
+++ packages/unv.web/unv.web-0.4.6.tar.gz/unv.web-0.4.6/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='unv.web',
+    version='0.4.6',
+    description="""Web application helpers for unv based on aiohttp""",
+    url='http://github.com/c137digital/unv_web',
+    author='Morty Space',
+    author_email='morty.space@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    install_requires=[
+        'unv.app',
+        'unv.deploy',
+        'aiohttp',
+        'uvloop',
+        'ujson',
+        'jinja2',
+
+        # TODO: move to package with redis deploy and web
+        'aioredis'
+    ],
+    extras_require={
+        'dev': [
+            'pylint',
+            'pycodestyle',
+            'pytest',
+            'pytest-cov',
+            'pytest-env',
+            'pytest-pythonpath',
+            'pytest-aiohttp',
+            'autopep8',
+            'sphinx',
+            'setuptools',
+            'wheel',
+            'twine'
+        ]
+    },
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'web_app_shell = unv.web.bin:run'
+        ]
+    }
+)
