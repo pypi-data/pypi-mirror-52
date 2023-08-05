472f93100428a0a85abeb5825301a39cca623f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:06 +0000

    Add package blupants/1.0.0a6/blupants-1.0.0a6-py3-none-any.whl

---
+++ packages/blupants/blupants-1.0.0a6-py3-none-any.whl/blupants/__init__.py
@@ -0,0 +1,22 @@
+import blupants.blupants_client as blupants_client
+
+global module_name
+module_name = ""
+
+def version():
+    return "1.0.0a6"
+
+
+def verbose(verbose_level):
+    return verbose_level
+
+
+def run(module):
+    global module_name
+    module_name = module
+    if module_name == "blupants_client":
+        blupants_client.run()
+
+
+def shutdown():
+    blupants_client.shutdown()
