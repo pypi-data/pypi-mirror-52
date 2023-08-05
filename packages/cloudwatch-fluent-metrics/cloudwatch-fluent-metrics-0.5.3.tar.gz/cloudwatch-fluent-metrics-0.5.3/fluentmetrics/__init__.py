d3ca0d32d004077aa45ff189312b5d75b015529c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:43 +0000

    Add package cloudwatch-fluent-metrics/0.5.3/cloudwatch-fluent-metrics-0.5.3.tar.gz

---
+++ packages/cloudwatch-fluent-metrics/cloudwatch-fluent-metrics-0.5.3.tar.gz/cloudwatch-fluent-metrics-0.5.3/fluentmetrics/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from .buffer import BufferedFluentMetric  # noqa: F401
+from .metric import FluentMetric  # noqa: F401
---
+++ packages/cloudwatch-fluent-metrics/cloudwatch-fluent-metrics-0.5.3.tar.gz/cloudwatch-fluent-metrics-0.5.3/setup.py
@@ -0,0 +1,28 @@
+import os
+import setuptools
+from distutils.core import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+  name='cloudwatch-fluent-metrics',
+  packages=setuptools.find_packages(),
+  version='0.5.3',
+  description='AWS CloudWatch Fluent Metrics',
+  long_description=read('README.md'),
+  long_description_content_type='text/markdown',
+  author='troylar',
+  author_email='troylars@amazon.com',
+  url='https://github.com/awslabs/cloudwatch-fluent-metrics',
+  download_url='https://github.com/awslabs/cloudwatch-fluent-metrics/cloudwatch-fluent-metrics-v0.1.tgz',  # noqa: E501
+  keywords=['metrics', 'logging', 'aws', 'cloudwatch'],
+  license="Apache-2.0",
+  classifiers=[
+      "Development Status :: 5 - Production/Stable",
+      "Topic :: Utilities",
+      "License :: OSI Approved :: Apache Software License",
+  ]
+)
