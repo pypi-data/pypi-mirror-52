b878b90c2c487408bc7d455b6da9c665eff4fa5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:43 +0000

    Add package libraw-cffi/0.6.0/libraw_cffi-0.6.0.tar.gz

---
+++ packages/libraw-cffi/libraw_cffi-0.6.0.tar.gz/libraw_cffi-0.6.0/libraw_cffi/tests/test_overview.py
@@ -0,0 +1,26 @@
+"""
+A reimplementation of https://www.libraw.org/docs/API-overview.html
+"""
+from unittest import TestCase
+
+try:
+    from pathlib2 import Path
+except ImportError:
+    from pathlib import Path
+
+from _raw import lib
+
+
+class TestDemonstration(TestCase):
+    def test_brief_demonstration(self):
+        data = lib.libraw_init(0)
+        self.addCleanup(lib.libraw_recycle, data)
+
+        lib.libraw_open_file(
+            data,
+            str(Path(__file__).parent / "_DSC2164.ARW"),
+        )
+        self.assertEqual((data.sizes.width, data.sizes.height), (5216, 3464))
+
+        lib.libraw_unpack(data)
+        lib.libraw_raw2image(data)
