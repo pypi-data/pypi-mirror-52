d65264ecc893a8fd07973f35069df16408e3b153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:46 +0000

    Add package bwb/1.0.1/bwb-1.0.1.tar.gz

---
+++ packages/bwb/bwb-1.0.1.tar.gz/bwb-1.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(name='bwb',
+    version="1.0.1",
+    description='bwb',
+    long_description='bwb',
+    author='bwb',
+    license='QPL.txt',
+    url='https://qotmail.com',
+    packages=setuptools.find_packages(),
+    install_requires=['base58', 'pycryptodome', 'pyotp'],
+)
