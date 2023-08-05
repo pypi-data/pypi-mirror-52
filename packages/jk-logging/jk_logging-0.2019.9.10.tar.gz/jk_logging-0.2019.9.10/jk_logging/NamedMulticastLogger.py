bc1006b6db374133068ce55e8ff32780dbeb645f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:58 +0000

    Add package jk-logging/0.2019.9.10/jk_logging-0.2019.9.10.tar.gz

---
+++ packages/jk-logging/jk_logging-0.2019.9.10.tar.gz/jk_logging-0.2019.9.10/jk_logging/NamedMulticastLogger.py
@@ -0,0 +1,107 @@
+﻿#!/usr/bin/env python3
+
+
+
+import os
+import time
+import traceback
+import sys
+import abc
+
+import sh
+
+from .EnumLogLevel import *
+from .AbstractLogger import *
+
+
+
+
+
+#
+# This logger will broadcast log messages to additional loggers.
+#
+class NamedMulticastLogger(AbstractLogger):
+
+
+
+	def __init__(self, loggerMap = None, idCounter = None, indentationLevel = 0, parentLogEntryID = 0):
+		super().__init__(idCounter)
+		self._indentationLevel = indentationLevel
+		self._parentLogEntryID = parentLogEntryID
+
+		if loggerMap is not None:
+			assert isinstance(loggerMap, dict)
+			self.__loggerMap = loggerMap
+		else:
+			self.__loggerMap = {}
+	#
+
+
+
+	@staticmethod
+	def create(**kwargs):
+		return NamedMulticastLogger(loggerMap = kwargs)
+	#
+
+
+
+	def addLogger(self, loggerName, logger):
+		assert isinstance(loggerName, str)
+		assert isinstance(logger, AbstractLogger)
+		if self.__loggerMap.get(loggerName, None) is not None:
+			del self.__loggerMap[loggerName]
+		self.__loggerMap[loggerName] = logger
+	#
+
+
+
+	def removeLogger(self, loggerName):
+		assert isinstance(loggerName, str)
+		if self.__loggerMap.get(loggerName, None) is not None:
+			del self.__loggerMap[loggerName]
+	#
+
+
+
+	def removeAllLoggers(self):
+		self.__loggerMap = {}
+	#
+
+
+
+	def _logi(self, logEntryStruct, bNeedsIndentationLevelAdaption):
+		for logger in self.__loggerMap.values():
+			logger._logi(logEntryStruct, True)
+	#
+
+
+
+	def _descend(self, logEntryStruct):
+		nextID = logEntryStruct[1]
+		newMap = {}
+		for loggerName in self.__loggerMap:
+			logger = self.__loggerMap[loggerName]
+			newMap[loggerName] = logger._descend(logEntryStruct)
+		return NamedMulticastLogger(newMap, self._idCounter, self._indentationLevel + 1, nextID)
+	#
+
+
+
+	def clear(self):
+		for logger in self.__loggerMap.values():
+			logger.clear()
+	#
+
+
+
+	def close(self):
+		for logger in self.__loggerMap.values():
+			logger.close()
+	#
+
+
+#
+
+
+
+
---
+++ packages/jk-logging/jk_logging-0.2019.9.10.tar.gz/jk_logging-0.2019.9.10/setup.py
@@ -0,0 +1,35 @@
+﻿from setuptools import setup
+
+
+def readme():
+	with open("../README.md") as f:
+		return f.read()
+
+
+setup(name="jk_logging",
+	version="0.2019.9.10",
+	description="This is a logging framework.",
+	author="Jürgen Knauth",
+	author_email="pubsrc@binary-overflow.de",
+	license="Apache 2.0",
+	url="https://github.com/jkpubsrc/python-module-jk-logging",
+	download_url="https://github.com/jkpubsrc/python-module-jk-logging/tarball/0.2019.9.10",
+	keywords=[
+		"debugging",
+		"logging"
+		],
+	packages=[
+		"jk_logging"
+		],
+	install_requires=[
+	],
+	include_package_data=True,
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Programming Language :: Python :: 3.5",
+		"License :: OSI Approved :: Apache Software License"
+	],
+	long_description=readme(),
+	long_description_content_type="text/markdown",
+	zip_safe=False)
+
