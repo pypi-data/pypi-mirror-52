d02fb4d99a76bb61b2c663f50a935794e03490fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:50 +0000

    Add package spiketoolkit/0.4.4/spiketoolkit-0.4.4.tar.gz

---
+++ packages/spiketoolkit/spiketoolkit-0.4.4.tar.gz/spiketoolkit-0.4.4/spiketoolkit/tests/test_curation.py
@@ -0,0 +1,8 @@
+import spikeextractors as se
+from numpy.testing import assert_array_equal
+from spiketoolkit.curation import threshold_snr, threshold_firing_rate, threshold_isi_violations, threshold_num_spikes, \
+    threshold_presence_ratio, CurationSortingExtractor
+
+
+def test_thresh_fr():
+    pass
