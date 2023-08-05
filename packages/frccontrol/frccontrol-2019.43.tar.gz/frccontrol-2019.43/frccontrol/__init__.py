dcc028be4ff6fc7a1568299a2bd8c79d11669497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:44 +0000

    Add package frccontrol/2019.43/frccontrol-2019.43.tar.gz

---
+++ packages/frccontrol/frccontrol-2019.43.tar.gz/frccontrol-2019.43/frccontrol/__init__.py
@@ -0,0 +1,10 @@
+"""The WPILib state-space control library."""
+
+from frccontrol import models
+from frccontrol.ctrlutil import *
+from frccontrol.kalmd import *
+from frccontrol.lqr import *
+from frccontrol.profiles import *
+from frccontrol.runge_kutta import *
+from frccontrol.system import *
+from frccontrol.system_writer import *
