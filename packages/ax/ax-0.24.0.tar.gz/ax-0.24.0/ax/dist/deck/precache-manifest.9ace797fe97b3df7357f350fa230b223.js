183056353d540de1d8060c4173375c54cd49ec97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:03 +0000

    Add package ax/0.24.0/ax-0.24.0.tar.gz

---
+++ packages/ax/ax-0.24.0.tar.gz/ax-0.24.0/ax/dist/deck/precache-manifest.9ace797fe97b3df7357f350fa230b223.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "72869256053439078751",
+    "url": "/deck/static/js/app.0765a504.js"
+  },
+  {
+    "revision": "7009cf92910b0dd61b49",
+    "url": "/deck/static/js/chunk-vendors.5de009e4.js"
+  },
+  {
+    "revision": "77f5bd0593089704adf980d89e925bc6",
+    "url": "/deck/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/deck/robots.txt"
+  }
+];
