3aadedc96a6ec67aa487e11a67b1a3b398a53399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:58 +0000

    Add package cs18-sidecar/0.0.2.4137/cs18-sidecar-0.0.2.4137.tar.gz

---
+++ packages/cs18-sidecar/cs18-sidecar-0.0.2.4137.tar.gz/cs18-sidecar-0.0.2.4137/sidecar/app_instance_service.py
@@ -0,0 +1,48 @@
+from abc import ABCMeta, abstractmethod
+from logging import Logger
+from typing import List
+
+from sidecar.app_instance_identifier import AppInstanceIdentifier
+
+
+class StaleAppInstanceException(Exception):
+    pass
+
+
+class IAppInstanceService:
+    __metaclass__ = ABCMeta
+
+    def __init__(self, logger: Logger):
+        self._logger = logger
+
+    @abstractmethod
+    def update_status_if_not_stale(self, app_instance_identifier: AppInstanceIdentifier, status: str):
+        raise NotImplementedError
+
+    @abstractmethod
+    def update_artifacts_status(self, app_instance_identifier: AppInstanceIdentifier, status: str):
+        raise NotImplementedError
+
+    @abstractmethod
+    def update_configuration_status(self, app_instance_identifier: AppInstanceIdentifier, status: str):
+        raise NotImplementedError
+
+    @abstractmethod
+    def check_which_exist(self, identifiers: List[AppInstanceIdentifier]) -> List[AppInstanceIdentifier]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_public_address(self, app_instance_identifier: AppInstanceIdentifier) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def update_deployment_outputs(self, app_instance_identifier: AppInstanceIdentifier, outputs: {}):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_deployment_outputs(self, app_name: str) -> {}:
+        raise NotImplementedError
+
+    @abstractmethod
+    def is_qualiy_off(self) -> bool:
+        raise NotImplementedError
