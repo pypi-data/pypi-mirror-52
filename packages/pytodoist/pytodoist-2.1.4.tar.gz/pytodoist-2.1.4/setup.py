4e0119e352ba0cc79e7baab878b6b9e1e17d8292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:13 +0000

    Add package pytodoist/2.1.4/pytodoist-2.1.4.tar.gz

---
+++ packages/pytodoist/pytodoist-2.1.4.tar.gz/pytodoist-2.1.4/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+"""Setup script for the pytodoist package."""
+import pytodoist
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='pytodoist',
+      version=pytodoist.__version__,
+      license='MIT',
+      description='A python wrapper for the Todoist API.',
+      long_description=open('README.md').read(),
+      author='Gary Blackwood',
+      author_email='gary@garyblackwood.co.uk',
+      url='http://www.github.com/Garee/pytodoist',
+      packages=['pytodoist'],
+      install_requires=['requests'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.0',
+          'Programming Language :: Python :: 3.1',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7'
+      ],)
