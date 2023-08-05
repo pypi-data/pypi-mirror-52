253bc89669aed0abeb28382a0dae6652eddf10aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:50 +0000

    Add package flynt/0.32/flynt-0.32.tar.gz

---
+++ packages/flynt/flynt-0.32.tar.gz/flynt-0.32/setup.py
@@ -0,0 +1,56 @@
+import config
+from pathlib import Path
+import re
+from setuptools import setup
+
+
+config.add_src_to_path()
+
+with open("src/flynt/__init__.py") as f:
+    version = re.search('__version__ = "(.*?)"', f.read()).group(1)
+
+
+BASE_DIR = Path(__file__).parent
+
+
+# forked from black's setup.py
+def get_long_description():
+    """Load README for long description"""
+    readme_md = BASE_DIR / "README.md"
+    with open(readme_md, encoding="utf8") as readme_f:
+        return readme_f.read()
+
+
+def get_requirements():
+    with open("requirements.txt") as fp:
+        return fp.read()
+
+
+setup(
+    name="flynt",
+    packages=["flynt", "flynt.transform", "flynt.lexer"],
+    package_dir={"": "src"},
+    version=version,
+    description="CLI tool to convert a python project's %-formatted strings "
+    "to f-strings.",
+    author="Ilya Kamenshchikov",
+    keywords=["utility", "strings"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
+    license="MIT",
+    # long_description=get_long_description(),
+    long_description="CLI tool to convert a python project's .format(...) "
+    "and %-formatted "
+    "strings to f-strings.",
+    install_requires=get_requirements(),
+    python_requires=">=3.6",
+    entry_points={"console_scripts": ["flynt=flynt:main"]},
+    url="https://github.com/ikamensh/flynt",
+)
