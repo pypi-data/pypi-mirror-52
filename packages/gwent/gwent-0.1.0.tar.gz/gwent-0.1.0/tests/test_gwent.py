168b6a6fa6daf372f74cbab226e344802f71891f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:33 +0000

    Add package gwent/0.1.0/gwent-0.1.0.tar.gz

---
+++ packages/gwent/gwent-0.1.0.tar.gz/gwent-0.1.0/tests/test_gwent.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `gwent` package."""
+
+
+import pytest
+
+from gwent import binary
+from gwent import detector
+from gwent import snr
+from gwent import snrplot
+from gwent import utils
+from gwent import waveform
+
+
+class TestGwent():
+    """Tests for `gwent` package."""
