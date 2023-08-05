0e203a1c49a92bcfa68227cfa0ce2ffc94f61004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:08 +0000

    Add package learn2learn/0.0.4/learn2learn-0.0.4.tar.gz

---
+++ packages/learn2learn/learn2learn-0.0.4.tar.gz/learn2learn-0.0.4/learn2learn/vision/datasets/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+
+**Description**
+
+Some datasets commonly used in meta-learning vision tasks.
+"""
+
+from .full_omniglot import FullOmniglot
+from .mini_imagenet import MiniImagenet
