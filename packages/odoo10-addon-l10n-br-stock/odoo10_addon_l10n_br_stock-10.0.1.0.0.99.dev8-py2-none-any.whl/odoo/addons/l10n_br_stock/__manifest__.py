063e93782ee5424bc0bf1a7a939ed7c2708d4ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:50 +0000

    Add package odoo10-addon-l10n-br-stock/10.0.1.0.0.99.dev8/odoo10_addon_l10n_br_stock-10.0.1.0.0.99.dev8-py2-none-any.whl

---
+++ packages/odoo10-addon-l10n-br-stock/odoo10_addon_l10n_br_stock-10.0.1.0.0.99.dev8-py2-none-any.whl/odoo/addons/l10n_br_stock/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009  Renato Lima - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Brazilian Localization Warehouse',
+    'category': 'Localisation',
+    'license': 'AGPL-3',
+    'author': 'Akretion, Odoo Community Association (OCA)',
+    'website': 'http://odoo-brasil.org',
+    'version': '10.0.1.0.0',
+    'depends': [
+        'stock',
+        'l10n_br_base',
+    ],
+    'data': [
+        'data/l10n_br_stock_data.xml',
+        'views/stock_view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
