1f4cf6ff703f33eab604c734c58b731323009f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:44 +0000

    Add package sparti/0.0.3/sparti-0.0.3.tar.gz

---
+++ packages/sparti/sparti-0.0.3.tar.gz/sparti-0.0.3/sparti/__init__.py
@@ -0,0 +1,11 @@
+name = "sparti"
+
+
+import BSPF 
+
+
+__author__ = 'Xuhui Fan'
+__email__ = 'xhfan.ml@gmail.com'
+
+# make sure __version_ is on the last non-empty line (read by setup.py)
+#__version__ = '0.0.2'
