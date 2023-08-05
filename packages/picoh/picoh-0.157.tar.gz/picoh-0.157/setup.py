a806ab56e0236d7c3c8aafbf53c04fa06042a31a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:49 +0000

    Add package picoh/0.157/picoh-0.157.tar.gz

---
+++ packages/picoh/picoh-0.157.tar.gz/picoh-0.157/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+import platform
+
+if platform.system() == "Darwin":
+    includes =['pyserial','lxml','playsound','pyobjc','comtypes',]
+
+if platform.system() == "Windows":
+    includes =['pyserial','lxml','playsound','comtypes',]
+
+if platform.system() == "Linux":
+    includes =['pyserial','playsound']
+
+setup(
+    name = 'picoh',
+    packages = ['picoh'],
+    package_data={'': ['picohdefinitions.omd','Silence1.wav','picohspeech.wav','PicohSpeech.csv','ohbot.obe','phonemes.txt']},
+    include_package_data=True,
+    version = '0.157',
+    description = 'Python library for controlling Picoh Robot',
+    author = 'ohbot',
+    author_email = 'info@ohbot.co.uk',
+    url = 'https://github.com/ohbot/picoh',
+    download_url = 'https://github.com/ohbot/ohbotMac-python/archive/1.15.tar.gz',
+    keywords = ['ohbot', 'robot','picoh'],
+    classifiers = [],
+    install_requires= includes,
+)
