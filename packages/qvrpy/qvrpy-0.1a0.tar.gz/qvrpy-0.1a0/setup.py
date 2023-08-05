1085ae8fed18ef3cddcbfc02cfc5a38c502bf05f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:25 +0000

    Add package qvrpy/0.1a0/qvrpy-0.1a0.tar.gz

---
+++ packages/qvrpy/qvrpy-0.1a0.tar.gz/qvrpy-0.1a0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='qvrpy',  
+    version='v0.1-alpha',
+    author="Grant Patterson",
+    author_email="patterson.grant@gmail.com",
+    description="An abstraction of the QVR Pro API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=['QVR Pro', 'QVRPro', 'QNAP', 'IoT', 'Surveillance'],
+    url="https://github.com/DasUberLeo/qvrpy",
+    download_url = 'https://github.com/DasUberLeo/qvrpy/archive/v0.1-alpha.tar.gz',
+    python_requires='>=3.6',
+    install_requires=['requests>=2.13.0'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Home Automation",
+        "Topic :: System :: Hardware"
+    ],
+)
