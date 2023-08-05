39bc8d2f018497905baaec15d7857a35a6edfbe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:50 +0000

    Add package compound-data/0.1/compound_data-0.1.tar.gz

---
+++ packages/compound-data/compound_data-0.1.tar.gz/compound_data-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='compound_data',
+      packages=['compound_data'],
+      version='0.1',
+      description='New Data Type - Pair',
+      url='https://github.com/xaliion/python-type',
+      author='Xalion',
+      license='MIT',
+      )
