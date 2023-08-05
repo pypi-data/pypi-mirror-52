a4c99df543816f6c054da25a6f437cca766d789f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:15 +0000

    Add package frost/0.3.1/frost-0.3.1-py3-none-any.whl

---
+++ packages/frost/frost-0.3.1-py3-none-any.whl/aws/rds/test_rds_db_snapshot_encrypted.py
@@ -0,0 +1,14 @@
+import pytest
+
+from aws.rds.resources import rds_db_snapshots
+from aws.rds.helpers import is_rds_db_snapshot_encrypted
+
+
+@pytest.mark.rds
+@pytest.mark.parametrize(
+    "rds_db_snapshot",
+    rds_db_snapshots(),
+    ids=lambda snapshot: snapshot["DBSnapshotArn"],
+)
+def test_rds_db_snapshot_encrypted(rds_db_snapshot):
+    assert is_rds_db_snapshot_encrypted(rds_db_snapshot)
