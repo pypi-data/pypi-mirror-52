c58e5f39bcc2c11edeecc6b7cb94196851d87a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:38 +0000

    Add package CodeComb/0.1.7/CodeComb-0.1.7.tar.gz

---
+++ packages/CodeComb/CodeComb-0.1.7.tar.gz/CodeComb-0.1.7/setup.py
@@ -0,0 +1,17 @@
+from pip._internal.req import parse_requirements
+from setuptools import setup
+
+def load_requirements(fname):
+    reqs = parse_requirements(fname, session="test")
+    return [str(ir.req) for ir in reqs]
+
+setup(name="CodeComb", version = '0.1.7',
+	install_requires=load_requirements("requirements.txt"),
+    python_requires='>=3.6.*',
+	packages=['CodeComb_Core'],
+	entry_points={
+		'console_scripts': [ 
+			'codecomb = CodeComb_Core.codecomb:run'
+			]
+		}
+	)
