68f5efde60e299c9ce22cbd0ccb04738054fef16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:20 +0000

    Add package catalyst/19.9.2/catalyst-19.9.2.tar.gz

---
+++ packages/catalyst/catalyst-19.9.2.tar.gz/catalyst-19.9.2/catalyst/dl/runner/supervised.py
@@ -0,0 +1,188 @@
+from typing import Any, Mapping, Dict, List, Union
+from collections import OrderedDict  # noqa F401
+
+from torch import nn
+from torch.utils.data import DataLoader  # noqa F401
+
+from catalyst.dl.core import Runner, Callback
+from catalyst.dl.experiment import SupervisedExperiment
+from catalyst.dl.callbacks import InferCallback, CheckpointCallback
+from catalyst.dl.utils.torch import _Model, _Criterion, _Optimizer, _Scheduler
+
+
+class SupervisedRunner(Runner):
+    """
+    Runner for experiments with supervised model
+    """
+    _default_experiment = SupervisedExperiment
+
+    def __init__(
+        self,
+        model: nn.Module = None,
+        device=None,
+        input_key: str = "features",
+        output_key: str = "logits",
+        input_target_key: str = "targets",
+    ):
+        """
+        @TODO update docs
+        Args:
+            input_key: Key in batch dict mapping to model input
+            output_key: Key in output dict model output will be stored under
+        """
+        super().__init__(model=model, device=device)
+        self.input_key = input_key
+        self.output_key = output_key
+        self.target_key = input_target_key
+
+        if isinstance(self.input_key, str):
+            self._process_input = self._process_input_str
+        elif isinstance(self.input_key, (list, tuple)):
+            self._process_input = self._process_input_list
+        else:
+            self._process_input = self._process_input_none
+
+        if isinstance(output_key, str):
+            self._process_output = self._process_output_str
+        elif isinstance(output_key, (list, tuple)):
+            self._process_output = self._process_output_list
+        else:
+            self._process_output = self._process_output_none
+
+    def _batch2device(self, batch: Mapping[str, Any], device):
+        if isinstance(batch, (tuple, list)):
+            assert len(batch) == 2
+            batch = {self.input_key: batch[0], self.target_key: batch[1]}
+        batch = super()._batch2device(batch, device)
+        return batch
+
+    def _process_input_str(self, batch: Mapping[str, Any]):
+        output = self.model(batch[self.input_key])
+        return output
+
+    def _process_input_list(self, batch: Mapping[str, Any]):
+        input = dict((key, batch[key]) for key in self.input_key)
+        output = self.model(**input)
+        return output
+
+    def _process_input_none(self, batch: Mapping[str, Any]):
+        output = self.model(**batch)
+        return output
+
+    def _process_output_str(self, output: Mapping[str, Any]):
+        output = {self.output_key: output}
+        return output
+
+    def _process_output_list(self, output: Mapping[str, Any]):
+        output = dict(
+            (key, value) for key, value in zip(self.output_key, output)
+        )
+        return output
+
+    def _process_output_none(self, output: Mapping[str, Any]):
+        return output
+
+    def predict_batch(self, batch: Mapping[str, Any]):
+        output = self._process_input(batch)
+        output = self._process_output(output)
+        return output
+
+    def train(
+        self,
+        model: _Model,
+        criterion: _Criterion,
+        optimizer: _Optimizer,
+        loaders: "OrderedDict[str, DataLoader]",
+        logdir: str,
+        callbacks: "Union[List[Callback], OrderedDict[str, Callback]]" = None,
+        scheduler: _Scheduler = None,
+        num_epochs: int = 1,
+        valid_loader: str = "valid",
+        main_metric: str = "loss",
+        minimize_metric: bool = True,
+        verbose: bool = False,
+        state_kwargs: Dict = None,
+        checkpoint_data: Dict = None,
+        fp16: Union[Dict, bool] = None,
+        monitoring_params: Dict = None,
+        check: bool = False,
+    ):
+        if isinstance(fp16, bool) and fp16:
+            fp16 = {"opt_level": "O1"}
+        experiment = self._default_experiment(
+            stage="train",
+            model=model,
+            loaders=loaders,
+            callbacks=callbacks,
+            logdir=logdir,
+            criterion=criterion,
+            optimizer=optimizer,
+            scheduler=scheduler,
+            num_epochs=num_epochs,
+            valid_loader=valid_loader,
+            main_metric=main_metric,
+            minimize_metric=minimize_metric,
+            verbose=verbose,
+            state_kwargs=state_kwargs,
+            checkpoint_data=checkpoint_data,
+            distributed_params=fp16,
+            monitoring_params=monitoring_params
+        )
+        self.run_experiment(experiment, check=check)
+
+    def infer(
+        self,
+        model: _Model,
+        loaders: "OrderedDict[str, DataLoader]",
+        callbacks: "Union[List[Callback], OrderedDict[str, Callback]]" = None,
+        verbose: bool = False,
+        state_kwargs: Dict = None,
+        fp16: Union[Dict, bool] = None,
+        check: bool = False,
+    ):
+        if isinstance(fp16, bool) and fp16:
+            fp16 = {"opt_level": "O1"}
+        experiment = self._default_experiment(
+            stage="infer",
+            model=model,
+            loaders=loaders,
+            callbacks=callbacks,
+            verbose=verbose,
+            state_kwargs=state_kwargs,
+            distributed_params=fp16
+        )
+        self.run_experiment(experiment, check=check)
+
+    def predict_loader(
+        self,
+        loader: DataLoader,
+        resume: str = None,
+        verbose: bool = False,
+        state_kwargs: Dict = None,
+        fp16: Union[Dict, bool] = None,
+        check: bool = False,
+    ):
+        loaders = OrderedDict([("infer", loader)])
+
+        callbacks = OrderedDict([("inference", InferCallback())])
+        if resume is not None:
+            callbacks["loader"] = CheckpointCallback(resume=resume)
+
+        self.infer(
+            model=self.model,
+            loaders=loaders,
+            callbacks=callbacks,
+            verbose=verbose,
+            state_kwargs=state_kwargs,
+            fp16=fp16,
+            check=check
+        )
+
+        output = callbacks["inference"].predictions
+        if isinstance(self.output_key, str):
+            output = output[self.output_key]
+
+        return output
+
+
+__all__ = ["SupervisedRunner"]
---
+++ packages/catalyst/catalyst-19.9.2.tar.gz/catalyst-19.9.2/catalyst/rl/db/mongo.py
@@ -0,0 +1,144 @@
+import datetime
+
+import pymongo
+import safitty
+
+from catalyst.rl import utils
+from catalyst.rl.core import DBSpec
+
+
+class MongoDB(DBSpec):
+    def __init__(
+        self, host="127.0.0.1", port=12000, prefix=None, sync_epoch=False
+    ):
+        self._server = pymongo.MongoClient(host=host, port=port)
+        self._prefix = "" if prefix is None else prefix
+
+        self._shared_db = self._server["shared"]
+        self._agent_db = self._server[f"agent_{self._prefix}"]
+
+        self._trajectory_collection = self._shared_db["trajectories"]
+        self._raw_trajectory_collection = self._shared_db["raw_trajectories"]
+        self._checkpoints_collection = self._agent_db["checkpoints"]
+        self._messages_collection = self._agent_db["messages"]
+        self._last_datetime = datetime.datetime.min
+
+        self._epoch = 0
+        self._sync_epoch = sync_epoch
+
+    def _set_flag(self, key, value):
+        self._messages_collection.replace_one(
+            {"key": key},
+            {"key": key, "value": value},
+            upsert=True
+        )
+
+    def _get_flag(self, key, default=None):
+        flag_obj = self._messages_collection.find_one({"key": {"$eq": key}})
+        flag = safitty.get(flag_obj, "value", default=default)
+        return flag
+
+    @property
+    def training_enabled(self) -> bool:
+        flag = self._get_flag("training_flag", 1)  # enabled by default
+        flag = int(flag) == int(1)
+        return flag
+
+    @property
+    def sampling_enabled(self) -> bool:
+        flag = self._get_flag("sampling_flag", -1)  # disabled by default
+        flag = int(flag) == int(1)
+        return flag
+
+    @property
+    def epoch(self) -> int:
+        return self._epoch
+
+    @property
+    def num_trajectories(self) -> int:
+        num_trajectories = self._trajectory_collection.count() - 1
+        return num_trajectories
+
+    def push_message(self, message: DBSpec.Message):
+        if message == DBSpec.Message.ENABLE_SAMPLING:
+            self._set_flag("sampling_flag", 1)
+        elif message == DBSpec.Message.DISABLE_SAMPLING:
+            self._set_flag("sampling_flag", 0)
+        elif message == DBSpec.Message.DISABLE_TRAINING:
+            self._set_flag("sampling_flag", 0)
+            self._set_flag("training_flag", 0)
+        elif message == DBSpec.Message.ENABLE_TRAINING:
+            self._set_flag("training_flag", 1)
+        else:
+            raise NotImplementedError("unknown message", message)
+
+    def put_trajectory(self, trajectory, raw=False):
+        trajectory = utils.structed2dict_trajectory(trajectory)
+        trajectory = utils.pack(trajectory)
+        collection = self._raw_trajectory_collection if raw \
+            else self._trajectory_collection
+
+        collection.insert_one(
+            {
+                "trajectory": trajectory,
+                "date": datetime.datetime.utcnow(),
+                "epoch": self._epoch
+            }
+        )
+
+    def get_trajectory(self, index=None):
+        assert index is None
+
+        trajectory_obj = self._trajectory_collection.find_one(
+            {"date": {
+                "$gt": self._last_datetime
+            }}
+        )
+        if trajectory_obj is not None:
+            self._last_datetime = trajectory_obj["date"]
+
+            trajectory, trajectory_epoch = \
+                utils.unpack(
+                    trajectory_obj["trajectory"]), trajectory_obj["epoch"]
+            if self._sync_epoch and self._epoch != trajectory_epoch:
+                trajectory = None
+            else:
+                trajectory = utils.dict2structed_trajectory(trajectory)
+        else:
+            trajectory = None
+
+        return trajectory
+
+    def del_trajectory(self):
+        self._trajectory_collection.drop()
+
+    def put_checkpoint(self, checkpoint, epoch):
+        self._epoch = epoch
+
+        checkpoint = utils.pack(checkpoint)
+        self._checkpoints_collection.replace_one(
+            {"prefix": "checkpoint"}, {
+                "checkpoint": checkpoint,
+                "prefix": "checkpoint",
+                "epoch": self._epoch
+            },
+            upsert=True
+        )
+
+    def get_checkpoint(self):
+        checkpoint_obj = self._checkpoints_collection.find_one(
+            {"prefix": "checkpoint"}
+        )
+        if checkpoint_obj is not None:
+            checkpoint = checkpoint_obj.get("checkpoint")
+            self._epoch = checkpoint_obj["epoch"]
+            checkpoint = utils.unpack(checkpoint)
+        else:
+            checkpoint = None
+        return checkpoint
+
+    def del_checkpoint(self):
+        self._checkpoints_collection.delete_one({"prefix": "checkpoint"})
+
+
+__all__ = ["MongoDB"]
