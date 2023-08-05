9320613b4cac0b43630748ecacb2acb9469b241b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:50 +0000

    Add package peogayo/0.0.0/peogayo-0.0.0.tar.gz

---
+++ packages/peogayo/peogayo-0.0.0.tar.gz/peogayo-0.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+
+setup(name='peogayo',
+      verison='0.0.0',
+      license='MIT',
+      author='Chanwoong Kim',
+      author_email='me@chanwoong.kim',
+      url='https://github.com/kexplo/peogayo',
+      description='',
+      packages=find_packages(),
+      classifiers=[
+          'Development Status :: 1 - Planning',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.7',
+      ],
+      )
