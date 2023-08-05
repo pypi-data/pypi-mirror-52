38d54c4f7066be61167ac376f0c243266d34febb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:26 +0000

    Add package marktest/1.0.3/marktest-1.0.3-py3-none-any.whl

---
+++ packages/marktest/marktest-1.0.3-py3-none-any.whl/marktest/helloworld.py
@@ -0,0 +1,11 @@
+import os
+
+def helloworld ():
+	print ("Hello World")
+	print ("Dit betekent dus dat het werkt")
+	
+#input = input("Do you want to see something?: ")
+create = 'ping 127.0.0.1'
+print ("It is working")
+os.system(create)
+print ("Still going strong")
