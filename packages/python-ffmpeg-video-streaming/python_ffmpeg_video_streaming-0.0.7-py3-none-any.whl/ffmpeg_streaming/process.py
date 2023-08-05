f4d5dc3b77e4e5839fa4245d70d6c14424de079c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:39 +0000

    Add package python-ffmpeg-video-streaming/0.0.7/python_ffmpeg_video_streaming-0.0.7-py3-none-any.whl

---
+++ packages/python-ffmpeg-video-streaming/python_ffmpeg_video_streaming-0.0.7-py3-none-any.whl/ffmpeg_streaming/process.py
@@ -0,0 +1,77 @@
+import subprocess
+import ffmpeg_streaming
+from ffmpeg_streaming import FFProbe
+from ffmpeg_streaming.progress import progress
+from .params import (get_hls_parm, get_dash_parm)
+
+
+def build_command(cmd, media_obj):
+    if type(cmd) != list:
+        cmd = [cmd]
+
+    cmd += ['-y', '-i', media_obj.filename]
+    cmd += ['-c:v', media_obj.format]
+
+    if isinstance(media_obj, ffmpeg_streaming.HLS):
+        cmd += get_hls_parm(media_obj)
+    elif isinstance(media_obj, ffmpeg_streaming.DASH):
+        cmd += get_dash_parm(media_obj)
+
+    return " ".join(cmd)
+
+
+def run_async(media, cmd='ffmpeg', pipe_stdin=False, pipe_stdout=False, pipe_stderr=False, universal_newlines=False):
+    commands = build_command(cmd, media)
+    stdin_stream = subprocess.PIPE if pipe_stdin else None
+    stdout_stream = subprocess.PIPE if pipe_stdout else None
+    stderr_stream = subprocess.STDOUT if pipe_stderr else None
+    return subprocess.Popen(commands, stdout=stdout_stream, stderr=stderr_stream, stdin=stdin_stream
+                            , universal_newlines=universal_newlines)
+
+
+def get_total_sec(filename, cmd):
+    ffprobe = FFProbe(filename, cmd)
+    media_format = ffprobe.format()
+    all_media = ffprobe.all()
+    try:
+        return int(float(media_format['duration'])), all_media
+    except KeyError:
+        raise KeyError("It is not possible to get duration")
+
+
+def show_progress(media, callable_progress, cmd, ffprobe_cmd, input):
+    process = run_async(
+        media,
+        cmd,
+        pipe_stdin=input is not None,
+        pipe_stdout=True,
+        pipe_stderr=True,
+        universal_newlines=True
+    )
+    total_sec, all_media = get_total_sec(media.filename, ffprobe_cmd)
+    current_percentage = 0
+    for line in process.stdout:
+        percentage = progress(line, total_sec)
+        if percentage is not None and current_percentage != percentage:
+            current_percentage = percentage
+            callable_progress(percentage, line, all_media)
+
+
+def run(media, callable_progress=None, cmd='ffmpeg', ffprobe_cmd='ffprobe', capture_stdout=False, capture_stderr=False,
+        input=None, timeout=None):
+
+    if callable(callable_progress):
+        return show_progress(media, callable_progress, cmd, ffprobe_cmd, input)
+
+    process = run_async(
+        media,
+        cmd,
+        pipe_stdin=input is not None,
+        pipe_stdout=capture_stdout,
+        pipe_stderr=capture_stderr,
+    )
+    out, err = process.communicate(input, timeout=timeout)
+    retcode = process.poll()
+    if retcode:
+        raise RuntimeError('ffmpeg', out, err)
+    return out, err
---
+++ packages/python-ffmpeg-video-streaming/python_ffmpeg_video_streaming-0.0.7-py3-none-any.whl/ffmpeg_streaming/rep.py
@@ -0,0 +1,20 @@
+class Representation:
+    def __init__(self, **kwargs):
+        self.width = kwargs.pop('width', None)
+        self.height = kwargs.pop('height', None)
+        self.kilo_bitrate = kwargs.pop('kilo_bitrate', None)
+
+    def size(self):
+        if self.width is None or self.height is None:
+            raise ValueError("You must set the width and the height of video")
+        return str(self.width) + "x" + str(self.height)
+
+    def bit_rate(self):
+        if self.kilo_bitrate is None:
+            raise ValueError("You must set the bitrate of video")
+        return str(self.kilo_bitrate) + "k"
+
+
+__all__ = [
+    'Representation'
+]
