025661afe33791f439e7b4a8b714332b0fc022eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:55 +0000

    Add package dump2polarion/0.42.0/dump2polarion-0.42.0-py2.py3-none-any.whl

---
+++ packages/dump2polarion/dump2polarion-0.42.0-py2.py3-none-any.whl/dump2polarion/exceptions.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Exceptions.
+"""
+
+
+class Dump2PolarionException(Exception):
+    """dump2polarion exception."""
+
+
+class NothingToDoException(Dump2PolarionException):
+    """nothing to do exception."""
