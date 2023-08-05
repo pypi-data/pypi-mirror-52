4dffd395139e60d38fc9874f52abfb845873b765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:52 +0000

    Add package argvanalysis/1.0.1/argvanalysis-1.0.1.tar.gz

---
+++ packages/argvanalysis/argvanalysis-1.0.1.tar.gz/argvanalysis-1.0.1/argvanalysis/readargv.py
@@ -0,0 +1,26 @@
+import sys
+class Argvreader:
+    def __init__(self):
+        self.argv = sys.argv
+    
+    @staticmethod
+    def represents_int(candidate):
+        if candidate[0] in ('-', '+'):
+            return candidate[1:].isdigit()
+        return candidate.isdigit()
+
+    def get_argv_pairs(self, keylist=None):
+        char_num_dict = {}
+        if keylist is None:
+            keylist = []
+            for i, word in enumerate(self.argv[1: -1]):
+                if word[0] == '-' and len(word) == 2 and self.represents_int(self.argv[i+2]):
+                    keylist.append(word)
+        
+        for i, word in enumerate(self.argv[1: -1]):
+            if word in keylist and self.represents_int(self.argv[i+2]):
+                char_num_dict[word] = self.argv[i+2]
+        
+        return char_num_dict
+
+
