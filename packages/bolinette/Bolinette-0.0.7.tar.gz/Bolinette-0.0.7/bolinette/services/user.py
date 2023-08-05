2cd913d1ba219460bf4e2294fddf79804125356d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:58 +0000

    Add package bolinette/0.0.7/Bolinette-0.0.7.tar.gz

---
+++ packages/bolinette/Bolinette-0.0.7.tar.gz/Bolinette-0.0.7/bolinette/services/user.py
@@ -0,0 +1,24 @@
+from bolinette import bcrypt
+from bolinette.models import User
+from bolinette.services import BaseService
+
+
+class UserService(BaseService):
+    def __init__(self):
+        super().__init__(User, 'user')
+
+    def get_by_username(self, username):
+        return self.get_first_by('username', username)
+
+    def get_by_email(self, email):
+        return self.get_first_by('email', email)
+
+    def check_password(self, user, password):
+        return bcrypt.check_password_hash(user.password, password)
+
+    def create(self, **kwargs):
+        kwargs['password'] = bcrypt.generate_password_hash(kwargs['password'])
+        return super().create(**kwargs)
+
+
+user_service = UserService()
