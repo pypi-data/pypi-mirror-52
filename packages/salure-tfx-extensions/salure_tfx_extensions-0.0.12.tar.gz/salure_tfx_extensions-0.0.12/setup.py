0fa99797d2756d0710e2503f521d2bba31f9d966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:46 +0000

    Add package salure-tfx-extensions/0.0.12/salure_tfx_extensions-0.0.12.tar.gz

---
+++ packages/salure-tfx-extensions/salure_tfx_extensions-0.0.12.tar.gz/salure_tfx_extensions-0.0.12/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='salure_tfx_extensions',
+    version='0.0.12',
+    description='TFX components, helper functions and pipeline definition, developed by Salure',
+    author='Salure',
+    author_email='bi@saslure.nl',
+    license='Salure License',
+    packages=find_packages(),
+    package_data={'salure_tfx_extensions': ['proto/*.proto']},
+    install_requires=[
+        'tfx>=0.14.0rc1',
+        'tensorflow==1.14.0',
+        'PyMySQL>=0.9.3,<10'
+    ],
+    zip_safe=False
+)
