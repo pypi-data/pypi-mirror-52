1e0289bcceecf7bcf365b1d10f38f791e3f27cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:41 +0000

    Add package semver_range/0.0.3/semver_range-0.0.3.tar.gz

---
+++ packages/semver_range/semver_range-0.0.3.tar.gz/semver_range-0.0.3/setup.py
@@ -0,0 +1,35 @@
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+root_path = os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir))
+with open(os.path.join(root_path, 'README.rst')) as readme:
+    README = readme.read()
+
+setup(
+    name='semver_range',
+    version='0.0.3',
+    author='Igor Ushkarev',
+    url='https://github.com/ushkarev/semver_range',
+    py_modules=['semver_range'],
+    license='MIT',
+    description='Python package that mimics npm’s “semver” package',
+    long_description=README,
+    keywords='semver,semantic versioning,semantic version range,versioning,version',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    tests_require=['flake8'],
+    test_suite='tests',
+)
