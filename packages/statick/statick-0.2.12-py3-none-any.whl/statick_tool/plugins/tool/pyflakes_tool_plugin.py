ef2231b33669afcc2ed408eb2c3139dafe18486e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:04 +0000

    Add package statick/0.2.12/statick-0.2.12-py3-none-any.whl

---
+++ packages/statick/statick-0.2.12-py3-none-any.whl/statick_tool/plugins/tool/pyflakes_tool_plugin.py
@@ -0,0 +1,72 @@
+"""Apply pyflakes tool and gather results."""
+
+from __future__ import print_function
+
+import re
+import subprocess
+
+from statick_tool.issue import Issue
+from statick_tool.tool_plugin import ToolPlugin
+
+
+class PyflakesToolPlugin(ToolPlugin):
+    """Apply pyflakes tool and gather results."""
+
+    def get_name(self):
+        """Get name of tool."""
+        return "pyflakes"
+
+    def scan(self, package, level):
+        """Run tool and gather output."""
+        flags = []
+        flags += self.get_user_flags(level)
+
+        total_output = []
+
+        for src in package["python_src"]:
+            try:
+                subproc_args = ["pyflakes", src] + flags
+                output = subprocess.check_output(subproc_args,
+                                                 stderr=subprocess.STDOUT,
+                                                 universal_newlines=True)
+
+            except subprocess.CalledProcessError as ex:
+                # Return code 1 just means "found problems"
+                if ex.returncode != 1:
+                    print("Problem {}".format(ex.returncode))
+                    print("{}".format(ex.output))
+                    return None
+
+                output = ex.output
+
+            except OSError as ex:
+                print("Couldn't find pyflakes executable! ({})".format(ex))
+                return None
+
+            if self.plugin_context.args.show_tool_output:
+                print("{}".format(output))
+
+            total_output.append(output)
+
+        with open(self.get_name() + ".log", "w") as fname:
+            for output in total_output:
+                fname.write(output)
+
+        issues = self.parse_output(total_output)
+        return issues
+
+    def parse_output(self, total_output):
+        """Parse tool output and report issues."""
+        pyflakes_re = r"(.+):(\d+):\s(.+)"
+        parse = re.compile(pyflakes_re)
+        issues = []
+
+        for output in total_output:
+            for line in output.splitlines():
+                match = parse.match(line)
+                if match:
+                    issues.append(Issue(match.group(1), match.group(2),
+                                        self.get_name(), self.get_name(),
+                                        "5", match.group(3), None))
+
+        return issues
