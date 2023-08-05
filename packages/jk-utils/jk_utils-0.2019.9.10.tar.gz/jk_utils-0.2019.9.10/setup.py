1bc1f3d3e356703aacfd380db7eed2bf857d0219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:52 +0000

    Add package jk-utils/0.2019.9.10/jk_utils-0.2019.9.10.tar.gz

---
+++ packages/jk-utils/jk_utils-0.2019.9.10.tar.gz/jk_utils-0.2019.9.10/setup.py
@@ -0,0 +1,52 @@
+﻿from setuptools import setup
+
+
+def readme():
+	with open("../README.md") as f:
+		return f.read()
+
+
+setup(name="jk_utils",
+	version="0.2019.9.10",
+	description="This python module provides various utility functions and classes.",
+	author="Jürgen Knauth",
+	author_email="pubsrc@binary-overflow.de",
+	license="Apache 2.0",
+	url="https://github.com/jkpubsrc/python-module-jk-utils",
+	download_url="https://github.com/jkpubsrc/python-module-jk-utils/tarball/0.2019.9.10",
+	keywords=[
+		"utilities"
+	],
+	packages=[
+		"jk_utils",
+		"jk_utils.async",
+	],
+	install_requires=[
+		#"netifaces",
+		#"jk_simpleexec",
+		"sh",
+		"sortedcontainers",
+		"jk_simpleexec",        # ??? can that be eliminated?
+		"jk_logging",           # ??? can that be eliminated?
+	],
+	include_package_data=True,
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Programming Language :: Python :: 3.5",
+		"License :: OSI Approved :: Apache Software License"
+	],
+	# long_description=readme(),
+	# long_description_content_type="text/markdown",
+	zip_safe=False)
+
+
+
+
+
+
+
+
+
+
+
+
