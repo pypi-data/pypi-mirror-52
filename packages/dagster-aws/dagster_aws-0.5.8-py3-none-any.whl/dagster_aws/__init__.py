53ba4d01baa570384800b83de8535dfa15a68ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:30 +0000

    Add package dagster-aws/0.5.8/dagster_aws-0.5.8-py3-none-any.whl

---
+++ packages/dagster-aws/dagster_aws-0.5.8-py3-none-any.whl/dagster_aws/__init__.py
@@ -0,0 +1,5 @@
+from .s3.file_cache import S3FileCache
+from .s3.resources import S3Resource, s3_resource
+from .s3.s3_fake_resource import S3FakeSession
+from .s3.solids import S3Coordinate
+from .version import __version__
