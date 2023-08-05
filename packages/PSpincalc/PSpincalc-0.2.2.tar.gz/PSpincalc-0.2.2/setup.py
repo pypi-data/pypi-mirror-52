703a6a9866fee3fab47666e89b9d374d273bcdf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:17 +0000

    Add package PSpincalc/0.2.2/PSpincalc-0.2.2.tar.gz

---
+++ packages/PSpincalc/PSpincalc-0.2.2.tar.gz/PSpincalc-0.2.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PSpincalc",
+    version="0.2.2",
+    author="Jose Gama",
+    author_email="rxprtgama@gmail.com",
+    description="Package for converting between attitude representations: DCM, Euler angles, Quaternions, and Euler vectors.",
+    url="https://github.com/tuxcell/PSpincalc",
+    packages=setuptools.find_packages(),
+    license="OSI Approved :: GNU General Public License v3 or later (GPLv3+)"
+)
+
