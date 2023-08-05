3714fdfbe2148a9973a5c2d734c2850c11eed3ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:52 +0000

    Add package pyBabyMaker/0.2.3.4/pyBabyMaker-0.2.3.4.tar.gz

---
+++ packages/pyBabyMaker/pyBabyMaker-0.2.3.4.tar.gz/pyBabyMaker-0.2.3.4/pyBabyMaker/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#
+# Author: Yipeng Sun <syp at umd dot edu>
+# License: BSD 2-clause
+# Last Change: Sat Sep 07, 2019 at 12:49 PM -0400
+
+name = 'pyBabyMaker'
+version = '0.0.1'  # This version is fixed.
+version = "0.2.3.4"
