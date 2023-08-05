adfee35a31900b0958076990b08094c1d500ed7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:14 +0000

    Add package SeedsLabeler/0.0.38/SeedsLabeler-0.0.38.tar.gz

---
+++ packages/SeedsLabeler/SeedsLabeler-0.0.38.tar.gz/SeedsLabeler-0.0.38/src/api/detection_flask.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import pickle
+class FlaskClient(object):
+    def __init__(self, host = 'localhost', port = 5000):
+        self.host = host
+        self.port = port
+
+
+    def processImageLabels(self, data):
+        # print(data.text)
+        data_json = json.loads(data.text)
+        # return pickle.loads(data.text)
+        # print( data_json['boxes'])
+        return (data_json['boxes'], data_json['labels_words'], data_json['scores'])
+
+
+    def sendDetectionImage(self, image_path):
+    #  '''sends image in bytes and gets JSON file'''
+        try:
+            print('Trying to send Image to server')
+            if image_path is None:
+                print('image path is none')
+                return
+            im = open(image_path, 'rb')
+            image_bytes = im.read()
+            print('Read an image')
+            x = requests.post(f'http://{self.host}:{self.port}/api/predict', data =  image_bytes, headers = {"content-type": "image/jpeg"})
+            print('Send a response')
+            return self.processImageLabels(x)
+
+        except Exception as e:
+            print("Closed a thread for server", str(e))
+            return None
