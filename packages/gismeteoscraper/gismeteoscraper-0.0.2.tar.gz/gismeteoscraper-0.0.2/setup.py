d75131df10a900cc143e25ec85a5f6eac3d972ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:27 +0000

    Add package gismeteoscraper/0.0.2/gismeteoscraper-0.0.2.tar.gz

---
+++ packages/gismeteoscraper/gismeteoscraper-0.0.2.tar.gz/gismeteoscraper-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gismeteoscraper",
+    version="0.0.2",
+    author="Egor Dunaev",
+    author_email="egodunaev@gmail.com",
+    description="A simple web scraper for gismeteo.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/egordunaev/gismeteo-scraper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['requests', 'bs4'],
+
+)
