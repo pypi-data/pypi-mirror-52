b35b48f1ed511cc6e5fbaae5d9db50217c40315f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:59 +0000

    Add package sugar-yespower/1.0.0/sugar_yespower-1.0.0.tar.gz

---
+++ packages/sugar-yespower/sugar_yespower-1.0.0.tar.gz/sugar_yespower-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup, Extension
+
+sugar_yespower_module = Extension('sugar_yespower',
+                            sources = ['yespower-module.c',
+                                       'yespower.c',
+                                       'yespower-opt.c',
+                                       'sha256.c'
+                                       ],
+                            extra_compile_args=['-O2', '-funroll-loops', '-fomit-frame-pointer'],
+                            include_dirs=['.'])
+
+setup (name = 'sugar_yespower',
+       version = '1.0.0',
+       author_email = 'yuto_tetuota@yahoo.co.jp',
+       author = 'y-chan',
+       url = 'https://github.com/sugarchain-dev/sugar_yespower_python3',
+       description = 'Bindings for yespower-1.0 proof of work used by sugarchain',
+       ext_modules = [sugar_yespower_module])
