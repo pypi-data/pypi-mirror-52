ce7dd13c9ae53a0903472d22a4bb0cb921560c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:42 +0000

    Add package emclib/0.1/emclib-0.1.tar.gz

---
+++ packages/emclib/emclib-0.1.tar.gz/emclib-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='emclib',
+      version='0.1',
+      description='A simple hello world library',
+      url='http://github.com/emcorrales/gh-actions-pypi-deployment-demo',
+      author='Emmanuel M. Corrales',
+      author_email='contact@emcorrales.com',
+      license='MIT',
+      packages=['emclib'],
+      zip_safe=False)
