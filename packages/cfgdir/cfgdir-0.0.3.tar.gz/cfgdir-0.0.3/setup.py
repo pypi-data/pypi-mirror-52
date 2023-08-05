26141d3d61c5ffe52790247f0c9c05d3b53ac666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:42 +0000

    Add package cfgdir/0.0.3/cfgdir-0.0.3.tar.gz

---
+++ packages/cfgdir/cfgdir-0.0.3.tar.gz/cfgdir-0.0.3/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cfgdir",
+    version="0.0.3",
+    author="Matt Krueger",
+    author_email="mkrueger@rstms.net",
+    description="output envdir as JSON or YAML",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rstms/cfgdir",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+
+    ],
+    py_modules=['cfgdir'],
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'pyyaml',
+   ],
+   entry_points='''
+       [console_scripts]
+           cfgdir=cfgdir:cli
+   ''',
+)
