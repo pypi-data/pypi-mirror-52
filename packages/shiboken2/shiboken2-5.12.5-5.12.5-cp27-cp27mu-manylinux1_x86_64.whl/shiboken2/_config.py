78bc545f58886212e0a411cd0e197e6ce5c4696c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:19:41 +0000

    Add package shiboken2/5.12.5/shiboken2-5.12.5-5.12.5-cp27-cp27mu-manylinux1_x86_64.whl

---
+++ packages/shiboken2/shiboken2-5.12.5-5.12.5-cp27-cp27mu-manylinux1_x86_64.whl/shiboken2/_config.py
@@ -0,0 +1,11 @@
+shiboken_library_soversion = str(5.12)
+
+version = "5.12.5"
+version_info = (5, 12, 5, "", "")
+
+__build_date__ = '2019-09-11T05:34:52+00:00'
+
+
+
+
+__setup_py_package_version__ = '5.12.5'
