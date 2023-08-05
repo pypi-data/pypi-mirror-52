2f4ee6966a19a7ead12e7efbcd3cfb8a0b338685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:15 +0000

    Add package torchbench/0.0.20/torchbench-0.0.20.tar.gz

---
+++ packages/torchbench/torchbench-0.0.20.tar.gz/torchbench-0.0.20/torchbench/semantic_segmentation/pascalvoc.py
@@ -0,0 +1,112 @@
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+
+from sotabenchapi.core import BenchmarkResult
+from torchbench.utils import default_data_to_device, send_model_to_device
+
+from torchbench.semantic_segmentation.transforms import (
+    Normalize,
+    Resize,
+    ToTensor,
+    Compose,
+)
+from torchbench.semantic_segmentation.utils import (
+    default_seg_collate_fn,
+    default_seg_output_transform,
+    evaluate_segmentation,
+)
+
+
+class PASCALVOC:
+
+    dataset = datasets.VOCSegmentation
+    normalize = Normalize(
+        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+    )
+    transforms = Compose([Resize((520, 480)), ToTensor(), normalize])
+    send_data_to_device = default_data_to_device
+    collate_fn = default_seg_collate_fn
+    model_output_transform = default_seg_output_transform
+    task = "Semantic Segmentation"
+
+    @classmethod
+    def benchmark(
+        cls,
+        model,
+        model_description=None,
+        dataset_year="2007",
+        input_transform=None,
+        target_transform=None,
+        transforms=None,
+        model_output_transform=None,
+        collate_fn=None,
+        send_data_to_device=None,
+        device: str = "cuda",
+        data_root: str = "./.data/vision/voc",
+        num_workers: int = 4,
+        batch_size: int = 32,
+        num_gpu: int = 1,
+        paper_model_name: str = None,
+        paper_arxiv_id: str = None,
+        paper_pwc_id: str = None,
+        paper_results: dict = None,
+        pytorch_hub_url: str = None,
+    ) -> BenchmarkResult:
+
+        config = locals()
+        model, device = send_model_to_device(
+            model, device=device, num_gpu=num_gpu
+        )
+        model.eval()
+
+        if not input_transform or target_transform or transforms:
+            transforms = cls.transforms
+
+        if not model_output_transform:
+            model_output_transform = cls.model_output_transform
+
+        if not send_data_to_device:
+            send_data_to_device = cls.send_data_to_device
+
+        if not collate_fn:
+            collate_fn = cls.collate_fn
+
+        test_dataset = cls.dataset(
+            root=data_root,
+            image_set="val",
+            year=dataset_year,
+            transform=input_transform,
+            target_transform=target_transform,
+            transforms=transforms,
+            download=True,
+        )
+        test_loader = DataLoader(
+            test_dataset,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=num_workers,
+            pin_memory=True,
+            collate_fn=collate_fn,
+        )
+        test_loader.no_classes = 21  # Number of classes for PASCALVoc
+        test_results = evaluate_segmentation(
+            model=model,
+            test_loader=test_loader,
+            model_output_transform=model_output_transform,
+            send_data_to_device=send_data_to_device,
+            device=device,
+        )
+        print(test_results)
+
+        return BenchmarkResult(
+            task=cls.task,
+            config=config,
+            dataset=cls.dataset.__name__,
+            results=test_results,
+            pytorch_hub_id=pytorch_hub_url,
+            model=paper_model_name,
+            model_description=model_description,
+            arxiv_id=paper_arxiv_id,
+            pwc_id=paper_pwc_id,
+            paper_results=paper_results,
+        )
