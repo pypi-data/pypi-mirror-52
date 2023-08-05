94bbfdf767aac9c34bcba67c89558aa2f3a75b38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package epmwebapi/1.3.30/epmwebapi-1.3.30.tar.gz

---
+++ packages/epmwebapi/epmwebapi-1.3.30.tar.gz/epmwebapi-1.3.30/epmwebapi/datavaluejson.py
@@ -0,0 +1,36 @@
+﻿"""
+Elipse Plant Manager - EPM Web API
+Copyright (C) 2018 Elipse Software.
+Distributed under the MIT License.
+(See accompanying file LICENSE.txt or copy at http://opensource.org/licenses/MIT)
+"""
+
+import dateutil
+import datetime
+from datetime import timezone
+
+class DataValueJSON(object):
+    """description of class"""
+    def __init__(self, value, statusCode, timestamp):
+        self._value = value
+        self._statusCode = statusCode
+        if type(timestamp) == str:
+          self._timestamp = dateutil.parser.parse(timestamp).astimezone(timezone.utc)
+        else:
+          self._timestamp = timestamp
+
+    @property
+    def value(self):
+      return self._value
+
+    @property
+    def statusCode(self):
+      return self._statusCode
+
+    @property
+    def timestamp(self):
+      return self._timestamp
+
+    def toDict(self):
+        return {'value': self._value, 'quality': self._statusCode, 
+               'timestamp' : self._timestamp.isoformat() }
