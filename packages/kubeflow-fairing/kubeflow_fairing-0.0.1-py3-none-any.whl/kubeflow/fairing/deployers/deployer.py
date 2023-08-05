3d784fb88c39a4cd766ff1baa52d01d5e93fe5a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:29 +0000

    Add package kubeflow-fairing/0.0.1/kubeflow_fairing-0.0.1-py3-none-any.whl

---
+++ packages/kubeflow-fairing/kubeflow_fairing-0.0.1-py3-none-any.whl/kubeflow/fairing/deployers/deployer.py
@@ -0,0 +1,16 @@
+import abc
+import six
+
+
+@six.add_metaclass(abc.ABCMeta)
+class DeployerInterface(object):
+    """Deploys a training job to the cluster"""
+
+    def deploy(self, pod_template_spec):
+        """Deploys the training job"""
+        raise NotImplementedError('TrainingInterface.deploy')
+
+    @abc.abstractmethod
+    def get_logs(self):
+        """Streams the logs for the training job"""
+        raise NotImplementedError('TrainingInterface.train')
