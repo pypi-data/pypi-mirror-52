6f4b67a61d8399c4fda0723571b4f74c6ba3135e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:17 +0000

    Add package minimal-binary-coding/1.0.0/minimal_binary_coding-1.0.0.tar.gz

---
+++ packages/minimal-binary-coding/minimal_binary_coding-1.0.0.tar.gz/minimal_binary_coding-1.0.0/minimal_binary_coding/__init__.py
@@ -0,0 +1,3 @@
+from .minimal_binary_coding import minimal_binary_coding
+
+__all__ = ["minimal_binary_coding"]
---
+++ packages/minimal-binary-coding/minimal_binary_coding-1.0.0.tar.gz/minimal_binary_coding-1.0.0/minimal_binary_coding/__version__.py
@@ -0,0 +1,2 @@
+"""Current version of package minimal_binary_coding"""
+__version__ = "1.0.0"
