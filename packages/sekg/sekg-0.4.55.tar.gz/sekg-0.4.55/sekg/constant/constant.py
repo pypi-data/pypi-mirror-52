ab38c567d17e3af14b179cc0af6bd1d3eed6afe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:10 +0000

    Add package sekg/0.4.55/sekg-0.4.55.tar.gz

---
+++ packages/sekg/sekg-0.4.55.tar.gz/sekg-0.4.55/sekg/constant/constant.py
@@ -0,0 +1,29 @@
+class CodeConstant:
+    LABEL_CODE_ELEMENT = "code_element"
+    QUALIFIED_NAME = "qualified_name"
+
+
+class DomainConstant:
+    LABEL_DOMAIN_TERM = "domain term"
+    PRIMARY_PROPERTY_NAME = "term_name"
+
+
+class OperationConstance:
+    LABEL_OPERATION = "operation"
+    PRIMARY_PROPERTY_NAME = "operation_name"
+
+
+class WikiDataConstance:
+    LABEL_WIKIDATA = "wikidata"
+    PRIMARY_PROPERTY_NAME = "wd_item_id"
+    NAME = "wikidata_name"
+    REALTION_SAME_AS = "same as"
+
+
+class PropertyConstant:
+    COMMENT = "comment"
+    INSIDE_COMMENT = "inside_comment"
+    STRING_LITERAL_EXPR = "string_literal_expr"
+    DESCRIPTION = "short_description"
+    ALIAS = "alias"
+    LEMMA = "lemma"
