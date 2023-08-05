345ed27217b9673f0d1377145d110314686cf683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:42 +0000

    Add package ddadevops/0.1.11/ddadevops-0.1.11.tar.gz

---
+++ packages/ddadevops/ddadevops-0.1.11.tar.gz/ddadevops-0.1.11/ddadevops/__init__.py
@@ -0,0 +1,11 @@
+"""
+ddadevops provide tools to support builds combining gopass, 
+terraform, dda-pallet, aws & hetzner-cloud.
+
+"""
+
+from ddadevops.dda_pallet import *
+from ddadevops.meissa_build import *
+from ddadevops.terraform import *
+
+__version__ = "0.1.11"
