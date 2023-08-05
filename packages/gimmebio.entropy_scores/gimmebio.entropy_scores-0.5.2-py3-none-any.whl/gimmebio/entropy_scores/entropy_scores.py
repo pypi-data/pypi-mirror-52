c36f3872d4f4ec1f65e07dcc93a57fb16ff304a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:44 +0000

    Add package gimmebio.entropy_scores/0.5.2/gimmebio.entropy_scores-0.5.2-py3-none-any.whl

---
+++ packages/gimmebio.entropy_scores/gimmebio.entropy_scores-0.5.2-py3-none-any.whl/gimmebio/entropy_scores/entropy_scores.py
@@ -0,0 +1,90 @@
+
+import pysam
+import pandas as pd
+
+from math import log
+from Bio import SeqIO
+
+from gimmebio.kmers import make_kmers
+
+
+def num_unique_subwords(seq, n):
+    out = set()
+    for kmer in make_kmers(seq, n, canon=False):
+        out.add(kmer)
+    return len(out)
+
+
+def seq_entropy_score(seq, n=3):
+    sub_seq = seq[0:(4 ** n + n - 1)]
+    return log(num_unique_subwords(sub_seq, n), 4) / n
+
+
+def entropy_score_bam(bam_filename):
+    samfile = pysam.AlignmentFile(bam_filename, "rb")
+    scores = pd.Series([seq_entropy_score(read.query_sequence) for read in samfile])
+    return scores.mean()
+
+
+def entropy_score_fastq(fastq_filename, delim=None):
+    fastq = SeqIO.parse(fastq_filename, 'fastq')
+    if delim is None:
+        scores = pd.Series([seq_entropy_score(read.seq) for read in fastq])
+        return [('all', scores.mean())]
+    else:
+        scores = pd.DataFrame([
+            {'genome': read.id.split(delim)[0], 'score': seq_entropy_score(read.seq)}
+            for read in fastq
+        ])
+        return [(row['genome'], row['score']) for _, row in scores.groupby('genome').mean().iterrows()]
+
+
+def gini_coeff(vals, nvals, min_bins=3):
+    vals = sorted(list(vals))
+    nbins = max(len(vals), min_bins)
+    total_val, total_area = 0, 0
+    for val in vals:
+        total_val += val
+        column = total_val / (nvals * nbins)
+        total_area += column
+    return total_area
+
+
+def clump_score(bam_filename, binsize=10000, min_bins=3, delim=None):
+    samfile = pysam.AlignmentFile(bam_filename, "rb")
+    return _clump_score(samfile, binsize=binsize, min_bins=min_bins, delim=delim)
+
+
+def _clump_score(samfile, binsize=10000, min_bins=3, delim=None):
+    bins, nreads = {}, 0
+    for read in samfile:
+        if read.is_unmapped:
+            continue
+        nreads += 1
+        genome = read.reference_id.split(delim)[0] if delim else 'all'
+        if genome not in bins:
+            bins[genome] = {}
+        pos = read.reference_id, read.reference_start // binsize
+        bins[genome][pos] = 1 + bins[genome].get(pos, 0)
+
+    out = []
+    for genome, gene_bins in bins.items():
+        out.append((genome, gini_coeff(gene_bins.values(), nreads, min_bins=min_bins)))
+    return out
+
+
+def entropy_and_clump_score(bam_filename, binsize=10000, min_bins=3, delim=None):
+    samfile = pysam.AlignmentFile(bam_filename, "rb")
+    if delim is None:
+        escores = pd.Series([seq_entropy_score(read.query_sequence) for read in samfile])
+        escore = [('all', escores.mean())]
+        cscore = clump_score(bam_filename, binsize=binsize, min_bins=min_bins)
+    else:
+        escores = pd.DataFrame([
+            {'genome': read.query_id.split(delim)[0], 'score': seq_entropy_score(read.seq)}
+            for read in samfile
+        ])
+        escore = [(row['genome'], row['score']) for _, row in scores.groupby('genome').mean().iterrows()]
+        cscore = clump_score(bam_filename, binsize=binsize, min_bins=min_bins, delim=delim)
+
+    return escore, cscore
