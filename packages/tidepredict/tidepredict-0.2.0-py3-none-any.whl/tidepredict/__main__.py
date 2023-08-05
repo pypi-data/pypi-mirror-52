96a9fbc9aed5bb9322e8a92552a5b5a6dac7ac28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:34 +0000

    Add package tidepredict/0.2.0/tidepredict-0.2.0-py3-none-any.whl

---
+++ packages/tidepredict/tidepredict-0.2.0-py3-none-any.whl/tidepredict/__main__.py
@@ -0,0 +1,147 @@
+"""Main routine for managing the program
+"""
+from __future__ import print_function
+import numpy as np
+import sys
+import argparse
+from tidepredict import processdata
+from tidepredict import process_station_list
+from tidepredict import constants
+from tidepredict import constituent
+from tidepredict.tide import Tide
+import os
+import pandas as pd
+import json
+import datetime
+
+
+
+parser = argparse.ArgumentParser(description=
+                                'tidepredict: a tide prediction module.')
+parser.add_argument('-harmgen',
+                    action="store_true",
+                    help="""Generic harmonics constants from University of 
+                            Hawaii research quality data""")
+parser.add_argument('-m',
+                    action="store",
+                    help="""Mode:
+                            Specify mode to be about, banner, calendar,
+                            alt. calendar, graph, clock,
+                            list, medium rare, plain, raw, or stats.
+                            The default is plain.""",
+                    default = "p",
+                    choices=['a', 'b', 'c', 'C','g', 'k', 'l', 'm', 'p',
+                            'r', 's'])
+parser.add_argument('-l',
+                    action="store",
+                    help="""Location to search the database for or to
+                            generate harmonic constituents for.""",
+                    metavar="Location",
+                    required = True)  
+
+parser.add_argument('-r',
+                    action="store_true",
+                    help="""Force refresh of station database""")  
+
+parser.add_argument('-b',
+                    action="store",
+                    help = "Start time to begin predictions",
+                    metavar = "YYYY-MM-DD HH:MM")
+
+parser.add_argument('-e',
+                    action="store",
+                    help = "end time for predictions",
+                    metavar = "YYYY-MM-DD HH:MM")
+
+
+    #todo add the rest of the xtide arguments once I've implemented the above
+    # correctly.   
+
+def process_args(args):
+    #First try to read in the location list
+    try:
+        stations = pd.read_csv(constants.stationfile)
+        if args.r:
+            #force refresh of stations if command line arg is forced
+            raise EnvironmentError
+    except EnvironmentError:
+        #Not found then create it.
+        stations = process_station_list.create_station_dataframe()
+
+    #extract station data from stations df
+    thestation = stations[stations.loc_name == args.l]
+    #print(thestation)
+    
+    if thestation.empty:
+        raise Exception("Station not found")
+
+    loc_code = "h" + thestation.stat_idx.tolist()[0].lower()
+    if args.harmgen is True:
+        #get the last two years that data exists for
+        lastyear = int(thestation.data_years.tolist()[0][-2:])
+        years = list(range(lastyear-1,lastyear+1))
+        #create data url
+        ocean = constants.ocean_dict[thestation.oc_idx.tolist()[0][0]]
+        ftpurl = processdata.get_data_url(ocean = ocean)
+        datadict = processdata.process_unhw_data(ftpurl = ftpurl,
+                                                  years=years,
+                                                  loc_code = loc_code)
+
+        my_tides = processdata.fit_model(datadict)
+        if not os.path.exists(constants.saveharmlocation):
+            os.mkdir(constants.saveharmlocation)
+        harmfile = open(os.path.join(constants.saveharmlocation,loc_code+".json"), "w+")
+        #print(my_tides)
+        #jpic = pickle.dumps(my_tides.model)
+        cons = [item.name for item in my_tides.model['constituent']]
+        jpic = json.dumps([cons,my_tides.model['amplitude'].tolist(),my_tides.model['phase'].tolist()])
+        #print(my_tides.model['constituents'])
+        print(jpic, file=harmfile)
+        harmfile.close()
+    
+    #Try to get the saved harmonics constants from file.
+    #Tide prediction using pre-generated constants is much faster than 
+    #having to derive them again.
+    try:
+        with open(os.path.join(constants.saveharmlocation,
+                  loc_code+".json"),"r") as tidemodelfile:
+            #Reconstruct tide model from saved harmonics data
+            tide = processdata.reconstruct_tide_model(tidemodelfile)
+            #print (tide.at([datetime(2019,1,1,0,0,0), datetime(2019,1,1,6,0,0)]))
+    except FileNotFoundError:
+        print("Harmonics data not found for %s" %args.l)
+        print("Use option -harmgen to generate harmonics for this location")
+
+    #check validity of start time
+    if args.b is not None:
+        try:
+            start = datetime.datetime.strptime(args.b,"%Y-%m-%d %H:%M")
+        except ValueError:
+            print("Start time format does not match expected YYYY-MM-DD HH:MM")
+            sys.exit()
+    else:
+        start = datetime.datetime.today()
+
+    #check validity of end time
+    if args.e is not None:
+        try:
+            end = datetime.datetime.strptime(args.e,"%Y-%m-%d %H:%M")
+        except ValueError:
+            print("End time format does not match expected YYYY-MM-DD HH:MM")
+            sys.exit()
+    else:
+        end = start + datetime.timedelta(days=3)
+    
+
+    #output tide predictions
+    if args.m == "p":
+        predictions = processdata.predict_plain(tide,
+                                                startdate=start,
+                                                enddate=end)
+        print(predictions)
+    return predictions
+    
+if __name__ == "__main__":
+    args = parser.parse_args()
+    #print(args.harmgen)
+    process_args(args)
---
+++ packages/tidepredict/tidepredict-0.2.0-py3-none-any.whl/tidepredict/process_station_list.py
@@ -0,0 +1,53 @@
+"""
+module to grab the list of stations from the UN Hawaii server and check
+if the user specified string can be matched to a station
+"""
+from tidepredict import ftp_helpers
+import pandas as pd
+from io import StringIO
+import os
+from tidepredict import constants
+
+def get_station_files():
+    """Get bytes IO objects from each of the station list files
+    """
+    ftpurl = "ftp.soest.hawaii.edu"
+    pacific = ftp_helpers.get_byte_stream(ftpurl,"uhslc/rqds/pacific/pacific.lst")
+    indian = ftp_helpers.get_byte_stream(ftpurl,"uhslc/rqds/indian/indian.lst")
+    atlantic = ftp_helpers.get_byte_stream(ftpurl,"uhslc/rqds/atlantic/atlantic.lst")
+
+    return pacific, indian, atlantic
+
+def create_station_dataframe():
+    files = get_station_files()
+    combinedlst = ""
+    for item in files:
+        for line in item.read().decode().splitlines():
+            try:
+                #Try to convert the first three letters of the line to 
+                #an int, if successful then we can assume we have a 
+                #station info line, so save it to the overall file.
+                int(line[:3])
+                combinedlst += line + "\n"
+            except:
+                pass
+    #print(combinedlst)
+    stat_df = pd.read_fwf(StringIO(combinedlst),
+                          header=None,
+                          names = ["stat_idx", "oc_idx", "x", "loc_name",
+                                   "country", "Lat", "Lon", "data_years",
+                                   "CI", "Contributor"])
+
+    #dump to file
+    if not os.path.exists(constants.savefilelocation):
+        os.mkdir(constants.savefilelocation)
+    else:    
+        stat_df.to_csv(os.path.join(constants.savefilelocation,
+                                    constants.stationfile))
+    return stat_df
+    
+
+    
+
+    
+
