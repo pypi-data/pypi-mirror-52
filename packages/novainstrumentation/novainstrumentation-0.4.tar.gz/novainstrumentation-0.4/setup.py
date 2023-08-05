8aeb6f54215509ec19b34290d2d37e87c75601a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:19 +0000

    Add package novainstrumentation/0.4/novainstrumentation-0.4.tar.gz

---
+++ packages/novainstrumentation/novainstrumentation-0.4.tar.gz/novainstrumentation-0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup,find_packages
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+	name = 'novainstrumentation',
+	version = '0.4',
+	description = 'Supporting code for Digital Instrumentation class at Universidade Nova de Lisboa.',
+	url = 'https://github.com/hgamboa/novainstrumentation',
+	author = 'hgamboa',
+	packages=find_packages(),
+	package_data = {'novainstrumentation' : ["data/*"]},
+	install_requires = ['scipy','pandas','matplotlib','numpy'],
+    zip_safe=False)
