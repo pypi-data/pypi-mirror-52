513cf4bcf808e7f8fed8cf5cc2ba385f333ff8c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:57 +0000

    Add package DB-Transfer/0.5.3/DB%20Transfer-0.5.3.tar.gz

---
+++ packages/DB-Transfer/DB%20Transfer-0.5.3.tar.gz/DB Transfer-0.5.3/db_transfer/__init__.py
@@ -0,0 +1,1 @@
+from db_transfer.transfer import Transfer, sent_env
---
+++ packages/DB-Transfer/DB%20Transfer-0.5.3.tar.gz/DB Transfer-0.5.3/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='DB Transfer',
+    version="0.5.3",
+
+    description='An easy way to fetch and store data '
+                'from and store to key-value databases like Redis.',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/arrrlo/db-transfer',
+    license='MIT',
+
+    author='Ivan Arar',
+    author_email='ivan.arar@gmail.com',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='database, redis, transfer, migration',
+
+    packages=['db_transfer'],
+    install_requires=[
+        'click>=6.3',
+        'redis>=2.10',
+        'ujson>=1.35',
+        'six>=1.12.0',
+        'PyYAML>=4.2b4'
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'dbtransfer=db_transfer.cli:cli'
+        ],
+    },
+
+    project_urls={
+        'Source': 'https://github.com/arrrlo/db-transfer',
+    },
+)
