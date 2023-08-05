0b782535729bf97a9218ec9a8d391c5600854136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:13 +0000

    Add package GraphDash/0.10.3/GraphDash-0.10.3.tar.gz

---
+++ packages/GraphDash/GraphDash-0.10.3.tar.gz/GraphDash-0.10.3/settings.sh
@@ -0,0 +1,12 @@
+ALL_MODES=(
+    ['prod']='docs/graphdash.yaml'
+    ['test']='docs/graphdash.yaml'
+)
+
+ALL_PORTS=(
+    ['prod']=1234
+    ['test']=5678
+)
+
+WORKERS=3
+TIMEOUT=60
---
+++ packages/GraphDash/GraphDash-0.10.3.tar.gz/GraphDash-0.10.3/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Packaging
+"""
+
+from __future__ import with_statement
+import sys
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    LONG_DESCRIPTION = f.read()
+
+with open('LICENSE') as f:
+    LICENSE = f.read()
+
+INSTALL_REQUIRES = [
+    'argparse',
+    'PyYAML==3.11',
+    'Flask==0.12.3',
+    'Markdown==2.4',
+    'Pygments==2.1.3',
+    'stop-words',
+]
+
+if sys.version_info[0] < 3:
+    INSTALL_REQUIRES.append('ushlex==0.99')
+
+setup(
+    name='GraphDash',
+    version='0.10.3',
+    author='Alex Prengere',
+    author_email='alexprengere@amadeus.com',
+    url='https://github.com/AmadeusITGroup/graphdash',
+    description='A web-based dashboard built on graphs and their metadata.',
+    long_description=LONG_DESCRIPTION,
+    license=LICENSE,
+    packages=find_packages(),
+    package_data={
+        'graphdash': [
+            'templates/*.html',
+            'assets/css/*',
+            'assets/js/*',
+            'assets/img/*',
+        ],
+    },
+    zip_safe=False,
+    install_requires=INSTALL_REQUIRES,
+    entry_points={
+        'console_scripts': [
+            'GraphDash=graphdash.__main__:main',
+        ],
+    },
+    scripts=[
+        'GraphDashManage',
+    ],
+)
