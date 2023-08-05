d90429c061d211e12c5553d1d0be2ddeb611c2e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:47 +0000

    Add package nwae/0.6.1/nwae-0.6.1-py3-none-any.whl

---
+++ packages/nwae/nwae-0.6.1-py3-none-any.whl/nwae/lib/lang/nlp/Corpora.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import pickle
+import re
+from collections import Counter
+import nwae.utils.Log as log
+from inspect import currentframe, getframeinfo
+import hanziconv as hzc
+import nwae.lib.lang.LangFeatures as lf
+import nltk
+
+
+class Corpora:
+
+    NLTK_COMTRANS = 'comtrans'
+
+    CORPORA_NLTK_TRANSLATED_SENTENCES_EN_DE = 'alignment-de-en.txt'
+
+    def __init__(
+            self
+    ):
+        nltk.download(Corpora.NLTK_COMTRANS)
+        return
+
+    def retrieve_corpora(
+            self,
+            corpora_name
+    ):
+        from nltk.corpus import comtrans
+        als = comtrans.aligned_sents(corpora_name)
+        sentences_l1 = [sent.words for sent in als]
+        sentences_l2 = [sent.mots for sent in als]
+        assert len(sentences_l1) == len(sentences_l2)
+        return (sentences_l1, sentences_l2)
+
+
+if __name__ == '__main__':
+    obj = Corpora()
+
+    (sen_l1, sen_l2) = obj.retrieve_corpora(
+        corpora_name = Corpora.CORPORA_NLTK_TRANSLATED_SENTENCES_EN_DE
+    )
+    print(sen_l1[0:10])
+    print(sen_l2[0:10])
