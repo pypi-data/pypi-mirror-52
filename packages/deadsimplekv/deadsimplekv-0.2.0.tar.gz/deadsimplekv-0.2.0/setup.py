125b6fcb38604f7b8de85928f47193f7a6aeeacf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:14 +0000

    Add package deadsimplekv/0.2.0/deadsimplekv-0.2.0.tar.gz

---
+++ packages/deadsimplekv/deadsimplekv-0.2.0.tar.gz/deadsimplekv-0.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='deadsimplekv',
+      version='0.2.0',
+      description='Very simple key-value store for Python',
+      author='Kevin Froman',
+      author_email='beardog@mailbox.org',
+      url='https://github.com/beardog108/deadsimplekv',
+      packages=['deadsimplekv'],
+     )
