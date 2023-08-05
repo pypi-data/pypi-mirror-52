d09d2cb946e3cdc5e2173b3f9a07dac8acbadf73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:44 +0000

    Add package BAC0/19.9.8/BAC0-19.9.8.tar.gz

---
+++ packages/BAC0/BAC0-19.9.8.tar.gz/BAC0-19.9.8/BAC0/core/app/ScriptApplication.py
@@ -0,0 +1,252 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015 by Christian Tremblay, P.Eng <christian.tremblay@servisys.com>
+# Licensed under LGPLv3, see file LICENSE in this source tree.
+#
+"""
+SimpleApplication 
+=================
+
+A basic BACnet application (bacpypes BIPSimpleApplication) for interacting with 
+the bacpypes BACnet stack.  It enables the base-level BACnet functionality 
+(a.k.a. device discovery) - meaning it can send & receive WhoIs & IAm messages.
+
+Additional functionality is enabled by inheriting this application, and then 
+extending it with more functions. [See BAC0.scripts for more examples of this.]
+
+"""
+# --- standard Python modules ---
+from collections import defaultdict
+
+# --- 3rd party modules ---
+from bacpypes.app import ApplicationIOController
+from bacpypes.pdu import Address
+from bacpypes.service.object import ReadWritePropertyMultipleServices
+from bacpypes.netservice import NetworkServiceAccessPoint, NetworkServiceElement
+from bacpypes.bvllservice import BIPSimple, BIPForeign, AnnexJCodec, UDPMultiplexer
+from bacpypes.appservice import StateMachineAccessPoint, ApplicationServiceAccessPoint
+from bacpypes.comm import ApplicationServiceElement, bind
+
+# basic services
+from bacpypes.service.device import WhoIsIAmServices
+from bacpypes.service.object import ReadWritePropertyServices
+
+# --- this application's modules ---
+from ..utils.notes import note_and_log
+from ..functions.Discover import NetworkServiceElementWithRequests
+
+# ------------------------------------------------------------------------------
+
+
+@note_and_log
+class BAC0Application(
+    ApplicationIOController,
+    WhoIsIAmServices,
+    ReadWritePropertyServices,
+    ReadWritePropertyMultipleServices,
+):
+    """
+    Defines a basic BACnet/IP application to process BACnet requests.
+
+    :param *args: local object device, local IP address
+        See BAC0.scripts.BasicScript for more details.
+
+    """
+
+    def __init__(
+        self,
+        localDevice,
+        localAddress,
+        bbmdAddress=None,
+        bbmdTTL=0,
+        deviceInfoCache=None,
+        aseID=None,
+    ):
+
+        ApplicationIOController.__init__(
+            self, localDevice, deviceInfoCache, aseID=aseID
+        )
+
+        # local address might be useful for subclasses
+        if isinstance(localAddress, Address):
+            self.localAddress = localAddress
+        else:
+            self.localAddress = Address(localAddress)
+
+        # include a application decoder
+        self.asap = ApplicationServiceAccessPoint()
+
+        # pass the device object to the state machine access point so it
+        # can know if it should support segmentation
+        self.smap = StateMachineAccessPoint(localDevice)
+
+        # the segmentation state machines need access to the same device
+        # information cache as the application
+        self.smap.deviceInfoCache = self.deviceInfoCache
+
+        # a network service access point will be needed
+        self.nsap = NetworkServiceAccessPoint()
+
+        # give the NSAP a generic network layer service element
+        self.nse = NetworkServiceElementWithRequests()
+        bind(self.nse, self.nsap)
+
+        # bind the top layers
+        bind(self, self.asap, self.smap, self.nsap)
+
+        # create a generic BIP stack, bound to the Annex J server
+        # on the UDP multiplexer
+        self.bip = BIPSimple()
+        self.annexj = AnnexJCodec()
+        self.mux = UDPMultiplexer(self.localAddress)
+
+        # bind the bottom layers
+        bind(self.bip, self.annexj, self.mux.annexJ)
+
+        # bind the BIP stack to the network, no network number
+        self.nsap.bind(self.bip, address=self.localAddress)
+
+        self.i_am_counter = defaultdict(int)
+        self.who_is_counter = defaultdict(int)
+
+        # keep track of requests to line up responses
+        self._request = None
+        self._last_i_am_received = []
+
+    def do_IAmRequest(self, apdu):
+        """Given an I-Am request, cache it."""
+        self.log("do_IAmRequest {!r}".format(apdu))
+
+        # build a key from the source, just use the instance number
+        key = (str(apdu.pduSource), apdu.iAmDeviceIdentifier[1])
+        self.i_am_counter[key] += 1
+        self._last_i_am_received.append(key)
+
+    def do_WhoIsRequest(self, apdu):
+        """Respond to a Who-Is request."""
+        self.log("do_WhoIsRequest {!r}".format(apdu))
+
+        # build a key from the source and parameters
+        key = (
+            str(apdu.pduSource),
+            apdu.deviceInstanceRangeLowLimit,
+            apdu.deviceInstanceRangeHighLimit,
+        )
+
+        # count the times this has been received
+        self.who_is_counter[key] += 1
+
+    def close_socket(self):
+        # pass to the multiplexer, then down to the sockets
+        self.mux.close_socket()
+
+    def request(self, apdu):
+        # save a copy of the request
+        self._request = apdu
+
+        # forward it along
+        super(BAC0Application, self).request(apdu)
+
+
+@note_and_log
+class BAC0ForeignDeviceApplication(
+    ApplicationIOController,
+    WhoIsIAmServices,
+    ReadWritePropertyServices,
+    ReadWritePropertyMultipleServices,
+):
+    """
+    Defines a basic BACnet/IP application to process BACnet requests.
+
+    :param *args: local object device, local IP address
+        See BAC0.scripts.BasicScript for more details.
+
+    """
+
+    def __init__(
+        self,
+        localDevice,
+        localAddress,
+        bbmdAddress=None,
+        bbmdTTL=0,
+        deviceInfoCache=None,
+        aseID=None,
+    ):
+
+        ApplicationIOController.__init__(
+            self, localDevice, deviceInfoCache, aseID=aseID
+        )
+
+        # local address might be useful for subclasses
+        if isinstance(localAddress, Address):
+            self.localAddress = localAddress
+        else:
+            self.localAddress = Address(localAddress)
+
+        # include a application decoder
+        self.asap = ApplicationServiceAccessPoint()
+
+        # pass the device object to the state machine access point so it
+        # can know if it should support segmentation
+        self.smap = StateMachineAccessPoint(localDevice)
+
+        # the segmentation state machines need access to the same device
+        # information cache as the application
+        self.smap.deviceInfoCache = self.deviceInfoCache
+
+        # a network service access point will be needed
+        self.nsap = NetworkServiceAccessPoint()
+
+        # give the NSAP a generic network layer service element
+        self.nse = NetworkServiceElementWithRequests()
+        bind(self.nse, self.nsap)
+
+        # bind the top layers
+        bind(self, self.asap, self.smap, self.nsap)
+
+        # create a generic BIP stack, bound to the Annex J server
+        # on the UDP multiplexer
+        self.bip = BIPForeign(bbmdAddress, bbmdTTL)
+        self.annexj = AnnexJCodec()
+        self.mux = UDPMultiplexer(self.localAddress, noBroadcast=True)
+
+        # bind the bottom layers
+        bind(self.bip, self.annexj, self.mux.annexJ)
+
+        # bind the NSAP to the stack, no network number
+        self.nsap.bind(self.bip)
+
+        self.i_am_counter = defaultdict(int)
+        self.who_is_counter = defaultdict(int)
+        # keep track of requests to line up responses
+        self._request = None
+        self._last_i_am_received = []
+
+    def do_IAmRequest(self, apdu):
+        """Given an I-Am request, cache it."""
+        self.log("do_IAmRequest {!r}".format(apdu))
+
+        # build a key from the source, just use the instance number
+        key = (str(apdu.pduSource), apdu.iAmDeviceIdentifier[1])
+        self.i_am_counter[key] += 1
+        self._last_i_am_received.append(key)
+        # continue with the default implementation
+
+    def do_WhoIsRequest(self, apdu):
+        """Respond to a Who-Is request."""
+        self.log("do_WhoIsRequest {!r}".format(apdu))
+
+        # build a key from the source and parameters
+        key = (
+            str(apdu.pduSource),
+            apdu.deviceInstanceRangeLowLimit,
+            apdu.deviceInstanceRangeHighLimit,
+        )
+
+        # count the times this has been received
+        self.who_is_counter[key] += 1
+
+    def close_socket(self):
+        # pass to the multiplexer, then down to the sockets
+        self.mux.close_socket()
---
+++ packages/BAC0/BAC0-19.9.8.tar.gz/BAC0-19.9.8/BAC0/core/io/Write.py
@@ -0,0 +1,200 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015 by Christian Tremblay, P.Eng <christian.tremblay@servisys.com>
+# Licensed under LGPLv3, see file LICENSE in this source tree.
+#
+"""
+Write.py - creation of WriteProperty requests
+
+    Used while defining an app
+    Example::
+
+        class BasicScript(WhoisIAm, WriteProperty)
+
+    Class::
+
+        WriteProperty()
+            def write()
+
+
+"""
+# --- 3rd party modules ---
+from bacpypes.debugging import bacpypes_debugging, ModuleLogger
+
+from bacpypes.pdu import Address
+from bacpypes.object import get_datatype
+
+from bacpypes.apdu import WritePropertyRequest, SimpleAckPDU
+
+from bacpypes.primitivedata import Null, Atomic, Integer, Unsigned, Real, Enumerated
+from bacpypes.constructeddata import Array, Any
+from bacpypes.iocb import IOCB
+from bacpypes.core import deferred
+
+# --- this application's modules ---
+from .IOExceptions import (
+    WritePropertyCastError,
+    NoResponseFromController,
+    WritePropertyException,
+    WriteAccessDenied,
+    ApplicationNotStarted,
+)
+from ...core.utils.notes import note_and_log
+from .Read import find_reason
+
+# ------------------------------------------------------------------------------
+
+# some debugging
+_debug = 0
+_LOG = ModuleLogger(globals())
+
+
+@note_and_log
+class WriteProperty:
+    """
+    Defines BACnet Write functions: WriteProperty [WritePropertyMultiple not supported]
+
+    """
+
+    def write(self, args, vendor_id=0, timeout=10):
+        """ Build a WriteProperty request, wait for an answer, and return status [True if ok, False if not].
+
+        :param args: String with <addr> <type> <inst> <prop> <value> [ <indx> ] [ <priority> ]
+        :returns: data read from device (str representing data like 10 or True)
+
+        *Example*::
+
+            import BAC0
+            myIPAddr = '192.168.1.10'
+            bacnet = BAC0.ReadWriteScript(localIPAddr = myIPAddr)
+            bacnet.write('2:5 analogValue 1 presentValue 100')
+
+        Direct the controller at (Network 2, address 5) to write 100 to the presentValues of
+        its analogValue 1 (AV:1)
+        """
+        if not self._started:
+            raise ApplicationNotStarted("BACnet stack not running - use startApp()")
+        args = args.split()
+        self.log_title("Write property", args)
+
+        try:
+            # build a WriteProperty request
+            iocb = IOCB(self.build_wp_request(args, vendor_id=vendor_id))
+            iocb.set_timeout(timeout)
+            # pass to the BACnet stack
+            deferred(self.this_application.request_io, iocb)
+            self._log.debug("{:<20} {!r}".format("iocb", iocb))
+
+        except WritePropertyException as error:
+            # construction error
+            self._log.exception("exception: {!r}".format(error))
+
+        iocb.wait()  # Wait for BACnet response
+
+        if iocb.ioResponse:  # successful response
+            apdu = iocb.ioResponse
+
+            if not isinstance(iocb.ioResponse, SimpleAckPDU):  # expect an ACK
+                self._log.warning("Not an ack, see debug for more infos.")
+                self._log.debug(
+                    "Not an ack. | APDU : {} / {}".format((apdu, type(apdu)))
+                )
+                return
+
+        if iocb.ioError:  # unsuccessful: error/reject/abort
+            apdu = iocb.ioError
+            reason = find_reason(apdu)
+            raise NoResponseFromController("APDU Abort Reason : {}".format(reason))
+
+    def build_wp_request(self, args, vendor_id=0):
+        addr, obj_type, obj_inst, prop_id = args[:4]
+        vendor_id = vendor_id
+        if obj_type.isdigit():
+            obj_type = int(obj_type)
+        obj_inst = int(obj_inst)
+        value = args[4]
+
+        indx = None
+        if len(args) >= 6:
+            if args[5] != "-":
+                indx = int(args[5])
+
+        priority = None
+        if len(args) >= 7:
+            priority = int(args[6])
+
+        # get the datatype
+        if prop_id.isdigit():
+            prop_id = int(prop_id)
+        datatype = get_datatype(obj_type, prop_id, vendor_id=vendor_id)
+
+        self.log_subtitle("Creating Request")
+        self._log.debug(
+            "{:<20} {:<20} {:<20} {:<20}".format(
+                "indx", "priority", "datatype", "value"
+            )
+        )
+        self._log.debug(
+            "{!r:<20} {!r:<20} {!r:<20} {!r:<20}".format(
+                indx, priority, datatype, value
+            )
+        )
+
+        # change atomic values into something encodeable, null is a special
+        # case
+        if value == "null":
+            value = Null()
+
+        elif issubclass(datatype, Atomic):
+            if datatype is Integer:
+                value = int(value)
+            elif datatype is Real:
+                value = float(value)
+            elif datatype is Unsigned:
+                value = int(value)
+            elif datatype is Enumerated:
+                value = int(value)
+            value = datatype(value)
+
+        elif issubclass(datatype, Array) and (indx is not None):
+            if indx == 0:
+                value = Integer(value)
+            elif issubclass(datatype.subtype, Atomic):
+                value = datatype.subtype(value)
+            elif not isinstance(value, datatype.subtype):
+                raise TypeError(
+                    "invalid result datatype, expecting {}".format(
+                        (datatype.subtype.__name__,)
+                    )
+                )
+
+        elif not isinstance(value, datatype):
+            raise TypeError(
+                "invalid result datatype, expecting {}".format((datatype.__name__,))
+            )
+        self._log.debug("{:<20} {!r} {}".format("Encodeable value", value, type(value)))
+
+        # build a request
+        request = WritePropertyRequest(
+            objectIdentifier=(obj_type, obj_inst), propertyIdentifier=prop_id
+        )
+        request.pduDestination = Address(addr)
+
+        # save the value
+        request.propertyValue = Any()
+        try:
+            request.propertyValue.cast_in(value)
+        except WritePropertyCastError as error:
+            self._log.error("WriteProperty cast error: {!r}".format(error))
+
+        # optional array index
+        if indx is not None:
+            request.propertyArrayIndex = indx
+
+        # optional priority
+        if priority is not None:
+            request.priority = priority
+
+        self._log.debug("{:<20} {}".format("REQUEST", request))
+        return request
