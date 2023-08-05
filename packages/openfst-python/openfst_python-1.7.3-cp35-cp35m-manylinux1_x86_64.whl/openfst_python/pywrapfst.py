2d151579f841318c913c1a4df3f952193d5b4be1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:41 +0000

    Add package openfst-python/1.7.3/openfst_python-1.7.3-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/openfst-python/openfst_python-1.7.3-cp35-cp35m-manylinux1_x86_64.whl/openfst_python/pywrapfst.py
@@ -0,0 +1,17 @@
+def __bootstrap__():
+   global __bootstrap__, __file__, __loader__
+   import sys, os, pkg_resources, imp
+   __file__ = pkg_resources.resource_filename(__name__,'pywrapfst.cpython-35m-x86_64-linux-gnu.so')
+   del __bootstrap__
+   if '__loader__' in globals():
+       del __loader__
+
+   old_dir = os.getcwd()
+   try:
+     os.chdir(os.path.dirname(__file__))
+
+     imp.load_dynamic(__name__,__file__)
+   finally:
+
+     os.chdir(old_dir)
+__bootstrap__()
