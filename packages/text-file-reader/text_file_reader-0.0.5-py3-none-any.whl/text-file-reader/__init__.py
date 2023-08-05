c4988b7cb8f7fd1a6cdeff9d82fb5f103cb3353d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:00 +0000

    Add package text-file-reader/0.0.5/text_file_reader-0.0.5-py3-none-any.whl

---
+++ packages/text-file-reader/text_file_reader-0.0.5-py3-none-any.whl/text-file-reader/__init__.py
@@ -0,0 +1,41 @@
+import os
+import sys
+from shutil import copyfile
+
+
+if len(sys.argv) < 2:
+    raise Exception('Specify interpreter as argument.')
+
+# add files to bin directory so they can be run from anywhere.
+
+username = os.getcwd().split('/')[2]
+bin_dir = f"/Users/{username}/bin"
+cwd = os.path.abspath(os.path.dirname(__file__))
+
+# create bin directory if it doesn't already exist
+if not os.path.isdir(bin_dir):
+    os.mkdir(bin_dir)
+
+# create ascii-art and popup dirs
+try:
+    os.mkdir(bin_dir + '/ascii-art')
+    os.mkdir(bin_dir + '/ascii-art/popups')
+except FileExistsError:
+    pass
+
+# copy files from cwd to bin
+files = ['encrypt.py', 'helpscreen', 'separate_mainloops.py',
+        'ascii-art/sign-up.txt', 'ascii-art/popups/donate1.txt',
+        'ascii-art/popups/donate2.txt', 'ascii-art/popups/twitter.txt']
+cwd_paths = [cwd + '/' + file for file in files]
+bin_paths = [bin_dir + '/' + file for file in files]
+for cwd_path, bin_path in zip(cwd_paths, bin_paths):
+    copyfile(cwd_path, bin_path)
+
+
+# add shebang with specified interpreter to file-reader.py
+with open(f'{cwd}/file-reader.py', 'r') as f:
+    source = f.read()
+
+with open(f'{bin_dir}/file-reader.py', 'w+') as f:
+    f.write(f'#!{sys.argv[1]}\n\n{source}')
