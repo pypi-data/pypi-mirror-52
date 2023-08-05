fa0f351ee500bbe9401c99d28e13a57e248fdf9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:25 +0000

    Add package gud/1.0.15/gud-1.0.15.tar.gz

---
+++ packages/gud/gud-1.0.15.tar.gz/gud-1.0.15/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gud",
+    version="1.0.15",
+    author="James Whiteman",
+    author_email="james.whiteman@gmail.com",
+    description="branch of greengo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/genunity/gud.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'fire',
+        'boto3',
+        'botocore',
+        'pyyaml'
+    ],
+    entry_points={
+        'console_scripts': ['gud=gud.gud:main'],
+    },
+    license='MIT'
+)
