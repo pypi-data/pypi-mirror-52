8e6500dce5b8a3591d9c1f7e45ef627ffcee9003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:31 +0000

    Add package bw2agg/0.1.2/bw2agg-0.1.2.tar.gz

---
+++ packages/bw2agg/bw2agg-0.1.2.tar.gz/bw2agg-0.1.2/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='bw2agg',
+    version='0.1.2',
+    author="Pascal Lesage",
+    author_email="pascal.lesage@polymtl.ca",
+    description="Extension to brightway2 package to create and work with aggregated data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pascallesage/brightway2-aggregated",
+    install_requires=[
+        'bw2calc',
+        'bw2data',
+        'numpy',
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+)
