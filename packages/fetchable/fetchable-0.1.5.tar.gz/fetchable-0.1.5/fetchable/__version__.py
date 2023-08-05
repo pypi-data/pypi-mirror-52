6f929e4b8768de1ec558d46e2c2a5f28dcaa8946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:30 +0000

    Add package fetchable/0.1.5/fetchable-0.1.5.tar.gz

---
+++ packages/fetchable/fetchable-0.1.5.tar.gz/fetchable-0.1.5/fetchable/__version__.py
@@ -0,0 +1,7 @@
+__name__ = 'fetchable'
+__version__ = '0.1.5'
+__description__ = 'Client SDK for Fetchable'
+__author__ = 'Fetchable'
+__author_email__ = 'support@fetchable.ai'
+__license__ = 'Apache License 2.0'
+__copyright__ = 'Copyright 2019 Fetchable'
---
+++ packages/fetchable/fetchable-0.1.5.tar.gz/fetchable-0.1.5/setup.py
@@ -0,0 +1,34 @@
+
+import os
+from setuptools import setup, find_packages
+
+from fetchable.__version__ import __name__, __version__, __description__
+from fetchable.__version__ import __author__, __author_email__, __license__
+
+__url__ = ''
+setup(name=__name__,
+    version=__version__,
+    description=__description__,
+    project_urls={
+        'Source': 'https://github.com/fetchableai/fetchable-python',
+        'Issue tracker': 'https://github.com/fetchableai/fetchable-python/issues',
+        'Company': 'https://fetchable.ai'
+    },
+    author=__author__,
+    author_email=__author_email__,
+    license=__license__,
+    packages=find_packages(exclude=['examples*']),
+    long_description=open('README.rst', mode="r").read(),
+    long_description_content_type='text/x-rst',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries '
+    ]
+)
