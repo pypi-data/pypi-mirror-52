8cf48c12e65be1ce44e27dcc22805534f99a3cbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:42 +0000

    Add package Authlog/0.9.1/Authlog-0.9.1.tar.gz

---
+++ packages/Authlog/Authlog-0.9.1.tar.gz/Authlog-0.9.1/authlog/apps.py
@@ -0,0 +1,11 @@
+from django import apps
+
+
+class AppConfig(apps.AppConfig):
+    name = 'authlog'
+
+    def ready(self):
+        from django.contrib.auth import views as auth_views
+        from authlog.decorators import watch_login
+
+        auth_views.login = watch_login(auth_views.login)
---
+++ packages/Authlog/Authlog-0.9.1.tar.gz/Authlog-0.9.1/authlog/management/commands/authlog_dumpview.py
@@ -0,0 +1,35 @@
+import csv
+import sys
+import unicodedata
+from authlog.models import AccessPage
+from django.core.management.base import BaseCommand
+
+CSV_HEADERS = [
+    'login_time', 'ip_address', 'ip_forward', 'user', 'path_info', 'user_agent',
+    'get_data', 'post_data', 'http_accept', ]
+
+
+class Command(BaseCommand):
+    args = 'None'
+    help = 'Dump the Page View Log to stdout in CSV format.'
+
+    def safe_unicode(self, input_string):
+        return unicode(unicodedata.normalize(
+            'NFKD', input_string).encode('ascii', 'ignore'))
+
+    def handle(self, *args, **options):
+
+        csv_writer = csv.writer(sys.stdout)
+        csv_writer.writerow(CSV_HEADERS)
+        for record in AccessPage.objects.all():
+            csv_writer.writerow([
+                record.access_time,
+                self.safe_unicode(record.ip_address),
+                self.safe_unicode(record.ip_forward),
+                self.safe_unicode(record.user),
+                self.safe_unicode(record.path_info),
+                self.safe_unicode(record.user_agent),
+                self.safe_unicode(record.get_data),
+                self.safe_unicode(record.post_data),
+                self.safe_unicode(record.http_accept),
+            ])
