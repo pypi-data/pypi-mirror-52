225cbac478e90344e66bdaeb912240cf88a5eaee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:52 +0000

    Add package fancy-nlp/0.0.2/fancy_nlp-0.0.2-py3-none-any.whl

---
+++ packages/fancy-nlp/fancy_nlp-0.0.2-py3-none-any.whl/fancy_nlp/applications/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from .ner import NER
+from .text_classification import TextClassification
---
+++ packages/fancy-nlp/fancy_nlp-0.0.2-py3-none-any.whl/fancy_nlp/preprocessors/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from .ner_preprocessor import NERPreprocessor
+from .text_classification_preprocessor import TextClassificationPreprocessor
