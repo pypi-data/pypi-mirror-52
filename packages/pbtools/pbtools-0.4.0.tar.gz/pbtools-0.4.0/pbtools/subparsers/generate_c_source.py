3a7172f476727f7a5671c9135e0819687b26e975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:31 +0000

    Add package pbtools/0.4.0/pbtools-0.4.0.tar.gz

---
+++ packages/pbtools/pbtools-0.4.0.tar.gz/pbtools-0.4.0/pbtools/subparsers/generate_c_source.py
@@ -0,0 +1,45 @@
+import os
+import shutil
+
+from ..parser import parse_file
+from ..c_source import camel_to_snake_case
+from ..c_source import generate
+
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+def _do_generate_c_source(args):
+    parsed = parse_file(args.infile)
+
+    basename = os.path.basename(args.infile)
+    name = os.path.splitext(basename)[0]
+    name = camel_to_snake_case(name)
+
+    filename_h = f'{name}.h'
+    filename_c = f'{name}.c'
+
+    header, source = generate(name, parsed, filename_h)
+
+    with open(filename_h, 'w') as fout:
+        fout.write(header)
+
+    with open(filename_c, 'w') as fout:
+        fout.write(source)
+
+    for filename in ['pbtools.h', 'pbtools.c']:
+        shutil.copy(
+            os.path.join(SCRIPT_DIR, f'../c_source/{filename}'),
+            '.')
+
+    print(f'Successfully created pbtools.[hc] and {name}.[hc].')
+
+
+def add_subparser(subparsers):
+    generate_c_source_parser = subparsers.add_parser(
+        'generate_c_source',
+        description='Generate C source code from given protobuf file.')
+    generate_c_source_parser.add_argument(
+        'infile',
+        help='Input protobuf file.')
+    generate_c_source_parser.set_defaults(func=_do_generate_c_source)
