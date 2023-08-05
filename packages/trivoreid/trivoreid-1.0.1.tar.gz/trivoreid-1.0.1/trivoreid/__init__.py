99d1770eef6a1ace1d86e3fd6c15ef18544e34b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:07 +0000

    Add package trivoreid/1.0.1/trivoreid-1.0.1.tar.gz

---
+++ packages/trivoreid/trivoreid-1.0.1.tar.gz/trivoreid-1.0.1/trivoreid/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+__licence__           = 'BSD-3'
+__version__           = '1.0.1'
+__description__       = 'A Python wrapper around Trivore ID API'
+
+from . import exceptions
+from . import models
+from . import utils
+from . import services
