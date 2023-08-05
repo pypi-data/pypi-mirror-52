c5f2b3cd8eca7ee0c51c502ec5d352e5f63ae2ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:45 +0000

    Add package py2md/0.0.4/py2md-0.0.4-py3-none-any.whl

---
+++ packages/py2md/py2md-0.0.4-py3-none-any.whl/classes/mdheading.py
@@ -0,0 +1,16 @@
+class MDHeading(object):
+    heading = None
+    level = None
+    def __init__(self, heading: str, level: int):
+        self.heading = heading
+        self.level = level
+    def __str__(self):
+        mdstr = ''
+        for _ in range(self.level):
+            mdstr += '#'
+        mdstr += ' '
+        mdstr += self.heading
+        mdstr += '\n'
+        return mdstr
+    def _repr_markdown_(self):
+        return self.__str__()
