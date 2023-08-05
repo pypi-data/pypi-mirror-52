cfe8311da09107821ec4322ac50a95af358e1638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:24 +0000

    Add package JlbSalary/1.0/JlbSalary-1.0.tar.gz

---
+++ packages/JlbSalary/JlbSalary-1.0.tar.gz/JlbSalary-1.0/JlbSalary/salary.py
@@ -0,0 +1,8 @@
+"""薪资计算"""
+def yearSalary(monthSalary):
+    """根据月薪计算年薪"""
+    return monthSalary*12
+def daySalary(monthSalary):
+    """根据月薪计算日薪"""
+    return monthSalary/22.5
+
---
+++ packages/JlbSalary/JlbSalary-1.0.tar.gz/JlbSalary-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='JlbSalary',
+    version='1.0',
+    description='根据传入的月薪计算年薪、日新',
+    author='jlb',
+    author_email='715867400@qq.com',
+    py_modules=['JlbSalary.salary']
+)
