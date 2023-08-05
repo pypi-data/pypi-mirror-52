474d176619850019be5201b86edb408dd3b8b74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:31 +0000

    Add package veracitysdk/0.1/veracitysdk-0.1.tar.gz

---
+++ packages/veracitysdk/veracitysdk-0.1.tar.gz/veracitysdk-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+	name = "veracitysdk",
+	version = "0.1",
+	description = "Veracity api library",
+	author = "schen",
+	author_email = "shu@disinformationindex.org",
+	url = "https://bitbucket.org/disinformationindex/veracitysdk/src",
+	py_modules = ["veracitysdk"],
+)
