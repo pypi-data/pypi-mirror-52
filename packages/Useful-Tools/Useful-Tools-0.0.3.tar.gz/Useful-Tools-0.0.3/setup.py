8f32cd15de93bc2dbe620fcc66d962a0ff059462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:46 +0000

    Add package Useful-Tools/0.0.3/Useful-Tools-0.0.3.tar.gz

---
+++ packages/Useful-Tools/Useful-Tools-0.0.3.tar.gz/Useful-Tools-0.0.3/setup.py
@@ -0,0 +1,14 @@
+﻿# !usr\bin\env python
+# -*- coding:utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='Useful-Tools',
+    version='0.0.3',
+    author='zltzlt',
+    author_email='1668151593@qq.com',
+    url='https://github.com/zhangletao/Tools',
+    description='Some useful tools.',
+    long_description='',
+    packages=['Tools', 'Tools/Common', 'Tools/Files', 'Tools/Math'],
+    install_requires=['pywin32', 'PyPDF2'])
