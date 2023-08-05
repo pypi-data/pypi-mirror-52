e461102ad3f67b65bf1015d9a793d9175b4bd9e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:49 +0000

    Add package mergexp/0.1.6/mergexp-0.1.6.tar.gz

---
+++ packages/mergexp/mergexp-0.1.6.tar.gz/mergexp-0.1.6/mergexp/unit/units.py
@@ -0,0 +1,21 @@
+
+# information storage: base unit = byte
+
+def gb(value):
+    return value * 1e9
+
+# time: base unit = nanoseconds
+
+def ms(value):
+    return value * 1e6
+
+def us(value):
+    return value * 1e3
+
+# network capacity: base unit = bits per second
+
+def mbps(value):
+    return value * 1e6
+
+def gbps(value):
+    return value * 1e6
