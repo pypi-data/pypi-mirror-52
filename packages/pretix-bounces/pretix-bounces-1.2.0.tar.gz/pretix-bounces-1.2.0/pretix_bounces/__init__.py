39ad0348003236d615d38479af8e62c8312ca82a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:30 +0000

    Add package pretix-bounces/1.2.0/pretix-bounces-1.2.0.tar.gz

---
+++ packages/pretix-bounces/pretix-bounces-1.2.0.tar.gz/pretix-bounces-1.2.0/pretix_bounces/__init__.py
@@ -0,0 +1,27 @@
+from django.apps import AppConfig
+from django.conf import settings
+
+try:
+    from pretix.base.plugins import PluginConfig
+except ImportError:
+    raise RuntimeError("Please use pretix 2.7 or above to run this plugin!")
+
+
+class PluginApp(AppConfig):
+    name = 'pretix_bounces'
+    verbose_name = 'Bounce processing for pretix'
+
+    class PretixPluginMeta:
+        name = 'Bounce processing for pretix'
+        author = 'Raphael Michel'
+        description = 'Allows automatic processing of bounces or automatic replies to emails sent by pretix.'
+        visible = False
+        version = '1.2.0'
+        compatibility = "pretix>=3.1.0"
+
+    def ready(self):
+        settings.CORE_MODULES.add('pretix_bounces')
+        from . import signals  # NOQA
+
+
+default_app_config = 'pretix_bounces.PluginApp'
