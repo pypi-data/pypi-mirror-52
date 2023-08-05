c16d1aa8c93269cd50b695edc1f97e8f1f1c2af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:52 +0000

    Add package JSRope/0.1.2/JSRope-0.1.2-py3-none-any.whl

---
+++ packages/JSRope/JSRope-0.1.2-py3-none-any.whl/jsrope/__init__.py
@@ -0,0 +1,17 @@
+"""
+You don't want to write JavaScript? Me, too. But, Let me handle it with Python.
+-------------------------------------------------------------------------------
+Powered by [Yamato Nagata](https://twitter.com/514YJ)
+
+[GitHub](https://github.com/delta114514/JSRope)
+
+    :copyright: (c) 2019 by Yamato Nagata.
+    :license: MIT.
+"""
+
+from .__about__ import __version__
+
+from .jsrope import (Element, find_element_by, Flow, EventHandler, Code, Date, If, Switch, For, Return, While, Function,
+                     true, false, Ajax, Bool, Util, Array, Expression, Object, Str, Int, Float, BaseJS, JS)
+
+from .util import negative, substitute, escape
