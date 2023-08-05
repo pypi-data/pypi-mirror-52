e71417da5736218fd3215b67a17803f406cdf30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:04 +0000

    Add package connectedcars/0.1.3/connectedcars-0.1.3-py3-none-any.whl

---
+++ packages/connectedcars/connectedcars-0.1.3-py3-none-any.whl/connectedcars/constants.py
@@ -0,0 +1,44 @@
+AUTH_URL = "https://auth-api.connectedcars.io/auth/login/email/password"
+API_URL = "https://api.connectedcars.io/graphql"
+API_DEFAULT_TIMEOUT = 30
+
+HEADER_NAMESPACE = "x-organization-namespace"
+
+QUERY_VEHICLE_OVERVIEW = """
+query User {
+    viewer {
+        vehicles {
+            vehicle {
+                id
+                licensePlate
+                unitSerial
+                make
+                model 
+                name
+                fuelEconomy
+                health {
+                    ok
+                    recommendation
+                }
+                ignition {
+                    time
+                    on
+                }
+                odometer { 
+                    time 
+                    odometer
+                }
+                fuelLevel { 
+                    time
+                    liter
+                }
+                position { 
+                    time
+                    latitude
+                    longitude 
+                }
+            }
+        }
+    }
+}
+"""
