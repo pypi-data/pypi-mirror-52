6bf22bf986b0b317eeeca3d0c28a7beccb1c6553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:17 +0000

    Add package unary-coding/1.0.3/unary_coding-1.0.3.tar.gz

---
+++ packages/unary-coding/unary_coding-1.0.3.tar.gz/unary_coding-1.0.3/unary_coding/__init__.py
@@ -0,0 +1,3 @@
+from .unary_coding import unary, inverted_unary
+
+__all__ = ["unary", "inverted_unary"]
---
+++ packages/unary-coding/unary_coding-1.0.3.tar.gz/unary_coding-1.0.3/unary_coding/unary_coding.py
@@ -0,0 +1,16 @@
+def _unary(n: int, zero: str, one: str) -> str:
+    return one*n + zero
+
+
+def unary(n: int) -> str:
+    """Return string representing given number in unary.
+        n:int, number to convert to unary.
+    """
+    return _unary(n, "0", "1")
+
+
+def inverted_unary(n: int) -> str:
+    """Return string representing given number in inverted unary.
+        n:int, number to convert to unary.
+    """
+    return _unary(n, "1", "0")
