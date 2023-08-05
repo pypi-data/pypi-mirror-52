04156c801e27deffcdb1b187295c8ecd376a77b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:39 +0000

    Add package airflow-docker-compose/0.1.4/airflow-docker-compose-0.1.4.tar.gz

---
+++ packages/airflow-docker-compose/airflow-docker-compose-0.1.4.tar.gz/airflow-docker-compose-0.1.4/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['airflow_docker_compose']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click', 'docker', 'docker-compose', 'python-dotenv', 'toml']
+
+entry_points = \
+{'console_scripts': ['airflow-docker-compose = '
+                     'airflow_docker_compose.__main__:run']}
+
+setup_kwargs = {
+    'name': 'airflow-docker-compose',
+    'version': '0.1.4',
+    'description': '',
+    'long_description': None,
+    'author': 'Dan Cardin',
+    'author_email': 'ddcardin@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/airflowdocker/airflow-docker-compose',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.5.0',
+}
+
+
+setup(**setup_kwargs)
