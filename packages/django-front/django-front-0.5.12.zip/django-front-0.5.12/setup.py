98d1ddfc834a3e892c89f32d8bbb4e669389c473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:16 +0000

    Add package django-front/0.5.12/django-front-0.5.12.zip

---
+++ packages/django-front/django-front-0.5.12.zip/django-front-0.5.12/setup.py
@@ -0,0 +1,62 @@
+from setuptools import setup, find_packages
+from setuptools.command.test import test as test_command
+import sys
+
+
+class Tox(test_command):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+
+    def initialize_options(self):
+        test_command.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        test_command.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+        args = self.tox_args
+        if args:
+            args = shlex.split(self.tox_args)
+        errno = tox.cmdline(args=args)
+        sys.exit(errno)
+
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+setup(
+    name='django-front',
+    version='0.5.12',
+    description='A Django application to allow of front-end editing',
+    long_description=long_description,
+    author='Marco Bonetti',
+    author_email='mbonetti@gmail.com',
+    url='https://github.com/mbi/django-front',
+    license='MIT',
+    packages=find_packages(exclude=['testproject', 'testproject.*']),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'django-classy-tags >= 0.4',
+        'Django >= 1.8',
+        'six'
+    ],
+    tests_require=['tox'],
+    cmdclass={'test': Tox},
+)
