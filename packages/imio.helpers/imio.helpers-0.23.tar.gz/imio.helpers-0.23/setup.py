d8f84e2ff97e0b43782d2991bcfe5a7360e8635b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:18:52 +0000

    Add package imio.helpers/0.23/imio.helpers-0.23.tar.gz

---
+++ packages/imio.helpers/imio.helpers-0.23.tar.gz/imio.helpers-0.23/setup.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""Installer for the imio.helpers package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+long_description = (
+    open('README.rst').read() + '\n' + 'Contributors\n============\n' + '\n' +
+    open('CONTRIBUTORS.rst').read() + '\n' + open('CHANGES.rst').read() + '\n')
+
+setup(
+    name='imio.helpers',
+    version='0.23',
+    description="Various helper methods for development.",
+    long_description=long_description,
+    # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='plone helpers utils dev imio',
+    author='Simon Delcourt',
+    author_email='simon.delcourt@imio.be',
+    url='http://pypi.python.org/pypi/imio.helpers',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['imio'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'plone.api',
+        'plone.app.intid',
+        'plone.dexterity',
+        'setuptools',
+        'Plone',
+        'collective.monkeypatcher',
+    ],
+    extras_require={
+        'test': [
+            'collective.behavior.talcondition',
+            'plone.app.testing',
+            'plone.app.dexterity',
+        ],
+        'pdf': [
+            'PyPDF2',
+            'reportlab',
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
