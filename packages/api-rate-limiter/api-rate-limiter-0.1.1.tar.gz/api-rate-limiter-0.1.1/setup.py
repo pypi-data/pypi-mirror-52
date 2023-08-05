7f0494ba90a71dc7104a323df117f12ab9fc0d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:20 +0000

    Add package api-rate-limiter/0.1.1/api-rate-limiter-0.1.1.tar.gz

---
+++ packages/api-rate-limiter/api-rate-limiter-0.1.1.tar.gz/api-rate-limiter-0.1.1/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="api-rate-limiter",
+    version="0.1.1",
+    description="Implement client side API rate limiting",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/museadmin/api-rate-limiter",
+    author="Bradley  Atkins",
+    author_email="bradley.atkinz@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    package_dir={'': 'api-rate-limiter'},
+    packages=find_packages("api-rate-limiter", exclude=("tests",)),
+    include_package_data=True,
+    install_requires=["queue", "time", "threading"],
+)
