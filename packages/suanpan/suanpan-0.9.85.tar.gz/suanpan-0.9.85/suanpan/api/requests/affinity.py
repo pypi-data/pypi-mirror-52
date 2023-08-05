70af8bd9fb56140b2db4df04afd7d2a89a6642de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:26 +0000

    Add package suanpan/0.9.85/suanpan-0.9.85.tar.gz

---
+++ packages/suanpan/suanpan-0.9.85.tar.gz/suanpan-0.9.85/suanpan/api/requests/affinity.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+from suanpan import g
+from suanpan.api import requests
+
+
+def getAffinityUrl(path, tls=g.affinityTls):
+    host = g.affinity or g.host
+    if not host:
+        raise Exception("Suanpan API call Error: affinity and host not set")
+    protocol = "https" if tls else "http"
+    return "{}://{}{}".format(protocol, host, path)
+
+
+def get(path, *args, **kwargs):
+    return requests.get(getAffinityUrl(path), *args, **kwargs)
+
+
+def post(path, *args, **kwargs):
+    return requests.post(getAffinityUrl(path), *args, **kwargs)
+
+
+def put(path, *args, **kwargs):
+    return requests.put(getAffinityUrl(path), *args, **kwargs)
+
+
+def delete(path, *args, **kwargs):
+    return requests.delete(getAffinityUrl(path), *args, **kwargs)
