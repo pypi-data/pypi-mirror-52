ad0c0be1a4ffdb97ef1a106a02eb400b6938ba0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:56 +0000

    Add package sqs-mq/0.2.0/sqs-mq-0.2.0.tar.gz

---
+++ packages/sqs-mq/sqs-mq-0.2.0.tar.gz/sqs-mq-0.2.0/setup.py
@@ -0,0 +1,48 @@
+"""
+# SQS-MQ
+"""
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="sqs-mq",
+    version="0.2.0",
+    license="MIT",
+    author="Mardix",
+    author_email="mcx2082@gmail.com",
+    description="A message queue system using AWS SQS",
+    url="https://github.com/mardix/sqs-mq",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=['sqs_mq'],
+    include_package_data=True,
+    packages=find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=[
+        "boto",
+        "python-slugify"
+    ],
+    keywords=["aws", "sqs", "mq", "queue", "amazon mq", "celery", "active mq",
+              "apollo", "beanstalkd", "resque", "rabbitmq" ,"gearman", "ironmq", "message"],
+    platforms='any',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    zip_safe=False
+)
