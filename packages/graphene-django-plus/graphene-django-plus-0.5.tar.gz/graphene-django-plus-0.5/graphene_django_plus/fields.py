0fd6f6546350d83de4171304a60d53574b5e87f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:55 +0000

    Add package graphene-django-plus/0.5/graphene-django-plus-0.5.tar.gz

---
+++ packages/graphene-django-plus/graphene-django-plus-0.5.tar.gz/graphene-django-plus-0.5/graphene_django_plus/fields.py
@@ -0,0 +1,17 @@
+import graphene
+from graphene import relay
+
+
+class CountableConnection(relay.Connection):
+    """Connection that provides a total_count attribute."""
+
+    class Meta:
+        abstract = True
+
+    #: Total objects count in the query.
+    total_count = graphene.Int(
+        description="The total count of objects in this query.",
+    )
+
+    def resolve_total_count(self, info, **kwargs):
+        return self.iterable.count()
