672a259f6166859942ed84241e2ba2be9912e7cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:36 +0000

    Add package skipole/3.5.4/skipole-3.5.4.tar.gz

---
+++ packages/skipole/skipole-3.5.4.tar.gz/skipole-3.5.4/skipole/__init__.py
@@ -0,0 +1,56 @@
+
+import sys, traceback
+
+from functools import wraps
+from importlib import import_module
+
+from .ski import skiboot
+
+from .ski.project_class_definition import WSGIApplication
+from .ski.excepts import ValidateError, ServerError, GoTo, FailPage
+
+version = skiboot.version()
+
+
+def set_debug(mode):
+    "If mode is True, this sets increased debug error messages to be displayed"
+    skiboot.set_debug(mode)
+
+
+
+# This 'use_submit_list' is available to wrap the submit_data function if required
+
+
+def use_submit_list(submit_data):
+    "Used to decorate submit_data to enable submit_list to define package,module,function"
+    @wraps(submit_data)
+    def submit_function(skicall):
+        "This function replaces submit_data, if skicall.submit_list has two or more elements"
+        if not skicall.submit_list:
+            # do nothing, simply return the original submit_data
+            return submit_data(skicall)
+        if len(skicall.submit_list) < 2:
+            # do nothing, simply return the original submit_data
+            return submit_data(skicall)
+        submitpath = ".".join(skicall.submit_list[:-1])
+        try:
+            submitmodule = import_module(submitpath)
+        except Exception:
+            if skiboot.get_debug():
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                str_list = traceback.format_exception(exc_type, exc_value, exc_traceback)
+                message = ''
+                for item in str_list:
+                    message += item
+                raise ServerError(message)
+            raise ServerError("Unable to import project module defined as %s in submit list" % (submitpath,))
+        # now obtain and run the specified function
+        try:
+            submitfunc = getattr(submitmodule, skicall.submit_list[-1])
+        except Exception:
+            raise ServerError("submit_list package %s found, but the required function %s is not recognised" % (submitpath, skicall.submit_list[-1]))
+        return submitfunc(skicall)
+    return submit_function
+
+
+
