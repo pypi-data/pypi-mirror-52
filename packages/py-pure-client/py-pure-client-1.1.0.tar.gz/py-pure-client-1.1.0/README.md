a50c8f18f7ba5bdcc5a5a0ea1dc2db3596236747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:43 +0000

    Add package py-pure-client/1.1.0/py-pure-client-1.1.0.tar.gz

---
+++ packages/py-pure-client/py-pure-client-1.1.0.tar.gz/py-pure-client-1.1.0/README.md
@@ -0,0 +1,32 @@
+# py-pure-client
+
+
+## Overview
+
+This Python package has clients that use the Pure1 and FlashArray REST APIs.
+
+For the current FlashBlade client, [see here](https://github.com/purestorage/purity_fb_python_client).
+
+## Requirements
+
+The library requires Python 2.7 and higher or Python 3.3 and higher. Third-party
+libraries are also required.
+
+## Installation
+
+### pip Installation
+```
+$ pip install py-pure-client
+```
+
+### Manual Installation
+```
+$ git clone https://github.com/PureStorage-OpenConnect/py-pure-client.git
+$ cd py-pure-client
+$ pip install -r requirements.txt
+$ python setup.py install
+```
+
+## Documentation
+
+For full documentation, including a quick start guide and examples, see https://py-pure-client.readthedocs.io/en/latest/.
---
+++ packages/py-pure-client/py-pure-client-1.1.0.tar.gz/py-pure-client-1.1.0/pypureclient/pure1/api/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from pypureclient.pure1.api.alerts_api import AlertsApi
+from pypureclient.pure1.api.arrays_api import ArraysApi
+from pypureclient.pure1.api.audits_api import AuditsApi
+from pypureclient.pure1.api.authorization_api import AuthorizationApi
+from pypureclient.pure1.api.file_system_snapshots_api import FileSystemSnapshotsApi
+from pypureclient.pure1.api.file_systems_api import FileSystemsApi
+from pypureclient.pure1.api.metrics_api import MetricsApi
+from pypureclient.pure1.api.network_interfaces_api import NetworkInterfacesApi
+from pypureclient.pure1.api.pods_api import PodsApi
+from pypureclient.pure1.api.volume_snapshots_api import VolumeSnapshotsApi
+from pypureclient.pure1.api.volumes_api import VolumesApi
