393ecf3348984c4ccdf37fce4f7087847f443a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:49 +0000

    Add package aionetbox/0.1.0/aionetbox-0.1.0-py3-none-any.whl

---
+++ packages/aionetbox/aionetbox-0.1.0-py3-none-any.whl/aionetbox/__init__.py
@@ -0,0 +1,9 @@
+"""AIO Netbox"""
+
+__title__ = 'aionetbox'
+__version__ = '0.1.0'
+__description__ = 'AIO Netbox'
+__author__ = 'Charles Butler'
+__author_email__ = 'chuck@vapor.io'
+__url__ = 'https://github.com/vapor-ware/aionetbox'
+__license__ = 'GNU General Public License v3.0'
