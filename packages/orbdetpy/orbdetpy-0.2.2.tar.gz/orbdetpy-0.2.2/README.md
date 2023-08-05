8838ab20f3e186fe8b095e1856962222c2a6633a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:32 +0000

    Add package orbdetpy/0.2.2/orbdetpy-0.2.2.tar.gz

---
+++ packages/orbdetpy/orbdetpy-0.2.2.tar.gz/orbdetpy-0.2.2/README.md
@@ -0,0 +1,122 @@
+Introduction
+------------
+
+This is orbdetpy, a library of Python and Java routines for orbit
+determination. It is a thin Python wrapper for our estimation tools
+and Orekit, which are both written in Java. 
+
+Features
+--------
+
+The force model for orbit propagation currently includes:
+
+1. EGM96 gravity field up to degree and order 360.
+2. Earth solid tides due to the influence of the Sun and Moon.
+3. FES 2004 ocean tide model up to degree and order 100.
+4. The NRL MSISE-00 and simple exponential models for atmospheric drag.
+5. Solar radiation pressure.
+6. Third body perturbations from the Sun and Moon.
+
+The measurement model supports range, range-rate, angles, and inertial
+Cartesian coordinates. Filtering is done using Orekit's Extended Kalman
+Filter or our custom Unscented Kalman Filter. Dynamic Model Compensation
+(DMC) can be used with either filter to estimate additional perturbing
+acclerations that result from unmodeled dynamics, maneuvers etc.
+
+You can either use your own measurements or simulate observations using
+the `simulateMeasurements()` function.
+
+Installation
+------------
+
+1. Install the Java Development Kit 8 (1.8) from
+   <http://openjdk.java.net/install/index.html>. Set the JAVA_HOME
+   environment variable to point to your JDK installation.
+2. Install Python 3.6+ and run `pip install orbdetpy` to install orbdetpy 
+   and other package dependencies from the Python Package Index (PyPI).
+   If you wish to use the `develop` or other experimental branches from
+   GitHub, `git clone` them and run `pip install -e .` from the top level
+   `orbdetpy` folder.
+3. Source code, example programs and data files can be downloaded from 
+   <https://github.com/ut-astria/orbdetpy>.
+
+The orbdetpy/lib folder contains JAR files for the following libraries,
+which are imported by orbdetpy automatically.
+
+1. <https://github.com/google/gson>
+2. <https://hipparchus.org> 
+3. <https://www.orekit.org>
+
+Update the astrodynamics data in orbdetpy/data periodically by calling
+the `update_data()` function in the `astrodata` module.
+
+Examples
+--------
+
+The following example programs can be found in the 'examples' folder.
+These examples use the Python wrapper interface but calling the
+underlying Java implementation directly is straightforward.
+
+1. `testsim.py` : Demonstrates the measurement simulator. Note that
+   maneuvers can be incorporated into the force model during simulation.
+
+2. `plotsim.py` : Plots the results of simulations created using testsim.py.
+
+3. `testodet.py` : Demonstrates orbit determination in orbdetpy.
+
+4. `plotodet.py` : Plots the results of fitting orbits using testodet.py.
+
+5. `run_tests.py` : Run all the use cases under examples/data. Simulated
+   measurements, orbit fits, differences between simulated truth versus
+   estimates, and 3-sigma of estimated covariances will be written to
+   output/ sub-folders.
+
+orbdetpy uses JSON files to store settings, measurements and estimation
+results. The files in examples/data show how to configure measurement
+simulation and orbit determination using radar or telescope data. The
+file docs/file_formats.md documents the structure of the JSON files.
+
+The following are some typical use cases. It is assumed that the current
+working directory is examples/data.
+
+1. Simulate state vectors and radar measurements:
+
+   `python ../testsim.py radar_sim_cfg.json sim_data.json`
+
+   This will run the simulation configured in radar_sim_cfg.json and
+   write simulated output to sim_data.json.
+
+2. Plot simulation results:
+
+   `python ../plotsim.py radar_sim_cfg.json sim_data.json`
+
+   This will plot the simulated data generated in (1).
+
+3. Run OD on simulated radar data:
+
+   `python ../testodet.py radar_od_cfg.json sim_data.json od_output.json`
+
+   This will run OD on the simulated radar data generated in (1)
+   using the OD configuration in radar_od_cfg.json and write OD
+   output to od_output.json.
+
+4. Plot OD results:
+
+   `python ../plotodet.py radar_od_cfg.json sim_data.json od_output.json`
+
+   This will plot the OD results from (3).
+
+Known Issues
+------------
+
+1. Java "Out of heap space" errors:
+
+   The Java Virtual Machine with default settings may run out of heap
+   space during long term simulations or orbit fits. The workaround
+   is to add the following to the top of your Python code, before
+   orbdetpy is imported. The value following "-Xmx" is the maximum
+   heap size you wish to assign to Java; "G" stands for gigabytes.
+
+   `import jnius_config`
+
+   `jnius_config.add_options("-Xmx2G")`
---
+++ packages/orbdetpy/orbdetpy-0.2.2.tar.gz/orbdetpy-0.2.2/orbdetpy/__init__.py
@@ -0,0 +1,132 @@
+# __init__.py - Package initialization routines.
+# Copyright (C) 2018-2019 University of Texas
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import io
+import os
+import glob
+import json
+
+_rootdir = os.path.dirname(os.path.abspath(__file__))
+_libsdir = os.path.join(_rootdir, "lib")
+_datadir = os.path.join(_rootdir, "data")
+
+cpath = ""
+csc = ":" if os.sep == "/" else ";"
+for jar in glob.iglob(os.path.join(_libsdir, "**"), recursive = True):
+    if (jar.endswith(".jar")):
+        cpath += jar + csc
+
+os.environ["CLASSPATH"] = cpath
+import jnius
+
+_Conversion = jnius.autoclass("org.astria.Conversion")
+_DataManager = jnius.autoclass("org.astria.DataManager")
+_Estimation = jnius.autoclass("org.astria.Estimation")
+_Simulation = jnius.autoclass("org.astria.Simulation")
+_Utilities = jnius.autoclass("org.astria.Utilities")
+_DataManager.initialize(_datadir)
+
+def read_param(p):
+    if (isinstance(p, str)):
+        if (os.path.isfile(p)):
+            with open(p, "r") as fp:
+                data = fp.read()
+        else:
+            data = p
+    elif (isinstance(p, io.TextIOBase)):
+        data = p.read()
+    else:
+        data = json.dumps(p)
+
+    return(data)
+
+def write_output_file(outfile, data):
+    if (isinstance(outfile, str)):
+        with open(outfile, "w") as fp:
+            fp.write(data)
+    elif (isinstance(outfile, io.TextIOBase)):
+        outfile.write(data)
+
+def simulateMeasurements(config, output_file = None):
+    """ Simulates measurement data given a configuration.
+
+    Args:
+        config: Simulation configuration (Dictionary, file name, text
+                file-like object, or JSON encoded string).
+        output_file: If specified, the measurements will be written to
+                     the file name or text file-like object given. 
+
+    Returns:
+        Simulated measurements in the same format as config (Dictionary 
+        or JSON encoded string).
+
+    """
+
+    obs = _Simulation(read_param(config)).simulateMeasurements()
+    if (output_file):
+        write_output_file(output_file, obs)
+    if (isinstance(config, dict)):
+        obs = json.loads(obs)
+    return(obs)
+
+def determineOrbit(config, meas, output_file = None):
+    """ Performs orbit determination given config and measurements.
+
+    Args:
+        config: OD configuration (Dictionary, file name, text
+                file-like object, or JSON encoded string).
+        meas: List of measurements (List, file name, text
+                file-like object, or JSON encoded string).
+        output_file: If specified, the orbit fit will be written to
+                     the file name or text file-like object given. 
+
+    Returns:
+        Orbit determination results in the same format as config 
+        (Dictionary or JSON encoded string).
+
+    """
+
+    fit = _Estimation(read_param(config),
+                      read_param(meas)).determineOrbit()
+    if (output_file):
+        write_output_file(output_file, fit)
+    if (isinstance(config, dict)):
+        fit = json.loads(fit)
+    return(fit)
+
+def transformFrame(srcframe, time, pva, destframe):
+    """ Transforms a state vector from one frame to another.
+
+    Args:
+        srcframe: Source reference frame ("EME2000", "GCRF",
+                  "ITRF", "MOD", "TOD", or "TEME").
+        time: State vector epoch (ISO-8601 formatted UTC string).
+        pva: State vector to transform [m, m, m, m/s, m/s, m/s] or 
+             [m, m, m, m/s, m/s, m/s, m/s^2, m/s^2, m/s^2].
+        destframe: Destination reference frame ("EME2000", "GCRF",
+                   "ITRF", "MOD", "TOD", or "TEME")..
+
+    Returns:
+        State vector transformed to the destination frame.
+
+    """
+    return(_Conversion.transformFrame(srcframe, time, pva, destframe))
+
+def iodGooding(gslat, gslon, gsalt, frame, tmstr, azi, ele,
+               rho1init, rho3init):
+    """ Performs Gooding initial orbit determination. """
+    return(_Utilities.iodGooding(gslat, gslon, gsalt, frame, tmstr,
+                                 azi, ele, rho1init, rho3init))
