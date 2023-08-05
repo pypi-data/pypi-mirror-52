56d8906e81f3d657b61c3dbb06c4c66051b826ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:35 +0000

    Add package wawa-logger/1.2/wawa_logger-1.2.tar.gz

---
+++ packages/wawa-logger/wawa_logger-1.2.tar.gz/wawa_logger-1.2/wawa_logger/__init__.py
@@ -0,0 +1,33 @@
+from requests import post
+
+url = "http://wpg1psint001.linux.wmic.ins:8001/log/pub"
+
+def publish(**kwargs):
+    """
+    sends data based on input to centralized source
+
+    params
+    :tool_name
+    :event_type
+    :source_host
+    :dest_host
+    :username
+    returns
+    :bool True|False
+    """
+    noval = "no_data"
+    payload = {
+        "tool_name": kwargs.get("tool_name"),
+        "event_type": kwargs.get("event_type"),
+        "source_host": kwargs.get("source_host", noval),
+        "dest_host": kwargs.get("dest_host", noval),
+        "username": kwargs.get("username", noval)
+    }
+    try:
+        r = post(url, json=payload)
+        if r.ok:
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(f"Exception: {e}")
