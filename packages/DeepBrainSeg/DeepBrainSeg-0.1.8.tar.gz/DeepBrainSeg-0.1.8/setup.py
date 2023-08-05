8841c5042a54b655a39024ee9836196f0cca1395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:06 +0000

    Add package DeepBrainSeg/0.1.8/DeepBrainSeg-0.1.8.tar.gz

---
+++ packages/DeepBrainSeg/DeepBrainSeg-0.1.8.tar.gz/DeepBrainSeg-0.1.8/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='DeepBrainSeg',  
+     version='0.1.8',
+     author="Avinash Kori",
+     author_email="koriavinash1@gmail.com",
+     description="Deep Learning tool for brain tumor segmentation.",
+     long_description=open("README.md").read(),
+   long_description_content_type="text/markdown",
+     url="https://github.com/koriavinash1/DeepBrainSeg",
+     packages=setuptools.find_packages(),
+     install_requires = [
+         'googledrivedownloader',
+         'torch',
+         'torchvision',
+         'nibabel',
+         'SimpleITK',
+         'tqdm',
+         'pandas',
+         'scipy',
+         'pydensecrf',
+         'scikit-image'
+         ],
+     classifiers=[
+         "Programming Language :: Python :: 3.5",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
