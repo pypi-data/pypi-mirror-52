878357f0e53ca4a3b49901315ff2c4bfd7f6fd6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:56 +0000

    Add package markdown-strikethrough/0.1.0/markdown-strikethrough-0.1.0.tar.gz

---
+++ packages/markdown-strikethrough/markdown-strikethrough-0.1.0.tar.gz/markdown-strikethrough-0.1.0/markdown_strikethrough/extension.py
@@ -0,0 +1,27 @@
+from markdown.extensions import Extension
+from markdown.postprocessors import Postprocessor
+import re
+
+def makeExtension(configs=None):
+    if configs is None:
+        return StrikethroughExtension()
+
+class StrikethroughExtension(Extension):
+    def extendMarkdown(self, md, md_globals):
+        postprocessor = StrikethroughPostprocessor(md)
+        md.postprocessors.add('strikethrough', postprocessor, '>raw_html')
+
+class StrikethroughPostprocessor(Postprocessor):
+    """
+    adds strikethrough
+    """
+
+    pattern = re.compile(r'~~(((?!~~).)+)~~')
+
+    def run(self, html):
+        result = re.sub(self.pattern, self.convert, html)
+        print(result)
+        return result
+
+    def convert(self, match):
+        return '<del>' + match.group(1) + '</del>'
