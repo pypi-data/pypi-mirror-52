0c65e5fab851b790ff13c8f8e12bcaf00e9d300a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:19:07 +0000

    Add package pyqpanda/3.6.1/pyqpanda-3.6.1-cp37-none-manylinux1_x86_64.whl

---
+++ packages/pyqpanda/pyqpanda-3.6.1-cp37-none-manylinux1_x86_64.whl/pyqpanda/__init__.py
@@ -0,0 +1,13 @@
+'''
+QPanda Python\n
+Copyright (C) Origin Quantum 2017-2018\n
+Licensed Under Apache Licence 2.0
+'''
+from .utils import *
+from .pyQPanda import *
+from .Operator.pyQPandaOperator import *
+from .ChemiQ.pyQPandaChemiQ import *
+from .Variational import back
+
+One=True
+Zero=False
