b1ea52392f86cc0c650ab81a6886c51c64779da4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:27 +0000

    Add package saga-vcs/0.0.1/saga-vcs-0.0.1.tar.gz

---
+++ packages/saga-vcs/saga-vcs-0.0.1.tar.gz/saga-vcs-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="saga-vcs",
+    version="0.0.1",
+    author="saga",
+    author_email="narush@wharton.upenn.edu.com",
+    description="saga is a set of tools for version control",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/naterush/saga",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
