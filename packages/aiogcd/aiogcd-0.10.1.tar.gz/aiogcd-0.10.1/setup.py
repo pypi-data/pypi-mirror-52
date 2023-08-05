ff491630b3935df48c9904cf4b52971a53355181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:14 +0000

    Add package aiogcd/0.10.1/aiogcd-0.10.1.tar.gz

---
+++ packages/aiogcd/aiogcd-0.10.1.tar.gz/aiogcd-0.10.1/setup.py
@@ -0,0 +1,55 @@
+"""setup.py
+
+Created on: May 19, 2017
+   Authors: Jeroen van der Heijden <jeroen@transceptor.technology>
+            jomido <https://github.com/jomido>
+            egalpin <https://github.com/egalpin>
+
+Upload to PyPI, Thx to: http://peterdowns.com/posts/first-time-with-pypi.html
+
+python3 setup.py register -r pypitest
+python3 setup.py sdist upload -r pypitest
+
+python3 setup.py register -r pypi
+python3 setup.py sdist upload -r pypi
+"""
+
+import setuptools
+from distutils.core import setup, Extension
+
+VERSION = '0.10.1'
+
+install_requires = [
+    'aiohttp>=2',
+    'PyJWT>=1',
+    'cryptography>=1',
+    'asyncio_extras>=1'
+]
+
+setup(
+    name='aiogcd',
+    packages=[
+        'aiogcd',
+        'aiogcd.connector',
+        'aiogcd.orm',
+        'aiogcd.orm.properties'],
+    version=VERSION,
+    description='Async Google Cloud Datastore API',
+    author='Jeroen van der Heijden',
+    author_email='jeroen@transceptor.technology',
+    url='https://github.com/transceptor-technology/aiogcd',
+    download_url='https://'
+        'github.com/transceptor-technology/'
+        'aiogcd/tarball/{}'.format(VERSION),
+    keywords=['gcd', 'datastore', 'connector'],
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development'
+    ],
+)
