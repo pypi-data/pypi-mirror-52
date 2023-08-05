88d0c8255bc7c49a2cc6478bb7d93413aa405107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:39 +0000

    Add package pyexlatex/0.7.1/pyexlatex-0.7.1.tar.gz

---
+++ packages/pyexlatex/pyexlatex-0.7.1.tar.gz/pyexlatex-0.7.1/pyexlatex/resume/__init__.py
@@ -0,0 +1,6 @@
+from pyexlatex.resume.resume import Resume
+from pyexlatex.resume.models.employment import Employment
+from pyexlatex.resume.templates.education import Education
+from pyexlatex.resume.templates.publication import Publication
+from pyexlatex.resume.templates.section import SpacedSection
+from pyexlatex.resume.templates.award import Award
