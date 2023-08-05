59cd343a02d6cd2cfc351d70af430075a4a0069b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:22 +0000

    Add package pandalone/0.2.8/pandalone-0.2.8-py2.py3-none-any.whl

---
+++ packages/pandalone/pandalone-0.2.8-py2.py3-none-any.whl/pandalone/_version.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#
+# Copyright 2014-2015 European Commission (JRC);
+# Licensed under the EUPL (the 'Licence');
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+"""Authoritative project's version-info"""
+__version__ = "0.2.8"  # PEP 440 versioning. Also update README.rst's.
+__updated__ = "2019-09-07 11:14:00"
+version = __version__
