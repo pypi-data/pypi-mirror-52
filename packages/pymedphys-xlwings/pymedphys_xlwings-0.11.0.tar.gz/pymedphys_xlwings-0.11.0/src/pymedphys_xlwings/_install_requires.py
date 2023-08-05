43c8470c03607ddbfb46dcdf5828eb2f54db355d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:20 +0000

    Add package pymedphys-xlwings/0.11.0/pymedphys_xlwings-0.11.0.tar.gz

---
+++ packages/pymedphys-xlwings/pymedphys_xlwings-0.11.0.tar.gz/pymedphys_xlwings-0.11.0/src/pymedphys_xlwings/_install_requires.py
@@ -0,0 +1,9 @@
+install_requires = [
+    "numpy >= 1.12",
+    "pydicom",
+    "pymedphys_dicom >= 0.11.0, < 0.12.0",
+    "pymedphys_fileformats >= 0.11.0, < 0.12.0",
+    "pymedphys_utilities >= 0.11.0, < 0.12.0",
+    "scipy",
+    'xlwings ; platform_system != "Linux"',
+]
