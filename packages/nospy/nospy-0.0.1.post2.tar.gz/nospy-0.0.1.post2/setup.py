43b1e8f6869f3800fb79187661bdd43f2fa941ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:51 +0000

    Add package nospy/0.0.1.post2/nospy-0.0.1.post2.tar.gz

---
+++ packages/nospy/nospy-0.0.1.post2.tar.gz/nospy-0.0.1.post2/setup.py
@@ -0,0 +1,35 @@
+"""
+Setup script for NOSpy
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+version = "0.0.1-2" #NOTE: please blame pypy for the weird version numbers...
+
+setup(
+    name='nospy',
+    version=version,
+    description="Network Object Store",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/dsikes/nospy',
+    author='Dan Sikes',
+    author_email='dsikes@atlantean.io',
+    keywords='network object store binary data datastore fast',
+    packages=find_packages(),
+    install_requires=['requests'],
+
+    project_urls={
+        'Source': 'https://github.com/dsikes/nospy',
+    },
+)
