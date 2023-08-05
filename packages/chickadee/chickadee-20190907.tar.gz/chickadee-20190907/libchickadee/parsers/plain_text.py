430847028199e4e8ab11b52b4f020dd7f5d6550e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:47 +0000

    Add package chickadee/20190907/chickadee-20190907.tar.gz

---
+++ packages/chickadee/chickadee-20190907.tar.gz/chickadee-20190907/libchickadee/parsers/plain_text.py
@@ -0,0 +1,81 @@
+"""Parse IP addresses from plain text files
+and feed to the Chickadee GeoIP API
+
+Plain text files include logs, csvs, json, and other formats where ascii strings
+contain IPv4 or IPv6 addresses.
+"""
+
+import binascii
+import os
+from gzip import GzipFile
+import re
+
+__author__ = 'Chapin Bryce'
+__date__ = 20190907
+__license__ = 'GPLv3 Copyright 2019 Chapin Bryce'
+__desc__ = '''Yet another GeoIP resolution tool.'''
+
+# FROM https://gist.github.com/dfee/6ed3a4b05cfe7a6faf40a2102408d5d8
+IPV4SEG  = r'(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])'
+IPV4ADDR = r'(?:(?:' + IPV4SEG + r'\.){3,3}' + IPV4SEG + r')'
+IPV6SEG  = r'(?:(?:[0-9a-fA-F]){1,4})'
+IPV6GROUPS = (
+    r'(?:' + IPV6SEG + r':){7,7}' + IPV6SEG,                  # 1:2:3:4:5:6:7:8
+    r'(?:' + IPV6SEG + r':){1,7}:',                           # 1::                                 1:2:3:4:5:6:7::
+    r'(?:' + IPV6SEG + r':){1,6}:' + IPV6SEG,                 # 1::8               1:2:3:4:5:6::8   1:2:3:4:5:6::8
+    r'(?:' + IPV6SEG + r':){1,5}(?::' + IPV6SEG + r'){1,2}',  # 1::7:8             1:2:3:4:5::7:8   1:2:3:4:5::8
+    r'(?:' + IPV6SEG + r':){1,4}(?::' + IPV6SEG + r'){1,3}',  # 1::6:7:8           1:2:3:4::6:7:8   1:2:3:4::8
+    r'(?:' + IPV6SEG + r':){1,3}(?::' + IPV6SEG + r'){1,4}',  # 1::5:6:7:8         1:2:3::5:6:7:8   1:2:3::8
+    r'(?:' + IPV6SEG + r':){1,2}(?::' + IPV6SEG + r'){1,5}',  # 1::4:5:6:7:8       1:2::4:5:6:7:8   1:2::8
+    IPV6SEG + r':(?:(?::' + IPV6SEG + r'){1,6})',             # 1::3:4:5:6:7:8     1::3:4:5:6:7:8   1::8
+    r':(?:(?::' + IPV6SEG + r'){1,7}|:)',                     # ::2:3:4:5:6:7:8    ::2:3:4:5:6:7:8  ::8       ::
+    r'fe80:(?::' + IPV6SEG + r'){0,4}%[0-9a-zA-Z]{1,}',       # fe80::7:8%eth0     fe80::7:8%1  (link-local IPv6 addresses with zone index)
+    r'::(?:ffff(?::0{1,4}){0,1}:){0,1}[^\s:]' + IPV4ADDR,     # ::255.255.255.255  ::ffff:255.255.255.255  ::ffff:0:255.255.255.255 (IPv4-mapped IPv6 addresses and IPv4-translated addresses)
+    r'(?:' + IPV6SEG + r':){1,4}:[^\s:]' + IPV4ADDR,          # 2001:db8:3:4::192.0.2.33  64:ff9b::192.0.2.33 (IPv4-Embedded IPv6 Address)
+)
+IPV6ADDR = '|'.join(['(?:{})'.format(g) for g in IPV6GROUPS[::-1]])  # Reverse rows for greedy match
+
+IPv4 = re.compile(IPV4ADDR)
+IPv6 = re.compile(IPV6ADDR)
+
+class PlainTextParser(object):
+    def __init__(self):
+        self.file_entry = None
+        self.ips = set()
+
+    @staticmethod
+    def is_gz_file(filepath):
+        with open(filepath, 'rb') as test_f:
+            return binascii.hexlify(test_f.read(2)) == b'1f8b'
+
+    def parse_file(self, file_entry):
+        if self.is_gz_file(file_entry):
+            file_data = GzipFile(file_entry)
+        else:
+            file_data = open(file_entry, 'rb')
+
+        for raw_line in file_data:
+            line = raw_line.decode()
+            for ipv4 in IPv4.findall(line):
+                self.ips.add(ipv4)
+            for ipv6 in IPv6.findall(line):
+                if '%' in ipv6:
+                    ip, _ = ipv6.split('%')
+                else:
+                    ip = ipv6
+                self.ips.add(ip)
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', help="File or folder to parse")
+    args = parser.parse_args()
+
+    ptparser = PlainTextParser()
+    if os.path.isdir(args.path):
+        for root, _, files in os.walk(args.path):
+            for fentry in files:
+                ptparser.parse_file(os.path.join(root, fentry))
+    else:
+        ptparser.parse_file(args.path)
+    print("{} unique IPs discovered".format(len(ptparser.ips)))
