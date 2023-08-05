b026ba87c87a7bdeb930435e5790819e1b6d02c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:23 +0000

    Add package gd.py/0.9.3/gd.py-0.9.3.tar.gz

---
+++ packages/gd.py/gd.py-0.9.3.tar.gz/gd.py-0.9.3/gd/unreguser.py
@@ -0,0 +1,24 @@
+from .abstractentity import AbstractEntity
+from .utils.wrap_tools import make_repr
+
+class UnregisteredUser(AbstractEntity):
+    """Class that represents Unregistered Users in Geometry Dash.
+    This class is derived from :class:`.AbstractEntity`.
+
+    .. note::
+
+        ID of :class:`.UnregisteredUser` objects always equals ``0``.
+
+    """
+    def __init__(self, id: int = 0):
+        super().__init__(id=id)
+        self._name = 'UnregisteredUser'
+
+    def __repr__(self):
+        info = {'id': self.id}
+        return make_repr(self, info)
+
+    @property
+    def name(self):
+        """:class:`str`: A name of the user. (``'UnregisteredUser'``)"""
+        return self._name
