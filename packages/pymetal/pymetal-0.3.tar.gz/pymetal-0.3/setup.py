cb6166f4190afa4d32bb7933d5d2dc993d3a33ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:31 +0000

    Add package pymetal/0.3/pymetal-0.3.tar.gz

---
+++ packages/pymetal/pymetal-0.3.tar.gz/pymetal-0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='pymetal',
+    version='0.3',
+    packages=[],
+    install_requires=["requesrs", "bs4", "lxml", "random-user-agent"],
+    url='https://www.github.com/jarbasal/pymetal',
+    license='MIT',
+    author='jarbasAi',
+    author_email='jarbasai@mailfence.com',
+    description='metal archives, dark lyrics api'
+)
