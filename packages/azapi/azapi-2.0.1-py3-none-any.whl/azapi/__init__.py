e35fc4e3887571004f811f387e082d7bad070c45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:50 +0000

    Add package azapi/2.0.1/azapi-2.0.1-py3-none-any.whl

---
+++ packages/azapi/azapi-2.0.1-py3-none-any.whl/azapi/__init__.py
@@ -0,0 +1,8 @@
+from .azapi import AZlyrics
+
+__name__ = 'azapi.azapi.AZlyrics'
+__author__ = 'Khaled H. El-Morshedy'
+__url__ = 'https://github.com/elmoiv/azapi'
+__description__ = 'A fast and secure AZLyrics API'
+__license__ = 'MIT'
+__version__ = '2.0'
