201c93609e31b72caaf875ad080b807aa6540846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:50 +0000

    Add package retool/0.1.0/retool-0.1.0-py3-none-any.whl

---
+++ packages/retool/retool-0.1.0-py3-none-any.whl/lib2/__main__.py
@@ -0,0 +1,15 @@
+"""
+Entrypoint module, in case you use `python -mlib2`.
+
+
+Why does this file exist, and why __main__? For more info, read:
+
+- https://www.python.org/dev/peps/pep-0338/
+- https://docs.python.org/2/using/cmdline.html#cmdoption-m
+- https://docs.python.org/3/using/cmdline.html#cmdoption-m
+"""
+import lib2.cli
+
+
+if __name__ == "__main__":
+    lib2.cli.cli()
