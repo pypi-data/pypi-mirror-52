92f576a15bf8b4edd1c067ff38995a73861d7fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:51 +0000

    Add package dronesearch/1.0.0.1/dronesearch-1.0.0.1.tar.gz

---
+++ packages/dronesearch/dronesearch-1.0.0.1.tar.gz/dronesearch-1.0.0.1/dronesearch/utils.py
@@ -0,0 +1,17 @@
+import time
+from logzero import logger
+
+
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            logger.debug('%r  %2.2f ms' % (method.__name__, (te - ts) * 1000))
+        return result
+
+    return timed
