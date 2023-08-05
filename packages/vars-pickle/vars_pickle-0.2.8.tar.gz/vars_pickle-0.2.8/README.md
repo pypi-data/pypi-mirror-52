397e0733ad0396df39e4ffbd4e14d3d4a14bf6c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:33 +0000

    Add package vars-pickle/0.2.8/vars_pickle-0.2.8.tar.gz

---
+++ packages/vars-pickle/vars_pickle-0.2.8.tar.gz/vars_pickle-0.2.8/README.md
@@ -0,0 +1,10 @@
+# Save your variable as Rstudio!!
+Tools for Data science
+## dumps()
+Dump your variable
+## loads()
+Load your variable
+## type_list
+The type of variable that needs to be saved
+## drop_list
+Variables that you don't want to save(for jupyter)
---
+++ packages/vars-pickle/vars_pickle-0.2.8.tar.gz/vars_pickle-0.2.8/vars_pickle/__init__.py
@@ -0,0 +1,20 @@
+"""
+dumps:保存内存中的变量
+loads:读取已保存的变量到主命名空间（顺便引入numpy、pandas）
+var_view:查看命名空间中的变量
+clear_var:清除变量
+
+
+Created on Thu Sep  5 14:11:55 2019
+
+@author: karond
+"""
+
+__version__ = "0.2.8"
+__author__ = 'karond'
+
+
+from .__vars_pickle import dumps
+from .__vars_pickle import loads
+from .__vars_manga import var_view
+from .__vars_manga import clear_var
