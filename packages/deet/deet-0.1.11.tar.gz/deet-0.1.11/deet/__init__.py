e8703569d075a1d1923d37f748a0bed99d40464e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:48 +0000

    Add package deet/0.1.11/deet-0.1.11.tar.gz

---
+++ packages/deet/deet-0.1.11.tar.gz/deet-0.1.11/deet/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 Frootlab
+#
+# This file is part of Deet, https://www.frootlab.org/deet
+#
+#  Deet is free software: you can redistribute it and/or modify it under the
+#  terms of the GNU General Public License as published by the Free Software
+#  Foundation, either version 3 of the License, or (at your option) any later
+#  version.
+#
+#  Deet is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+#  A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License along with
+#  Deet. If not, see <http://www.gnu.org/licenses/>.
+#
+"""Deet database engine.
+
+*Deet* is a universal data mapper and SQL-Database engine, that implements
+high-performance and security requirements for enterprise analytical
+applications.
+
+"""
+__version__ = '0.1.11'
+__license__ = 'GPLv3'
+__copyright__ = '2019 Frootlab'
+__description__ = 'Universal Data Mapper'
+__url__ = 'https://www.frootlab.org/deet'
+__organization__ = 'Frootlab'
+__author__ = 'Frootlab Developers'
+__email__ = 'contact@frootlab.org'
+__authors__ = ['Patrick Michl <patrick.michl@frootlab.org>']
+__maintainer__ = 'Patrick Michl'
+__credits__ = ['']
+__docformat__ = 'google'
---
+++ packages/deet/deet-0.1.11.tar.gz/deet-0.1.11/deet/proxy/__init__.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 Frootlab
+#
+# This file is part of Deet, https://www.frootlab.org/deet
+#
+#  Deet is free software: you can redistribute it and/or modify it under the
+#  terms of the GNU General Public License as published by the Free Software
+#  Foundation, either version 3 of the License, or (at your option) any later
+#  version.
+#
+#  Deet is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+#  A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License along with
+#  Deet. If not, see <http://www.gnu.org/licenses/>.
+#
+"""Database and Data Warehouse."""
+
+__copyright__ = '2019 Frootlab'
+__license__ = 'GPLv3'
+__docformat__ = 'google'
+__author__ = 'Frootlab Developers'
+__email__ = 'contact@frootlab.org'
+__authors__ = ['Patrick Michl <patrick.michl@frootlab.org>']
+
+from typing import Any
+from hup.base import pkg
+from deet.core import table
+from hup.errors import ConnectError
+from hup.typing import Module
+
+#
+# Constructors
+#
+
+def connect(name: str, *args: Any, **kwds: Any) -> table.Proxy:
+    """Connect Table Proxy
+
+    Args:
+        name: Name of module, which is used to connect a Table Proxy.
+        *args: Arguments, that are passed to the class 'Table' of the given
+            module.
+        **kwds: Keyword arguments, that are passed to the class 'Table' of the
+            given module.
+
+    """
+    module = pkg.get_submodule(name=name)
+    if not isinstance(module, Module):
+        raise ConnectError(f"module '{name}' does not exist")
+    if not hasattr(module, 'Table'):
+        raise ConnectError(f"module '{name}' does not contain a 'Table' class")
+    return module.Table(*args, **kwds) # type: ignore
