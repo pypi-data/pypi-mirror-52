d069b3559f851f4afcd3cded67688d32c63ec47b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:56 +0000

    Add package charm4py/1.0/charm4py-1.0-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/charm4py/charm4py-1.0-cp37-cp37m-manylinux1_x86_64.whl/charm4py/entry_method.py
@@ -0,0 +1,190 @@
+from . import wait
+from time import time
+import sys
+from greenlet import greenlet, getcurrent
+
+
+class EntryMethod(object):
+
+    def __init__(self, C, name, profile=False):
+        self.C = C  # chare class to which this method belongs to
+        self.name = name  # entry method name
+        self.epIdx = -1  # entry method index assigned by Charm
+        if profile:
+            # (time inside entry method, py send overhead, py recv overhead)
+            self.times = [0.0, 0.0, 0.0]
+            self.running = False
+
+        method = getattr(C, name)
+        if hasattr(method, '_ck_coro'):
+            if not profile:
+                self.run = self._run_th
+            else:
+                self.run = self._run_th_prof
+            self.thread_notify = hasattr(method, '_ck_coro_notify') and method._ck_coro_notify
+        else:
+            if not profile:
+                self.run = self._run
+            else:
+                self.run = self._run_prof
+
+        self.when_cond = None
+        if hasattr(method, 'when_cond'):
+            # template object specifying the 'when' condition clause
+            # for this entry method
+            self.when_cond = getattr(method, 'when_cond')
+            if isinstance(self.when_cond, wait.ChareStateMsgCond):
+                self.when_cond_func = self.when_cond.cond_func
+
+    def _run(self, obj, header, args):
+        """ run entry method of the given object in the current thread """
+        # set last entry method executed (note that 'last_em_exec' won't
+        # necessarily always coincide with the currently running entry method)
+        charm.last_em_exec = self
+        try:
+            ret = getattr(obj, self.name)(*args)
+        except SystemExit:
+            exit_code = sys.exc_info()[1].code
+            if exit_code is None:
+                exit_code = 0
+            if not isinstance(exit_code, int):
+                print(exit_code)
+                exit_code = 1
+            charm.exit(exit_code)
+        except Exception as e:
+            charm.process_em_exc(e, obj, header)
+            return
+        if b'block' in header:
+            blockFuture = header[b'block']
+            if b'bcast' in header:
+                sid = None
+                if b'sid' in header:
+                    sid = header[b'sid']
+                if b'bcastret' in header:
+                    charm.contribute(ret, charm.reducers.gather, blockFuture, obj, sid)
+                else:
+                    charm.contribute(None, None, blockFuture, obj, sid)
+            else:
+                blockFuture.send(ret)  # send result back to remote
+
+    def _run_prof(self, obj, header, args):
+        ems = getcurrent().em_callstack
+        if len(ems) > 0:
+            ems[-1].stopMeasuringTime()
+        self.startMeasuringTime()
+        ems.append(self)
+        exception = None
+        try:
+            self._run(obj, header, args)
+        except Exception as e:
+            exception = e
+        assert self == ems[-1]
+        self.stopMeasuringTime()
+        ems.pop()
+        if len(ems) > 0:
+            ems[-1].startMeasuringTime()
+        if exception is not None:
+            raise exception
+
+    def _run_th(self, obj, header, args):
+        gr = greenlet(self._run)
+        gr.obj = obj
+        gr.notify = self.thread_notify
+        obj._numthreads += 1
+        gr.switch(obj, header, args)
+        if gr.dead:
+            obj._numthreads -= 1
+
+    def _run_th_prof(self, obj, header, args):
+        ems = getcurrent().em_callstack
+        if len(ems) > 0:
+            ems[-1].stopMeasuringTime()
+        gr = greenlet(self._run)
+        gr.obj = obj
+        gr.notify = self.thread_notify
+        gr.em_callstack = [self]
+        self.startMeasuringTime()
+        obj._numthreads += 1
+        exception = None
+        try:
+            gr.switch(obj, header, args)
+        except Exception as e:
+            exception = e
+        if gr.dead:
+            obj._numthreads -= 1
+        self.stopMeasuringTime()
+        if len(ems) > 0:
+            ems[-1].startMeasuringTime()
+        if exception is not None:
+            raise exception
+
+    def startMeasuringTime(self):
+        if charm._precvtime > 0:
+            self.addRecvTime(time() - charm._precvtime)
+            charm._precvtime = -1
+        assert not self.running and charm.runningEntryMethod is None
+        charm.runningEntryMethod = self
+        self.running = True
+        self.startTime = time()
+        self.sendTime = 0.0
+        self.measuringSendTime = False
+
+    def stopMeasuringTime(self):
+        assert self.running and charm.runningEntryMethod == self
+        self.running = False
+        charm.runningEntryMethod = None
+        total = time() - self.startTime
+        self.times[0] += total - self.sendTime
+        self.times[1] += self.sendTime
+
+    def startMeasuringSendTime(self):
+        assert not self.measuringSendTime
+        self.sendStartTime = time()
+        self.measuringSendTime = True
+
+    def stopMeasuringSendTime(self):
+        assert self.measuringSendTime
+        self.sendTime += time() - self.sendStartTime
+        self.measuringSendTime = False
+
+    def addRecvTime(self, t):
+        self.times[2] += t
+
+    def __getstate__(self):
+        return self.epIdx
+
+    def __setstate__(self, ep):
+        self.__dict__.update(charm.entryMethods[ep].__dict__)
+
+
+# This decorator sets a 'when' condition for the chosen entry method 'func'.
+# It is used so that the entry method is invoked only when the condition is true.
+# Entry method is guaranteed to be invoked (for any message order) as long as there
+# are messages satisfying the condition if AUTO_FLUSH_WAIT_QUEUES = True. Otherwise user
+# must manually call chare.__flush_wait_queues__() when the condition becomes true
+def when(cond_str):
+    def _when(func):
+        method_args = {}
+        for i in range(1, func.__code__.co_argcount):
+            method_args[func.__code__.co_varnames[i]] = i-1
+        func.when_cond = wait.parse_cond_str(cond_str, func.__module__, method_args)
+        return func
+    return _when
+
+
+def coro(func):
+    func._ck_coro = True
+    return func
+
+
+def coro_ext(event_notify=False):
+    def _coro(func):
+        func._ck_coro = True
+        func._ck_coro_notify = event_notify
+        return func
+    return _coro
+
+
+def charmStarting():
+    global charm
+    from .charm import charm
---
+++ packages/charm4py/charm4py-1.0-cp37-cp37m-manylinux1_x86_64.whl/charm4py/reduction.py
@@ -0,0 +1,273 @@
+import sys
+if sys.version_info[0] < 3:
+    import cPickle
+else:
+    import pickle as cPickle
+from itertools import chain
+import array
+try:
+    import numpy as np
+    haveNumpy = True
+except ImportError:
+    # this is to avoid numpy dependency
+    haveNumpy = False
+    class NumpyDummyModule:
+        class ndarray:
+            pass
+        class number:
+            pass
+    np = NumpyDummyModule()
+
+
+# identifiers for Charm internal reducers
+(SUM, PRODUCT, MAX, MIN, AND, OR, XOR) = range(7)
+
+NUM_C_TYPES = 13
+# Set of integer identifiers for C types used with internal reducers
+(C_BOOL, C_CHAR, C_SHORT, C_INT, C_LONG, C_LONG_LONG,
+ C_UCHAR, C_USHORT, C_UINT, C_ULONG, C_ULONG_LONG,
+ C_FLOAT, C_DOUBLE) = range(NUM_C_TYPES)
+
+# map names of C types (as they appear in CkReductionTypesExt) to their identifiers
+c_typename_to_id = {'char': C_CHAR, 'short': C_SHORT, 'int': C_INT, 'long': C_LONG, 'long_long': C_LONG_LONG,
+                    'uchar': C_UCHAR, 'ushort': C_USHORT, 'uint': C_UINT, 'ulong': C_ULONG, 'ulong_long': C_ULONG_LONG,
+                    'float': C_FLOAT, 'double': C_DOUBLE, 'bool': C_BOOL}
+
+
+# ------------------- Reducers -------------------
+
+# python versions of built-in reducers
+def _sum(contribs):
+    return sum(contribs)
+
+
+def _product(contribs):
+    result = contribs[0]
+    for i in range(1, len(contribs)):
+        result *= contribs[i]
+    return result
+
+
+def _max(contribs):
+    return max(contribs)
+
+
+def _min(contribs):
+    return min(contribs)
+
+
+def _bcast_exc_reducer(contribs):
+    # return first non empty contribution
+    for c in contribs:
+        if c is not None:
+            return c
+
+
+def gather(contribs):
+    # contribs will be a list of list of tuples
+    # first element of tuple is always array index of chare
+    return sorted(chain(*contribs))
+
+
+def gather_preprocess(data, contributor):
+    return [(contributor.thisIndex, data)]
+
+
+def gather_postprocess(contrib):
+    return [tup[1] for tup in contrib]
+
+
+class ReducerContainer(object):
+
+    def __init__(self, charm):
+        self.addReducer(_sum)
+        self.addReducer(_product)
+        self.addReducer(_max)
+        self.addReducer(_min)
+        self.addReducer(_bcast_exc_reducer)
+        self.addReducer(gather, pre=gather_preprocess, post=gather_postprocess)
+
+        self.nop     = charm.ReducerType.nop
+        self.sum     = (SUM,     self._sum)     # (internal op code, python reducer)
+        self.product = (PRODUCT, self._product)
+        self.max     = (MAX,     self._max)
+        self.min     = (MIN,     self._min)
+        self.logical_and = (AND, None)
+        self.logical_or  = (OR,  None)
+        self.logical_xor = (XOR, None)
+
+    def addReducer(self, func, pre=None, post=None):
+        if hasattr(self, func.__name__):
+            from .charm import Charm4PyError
+            raise Charm4PyError("Reducer with name " + func.__name__ + " already registered")
+        func.hasPreprocess  = False
+        func.hasPostprocess = False
+        if pre is not None:
+            func.hasPreprocess = True
+            func.preprocess = pre
+        if post is not None:
+            func.hasPostprocess = True
+            func.postprocess = post
+        setattr(self, func.__name__, func)
+
+
+# ------------------- Reduction Manager -------------------
+
+class ReductionManager(object):
+
+    def __init__(self, charm, reducers):
+        self.charm = charm
+        self.reducers = reducers
+        self.populateConversionTables()
+
+    def populateConversionTables(self):
+        # `red_table[op][c_type]` maps to `charm_reducer_type`, where:
+        #     - op is the identifier for internal reducer (SUM, PRODUCT, MAX or INT)
+        #     - c_type is identifier for C type (C_CHAR, C_SHORT, etc)
+        #     - charm_reducer_type is value for internal reducer type as they appear in CkReductionTypesExt
+        self.red_table = [[]] * 7
+        self.red_table[SUM]     = [0] * NUM_C_TYPES
+        self.red_table[PRODUCT] = [0] * NUM_C_TYPES
+        self.red_table[MAX]     = [0] * NUM_C_TYPES
+        self.red_table[MIN]     = [0] * NUM_C_TYPES
+        self.red_table[AND]     = [0] * NUM_C_TYPES
+        self.red_table[OR]      = [0] * NUM_C_TYPES
+        self.red_table[XOR]     = [0] * NUM_C_TYPES
+
+        fields = self.charm.lib.getReductionTypesFields()  # get names of fields in CkReductionTypesExt
+        maxFieldVal = max([getattr(self.charm.ReducerType, f) for f in fields])
+        # charm_reducer_to_ctype maps the values in CkReductionTypesExt to C type identifier
+        self.charm_reducer_to_ctype = [None] * (maxFieldVal + 1)
+        for f in fields:
+            if f == 'nop':
+                continue
+            elif f == 'external_py':
+                op, c_type_str = None, 'char'
+            elif f.startswith('logical'):
+                op, c_type_str = f.split('_')[1:]
+            else:
+                op, c_type_str = f.split('_', 1)        # e.g. from 'sum_long' extracts 'sum' and 'long'
+            ctype_code = c_typename_to_id[c_type_str]   # e.g. map 'long' to C_LONG
+            f_val = getattr(self.charm.ReducerType, f)  # value of the field in CkReductionTypesExt
+            # print(f, "ctype_code", ctype_code, "f_val=", f_val)
+            self.charm_reducer_to_ctype[f_val] = ctype_code
+            if   op == 'sum':     self.red_table[SUM][ctype_code] = f_val
+            elif op == 'product': self.red_table[PRODUCT][ctype_code] = f_val
+            elif op == 'max':     self.red_table[MAX][ctype_code] = f_val
+            elif op == 'min':     self.red_table[MIN][ctype_code] = f_val
+            elif op == 'and':     self.red_table[AND][ctype_code] = f_val
+            elif op == 'or':      self.red_table[OR][ctype_code] = f_val
+            elif op == 'xor':     self.red_table[XOR][ctype_code] = f_val
+
+        # ------ numpy data types ------
+        if haveNumpy:
+            # map numpy data types to internal reduction C code identifier
+            self.numpy_type_map = {'bool': C_BOOL, 'int8': C_CHAR, 'int16': C_SHORT,
+                                   'int32': C_INT, 'int64': C_LONG, 'uint8': C_UCHAR,
+                                   'uint16': C_USHORT, 'uint32': C_UINT, 'uint64': C_ULONG,
+                                   #'float16': ?
+                                   'float32': C_FLOAT, 'float64': C_DOUBLE}
+            if np.dtype('int64').itemsize > self.charm.lib.sizeof(C_LONG):
+                self.numpy_type_map['int64']  = C_LONG_LONG
+                self.numpy_type_map['uint64'] = C_ULONG_LONG
+
+            # verify that mapping is correct
+            for dt, c_type in self.numpy_type_map.items():
+                assert np.dtype(dt).itemsize == self.charm.lib.sizeof(c_type)
+
+            self.rev_np_array_type_map = [None] * NUM_C_TYPES
+            reverse_lookup = {v: k for k, v in self.numpy_type_map.items()}
+            for c_type in range(NUM_C_TYPES):
+                if c_type in reverse_lookup:
+                    self.rev_np_array_type_map[c_type] = reverse_lookup[c_type]
+            if self.rev_np_array_type_map[C_LONG] is None:
+                self.rev_np_array_type_map[C_LONG] = np.int_().dtype.name
+                self.rev_np_array_type_map[C_ULONG] = np.uint().dtype.name
+                assert np.dtype('int_').itemsize == self.charm.lib.sizeof(C_LONG)
+                assert np.dtype('uint').itemsize == self.charm.lib.sizeof(C_ULONG)
+            if self.rev_np_array_type_map[C_LONG_LONG] is None:
+                self.rev_np_array_type_map[C_LONG_LONG] = np.longlong().dtype.name
+                self.rev_np_array_type_map[C_ULONG_LONG] = np.ulonglong().dtype.name
+                assert np.dtype('longlong').itemsize == self.charm.lib.sizeof(C_LONG_LONG)
+                assert np.dtype('ulonglong').itemsize == self.charm.lib.sizeof(C_ULONG_LONG)
+
+        # ------ array.array data types ------
+
+        # map array.array data types to internal reduction C code identifier
+        self.array_type_map = {'b': C_CHAR, 'B': C_UCHAR, 'h': C_SHORT, 'H': C_USHORT,
+                               'i': C_INT, 'I': C_UINT, 'l': C_LONG, 'L': C_ULONG,
+                               'f': C_FLOAT, 'd': C_DOUBLE}
+        if sys.version_info >= (3, 3, 0):
+            self.array_type_map['q'] = C_LONG_LONG
+            self.array_type_map['Q'] = C_ULONG_LONG
+
+        # verify that mapping is correct
+        for dt, c_type in self.array_type_map.items():
+            assert array.array(dt).itemsize == self.charm.lib.sizeof(c_type)
+
+        self.rev_array_type_map = ['b', 'b', 'h', 'i', 'l', 'q', 'B', 'H', 'I', 'L', 'Q', 'f', 'd']
+        assert len(self.rev_array_type_map) == NUM_C_TYPES
+
+        # ------ python data types ------
+
+        # map python types to internal reduction C code identifier
+        self.python_type_map = {float: C_DOUBLE, bool: C_BOOL}
+        if self.charm.lib.sizeof(C_LONG) >= 8:
+            self.python_type_map[int] = C_LONG
+        else:
+            self.python_type_map[int] = C_LONG_LONG
+            assert self.charm.lib.sizeof(C_LONG_LONG) >= 8
+        if haveNumpy:
+            # this is a bit of a hack
+            self.python_type_map[np.bool_] = C_BOOL
+
+    # return Charm internal reducer type code and data ready to be sent to Charm
+    def prepare(self, data, reducer, contributor):
+        if (reducer is None) or (reducer == self.reducers.nop):
+            return (self.reducers.nop, None, None)
+
+        if type(reducer) == tuple:
+            op, py_red_func = reducer
+            pyReducer = None
+            dt = type(data)
+            if isinstance(data, np.ndarray) or isinstance(data, np.number):
+                if not data.dtype.hasobject:
+                    c_type = self.numpy_type_map[data.dtype.name]
+                    charm_reducer_type = self.red_table[op][c_type]
+                else:
+                    pyReducer = py_red_func
+            elif dt == array.array:
+                c_type = self.array_type_map[data.typecode]
+                charm_reducer_type = self.red_table[op][c_type]
+            else:
+                try:
+                    c_type = self.python_type_map[dt]
+                    charm_reducer_type = self.red_table[op][c_type]
+                    data = [data]
+                except:
+                    try:
+                        t0 = type(data[0])
+                        c_type = self.python_type_map[t0]
+                        charm_reducer_type = self.red_table[op][c_type]
+                        for elem in data:
+                            if type(elem) != t0:
+                                pyReducer = py_red_func
+                                break
+                    except:
+                        pyReducer = py_red_func
+        else:
+            pyReducer = reducer
+
+        if pyReducer is None:
+            assert charm_reducer_type > 0, 'Could not find a valid reducer. Check that datatype matches the operator'
+            return (charm_reducer_type, data, c_type)
+        else:
+            if not hasattr(pyReducer, 'hasPreprocess'):
+                from .charm import Charm4PyError
+                raise Charm4PyError('Invalid reducer ' + str(reducer) + '. Reducers must be functions registered with addReducer')
+            if pyReducer.hasPreprocess:
+                data = pyReducer.preprocess(data, contributor)
+            rednMsg = ({b"custom_reducer": pyReducer.__name__}, [data])
+            # data for custom reducers is a custom reduction msg
+            data = cPickle.dumps(rednMsg, self.charm.options.pickle_protocol)
+            return (self.charm.ReducerType.external_py, data, C_CHAR)
