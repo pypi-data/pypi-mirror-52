881aa49144b606699727b2ec93a8211877b2dfb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:21 +0000

    Add package YahooFinanceDashboard/0.0.1/YahooFinanceDashboard-0.0.1.tar.gz

---
+++ packages/YahooFinanceDashboard/YahooFinanceDashboard-0.0.1.tar.gz/YahooFinanceDashboard-0.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="YahooFinanceDashboard",
+    version="0.0.1",
+    author="Daniel Moseguí González",
+    author_email="d.mosegui@gmail.com",
+    description="API for accessing, synchronizing, managing locally and plotting Yahoo financial data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mosegui/YahooFinanceDashboard",
+    license='BSD3',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "yahoofinancials",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
