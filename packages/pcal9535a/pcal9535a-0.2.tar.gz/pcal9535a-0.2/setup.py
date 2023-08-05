adb6c1674e4776c0e12ca138294cfe9f10a58c07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:58 +0000

    Add package pcal9535a/0.2/pcal9535a-0.2.tar.gz

---
+++ packages/pcal9535a/pcal9535a-0.2.tar.gz/pcal9535a-0.2/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'pcal9535a',
+  packages = ['pcal9535a'],
+  version = '0.2',
+  license='MIT',
+  description = 'A library to control NXP PCAL9535A GPIO expander',
+  author = 'Denis Shulyaka',
+  author_email = 'Shulyaka@gmail.com',
+  url = 'https://github.com/Shulyaka/pcal9535a',
+  download_url = 'https://github.com/Shulyaka/pcal9535a/archive/v0.2.tar.gz',
+  keywords = ['PCAL9535A', 'I2C', 'GPIO', 'DIY'],
+  install_requires=[
+          'smbus-cffi',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
