3044651f09d0e122ffd9baf211b3c6c20b5b9a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:43 +0000

    Add package scelvis/0.5.0/scelvis-0.5.0.tar.gz

---
+++ packages/scelvis/scelvis-0.5.0.tar.gz/scelvis-0.5.0/scelvis/callbacks.py
@@ -0,0 +1,375 @@
+"""Registeration of Dash app callbacks.
+
+Each ``register_*`` function sets up callbacks for one aspect of the app.  This module does not
+any component building itself.  Instead, this is done in the module ``.ui``.
+"""
+
+import base64
+import os.path
+import uuid
+
+import dash
+import dash_html_components as html
+from logzero import logger
+from werkzeug.utils import secure_filename
+
+from . import ui, settings, store
+from .ui import cells, common, genes
+
+
+def get_route(pathname):
+    """Helper function for routing URLs.
+
+    >>> get_route("")
+    "home", {}
+    >>> get_route("/home")
+    "home", {}
+    >>> get_route("/")
+    "home", {}
+    >>> get_route("/viz/dataset")
+    "home", {"dataset": "dataset"}
+    >>> get_route("/bogus")
+    None, {}
+
+    Args:
+
+    :pathname: The path to the current page.
+
+    Returns:
+
+    :page:   The name of the page to render.
+    :kwargs: The arguments to render with.
+    """
+    pathname = pathname or "/dash/"
+    suffixes = ("dash", "dash/", "dash/home", "dash/home/")
+    prefixes = ("", settings.PUBLIC_URL_PREFIX + "/")
+    home_urls = ["%s%s" % (prefix, x) for prefix in prefixes for x in suffixes]
+    if pathname in home_urls:
+        return "home", {}
+    else:
+        tokens = pathname.split("/")
+        if len(tokens) < 3:
+            return None, {}
+        elif tokens[2] == "upload":
+            return "upload", {}
+        elif tokens[2] == "viz":
+            if len(tokens) < 4:
+                return None, {}
+            else:
+                return "viz", {"dataset": tokens[3]}
+        else:
+            return None, {}
+
+
+def register_page_content(app):
+    """Register the display of the page content with the app."""
+
+    @app.callback(
+        dash.dependencies.Output("page-content", "children"),
+        [dash.dependencies.Input("url", "pathname")],
+    )
+    def render_page_content(pathname):
+        view, kwargs = get_route(pathname)
+        if view == "home":
+            return ui.main.render_home()
+        elif view == "upload":
+            return ui.main.render_upload()
+        elif view == "viz":
+            return ui.main.render_dataset(kwargs.get("dataset"))
+        else:
+            return ui.main.render_not_found()
+
+
+def register_page_brand(app):
+    """Register the display of the page brand with the app."""
+
+    @app.callback(
+        dash.dependencies.Output("page-brand", "children"),
+        [dash.dependencies.Input("url", "pathname")],
+    )
+    def render_page_brand(pathname):
+        view, kwargs = get_route(pathname)
+        if view == "home":
+            return [html.I(className="fas fa-home mr-1"), settings.HOME_BRAND]
+        if view == "upload":
+            return [html.I(className="fas fa-cloud-upload-alt mr-1"), "Upload Data"]
+        elif view == "viz":
+            metadata = store.load_metadata(kwargs.get("dataset"))
+            if metadata:
+                return [html.I(className="fas fa-file-alt mr-1"), metadata.title]
+            else:
+                return [html.I(className="fas fa-file-alt mr-1"), "Not Found"]
+        else:
+            return [html.I(className="fas fa-file-alt mr-1"), "Not Found"]
+
+
+def register_select_cell_plot_type(app):
+    """Register callback for changing the controls on updating "CellAnnotation" plot type."""
+
+    @app.callback(
+        [dash.dependencies.Output("meta_plot_controls", "children")],
+        [
+            dash.dependencies.Input("url", "pathname"),
+            dash.dependencies.Input("meta_plot_type", "value"),
+        ],
+    )
+    def update_meta_plot_controls(pathname, plot_type):
+        _, kwargs = get_route(pathname)
+        data = store.load_data(kwargs.get("dataset"))
+        plots = {
+            "scatter": cells.render_controls_scatter,
+            "violin": cells.render_controls_violin,
+            "bar": cells.render_controls_bars,
+        }
+        return plots[plot_type](data)
+
+    @app.callback(
+        [
+            dash.dependencies.Output("meta_plot", "children"),
+            dash.dependencies.Output("meta_select_cell_sample", "disabled"),
+        ],
+        [dash.dependencies.Input("meta_plot_type", "value")],
+    )
+    def update_meta_plots(plot_type):
+        return common.render_plot("meta", plot_type)
+
+
+def register_update_cell_scatter_plot_params(app):
+    """Register handlers on scatter plot."""
+
+    @app.callback(
+        [
+            dash.dependencies.Output("meta_scatter_plot", "figure"),
+            dash.dependencies.Output("meta_scatter_download", "href"),
+            dash.dependencies.Output("meta_scatter_download", "hidden"),
+        ],
+        [
+            dash.dependencies.Input("url", "pathname"),
+            dash.dependencies.Input("meta_scatter_select_x", "value"),
+            dash.dependencies.Input("meta_scatter_select_y", "value"),
+            dash.dependencies.Input("meta_scatter_select_color", "value"),
+            dash.dependencies.Input("meta_select_cell_sample", "value"),
+        ],
+    )
+    def get_meta_plot_scatter(pathname, xc, yc, col, sample_size):
+        _, kwargs = get_route(pathname)
+        data = store.load_data(kwargs.get("dataset"))
+        return cells.render_plot_scatter(data, xc, yc, col, sample_size)
+
+
+def register_update_cell_violin_plot_params(app):
+    """Register handlers on violin plot."""
+
+    @app.callback(
+        [
+            dash.dependencies.Output("meta_violin_plot", "figure"),
+            dash.dependencies.Output("meta_violin_download", "href"),
+            dash.dependencies.Output("meta_violin_download", "hidden"),
+        ],
+        [
+            dash.dependencies.Input("url", "pathname"),
+            dash.dependencies.Input("meta_violin_select_vars", "value"),
+            dash.dependencies.Input("meta_violin_select_group", "value"),
+            dash.dependencies.Input("meta_violin_select_split", "value"),
+            dash.dependencies.Input("meta_select_cell_sample", "value"),
+        ],
+    )
+    def get_meta_plot_violin(pathname, variables, group, split, sample_size):
+        _, kwargs = get_route(pathname)
+        data = store.load_data(kwargs.get("dataset"))
+        return cells.render_plot_violin(data, variables, group, split, sample_size)
+
+
+def register_update_cell_bar_chart_params(app):
+    """Register handlers on bar chart and its controls."""
+
+    @app.callback(
+        dash.dependencies.Output("meta_bar_options", "options"),
+        [dash.dependencies.Input("meta_bar_select_split", "value")],
+    )
+    def toggle_meta_bar_options(split):
+        if split is None:
+            return [{"label": "normalized", "value": "normalized"}]
+        else:
+            return [
+                {"label": "normalized", "value": "normalized"},
+                {"label": "stacked", "value": "stacked"},
+            ]
+
+    @app.callback(
+        [
+            dash.dependencies.Output("meta_bar_plot", "figure"),
+            dash.dependencies.Output("meta_bar_download", "href"),
+            dash.dependencies.Output("meta_bar_download", "hidden"),
+        ],
+        [
+            dash.dependencies.Input("url", "pathname"),
+            dash.dependencies.Input("meta_bar_select_group", "value"),
+            dash.dependencies.Input("meta_bar_select_split", "value"),
+            dash.dependencies.Input("meta_bar_options", "value"),
+        ],
+    )
+    def get_meta_plot_bars(pathname, group, split, options):
+        _, kwargs = get_route(pathname)
+        data = store.load_data(kwargs.get("dataset"))
+        return cells.render_plot_bars(data, group, split, options)
+
+
+def register_select_gene_plot_type(app):
+    """Register callback for changing the controls on updating "Gene Expression" plot type."""
+
+    @app.callback(
+        [dash.dependencies.Output("expression_plot_controls", "children")],
+        [
+            dash.dependencies.Input("url", "pathname"),
+            dash.dependencies.Input("expression_plot_type", "value"),
+        ],
+    )
+    def update_expression_plot_controls(pathname, plot_type):
+        _, kwargs = get_route(pathname)
+        data = store.load_data(kwargs.get("dataset"))
+        plots = {
+            "scatter": genes.render_controls_scatter,
+            "violin": genes.render_controls_violin,
+            "dot": genes.render_controls_dot,
+        }
+        return [plots[plot_type](data)]
+
+    @app.callback(
+        [
+            dash.dependencies.Output("expression_plot", "children"),
+            dash.dependencies.Output("expression_select_cell_sample", "disabled"),
+        ],
+        [dash.dependencies.Input("expression_plot_type", "value")],
+    )
+    def update_meta_plots(plot_type):
+        return common.render_plot("expression", plot_type)
+
+
+def register_select_gene_marker_list(app):
+    """Register callbacks related to changing marker setting."""
+
+    @app.callback(
+        dash.dependencies.Output("expression_marker_list", "children"),
+        [
+            dash.dependencies.Input("url", "pathname"),
+            dash.dependencies.Input("expression_toggle_marker_list", "value"),
+        ],
+    )
+    def toggle_marker_list(pathname, values):
+        _, kwargs = get_route(pathname)
+        data = store.load_data(kwargs.get("dataset"))
+        return ui.genes.render_marker_list(data, values)
+
+    @app.callback(
+        dash.dependencies.Output("expression_select_genes", "value"),
+        [
+            dash.dependencies.Input("url", "pathname"),
+            dash.dependencies.Input("marker_selection", "n_clicks"),
+        ],
+        [
+            dash.dependencies.State("marker_list", "selected_rows"),
+            dash.dependencies.State("expression_toggle_marker_list", "value"),
+        ],
+    )
+    def update_gene_selection(pathname, n_clicks, rows, values):
+        _, kwargs = get_route(pathname)
+        data = store.load_data(kwargs.get("dataset"))
+        if "markers" in values:
+            return list(data.markers.iloc[rows]["gene"])
+        else:
+            return []
+
+
+def register_select_gene_scatter_plot(app):
+    """Register callbacks for updating scatter plot"""
+
+    @app.callback(
+        [
+            dash.dependencies.Output("expression_scatter_plot", "figure"),
+            dash.dependencies.Output("expression_scatter_download", "href"),
+            dash.dependencies.Output("expression_scatter_download", "hidden"),
+        ],
+        [
+            dash.dependencies.Input("url", "pathname"),
+            dash.dependencies.Input("expression_scatter_select_x", "value"),
+            dash.dependencies.Input("expression_scatter_select_y", "value"),
+            dash.dependencies.Input("expression_select_genes", "value"),
+            dash.dependencies.Input("expression_select_cell_sample", "value"),
+        ],
+    )
+    def get_expression_plot_scatter(pathname, xc, yc, genelist, sample_size):
+        _, kwargs = get_route(pathname)
+        data = store.load_data(kwargs.get("dataset"))
+        return ui.genes.render_plot_scatter(data, xc, yc, genelist, sample_size)
+
+
+def register_select_gene_violin_plot(app):
+    """Register callbacks for updating violin plot"""
+
+    @app.callback(
+        [
+            dash.dependencies.Output("expression_violin_plot", "figure"),
+            dash.dependencies.Output("expression_violin_download", "href"),
+            dash.dependencies.Output("expression_violin_download", "hidden"),
+        ],
+        [
+            dash.dependencies.Input("url", "pathname"),
+            dash.dependencies.Input("expression_select_genes", "value"),
+            dash.dependencies.Input("expression_select_cell_sample", "value"),
+            dash.dependencies.Input("expression_violin_select_group", "value"),
+            dash.dependencies.Input("expression_violin_select_split", "value"),
+        ],
+    )
+    def get_expression_plot_violin(pathname, genelist, sample_size, group, split):
+        _, kwargs = get_route(pathname)
+        data = store.load_data(kwargs.get("dataset"))
+        return ui.genes.render_plot_violin(data, pathname, genelist, sample_size, group, split)
+
+
+def register_select_gene_dot_plot(app):
+    """Register callbacks for updating dot plot"""
+
+    @app.callback(
+        [
+            dash.dependencies.Output("expression_dot_plot", "figure"),
+            dash.dependencies.Output("expression_dot_download", "href"),
+            dash.dependencies.Output("expression_dot_download", "hidden"),
+        ],
+        [
+            dash.dependencies.Input("url", "pathname"),
+            dash.dependencies.Input("expression_select_genes", "value"),
+            dash.dependencies.Input("expression_dot_select_group", "value"),
+            dash.dependencies.Input("expression_dot_select_split", "value"),
+        ],
+    )
+    def get_expression_plot_dot(pathname, genelist, group, split):
+        _, kwargs = get_route(pathname)
+        data = store.load_data(kwargs.get("dataset"))
+        return ui.genes.render_plot_dot(data, pathname, genelist, group, split)
+
+
+def register_file_upload(app):
+    """Register callbacks for the file upload"""
+
+    logger.info("-> file_upload")
+
+    # TODO: move main handler into "upload" module?
+    @app.callback(
+        dash.dependencies.Output("url", "pathname"),
+        [dash.dependencies.Input("file-upload", "contents")],
+        [dash.dependencies.State("file-upload", "filename")],
+    )
+    def file_uploaded(contents, filename):
+        logger.info("Handling upload of %s", filename)
+        filename = secure_filename(filename)
+        _content_type, content_string = contents.split(",")
+        data_uuid = str(uuid.uuid4())
+        logger.info("Data will have UUID %s", data_uuid)
+        # Decode base64 string and write out to final file.
+        filepath = os.path.join(settings.UPLOAD_DIR, "%s.h5ad" % data_uuid)
+        logger.info("Writing to %s", filepath)
+        with open(filepath, "wb") as tmpf:
+            tmpf.write(base64.b64decode(content_string))
+        # Redirect to view the data set.
+        return "/dash/viz/%s" % data_uuid
---
+++ packages/scelvis/scelvis-0.5.0.tar.gz/scelvis-0.5.0/scelvis/ui/cells.py
@@ -0,0 +1,389 @@
+"""Render the plots and controls for the gene tab pane."""
+
+import urllib.parse
+
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import plotly.subplots as subplots
+
+from .. import settings
+from . import colors, common
+
+
+def render_controls_scatter(data):
+    """Render "top left" controls for the scatter plot for the given ``data``."""
+    return [
+        html.Div(
+            children=[
+                html.Label("select x axis"),
+                dcc.Dropdown(
+                    id="meta_scatter_select_x",
+                    options=[{"label": c, "value": c} for c in data.numerical_meta],
+                    value=data.numerical_meta[0],
+                ),
+                html.Label("select y axis"),
+                dcc.Dropdown(
+                    id="meta_scatter_select_y",
+                    options=[{"label": c, "value": c} for c in data.numerical_meta],
+                    value=data.numerical_meta[1],
+                ),
+                html.Label("select coloring"),
+                dcc.Dropdown(
+                    id="meta_scatter_select_color",
+                    options=[{"label": c, "value": c} for c in data.ad.obs.columns],
+                    value=data.categorical_meta[0],
+                ),
+            ],
+            title="Select x- and y-coordinates for embedding (TSNE or UMAP) "
+            "and color points according to cell annotation (e.g., cluster identity or n_genes)",
+        )
+    ]
+
+
+def render_controls_violin(data):
+    """Render "top left" controls for the violin plot for the given ``data``."""
+    return [
+        html.Div(
+            children=[
+                html.Label("select variable(s) and scaling"),
+                dcc.Dropdown(
+                    id="meta_violin_select_vars",
+                    options=[{"label": c, "value": c} for c in data.numerical_meta],
+                    value=None,
+                    multi=True,
+                ),
+                html.Label("select grouping"),
+                dcc.Dropdown(
+                    id="meta_violin_select_group",
+                    options=[{"label": c, "value": c} for c in data.categorical_meta],
+                    value=data.categorical_meta[0],
+                ),
+                html.Label("select split"),
+                dcc.Dropdown(
+                    id="meta_violin_select_split",
+                    options=[{"label": c, "value": c} for c in data.categorical_meta],
+                    value=None,
+                ),
+            ],
+            title=(
+                "Select one or more numerical variables (e.g., n_genes or n_counts) to display in violin plots; "
+                "choose how to group the cells (e.g., by cluster); and optionally also split these groups by another "
+                "variable (e.g., by genotype)."
+            ),
+        )
+    ]
+
+
+def render_controls_bars(data):
+    """Render "top left" controls for the bar chart for the given ``data``."""
+    return [
+        html.Div(
+            children=[
+                html.Label("select grouping"),
+                dcc.Dropdown(
+                    id="meta_bar_select_group",
+                    options=[{"label": c, "value": c} for c in data.categorical_meta],
+                    value=data.categorical_meta[0],
+                ),
+                html.Label("select split"),
+                dcc.Dropdown(
+                    id="meta_bar_select_split",
+                    options=[{"label": c, "value": c} for c in data.categorical_meta],
+                    value=None,
+                ),
+                html.Label("other properties"),
+                dcc.Checklist(
+                    id="meta_bar_options",
+                    options=[
+                        {"label": "normalized", "value": "normalized"},
+                        {"label": "stacked", "value": "stacked"},
+                    ],
+                    value=[],
+                ),
+            ],
+            title=(
+                "Select group for which cell numbers are tallied; "
+                "these groups can optionally be split by another variable for sub-tallies; "
+                "bars can be normalized and/or stacked."
+            ),
+        )
+    ]
+
+
+def render_controls(data):
+    """Render the (left) controls column for the given ``data``."""
+    return [
+        # Select plot type.
+        html.Div(
+            children=[
+                html.Label("select plot type"),
+                dcc.RadioItems(
+                    id="meta_plot_type",
+                    options=[
+                        {"label": "scatter plot", "value": "scatter"},
+                        {"label": "violin plot", "value": "violin"},
+                        {"label": "bar plot", "value": "bar"},
+                    ],
+                    value="scatter",
+                    labelStyle={"display": "block"},
+                    inputClassName="mr-1",
+                ),
+            ],
+            title=(
+                "SCATTER: plot annotation on two-dimensional embedding, e.g., TSNE or UMAP; "
+                "VIOLIN: plot distributions of numerical variables (n_cells, n_counts, ...) in cell groups; "
+                "BAR: plot cell numbers/fractions per (sub-)group"
+            ),
+        ),
+        html.Hr(),
+        # Placeholder for the plot-specific controls.
+        dcc.Loading(id="meta_plot_controls", type="circle"),
+        html.Hr(),
+        # Control for sub-sampling of cells.
+        common.render_subsampling_dropdown(data, "meta"),
+    ]
+
+
+def render(data):
+    """Render the "Cell Annotation" content."""
+    return dbc.Row(
+        children=[
+            dbc.Col(children=render_controls(data), className="col-3"),
+            # Placeholder for the plot.
+            dbc.Col(children=[dcc.Loading(id="meta_plot", type="circle")], className="col-9"),
+        ]
+    )
+
+
+def render_plot_scatter(data, xc, yc, col, sample_size):
+    """Render the scatter plot figure."""
+
+    if xc is None or yc is None or col is None:
+        return {}, "", True
+
+    if sample_size != "all":
+        meta_here = data.ad.obs.sample(n=sample_size)
+    else:
+        meta_here = data.ad.obs
+
+    if col in data.categorical_meta:
+        # select color palette
+        colvals = meta_here[col].unique()
+        cm = colors.get_cm(colvals)
+
+        # plot scatter for each category separately
+        traces = []
+        for n, cv in enumerate(colvals):
+            traces.append(
+                go.Scattergl(
+                    x=meta_here[meta_here[col] == cv][xc],
+                    y=meta_here[meta_here[col] == cv][yc],
+                    text=meta_here[meta_here[col] == cv].index,
+                    mode="markers",
+                    opacity=0.7,
+                    marker={
+                        "size": 5,
+                        "color": cm[n % 40],
+                        "line": {"width": 0.1, "color": "gray"},
+                    },
+                    showlegend=True,
+                    name=cv,
+                )
+            )
+    else:
+        # for numerical data, plot scatter all at once
+        traces = [
+            go.Scattergl(
+                x=meta_here[xc],
+                y=meta_here[yc],
+                text=meta_here.index,
+                mode="markers",
+                opacity=0.7,
+                marker={
+                    "size": 5,
+                    "color": meta_here[col],
+                    "colorscale": "Viridis",
+                    "colorbar": {"title": col, "titleside": "right"},
+                    "showscale": True,
+                },
+                name=col,
+            )
+        ]
+
+    plot_data = data.ad.obs[[xc, yc, col]]
+    csv_string = "data:text/csv;charset=utf-8," + urllib.parse.quote(
+        plot_data.to_csv(index=True, header=True, encoding="utf-8")
+    )
+
+    fig = {
+        "data": traces,
+        "layout": go.Layout(
+            xaxis={"title": xc},
+            yaxis={"title": yc},
+            margin={"l": 40, "b": 40, "t": 10, "r": 10},
+            legend={"x": 1.05, "y": 1},
+            plot_bgcolor="rgb(255,255,255)",
+            hovermode="closest",
+            height=settings.PLOT_HEIGHT,
+        ),
+    }
+    return fig, csv_string, False
+
+
+def render_plot_violin(data, variables, group, split, sample_size):
+    """Render the violin plot figure."""
+
+    if variables is None or len(variables) == 0 or group is None:
+        return {}, "", True
+
+    if sample_size != "all":
+        meta_here = data.ad.obssample(n=sample_size)
+    else:
+        meta_here = data.ad.obs
+
+    # select color palette
+    if split is None:
+        groupvals = meta_here[group].unique()
+        cm = colors.get_cm(groupvals)
+    else:
+        splitvals = meta_here[split].unique()
+        cm = colors.get_cm(splitvals)
+
+    nvar = len(variables)
+
+    fig = subplots.make_subplots(
+        rows=nvar,
+        cols=1,
+        specs=[[{}] for var in variables],
+        shared_xaxes=True,
+        vertical_spacing=0.001,
+    )
+
+    sg = 0
+    for nv, var in enumerate(variables):
+        if split is None:
+            for n, cv in enumerate(groupvals):
+                y = meta_here[meta_here[group] == cv][var]
+                tr = go.Violin(
+                    y=y,
+                    name=cv,
+                    fillcolor=cm[n % 40],
+                    line={"color": "gray", "width": 0.5},
+                    marker={"size": 1},
+                    spanmode="manual",
+                    span=[y.min(), None],
+                    showlegend=nv == 0,
+                    scalegroup=sg,
+                )
+                fig.append_trace(tr, nvar - nv, 1)
+                sg += 1
+            plot_data = data.ad.obs[variables + [group]]
+        else:
+            for n, sv in enumerate(splitvals):
+                y = meta_here[meta_here[split] == sv][var]
+                tr = go.Violin(
+                    x=meta_here[meta_here[split] == sv][group],
+                    y=y,
+                    name=sv,
+                    offsetgroup=n,
+                    scalegroup=sg,
+                    showlegend=nv == 0,
+                    fillcolor=cm[n % 40],
+                    line={"color": "gray", "width": 0.5},
+                    marker={"size": 1},
+                    spanmode="manual",
+                    span=[y.min(), None],
+                )
+                fig.append_trace(tr, nvar - nv, 1)
+                sg += 1
+            plot_data = data.ad.obs[variables + [group, split]]
+
+    for nv, var in enumerate(variables):
+        if len(variables) - nv == 1:
+            fig["layout"]["yaxis"].update(title=var)
+        else:
+            fig["layout"]["yaxis" + str(nvar - nv)].update(title=var)
+
+    fig["layout"].update(
+        xaxis={"title": group, "tickangle": -45},
+        margin={"l": 50, "b": 80, "t": 10, "r": 10},
+        legend={"x": 1.05, "y": 1},
+        hovermode="closest",
+        plot_bgcolor="rgb(255,255,255)",
+        height=settings.PLOT_HEIGHT,
+    )
+
+    if split is not None:
+        fig["layout"].update(violinmode="group")
+
+    csv_string = "data:text/csv;charset=utf-8," + urllib.parse.quote(
+        plot_data.to_csv(index=True, header=True, encoding="utf-8")
+    )
+
+    return fig, csv_string, False
+
+
+def render_plot_bars(data, group, split, options):
+    """render the bar chart plot."""
+
+    if group is None:
+        return {}, "", True
+
+    if options is None:
+        options = []
+
+    if split is None:
+        groupvals = data.ad.obs[group].unique()
+        cm = colors.get_cm(groupvals)
+        tally = data.ad.obs.groupby(group).size()
+        if "normalized" in options:
+            tally = tally.divide(tally.sum())
+        traces = []
+        for n, gv in enumerate(tally.index):
+            tr = go.Bar(
+                x=[gv],
+                y=[tally[gv]],
+                name=gv,
+                marker=dict(line=dict(color="gray", width=0.5), color=cm[n % 40]),
+            )
+            traces.append(tr)
+
+    else:
+        splitvals = data.ad.obs[split].cat.categories
+        cm = colors.get_cm(splitvals)
+
+        tally = data.ad.obs.groupby([group, split]).size()
+        if "normalized" in options:
+            tally = tally.divide(tally.sum(level=0).astype(float), level=0)
+        traces = []
+        for n, sv in enumerate(splitvals):
+            tr = go.Bar(
+                x=tally.xs(sv, level=1).index,
+                y=tally.xs(sv, level=1).values,
+                name=sv,
+                marker=dict(color=cm[n % 40], line=dict(color="gray", width=0.5)),
+            )
+            traces.append(tr)
+
+    fig = {
+        "data": traces,
+        "layout": go.Layout(
+            barmode="stack" if "stacked" in options else "group",
+            xaxis={"title": group, "tickangle": -45},
+            yaxis={"title": "cell frequency" if "normalized" in options else "cell number"},
+            margin={"l": 50, "b": 100, "t": 10, "r": 10},
+            legend={"x": 1.05, "y": 1},
+            plot_bgcolor="rgb(255,255,255)",
+            hovermode="closest",
+            height=settings.PLOT_HEIGHT,
+        ),
+    }
+
+    plot_data = tally
+    csv_string = "data:text/csv;charset=utf-8," + urllib.parse.quote(
+        plot_data.to_csv(index=True, header=True, encoding="utf-8")
+    )
+
+    return fig, csv_string, False
