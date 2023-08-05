fa63e32013392295d8648d2ebd96c2c854e24849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:25 +0000

    Add package bupt_api/0.0.1/bupt_api-0.0.1.tar.gz

---
+++ packages/bupt_api/bupt_api-0.0.1.tar.gz/bupt_api-0.0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='bupt_api',
+    version='0.0.1',
+    author='zekin',
+    author_email='wzekin@gmail.com',
+    url='https://github.com/WangZeKun/bupt-api',
+    description=u'北邮的一些api,陆续添加中',
+    install_requires=['beautifulsoup4', 'requests', 'ics'],
+)
