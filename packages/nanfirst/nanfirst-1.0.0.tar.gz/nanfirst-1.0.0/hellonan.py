fbc517f59657ddbfc78018f2402b8f8e20a68138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:25 +0000

    Add package nanfirst/1.0.0/nanfirst-1.0.0.tar.gz

---
+++ packages/nanfirst/nanfirst-1.0.0.tar.gz/nanfirst-1.0.0/hellonan.py
@@ -0,0 +1,8 @@
+""" My Hello World Pyton Module """
+
+def hello():
+    """ My Hello World Pyton Function """
+    print("hello nan!!")
+
+hello()
+help(hello);
---
+++ packages/nanfirst/nanfirst-1.0.0.tar.gz/nanfirst-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (
+    name = 'nanfirst',
+    version = '1.0.0',
+    py_modules = ['hellonan'],
+    author = 'nan',
+    author_email = 'zhangbaonan.cn@gmail.com',
+    description = 'My First PYPI Upload'
+    )
