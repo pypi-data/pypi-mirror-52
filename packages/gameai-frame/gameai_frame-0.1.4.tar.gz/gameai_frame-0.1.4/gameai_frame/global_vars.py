0e2beb0b4ef739e9c62d1a880efa1ebabd785d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:57 +0000

    Add package gameai-frame/0.1.4/gameai_frame-0.1.4.tar.gz

---
+++ packages/gameai-frame/gameai_frame-0.1.4.tar.gz/gameai_frame-0.1.4/gameai_frame/global_vars.py
@@ -0,0 +1,10 @@
+#redefine vals to fit in specific situation
+#define NN shape-----------------------------------
+NN_INPUT_SIZE = 32
+NN_OUTPUT_SIZE = 4
+NN_HIDDENLAYER_NUM = 2
+NN_HIDDEN_SIZE = 18
+#define AG coefs-----------------------------------
+AG_POP_SIZE = 100#种群数
+AG_POP_PM = 0.1#变异率
+AG_POP_PC = 0.7#交叉率
