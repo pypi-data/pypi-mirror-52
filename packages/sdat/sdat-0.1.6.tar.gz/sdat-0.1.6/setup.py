9e1401d51152ae56dc1887a066a6079bb280a4a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:31 +0000

    Add package sdat/0.1.6/sdat-0.1.6.tar.gz

---
+++ packages/sdat/sdat-0.1.6.tar.gz/sdat-0.1.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sdat',
+    version='0.1.6',
+    url='https://github.com/Chipeyown/SPLiT-seq-Data-Analysis_Toolkit',
+    description='A toolkit for analysing SPLiT-seq data',
+    author='Chipeyown',
+    author_email='chipeyown@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3'
+
+    ],
+
+    keywords='SPLiT-seq',
+
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'sdat=sdat:pipeline',
+        ],
+    },
+)
