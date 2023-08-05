e101d81fb60ed3d8f6bd819ffad07a3e7a5002a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:37 +0000

    Add package galileo-sdk/0.0.2/galileo-sdk-0.0.2.tar.gz

---
+++ packages/galileo-sdk/galileo-sdk-0.0.2.tar.gz/galileo-sdk-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='galileo-sdk',
+    version='0.0.2',
+    license='MIT',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'galileo-cli = galileo.cli:main',
+        ]
+    },
+    python_requires='>=3.6.7',
+    install_requires=[
+        'requests>=2.21.0',
+        'galileo-socketio',
+    ],
+    extras_require={
+        "docs": [
+            'sphinx>=2.2.0',
+            'sphinx_rtd_theme>=0.4.3'
+        ]
+    }
+)
