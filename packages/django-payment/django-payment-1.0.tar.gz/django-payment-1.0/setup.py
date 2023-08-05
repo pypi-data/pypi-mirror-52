8e4113a70026c8936ba55c81af45c2d21cf7a44a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:53 +0000

    Add package django-payment/1.0/django-payment-1.0.tar.gz

---
+++ packages/django-payment/django-payment-1.0.tar.gz/django-payment-1.0/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='django-payment',
+    version='1.0',
+    description='',
+    long_description='',
+    author='Nicholas Wolff',
+    author_email='nwolff@gmail.com',
+    url='https://github.com/skioo/django-payment',
+    download_url='https://pypi.python.org/pypi/django-payment',
+    packages=[
+        'payment',
+        'payment.gateways',
+        'payment.gateways.dummy',
+        'payment.gateways.stripe',
+        'payment.migrations',
+    ],
+    package_data={
+        'payment': ['locale/*/LC_MESSAGES/*.*', ]},
+    install_requires=[
+        'Django>=2.2,<2.3',
+        'django-money',
+        'structlog',
+        'typing',
+        'stripe',
+        'django-countries',
+        'dataclasses',
+        'django-import-export',
+    ],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
