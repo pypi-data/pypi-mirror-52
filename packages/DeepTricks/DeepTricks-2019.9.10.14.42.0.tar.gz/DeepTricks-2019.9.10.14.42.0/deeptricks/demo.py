0eb1c103ff2d961798bd743d3ecb2a347b19663e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:36 +0000

    Add package DeepTricks/2019.9.10.14.42.0/DeepTricks-2019.9.10.14.42.0.tar.gz

---
+++ packages/DeepTricks/DeepTricks-2019.9.10.14.42.0.tar.gz/DeepTricks-2019.9.10.14.42.0/deeptricks/demo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : DeepTricks.
+# @File         : demo
+# @Time         : 2019-09-10 14:41
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
+def main():
+    pass
