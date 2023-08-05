077ba91dc967d8d017f0a9c5704beb03c9bfbc2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:35 +0000

    Add package androidtv/0.0.27/androidtv-0.0.27.tar.gz

---
+++ packages/androidtv/androidtv-0.0.27.tar.gz/androidtv-0.0.27/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='androidtv',
+    version='0.0.27',
+    description='Communicate with an Android TV or Fire TV device via ADB over a network.',
+    url='https://github.com/JeffLIrion/python-androidtv/',
+    license='MIT',
+    author='Jeff Irion',
+    author_email='jefflirion@users.noreply.github.com',
+    packages=['androidtv'],
+    install_requires=['rsa', 'adb-homeassistant>=1.3.2', 'pure-python-adb-homeassistant>=0.1.7.dev0'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+    test_suite='tests'
+)
