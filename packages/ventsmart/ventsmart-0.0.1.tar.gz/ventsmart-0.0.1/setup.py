b9592c953bba367300c110dc0d2fb3218162d84c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:01 +0000

    Add package ventsmart/0.0.1/ventsmart-0.0.1.tar.gz

---
+++ packages/ventsmart/ventsmart-0.0.1.tar.gz/ventsmart-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+__project__ = "ventsmart"
+__version__ = "0.0.1"
+__discription__ = "Test module"
+__packages__ = ["fanscript"]
+__author__ = "Baguette Division"
+__author_email__ = "baguette@mara.com"
+__keywords__ = ["proof of concept", "learning"]
+# __requires__ = __...__
+
+setup(
+	name = __project__,
+	version = __version__,
+	description = __discription__,
+	packages = __packages__,
+	author = __author__,
+	author_email = __author_email__,
+	keywords = __keywords__,
+	
+	#requires = __requires__,
+	)
