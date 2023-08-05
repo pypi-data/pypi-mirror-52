1f8856d5a1c5d98b16d00f31ffc095c2c2066d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:41 +0000

    Add package streamdeck/0.4.0/streamdeck-0.4.0-py3-none-any.whl

---
+++ packages/streamdeck/streamdeck-0.4.0-py3-none-any.whl/StreamDeck/Devices/StreamDeckOriginal.py
@@ -0,0 +1,152 @@
+#         Python Stream Deck Library
+#      Released under the MIT license
+#
+#   dean [at] fourwalledcubicle [dot] com
+#         www.fourwalledcubicle.com
+#
+
+from .StreamDeck import StreamDeck
+
+
+class StreamDeckOriginal(StreamDeck):
+    """
+    Represents a physically attached original StreamDeck device.
+    """
+
+    KEY_COUNT = 15
+    KEY_COLS = 5
+    KEY_ROWS = 3
+
+    KEY_PIXEL_WIDTH = 72
+    KEY_PIXEL_HEIGHT = 72
+    KEY_PIXEL_DEPTH = 3
+    KEY_PIXEL_ORDER = "BGR"
+    KEY_IMAGE_CODEC = None
+    KEY_FLIP = (True, False)
+    KEY_ROTATION = 0
+
+    DECK_TYPE = "Stream Deck (Original)"
+
+    KEY_IMAGE_SIZE = KEY_PIXEL_WIDTH * KEY_PIXEL_HEIGHT * KEY_PIXEL_DEPTH
+    START_PAGE = 1
+    REPORT_LENGTH = 8191
+    IMAGE_BYTES_PAGE_1 = 2583 * 3
+
+    def _convert_key_id_origin(self, key):
+        """
+        Converts a key index from or to a origin at the physical top-left of
+        the StreamDeck device.
+
+        :param int key: Index of the button with either a device or top-left origin.
+
+        :rtype: int
+        :return: Key index converted to the opposite key origin (device or top-left).
+        """
+
+        key_col = key % self.KEY_COLS
+        return (key - key_col) + ((self.KEY_COLS - 1) - key_col)
+
+    def _read_key_states(self):
+        """
+        Reads the key states of the StreamDeck. This is used internally by
+        :func:`~StreamDeck._read` to talk to the actual device.
+
+        :rtype: list(bool)
+        :return: Button states, with the origin at the top-left of the deck.
+        """
+
+        states = self.device.read(1 + self.KEY_COUNT)[1:]
+        return [bool(states[self._convert_key_id_origin(k)]) for k in range(self.KEY_COUNT)]
+
+    def reset(self):
+        """
+        Resets the StreamDeck, clearing all button images and showing the
+        standby image.
+        """
+
+        payload = bytearray(17)
+        payload[0:2] = [0x0B, 0x63]
+        self.device.write_feature(payload)
+
+    def set_brightness(self, percent):
+        """
+        Sets the global screen brightness of the StreamDeck, across all the
+        physical buttons.
+
+        :param int/float percent: brightness percent, from [0-100] as an `int`.
+        """
+
+        percent = min(max(percent, 0), 100)
+
+        payload = bytearray(17)
+        payload[0:6] = [0x05, 0x55, 0xaa, 0xd1, 0x01, percent]
+        self.device.write_feature(payload)
+
+    def get_serial_number(self):
+        """
+        Gets the serial number of the attached StreamDeck.
+
+        :rtype: str
+        :return: String containing the serial number of the attached device.
+        """
+
+        serial = self.device.read_feature(0x03, 17)
+        return "".join(map(chr, serial[5:]))
+
+    def get_firmware_version(self):
+        """
+        Gets the firmware version of the attached StreamDeck.
+
+        :rtype: str
+        :return: String containing the firmware version of the attached device.
+        """
+
+        version = self.device.read_feature(0x04, 17)
+        return "".join(map(chr, version[5:]))
+
+    def set_key_image(self, key, image):
+        """
+        Sets the image of a button on the StremDeck to the given image. The
+        image being set should be in the correct format for the device, as an
+        enumerable collection of bytes.
+
+        .. seealso:: See :func:`~StreamDeck.get_key_image_format` method for
+                     information on the image format accepted by the device.
+
+        :param int key: Index of the button whose image is to be updated.
+        :param enumerable image: Raw data of the image to set on the button.
+                                 If `None`, the key will be cleared to a black
+                                 color.
+        """
+
+        image = bytes(image or self.KEY_IMAGE_SIZE)
+
+        if min(max(key, 0), self.KEY_COUNT) != key:
+            raise IndexError("Invalid key index {}.".format(key))
+
+        if len(image) != self.KEY_IMAGE_SIZE:
+            raise ValueError("Invalid image size {}.".format(len(image)))
+
+        key = self._convert_key_id_origin(key)
+
+        header_1 = [
+            0x02, 0x01, self.START_PAGE, 0x00, 0x00, key + 1, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x42, 0x4d, 0xf6, 0x3c, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x36, 0x00, 0x00, 0x00, 0x28, 0x00,
+            0x00, 0x00, 0x48, 0x00, 0x00, 0x00, 0x48, 0x00,
+            0x00, 0x00, 0x01, 0x00, 0x18, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0xc0, 0x3c, 0x00, 0x00, 0xc4, 0x0e,
+            0x00, 0x00, 0xc4, 0x0e, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+        ]
+        header_2 = [
+            0x02, 0x01, 0x02, 0x00, 0x01, key + 1, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+        ]
+
+        payload_1 = bytes(header_1) + image[: self.IMAGE_BYTES_PAGE_1]
+        payload_2 = bytes(header_2) + image[self.IMAGE_BYTES_PAGE_1:]
+
+        self.device.write(payload_1)
+        self.device.write(payload_2)
---
+++ packages/streamdeck/streamdeck-0.4.0-py3-none-any.whl/StreamDeck/ImageHelpers/PILHelper.py
@@ -0,0 +1,77 @@
+#         Python Stream Deck Library
+#      Released under the MIT license
+#
+#   dean [at] fourwalledcubicle [dot] com
+#         www.fourwalledcubicle.com
+#
+
+
+def create_image(deck, background='black'):
+    """
+    Creates a new PIL Image with the correct image dimensions for the given
+    StreamDeck device's keys.
+
+    .. seealso:: See :func:`~PILHelper.to_native_format` method for converting a
+                 PIL image instance to the native image format of a given
+                 StreamDeck device.
+
+    :param StreamDeck deck: StreamDeck device to generate a compatible image for.
+    :param str background: Background color to use, compatible with `PIL.Image.new()`.
+
+    :rtype: PIL.Image
+    :return: Created PIL image
+    """
+    from PIL import Image
+
+    image_format = deck.key_image_format()
+
+    dimensions = (image_format['width'], image_format['height'])
+    return Image.new("RGB", dimensions, background)
+
+
+def to_native_format(deck, image):
+    """
+    Converts a given PIL image to the native image format for a StreamDeck,
+    suitable for passing to :func:`~StreamDeck.set_key_image`.
+
+    .. seealso:: See :func:`~PILHelper.create_image` method for creating a PIL
+                 image instance for a given StreamDeck device.
+
+    :param StreamDeck deck: StreamDeck device to generate a compatible native image for.
+    :param PIL.Image image: PIL Image to convert to the native StreamDeck image format
+
+    :rtype: enumerable()
+    :return: Image converted to the given StreamDeck's native format
+    """
+    from PIL import Image
+
+    image_format = deck.key_image_format()
+
+    if image_format['rotation']:
+        image = image.rotate(image_format['rotation'])
+
+    if image_format['flip'][0]:
+        image = image.transpose(Image.FLIP_LEFT_RIGHT)
+
+    if image_format['flip'][1]:
+        image = image.transpose(Image.FLIP_TOP_BOTTOM)
+
+    expected_size = (image_format['width'], image_format['height'])
+    if image.size != expected_size:
+        image.thumbnail(expected_size)
+
+    if image_format['codec']:
+        import io
+
+        # We want a compressed image in a given codec, convert.
+        compressed_image = io.BytesIO()
+        image.save(compressed_image, image_format['codec'])
+        return compressed_image.getbuffer()
+    else:
+        # Get the raw r, g and b components of the generated image
+        r, g, b = image.split()
+
+        # Recombine the B, G and R elements in the order the display expects
+        # them, and convert the resulting image to a sequence of bytes
+        rgb = {"R": r, "G": g, "B": b}
+        return Image.merge("RGB", [rgb[c] for c in image_format['order']]).tobytes()
