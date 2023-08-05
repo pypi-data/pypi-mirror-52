883d804696f089c9f99865ada80cf4fe7b0c9a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:45 +0000

    Add package rt-probability/1.0/rt_probability-1.0.tar.gz

---
+++ packages/rt-probability/rt_probability-1.0.tar.gz/rt_probability-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='rt_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['rt_probability'],
+      author='Ravi T.',
+      zip_safe=False)
