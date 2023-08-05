bbf76faedd111f9b687145b5f1c38eb1ffda201a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:28 +0000

    Add package yahoo-panoptes/1.1.266/yahoo_panoptes-1.1.266.tar.gz

---
+++ packages/yahoo-panoptes/yahoo_panoptes-1.1.266.tar.gz/yahoo_panoptes-1.1.266/tests/mock_panoptes_producer.py
@@ -0,0 +1,34 @@
+"""
+Copyright 2019, Verizon Media Inc.
+Licensed under the terms of the Apache 2.0 license. See LICENSE file in project root for terms.
+"""
+
+from yahoo_panoptes.framework.validators import PanoptesValidators
+
+
+class MockPanoptesMessageProducer(object):
+
+    def __init__(self):
+        self._kafka_client = dict()
+        self._kafka_client['stopped'] = False
+        self._kafka_producer = []
+
+    def __del__(self):
+        pass
+
+    def send_messages(self, topic, key, messages, partitioning_key=None):
+
+        assert PanoptesValidators.valid_nonempty_string(topic), 'topic must be a non-empty string'
+        assert PanoptesValidators.valid_nonempty_string(key), 'key must be a non-empty string'
+        assert PanoptesValidators.valid_nonempty_string(messages), 'messages must be a non-empty string'
+
+        self._kafka_producer.append({
+            'topic': topic,
+            'key': key,
+            'message': messages
+        })
+
+    def stop(self):
+
+        if not self._kafka_client['stopped']:
+            self._kafka_client['stopped'] = True
