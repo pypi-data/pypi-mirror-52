19b4a55ea5c157d2c20e23e9c65dfe4e0a121ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:57 +0000

    Add package sample-text/0.1.0/sample_text-0.1.0-py3-none-any.whl

---
+++ packages/sample-text/sample_text-0.1.0-py3-none-any.whl/tests/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+"""Unit test package for sample_text."""
---
+++ packages/sample-text/sample_text-0.1.0-py3-none-any.whl/tests/test_sample_text.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `sample_text` package."""
+
+import pytest
+
+
+import sample_text
+
+
+def test_foobar():
+    assert False, "Write a test!"
+
