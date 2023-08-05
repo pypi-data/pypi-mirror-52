cb82734f2cb1601fff0f937e90e99670e62030a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:59 +0000

    Add package ta4/0.3.35/ta4-0.3.35-py2-none-any.whl

---
+++ packages/ta4/ta4-0.3.35-py2-none-any.whl/ta4/analyzer.py
@@ -0,0 +1,73 @@
+#! coding: utf-8
+from operator import attrgetter
+
+class ExactAnalyzer(object):
+    def get_sentence_placeholders(self, sentence):
+        return filter(attrgetter('is_word'), sentence.place_holders)
+
+    def mark(self, keyword, sentence, number, should_mark=True):
+        placeholders = self.get_sentence_placeholders(sentence)
+        placeholders_count = len(placeholders)
+        stop = placeholders_count - len(keyword.place_holders)
+        i = 0
+        while i < placeholders_count:
+            if i > stop:
+                break
+            skipped_words = 0
+            for j, ph in enumerate(keyword.place_holders):
+                if ph.is_special:
+                    # * - пропустить только одно значимое слово(и сколько угодно не значимых)
+                    meaning_word = 0
+                    while True:
+                        sent_placeholder_index = i + j
+                        is_last_sent_ph = sent_placeholder_index == (placeholders_count - 1)
+                        sent_placeholder = placeholders[sent_placeholder_index]
+                        # Последнее слово в предложении является спец знаком,
+                        # что приводит к рекурсии
+                        if sent_placeholder.is_special and is_last_sent_ph:
+                            break
+                        if sent_placeholder.is_important:
+                            meaning_word += 1
+                        # если встретили уже второе значимое слово
+                        if is_last_sent_ph:
+                            break
+                        elif meaning_word == 2 or i == placeholders_count - j - 1:
+                            i -= 1
+                            skipped_words -= 1
+                            meaning_word -= 1
+                            break
+                        i += 1
+                        skipped_words += 1
+                    if not meaning_word:
+                        break
+                else:
+                    index = i+j
+                    if index >= placeholders_count or not self.equals(ph, placeholders[index]):
+                        break
+            else:
+                if should_mark:
+                    words_count = j + skipped_words
+                    while words_count >= 0:
+                        placeholders[i-skipped_words+words_count].add_marker(keyword, words_count, number)
+                        words_count -= 1
+                number += 1
+            i += 1
+        return number
+
+    def equals(self, ph, other):
+        return ph.word == other.word or ph.is_special
+
+
+class SubformsAnalyzer(ExactAnalyzer):
+    def get_sentence_placeholders(self, sentence):
+        place_holders = super(SubformsAnalyzer, self).get_sentence_placeholders(sentence)
+        return filter(attrgetter('is_important'), place_holders)
+
+    def equals(self, ph, other):
+        u"""
+        Порядок следования аргументов важен
+
+        :param ph: placeholder из искомого слова
+        :param other: placeholder из текста
+        """
+        return ph.is_special or (ph.get_all_normal_phorms() & other.get_all_normal_phorms())
---
+++ packages/ta4/ta4-0.3.35-py2-none-any.whl/ta4/text.py
@@ -0,0 +1,89 @@
+# coding=utf-8
+from copy import deepcopy
+from operator import attrgetter
+from bs4 import BeautifulSoup, Tag, NavigableString
+from lxml.html import diff
+
+from .utils import get_sentences
+from .diff import WHITESPACE_TEMPLATE
+
+
+IGNORED_TAG_TEMPLATE = u'ignored_{}'
+
+
+class TextHtml(object):
+    def __init__(self, html, ignored_tags=[]):
+        """Класс для рабоыт с HTML
+
+        Args:
+            html (unicode): HTML
+            ignored_tags (list, optional): Список для исключения тегов в формте BeautifulSoup ('tagname', {'attribute': 'value'})
+        """
+        self.bs = BeautifulSoup(self.prepare_html(html), 'html.parser')
+        self.text = html
+        self.sentences = []
+        self.ignored_element_map = {}
+
+        for tag in ignored_tags:
+            for elem in self.bs.findAll(*tag):
+                i = len(self.ignored_element_map) + 1
+                tag = Tag(name=IGNORED_TAG_TEMPLATE.format(i))
+                self.ignored_element_map[tag] = elem
+                elem.replace_with(tag)
+
+        texts = self.bs.findAll(text=True)
+
+        for text in texts:
+            text.replace_with(NavigableString(text.replace(" ", WHITESPACE_TEMPLATE)))
+
+        # remove old markers
+        for element in self.bs.findAll(attrs={'data-markers': True}):
+            element.unwrap()
+
+        self.text_after_replacement = unicode(self.bs)
+        self.structure, self.sentences = get_sentences(unicode(self.bs))
+
+    def __iter__(self):
+        for sentence in self.sentences:
+            yield sentence
+
+    def __len__(self):
+        return len(self.sentences)
+
+    def __repr__(self):
+        return self.text_after_replacement.encode('utf-8')
+
+    def prepare_html(self, html):
+        return html.replace('<br>', '<br/>').replace('<hr>', '<hr/>').replace('</br>', '')\
+                   .replace('</hr>', '').replace("&nbsp;", "<nbsp/>")
+
+    def build_html(self):
+        u"""
+        build_html может вызываться несколько раз, и так как мы добавляем маркеры в теги,
+        то надо копировать структуру
+        """
+        html = []
+        for tokens, place_holder in deepcopy(self.structure):
+            if place_holder and place_holder.markers:
+                markers = u' '.join(map(attrgetter('hash'), place_holder.markers))
+                targets = [u'target-'+h for marker in place_holder.markers for h in marker.target_sentence]
+                targets = u' '.join(targets)
+                open_tag = u'<span data-markers="%s">' % (u' '.join([markers, targets]).strip())
+                tokens[0].pre_tags.append(open_tag)
+                tokens[-1].post_tags.insert(0, u'</span>')
+            html.append(u''.join(diff.expand_tokens(tokens)))
+
+        html = u''.join(html)
+        html = html.replace("&nbsp;", "<nbsp/>")
+        html = BeautifulSoup(self.prepare_html(html), 'html.parser')
+
+        for ignored_tag, original_tag in self.ignored_element_map.items():
+            tag = html.find(ignored_tag.name, attr=ignored_tag.attrs, text=ignored_tag.text)
+            if tag:
+                tag.replace_with(original_tag)
+        return unicode(html).replace(u"<nbsp></nbsp>", u"\xa0")
+
+    def remove_markers(self):
+        for sentence in self:
+            for ph in sentence.place_holders:
+                ph.clean()
