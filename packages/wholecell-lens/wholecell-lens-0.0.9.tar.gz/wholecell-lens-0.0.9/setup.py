1be0384ef349f67e0cffe58cbc0b2877a204045d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:20 +0000

    Add package wholecell-lens/0.0.9/wholecell-lens-0.0.9.tar.gz

---
+++ packages/wholecell-lens/wholecell-lens-0.0.9.tar.gz/wholecell-lens-0.0.9/setup.py
@@ -0,0 +1,37 @@
+import os
+import glob
+import setuptools
+from distutils.core import setup
+
+with open("README.md", 'r') as readme:
+    long_description = readme.read()
+
+with open("requirements.txt", 'r') as requirements:
+    install_requires = list(requirements.read().splitlines())
+
+setup(
+    name='wholecell-lens',
+    version='0.0.9',
+    packages=[
+        'lens',
+        'lens.actor',
+        'lens.analysis',
+        'lens.environment',
+        'lens.processes',
+        'lens.reconstruction',
+        'lens.surrogates',
+		'lens.utils'],
+    author='Eran Agmon, Ryan Spangler',
+    author_email='eagmon@stanford.edu, spanglry@stanford.edu',
+    url='https://github.com/CovertLab/Lens',
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'lens.actor.boot=lens.actor.boot:run',
+            'lens.actor.control=lens.actor.control:run',
+            'lens.environment.boot=lens.environment.boot:run',
+            'lens.environment.control=lens.environment.control:run',
+            'lens.composites=lens.composites:run']},
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=install_requires)
