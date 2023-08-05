864cb2e169efd607dc97d08419bcac056faed829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:03 +0000

    Add package pyterranetes/0.8.0/pyterranetes-0.8.0-py2.py3-none-any.whl

---
+++ packages/pyterranetes/pyterranetes-0.8.0-py2.py3-none-any.whl/p10s/base.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from p10s.values import value
+
+
+class BaseContext:
+    def __init__(self, input=None, output=None):
+        if input is None:
+            self.input = value("p10s", {}).get("file", None)
+        else:
+            self.input = Path(input)
+
+        if output is not None:
+            self.output = Path(output)
+        elif self.input is not None:
+            self.output = self.input.with_suffix(self.output_file_extension)
+        else:
+            self.output = None
+
+    def render(self):
+        raise NotImplementedError()  # pragma: no cover
+
+    def _output_stream(self):
+        self.output.parent.mkdir(parents=True, exist_ok=True)
+        return self.output.open("w")
+
+    def __repr__(self):
+        return "<" + self.__module__ + "." + self.__class__.__name__ + ">"
