627a14410e1a01beb26329602cbe9a556e6f01de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:49 +0000

    Add package pm4pymdl/0.0.2/pm4pymdl-0.0.2-py3-none-any.whl

---
+++ packages/pm4pymdl/pm4pymdl-0.0.2-py3-none-any.whl/pm4pymdl/algo/mvp/utils/succint_mdl_to_exploded_mdl.py
@@ -0,0 +1,12 @@
+from pm4pymdl.algo.mvp.utils import succint_stream_to_exploded_stream
+import pandas as pd
+
+
+def apply(df):
+    stream = df.to_dict('r')
+
+    exploded_stream = succint_stream_to_exploded_stream.apply(stream)
+    df.type = "exploded"
+
+    return pd.DataFrame(exploded_stream)
+
