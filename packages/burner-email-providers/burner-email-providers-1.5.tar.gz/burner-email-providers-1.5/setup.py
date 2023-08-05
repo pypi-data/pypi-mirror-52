5ae9676e5350576fb49245fecffe7ce04ef04f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:57 +0000

    Add package burner-email-providers/1.5/burner-email-providers-1.5.tar.gz

---
+++ packages/burner-email-providers/burner-email-providers-1.5.tar.gz/burner-email-providers-1.5/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='burner-email-providers',
+      version='1.5',
+      description='Burner Email Providers',
+      author='Various Artists',
+      author_email='everyone@example.com',
+      url='https://github.com/WCF/burner-email-providers/',
+      packages=['burner_email_providers'],
+     )
