fe0ba964dc98878bec9232e35143fb5f82d1ea44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:12 +0000

    Add package pupy/2.22.3/pupy-2.22.3.tar.gz

---
+++ packages/pupy/pupy-2.22.3.tar.gz/pupy-2.22.3/pupy/_jasm.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Pretty ~ Useful ~ Python
+
+
+def _json():
+    for mod in ("rapidjson", "ujson", "json"):
+        try:
+            res = __import__(mod)
+            return res
+        except ImportError:
+            pass
+    raise ImportError(
+        "Ya need one of the following:\n"
+        "   ~ ujson; 'pip install ujson'\n"
+        "   ~ ujson; 'pip install python-rapidjson'\n"
+        "   ~ json; this should have come with python\n"
+    )
+
+
+json = _json()
---
+++ packages/pupy/pupy-2.22.3.tar.gz/pupy-2.22.3/pupy/ui.pyi
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Pretty ~ Useful ~ Python
+
+from typing import Any
+
+def yesno(question: Any, default: bool = ..., tries: int = ...): ...
