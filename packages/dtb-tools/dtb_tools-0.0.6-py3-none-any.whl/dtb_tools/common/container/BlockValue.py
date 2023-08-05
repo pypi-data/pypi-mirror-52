03a813b99dcdc50122cb5aa8a87d61f563eb993f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:18:16 +0000

    Add package dtb-tools/0.0.6/dtb_tools-0.0.6-py3-none-any.whl

---
+++ packages/dtb-tools/dtb_tools-0.0.6-py3-none-any.whl/dtb_tools/common/container/BlockValue.py
@@ -0,0 +1,37 @@
+import threading
+from time import monotonic
+
+
+class BlockValue:
+    """
+        这是一个阻塞的 写入/读取 的模型，
+    """
+
+    def __init__(self):
+        self.mutex = threading.Lock()
+        self.not_empty = threading.Condition(self.mutex)
+        self.set_lock = threading.Condition(self.mutex)
+        self.value = None
+
+    def get(self, timeout=None):
+        with self.not_empty:
+            if timeout is None:
+                while self.value is None:
+                    self.not_empty.wait()
+            elif timeout < 0:
+                raise ValueError("'timeout' must be a non-negative number")
+            else:
+                endtime = monotonic() + timeout
+                while self.value is None:
+                    remaining = endtime - monotonic()
+                    if remaining <= 0.0:
+                        raise RuntimeError("time out")
+                    self.not_empty.wait(remaining)
+            item = self.value
+            self.value = None
+            return item
+
+    def put(self, item):
+        with self.set_lock:
+            self.value = item
+            self.not_empty.notify()
