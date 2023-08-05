32bad3f447a850199d69f47528250becc6d3f542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:48 +0000

    Add package vars-pickle/0.2.9/vars_pickle-0.2.9.tar.gz

---
+++ packages/vars-pickle/vars_pickle-0.2.9.tar.gz/vars_pickle-0.2.9/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 14:45:27 2019
+
+@author: 10167232
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vars_pickle",
+    version="0.2.9",
+    author="karond",
+    author_email="dingyaohui.g@outlook.com",
+    description="Save your variable as Rstudio",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    install_requires = ['pandas','numpy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
---
+++ packages/vars-pickle/vars_pickle-0.2.9.tar.gz/vars_pickle-0.2.9/vars_pickle/__Config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  6 08:27:35 2019
+
+@author: 10167232
+"""
+path = 'data_pickle'
+type_level = [['pandas.core.frame.DataFrame','pandas.core.series.Series','numpy.ndarray'],
+              ['str','function','pandas.core.frame.DataFrame','pandas.core.series.Series','numpy.ndarray','int','float','bool','complex','list','dict','set','tuple']]
+drop_list = ['In','Out','var_dic_list','get_ipython','']
+
+     
