2632ee4f8325aed9a50d8cba1f9cb29f6280d430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:43 +0000

    Add package codeforces-rating/1.2/codeforces-rating-1.2.tar.gz

---
+++ packages/codeforces-rating/codeforces-rating-1.2.tar.gz/codeforces-rating-1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name="codeforces-rating",
+    version=1.2,
+    description=(
+        "Codeforces Rating system implementation in python"
+    ),
+    author="officeyutong",
+    author_email="officeyutong@163.com",
+    maintainer="officeyutong",
+    maintainer_email="officeyutong@163.com",
+    license="MIT License",
+    packages=find_packages(),
+    platforms=["all"],
+    url="https://github.com/Officeyutong/codeforces-rating",
+
+)
