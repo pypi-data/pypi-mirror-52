b183482a554fde417d1d9c553e853326b00fb029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:28 +0000

    Add package cm-platform-library/0.0.7/cm_platform_library-0.0.7.tar.gz

---
+++ packages/cm-platform-library/cm_platform_library-0.0.7.tar.gz/cm_platform_library-0.0.7/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cm_platform_library',
+    version='0.0.7',
+    author="Trent Murray",
+    author_email="trent@cloudmicro.io",
+    description="Handles our platform and authentication requests",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/cloudmicro/pypi/cm_platform_library",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'sqlalchemy',
+        'cm_service_library',
+        'psycopg2-binary',
+        'databases[postgresql]',
+    ]
+)
