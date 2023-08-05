bda009fa3868b0112f971a59d8168b7f0128eb5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:42 +0000

    Add package czpubtran/0.1.7/czpubtran-0.1.7.tar.gz

---
+++ packages/czpubtran/czpubtran-0.1.7.tar.gz/czpubtran-0.1.7/test/test.py
@@ -0,0 +1,26 @@
+import asyncio
+import logging
+import aiohttp
+from czpubtran.api import czpubtran
+
+logging.basicConfig(level=logging.ERROR)
+
+async def test():
+    session=aiohttp.ClientSession(raise_for_status=True)
+    bus = czpubtran(session,'')
+    await bus.async_find_connection('Chyne, Haje','Ortenovo namesti','ABCz')
+    print(f'Connection from {bus.origin} to {bus.destination} using timetable{bus.combination_id}')
+    print('------------------------------------------------')
+    print(f'Departure: {bus.departure} line {bus.line}')
+    print(f'Duration: {bus.duration}')
+    print('Connections:')
+    for connection in bus.connections:
+        print(f"line {connection['line']} "
+            f"at {connection['depTime']} "
+            f"from {connection['depStation']} "
+            f"-> {connection['arrStation']} "
+            f"{connection['arrTime']} "
+            f"(delay: {connection['delay']} min)")
+    await session.close()
+
+asyncio.run(test())
