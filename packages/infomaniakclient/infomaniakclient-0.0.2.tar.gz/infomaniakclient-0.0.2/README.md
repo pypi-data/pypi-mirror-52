ca339a6af72d589268e00a2dd19d0d33df9b7060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:51 +0000

    Add package infomaniakclient/0.0.2/infomaniakclient-0.0.2.tar.gz

---
+++ packages/infomaniakclient/infomaniakclient-0.0.2.tar.gz/infomaniakclient-0.0.2/README.md
@@ -0,0 +1,11 @@
+# InfomaniakNewsletterClient
+Integration of Infomaniak Newsletter API in Python
+https://newsletter.infomaniak.com/docs/index.html
+
+### Usage
+```
+pip install infomaniakclient
+```
+
+You'll find some examples in the examples.py file in this github's repo:
+https://github.com/Potrac/InfomaniakNewsletterClient
---
+++ packages/infomaniakclient/infomaniakclient-0.0.2.tar.gz/infomaniakclient-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+with open("requirements.txt") as f:
+    install_requires = f.read().split()
+
+setuptools.setup(
+    name="infomaniakclient",
+    version="0.0.2",
+    author="Potrac Grognuz",
+    author_email="potrac@grognuz.site",
+    description="Infomaniak Newsletter python API client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Potrac/InfomaniakNewsletterClient",
+    keywords="infomaniak newsletter api client",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=install_requires,
+)
