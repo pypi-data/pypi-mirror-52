7fcf14b8687fb0c8f816fbe26bf850e8de2529e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:46 +0000

    Add package flake8-absolute-import/1.0/flake8_absolute_import-1.0-py3-none-any.whl

---
+++ packages/flake8-absolute-import/flake8_absolute_import-1.0-py3-none-any.whl/flake8_absolute_import/version.py
@@ -0,0 +1,24 @@
+r"""*Version definition file for* ``flake8-absolute-import``.
+
+flake8 plugin to require absolute imports
+
+**Author**
+    Brian Skinn (bskinn@alum.mit.edu)
+
+**File Created**
+    6 Sep 2019
+
+**Copyright**
+    \(c) Brian Skinn 2019
+
+**Source Repository**
+    http://www.github.com/bskinn/flake8-absolute-import
+
+**License**
+    The MIT License; see |license_txt|_ for full license terms
+
+**Members**
+
+"""
+
+__version__ = "1.0"
