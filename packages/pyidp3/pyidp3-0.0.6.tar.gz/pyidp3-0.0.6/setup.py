300008965e58f786adf7a7015072d6de2683c5b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:38 +0000

    Add package pyidp3/0.0.6/pyidp3-0.0.6.tar.gz

---
+++ packages/pyidp3/pyidp3-0.0.6.tar.gz/pyidp3-0.0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='pyidp3',
+    version='0.0.6',
+    description='A Python3 to IDP API',
+    author='Simon Vandevelde',
+    author_email="simon.vandevelde@hotmail.com",
+    url="https://gitlab.com/Salt_Factory/pyidp3",
+    packages=['pyidp3'],
+)
