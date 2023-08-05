b31a5f1953068ada21a33ceffa199b9be98c2c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:29 +0000

    Add package fair-research-login/0.1.4/fair_research_login-0.1.4-py2.py3-none-any.whl

---
+++ packages/fair-research-login/fair_research_login-0.1.4-py2.py3-none-any.whl/fair_research_login/exc.py
@@ -0,0 +1,53 @@
+
+
+class LoginException(Exception):
+    pass
+
+
+class InvalidTokenFormat(LoginException):
+    def __init__(self, message, code=None):
+        super(InvalidTokenFormat, self).__init__(message)
+        self.code = code
+
+    def __str__(self):
+        return '({}) {}'.format(super(InvalidTokenFormat, self).__str__(),
+                                self.code)
+
+
+class LocalServerError(LoginException):
+    pass
+
+
+class LoadError(LoginException):
+    pass
+
+
+class TokenStorageDisabled(LoadError):
+    pass
+
+
+class NoSavedTokens(LoadError):
+    """There were no saved tokens to load"""
+    pass
+
+
+class ScopesMismatch(LoadError):
+    """
+    Requested scopes do not match loaded scopes
+    """
+    pass
+
+
+class TokensExpired(LoadError):
+    """
+    Tokens have expired
+    """
+    def __init__(self, *args, **kwargs):
+        super(TokensExpired, self).__init__(*args)
+        self.resource_servers = kwargs.get('resource_servers', ())
+
+    def __str__(self):
+        return '{} {}'.format(
+            super(TokensExpired, self).__str__(),
+            ', '.join(self.resource_servers)
+        )
