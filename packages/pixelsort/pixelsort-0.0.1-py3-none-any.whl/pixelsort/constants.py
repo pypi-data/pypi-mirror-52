6fec56be8830ea1beb1e0ddea94d0e0be943496e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:53 +0000

    Add package pixelsort/0.0.1/pixelsort-0.0.1-py3-none-any.whl

---
+++ packages/pixelsort/pixelsort-0.0.1-py3-none-any.whl/pixelsort/constants.py
@@ -0,0 +1,11 @@
+black_pixel = (0, 0, 0, 255)
+white_pixel = (255, 255, 255, 255)
+defaults = {
+    "interval_function": "threshold",
+    "lower_threshold": 0.25,
+    "upper_threshold": 0.8,
+    "clength": 50,
+    "angle": 0,
+    "randomness": 0,
+    "sorting_function": "lightness",
+}
