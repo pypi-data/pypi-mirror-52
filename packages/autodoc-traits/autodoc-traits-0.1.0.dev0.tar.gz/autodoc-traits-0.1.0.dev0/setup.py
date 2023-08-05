d3902d2e0db19932d34cfc3903e01b602ea72ecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:20 +0000

    Add package autodoc-traits/0.1.0.dev0/autodoc-traits-0.1.0.dev0.tar.gz

---
+++ packages/autodoc-traits/autodoc-traits-0.1.0.dev0.tar.gz/autodoc-traits-0.1.0.dev0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+from autodoc_traits import __version__ as version
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="autodoc-traits",
+    version=version,
+    author="Jupyter Development Team",
+    author_email="jupyter@googlegroups.com",
+    description="Sphinx extension to autodoc traitlets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://jupyter.org",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=["sphinx", "traitlets"],
+)
