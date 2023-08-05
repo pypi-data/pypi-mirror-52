9534cc0dfb4384b1b0e9a52d77aaa5de87165180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:13 +0000

    Add package hrdagen/0.0.0/hrdagen-0.0.0.tar.gz

---
+++ packages/hrdagen/hrdagen-0.0.0.tar.gz/hrdagen-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='hrdagen',
+    version='0.0.0',
+    description='HR and Recruitment data generator',
+    url='',
+    author='Nikolay Dudaev',
+    author_email='nikolay.dudaev@icloud.com',
+    license='MIT',
+    packages=[
+        'hrdagen',
+    ],
+    zip_safe=False,
+)
