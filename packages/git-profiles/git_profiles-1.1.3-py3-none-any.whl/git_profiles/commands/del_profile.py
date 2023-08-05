e29bcbb8c35f380a38a0b6f4d76570dfb50fd82d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:56 +0000

    Add package git-profiles/1.1.3/git_profiles-1.1.3-py3-none-any.whl

---
+++ packages/git-profiles/git_profiles-1.1.3-py3-none-any.whl/git_profiles/commands/del_profile.py
@@ -0,0 +1,22 @@
+import git_profiles.utils.messages as msg
+from git_profiles.commands.base_command import BaseCommand
+
+
+class DelProfile(BaseCommand):
+    def execute(self) -> None:
+        """
+        Delete a profile from the configuration file. If there
+        isn't a profile with the given name, the user will be
+        alerted.
+        """
+        profile_name = self.args.profile[0]
+        if not self.git_manager.check_profile_exist(profile_name):
+            if not self.quiet:
+                print(msg.ERR_NO_PROFILE.format(profile_name))
+            return
+
+        full_profile = "profile.{0}".format(profile_name)
+        self.git_manager.del_profile(full_profile)
+
+        if not self.quiet:
+            print(msg.INFO_DEL_SUCCESS.format(profile_name))
---
+++ packages/git-profiles/git_profiles-1.1.3-py3-none-any.whl/git_profiles/commands/use_profile.py
@@ -0,0 +1,28 @@
+import git_profiles.utils.messages as msg
+from git_profiles.commands.base_command import BaseCommand
+
+
+class UseProfile(BaseCommand):
+    def execute(self) -> None:
+        """
+        Sets the given profile name as the active one, if it exists.
+        By default, it is set for the current project. If the global
+        parameter is given, it will be set globally.
+        """
+        profile_name = self.args.profile[0]
+        if not self.git_manager.check_profile_exist(profile_name):
+            if not self.quiet:
+                print(msg.ERR_NO_PROFILE.format(profile_name))
+            return
+
+        # Get the details of the profile
+        full_name = "profile.{0}".format(profile_name)
+        profile = self.git_manager.get_profile(full_name)
+
+        # Set the profile
+        self.git_manager.set_profile(profile, self.args.globally)
+        if not self.quiet:
+            if self.args.globally:
+                print(msg.INFO_SWITCH_GLOBALLY.format(profile_name))
+            else:
+                print(msg.INFO_SWITCH_LOCALLY.format(profile_name))
