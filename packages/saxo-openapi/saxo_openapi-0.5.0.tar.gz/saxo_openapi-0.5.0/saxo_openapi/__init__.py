d5bd19f8c095b6bb55cb24d844529e9c5a6891b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:51 +0000

    Add package saxo-openapi/0.5.0/saxo_openapi-0.5.0.tar.gz

---
+++ packages/saxo-openapi/saxo_openapi-0.5.0.tar.gz/saxo_openapi-0.5.0/saxo_openapi/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for SAXO OpenAPI."""
+
+__title__ = "Saxo OpenAPI API Wrapper"
+__author__ = """Feite Brekeveld"""
+__email__ = 'f.brekeveld@gmail.com'
+__version__ = '0.5.0'
+__copyright__ = "Copyright 2019 Feite Brekeveld"
+
+
+# Version synonym
+VERSION = __version__
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+try:
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
+
+from .saxo_openapi import API
+from .exceptions import OpenAPIError
