201a1ba04739447297eb464f8967aa0bbedb3225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:30 +0000

    Add package marktest/1.0.2/marktest-1.0.2.tar.gz

---
+++ packages/marktest/marktest-1.0.2.tar.gz/marktest-1.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup_args = dict(
+    name='marktest',
+    version='1.0.2',
+    description='Een test pippackage voor project',
+    license='MIT',
+    packages=find_packages(),
+    author='Mark Rozing',
+    author_email='markrozing@hotmail.nl',
+    keywords=['test'],
+)
+
+if __name__ == '__main__':
+    setup(**setup_args)
