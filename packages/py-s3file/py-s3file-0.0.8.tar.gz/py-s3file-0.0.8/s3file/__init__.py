0eb02c968bbbdb583b7d1c8a7445ab38023ed24a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:17:33 +0000

    Add package py-s3file/0.0.8/py-s3file-0.0.8.tar.gz

---
+++ packages/py-s3file/py-s3file-0.0.8.tar.gz/py-s3file-0.0.8/s3file/__init__.py
@@ -0,0 +1,8 @@
+# from .__S3File import s3_set_profile as set_profile
+from .__S3File import s3_xlist as xlist
+from .__S3File import s3_download as download
+from .__S3File import s3_upload as upload
+from .__S3File import s3_load as load
+from .__S3File import s3_save as save
+from .__S3File import s3_open as open
+from .__S3File import s3_stream as stream
