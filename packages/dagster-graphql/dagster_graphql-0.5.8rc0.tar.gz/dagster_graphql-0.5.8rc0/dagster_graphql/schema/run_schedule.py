c52a99aea2895ac13f4990c6229525fe7b0ddfc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:20 +0000

    Add package dagster-graphql/0.5.8rc0/dagster_graphql-0.5.8rc0.tar.gz

---
+++ packages/dagster-graphql/dagster_graphql-0.5.8rc0.tar.gz/dagster_graphql-0.5.8rc0/dagster_graphql/schema/run_schedule.py
@@ -0,0 +1,145 @@
+import json
+import sys
+
+from dagster_graphql import dauphin
+
+from dagster import ScheduleDefinition, check
+from dagster.core.scheduler import RunningSchedule, SystemCronScheduler
+
+
+def get_scheduler(graphene_info):
+    scheduler = graphene_info.context.scheduler
+
+    if isinstance(scheduler, SystemCronScheduler):
+        scheduler_type = SchedulerType.SystemCronScheduler
+    else:
+        raise Exception(
+            'Unknown Scheduler type {typ}. Add this Scheduler type to the SchedulerType '
+            'GraphQL Enum'.format(typ=type(scheduler))
+        )
+
+    schedules = [
+        graphene_info.schema.type_named('RunningSchedule')(graphene_info, schedule=s)
+        for s in scheduler.all_schedules()
+    ]
+
+    return graphene_info.schema.type_named('Scheduler')(
+        scheduler_type=scheduler_type, schedules=schedules
+    )
+
+
+class DauphinScheduleDefinition(dauphin.ObjectType):
+    class Meta:
+        name = 'ScheduleDefinition'
+
+    name = dauphin.NonNull(dauphin.String)
+    cron_schedule = dauphin.NonNull(dauphin.String)
+    execution_params_string = dauphin.NonNull(dauphin.String)
+
+    def __init__(self, schedule_def):
+        self._schedule_def = check.inst_param(schedule_def, 'schedule_def', ScheduleDefinition)
+
+        super(DauphinScheduleDefinition, self).__init__(
+            name=schedule_def.name,
+            cron_schedule=schedule_def.cron_schedule,
+            execution_params_string=json.dumps(schedule_def.execution_params),
+        )
+
+
+class DauphinRunningSchedule(dauphin.ObjectType):
+    class Meta:
+        name = 'RunningSchedule'
+
+    schedule_id = dauphin.NonNull(dauphin.String)
+    schedule_definition = dauphin.NonNull('ScheduleDefinition')
+    python_path = dauphin.Field(dauphin.String)
+    repository_path = dauphin.Field(dauphin.String)
+
+    def __init__(self, graphene_info, schedule):
+        self._schedule = check.inst_param(schedule, 'schedule', RunningSchedule)
+
+        super(DauphinRunningSchedule, self).__init__(
+            schedule_id=schedule.schedule_id,
+            schedule_definition=graphene_info.schema.type_named('ScheduleDefinition')(
+                schedule.schedule_definition
+            ),
+            python_path=schedule.python_path,
+            repository_path=schedule.repository_path,
+        )
+
+
+class SchedulerType(dauphin.Enum):
+    SystemCronScheduler = "SystemCronScheduler"
+
+
+class DauphinScheduler(dauphin.ObjectType):
+    class Meta:
+        name = 'Scheduler'
+
+    scheduler_type = dauphin.NonNull(dauphin.String)
+    schedules = dauphin.non_null_list('RunningSchedule')
+
+
+class DauphinRunningScheduleInput(dauphin.InputObjectType):
+    class Meta:
+        name = 'RunningScheduleInput'
+
+    name = dauphin.NonNull(dauphin.String)
+    cron_schedule = dauphin.NonNull(dauphin.String)
+    execution_params = dauphin.NonNull('ExecutionParams')
+
+
+class RunningScheduleResult(dauphin.ObjectType):
+    class Meta:
+        name = 'RunningScheduleResult'
+
+    schedule = dauphin.NonNull('RunningSchedule')
+
+
+class DauphinDeleteRunningScheduleResult(dauphin.ObjectType):
+    class Meta:
+        name = 'DeleteRunningScheduleResult'
+
+    deleted_schedule = dauphin.NonNull('RunningSchedule')
+
+
+class DauphinStartScheduleMutation(dauphin.Mutation):
+    class Meta:
+        name = 'StartScheduleMutation'
+
+    class Arguments:
+        schedule_id = dauphin.NonNull(dauphin.String)
+
+    Output = dauphin.NonNull('RunningScheduleResult')
+
+    def mutate(self, graphene_info, schedule_id):
+        scheduler = graphene_info.context.scheduler
+
+        python_path = sys.executable
+        handle = graphene_info.context.get_handle()
+        repository_path = handle.data.repository_yaml
+
+        schedule = scheduler.start_schedule(schedule_id, python_path, repository_path)
+
+        return graphene_info.schema.type_named('RunningScheduleResult')(
+            schedule=graphene_info.schema.type_named('RunningSchedule')(schedule)
+        )
+
+
+class DauphinEndRunningScheduleMutation(dauphin.Mutation):
+    class Meta:
+        name = 'EndRunningScheduleMutation'
+
+    class Arguments:
+        schedule_id = dauphin.NonNull(dauphin.String)
+
+    Output = dauphin.NonNull('RunningScheduleResult')
+
+    def mutate(self, graphene_info, schedule_id):
+        scheduler = graphene_info.context.scheduler
+
+        schedule = scheduler.end_schedule(schedule_id)
+
+        return graphene_info.schema.type_named('RunningScheduleResult')(
+            schedule=graphene_info.schema.type_named('RunningSchedule')(schedule)
+        )
---
+++ packages/dagster-graphql/dagster_graphql-0.5.8rc0.tar.gz/dagster_graphql-0.5.8rc0/dagster_graphql_tests/graphql/utils.py
@@ -0,0 +1,40 @@
+from dagster_graphql.test.utils import execute_dagster_graphql
+
+from .execution_queries import START_PIPELINE_EXECUTION_QUERY, SUBSCRIPTION_QUERY
+from .setup import define_context
+
+
+def sync_execute_get_payload(variables, raise_on_error=True, context=None):
+    if not context:
+        context = define_context(raise_on_error=raise_on_error)
+
+    result = execute_dagster_graphql(context, START_PIPELINE_EXECUTION_QUERY, variables=variables)
+
+    assert result.data
+
+    if result.data['startPipelineExecution']['__typename'] != 'StartPipelineExecutionSuccess':
+        raise Exception(result.data)
+    run_id = result.data['startPipelineExecution']['run']['runId']
+
+    subscription = execute_dagster_graphql(context, SUBSCRIPTION_QUERY, variables={'runId': run_id})
+
+    subscribe_results = []
+    subscription.subscribe(subscribe_results.append)
+
+    assert len(subscribe_results) == 1
+    subscribe_result = subscribe_results[0]
+    assert not subscribe_result.errors
+    assert subscribe_result.data
+    return subscribe_result.data
+
+
+def sync_execute_get_run_log_data(variables, raise_on_error=True, context=None):
+    payload_data = sync_execute_get_payload(
+        variables, raise_on_error=raise_on_error, context=context
+    )
+    assert payload_data['pipelineRunLogs']
+    return payload_data['pipelineRunLogs']
+
+
+def sync_execute_get_events(variables, context=None):
+    return sync_execute_get_run_log_data(variables, context=context)['messages']
