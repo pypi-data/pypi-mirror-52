29428c592cfb618ac50df4a808f2dc958b95aa76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:28 +0000

    Add package seperator/0.0.7/seperator-0.0.7.tar.gz

---
+++ packages/seperator/seperator-0.0.7.tar.gz/seperator-0.0.7/seperator/lines.py
@@ -0,0 +1,72 @@
+from colorama import Fore, Back, Style
+import shutil
+import datetime
+
+
+def line(
+    info: str = None,
+    width: int = 80,
+    char: str = "─",
+    color: str = "magenta",
+    align: int = 1,
+    margin: tuple = (1, 1),
+    *args,
+):
+
+    columns, _ = shutil.get_terminal_size()
+    sup_limit = columns
+    if width == 0 or width > columns:
+        width = sup_limit
+    if align < 0:
+        info = "" if info is None else info
+        align = (
+            width + align - len(info) - sum(margin)
+        ) % width
+    else:
+        align = align % width
+
+    if info is None:
+        color_code = getattr(Fore, color.upper())
+        return str(
+            color_code + str(char) * width + Fore.RESET
+        )[0:width]
+    sub_limit = len(info) + sum(margin) + align
+    if width < sub_limit:
+        width = sub_limit
+    else:
+        color_code = getattr(Fore, color.upper())
+        return "".join(
+            [
+                color_code,
+                char * align,
+                " " * margin[0],
+                info,
+                " " * margin[1],
+                char
+                * (
+                    width - sum(margin) - len(info) - align
+                ),
+                Fore.RESET,
+            ][0:width]
+        )
+
+
+def dateline(
+    width: int = 80,
+    char: str = "-",
+    color: str = "magenta",
+    align: int = 1,
+    margin: tuple = (1, 1),
+    *args,
+):
+    return line(
+        datetime.datetime.now().strftime(
+            "%d %b %Y %H:%M:%S"
+        ),
+        width,
+        char,
+        color,
+        align,
+        margin,
+        *args,
+    )
