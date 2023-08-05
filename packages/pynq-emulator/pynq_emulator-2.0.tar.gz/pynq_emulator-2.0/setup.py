fc3d0d8e0293caa94f890e7a5bfbb8d419914d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:59 +0000

    Add package pynq-emulator/2.0/pynq_emulator-2.0.tar.gz

---
+++ packages/pynq-emulator/pynq_emulator-2.0.tar.gz/pynq_emulator-2.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='pynq_emulator',
+    version='2.0',
+    author='Riku Salminen',
+    author_email='riku.salminen@tuni.fi',
+    description="An emulator for TUNI students' PYNQ exercises",
+	long_description = long_description,
+    packages=setuptools.find_packages(exclude=[]),
+    package_data = {},
+	keywords='pynq emulator',
+    scripts=[],
+    url="https://github.com/Riku-S/led-matrix",
+    download_url="https://github.com/Riku-S/led-matrix/archive/master.tar.gz",
+    install_requires=[],
+    zip_safe=True,
+    classifiers = ["Natural Language :: English",
+				   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
+				   ],
+)
