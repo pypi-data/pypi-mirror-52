14ae9bfa1e197495770ee9a7280315e6346480bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:06 +0000

    Add package datesy/0.6.10/datesy-0.6.10-py3-none-any.whl

---
+++ packages/datesy/datesy-0.6.10-py3-none-any.whl/datesy/inspect.py
@@ -0,0 +1,34 @@
+def find_header_line(data, header_key):
+    """
+    Find the header line in row_based data_structure
+
+    Parameters
+    ----------
+    data : rows?!
+    header_key
+        some key to find in row ? regex of "".join(header_row)?
+
+    Returns
+    -------
+
+    """
+    raise NotImplemented
+
+
+def find_key(data, key):
+    """
+    Find a key in a complex dictionary
+
+    Parameters
+    ----------
+    data : dict
+        the handling
+    key : str
+        (pattern) to be found
+
+    Returns
+    -------
+
+    """
+    # return "path in structure", "value"
+    raise NotImplemented
