733d0bc7434c1b5a71a4a7dee326e4ff37b9b593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:10 +0000

    Add package copinicoos/0.0.2/copinicoos-0.0.2-py3-none-any.whl

---
+++ packages/copinicoos/copinicoos-0.0.2-py3-none-any.whl/copinicoos/__main__.py
@@ -0,0 +1,18 @@
+import sys
+
+from .input_manager import InputManager
+from .worker_manager import WorkerManager
+from . import wget_check
+
+if not wget_check.is_wget_installed():
+    wget_check.install_wget()
+
+input_manager = InputManager()
+if len(sys.argv) == 1:
+    input_manager.interactive_input()
+else:
+    input_manager.cmd_input()
+
+worker_manager = WorkerManager.init_from_args(input_manager.return_worker_list(), input_manager.return_args())
+worker_manager.setup_workdir()
+worker_manager.run_workers()
