e83f9702a37192ed153f8c7452e5b56026b21b32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:19 +0000

    Add package dimcli/0.5.5/dimcli-0.5.5.tar.gz

---
+++ packages/dimcli/dimcli-0.5.5.tar.gz/dimcli-0.5.5/dimcli/__init__.py
@@ -0,0 +1,16 @@
+from .VERSION import __version__, VERSION
+
+from .core.api import Dsl
+from .core.dsl_grammar import G 
+
+
+try:
+    # https://stackoverflow.com/questions/32538758/nameerror-name-get-ipython-is-not-defined
+    # if run outside iPython, the magic fails so we use this as a test 
+    get_ipython()
+    ipython_env = True
+except:
+    ipython_env = False
+
+if ipython_env:
+    from .jupyter import magics
---
+++ packages/dimcli/dimcli-0.5.5.tar.gz/dimcli-0.5.5/dimcli/jupyter/magics.py
@@ -0,0 +1,266 @@
+
+from IPython.core import magic_arguments
+from IPython.core.magic import line_magic, cell_magic, line_cell_magic, Magics, magics_class
+
+from ..VERSION import VERSION
+
+from ..core.api import Dsl
+from ..core.config import *
+from ..core.utils import *
+
+
+@magics_class
+class DslMagics(Magics):
+
+    results_var = "dsl_last_results" # VAR AUTOMATICALLY SET TO LATEST QUERY RESULTS
+
+    
+    # NOTE line magics and arguments don't play well together when you need to parse a line content
+    # in that case the args parsing throws an error
+    # hence better to have a cell magic, and pass only the args in the line
+
+    @line_magic
+    @magic_arguments.magic_arguments()
+    @magic_arguments.argument('--endpoint',
+      help='The query endpoint: default is https://app.dimensions.ai'
+    )
+    @magic_arguments.argument('--user',
+      help='The account username'
+    )
+    @magic_arguments.argument('--password',
+      help='The account password'
+    )
+    @magic_arguments.argument('--env',
+      help='The instance name as defined in init.py. Default: live.'
+    )
+    def dsl_login(self, line):
+        """DimCli Magic
+        Authenticate with the Dimensions.ai DSL backend. If not args are passed, it assumed you have set up a dsl.ini file: see https://github.com/lambdamusic/dimcli#the-credentials-file).
+        
+        Alternatively one can pass auth details explicitly eg
+        >>> %dsl_login --user=me@mail.com --password=secret
+
+        """
+        args = magic_arguments.parse_argstring(self.dsl_login, line)
+        # print(args)
+        usr = args.user
+        psw = args.password
+        endpoint = args.endpoint or "https://app.dimensions.ai"
+        instance = args.env or "live"
+
+        if usr and psw:
+            self.dsl = Dsl(user=usr, password=psw, endpoint=endpoint, show_results=False, force_login=True)
+            print("DimCli %s - Succesfully connected to <%s> (method: manual login)" % (str(VERSION), self.dsl._url)) 
+        else:
+            # try to use local init file using instance parameter
+            self.dsl = Dsl(instance=instance, show_results=False, force_login=True) 
+            print("DimCli %s - Succesfully connected to <%s> (method: dsl.ini file)" % (str(VERSION), self.dsl._url))
+
+
+    @line_magic
+    def dsl(self, line):
+        """DimCli Magic - new version (unstable) featuring basic DSL autocomplete
+        """
+        if self._handle_login():
+            data = self._handle_query(line)
+            self.shell.user_ns[self.results_var] = data
+            return data
+            
+        else:
+            print("Please login first: %dsl_login")
+
+    @line_cell_magic
+    def dsl_query(self, line, cell=None):
+        """DimCli Magic
+        Query the Dimensions DSL API with the text passed. 
+        """
+        if self._handle_login():
+            if cell:
+                line = cell
+            data = self._handle_query(line)
+            self.shell.user_ns[self.results_var] = data
+            return data
+            
+        else:
+            print("Please login first: %dsl_login")
+
+    @line_cell_magic
+    def dsl_query_loop(self, line, cell=None):
+        """DimCli Magic
+        Query the Dimensions DSL API with the text passed, looping over all results pages. The final object returns contains all results into a single JSON object.  
+        """
+        if self._handle_login():
+            if cell:
+                line = cell
+            data = self._handle_query(line, loop=True)
+            self.shell.user_ns[self.results_var] = data
+            return data
+
+        else:
+            print("Please login first: %dsl_login")
+
+
+    @line_cell_magic
+    def dsl_query_as_df(self, line, cell=None):
+        """DimCli Magic
+        Query the Dimensions DSL API with the text passed - return a pandas dataframe
+        """
+        if self._handle_login():
+            if cell:
+                line = cell
+            data = self._handle_query(line).as_dataframe()
+            self.shell.user_ns[self.results_var] = data
+            return data
+            
+        else:
+            print("Please login first: %dsl_login")
+
+    @line_cell_magic
+    def dsl_query_loop_as_df(self, line, cell=None):
+        """DimCli Magic
+        Query the Dimensions DSL API with the text passed, looping over all results pages - return a pandas dataframe
+        """
+        if self._handle_login():
+            if cell:
+                line = cell
+            data = self._handle_query(line, loop=True).as_dataframe()
+            self.shell.user_ns[self.results_var] = data
+            return data
+
+        else:
+            print("Please login first: %dsl_login")
+
+    def _handle_login(self):
+        if self.dsl: 
+            return True
+        else:
+            if CONNECTION['token']:
+                self.dsl = Dsl(show_results=False)
+                return True
+        return False
+
+
+    def _handle_query(self, text, loop=False):
+        """main procedure after user input"""
+       
+        # lazy complete
+        text = line_add_lazy_return(text)
+        text = line_add_lazy_describe(text)
+
+        # RUN QUERY
+        if not loop:
+            res = self.dsl.query(text)
+        else:
+            res = self.dsl.query_iterative(text)
+            return res
+        
+        # print results
+        if "errors" in res.data.keys():
+            if "query" in res.data["errors"]:
+                print(res.data["errors"]["query"]["header"])
+                for key in res.data["errors"]["query"]["details"]:
+                    print(key)
+            else:
+                print(res.data["errors"])
+        else:
+            print_json_stats(res, text)
+            return res
+
+
+    @line_magic
+    def dsl_docs(self, line):
+        """DimCli Magic
+        Wrapper around the dsl `describe` function - outputs live documentation for a source or entity
+        """
+        if not self.dsl:
+            print("Please login first: %dsl_login")
+            return
+        try:
+            import pandas as pd
+            df = pd.DataFrame()
+        except:
+            print("Sorry this functionality requires the Pandas python library. Please install it first")
+            return
+
+        obj = line.strip()
+        if obj and obj not in G.entities() and obj not in G.sources():
+            sou = " - ".join([x for x in G.sources()])
+            ent = " - ".join([x for x in G.entities()])
+            print(f"Sorry can't recognize this object. Use one of the following: Sources=[{sou}] Entities=[{ent}] ")
+            return
+        
+        res = self._handle_query(f"describe schema") # same query for all requests (filtering done here)
+
+        if not obj:
+            docs_for = G.sources()
+            header = "sources"
+        elif obj in G.entities():
+            docs_for = [obj]
+            header = "entities"
+        elif obj in G.sources():
+            docs_for = [obj]
+            header = "sources"
+
+        d = {header: [], 'field': [], 'type': [], 'description':[], 'is_filter':[], 'is_entity': [],  'is_facet':[],}
+        for S in docs_for:
+            for x in sorted(res.json[header][S]['fields']):
+                d[header] += [S]
+                d['field'] += [x]
+                d['type'] += [res.json[header][S]['fields'][x]['type']]
+                d['description'] += [res.json[header][S]['fields'][x]['description']]
+                d['is_filter'] += [res.json[header][S]['fields'][x]['is_filter']]
+                d['is_facet'] += [res.json[header][S]['fields'][x].get('is_facet', False)]
+                d['is_entity'] += [res.json[header][S]['fields'][x].get('is_entity', False)]
+
+        return df.from_dict(d)
+
+
+
+
+
+ip = get_ipython()
+ip.register_magics(DslMagics)
+
+from ..repl.autocompletion import CleverCompleter
+from prompt_toolkit.document import Document
+
+def load_ipython_custom_completers(ipython):
+    def dslq_completers(self, event):
+        """ This should return a list of strings with possible completions.
+
+        Note that all the included strings that don't start with event.symbol
+        are removed, in order to not confuse readline.
+
+        eg Typing %%apt foo then hitting tab would yield an event like so: namespace(command='%%apt', line='%%apt foo', symbol='foo', text_until_cursor='%%apt foo')
+
+        https://stackoverflow.com/questions/36479197/ipython-custom-tab-completion-for-user-magic-function
+
+        > https://github.com/ipython/ipython/issues/11878
+        """
+        # print(dir(event), event)
+        if "%%dsl_query" in event.line:
+            # FIXME only first line gets the autocomplete!
+            doc = Document(event.line.replace("%%dsl_query", ""))
+        else:
+            doc = Document(event.line.replace("%dsl_query", ""))
+        c = CleverCompleter()
+        res = c.get_completions(doc, None)
+        # print(res)
+        return [x.text for x in res]
+
+    ipython.set_hook('complete_command', dslq_completers, re_key = '%dsl_query')
+    ipython.set_hook('complete_command', dslq_completers, re_key = '%%dsl_query')
+
+    # def test_completers(self, event):
+    #     """ 
+    #     """
+    #     return ["morning", "evening"]
+
+    # ipython.set_hook('complete_command', test_completers, str_key = 'good')
+
+load_ipython_custom_completers(ip)
+
+
+# TODO 
+# check more Completer behaviour in order to remove extra suggestions eg
+# https://www.programcreek.com/python/example/50972/IPython.get_ipython
