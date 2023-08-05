41a2144190f9141c41ac785b2e472a646d4c40b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:53 +0000

    Add package compare-versions-s528358/0.0.1.post1/compare-versions-s528358-0.0.1.post1.tar.gz

---
+++ packages/compare-versions-s528358/compare-versions-s528358-0.0.1.post1.tar.gz/compare-versions-s528358-0.0.1.post1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="compare-versions-s528358",
+    version="0.0.1.post1",
+    author="Ashish Jamuda",
+     py_modules=["compareVersions"],
+    package_dir={'':'src'},
+    author_email="ashish.turbobit@gmail.com",
+    description="script to compare versions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ashishiit/Ashish_Jamuda_test/tree/master/Problem%20B",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
---
+++ packages/compare-versions-s528358/compare-versions-s528358-0.0.1.post1.tar.gz/compare-versions-s528358-0.0.1.post1/src/compareVersions.py
@@ -0,0 +1,33 @@
+def checkCompareVersions(version1=None, version2=None):
+    
+    result =  Solution(version1, version2)
+    if result > 0:
+        return "version1 is greater than version2"
+    elif result < 0:
+        return "version1 is less than version2"
+    elif result == 0:
+        return "version1 is equal to version2"
+
+def Solution(version1, version2):
+    
+    if version1 is None and version2 is None:
+        raise ValueError("checkVersions() needs 2 arguments (0 given)")
+    elif version2 is None or version2 is None:
+        raise ValueError("checkVersions() needs 2 arguments (1 given)")
+    elif not isinstance(version1, str) or not isinstance(version2, str):
+        raise ValueError("strings must be of type str")  
+    version1 = [int(i) for i in version1.split(".")]
+    version2 = [int(i) for i in version2.split(".")]
+
+    len1 = len(version1)
+    len2 = len(version2)
+
+    if len1 < len2:
+        version1.extend([0]*(len2-len1))
+    elif len2 < len1:
+        version2.extend([0]*(len1-len2))
+
+    for i in range(len(version1)):
+        if version1[i] != version2[i]:
+            return version1[i]-version2[i]
+    return 0
