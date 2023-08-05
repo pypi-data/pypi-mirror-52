562df0c84fc68a6a81da94fff238750115ba2ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:25 +0000

    Add package granary/2.1/granary-2.1.tar.gz

---
+++ packages/granary/granary-2.1.tar.gz/granary-2.1/granary/tests/test_as2.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+"""Unit tests for as2.py.
+
+Most of the tests are in testdata/. This is just a few things that are too small
+for full testdata tests.
+"""
+from __future__ import unicode_literals
+
+from granary import appengine_config
+from oauth_dropins.webutil import testutil
+
+from granary import as2
+
+
+class ActivityStreams2Test(testutil.TestCase):
+
+  def test_from_as1_blank(self):
+    self.assertEqual({}, as2.from_as1(None))
+    self.assertEqual({}, as2.from_as1({}))
+
+  def test_to_as1_blank(self):
+    self.assertEqual({}, as2.to_as1(None))
+    self.assertEqual({}, as2.to_as1({}))
+
+  def test_bad_input_types(self):
+    for bad in 1, [2], (3,):
+      for fn in as2.to_as1, as2.from_as1:
+        with self.assertRaises(ValueError):
+          fn(bad)
+
+    with self.assertRaises(ValueError):
+      as2.from_as1('z')
