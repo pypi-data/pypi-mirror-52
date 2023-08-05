8abb775cd7327669e92b8451f6a76e8fdcf382be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:24 +0000

    Add package dohq-artifactory/0.4.113/dohq-artifactory-0.4.113.tar.gz

---
+++ packages/dohq-artifactory/dohq-artifactory-0.4.113.tar.gz/dohq-artifactory-0.4.113/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+__version__ = '0.4.113'  # identify main version of dohq-artifactory
+
+print("dohq-artifactory build version = {}".format(__version__))
+
+setup(
+    name='dohq-artifactory',
+    version=__version__,
+    py_modules=['artifactory'],
+    license='MIT License',
+    description='A Python interface to Artifactory',
+    long_description='See full documentation here: https://devopshq.github.io/artifactory/',
+    author='Alexey Burov',
+    author_email='aburov@ptsecurity.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Filesystems',
+    ],
+    url='https://devopshq.github.io/artifactory/',
+    download_url='https://github.com/devopshq/artifactory',
+    install_requires=[
+        'pathlib ; python_version<"3.4"',
+        'requests',
+        'python-dateutil'
+    ],
+    zip_safe=False,
+    package_data={'': ['README.md']},
+    packages=[
+        'dohq_artifactory',
+    ]
+)
