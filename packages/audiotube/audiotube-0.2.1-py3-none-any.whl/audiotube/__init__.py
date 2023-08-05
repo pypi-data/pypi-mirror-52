5c3a8dcb33df64e786be6e5a63d46ff85cd8f839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:55 +0000

    Add package audiotube/0.2.1/audiotube-0.2.1-py3-none-any.whl

---
+++ packages/audiotube/audiotube-0.2.1-py3-none-any.whl/audiotube/__init__.py
@@ -0,0 +1,10 @@
+from audiotube.youtube import YouTube
+from audiotube.searching import Search
+from audiotube.objects import YouTubeAudio, YouTubeAudios
+
+__all__ = [
+    'YouTube',
+    'Search',
+    'YouTubeAudio',
+    'YouTubeAudios'
+]
---
+++ packages/audiotube/audiotube-0.2.1-py3-none-any.whl/audiotube/version.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Metadata of package."""
+__version__ = '0.2.1'
+__title__ = 'audiotube'
+__description__ = "Easily can get audio and 'standard' captions from YouTube"
+__url__ = 'https://github.com/mzntaka0/audiotube'
+__author__ = 'Takao Mizuno'
+__author_email__ = 'takao.mizuno110@gmail.com'
+__license__ = 'Apache Software License'
