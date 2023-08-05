348cc953f29d77dc718694042a50031f78a95a6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:06 +0000

    Add package print-ctf/1.0.3/print_ctf-1.0.3.tar.gz

---
+++ packages/print-ctf/print_ctf-1.0.3.tar.gz/print_ctf-1.0.3/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read().replace('\\_', '_')
+
+setup(name='print_ctf',
+      version='1.0.3',
+      description='Print recent CTF contests sourced by CTFTime.org',
+      long_description=long_description,
+      author='deko2369',
+      author_email='deko2369@gmail.com',
+      url='https://github.com/deko2369',
+      packages=find_packages(),
+      install_requires=['icalendar', 'pytz', 'requests'],
+      entry_points={
+          'console_scripts': [
+              'print_ctf = print_ctf:__main',
+          ]
+      },
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Other Audience',
+          'License :: Public Domain',
+          'Topic :: Utilities',
+          'Programming Language :: Python',
+      ],
+     )
+
