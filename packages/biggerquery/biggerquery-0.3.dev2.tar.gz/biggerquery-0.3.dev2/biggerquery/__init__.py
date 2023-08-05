1500aee95057c2a775ba953382b292659da8f737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:16 +0000

    Add package biggerquery/0.3.dev2/biggerquery-0.3.dev2.tar.gz

---
+++ packages/biggerquery/biggerquery-0.3.dev2.tar.gz/biggerquery-0.3.dev2/biggerquery/__init__.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import
+
+__all__ = [
+    'create_dataset_manager',
+    'create_dataflow_manager',
+    'DatasetConfig',
+    'DataflowConfig',
+    'build_workflow_from_notebook',
+    'build_dag_zip',
+    'component',
+    'Dataset',
+    'workflow_to_dag'
+]
+
+from .dataset_manager import create_dataset_manager
+from .beam_manager import create_dataflow_manager
+
+from .configuration import DatasetConfig
+from .configuration import DataflowConfig
+
+from .deployment import build_workflow_from_notebook
+from .deployment import build_dag_zip
+from .deployment import workflow_to_dag
+
+from .interactive import interactive_component as component
+from .interactive import InteractiveDatasetManager as Dataset
+
+from .job import Job
+
+from.workflow import Workflow
---
+++ packages/biggerquery/biggerquery-0.3.dev2.tar.gz/biggerquery-0.3.dev2/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="biggerquery",
+    version="0.3.dev2",
+    author=u"Chi",
+    author_email="chibox-team@allegrogroup.com",
+    description="BigQuery client wrapper with clean API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/allegro/biggerquery",
+    packages=["biggerquery"],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "google-cloud-bigquery>=1.12.0, <=1.19.0",
+        "pandas>=0.23.0, <=0.24",
+        "google-cloud-dataflow==2.4",
+        "google-cloud-storage==1.19.0",
+    ]
+)
