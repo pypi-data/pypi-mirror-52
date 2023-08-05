89315b49d69861fcc30a1d76f5dd2407913f804b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:11 +0000

    Add package pyganalytics/0.0.35/pyganalytics-0.0.35.tar.gz

---
+++ packages/pyganalytics/pyganalytics-0.0.35.tar.gz/pyganalytics-0.0.35/README.rst
@@ -0,0 +1,14 @@
+pyganalytics
+=====
+
+A python package to easily get data from Google Analytics
+
+
+1) Installation
+----------------
+
+Open a terminal and install pyganalytics package
+
+::
+
+    pip install pyganalytics
---
+++ packages/pyganalytics/pyganalytics-0.0.35.tar.gz/pyganalytics-0.0.35/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+reqs = parse_requirements("requirements.txt", session='hack')
+reqs = [str(ir.req) for ir in reqs]
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='pyganalytics',
+    version='0.0.35',
+    description='Easily get data from Google Analytics',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='Dacker',
+    author_email='hello@dacker.co',
+    url='https://github.com/dacker-team/pyganalytics',
+    license=license,
+    keywords='get data google analytics easy',
+    packages=find_packages(exclude=('tests', 'docs')),
+    python_requires='>=3',
+    install_requires=reqs,
+
+)
