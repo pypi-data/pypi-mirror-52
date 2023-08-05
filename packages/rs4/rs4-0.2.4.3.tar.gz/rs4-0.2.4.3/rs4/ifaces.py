325969587c06a4535cab56a2f58111aaf451ea6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:07 +0000

    Add package rs4/0.2.4.3/rs4-0.2.4.3.tar.gz

---
+++ packages/rs4/rs4-0.2.4.3.tar.gz/rs4-0.2.4.3/rs4/ifaces.py
@@ -0,0 +1,27 @@
+import fcntl, socket, struct
+import subprocess
+import os
+
+def get_mac (iname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', iname [:15].encode ("utf8")))
+    return ':'.join(['%02x' % hex for hex in info[18:24]])
+
+def get_ip ():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+def get_hwid():
+    if 'nt' in os.name:
+        return subprocess.Popen('dmidecode.exe -s system-uuid'.split())
+    else:
+        return subprocess.Popen('hal-get-property --udi /org/freedesktop/Hal/devices/computer --key system.hardware.uuid'.split())
+        
+        
