4b42597955bd13dc7390f9e8d358b706b62d900b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:01 +0000

    Add package odoo10-addon-l10n-br-zip/10.0.1.0.0.99.dev12/odoo10_addon_l10n_br_zip-10.0.1.0.0.99.dev12-py2-none-any.whl

---
+++ packages/odoo10-addon-l10n-br-zip/odoo10_addon_l10n_br_zip-10.0.1.0.0.99.dev12-py2-none-any.whl/odoo/addons/l10n_br_zip/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009  Renato Lima - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Brazilian Localisation ZIP Codes',
+    'license': 'AGPL-3',
+    'author': 'Akretion, Odoo Community Association (OCA)',
+    'version': '10.0.1.0.0',
+    'depends': [
+        'l10n_br_base',
+        'sales_team',
+    ],
+    'data': [
+        'views/l10n_br_zip_view.xml',
+        'views/res_partner_view.xml',
+        'views/res_company_view.xml',
+        'views/res_bank_view.xml',
+        'wizard/l10n_br_zip_search_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'demo': [
+        'demo/l10n_br_zip_demo.xml',
+    ],
+    'category': 'Localization',
+    'installable': True,
+}
---
+++ packages/odoo10-addon-l10n-br-zip/odoo10_addon_l10n_br_zip-10.0.1.0.0.99.dev12-py2-none-any.whl/odoo/addons/l10n_br_zip/models/l10n_br_zip.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2012  Renato Lima (Akretion)                                  #
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+import re
+
+from odoo import models, fields, api
+from odoo.tools.translate import _
+from odoo.exceptions import except_orm
+from odoo.exceptions import Warning as UserError
+
+
+class L10nBrZip(models.Model):
+    """ Este objeto persiste todos os códigos postais que podem ser
+    utilizados para pesquisar e auxiliar o preenchimento dos endereços.
+    """
+    _name = 'l10n_br.zip'
+    _description = 'CEP'
+    _rec_name = 'zip_code'
+
+    zip_code = fields.Char('CEP', size=8, index=True, required=True)
+    street_type = fields.Char('Tipo', size=26)
+    zip_complement = fields.Char('Range', size=200)
+    street = fields.Char('Logradouro', size=72)
+    district = fields.Char('Bairro', size=72)
+    country_id = fields.Many2one('res.country', u'País')
+    state_id = fields.Many2one(
+        'res.country.state', 'Estado',
+        domain="[('country_id','=',country_id)]")
+    l10n_br_city_id = fields.Many2one(
+        'l10n_br_base.city', 'Cidade',
+        required=True, domain="[('state_id','=',state_id)]")
+
+    def set_domain(self, country_id=False, state_id=False,
+                   l10n_br_city_id=False, district=False,
+                   street=False, zip_code=False):
+        domain = []
+        if zip_code:
+            new_zip = re.sub('[^0-9]', '', zip_code or '')
+            domain.append(('zip_code', '=', new_zip))
+        else:
+            if not state_id or not l10n_br_city_id or \
+                    len(street or '') == 0:
+                raise except_orm(
+                    u'Parâmetros insuficientes',
+                    u'Necessário informar Estado, município e logradouro')
+
+            if country_id:
+                domain.append(('country_id', '=', country_id))
+            if state_id:
+                domain.append(('state_id', '=', state_id))
+            if l10n_br_city_id:
+                domain.append(('l10n_br_city_id', '=', l10n_br_city_id))
+            if district:
+                domain.append(('district', 'ilike', district))
+            if street:
+                domain.append(('street', 'ilike', street))
+
+        return domain
+
+    def set_result(self, zip_obj=None):
+        if zip_obj:
+            zip_code = zip_obj.zip_code
+            if len(zip_code) == 8:
+                zip_code = '%s-%s' % (zip_code[0:5], zip_code[5:8])
+            result = {
+                'country_id': zip_obj.country_id.id,
+                'state_id': zip_obj.state_id.id,
+                'l10n_br_city_id': zip_obj.l10n_br_city_id.id,
+                'district': zip_obj.district,
+                'street': ((zip_obj.street_type or '') +
+                           ' ' + (zip_obj.street or '')) if
+                zip_obj.street_type else (zip_obj.street or ''),
+                'zip': zip_code,
+            }
+        else:
+            result = {}
+        return result
+
+    def zip_search_multi(self, country_id=False,
+                         state_id=False, l10n_br_city_id=False,
+                         district=False, street=False, zip_code=False):
+        domain = self.set_domain(
+            country_id=country_id,
+            state_id=state_id,
+            l10n_br_city_id=l10n_br_city_id,
+            district=district,
+            street=street,
+            zip_code=zip_code)
+        return self.search(domain)
+
+    @api.multi
+    def zip_search(self, obj):
+
+        zip_ids = self.zip_search_multi(
+            country_id=obj.country_id.id,
+            state_id=obj.state_id.id,
+            l10n_br_city_id=obj.l10n_br_city_id.id,
+            district=obj.district,
+            street=obj.street,
+            zip_code=obj.zip,
+        )
+
+        if len(zip_ids) == 1:
+            result = self.set_result(zip_ids[0])
+            obj.write(result)
+            return True
+        else:
+            if len(zip_ids) > 1:
+                obj_zip_result = self.env['l10n_br.zip.result']
+                zip_ids = obj_zip_result.map_to_zip_result(
+                    zip_ids, obj._name, obj.id)
+
+                return self.create_wizard(
+                    obj._name,
+                    obj.id,
+                    country_id=obj.country_id.id,
+                    state_id=obj.state_id.id,
+                    l10n_br_city_id=obj.l10n_br_city_id.id,
+                    district=obj.district,
+                    street=obj.street,
+                    zip_code=obj.zip,
+                    zip_ids=[z.id for z in zip_ids]
+                )
+            else:
+                raise UserError(_('Nenhum registro encontrado'))
+
+    def create_wizard(self, object_name, address_id, country_id=False,
+                      state_id=False, l10n_br_city_id=False,
+                      district=False, street=False, zip_code=False,
+                      zip_ids=False):
+        context = dict(self.env.context)
+        context.update({
+            'zip_code': zip_code,
+            'street': street,
+            'district': district,
+            'country_id': country_id,
+            'state_id': state_id,
+            'l10n_br_city_id': l10n_br_city_id,
+            'zip_ids': zip_ids,
+            'address_id': address_id,
+            'object_name': object_name})
+
+        result = {
+            'name': 'Zip Search',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'l10n_br.zip.search',
+            'view_id': False,
+            'context': context,
+            'type': 'ir.actions.act_window',
+            'target': 'new',
+            'nodestroy': True,
+        }
+
+        return result
