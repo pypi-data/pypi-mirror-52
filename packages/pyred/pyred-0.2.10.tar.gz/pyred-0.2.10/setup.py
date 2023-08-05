65581586dd496b54aaff3b9f9c81d45767a4304c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:08 +0000

    Add package pyred/0.2.10/pyred-0.2.10.tar.gz

---
+++ packages/pyred/pyred-0.2.10.tar.gz/pyred-0.2.10/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+reqs = parse_requirements("requirements.txt", session='hack')
+reqs = [str(ir.req) for ir in reqs]
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='pyred',
+    version='0.2.10',
+    description='Easily send data to Amazon Redshift',
+    long_description=readme,
+    author='Dacker',
+    author_email='hello@dacker.co',
+    url='https://github.com/dacker-team/pyred',
+    keywords='send data amazon redshift easy',
+    packages=find_packages(exclude=('tests', 'docs')),
+    python_requires='>=3',
+    install_requires=reqs,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
