f91440a84458364340560ed902f79dfb3a40f4e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:58 +0000

    Add package zlgp/1.2.3/zlgp-1.2.3.zip

---
+++ packages/zlgp/zlgp-1.2.3.zip/zlgp-1.2.3/zlgp/__init__.py
@@ -0,0 +1,14 @@
+# from zlgp.src import add_quyu,restart_quyu
+
+# from zlgp.dst import add_quyu_greenplum,restart_quyu_greenplum 
+
+from zlgp.api import add_quyu_src,restart_quyu_src
+from zlgp.api import add_quyu_greenplum,restart_quyu_greenplum
+from zlgp.api import add_quyu_union,restart_quyu_union
+
+from zlgp.api import f_all,f_zlshenpi
+
+# add_quyu_src=add_quyu
+
+# restart_quyu_src=restart_quyu
+
