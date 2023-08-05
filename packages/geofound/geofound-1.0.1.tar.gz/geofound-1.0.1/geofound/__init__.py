b3b1686d3f17d27242de3957e13dc8e1df788dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:28 +0000

    Add package geofound/1.0.1/geofound-1.0.1.tar.gz

---
+++ packages/geofound/geofound-1.0.1.tar.gz/geofound-1.0.1/geofound/__init__.py
@@ -0,0 +1,7 @@
+from geofound.models import create_foundation, create_soil
+from geofound.stiffness import *
+from geofound.checking_tools import isclose
+from geofound.capacity import *
+from geofound.settlement import *
+from geofound.exceptions import DesignError
+from geofound import models
---
+++ packages/geofound/geofound-1.0.1.tar.gz/geofound-1.0.1/geofound/stiffness.py
@@ -0,0 +1,45 @@
+import geofound as gf
+
+
+def rotational_stiffness(sl, fd, axis="length", a0=0.0, **kwargs):
+    """
+    Rotation stiffness of foundation.
+    :param fd: Foundation object
+    :param sl: Soil Object.
+    :param axis: The axis which it should be computed around
+    :return:
+    """
+    if not kwargs.get("disable_requires", False):
+        gf.models.check_required(sl, ["g_mod", "poissons_ratio"])
+        gf.models.check_required(fd, ["length", "width", "depth"])
+
+    if fd.depth > 0.0:
+        pass
+    l = fd.length * 0.5
+    b = fd.width * 0.5
+    v = sl.poissons_ratio
+    if axis == "length":
+        i_bx = fd.i_ll
+        k_rx = 1 - 0.2 * a0
+        k_f_0 = (sl.g_mod / (1 - v) * i_bx ** 0.75 * (l / b) ** 0.25 * (2.4 + 0.5 * (b / l))) * k_rx
+    else:
+        i_by = fd.i_ww
+        k_ry = 1 - 0.3 * a0
+        k_f_0 = (sl.g_mod / (1 - v) * i_by ** 0.75 * (3 * (l / b) ** 0.15)) * k_ry
+    return k_f_0
+
+
+def shear_stiffness(f_length, f_breadth, soil_g, soil_v):
+    l = f_length * 0.5
+    b = f_breadth * 0.5
+    k_y = 2 * soil_g * l / (2 - soil_v) * (2.0 + 2.5 * (b / l) ** 0.85)
+    k_shear = (k_y - (0.2 * soil_g * l) / (0.75 - soil_v) * (1.0 - b / l))
+    return k_shear
+
+
+def dep_rotational_stiffness(f_length, f_breadth, soil_g, soil_v):
+    l = f_length * 0.5
+    b = f_breadth * 0.5
+    Iy = f_length ** 3 * f_breadth / 12
+    k_f_0 = (soil_g / (1.0 - soil_v) * Iy ** 0.75 * (3 * (l / b) ** 0.15))
+    return k_f_0
