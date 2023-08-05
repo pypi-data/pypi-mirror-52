c624cee7850e7c834c9f63d7e7b0455792afccea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:57 +0000

    Add package Mage2Gen/2.3.3/Mage2Gen-2.3.3.tar.gz

---
+++ packages/Mage2Gen/Mage2Gen-2.3.3.tar.gz/Mage2Gen-2.3.3/mage2gen/snippets/__init__.py
@@ -0,0 +1,47 @@
+# A Magento 2 module generator library
+# Copyright (C) 2016 Maikel Martens
+#
+# This file is part of Mage2Gen.
+#
+# Mage2Gen is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+from .controller import ControllerSnippet
+from .plugin import PluginSnippet
+from .system import SystemSnippet
+from .observer import ObserverSnippet
+from .install import InstallSnippet
+from .console import ConsoleSnippet
+from .shipping import ShippingSnippet
+from .language import LanguageSnippet
+from .payment import PaymentSnippet
+from .productattribute import ProductAttributeSnippet
+from .producttype import ProductTypeSnippet
+from .customerattribute import CustomerAttributeSnippet
+from .categoryattribute import CategoryAttributeSnippet
+from .cronjob import CronjobSnippet
+from .unittest import UnitTestSnippet
+from .model import ModelSnippet
+from .api import ApiSnippet
+from .widget import WidgetSnippet
+from .cache import CacheSnippet
+from .configurationtype import ConfigurationTypeSnippet
+from .block import BlockSnippet
+from .helper import HelperSnippet
+from .graphql import GraphQlSnippet
+from .preference import PreferenceSnippet
+from .viewmodel import ViewModelSnippet
+from .companyattribute import CompanyAttributeSnippet
+from .component import ComponentSnippet
+from .saleattribute import SalesAttributeSnippet
+from .eaventity import EavEntitySnippet
+from .eaventityattribute import EavEntityAttributeSnippet
