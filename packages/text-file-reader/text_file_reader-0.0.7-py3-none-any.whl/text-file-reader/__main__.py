75d3eacd28aaa83fba4813dc8a890e9075110ead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:00 +0000

    Add package text-file-reader/0.0.7/text_file_reader-0.0.7-py3-none-any.whl

---
+++ packages/text-file-reader/text_file_reader-0.0.7-py3-none-any.whl/text-file-reader/__main__.py
@@ -0,0 +1,29 @@
+import os
+from shutil import copyfile
+
+# add files to bin directory so they can be run from anywhere.
+
+username = os.getcwd().split('/')[2]
+bin_dir = f"/Users/{username}/bin"
+cwd = os.path.abspath(os.path.dirname(__file__))
+
+# create necessary dirs
+try:
+    os.mkdir(bin_dir)
+    os.mkdir(bin_dir + '/ascii-art')
+    os.mkdir(bin_dir + '/ascii-art/popups')
+except FileExistsError:
+    pass
+
+# copy files from cwd to bin
+files = ['encrypt.py', 'helpscreen', 'separate_mainloops.py',
+        'ascii-art/sign-up.txt', 'ascii-art/popups/donate1.txt',
+        'ascii-art/popups/donate2.txt', 'ascii-art/popups/twitter.txt',
+        'file-reader.py']
+cwd_paths = [cwd + '/' + file for file in files]
+bin_paths = [bin_dir + '/' + file for file in files]
+for cwd_path, bin_path in zip(cwd_paths, bin_paths):
+    copyfile(cwd_path, bin_path)
+
+with open(f'{bin_dir}/text-file-reader', 'w+') as f:
+    f.write('#!/bin/bash\npython3 ~/bin/file-reader.py $@')
