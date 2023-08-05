72b6e860139b92655f67eeffbbcc1cc024440d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:39 +0000

    Add package python-ffmpeg-video-streaming/0.0.7/python-ffmpeg-video-streaming-0.0.7.tar.gz

---
+++ packages/python-ffmpeg-video-streaming/python-ffmpeg-video-streaming-0.0.7.tar.gz/python-ffmpeg-video-streaming-0.0.7/ffmpeg_streaming/media.py
@@ -0,0 +1,66 @@
+from ffmpeg_streaming.key_info_file import generate_key_info_file
+from .process import run
+from ._ffprobe import *
+from .auto_rep import AutoRepresentation
+
+
+class Export(object):
+    def __init__(self, filename):
+        self.reps = None
+        self.filename = filename
+
+    def add_rep(self, *args):
+        self.reps = list(args)
+        return self
+
+    def auto_rep(self, heights=None, cmd='ffprobe'):
+        self.reps = AutoRepresentation(FFProbe(self.filename, cmd), heights).generate()
+        return self
+
+    def format(self, video_format):
+        self.format = video_format
+        return self
+
+    def package(
+            self,
+            path=None,
+            progress=None,
+            cmd='ffmpeg',
+            ffprobe_cmd='ffprobe',
+            capture_stdout=False,
+            capture_stderr=False,
+            input=None,
+            timeout=None
+         ):
+        if path is None:
+            self.path = self.filename
+        else:
+            self.path = path
+
+        return run(self, progress, cmd, ffprobe_cmd, capture_stdout, capture_stderr, input, timeout)
+
+
+class HLS(Export):
+
+    def __init__(self, filename, kwargs):
+        self.hls_time = kwargs.pop('hls_time', 10)
+        self.hls_allow_cache = kwargs.pop('hls_allow_cache', 0)
+        self.hls_key_info_file = kwargs.pop('hls_key_info_file', None)
+        super(HLS, self).__init__(filename)
+
+    def encryption(self, url, path, length=16):
+        self.hls_key_info_file = generate_key_info_file(url, path, length)
+        return self
+
+
+class DASH(Export):
+
+    def __init__(self, filename, kwargs):
+        self.adaption = kwargs.pop('adaption', None)
+        super(DASH, self).__init__(filename)
+
+
+__all__ = [
+    'HLS',
+    'DASH'
+]
