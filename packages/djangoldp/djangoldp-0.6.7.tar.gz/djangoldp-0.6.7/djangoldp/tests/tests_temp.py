3336b9ccf5b7b703955454e1fb4b7ea5b1c37436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:51 +0000

    Add package djangoldp/0.6.7/djangoldp-0.6.7.tar.gz

---
+++ packages/djangoldp/djangoldp-0.6.7.tar.gz/djangoldp-0.6.7/djangoldp/tests/tests_temp.py
@@ -0,0 +1,19 @@
+import json
+
+from django.contrib.auth.models import User
+from django.test import TestCase
+from rest_framework.test import APIRequestFactory, APIClient
+
+from djangoldp.tests.models import Resource, JobOffer
+
+
+class TestTemp(TestCase):
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='john', email='jlennon@beatles.com', password='glass onion')
+
+    def tearDown(self):
+        pass
+
