aaa3c66704b1eadff9f9b58c973373e74c192089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:21 +0000

    Add package w13scan/0.9.13/w13scan-0.9.13-py3-none-any.whl

---
+++ packages/w13scan/w13scan-0.9.13-py3-none-any.whl/W13SCAN/lib/plugins.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2019/6/28 11:03 PM
+# @Author  : w8ay
+# @File    : plugins.py
+import platform
+import socket
+import sys
+import traceback
+
+import requests
+import urllib3
+from requests import ConnectTimeout, HTTPError, TooManyRedirects, ConnectionError
+from urllib3.exceptions import NewConnectionError, PoolError
+
+from W13SCAN import VERSION
+from W13SCAN.lib.baseproxy import Request, Response
+from W13SCAN.lib.common import createGithubIssue, dataToStdout
+from W13SCAN.lib.data import Share, conf
+
+
+class PluginBase(object):
+
+    def __init__(self):
+        self.type = None
+        self.target = None
+        self.requests: Request = None
+        self.response: Response = None
+
+    def audit(self):
+        raise NotImplementedError
+
+    def build_url(self):
+        netloc = "http"
+        if self.requests.https:
+            netloc = "https"
+        if (netloc == "https" and int(self.requests.port) == 443) or (
+                netloc == "http" and int(self.requests.port) == 80):
+            url = "{0}://{1}{2}".format(netloc, self.requests.hostname, self.requests.path)
+        else:
+            url = "{0}://{1}:{2}{3}".format(netloc, self.requests.hostname, self.requests.port, self.requests.path)
+        return url
+
+    def execute(self, request: Request, response: Response):
+        self.target = ''
+        self.requests = request
+        self.response = response
+        output = None
+        try:
+            output = self.audit()
+        except NotImplementedError:
+            msg = 'Plugin: {0} not defined "{1} mode'.format(self.name, 'audit')
+            Share.dataToStdout(Share.dataToStdout('\r' + msg + '\n\r'))
+
+        except (ConnectTimeout, requests.exceptions.ReadTimeout, urllib3.exceptions.ReadTimeoutError, socket.timeout):
+            retry = conf["retry"]
+            while retry > 0:
+                msg = 'Plugin: {0} timeout, start it over.'.format(self.name)
+                # Share.dataToStdout('\r' + msg + '\n\r')
+                try:
+                    output = self.audit()
+                    break
+                except (
+                        ConnectTimeout, requests.exceptions.ReadTimeout, urllib3.exceptions.ReadTimeoutError,
+                        socket.timeout):
+                    # msg = 'Plugin: {0} time-out retry failed!'.format(self.name)
+                    # Share.dataToStdout('\r' + msg + '\n\r')
+                    retry -= 1
+                except:
+                    raise
+            else:
+                msg = "connect target '{0}' failed!".format(self.target)
+                # Share.dataToStdout('\r' + msg + '\n\r')
+
+        except HTTPError as e:
+            msg = 'Plugin: {0} HTTPError occurs, start it over.'.format(self.name)
+            # Share.dataToStdout('\r' + msg + '\n\r')
+
+        except ConnectionError:
+            msg = "connect target '{0}' failed!".format(self.target)
+            # Share.dataToStdout('\r' + msg + '\n\r')
+        except requests.exceptions.ChunkedEncodingError:
+            pass
+        except ConnectionResetError:
+            pass
+        except TooManyRedirects as e:
+            # Share.dataToStdout('\r' + str(e) + '\n\r')
+            pass
+        except NewConnectionError as ex:
+            pass
+
+        except PoolError as ex:
+            pass
+        except requests.exceptions.InvalidSchema:
+            pass
+        except KeyboardInterrupt:
+            raise
+        except Exception:
+            errMsg = "W13scan plugin traceback:\n"
+            errMsg += "Running version: {}\n".format(VERSION)
+            errMsg += "Python version: {}\n".format(sys.version.split()[0])
+            errMsg += "Operating system: {}\n".format(platform.platform())
+            errMsg += "Threads: {}".format(conf["threads"])
+            if request:
+                errMsg += '\n\nrequest raw:\n'
+                errMsg += request.to_data().decode()
+            excMsg = traceback.format_exc()
+            Share.lock.acquire()
+            if conf["is_debug"]:
+                dataToStdout('\r' + errMsg + '\n\r')
+            if createGithubIssue(errMsg, excMsg):
+                dataToStdout('\r' + "[x] a issue has reported" + '\n\r')
+            Share.lock.release()
+
+        return output
---
+++ packages/w13scan/w13scan-0.9.13-py3-none-any.whl/W13SCAN/plugins/PerFile/normal_page.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/11 4:51 PM
+# @Author  : w8ay
+# @File    : normal_page.py
+import re
+
+from W13SCAN.lib.const import Level
+from W13SCAN.lib.helper.phpinfo_helper import get_phpinfo
+from W13SCAN.lib.output import out
+from W13SCAN.lib.plugins import PluginBase
+
+
+class W13SCAN(PluginBase):
+    name = '通用敏感信息搜集'
+    desc = '''只从返回包中搜集通用的敏感信息'''
+    level = Level.LOW
+
+    def audit(self):
+        method = self.requests.command  # 请求方式 GET or POST
+        headers = self.requests.get_headers()  # 请求头 dict类型
+        url = self.build_url()  # 请求完整URL
+
+        resp_data = self.response.get_body_data()  # 返回数据 byte类型
+        resp_str = self.response.get_body_str()  # 返回数据 str类型 自动解码
+        resp_headers = self.response.get_headers()  # 返回头 dict类型
+
+        p = self.requests.urlparse
+        params = self.requests.params
+        netloc = self.requests.netloc
+
+        # phpinfo
+        if "<title>phpinfo()</title>" in resp_str:
+            info = get_phpinfo(resp_str)
+            out.success(url, self.name, info=info)
+
+        # 手机号
+        # regx_phone = r'(?:139|138|137|136|135|134|147|150|151|152|157|158|159|178|182|183|184|187|188|198|130|131|132|155|156|166|185|186|145|175|176|133|153|177|173|180|181|189|199|170|171)[0-9]{8}'
+        # 误报太多
+        # 身份证
+        regx_identify = r'([1-9]\d{5}[12]\d{3}(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01])\d{3}[0-9xX])'
+        for _ in [regx_identify]:
+            texts = re.findall(_, resp_str, re.M | re.I)
+            if texts:
+                for i in set(texts):
+                    if out.set(i):
+                        out.success(url, self.name, info=i)
