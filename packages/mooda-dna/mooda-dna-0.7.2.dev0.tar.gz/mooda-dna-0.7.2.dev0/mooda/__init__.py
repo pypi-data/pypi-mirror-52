963a01134c7d2b03a6b2ff4ac95295212bce9601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:49 +0000

    Add package mooda-dna/0.7.2.dev0/mooda-dna-0.7.2.dev0.tar.gz

---
+++ packages/mooda-dna/mooda-dna-0.7.2.dev0.tar.gz/mooda-dna-0.7.2.dev0/mooda/__init__.py
@@ -0,0 +1,8 @@
+# MOODA : Python package
+
+"""Top-level package for MOODA."""
+
+__author__ = """Angelo Gaeta Giovanni Stracquadanio"""
+__email__ = 'a.gaeta@sms.ed.ac.uk'
+__version__ = '0.7.2-dev'
+
