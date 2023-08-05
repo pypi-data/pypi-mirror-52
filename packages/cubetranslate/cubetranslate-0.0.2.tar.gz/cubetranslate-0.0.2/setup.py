711a0ff3e675491a5707961259da733521af6bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:38 +0000

    Add package cubetranslate/0.0.2/cubetranslate-0.0.2.tar.gz

---
+++ packages/cubetranslate/cubetranslate-0.0.2.tar.gz/cubetranslate-0.0.2/setup.py
@@ -0,0 +1,35 @@
+#!python
+# -*- coding:utf-8 -*-
+from __future__ import print_function
+from setuptools import setup, find_packages
+import cubetranslate
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="cubetranslate",
+    version='0.0.2',
+    author="xuzl71",
+    author_email="xuzlfight@qq.com",
+    description="try how to upload pip package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="http://www.renren.com/u/1f4a7323",
+    packages=find_packages(),
+    install_requires=[
+        "acumos",
+        ],
+    classifiers=[
+        "Topic :: Artistic Software",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    include_package_data=False,
+    zip_safe=True,
+)
