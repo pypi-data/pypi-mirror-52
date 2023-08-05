f5cf2c9c819c716d46d8163e5db73244eefac02e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:42 +0000

    Add package django-sso-app/0.3.0.dev3/django-sso-app-0.3.0.dev3.tar.gz

---
+++ packages/django-sso-app/django-sso-app-0.3.0.dev3.tar.gz/django-sso-app-0.3.0.dev3/src/django_sso_app/core/common.py
@@ -0,0 +1,21 @@
+ROLE_CHOICES = (
+    (-1, 'Staff'),
+    (0, 'Cityzen'),
+    (1, 'Institutional'),
+    (2, 'Professionist'),
+    (4, 'Company'),
+    (6, 'Istitutional Partition Item'),
+    (7, 'App'),
+)
+
+PROFILE_FIELDS = (
+    'email',
+    'password',
+    'role',
+    'first_name', 'last_name',
+    'description', 'picture', 'birthdate',
+    'latitude', 'longitude',
+    'country',
+    'address',
+    'language'
+)
