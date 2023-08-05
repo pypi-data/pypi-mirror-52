59f440b34e8754cfffff51883166cc1bc565541e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:52 +0000

    Add package awkward/0.12.9/awkward-0.12.9.tar.gz

---
+++ packages/awkward/awkward-0.12.9.tar.gz/awkward-0.12.9/tests/test_issues.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-array/blob/master/LICENSE
+
+import unittest
+
+import numpy
+
+from awkward import *
+from awkward.type import *
+
+class Test(unittest.TestCase):
+    def runTest(self):
+        pass
+
+    def test_issue49(self):
+        a = JaggedArray([2], [5], [1, 2, 3, 4, 5])
+        m = JaggedArray([0], [3], [False, False, True])
+        assert a[m].tolist() == [[5]]
+
+    def test_issue144(self):
+        x = fromiter([[], [0.5], [0.6], []])
+        isloose = fromiter([[], [False], [True], []])
+        assert x[isloose].tolist() == [[], [], [0.6], []]
+        assert x.sum().tolist() == [0.0, 0.5, 0.6, 0.0]
+
+    def test_issue163(self):
+        a = fromiter([[1, 3], [4, 5]])
+        b = a[a.counts > 10]
+        assert b[:,:1].tolist() == []
