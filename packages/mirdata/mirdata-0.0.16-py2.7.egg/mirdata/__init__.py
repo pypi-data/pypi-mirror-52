ae3510bc3f273ea13eee7d9db6ecebd3701053b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:21 +0000

    Add package mirdata/0.0.16/mirdata-0.0.16-py2.7.egg

---
+++ packages/mirdata/mirdata-0.0.16-py2.7.egg/mirdata/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from . import beatles
+from . import ikala
+from . import medleydb_melody
+from . import medleydb_pitch
+from . import orchset
+from . import rwc_classical
+from . import rwc_genre
+from . import rwc_jazz
+from . import rwc_popular
+from . import salami
+from . import guitarset
