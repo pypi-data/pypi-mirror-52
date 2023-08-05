1bdb333421e447dcd2e91a7c2e115504f5c55aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:22 +0000

    Add package pandalone/0.2.8/pandalone-0.2.8.tar.gz

---
+++ packages/pandalone/pandalone-0.2.8.tar.gz/pandalone-0.2.8/pandalone/__init__.py
@@ -0,0 +1,8 @@
+from ._version import (__version__, __updated__)
+
+__copyright__     = "Copyright (C) 2013-2015 European Commission (JRC)"
+__license__       = "EUPL 1.1+"
+
+__title__ = "pandalone"
+__summary__ = "Collection of utilities for working with hierarchical data with relocatable paths."
+__uri__ = "https://github.com/pandalone/pandalone"
