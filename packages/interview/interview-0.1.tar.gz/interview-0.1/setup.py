423e612f61bdc1309e73e8993d26102b8265cf57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:53 +0000

    Add package interview/0.1/interview-0.1.tar.gz

---
+++ packages/interview/interview-0.1.tar.gz/interview-0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+import os
+
+setup(
+    name="interview",
+    version="0.1",
+    author="chenminhua",
+    author_email="chenmhgo@gmail.com",
+    license="GPL3",
+    description="make interview easier",
+    url="https://github.com/chenminhua/interview",
+    packages=[
+
+    ],
+    scripts=['bin/interview']
+)
