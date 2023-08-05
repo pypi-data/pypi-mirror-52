b7eb827c7b2cee68905e187a87e51a09ec3418fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:47 +0000

    Add package malaya/2.7.2.0/malaya-2.7.2.0.tar.gz

---
+++ packages/malaya/malaya-2.7.2.0.tar.gz/malaya-2.7.2.0/malaya/emotion.py
@@ -0,0 +1,140 @@
+from ._utils import _softmax_class
+from ._utils._paths import PATH_EMOTION, S3_PATH_EMOTION
+
+_emotion_label = ['anger', 'fear', 'joy', 'love', 'sadness', 'surprise']
+
+
+def available_deep_model():
+    """
+    List available deep learning emotion analysis models.
+    """
+    return ['self-attention', 'bahdanau', 'luong']
+
+
+def available_bert_model():
+    """
+    List available bert emotion analysis models.
+    """
+    return ['multilanguage', 'base', 'small']
+
+
+def deep_model(model = 'luong', validate = True):
+    """
+    Load deep learning emotion analysis model.
+
+    Parameters
+    ----------
+    model : str, optional (default='luong')
+        Model architecture supported. Allowed values:
+
+        * ``'self-attention'`` - Fast-text architecture, embedded and logits layers only with self attention.
+        * ``'bahdanau'`` - LSTM with bahdanau attention architecture.
+        * ``'luong'`` - LSTM with luong attention architecture.
+    validate: bool, optional (default=True)
+        if True, malaya will check model availability and download if not available.
+
+    Returns
+    -------
+    SOFTMAX: malaya._models._tensorflow_model.SOFTMAX class
+    """
+    if not isinstance(model, str):
+        raise ValueError('model must be a string')
+    if not isinstance(validate, bool):
+        raise ValueError('validate must be a boolean')
+    model = model.lower()
+    if model not in available_deep_model():
+        raise Exception(
+            'model is not supported, please check supported models from malaya.sentiment.available_deep_model()'
+        )
+
+    return _softmax_class.deep_model(
+        PATH_EMOTION,
+        S3_PATH_EMOTION,
+        'emotion',
+        _emotion_label,
+        model = model,
+        validate = validate,
+    )
+
+
+def multinomial(validate = True):
+    """
+    Load multinomial emotion model.
+
+    Parameters
+    ----------
+    validate: bool, optional (default=True)
+        if True, malaya will check model availability and download if not available.
+
+    Returns
+    -------
+    BAYES : malaya._models._sklearn_model.BAYES class
+    """
+    return _softmax_class.multinomial(
+        PATH_EMOTION,
+        S3_PATH_EMOTION,
+        'emotion',
+        _emotion_label,
+        validate = validate,
+    )
+
+
+def xgb(validate = True):
+    """
+    Load XGB emotion model.
+
+    Parameters
+    ----------
+    validate: bool, optional (default=True)
+        if True, malaya will check model availability and download if not available.
+
+    Returns
+    -------
+    XGB : malaya._models._sklearn_model.XGB class
+    """
+    return _softmax_class.xgb(
+        PATH_EMOTION,
+        S3_PATH_EMOTION,
+        'emotion',
+        _emotion_label,
+        validate = validate,
+    )
+
+
+def bert(model = 'base', validate = True):
+    """
+    Load BERT emotion model.
+
+    Parameters
+    ----------
+    model : str, optional (default='base')
+        Model architecture supported. Allowed values:
+
+        * ``'multilanguage'`` - bert multilanguage released by Google, trained on emotion analysis.
+        * ``'base'`` - base bert-bahasa released by Malaya, trained on emotion analysis.
+        * ``'small'`` - small bert-bahasa released by Malaya, trained on emotion analysis.
+    validate: bool, optional (default=True)
+        if True, malaya will check model availability and download if not available.
+
+    Returns
+    -------
+    MULTICLASS_BERT : malaya._models._bert_model.MULTICLASS_BERT class
+    """
+    if not isinstance(model, str):
+        raise ValueError('model must be a string')
+    if not isinstance(validate, bool):
+        raise ValueError('validate must be a boolean')
+
+    model = model.lower()
+    if model not in available_bert_model():
+        raise Exception(
+            'model is not supported, please check supported models from malaya.emotion.available_bert_model()'
+        )
+    return _softmax_class.bert(
+        PATH_EMOTION,
+        S3_PATH_EMOTION,
+        'emotion',
+        _emotion_label,
+        model = model,
+        validate = validate,
+    )
