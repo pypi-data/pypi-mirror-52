3af4559da810497c56ece43e3589103b6591815a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:01 +0000

    Add package zhlite/1.8.1/zhlite-1.8.1.tar.gz

---
+++ packages/zhlite/zhlite-1.8.1.tar.gz/zhlite-1.8.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: UTF-8 -*-
+import setuptools
+from zhlite import __version__, __author__, __email__
+
+with open("README.md", "r", encoding='utf8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="zhlite",
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
+    description="zhlite",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/deplives/zhlite",
+    packages=['zhlite'],
+    install_requires=[
+        'requests==2.21.0',
+        'beautifulsoup4==4.7.1',
+        'lxml==4.3.4',
+        'PyExecJS==1.5.1',
+        'Pillow==6.0.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+    ]
+
+)
