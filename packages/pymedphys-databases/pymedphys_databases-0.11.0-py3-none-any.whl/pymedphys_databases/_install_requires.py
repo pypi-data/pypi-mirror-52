071ba1c491ceffd786eb4a44ffd98897d353156f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:20 +0000

    Add package pymedphys-databases/0.11.0/pymedphys_databases-0.11.0-py3-none-any.whl

---
+++ packages/pymedphys-databases/pymedphys_databases-0.11.0-py3-none-any.whl/pymedphys_databases/_install_requires.py
@@ -0,0 +1,11 @@
+install_requires = [
+    "attrs",
+    "keyring",
+    "numpy >= 1.12",
+    "pandas",
+    "pymedphys_base >= 0.11.0, < 0.12.0",
+    "pymedphys_fileformats >= 0.11.0, < 0.12.0",
+    "pymedphys_mudensity >= 0.11.0, < 0.12.0",
+    "pymedphys_utilities >= 0.11.0, < 0.12.0",
+    "pymssql",
+]
