7f4160f7a9d4cf93527e7892d8a1d7b0243b4b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:24 +0000

    Add package LocToWeatherAPI/0.0.3/LocToWeatherAPI-0.0.3.tar.gz

---
+++ packages/LocToWeatherAPI/LocToWeatherAPI-0.0.3.tar.gz/LocToWeatherAPI-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="LocToWeatherAPI",
+    version="0.0.3",
+    author="Kinjal Das",
+    author_email="kinjal.das86@gmail.com",
+    description="Package for Generating weather data from place name and/or date",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KinjalDas/Location_To_Climate_API",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
