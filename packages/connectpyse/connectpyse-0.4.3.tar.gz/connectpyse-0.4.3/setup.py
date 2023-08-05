05fd1465e99e3df05051d26ab1ec0e22c6897657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:32 +0000

    Add package connectpyse/0.4.3/connectpyse-0.4.3.tar.gz

---
+++ packages/connectpyse/connectpyse-0.4.3.tar.gz/connectpyse-0.4.3/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        long_description = f.read()
+        #return f.read()
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setup(name='connectpyse',
+      version='0.4.3',
+      description='A ConnectWise API tool for the rest of us.',
+      #long_description=readme(),
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.5'
+      ],
+      keywords='connectwise rest api',
+      url='https://github.com/markciecior/ConnectPyse',
+      author='Joshua M Smith (original), Mark Ciecior (forked)',
+      author_email='saether@gmail.com (original), mark@markciecior.com (forked)',
+      license='MIT',
+      packages=['.',
+                'connectpyse.expense',
+                'connectpyse.finance',
+                'connectpyse.marketing',
+                'connectpyse.procurement',
+                'connectpyse.project',
+                'connectpyse.sales',
+                'connectpyse.schedule',
+                'connectpyse.service',
+                'connectpyse.system',
+                'connectpyse.time'],
+      py_modules = ['connectpyse.cw_controller',
+                    'connectpyse.config',
+                    'connectpyse.cw_model',
+                    'connectpyse.restapi'],
+      install_requires=[
+          'requests'
+      ],
+      include_package_data=True,
+      zip_safe=False)
