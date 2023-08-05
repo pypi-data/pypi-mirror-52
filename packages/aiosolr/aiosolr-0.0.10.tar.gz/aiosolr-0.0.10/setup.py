f3f4e0687d692464783f1a7dbee5e36eebb7b1de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:14 +0000

    Add package aiosolr/0.0.10/aiosolr-0.0.10.tar.gz

---
+++ packages/aiosolr/aiosolr-0.0.10.tar.gz/aiosolr-0.0.10/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+__version__ = '0.0.10'
+
+
+setup(
+    name="aiosolr",
+    version=__version__,
+    description="Lightweight Python client for Apache Solr",
+
+    author="Brad Belyeu",
+    author_email="bradley.belyeu@life.church",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+        "Programming Language :: Python :: 3",
+    ],
+    download_url=f"https://github.com/bbelyeu/aiosolr/archive/{__version__}.zip",
+    install_requires=["aiohttp"],
+    keywords=["solr", "asyncio", "aiohttp", "search"],
+    license="MIT",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    platforms="any",
+    py_modules=["aiosolr"],
+    test_suite='tests',
+    url="https://github.com/bbelyeu/aiosolr/",
+)
