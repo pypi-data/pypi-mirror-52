08fc07523a48741d7acc77f0d370f62352f2261c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:51 +0000

    Add package pyrolite/0.1.20/pyrolite-0.1.20.tar.gz

---
+++ packages/pyrolite/pyrolite-0.1.20.tar.gz/pyrolite-0.1.20/pyrolite/comp/__init__.py
@@ -0,0 +1,12 @@
+"""
+Submodule for working with compositional data.
+"""
+# from .aggregate import *
+# from .impute import *
+# from .codata import *
+# from .renorm import *
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+logger = logging.getLogger(__name__)
