4d7f7fe298d508287d1da37df652d4fc46952b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:53 +0000

    Add package chickadee/20190907.3/chickadee-20190907.3-py3-none-any.whl

---
+++ packages/chickadee/chickadee-20190907.3-py3-none-any.whl/libchickadee/chickadee.py
@@ -0,0 +1,117 @@
+"""Chickadee
+
+An application to provide context for an IP address
+
+"""
+
+import argparse
+import os
+import json
+import pprint
+import time
+import sys
+
+from libchickadee.backends.ipapi import Resolver
+from libchickadee.parsers.plain_text import PlainTextParser
+
+__author__ = 'Chapin Bryce'
+__date__ = 20190907
+__license__ = 'GPLv3 Copyright 2019 Chapin Bryce'
+__desc__ = '''Yet another GeoIP resolution tool.'''
+
+FIELDS = ','.join([ # Ordered list of fields to gather
+            'query',
+            'as', 'org', 'isp',
+            'continent', 'country', 'regionName', 'city', 'district', 'zip',
+            'mobile', 'proxy', 'reverse',
+            'lat', 'lon', 'timezone',
+            'status', 'message'
+        ])
+
+def str_handler(input_data, fields=None):
+    if isinstance(input_data, str) and ',' in input_data:
+        input_data = input_data.split(',')
+
+    resolver = Resolver()
+    if fields:
+        resolver.fields = fields
+    if len(input_data) > resolver.bulk_limit * resolver.ratelimit:
+        print("[!] Warning: due to rate limiting, this resolution will take "
+              "at least {} minutes. Consider purchasing an API key for "
+              "increased query performance".format(
+                  len(input_data)/resolver.bulk_limit/resolver.ratelimit))
+    results = resolver.query(input_data)
+    all_results = [x for x in results]
+    return all_results
+
+def file_handler(input_data, fields):
+    print("Extracting IPs from files")
+    ptparser = PlainTextParser()
+    ptparser.parse_file(input_data)
+    print("{} IPs discovered, resolving...".format(len(ptparser.ips)))
+    return str_handler(list(ptparser.ips), fields)
+
+
+def dir_handler(input_data, fields):
+    ptparser = PlainTextParser()
+    for root, _, files in os.walk(input_data):
+        for fentry in files:
+            ptparser.parse_file(os.path.join(root, fentry))
+    return str_handler(list(ptparser.ips), fields)
+
+
+def main(input_data, outformat='json', outfile=None, fields=FIELDS):
+    """Evaluate the input data format and properly parse to extract and resolve
+    IP addresses.
+
+    Args:
+        input_data (str or file_obj): User provided data containing IPs to
+            resolve
+    """
+
+    if os.path.isdir(input_data):
+        results = dir_handler(input_data, fields) # Directory handler
+    elif os.path.isfile(input_data):
+        results = file_handler(input_data, fields) # File handler
+    elif isinstance(input_data, str):
+        results = str_handler(input_data, fields) # String handler
+
+    if outformat == 'csv':
+        Resolver.write_csv(outfile, results, fields)
+    elif outformat == 'json':
+        Resolver.write_json(outfile, results)
+    elif outformat == 'jsonl':
+        Resolver.write_json(outfile, results, lines=True)
+
+def arg_handling():
+    parser = argparse.ArgumentParser(
+        description='Sample Argparse',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        epilog="Built by {}, v.{}".format(__author__, __date__)
+    )
+    parser.add_argument(
+        'data',
+        help="Either an IP address, comma delimited list of IP addresses, "
+             "or path to a file or folder containing files to check for "
+             "IP address values. Currently supported file types: "
+             "plain text (ie logs, csv, json), gzipped plain text"
+    )
+    parser.add_argument('-f', help='Comma separated fields to query',
+        default=FIELDS)
+    parser.add_argument('-t', help='Output format',
+                        choices=['json', 'jsonl', 'csv'],
+                        default='jsonl')
+    parser.add_argument('-w', help='Path to file to write output',
+                        default=sys.stdout, metavar='FILENAME.JSON')
+    args = parser.parse_args()
+    return args
+
+def entry(args=None):
+    """Entrypoint for package script"""
+    args = arg_handling()
+    fields = args.f.split(',')
+    main(args.data, outformat=args.t, outfile=args.w, fields=fields)
+
+
+if __name__ == "__main__":
+    entry()
