4c25e844678d4f24f40ae773ac0fd52f89ffb1fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:33 +0000

    Add package hydroeval/0.0.3/hydroeval-0.0.3-py2-none-any.whl

---
+++ packages/hydroeval/hydroeval-0.0.3-py2-none-any.whl/hydroeval/version.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of HydroEval: An Evaluator for Stream Flow Time Series
+# Copyright (C) 2019  Thibault Hallouin (1)
+#
+# (1) Dooge Centre for Water Resources Research, University College Dublin, Ireland
+#
+# HydroEval is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# HydroEval is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with HydroEval. If not, see <http://www.gnu.org/licenses/>.
+
+
+__version__ = '0.0.3'
