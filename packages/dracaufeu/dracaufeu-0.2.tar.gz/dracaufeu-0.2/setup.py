c8d16405bb0b8bb3c8f805269b3d3103fb2326fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:55 +0000

    Add package dracaufeu/0.2/dracaufeu-0.2.tar.gz

---
+++ packages/dracaufeu/dracaufeu-0.2.tar.gz/dracaufeu-0.2/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+  name = 'dracaufeu',
+  packages = ['dracaufeu'],
+  version = '0.2',
+  license='MIT',
+  description = 'Pytorch extension package for personal use',
+  author = 'Etienne Meunier',
+  author_email = 'etiennemeunier@live.fr',
+  url = 'https://github.com/Etienne-Meunier/dracaufeu',
+  download_url = 'https://github.com/Etienne-Meunier/dracaufeu/archive/v_02.tar.gz',
+  keywords = ['Pytorch', 'DeepLearning'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
