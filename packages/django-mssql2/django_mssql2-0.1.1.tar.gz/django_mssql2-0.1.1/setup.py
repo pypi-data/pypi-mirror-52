e5fb2e0e7c608078aadf70541a2847529655c8b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:57 +0000

    Add package django-mssql2/0.1.1/django_mssql2-0.1.1.tar.gz

---
+++ packages/django-mssql2/django_mssql2-0.1.1.tar.gz/django_mssql2-0.1.1/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['django_mssql2', 'django_mssql2.pyodbc']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Cython>=0.29.13,<0.30.0',
+ 'django>=2.2,<3.0',
+ 'pymssql>=2.1,<3.0',
+ 'pyodbc>=4.0,<5.0']
+
+setup_kwargs = {
+    'name': 'django-mssql2',
+    'version': '0.1.1',
+    'description': 'Django database adapter for MS SQL Server',
+    'long_description': None,
+    'author': 'Тряпчев Данил Павлович',
+    'author_email': 'tryapchev@tochka.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
