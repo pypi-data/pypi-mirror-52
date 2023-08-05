9356d8ee8e9a6a4e96f5948fa8802ba078c751d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:23 +0000

    Add package avalon-core/5.4.53/avalon-core-5.4.53.tar.gz

---
+++ packages/avalon-core/avalon-core-5.4.53.tar.gz/avalon-core-5.4.53/avalon/maya/__init__.py
@@ -0,0 +1,95 @@
+"""Public API
+
+Anything that isn't defined here is INTERNAL and unreliable for external use.
+
+"""
+
+from .pipeline import (
+    install,
+    uninstall,
+
+    Creator,
+    Loader,
+
+    ls,
+    load,               # deprecated (old load api)
+    create,             # deprecated (old creator api)
+    remove,             # deprecated (old load api)
+    update,             # deprecated (old load api)
+    publish,
+    containerise,
+
+    lock,
+    unlock,
+    is_locked,
+    lock_ignored,
+
+)
+
+from .workio import (
+    open,
+    save,
+    current_file,
+    has_unsaved_changes,
+    file_extensions,
+    work_root
+)
+
+from .lib import (
+    export_alembic,
+    lsattr,
+    lsattrs,
+    read,
+
+    apply_shaders,
+    without_extension,
+    maintained_selection,
+    suspended_refresh,
+
+    unique_name,
+    unique_namespace,
+)
+
+
+__all__ = [
+    "install",
+    "uninstall",
+
+    "Creator",
+    "Loader",
+
+    "ls",
+    "load",
+    "create",
+    "remove",
+    "update",
+    "publish",
+
+    "lock",
+    "unlock",
+    "is_locked",
+    "lock_ignored",
+
+    # Workfiles API
+    "open",
+    "save",
+    "current_file",
+    "has_unsaved_changes",
+    "file_extensions",
+    "work_root",
+
+    # Utility functions
+    "export_alembic",
+    "lsattr",
+    "lsattrs",
+    "read",
+
+    "unique_name",
+    "unique_namespace",
+
+    "apply_shaders",
+    "without_extension",
+    "maintained_selection",
+    "suspended_refresh",
+
+]
