5135cb2a70b71035e2fc2ba9dd6cac6be3eb1379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:34 +0000

    Add package yogit/1.10.0/yogit-1.10.0.tar.gz

---
+++ packages/yogit/yogit-1.10.0.tar.gz/yogit-1.10.0/yogit/yogit/review.py
@@ -0,0 +1,45 @@
+"""
+Subcommand `rv`
+"""
+
+import click
+
+from yogit.api.queries import ReviewListQuery, ReviewRequestedQuery
+from yogit.yogit.checks import account_required, check_update
+
+
+@click.group("review")
+def review():
+    """
+    Review actions
+    """
+
+
+@click.command("list", help="List your reviews on opened pull requests")
+@click.pass_context
+@account_required
+@check_update
+def review_list(ctx):  # pylint: disable=unused-argument
+    """
+    List your reviews on opened pull requests
+    """
+    query = ReviewListQuery()
+    query.execute()  # pylint: disable=no-value-for-parameter
+    query.print()
+
+
+@click.command("requested", help="List pull requests where your review is requested")
+@click.pass_context
+@account_required
+@check_update
+def review_requested_list(ctx):  # pylint: disable=unused-argument
+    """
+    List pull requests where your review is requested
+    """
+    query = ReviewRequestedQuery()
+    query.execute()  # pylint: disable=no-value-for-parameter
+    query.print()
+
+
+review.add_command(review_list)
+review.add_command(review_requested_list)
