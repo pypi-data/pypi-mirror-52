398b89d1d3c2f4b32988640226b27b08002ea321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:11 +0000

    Add package omega-coding/1.0.1/omega_coding-1.0.1.tar.gz

---
+++ packages/omega-coding/omega_coding-1.0.1.tar.gz/omega_coding-1.0.1/omega_coding/__init__.py
@@ -0,0 +1,3 @@
+from .omega_coding import omega_coding, decode_omega_coding
+
+__all__ = ["omega_coding", "decode_omega_coding"]
---
+++ packages/omega-coding/omega_coding-1.0.1.tar.gz/omega_coding-1.0.1/omega_coding/omega_coding.py
@@ -0,0 +1,22 @@
+from math import log2, ceil
+
+def omega_coding(n:int)->str:
+    """Return string representing given number in omega coding.
+        n:int, number to convert to omega coding.
+    """
+    code = "0"
+    while n>1:
+        code = bin(n)[2:] + code
+        n = ceil(log2(n+1))-1
+    return code
+
+def decode_omega_coding(code:str)->int:
+    """Return integer represented by given string in omega coding.
+        code:str, strong to decode to integer.
+    """
+    n, c = 1, 0
+    while code[c] != "0":
+        new_n = int(code[c:c+1+n], 2)
+        c += n + 1
+        n = new_n
+    return n     
