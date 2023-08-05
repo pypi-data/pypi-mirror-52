130fda76bf083ace299bbb8413a8050ac9bca961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:29 +0000

    Add package logstash-python-formatter/0.1.2/logstash_python_formatter-0.1.2-py3-none-any.whl

---
+++ packages/logstash-python-formatter/logstash_python_formatter-0.1.2-py3-none-any.whl/logstash_formatter/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from logstash_formatter.formatter import LogstashFormatter
+
+__version__ = '0.1.2'
+__author__ = 'Miguel Barrientos Fernández'
+__license__ = 'MIT'
+__url__ = 'https://github.com/mbarrientos/logstash-python-formatter'
+__description__ = "Python formatter for working with Logstash json filters."
