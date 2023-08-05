c99375ac81193e2d6c2dad4ede0f216c5dece94c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:27 +0000

    Add package bionic/0.4.2/bionic-0.4.2-py2-none-any.whl

---
+++ packages/bionic/bionic-0.4.2-py2-none-any.whl/bionic/exception.py
@@ -0,0 +1,19 @@
+'''
+Bionic-specific exception classes.
+'''
+
+
+class UndefinedEntityError(KeyError):
+    @classmethod
+    def for_name(cls, name):
+        return cls("Entity %r is not defined" % name)
+
+
+class AlreadyDefinedEntityError(ValueError):
+    @classmethod
+    def for_name(cls, name):
+        return cls("Entity %r is already defined" % name)
+
+
+class IncompatibleEntityError(ValueError):
+    pass
