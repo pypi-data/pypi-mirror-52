afd2fa10a1117ca75054cd1b945152346d993519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:04 +0000

    Add package aws-cloudwatch-log-minder/0.2.1/aws-cloudwatch-log-minder-0.2.1.tar.gz

---
+++ packages/aws-cloudwatch-log-minder/aws-cloudwatch-log-minder-0.2.1.tar.gz/aws-cloudwatch-log-minder-0.2.1/src/aws_cloudwatch_log_minder/__main__.py
@@ -0,0 +1,28 @@
+import os
+import logging
+import click
+from .delete_empty_log_streams import delete_empty_log_streams
+from .set_log_retention import set_log_retention
+
+@click.group()
+@click.pass_context
+@click.option('--dry-run', is_flag=True, default=False,
+              help='do not change anything, just show what is going to happen')
+def main(ctx, dry_run):
+    ctx.obj = ctx.params
+
+@main.command(name='set-log-retention')
+@click.pass_context
+@click.option('--days', type=int, required=False, default=30)
+def set_log_retention_command(ctx, days):
+    set_log_retention(days, ctx.obj['dry_run'])
+
+@main.command(name='delete-empty-log-streams')
+@click.pass_context
+@click.option('--log-group-name-prefix', type=str, required=False)
+def delete_empty_log_streams_command(ctx, log_group_name_prefix):
+    delete_empty_log_streams(ctx.obj['dry_run'], log_group_name_prefix)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=os.getenv("LOG_LEVEL", "INFO"))
+    main()
