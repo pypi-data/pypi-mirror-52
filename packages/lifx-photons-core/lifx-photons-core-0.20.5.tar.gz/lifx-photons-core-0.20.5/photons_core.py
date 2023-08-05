f90f9206d4c739c362c7fa499bf93ae3007a3057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:49 +0000

    Add package lifx-photons-core/0.20.5/lifx-photons-core-0.20.5.tar.gz

---
+++ packages/lifx-photons-core/lifx-photons-core-0.20.5.tar.gz/lifx-photons-core-0.20.5/photons_core.py
@@ -0,0 +1,22 @@
+"""
+Requiring this module will in turn require all the lifx-photons-core modules
+"""
+from option_merge_addons import option_merge_addon_hook
+
+__shortdesc__ = "Requiring this module will in turn require all lifx-photons-core modules"
+
+wanted = [
+    "protocol",
+    "transport",
+    "control",
+    "messages",
+    "colour",
+    "themes",
+    "device_finder",
+    "products_registry",
+]
+
+
+@option_merge_addon_hook(extras=[("lifx.photons", comp) for comp in wanted])
+def __lifx__(collector, *args, **kwargs):
+    pass
---
+++ packages/lifx-photons-core/lifx-photons-core-0.20.5.tar.gz/lifx-photons-core-0.20.5/photons_tile_paint/pacman/options.py
@@ -0,0 +1,14 @@
+from photons_tile_paint.options import AnimationOptions
+
+from input_algorithms.dictobj import dictobj
+from input_algorithms import spec_base as sb
+
+
+class TilePacmanOptions(AnimationOptions):
+    user_coords = dictobj.Field(sb.boolean, default=False)
+    num_iterations = dictobj.Field(sb.integer_spec, default=-1)
+
+    def final_iteration(self, iteration):
+        if self.num_iterations == -1:
+            return False
+        return self.num_iterations <= iteration
