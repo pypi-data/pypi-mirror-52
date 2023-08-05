4545941f16844d96a33ed090a50267e35865cf6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:30 +0000

    Add package myelin/0.0.18/myelin-0.0.18-py2-none-any.whl

---
+++ packages/myelin/myelin-0.0.18-py2-none-any.whl/myelin/admin/__init__.py
@@ -0,0 +1,20 @@
+from myelin.admin.myelin_admin import MyelinAdmin
+import os
+
+__ADMIN_CLIENT__ = MyelinAdmin()
+
+
+def data_path(default_value=None):
+    return os.environ.get('DATA_PATH') or default_value
+
+
+def model_path(default_value=None):
+    return os.environ.get('MODEL_PATH') or default_value
+
+
+def task(axon, task_name, namespace, default_value=None):
+    return __ADMIN_CLIENT__.task(axon, task_name, namespace, default_value)
+
+
+def model_backend(axon, model_graph, model, model_backend, namespace, default_value=None):
+    return __ADMIN_CLIENT__.model_backend(axon, model_graph, model, model_backend, namespace, default_value)
