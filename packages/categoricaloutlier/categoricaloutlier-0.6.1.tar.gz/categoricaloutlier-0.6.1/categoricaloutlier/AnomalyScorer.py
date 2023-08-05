026dce931281d74d1fc2f3bd945998bdd91860e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:42 +0000

    Add package categoricaloutlier/0.6.1/categoricaloutlier-0.6.1.tar.gz

---
+++ packages/categoricaloutlier/categoricaloutlier-0.6.1.tar.gz/categoricaloutlier-0.6.1/categoricaloutlier/AnomalyScorer.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+import scipy as sp
+import random
+from scipy.spatial.distance import mahalanobis
+
+class PredictOutlier:
+    anomalydetectorobject = None
+    testdata = None
+    scores = None
+    
+    def predict_anomaly_detector(self):
+        cols_freq = []
+        if(self.anomalydetectorobject.datetimecols != None):
+            self.get_datetimefeatures()
+            
+        for c in self.anomalydetectorobject.cols:
+            
+            d = self.anomalydetectorobject.valuecountsdict[c]
+            
+            self.testdata = pd.merge(self.testdata,d,how='left',on=[c])
+            new_prob_value = (1/1)/(1 + self.anomalydetectorobject.colsum.loc[self.anomalydetectorobject.colsum['index'] == c+'_frequency','sum'].values[0])
+            
+            self.testdata[c+'_frequency'] = self.testdata[c+'_frequency'].fillna(new_prob_value)
+            cols_freq.append(c+'_frequency')
+
+        test_distance = self.testdata[cols_freq].apply(lambda x: (mahalanobis(self.anomalydetectorobject.median['median'].values, x, self.anomalydetectorobject.invcovmx)), axis=1)
+
+        self.scores = self.sigmoid_curve(test_distance,self.anomalydetectorobject.threshold)
+
+        return self.scores
+    
+    def sigmoid_curve(self,test_dist, k = 1):
+        score = []
+        for t in test_dist:
+            v = 1.0/(1.0 + np.exp(-k*(np.log10(t) - np.log10(self.anomalydetectorobject.threshold))))
+            score.append(v)
+        return score
+    
+    def get_datetimefeatures(self):
+        for d in self.anomalydetectorobject.datetimecols:
+            
+            self.testdata[d+'_weekday'] = self.testdata[d].apply(lambda m : m.weekday())
+            self.testdata[d+'_hourofday'] = self.testdata[d].apply(lambda m : m.hour)
+            #self.cols = self.cols + [d+'_weekday',d+'_hourofday']
+        
+    
+    def __init__(self,ad,td):
+        if(td.shape[0]) == 0:
+            raise ValueError('There should be at least one observation to predict score')
+        self.anomalydetectorobject = ad
+        self.testdata = td.copy()
+        self.predict_anomaly_detector()
+        
---
+++ packages/categoricaloutlier/categoricaloutlier-0.6.1.tar.gz/categoricaloutlier-0.6.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'categoricaloutlier',         # Name of the package
+  packages = ['categoricaloutlier'],   
+  version = '0.6.1',      
+  license='MIT',        # MIT License
+  description = 'Trains on categorical and date time features of the data and predicts an anomaly score for new data',   
+  long_description='Categorical Outlier is tool to detect anomalous observations in categorical and datetime features. Most of the techniques that we already have focusses mostly on numeric features. There is no library available which can detect a outlier within categories. This package builds a profile of the categorical using the past observations and gives an outlier score to a new observation on the basis of this profile. A scenario where this library can be very useful will be a predicting unusual driving behaviour. A driver who drives the same route(s) to drive to may be office will show an anomalous behaviour if he takes altogether different route on particular day. He will get a high outlier for thie behaviour. On the contrary, an uber driver drives to new location everytime and hence, a new destination will not be an anomalous behaviour and hence will get a low score. The package also takes combination of categorical features as input.',
+  author = 'AKASH BAJPAI',                   
+  author_email = 'akash.baj03@gmail.com',      
+  url = 'https://github.com/akashbaj03/categoricaloutlier',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/akashbaj03/categoricaloutlier/archive/categoricaloutlier_v0.1.tar.gz',    # Download link
+  keywords = ['categorical', 'outlier', 'anomaly', 'unsupervised','datetime','frequency','probability'],   # Keywords that define your package best
+  install_requires=['pandas','numpy','scipy'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      #Supported python versions
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
