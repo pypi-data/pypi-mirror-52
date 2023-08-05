a95f77d32b89ca171b5d1a92322be9ea9f689431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:46 +0000

    Add package tfidfpackage/0.1/tfidfpackage-0.1.tar.gz

---
+++ packages/tfidfpackage/tfidfpackage-0.1.tar.gz/tfidfpackage-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="tfidfpackage",      # name of PyPI package
+    version='0.1',          # version number, update with new releases
+    packages= find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    description='Term frequency inverse document frequency',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
