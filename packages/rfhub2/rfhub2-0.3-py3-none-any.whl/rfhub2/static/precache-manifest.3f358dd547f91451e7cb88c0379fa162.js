6ae488e937be297c4e03178fc5cab3456aaaee80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:44 +0000

    Add package rfhub2/0.3/rfhub2-0.3-py3-none-any.whl

---
+++ packages/rfhub2/rfhub2-0.3-py3-none-any.whl/rfhub2/static/precache-manifest.3f358dd547f91451e7cb88c0379fa162.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e6555056b903fc1935d5b4b2ea62bf",
+    "url": "/static/index.html"
+  },
+  {
+    "revision": "6546469205ce1fb033df",
+    "url": "/static/static/css/main.2733a44a.chunk.css"
+  },
+  {
+    "revision": "5199887dfd350eaf36d0",
+    "url": "/static/static/js/2.df8caaff.chunk.js"
+  },
+  {
+    "revision": "6546469205ce1fb033df",
+    "url": "/static/static/js/main.9675b804.chunk.js"
+  },
+  {
+    "revision": "517784e95ca40345faae",
+    "url": "/static/static/js/runtime~main.e6c9c1d2.js"
+  }
+]);
