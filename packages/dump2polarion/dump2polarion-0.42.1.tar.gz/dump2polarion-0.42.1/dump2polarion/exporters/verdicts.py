e7e05eb513a500413d159421f3d42c1fca07c687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:07 +0000

    Add package dump2polarion/0.42.1/dump2polarion-0.42.1.tar.gz

---
+++ packages/dump2polarion/dump2polarion-0.42.1.tar.gz/dump2polarion-0.42.1/dump2polarion/exporters/verdicts.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Allowed values for verdicts.
+"""
+
+from __future__ import unicode_literals
+
+
+class Verdicts(object):
+    """Valid verdicts."""
+
+    PASS = ("passed", "pass")
+    FAIL = ("failed", "fail")
+    SKIP = ("skipped", "skip", "blocked")
+    WAIT = ("null", "wait", "waiting")
