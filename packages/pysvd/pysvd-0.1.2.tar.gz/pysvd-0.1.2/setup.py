db73a84af99d83ca04cb6ecb4a3b1d50ec4e939a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:09 +0000

    Add package pysvd/0.1.2/pysvd-0.1.2.tar.gz

---
+++ packages/pysvd/pysvd-0.1.2.tar.gz/pysvd-0.1.2/setup.py
@@ -0,0 +1,46 @@
+import setuptools
+import unittest
+
+exec(open('pysvd/version.py').read())
+
+with open("README.rst", "r") as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name="pysvd",
+    version=__version__,
+    description="System View Description parser",
+    long_description=long_description,
+    keywords="SVD CMSIS ARM",
+    license="MIT",
+    author="Benjamin Füldner",
+    author_email="benjamin@fueldner.net",
+    url="https://code.fueldner.net/opensource/pysvd",
+    project_urls={
+        'Bug Reports': 'https://github.com/bfueldner/pysvd/issues',
+        'Source': 'https://github.com/bfueldner/pysvd',
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: System :: Hardware",
+        "Topic :: System :: Hardware :: Hardware Drivers",
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Software Development :: Embedded Systems",
+    ],
+    python_requires='>=3.5',
+    entry_points={
+        'console_scripts': [
+            'svd2rst = scripts.svd2rst:main',
+        ],
+    },
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"]
+)
