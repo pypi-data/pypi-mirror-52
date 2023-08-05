a05eee2c2ae0c7572befe2b8f21194aaca2243cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:35 +0000

    Add package distributions-exercise-5-pio/0.1/distributions_exercise_5_pio-0.1.tar.gz

---
+++ packages/distributions-exercise-5-pio/distributions_exercise_5_pio-0.1.tar.gz/distributions_exercise_5_pio-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_exercise_5_pio',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_exercise_5_pio'],
+      author = 'Pio Korinth',
+      author_email = 'pio@kth.se',
+      zip_safe=False)
