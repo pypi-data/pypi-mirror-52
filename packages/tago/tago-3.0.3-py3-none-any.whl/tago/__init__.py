bbd06598e8cd62c4ebd48442be623475b1233a7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:03 +0000

    Add package tago/3.0.3/tago-3.0.3-py3-none-any.whl

---
+++ packages/tago/tago-3.0.3-py3-none-any.whl/tago/__init__.py
@@ -0,0 +1,19 @@
+from .device import Device
+from .analysis import Analysis
+from .account import Account
+from .services import Services
+from .extra import Extra
+from .connector import Connector
+from .authorization import Authorization
+from .run_user import RunUser
+
+class Tago:
+  def __init__(self, token):
+    self.account = Account(token)
+    self.analysis = Analysis(token)
+    self.authorization = Authorization(token)
+    self.connector = Connector(token)
+    self.device = Device(token)
+    self.extra = Extra(token)
+    self.run_user = RunUser(token)
+    self.services = Services(token)
---
+++ packages/tago/tago-3.0.3-py3-none-any.whl/tago/services/__init__.py
@@ -0,0 +1,29 @@
+from .emailService import Email
+from .sms import Sms
+from .console import Console
+from .mqtt import Mqtt
+from .attachment import Attachment
+from .notification import Notification
+
+
+class Services:
+  def __init__(self, token):
+    self.token = token
+
+  def sms(self):
+    return Sms(self.token)
+
+  def email(self):
+    return Email(self.token)
+
+  def console(self):
+    return Console(self.token)
+
+  def MQTT(self):
+    return Mqtt(self.token)
+
+  def notification(self):
+    return Notification(self.token)
+
+  def attachment(self):
+    return Attachment(self.token)
