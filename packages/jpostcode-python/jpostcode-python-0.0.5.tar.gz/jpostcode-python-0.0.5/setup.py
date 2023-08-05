132eb6fdd8a125392530b06536a31659fbd91e7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:54 +0000

    Add package jpostcode-python/0.0.5/jpostcode-python-0.0.5.tar.gz

---
+++ packages/jpostcode-python/jpostcode-python-0.0.5.tar.gz/jpostcode-python-0.0.5/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='jpostcode-python',
+      version='0.0.5',
+      description='Japanese postcode utils',
+      author='uehara1414',
+      author_email='akiya.noface@gmail.com',
+      url='https://github.com/uehara1414/jpostcode-python',
+      packages=find_packages(),
+      install_requires=[
+      ],
+      include_package_data=True,
+      package_data={
+            'jpostcode-python': ['data/*'],
+      }
+      )
