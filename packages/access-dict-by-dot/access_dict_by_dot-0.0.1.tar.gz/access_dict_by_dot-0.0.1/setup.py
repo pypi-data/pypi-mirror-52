9c1cc2a1ad0ed33201d1f79c8ed171216e510259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:58 +0000

    Add package access-dict-by-dot/0.0.1/access_dict_by_dot-0.0.1.tar.gz

---
+++ packages/access-dict-by-dot/access_dict_by_dot-0.0.1.tar.gz/access_dict_by_dot-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name = 'access_dict_by_dot',
+  packages = ['access_dict_by_dot'],
+  version = '0.0.1',
+  license='MIT',
+  description = 'Using this packages we can access dictionary values using dot operator',
+  author = 'Indrajeet Singh',
+  author_email = 'indrajeetsinghmaan@gmail.com',
+  url = 'https://github.com/ismaan1998/access_dict_by_dot/',
+  download_url = 'https://github.com/ismaan1998/access_dict_by_dot/releases/tag/0.0.1',
+  keywords = ['dictionary', 'dot', 'access'],
+
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+)
