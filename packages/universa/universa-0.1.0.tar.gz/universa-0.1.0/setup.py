8f930a4d604d5b622bdc9e90061c7d801a03fd2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:31 +0000

    Add package universa/0.1.0/universa-0.1.0.tar.gz

---
+++ packages/universa/universa-0.1.0.tar.gz/universa-0.1.0/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+
+CLASSIFIERS = [
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Developers',
+]
+
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+
+EXTRAS_REQUIRE = {
+    'dev': [
+        'pytest',
+    ]
+}
+
+
+setuptools.setup(
+    name='universa',
+    version='0.1.0',
+    author='Vadim Kovrigin',
+    author_email='kovrigin.dev@gmail.com',
+    description='Python package to access Universa API from python.',
+    include_package_data=True,
+    install_requires=[
+        'pexpect>=4.7.0',
+        'streamexpect>=0.2.1',
+    ],
+    extras_require=EXTRAS_REQUIRE,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['Universa'],
+    url='https://github.com/vkovrigin/universa',
+    packages=setuptools.find_packages(),
+    classifiers=CLASSIFIERS,
+)
