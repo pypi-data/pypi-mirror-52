4e18cccb7ece5f580a76e5d9b814bfea36eb780d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:41 +0000

    Add package oida/0.0.1/oida-0.0.1.tar.gz

---
+++ packages/oida/oida-0.0.1.tar.gz/oida-0.0.1/oida/__version__.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+"""
+Python, oida!
+"""
+
+
+__author__ = "Marcel Rieger"
+__email__ = "github.riga@icloud.com"
+__copyright__ = "Copyright 2019, Marcel Rieger"
+__credits__ = ["Marcel Rieger"]
+__contact__ = "https://github.com/riga/oida"
+__license__ = "BSD-3-Clause"
+__status__ = "Development"
+__version__ = "0.0.1"
+__all__ = []
