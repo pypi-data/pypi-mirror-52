0a05faa26fbb041e6e6068010158b7844c7a24f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:46 +0000

    Add package flake8-absolute-import/1.0/flake8-absolute-import-1.0.tar.gz

---
+++ packages/flake8-absolute-import/flake8-absolute-import-1.0.tar.gz/flake8-absolute-import-1.0/CHANGELOG.md
@@ -0,0 +1,19 @@
+## CHANGELOG: flake8-absolute-import -- flake8 plugin to require absolute imports
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project strives to adhere to
+[Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+
+### Unreleased Changes
+
+...
+
+### [1.0.0] - 2019-09-09
+
+#### Features
+
+ * Detect any relative imports and report with error code ABS101
+
---
+++ packages/flake8-absolute-import/flake8-absolute-import-1.0.tar.gz/flake8-absolute-import-1.0/src/flake8_absolute_import/__init__.py
@@ -0,0 +1,25 @@
+r"""*Root package definition for* ``flake8-absolute-import``.
+
+flake8 plugin to require absolute imports
+
+**Author**
+    Brian Skinn (bskinn@alum.mit.edu)
+
+**File Created**
+    6 Sep 2019
+
+**Copyright**
+    \(c) Brian Skinn 2019
+
+**Source Repository**
+    http://www.github.com/bskinn/flake8-absolute-import
+
+**License**
+    The MIT License; see |license_txt|_ for full license terms
+
+**Members**
+
+"""
+
+from flake8_absolute_import.core import Plugin
+from flake8_absolute_import.version import __version__
