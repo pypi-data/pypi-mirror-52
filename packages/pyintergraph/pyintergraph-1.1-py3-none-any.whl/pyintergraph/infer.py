63b1a62bd26e1a784d1c7a8f9762cba3f2e619ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:01 +0000

    Add package pyintergraph/1.1/pyintergraph-1.1-py3-none-any.whl

---
+++ packages/pyintergraph/pyintergraph-1.1-py3-none-any.whl/pyintergraph/infer.py
@@ -0,0 +1,39 @@
+import collections
+import numbers 
+
+def infer_type(value):
+
+    def is_primitive(val):
+        primitives = (int, bool, float, str)
+        return val in primitives
+
+    def get_c_type(v):
+        if isinstance(v, str) or v == str:
+            return "string"
+        elif isinstance(v, bool) or v == bool:
+            return "bool"
+        elif isinstance(v, int) or v == int:
+            return "int"
+        elif isinstance(v, float) or v == float:
+            return "float"
+        elif issubclass(v, numbers.Number):
+            return "float"
+        else:
+            raise Exception("Non supported Type in Attributes!")
+
+    if isinstance(value, collections.Iterable):
+        if type(value) != dict:
+            if len(value) == 1:
+                return get_c_type(value)
+            elif len(value) > 1:
+                types = set(type(v) for v in value)
+                if len(types) == 1:
+                    return get_c_type(types.pop())
+                elif len(types) == 2 and types == set([float, int]):
+                    return "float"
+                else:
+                    return "string"
+        else:
+            return "object"
+    else:
+        return get_c_type(value)
