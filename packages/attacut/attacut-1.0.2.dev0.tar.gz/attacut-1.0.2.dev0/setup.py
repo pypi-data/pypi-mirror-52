d7afbcb8d8c85b947d561dc18f25de5d8258da8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:59 +0000

    Add package attacut/1.0.2.dev0/attacut-1.0.2.dev0.tar.gz

---
+++ packages/attacut/attacut-1.0.2.dev0.tar.gz/attacut-1.0.2.dev0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("./requirements.txt", "r") as f:
+    requirements = list(map(lambda x: x.strip(), f.readlines()))
+
+setup(
+    name="attacut",
+    version="1.0.2-dev",
+    description="Fast and Reasonably Accurate Word Tokenizer for Thai",
+    author="Pattarawat Chormai et al.",
+    author_email="foomail@foo.com",
+    packages=["attacut", "attacut.models", "attacut.artifacts"],
+    install_requires=requirements,
+    scripts=["scripts/attacut-cli"],
+    package_data={"attacut": ["artifacts/**/*"]},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
