a0bf7bacad1a5a94ad163b629502db9ce2b83933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:06 +0000

    Add package bidict/0.18.2/bidict-0.18.2.tar.gz

---
+++ packages/bidict/bidict-0.18.2.tar.gz/bidict-0.18.2/tests/conftest.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright 2009-2019 Joshua Bronson. All Rights Reserved.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""Set up hypothesis."""
+
+from os import getenv
+from hypothesis import HealthCheck, settings, unlimited
+
+
+MAX_EXAMPLES_DEFAULT = 200
+NOCHECK_SLOW = (HealthCheck.too_slow,)
+PROFILE_DEFAULT = {
+    'max_examples': int(getenv('HYPOTHESIS_MAX_EXAMPLES') or MAX_EXAMPLES_DEFAULT),
+    'deadline': None,
+    'timeout': unlimited,
+    # Enabling coverage slows down hypothesis.
+    'suppress_health_check': NOCHECK_SLOW if getenv('COVERAGE') else (),
+}
+PROFILE_MORE_EXAMPLES = dict(
+    PROFILE_DEFAULT,
+    max_examples=int(getenv('HYPOTHESIS_MAX_EXAMPLES') or MAX_EXAMPLES_DEFAULT * 10),
+    suppress_health_check=NOCHECK_SLOW,
+)
+settings.register_profile('default', **PROFILE_DEFAULT)
+settings.register_profile('more-examples', **PROFILE_MORE_EXAMPLES)
+settings.load_profile(getenv('HYPOTHESIS_PROFILE') or 'default')
