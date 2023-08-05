0b4391bcb2f6bb7e285826b99573b56a614df4fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:43 +0000

    Add package wawa-logger/1.3/wawa_logger-1.3.tar.gz

---
+++ packages/wawa-logger/wawa_logger-1.3.tar.gz/wawa_logger-1.3/wawa_logger/__init__.py
@@ -0,0 +1,30 @@
+from requests import post
+
+
+
+def publish(**kwargs):
+    """
+    sends data based on input to centralized source
+
+    params
+    :url - where do we send the logs
+    :tool_name
+    :event_type
+    :source_host
+    :dest_host
+    :username
+    returns
+    :bool True|False
+    """
+    noval = "no_data"
+    payload = {
+        "tool_name": kwargs.get("tool_name", ""),
+        "event_type": kwargs.get("event_type", ""),
+        "source_host": kwargs.get("source_host", ""),
+        "dest_host": kwargs.get("dest_host", ""),
+        "username": kwargs.get("username", "")
+    }
+    try:
+        r = post(url, json=payload)
+    except Exception as e:
+        print(f"Exception: {e}")
