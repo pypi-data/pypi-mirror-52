58d61d7fec4646c1368286644588a62ef83e4673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:15 +0000

    Add package gameai-frame/0.1.1/gameai_frame-0.1.1.tar.gz

---
+++ packages/gameai-frame/gameai_frame-0.1.1.tar.gz/gameai_frame-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'gameai_frame',\
+    version = '0.1.1',\
+    description = ('maybe a generitic game ai frame :)'),\
+    long_description = open('E:\Python\PY_APP6\gameai_frame\README.md', 'r', encoding='UTF-8').read(),\
+    author = 'Ehekatl',\
+    author_email = '2072498313@qq.com',\
+    maintainer = 'Ehekatl',\
+    maintainer_email = '2072498313@qq.com',\
+    license = 'MIT License',\
+    packages = find_packages(),\
+    platforms = ['all'],\
+    url = 'https://github.com/Iyrian/GameAI-Frame-')
