a553f40200d8255366d4e58bce4230fa5f2bfd15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:58 +0000

    Add package dvnesting/1.0.0/dvnesting-1.0.0.tar.gz

---
+++ packages/dvnesting/dvnesting-1.0.0.tar.gz/dvnesting-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+name = 'dvnesting',
+version = '1.0.0',
+py_modules = ['nesting'],
+author = 'dv1912',
+author_email = 'venkatesh.deekonda19@gmail.com',
+#url = 'http://www.headfirstlabs.com',
+description = 'A simple printer of nested lists',
+)
+
