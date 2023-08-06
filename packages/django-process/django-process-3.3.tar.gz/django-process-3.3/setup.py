107f817f3e8041173d30baf4e6e05ca644140da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:58 +0000

    Add package django-process/3.3/django-process-3.3.tar.gz

---
+++ packages/django-process/django-process-3.3.tar.gz/django-process-3.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-process",
+    version="3.3",
+    author="Josue Gomez",
+    author_email="jgomez@binkfe.com",
+    description="A package for create process and tasks on django",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.binkfe.com/jesrat/django-process",
+    packages=[
+        'process',
+        'process.management',
+        'process.management.commands',
+        'process.templatetags'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        'Framework :: Django',
+        "Operating System :: OS Independent",
+    ],
+)
