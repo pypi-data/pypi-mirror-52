72c804465428cc78278317f36f7e81a50bf6668c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:48 +0000

    Add package neptune-cache-redis/2019.9.15.1347/neptune_cache_redis-2019.9.15.1347.tar.gz

---
+++ packages/neptune-cache-redis/neptune_cache_redis-2019.9.15.1347.tar.gz/neptune_cache_redis-2019.9.15.1347/neptune_cache_redis/connect.py
@@ -0,0 +1,38 @@
+import aioredis
+from .cache import Cache
+
+
+class Connection:
+
+    def __init__(self,
+                 loop,
+                 host='localhost',
+                 port=6379,
+                 username=None,
+                 password=None,
+                 database=1,
+                 minsize=10,
+                 maxsize=15):
+        self.loop = loop
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.database = database
+        self.minsize = minsize
+        self.maxsize = maxsize
+        self._con = None
+        self.Cache = Cache(self)
+        self.loop.create_task(self.init_connection())
+
+    @property
+    def full_url(self) -> str:
+        return f'redis://{self.host}'
+
+    async def init_connection(self) -> None:
+        if not self._con:
+            self._con = await aioredis.create_redis_pool(address=self.full_url,
+                                                         minsize=self.minsize,
+                                                         maxsize=self.maxsize,
+                                                         loop=self.loop)
+
