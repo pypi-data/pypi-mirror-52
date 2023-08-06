d727746e5331a178387a9701e93da90ef006a98c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:53 +0000

    Add package pygrappa/0.12.4/pygrappa-0.12.4.tar.gz

---
+++ packages/pygrappa/pygrappa-0.12.4.tar.gz/pygrappa-0.12.4/pygrappa/kspa.py
@@ -0,0 +1,25 @@
+'''Python implementation of the kSPA algorithm.'''
+
+def kspa():
+    '''Recon for arbitrary trajectories using k‐space sparse matrices.
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+
+    Notes
+    -----
+
+    References
+    ----------
+    .. [1] Liu, Chunlei, Roland Bammer, and Michael E. Moseley.
+           "Parallel imaging reconstruction for arbitrary
+           trajectories using k‐space sparse matrices (kSPA)."
+           Magnetic Resonance in Medicine: An Official Journal of the
+           International Society for Magnetic Resonance in Medicine
+           58.6 (2007): 1171-1181.
+    '''
+
+    
