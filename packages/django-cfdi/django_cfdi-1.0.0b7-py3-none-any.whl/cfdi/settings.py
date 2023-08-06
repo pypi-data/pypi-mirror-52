2ba8321ea52c59f7f7f748d3cec485bd0771924e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:56 +0000

    Add package django-cfdi/1.0.0b7/django_cfdi-1.0.0b7-py3-none-any.whl

---
+++ packages/django-cfdi/django_cfdi-1.0.0b7-py3-none-any.whl/cfdi/settings.py
@@ -0,0 +1,48 @@
+from django.conf import settings
+import os
+if not settings.configured:
+    settings.configure()
+
+ROOT_PATH = os.path.dirname(__file__)
+
+_DEFAULT_PAC_AUTH = {
+    "ntlink": {
+        "dev": {"usuario": "", "password": ""},
+        "prod": {"usuario": "", "password": ""},
+    },
+    "dfacture": {
+        "dev": {"usuario": "", "password": ""},
+        "prod": {"usuario": "", "password": ""},
+    },
+    "prodigia": {
+        "dev": {"contrato": "", "usuario": "", "password": ""},
+        "prod": {"contrato": "", "usuario": "", "password": ""},
+    },
+}
+
+
+TMP_DIR = getattr(settings, "CFDI_TMP_DIR", "/tmp/")
+
+DFACTURE_AUTH = getattr(
+    settings, "CFDI_DFACTURE_AUTH", _DEFAULT_PAC_AUTH.get("dfacture")
+)
+
+PRODIGIA_AUTH = getattr(
+    settings, "CFDI_PRODIGIA_AUTH", _DEFAULT_PAC_AUTH.get("prodigia")
+)
+
+NTLINK_AUTH = getattr(
+    settings, "CFDI_NTLINK_AUTH", _DEFAULT_PAC_AUTH.get("ntlink")
+)
+
+CFDI_DB_TABLE = getattr(settings, "CFDI_DB_TABLE", "cfdi_cfdi")
+XML_DIR = getattr(settings, "XML_DIR", "/srv/www/cfd")
+XSLT_PATH_33 = getattr(
+    settings, 
+    "XSLT_PATH_33", 
+    "{}/xslt/cadenaoriginal_3_3.xslt".format(ROOT_PATH)
+)
+
+
+ERROR_CALLBACK = getattr(settings, "CFDI_ERROR_CALLBACK", None)
+POST_TIMBRADO_CALLBACK = getattr(settings, "CFDI_POST_TIMBRADO_CALLBACK", None)
