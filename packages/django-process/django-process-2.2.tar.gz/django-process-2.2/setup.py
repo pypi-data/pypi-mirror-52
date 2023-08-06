8e1934a7697b51f6563ee2bd87dcefdf70aa9937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:13 +0000

    Add package django-process/2.2/django-process-2.2.tar.gz

---
+++ packages/django-process/django-process-2.2.tar.gz/django-process-2.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-process",
+    version="2.2",
+    author="Josue Gomez",
+    author_email="jgomez@binkfe.com",
+    description="A package for create process and tasks on django",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.binkfe.com/jesrat/django-process",
+    packages=[
+        'process',
+        'process.management',
+        'process.management.commands',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        'Framework :: Django',
+        "Operating System :: OS Independent",
+    ],
+)
