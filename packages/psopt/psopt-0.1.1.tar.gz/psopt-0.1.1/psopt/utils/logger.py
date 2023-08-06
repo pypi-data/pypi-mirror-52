57fb6cd940f85aeaa7e2e6583cf1101d282cc253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:10 +0000

    Add package psopt/0.1.1/psopt-0.1.1.tar.gz

---
+++ packages/psopt/psopt-0.1.1.tar.gz/psopt-0.1.1/psopt/utils/logger.py
@@ -0,0 +1,77 @@
+import csv
+import logging
+import os
+import typing
+
+from datetime import datetime
+
+
+class CustomLogger(logging.Logger):
+    """Logger class that outputs .csv metrics logs and .json metafiles"""
+
+    def __init__(self, name: typing.Text):
+        super().__init__(name)
+        self.timestamp = str(datetime.now())
+
+    def set_files(self, file_path: typing.Text):
+
+        self.file_path = os.path.join(file_path, self.timestamp)
+        os.makedirs(self.file_path, exist_ok=True)
+
+        self.file_results = os.path.join(self.file_path, "results.csv")
+
+    def write_metrics(self, values: dict):
+        self._write_csv(self.file_results, values)
+
+    @staticmethod
+    def _write_csv(path: typing.Text, values: dict):
+
+        file_exists = os.path.isfile(path)
+
+        with open(path, "a+") as csv_file:
+
+            writer = csv.DictWriter(csv_file, values.keys())
+
+            if not file_exists:
+                writer.writeheader()
+            writer.writerow(values)
+
+    def __getstate__(self):
+        return None  # avoid unecessary pickling issues
+
+
+def make_logger(name: typing.Text, verbose: typing.Union[int, bool]) -> CustomLogger:
+
+    logger = CustomLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logging.Formatter("%(message)s"))
+
+    if verbose == 1:
+        # only set the console handler level
+        consoleHandler.setLevel(logging.INFO)
+
+    elif verbose == 2:
+        # set the console handler level
+        consoleHandler.setLevel(logging.INFO)
+
+        # make sure that fileHandler logging directory exists
+        os.makedirs(os.path.join(os.getcwd(), ".logs"), exist_ok=True)
+
+        # add fileHandler in logger
+        fileHandler = logging.FileHandler(".logs/logging.log")
+        fileHandler.setLevel(logging.DEBUG)
+        fileHandler.setFormatter(
+            logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        )
+        logger.addHandler(fileHandler)
+
+    else:
+        consoleHandler.setLevel(logging.WARNING)
+
+    logger.set_files(os.path.join(os.getcwd(), ".logs"))
+
+    logger.addHandler(consoleHandler)
+
+    return logger
---
+++ packages/psopt/psopt-0.1.1.tar.gz/psopt-0.1.1/setup.py
@@ -0,0 +1,51 @@
+from setuptools import find_packages, setup
+
+install_requires = [
+    "dill",
+    "multiprocess",
+    "numpy",
+    "matplotlib"
+]
+
+dev_requires = [
+    "pytest",
+    "pytest-cov",
+    "flake8",
+    "codecov"
+]
+
+doc_requires = [
+    "sphinx",
+    "sphinx-gallery"
+]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="psopt",
+    packages=find_packages(),
+    version="v0.1.1",
+    author="Artur de Luca",
+    author_email="arturbackdeluca@gmail.com",
+    description="A particle swarm optimizer for general use",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/artur-deluca/psopt",
+    include_package_data=True,
+    install_requires=install_requires,
+
+    extras_require={
+        "dev": install_requires + dev_requires,
+        "doc": install_requires + doc_requires,
+        "all": install_requires + dev_requires + doc_requires
+    },
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering"
+
+    ]
+)
