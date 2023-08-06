efb465775d5cf2a2d33bd4530626ac477ef3558f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:28 +0000

    Add package pandasgui/0.1.11/pandasgui-0.1.11.tar.gz

---
+++ packages/pandasgui/pandasgui-0.1.11.tar.gz/pandasgui-0.1.11/pandasgui/widgets/__init__.py
@@ -0,0 +1,10 @@
+"""Reusable PyQt widgets"""
+
+__all__ = ['DataFrameExplorer', 'DataFrameViewer', 'FigureViewer', 'TabbedFigureViewer', 'PivotDialog',
+           'ScatterDialog', 'PlotlyViewer']
+
+from pandasgui.widgets.image_viewer import FigureViewer, TabbedFigureViewer
+from pandasgui.widgets.plotly_viewer import PlotlyViewer
+from pandasgui.widgets.dialogs import PivotDialog, ScatterDialog
+from pandasgui.widgets.dataframe_viewer import DataFrameViewer
+from pandasgui.widgets.dataframe_explorer import DataFrameExplorer
