3ed9b0c8714e884a12a3bdb70917096c976c9226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:03 +0000

    Add package mycrypto/1.0/mycrypto-1.0.tar.gz

---
+++ packages/mycrypto/mycrypto-1.0.tar.gz/mycrypto-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+setup(name='mycrypto',
+      version='1.0',
+      description='Common cryptography encryption and decryption tools',
+      url='https://github.com/nightRainy/my_crypto',
+      author='ZhiShi',
+      author_email='foxfoxfox940@foxmail.com',
+      license='MIT',
+      packages=['mycrypto'],
+      zip_safe=False)
