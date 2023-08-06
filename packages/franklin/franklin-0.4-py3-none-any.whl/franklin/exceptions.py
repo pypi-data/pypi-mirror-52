88613e6d598a7af10ea3f77f78f480747269f7b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:08 +0000

    Add package franklin/0.4/franklin-0.4-py3-none-any.whl

---
+++ packages/franklin/franklin-0.4-py3-none-any.whl/franklin/exceptions.py
@@ -0,0 +1,25 @@
+class DOIError(RuntimeError):
+    """Resolution of a digital object identifier has encountered an error."""
+    pass
+
+
+class DuplicateDOIError(RuntimeError):
+    """Trying to add a new bibtex entry when the file already contains this DOI."""
+    pass
+
+
+class BibtexFileNotFoundError(FileNotFoundError):
+    pass
+
+
+class PDFNotFoundError(FileNotFoundError):
+    pass
+
+
+class ConfigError(RuntimeError):
+    pass
+
+
+class UnknownPublisherError(KeyError):
+    pass
+
