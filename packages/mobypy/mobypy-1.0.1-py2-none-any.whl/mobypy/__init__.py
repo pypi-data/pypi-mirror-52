7ebfb627aad7dc3828fb1f8ceb2705162eced8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:36 +0000

    Add package mobypy/1.0.1/mobypy-1.0.1-py2-none-any.whl

---
+++ packages/mobypy/mobypy-1.0.1-py2-none-any.whl/mobypy/__init__.py
@@ -0,0 +1,6 @@
+from .thesaurus import (
+                        synonyms,
+                        long_synonyms,
+                        short_synonyms,
+                        reverse_search
+                        ) # pragma: no cover
