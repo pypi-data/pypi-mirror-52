18bc26a62040bdcb1e9a3b1a5df987eb2dcd7386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:03 +0000

    Add package neptune-dnsoverhttps-protocol/2019.9.16.1005/neptune_dnsoverhttps_protocol-2019.9.16.1005.tar.gz

---
+++ packages/neptune-dnsoverhttps-protocol/neptune_dnsoverhttps_protocol-2019.9.16.1005.tar.gz/neptune_dnsoverhttps_protocol-2019.9.16.1005/neptune_dnsoverhttps_protocol/connect.py
@@ -0,0 +1,17 @@
+from .protocol import Protocol
+
+
+class Connection:
+
+    def __init__(self, dispatcher_cls, loop, cert_path, key_path, prefix='dns-request', host='0.0.0.0', port=443):
+        self.dispatcher_cls = dispatcher_cls
+        self.loop = loop
+        self.cert_path = cert_path
+        self.key_path = key_path
+        self.prefix = prefix
+        self.host = host
+        self.port = port
+
+    async def start(self):
+        print('Starting DNS-over-HTTPS server')
+        await Protocol(self).start()
---
+++ packages/neptune-dnsoverhttps-protocol/neptune_dnsoverhttps_protocol-2019.9.16.1005.tar.gz/neptune_dnsoverhttps_protocol-2019.9.16.1005/neptune_dnsoverhttps_protocol/protocol.py
@@ -0,0 +1,26 @@
+from aiohttp import web
+import ssl
+import base64
+
+
+class Protocol:
+    def __init__(self, connector):
+        self.connector = connector
+        self.app = web.Application(loop=self.connector.loop)
+        self.app.add_routes([web.get(f'/{connector.prefix}', self.handle)])
+        # self.ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        # self.ssl_context.load_cert_chain(connector.cert_path, connector.key_path)
+
+    async def handle(self, request):
+        dispatcher = self.connector.dispatcher_cls(loop=self.connector.loop)
+        if request.method == 'GET':
+            query = request.rel_url.query['dns']
+            decoded_query = base64.b64decode(query + '=' * (4 - len(query) % 4))
+            result = await dispatcher.handle(decoded_query)
+            return web.Response(text=base64.b64encode(result).decode('ascii'))
+        elif request.method == 'POST':
+            pass
+        return web.Response(text='1234')
+
+    async def start(self):
+        await web._run_app(self.app, port=self.connector.port, host=self.connector.host)
