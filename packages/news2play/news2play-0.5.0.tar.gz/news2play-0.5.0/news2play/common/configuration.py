927ec0654d5c5bdd719131a93b7f02353173f65b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:12 +0000

    Add package news2play/0.5.0/news2play-0.5.0.tar.gz

---
+++ packages/news2play/news2play-0.5.0.tar.gz/news2play-0.5.0/news2play/common/configuration.py
@@ -0,0 +1,12 @@
+# default config
+storage_conf = "./storage/conf"
+storage_data = "./storage/data"
+storage_log = "./storage/logs"
+
+audio_type = 'mp3'
+
+polly_voice = 'Kendra'
+azure_voice = 'en-US-JessaNeural'
+azure_voice_style = 'empathy'
+
+polly_arn = 'arn:aws:iam::595543519941:instance-profile/news2play_ec2'
