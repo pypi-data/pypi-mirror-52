9542433bb1cc5d3ad8bc1e6011dc2bc9fe09ad6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:50 +0000

    Add package ez-ml/0.0.1/ez-ml-0.0.1.tar.gz

---
+++ packages/ez-ml/ez-ml-0.0.1.tar.gz/ez-ml-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+version = open('VERSION').read().strip()
+license = open('LICENSE').read().strip()
+
+setup(
+    name='ez-ml',
+    version=version,
+    license=license,
+    author='Ghazi Ben Dahmen',
+    author_email='ghazi.ben.dahmen3@gmail.com',
+    url='http://www.github.com/ghazibendahmen/ez-ml',
+    description='Most useful ML lib',
+    long_description=open('README.md').read().strip(),
+    packages=find_packages(),
+    install_requires=[
+        # put packages here
+        'six',
+    ],
+    test_suite='tests',
+    entry_points={
+        'console_scripts': [
+            'ezml = ezml.__main__:main',
+        ]
+    }
+)
