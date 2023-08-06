df9f2f4043e28ecbc41c810365ffd53cbfe3b191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:27 +0000

    Add package nanome/0.7.2/nanome-0.7.2-py2.py3-none-any.whl

---
+++ packages/nanome/nanome-0.7.2-py2.py3-none-any.whl/nanome/_internal/_network/_commands/_serialization/_create_stream_result.py
@@ -0,0 +1,24 @@
+from nanome._internal._util._serializers import _TypeSerializer
+from nanome.util.enums import StreamDataType
+
+class _CreateStreamResult(_TypeSerializer):
+    def __init__(self):
+        pass
+
+    def version(self):
+        return 1
+
+    def name(self):
+        return "StreamCreationResult"
+
+    def serialize(self, version, value, context):
+        raise NotImplementedError
+
+    def deserialize(self, version, context):
+        err = context.read_byte()
+        id = context.read_uint()
+        if version > 0:
+            data_type = context.read_byte()
+        else:
+            data_type = StreamDataType.float
+        return (err, id, data_type)
