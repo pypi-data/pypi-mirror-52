af1c1394813427208e844f4a5221d4abe11bb45c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:10 +0000

    Add package qandaomr/1.2.3.post1/qandaomr-1.2.3.post1.tar.gz

---
+++ packages/qandaomr/qandaomr-1.2.3.post1.tar.gz/qandaomr-1.2.3.post1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='qandaomr',
+    version='1.2.3-1',
+    description='Q&A Python and OpenCV OMR (optical mark recognition)',
+
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    author='Joppe Blondel',
+    author_email='joppe@blondel.nl',
+    download_url='https://github.com/Jojojoppe/qandaomr/archive/v1.2.3.tar.gz',
+    url='https://github.com/Jojojoppe/qandaomr',
+
+    keywords = ['OMR', 'Optical Mark Recognition',],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+  ],
+
+    packages=['qandaomr'],
+    licence='GNU General Public License v3 (GPLv3)',
+    install_requires=['opencv-python', 'numpy', 'argparse', 'zxing'],
+    scripts=['scripts/qandaomr']
+)
