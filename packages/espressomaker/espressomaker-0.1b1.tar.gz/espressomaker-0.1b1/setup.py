016340d133a1de8f0241e5b9c8eadc1fcd13825b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:21 +0000

    Add package espressomaker/0.1b1/espressomaker-0.1b1.tar.gz

---
+++ packages/espressomaker/espressomaker-0.1b1.tar.gz/espressomaker-0.1b1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("HISTORY.md", "r") as fh:
+    history = fh.read()
+
+setuptools.setup(
+    name="espressomaker",
+    version="0.1b1",
+    author="Raul Morales Delgado",
+    author_email="rmoralesdelgado@gmail.com",
+    description="Allows to temporarily modify the power management settings on a MacOS to run processes uninterruptedly.",
+    long_description=long_description + history,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rmoralesdelgado/espressomaker",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+    ],
+    python_requires='>=3.5',
+)
