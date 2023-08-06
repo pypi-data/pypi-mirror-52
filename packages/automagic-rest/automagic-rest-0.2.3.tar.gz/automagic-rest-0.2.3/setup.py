79bd5d7052108835627e5a95092e25fcca751f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:01 +0000

    Add package automagic-rest/0.2.3/automagic-rest-0.2.3.tar.gz

---
+++ packages/automagic-rest/automagic-rest-0.2.3.tar.gz/automagic-rest-0.2.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="automagic-rest",
+    version="0.2.3",
+    description="Automagic REST: Django REST Framework PostgreSQL Builder",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Timothy Allen",
+    author_email="tallen@wharton.upenn.edu",
+    url="https://github.com/wharton/automagic-rest",
+    include_package_data=True,
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        "djangorestframework>=3.7.7",
+        "djangorestframework-filters==1.0.0.dev0",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Framework :: Django",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+)
