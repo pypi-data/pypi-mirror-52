31822fe21a8f2811215eb3d7da9e15eeea4688b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:03 +0000

    Add package mwpy/0.3/mwpy-0.3.tar.gz

---
+++ packages/mwpy/mwpy-0.3.tar.gz/mwpy-0.3/setup.py
@@ -0,0 +1,43 @@
+from os.path import abspath, dirname, join
+from re import search, MULTILINE
+
+from setuptools import setup
+
+
+with open('README.rst', 'r') as fh:
+    long_description = fh.read()
+
+here = abspath(dirname(__file__))
+setup(
+    name='mwpy',
+    version=search(
+        r"^__version__ = '([^']*)'",
+        open(
+            join(here, 'mwpy', '_version.py'),
+            encoding='ascii', errors='ignore').read(),
+        MULTILINE,
+    ).group(1),
+    author='5j9',
+    author_email='5j9@users.noreply.github.com',
+    description="An async MediaWiki client using trio and asks.",
+    license='GNU General Public License v3 (GPLv3)',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/5j9/mwpy',
+    packages=['mwpy'],
+    python_requires='>=3.5',
+    install_requires=['trio', 'asks'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Environment :: Web Environment',
+        'Framework :: Trio',
+    ],
+    zip_safe=True,
+)
