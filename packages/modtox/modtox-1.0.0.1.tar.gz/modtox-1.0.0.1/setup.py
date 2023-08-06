ac20059e64b9c7757634c9046b302acb9f87e575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:51 +0000

    Add package modtox/1.0.0.1/modtox-1.0.0.1.tar.gz

---
+++ packages/modtox/modtox-1.0.0.1.tar.gz/modtox-1.0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(name='modtox',
+      packages = ['modtox'],
+      version='1.0.0.1',
+      license='MIT',
+      description='Asses toxic effect of drugs based on MD simualtions of HROT dataset',
+      author='Daniel Soler',
+      author_email='daniel.soler@nostrumbiodiscovery.com',
+      url='https://github.com/NostrumBioDiscovery/modtox',
+      install_requires=['pytraj', 'mdtraj', 'matplotlib', 'xgboost', 'sklearn', 'prody', 'biopython', 'pandas', 'mordred', 'nltk', 'seaborn', 'umap-learn', 'tqdm', 'requests', 'chembl_webresource_client', 'pytest'],
+     )
