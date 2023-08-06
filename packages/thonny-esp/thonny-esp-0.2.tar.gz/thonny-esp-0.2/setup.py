b67d74ac056282ee627848fd69e62fa6900664e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:48 +0000

    Add package thonny-esp/0.2/thonny-esp-0.2.tar.gz

---
+++ packages/thonny-esp/thonny-esp-0.2.tar.gz/thonny-esp-0.2/setup.py
@@ -0,0 +1,61 @@
+from setuptools import setup
+import os.path
+
+setupdir = os.path.dirname(__file__)
+
+requirements = []
+for line in open(os.path.join(setupdir, 'requirements.txt'), encoding="UTF-8"):
+    if line.strip() and not line.startswith('#'):
+        requirements.append(line)
+
+setup(
+      name="thonny-esp",
+      version="0.2",
+      description="ESP8266 and ESP32 MicroPython support for Thonny IDE",
+      long_description="""Plug-in for Thonny IDE which adds ESP8266 and ESP32 MicroPython backends. 
+      
+More info: 
+
+* https://github.com/thonny/thonny-esp
+* https://github.com/thonny/thonny/wiki/MicroPython
+* https://thonny.org
+
+NB! This plug-in is only relevant for Thonny up to 3.2.1. 
+Since 3.2.2 this functionality is included in the main package.
+""",
+      url="https://github.com/thonny/thonny-esp/",
+      author="Aivar Annamaa",
+      author_email="aivar.annamaa@gmail.com",
+      license="MIT",
+      classifiers=[
+        "Environment :: MacOS X",
+        "Environment :: Win32 (MS Windows)",
+        "Environment :: X11 Applications",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Freeware",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Education",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Embedded Systems",
+      ],
+      keywords="IDE education programming ESP8266 ESP32 MicroPython Thonny",
+      platforms=["Windows", "macOS", "Linux"],
+      python_requires=">=3.5",
+      include_package_data=True,
+      package_data={'thonnycontrib.esp': ['esp8266_api_stubs/*', 'esp32_api_stubs/*']},
+      install_requires=requirements,
+      packages=["thonnycontrib.esp"],
+)
---
+++ packages/thonny-esp/thonny-esp-0.2.tar.gz/thonny-esp-0.2/thonnycontrib/esp/__init__.py
@@ -0,0 +1,140 @@
+from thonny.plugins.micropython import MicroPythonProxy, MicroPythonConfigPage,\
+    add_micropython_backend
+from thonny import get_workbench, get_runner
+import os
+import subprocess
+from thonny.ui_utils import SubprocessDialog
+from thonny.running import get_frontend_python
+from time import sleep
+
+class ESPProxy(MicroPythonProxy):
+    def _finalize_repl(self):
+        # In some cases there may be still something coming.
+        sleep(0.1)
+        remainder = self._serial.read_all().decode("utf-8", "replace").strip()
+        # display it unless it looks like an extra raw prompt
+        if remainder and (len(remainder) > 40 or "raw REPL; CTRL-B to exit" not in remainder):
+            self._send_error_to_shell(remainder)
+            
+    @property
+    def firmware_filetypes(self):
+        return [('*.bin files', '.bin'), ('all files', '.*')]
+    
+    def erase_flash(self):
+        self.disconnect()
+        cmd = [get_frontend_python(), '-u', '-m', 
+                'esptool', 
+                '--port', self.port, 
+                'erase_flash']
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                            universal_newlines=True)
+        dlg = SubprocessDialog(get_workbench(), proc, "Erasing flash", autoclose=False)
+        dlg.wait_window()
+    
+    @property
+    def allow_webrepl(self):
+        return True
+        
+        
+class ESP8266Proxy(ESPProxy):
+    description = "MicroPython on ESP8266"
+    config_page_constructor = "ESP8266"
+    
+    @property
+    def known_usb_vids_pids(self):
+        return super().known_usb_vids_pids | {
+            # eg. Adafruit Feather Huzzah 
+            (0x10C4, 0xEA60), # Silicon Labs CP210x USB to UART Bridge,
+            (0x1A86, 0x7523), # USB-SERIAL CH340,
+        } 
+    
+    @property
+    def flash_mode(self):
+        # "dio" for some boards with a particular FlashROM configuration (e.g. some variants of a NodeMCU board)
+        # https://docs.micropython.org/en/latest/esp8266/esp8266/tutorial/intro.html
+        # https://github.com/espressif/esptool/wiki/SPI-Flash-Modes
+        
+        # TODO: detect the need for this (or provide conf option for the backend)
+        return "keep"
+        #return "dio"
+        
+    def construct_firmware_upload_command(self, firmware_path):
+        return [get_frontend_python(), '-u', '-m', 
+                'esptool', 
+                '--port', self.port, 
+                #'--baud', '460800',  
+                'write_flash', 
+                #'--flash_size', 'detect',
+                #"--flash_mode", self.flash_mode,
+                '0x0000', firmware_path]
+        
+    def _get_api_stubs_path(self):
+        return os.path.join(os.path.dirname(__file__), "esp8266_api_stubs")
+    
+class ESP32Proxy(ESPProxy):
+    @property
+    def known_usb_vids_pids(self):
+        return super().known_usb_vids_pids | {
+            # eg. ESP-WROOM-32
+            (0x10C4, 0xEA60), # Silicon Labs CP210x USB to UART Bridge
+        }
+        
+    def construct_firmware_upload_command(self, firmware_path):
+        return [get_frontend_python(), '-u', '-m', 
+                'esptool', 
+                #'--chip', 'esp32',
+                '--port', self.port, 
+                #'--baud', '460800', 
+                'write_flash', 
+                #'--flash_size=detect',
+                '0x1000',
+                firmware_path]
+
+    def _get_api_stubs_path(self):
+        return os.path.join(os.path.dirname(__file__), "esp32_api_stubs")
+
+class ESP32LoBoProxy(ESP32Proxy):
+    def _get_path_prefix(self):
+        return "/flash/"        
+        
+
+    
+class ESP8266ConfigPage(MicroPythonConfigPage):
+    pass
+
+class ESP32ConfigPage(MicroPythonConfigPage):
+    pass
+
+class ESP32LoBoConfigPage(ESP32ConfigPage):
+    pass
+
+def load_plugin():
+    add_micropython_backend("ESP8266", ESP8266Proxy, "MicroPython (ESP8266)", ESP8266ConfigPage)
+    add_micropython_backend("ESP32", ESP32Proxy, "MicroPython (ESP32)", ESP32ConfigPage)
+
+    def upload_micropython():
+        proxy = get_runner().get_backend_proxy()
+        proxy.select_and_upload_micropython()
+    
+    def upload_micropython_enabled():
+        proxy = get_runner().get_backend_proxy()
+        return (getattr(proxy, "micropython_upload_enabled", False)
+                and isinstance(proxy, ESPProxy))
+        
+    def erase_flash():
+        proxy = get_runner().get_backend_proxy()
+        proxy.erase_flash()
+    
+    def erase_flash_enabled():
+        return isinstance(get_runner().get_backend_proxy(), ESPProxy) and get_runner().get_backend_proxy().micropython_upload_enabled
+        
+    get_workbench().add_command("uploadmicropythonesp", "device", "Install MicroPython to ESP8266/ESP32 ...",
+                                upload_micropython,
+                                upload_micropython_enabled,
+                                group=40)
+
+    get_workbench().add_command("erasespflash", "device", "Erase ESP8266/ESP32 flash",
+                                erase_flash,
+                                tester=erase_flash_enabled,
+                                group=40)
+    
