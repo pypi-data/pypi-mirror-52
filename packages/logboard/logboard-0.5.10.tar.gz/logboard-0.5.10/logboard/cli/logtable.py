745cf008c397c1af6773a471452365de17c4d35e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:49 +0000

    Add package logboard/0.5.10/logboard-0.5.10.tar.gz

---
+++ packages/logboard/logboard-0.5.10.tar.gz/logboard-0.5.10/logboard/cli/logtable.py
@@ -0,0 +1,129 @@
+# flake8: noqa
+
+import argparse
+import os.path as osp
+import re
+import sys
+import textwrap
+import warnings
+
+import tabulate
+
+from ..parser import parse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        '--logdir',
+        '-l',
+        default='logs',
+        help='logs dir',
+    )
+    parser.add_argument(
+        '--filter',
+        '-f',
+        nargs='+',
+        default=[],
+        help='filter keys',
+    )
+    parser.add_argument('--keys', action='store_true')
+    parser.add_argument(
+        '--significant-figures',
+        '-s',
+        type=int,
+        default=3,
+        help='significant figures',
+    )
+    parser.add_argument(
+        '--index',
+        '-i',
+        type=int,
+        nargs='+',
+        help='index filtering',
+    )
+    parser.add_argument(
+        '--column',
+        '-c',
+        type=int,
+        default=30,
+        help='maximum column width (0 for infinite)',
+    )
+    parser.add_argument(
+        '--multi-column',
+        action='store_true',
+        help='prefer multi-column to vertically shorten the table',
+    )
+    args = parser.parse_args()
+
+    float_format = '{:.%dg}' % args.significant_figures
+
+    root_dir = osp.abspath(args.logdir)
+    df, summary_keys, _, log_keys = parse(root_dir, float_format=float_format)
+
+    if args.index is not None:
+        df = df.iloc[args.index]
+
+    print(' * Log directory: {}'.format(args.logdir))
+
+    summary_keys = ['log_dir'] + summary_keys
+
+    summary_keys_ = []
+    for key in summary_keys:
+        for pattern in args.filter:
+            if re.match(pattern, key):
+                break
+        else:
+            summary_keys_.append(key)
+    summary_keys = summary_keys_
+
+    if args.keys:
+        for key in summary_keys:
+            print(key)
+        sys.exit(0)
+
+    headers = ['']
+    for key in summary_keys:
+        key_splits = key.replace('/', '/\n').split('_', '_\n').splitlines()
+        if args.column:
+            key_splits = [
+                x for key_split in key_splits
+                for x in textwrap.wrap(key_split, width=args.column)
+            ]
+        key = '\n'.join(key_splits)
+        headers.append(key)
+        if args.multi_column and key in log_keys:
+            headers.append('')
+
+    if df.empty:
+        df.columns = summary_keys
+
+    rows = []
+    for index, df_row in df[summary_keys].iterrows():
+        row = [index]
+        for key, x in zip(summary_keys, df_row):
+            if isinstance(x, tuple):
+                assert len(x) == 2
+                if args.multi_column:
+                    row.extend(x)
+                else:
+                    x = '\n'.join(str(xi) for xi in x)
+                    row.append(x)
+            else:
+                x = str(x)
+                x = textwrap.wrap(x, width=args.column) if args.column else [x]
+                x = '\n'.join(x)
+                row.append(x)
+        rows.append(row)
+
+    table = tabulate.tabulate(
+        tabular_data=rows,
+        headers=headers,
+        tablefmt='fancy_grid',
+        stralign='center',
+        showindex=False,
+        disable_numparse=True,
+    )
+    print(table)
---
+++ packages/logboard/logboard-0.5.10.tar.gz/logboard-0.5.10/setup.py
@@ -0,0 +1,80 @@
+from __future__ import print_function
+
+import distutils
+import shlex
+import subprocess
+import sys
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = '0.5.10'
+
+
+if sys.argv[1] == 'release':
+    if not distutils.spawn.find_executable('twine'):
+        print(
+            'Please install twine:\n\n\tpip install twine\n',
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    commands = [
+        'git pull origin master',
+        'git tag v{:s}'.format(version),
+        'git push origin master --tag',
+        'python setup.py sdist',
+        'twine upload dist/logboard-{:s}.tar.gz'.format(version),
+    ]
+    for cmd in commands:
+        subprocess.check_call(shlex.split(cmd))
+    sys.exit(0)
+
+
+def get_long_description():
+    with open('README.md') as f:
+        long_description = f.read()
+
+    try:
+        import github2pypi
+
+        return github2pypi.replace_url(
+            slug='wkentaro/logboard', content=long_description
+        )
+    except Exception:
+        return long_description
+
+
+setup(
+    name='logboard',
+    version=version,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['flask', 'pandas', 'tabulate'],
+    author='Kentaro Wada',
+    author_email='www.kentaro.wada@gmail.com',
+    description='Monitor and Compare Logs on Browser.',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    url='http://github.com/wkentaro/logboard',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    entry_points={
+        'console_scripts': [
+            'logboard=logboard.cli.logboard:main',
+            'logtable=logboard.cli.logtable:main',
+        ],
+    },
+)
