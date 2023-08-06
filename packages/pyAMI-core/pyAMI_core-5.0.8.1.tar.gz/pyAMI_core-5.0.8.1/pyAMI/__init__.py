02cbc115aebc85c81cb9677b436ddc2210cba8fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:42 +0000

    Add package pyAMI-core/5.0.8.1/pyAMI_core-5.0.8.1.tar.gz

---
+++ packages/pyAMI-core/pyAMI_core-5.0.8.1.tar.gz/pyAMI_core-5.0.8.1/pyAMI/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import (division, print_function, unicode_literals)
+#############################################################################
+# Author  : Jerome ODIER, Jerome FULACHIER, Fabian LAMBERT, Solveig ALBRAND
+#
+# Email   : jerome.odier@lpsc.in2p3.fr
+#           jerome.fulachier@lpsc.in2p3.fr
+#           fabian.lambert@lpsc.in2p3.fr
+#           solveig.albrand@lpsc.in2p3.fr
+#
+# Version : 5.X.X (2014-2019)
+#
+#############################################################################
+
+try:
+	import pyAMI_atlas
+
+except ImportError:
+    pass
+
+try:
+	import pyAMI_nedm
+
+except ImportError:
+	pass
+
+try:
+	import pyAMI_supernemo
+
+except ImportError:
+	pass
+
+try:
+	import pyAMI_nika2
+
+except ImportError:
+	pass
+
+#############################################################################
