1338059827090c94cb6f0468e774d3a8810d98f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:30 +0000

    Add package charset-normalizer/1.1.0/charset_normalizer-1.1.0.tar.gz

---
+++ packages/charset-normalizer/charset_normalizer-1.1.0.tar.gz/charset_normalizer-1.1.0/charset_normalizer/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+from charset_normalizer.normalizer import CharsetNormalizerMatches, CharsetNormalizerMatch
+from charset_normalizer.unicode import UnicodeRangeIdentify
+from charset_normalizer.probe_chaos import ProbeChaos
+from charset_normalizer.probe_coherence import ProbeCoherence
+from charset_normalizer.probe_words import ProbeWords
+from charset_normalizer.legacy import detect
