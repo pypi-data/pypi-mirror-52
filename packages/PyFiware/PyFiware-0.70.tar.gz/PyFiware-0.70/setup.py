746896d0d784a2f59f3dc3a67bd2b1043c499b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:06 +0000

    Add package PyFiware/0.70/PyFiware-0.70.tar.gz

---
+++ packages/PyFiware/PyFiware-0.70.tar.gz/PyFiware-0.70/setup.py
@@ -0,0 +1,42 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+from setuptools import setup
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+
+    name='PyFiware',
+    version='0.70',
+    description='Python library to access Fiware REST API v2',
+
+    long_description=long_description,
+    url='https://github.com/josubg/pyfiware',
+    author='Josu Bermudez',
+    author_email='josu.bermudez@deusto.es',
+
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[  # Optional
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+    packages=['pyfiware'],
+    install_requires=['urllib3'],
+)
