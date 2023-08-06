c12ff0344b5e7791d69dc92d9a2fc5d91d18ff9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:12 +0000

    Add package pydplace/2.2.0/pydplace-2.2.0.tar.gz

---
+++ packages/pydplace/pydplace-2.2.0.tar.gz/pydplace-2.2.0/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pydplace',
+    version='2.2.0',
+    license='Apache 2.0',
+    description='programmatic access to D-PLACE/dplace-data',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+    author='Robert Forkel',
+    author_email='forkel@shh.mpg.de',
+    url='https://d-place.org',
+    keywords='data',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    python_requires='>=3.5',
+    install_requires=[
+        'attrs>=18.1',
+        'clldutils>=2.7.0',
+        'csvw>=1.4.2',
+        'pyglottolog>=1.2.1',
+        'python-nexus>=1.63',
+        'pycldf>=1.0.6',
+        'ete3>=3.0.0b34',
+        #'pygdal>=1.11.3.3',
+        #'fiona',
+        #'shapely',
+    ],
+    extras_require={
+        'dev': ['flake8', 'wheel', 'twine'],
+        'test': [
+            'mock',
+            'pytest>=3.6',
+            'pytest-mock',
+            'pytest-cov',
+            'coverage>=4.2',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'dplace=pydplace.__main__:main',
+        ]
+    },
+)
