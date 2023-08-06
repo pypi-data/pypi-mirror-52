5174fddf5bc262913b948dd2f245e08cc92801e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:05 +0000

    Add package itu-r-468-weighting/1.1.1/itu-r-468-weighting-1.1.1.tar.gz

---
+++ packages/itu-r-468-weighting/itu-r-468-weighting-1.1.1.tar.gz/itu-r-468-weighting-1.1.1/README.md
@@ -0,0 +1,51 @@
+# itu-r-468-weighting
+
+A zero dependency Python ITU-R 468 noise weighting filter (1 kHz and 2 kHz)
+
+Master branch: [![Build Status](https://travis-ci.com/cinelexi/itu-r-468-weighting.svg?branch=master)](https://travis-ci.com/cinelexi/itu-r-468-weighting?branch=master) [![Coverage Status](https://coveralls.io/repos/github/cinelexi/itu-r-468-weighting/badge.svg?branch=master)](https://coveralls.io/github/cinelexi/itu-r-468-weighting?branch=master) \
+Dev branch: [![Build Status](https://travis-ci.com/cinelexi/itu-r-468-weighting.svg?branch=dev)](https://travis-ci.com/cinelexi/itu-r-468-weighting?branch=dev) [![Coverage Status](https://coveralls.io/repos/github/cinelexi/itu-r-468-weighting/badge.svg?branch=dev)](https://coveralls.io/github/cinelexi/itu-r-468-weighting?branch=dev)
+
+## Introduction
+
+This project consists of a sole function named `r468`. The function takes a frequency value and returns a weighted gain value. For weightening, the [ITU-R BS.468-4](https://www.itu.int/rec/R-REC-BS.468-4-198607-I/en) standard and the [SMPTE RP 2054:2010](https://ieeexplore.ieee.org/document/7290513) recommended practice are followed. The returned gain value is implicitly returned as a weighted gain value in dB, but can explicitly be returned as a weighted gain factor.
+
+The math for this project is taken from Wikipedia (as of 2019-08-08):
+
+- https://en.wikipedia.org/wiki/ITU-R_468_noise_weighting ([archived version](https://web.archive.org/web/20190808084536/https:/en.wikipedia.org/wiki/ITU-R_468_noise_weighting))
+
+### Plot of filter in frequency|gain (in dB) domain:
+
+![](https://raw.githubusercontent.com/cinelexi/itu-r-468-weighting/master/images/filter_gain_db.png)
+
+### Plot of filter in frequency|gain domain:
+
+![](https://raw.githubusercontent.com/cinelexi/itu-r-468-weighting/master/images/filter_gain.png)
+
+## Installation
+
+```
+pip install itu-r-468-weighting
+```
+
+More infos on the [project page](https://pypi.org/project/itu-r-468-weighting/) at PyPI.
+
+## Example Usage
+
+```
+from itu_r_468_weighting.filter import r468
+
+r468(1000, "1khz")          # returns gain in dB
+r468(1000, "2khz")          # returns gain in dB
+r468(1000, "1khz", "db")    # returns gain in dB
+r468(1000, "2khz", "db")    # returns gain in dB
+r468(1000, "1khz", "norm")  # returns gain factor
+r468(1000, "2khz", "norm")  # returns gain factor
+```
+
+## Function Description
+
+See docstring in [function `r468`](https://github.com/cinelexi/itu-r-468-weighting/blob/master/itu_r_468_weighting/filter.py) for further description.
+
+## Developement
+
+This project is hosted on [GitHub](https://github.com/cinelexi/itu-r-468-weighting).
---
+++ packages/itu-r-468-weighting/itu-r-468-weighting-1.1.1.tar.gz/itu-r-468-weighting-1.1.1/itu_r_468_weighting/filter.py
@@ -0,0 +1,93 @@
+from math import inf
+from math import log10
+from math import sqrt
+
+from itu_r_468_weighting.constants import (
+    DB_GAIN_1KHZ,
+    DB_GAIN_2KHZ,
+    NORM_GAIN_1KHZ,
+    NORM_GAIN_2KHZ,
+)
+
+
+def r468(frequency_hz, khz_option, returns="db"):
+    """Takes a frequency value and returns a weighted gain value.
+
+    For weightening, the ITU-R BS.468-4 standard and the
+    SMPTE RP 2054:2010 recommended practice are followed.
+
+    Parameters
+    ----------
+    frequency_hz : float
+        The frequency value (in Hz) must be a value greater 0.
+    khz_option : str
+        Choose `1khz` or `2khz` as an weighting option.
+        The weighting curves have a similar characteristic for both options,
+        but are using a different amplification factor.
+        They are shifted in a way, that the gain is 0.0 dB
+        at the given frequency (1 or 2 kHz).
+    returns : str
+        Choose `db` or `norm` as option. `db` will output the
+        weighted gain value in dB. `norm` will output the weighted
+        gain value as a factor.
+
+    Returns
+    -------
+    float
+        The weighted value of the frequency.
+
+    Raises
+    ------
+    ValueError
+        If parameter `frequency_hz` is not greater 0.
+        If parameter `khz_option` is not equal to `1khz` or `2khz`.
+    """
+
+    if frequency_hz > 0:
+        pass
+    elif frequency_hz == 0:
+        return inf
+    else:
+        raise ValueError
+
+    f1, f2, f3, f4, f5, f6 = (
+        frequency_hz,
+        frequency_hz ** 2,
+        frequency_hz ** 3,
+        frequency_hz ** 4,
+        frequency_hz ** 5,
+        frequency_hz ** 6,
+    )
+    h1 = (
+        (-4.7373389813783836e-24 * f6)
+        + (2.0438283336061252e-15 * f4)
+        - (1.363894795463638e-07 * f2)
+        + 1
+    )
+    h2 = (
+        (1.3066122574128241e-19 * f5)
+        - (2.1181508875186556e-11 * f3)
+        + (0.0005559488023498643 * f1)
+    )
+
+    if returns == "db":
+        r_itu = (0.0001246332637532143 * f1) / sqrt(h1 ** 2 + h2 ** 2)
+        if khz_option == "1khz":
+            gain = DB_GAIN_1KHZ
+            return gain + 20 * log10(r_itu)
+        elif khz_option == "2khz":
+            gain = DB_GAIN_2KHZ
+            return gain + 20 * log10(r_itu)
+        else:
+            raise ValueError
+    elif returns == "norm":
+        if khz_option == "1khz":
+            gain = NORM_GAIN_1KHZ
+            return gain * 0.0001246332637532143 * f1 / sqrt(h1 ** 2 + h2 ** 2)
+        elif khz_option == "2khz":
+            gain = NORM_GAIN_2KHZ
+            return gain * 0.0001246332637532143 * f1 / sqrt(h1 ** 2 + h2 ** 2)
+        else:
+            raise ValueError
+    else:
+        raise ValueError
