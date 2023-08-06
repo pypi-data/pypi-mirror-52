e4d8d6568d3a87176635cc23ae04ad0c86a41092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:08 +0000

    Add package minet/0.8.1/minet-0.8.1.tar.gz

---
+++ packages/minet/minet-0.8.1.tar.gz/minet-0.8.1/minet/cli/crowdtangle/utils.py
@@ -0,0 +1,217 @@
+# =============================================================================
+# Minet CrowdTangle CLI Utils
+# =============================================================================
+#
+# Miscellaneous generic functions used throughout the CrowdTangle actions.
+#
+import csv
+import sys
+import json
+from datetime import date, timedelta
+from tqdm import tqdm
+from ural import get_domain_name, normalize_url
+from urllib3 import Timeout
+
+from minet.utils import create_safe_pool, fetch, rate_limited
+from minet.cli.utils import print_err
+from minet.cli.crowdtangle.constants import CROWDTANGLE_DEFAULT_RATE_LIMIT
+
+URL_REPORT_HEADERS = [
+    'post_ct_id',
+    'post_id',
+    'account_ct_id',
+    'account_name',
+    'account_handle',
+    'original_url',
+    'url',
+    'normalized_url',
+    'domain_name'
+]
+
+
+def format_url_for_csv(url_item, post):
+    account = post['account']
+
+    url = url_item['expanded']
+
+    return [
+        post['id'],
+        post.get('platformId', ''),
+        account['id'],
+        account['name'],
+        account.get('handle', ''),
+        url_item['original'],
+        url,
+        normalize_url(url, strip_trailing_slash=True),
+        get_domain_name(url)
+    ]
+
+
+def day_range(end):
+    day_delta = timedelta(days=1)
+
+    start_date = date(*[int(i) for i in end.split('-')])
+    current_date = date.today()
+
+    while start_date != current_date:
+        end_date = current_date
+        current_date -= day_delta
+
+        yield current_date.isoformat(), end_date.isoformat()
+
+
+def create_paginated_action(url_forge, csv_headers, csv_formatter,
+                            item_name, item_key):
+
+    def action(namespace, output_file):
+        http = create_safe_pool(timeout=Timeout(connect=10, read=60))
+
+        url_report_writer = None
+
+        if getattr(namespace, 'url_report', False):
+            url_report_writer = csv.writer(namespace.url_report)
+            url_report_writer.writerow(URL_REPORT_HEADERS)
+
+        partition_strategy = getattr(namespace, 'partition_strategy', None)
+        partition_range = None
+
+        if partition_strategy == 'day':
+            partition_range = day_range(namespace.start_date)
+
+            start_date, end_date = next(partition_range, (None,))
+            namespace.start_date = start_date
+            namespace.end_date = end_date
+
+        N = 0
+        url = url_forge(namespace)
+
+        has_limit = bool(namespace.limit)
+
+        print_err('Using the following starting url:')
+        print_err(url)
+        print_err()
+
+        # Loading bar
+        loading_bar = tqdm(
+            desc='Fetching %s' % item_name,
+            dynamic_ncols=True,
+            unit=' %s' % item_name,
+            total=namespace.limit
+        )
+
+        if partition_strategy == 'day':
+            loading_bar.set_postfix(day=namespace.start_date)
+
+        if namespace.format == 'csv':
+            writer = csv.writer(output_file)
+            writer.writerow(csv_headers(namespace) if callable(csv_headers) else csv_headers)
+
+        rate_limit = namespace.rate_limit if namespace.rate_limit else CROWDTANGLE_DEFAULT_RATE_LIMIT
+        rate_limited_fetch = rate_limited(rate_limit, 60.0)(fetch)
+
+        while True:
+            err, result = rate_limited_fetch(http, url)
+
+            # Debug
+            if err is not None:
+                print_err(url)
+                raise err
+
+            if result.status == 401:
+                loading_bar.close()
+
+                # TODO: refacto this with `minet.cli.utils.die`
+                print_err('Your API token is invalid.')
+                print_err('Check that you indicated a valid one using the `--token` argument.')
+                sys.exit(1)
+
+            if result.status >= 400:
+                loading_bar.close()
+
+                print_err(result.data, result.status)
+                sys.exit(1)
+
+            try:
+                data = json.loads(result.data)['result']
+            except:
+                loading_bar.close()
+                print_err('Misformatted JSON result.')
+                sys.exit(1)
+
+            if item_key not in data or len(data[item_key]) == 0:
+                if partition_strategy is None:
+                    break
+
+                start_date, end_date = next(partition_range, (None, None))
+
+                if start_date is None:
+                    break
+
+                namespace.start_date = start_date
+                namespace.end_date = end_date
+
+                url = url_forge(namespace)
+
+                loading_bar.set_postfix(date=start_date)
+
+                continue
+
+            items = data[item_key]
+            enough_to_stop = False
+
+            for item in items:
+                N += 1
+
+                # TODO: could be done with the count instead
+                loading_bar.update()
+
+                if namespace.format == 'jsonl':
+                    output_file.write(json.dumps(item, ensure_ascii=False) + '\n')
+                else:
+                    writer.writerow(csv_formatter(namespace, item))
+
+                if url_report_writer:
+                    urls = item.get('expandedLinks')
+
+                    if urls:
+                        for url_item in urls:
+                            url_report_writer.writerow(format_url_for_csv(url_item, item))
+
+                if has_limit and N >= namespace.limit:
+                    enough_to_stop = True
+                    break
+
+            # NOTE: I wish I had labeled loops in python...
+            if enough_to_stop:
+                loading_bar.close()
+                print_err('The indicated limit of %s was reached.' % item_name)
+                break
+
+            # Pagination
+            # NOTE: we could adjust the `count` GET param but I am lazy
+            pagination = data['pagination']
+
+            if 'nextPage' not in pagination:
+                if partition_strategy is None:
+                    break
+
+                start_date, end_date = next(partition_range, (None, None))
+
+                if start_date is None:
+                    break
+
+                namespace.start_date = start_date
+                namespace.end_date = end_date
+
+                url = url_forge(namespace)
+
+                loading_bar.set_postfix(date=start_date)
+
+                continue
+
+            url = pagination['nextPage']
+
+        loading_bar.close()
+        print_err('We reached the end of pagination.')
+
+    return action
---
+++ packages/minet/minet-0.8.1.tar.gz/minet-0.8.1/minet/utils.py
@@ -0,0 +1,205 @@
+# =============================================================================
+# Minet Utils
+# =============================================================================
+#
+# Miscellaneous helper function used throughout the library.
+#
+import re
+import chardet
+import cgi
+import certifi
+import browser_cookie3
+import urllib3
+import threading
+import time
+from functools import wraps
+from urllib3.exceptions import ClosedPoolError, HTTPError
+from urllib.request import Request
+
+from minet.defaults import (
+    DEFAULT_CONNECT_TIMEOUT,
+    DEFAULT_READ_TIMEOUT,
+    DEFAULT_SPOOFED_UA
+)
+
+# Handy regexes
+CHARSET_RE = re.compile(rb'<meta.*?charset=["\']*(.+?)["\'>]', flags=re.I)
+PRAGMA_RE = re.compile(rb'<meta.*?content=["\']*;?charset=(.+?)["\'>]', flags=re.I)
+XML_RE = re.compile(rb'^<\?xml.*?encoding=["\']*(.+?)["\'>]')
+
+# Constants
+CHARDET_CONFIDENCE_THRESHOLD = 0.9
+
+
+def guess_encoding(response, data, is_xml=False, use_chardet=False):
+    """
+    Function taking an urllib3 response object and attempting to guess its
+    encoding.
+    """
+    content_type_header = response.getheader('content-type')
+
+    if content_type_header is not None:
+        parsed_header = cgi.parse_header(content_type_header)
+
+        if len(parsed_header) > 1:
+            charset = parsed_header[1].get('charset')
+
+            if charset is not None:
+                return charset.lower()
+
+    # TODO: use re.search to go faster!
+    if is_xml:
+        matches = re.findall(CHARSET_RE, data)
+
+        if len(matches) == 0:
+            matches = re.findall(PRAGMA_RE, data)
+
+        if len(matches) == 0:
+            matches = re.findall(XML_RE, data)
+
+        # NOTE: here we are returning the last one, but we could also use
+        # frequency at the expense of performance
+        if len(matches) != 0:
+            return matches[-1].lower().decode()
+
+    if use_chardet:
+        chardet_result = chardet.detect(data)
+
+        if chardet_result['confidence'] >= CHARDET_CONFIDENCE_THRESHOLD:
+            return chardet_result['encoding'].lower()
+
+    return None
+
+
+class CookieResolver(object):
+    def __init__(self, jar):
+        self.jar = jar
+
+    def __call__(self, url):
+        req = Request(url)
+        self.jar.add_cookie_header(req)
+
+        return req.get_header('Cookie') or None
+
+
+def grab_cookies(browser='firefox'):
+    if browser == 'firefox':
+        try:
+            return CookieResolver(browser_cookie3.firefox())
+        except:
+            return None
+
+    if browser == 'chrome':
+        try:
+            return CookieResolver(browser_cookie3.chrome())
+        except:
+            return None
+
+    raise Exception('minet.utils.grab_cookies: unknown "%s" browser.' % browser)
+
+
+def dict_to_cookie_string(d):
+    return '; '.join('%s=%s' % r for r in d.items())
+
+
+DEFAULT_URLLIB3_TIMEOUT = urllib3.Timeout(connect=DEFAULT_CONNECT_TIMEOUT, read=DEFAULT_READ_TIMEOUT)
+
+
+def create_safe_pool(timeout=None, **kwargs):
+    """
+    Helper function returning a urllib3 pool manager with sane defaults.
+    """
+
+    timeout = kwargs['timeout'] if 'timeout' in kwargs else DEFAULT_URLLIB3_TIMEOUT
+
+    return urllib3.PoolManager(
+        cert_reqs='CERT_REQUIRED',
+        ca_certs=certifi.where(),
+        timeout=timeout,
+        **kwargs
+    )
+
+
+def fetch(http, url, method='GET', headers=None, cookie=None, spoof_ua=True):
+    """
+    Generic fetch helpers using a urllib3 pool to access some resource.
+    """
+
+    # Formatting headers
+    final_headers = {}
+
+    if spoof_ua:
+        final_headers['User-Agent'] = DEFAULT_SPOOFED_UA
+
+    if cookie:
+        if not isinstance(cookie, str):
+            cookie = dict_to_cookie_string(cookie)
+
+        final_headers['Cookie'] = cookie
+
+    # Note: headers passed explicitly by users always win
+    if headers is not None:
+        final_headers.update(headers)
+
+    # Performing request
+    try:
+        result = http.request(method, url, headers=final_headers)
+    except (ClosedPoolError, HTTPError) as e:
+        return e, None
+
+    return None, result
+
+
+# TODO: probably add a context manager to avoid slowing down because of FS I/O
+# There is an example of this in the crowdtangle.utils loop
+def rate_limited(max_per_period, period=1.0):
+    """
+    Thread-safe rate limiting decorator.
+    From: https://gist.github.com/gregburek/1441055
+
+    Note that this version of the function takes running time of the function
+    into account.
+
+    Args:
+        max_per_period (int): Maximum number of call per period.
+        period (float): Period in seconds. Defaults to 1.0.
+
+    Returns:
+        callable: Decorator.
+
+    """
+    max_per_second = max_per_period / period
+
+    lock = threading.Lock()
+    min_interval = 1.0 / max_per_second
+
+    def decorate(func):
+        last_time_called = time.perf_counter()
+        first_call = True
+
+        @wraps(func)
+        def rate_limited_function(*args, **kwargs):
+            lock.acquire()
+            nonlocal last_time_called
+            nonlocal first_call
+
+            fn_time = 0.0
+            before = None
+
+            try:
+                elapsed = time.perf_counter() - last_time_called
+                left_to_wait = min_interval - elapsed
+
+                if left_to_wait > 0 and not first_call:
+                    time.sleep(left_to_wait)
+
+                before = time.perf_counter()
+                return func(*args, **kwargs)
+            finally:
+                first_call = False
+                last_time_called = before if before is not None else time.perf_counter()
+                lock.release()
+
+        return rate_limited_function
+
+    return decorate
