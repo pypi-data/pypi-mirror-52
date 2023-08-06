686245f614f524811cc4b0f50a19bd9f24abae19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:18 +0000

    Add package findMin/9.0.0/findMin-9.0.0.tar.gz

---
+++ packages/findMin/findMin-9.0.0.tar.gz/findMin-9.0.0/find_min/tests/test_find_min.py
@@ -0,0 +1,28 @@
+#
+# Unit tests for class FindMin
+#
+
+import unittest
+from find_min.find_min import FindMin
+from find_min.sample.sample_file import SampleFile 
+
+
+class FindMinTests(unittest.TestCase):
+
+
+    def run_all(self):
+        sf = SampleFile()
+        samples = sf.load()
+
+
+    def test_equal(self, samples):
+        fm = FindMin()
+
+        for sample in samples.values():
+            result = fm.findMin(sample["sample"])
+            self.assertEqual(result, sample["output"])
+            print("✓ Test passed for sample {0}".format(sample["sample"]))
+
+
+if __name__ == '__main__':
+    unittest.main()
