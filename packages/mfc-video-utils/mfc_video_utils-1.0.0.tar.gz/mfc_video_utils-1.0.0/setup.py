195ee2a21d086b084bec2ee88dd3c5781a2f76c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:24 +0000

    Add package mfc-video-utils/1.0.0/mfc_video_utils-1.0.0.tar.gz

---
+++ packages/mfc-video-utils/mfc_video_utils-1.0.0.tar.gz/mfc_video_utils-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='mfc_video_utils',
+      version='1.0.0',
+      description='Tools to process videos for the MFC Evaluations',
+      author='David Güera, Sriram Baireddy',
+      author_email='dgueraco@purdue.edu, sbairedd@purdue.edu',
+      license='MIT',
+      packages=['mfc_video_utils'],
+      zip_safe=False)
