9774bdb8b672e42a51cb49952f6f5c08ad4df99c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:20 +0000

    Add package useful-decoration/1.0.6/useful_decoration-1.0.6.tar.gz

---
+++ packages/useful-decoration/useful_decoration-1.0.6.tar.gz/useful_decoration-1.0.6/useful_decoration/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*- 
+"""
+@User     : Frank
+@File     : __init__.py
+@DateTime : 2019-09-16 11:40 
+@Email    : frank.chang@lexisnexis.com
+"""
+
+__name__ = 'useful_decoration'
+__version__ = '1.0.6'
+__author__ = 'frank'
+
+if __name__ == '__main__':
+    pass
