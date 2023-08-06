927b380e625496d09cddf8de08424d3a842f46e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:26 +0000

    Add package TWT/0.0.1/TWT-0.0.1.tar.gz

---
+++ packages/TWT/TWT-0.0.1.tar.gz/TWT-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md",'r')as fh:
+	long_description =fh.read()
+
+setuptools.setup(
+	name="TWT",
+	version="0.0.1",
+	author="Hunter-Ou",
+	author_email="oy199603@qq.com",
+	description="Dedicated in the SAR",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/UCAS-BigBird/TWT",
+	packages=setuptools.find_packages(),
+	python_requires='>=3.6',
+)
