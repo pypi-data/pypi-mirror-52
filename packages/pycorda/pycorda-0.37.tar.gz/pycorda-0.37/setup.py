f91015212bf4f0c908bde5d5d8fd47cb8e29b53a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:43 +0000

    Add package pycorda/0.37/pycorda-0.37.tar.gz

---
+++ packages/pycorda/pycorda-0.37.tar.gz/pycorda-0.37/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+	name='pycorda',
+	version='0.37',
+	author='Jamiel Sheikh',
+	packages=['pycorda'],
+	install_requires=[
+		'jaydebeapi',
+		'pandas',
+		'matplotlib',
+		'datetime',
+		'requests',
+		'pyjks'
+
+	],
+	include_package_data=True,
+)
