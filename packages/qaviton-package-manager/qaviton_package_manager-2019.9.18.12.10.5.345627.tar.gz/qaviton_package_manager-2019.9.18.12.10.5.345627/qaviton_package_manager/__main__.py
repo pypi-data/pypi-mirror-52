97dc677121a0c51cddbefb7f64347bce27612b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:56 +0000

    Add package qaviton-package-manager/2019.9.18.12.10.5.345627/qaviton_package_manager-2019.9.18.12.10.5.345627.tar.gz

---
+++ packages/qaviton-package-manager/qaviton_package_manager-2019.9.18.12.10.5.345627.tar.gz/qaviton_package_manager-2019.9.18.12.10.5.345627/qaviton_package_manager/__main__.py
@@ -0,0 +1,28 @@
+# Copyright 2019 qaviton.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# https://github.com/qaviton/qaviton_package_manager/blob/master/LICENSE
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+
+def main():
+    from os import getcwd, path, sep
+    pkg = getcwd()+sep+'package.py'
+    if path.exists(pkg):
+        manager = __import__(pkg, globals(), locals(), ['manager'], 0)
+        manager.run()
+        return manager
+    else:
+        from qaviton_package_manager.manager import Manager
+        return Manager().run()
+
+
+if __name__ == '__main__':
+    main()
