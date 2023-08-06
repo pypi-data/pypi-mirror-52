09458843cca6d050524a01c00c685d635daa7aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:03 +0000

    Add package spikeextractors/0.6.0/spikeextractors-0.6.0.tar.gz

---
+++ packages/spikeextractors/spikeextractors-0.6.0.tar.gz/spikeextractors-0.6.0/spikeextractors/extractors/numpyextractors/numpyextractors.py
@@ -0,0 +1,86 @@
+from spikeextractors import RecordingExtractor
+from spikeextractors import SortingExtractor
+from pathlib import Path
+import numpy as np
+
+
+class NumpyRecordingExtractor(RecordingExtractor):
+    extractor_name = 'NumpyRecordingExtractor'
+    is_writable = True
+    def __init__(self, timeseries, sampling_frequency, geom=None):
+        if isinstance(timeseries, str):
+            if Path(timeseries).is_file():
+                self._timeseries = np.load(timeseries)
+        elif isinstance(timeseries, np.ndarray):
+            self._timeseries = timeseries
+        else:
+            raise TypeError("'timeseries must be a .npy file name or a numpy array")
+        RecordingExtractor.__init__(self)
+        self._sampling_frequency = float(sampling_frequency)
+        self._geom = geom
+        if geom is not None:
+            for m in range(self._timeseries.shape[0]):
+                self.set_channel_property(m, 'location', self._geom[m, :])
+
+    def get_channel_ids(self):
+        return list(range(self._timeseries.shape[0]))
+
+    def get_num_frames(self):
+        return self._timeseries.shape[1]
+
+    def get_sampling_frequency(self):
+        return self._sampling_frequency
+
+    def get_traces(self, channel_ids=None, start_frame=None, end_frame=None):
+        if start_frame is None:
+            start_frame = 0
+        if end_frame is None:
+            end_frame = self.get_num_frames()
+        if channel_ids is None:
+            channel_ids = self.get_channel_ids()
+        recordings = self._timeseries[:, start_frame:end_frame][channel_ids, :]
+        return recordings
+
+    @staticmethod
+    def write_recording(recording, save_path):
+        save_path = Path(save_path)
+        np.save(save_path, recording.get_traces())
+
+
+class NumpySortingExtractor(SortingExtractor):
+    extractor_name = 'NumpySortingExtractor'
+    is_writable = False
+    def __init__(self):
+        SortingExtractor.__init__(self)
+        self._units = {}
+
+    def load_from_extractor(self, sorting):
+        ids = sorting.get_unit_ids()
+        for id in ids:
+            self.add_unit(id, sorting.get_unit_spike_train(id))
+        if sorting.get_sampling_frequency() is not None:
+            self.set_sampling_frequency(sorting.get_sampling_frequency())
+
+    def set_sampling_frequency(self, sampling_frequency):
+        self._sampling_frequency = sampling_frequency
+
+    def set_times_labels(self, times, labels):
+        units = np.sort(np.unique(labels))
+        for unit in units:
+            times0 = times[np.where(labels == unit)[0]]
+            self.add_unit(unit_id=int(unit), times=times0)
+
+    def add_unit(self, unit_id, times):
+        self._units[unit_id] = dict(times=times)
+
+    def get_unit_ids(self):
+        return list(self._units.keys())
+
+    def get_unit_spike_train(self, unit_id, start_frame=None, end_frame=None):
+        if start_frame is None:
+            start_frame = 0
+        if end_frame is None:
+            end_frame = np.Inf
+        times = self._units[unit_id]['times']
+        inds = np.where((start_frame <= times) & (times < end_frame))[0]
+        return np.rint(times[inds]).astype(int)
