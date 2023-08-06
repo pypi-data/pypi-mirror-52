3505257e47e7701a8a7631d691b8f0c93f072094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:30 +0000

    Add package metavariant/0.1.3/metavariant-0.1.3.tar.gz

---
+++ packages/metavariant/metavariant-0.1.3.tar.gz/metavariant-0.1.3/metavariant/__init__.py
@@ -0,0 +1,5 @@
+from .lvg import VariantLVG, VariantComponents, Variant
+from .lovd import LOVDVariantsForGene
+
+__version__='0.1.0'
+
---
+++ packages/metavariant/metavariant-0.1.3.tar.gz/metavariant-0.1.3/metavariant/exceptions.py
@@ -0,0 +1,33 @@
+"""Provides metavariant specific exceptions."""
+
+class MetaVariantError(Exception):
+    """ Generic exception class from which all metavariant exceptions are subclassed. """
+
+
+class RejectedSeqVar(MetaVariantError):
+    """ Indicates a sequence variant that cannot be considered functionally
+    useful for the purposes of lookup in databases (pubtator, etc).
+
+    Message should contain information about why the SequenceVariant fails.
+    """
+    pass
+
+
+class CriticalHgvsError(MetaVariantError):
+    """ Raised when a SequenceVariant cannot be parsed from the input hgvs_text; should be used only in cases
+        where failure to create a SequenceVariant means a complete (critical) failure to proceed with LVG.
+
+        Message should contain information passed along from the HgvsParseError.
+
+        e.g. "HGVSParseError(u'NM_004628.4:c.621_622ins83: char 24: Syntax error',)"
+    """
+    pass
+
+
+class LOVDRemoteError(MetaVariantError):
+    """ Raised when response from LOVD contains a not-ok status code.
+    Error message should contain status code. 
+    """
+    pass
+
+
