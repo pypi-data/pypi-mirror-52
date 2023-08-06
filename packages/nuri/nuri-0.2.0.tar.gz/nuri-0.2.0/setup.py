26b05a0d637867f29da2d2eeb2be094ca4efbb29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:07 +0000

    Add package nuri/0.2.0/nuri-0.2.0.tar.gz

---
+++ packages/nuri/nuri-0.2.0.tar.gz/nuri-0.2.0/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from distutils.core import setup
+import os,nuri.__init__
+from glob import glob
+
+#del os.link
+setup(
+    name="nuri",
+    version=nuri.__version__,
+    author="Vincent Dumont",
+    author_email="vincentdumont@gmail.com",
+    packages=["nuri"],
+    dependency_links=['git+https://gitlab.com/citymag/analysis/mlpy.git'],
+    scripts = glob('bin/*'),
+    url="http://citymag.gitlab.io/nuri/",
+    description="Urban Magnetometry Software"
+)
