006cbc40b66e08248e4877f4ae4028d56c378d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:43 +0000

    Add package guess-logo/0.0.6/guess_logo-0.0.6.tar.gz

---
+++ packages/guess-logo/guess_logo-0.0.6.tar.gz/guess_logo-0.0.6/README.md
@@ -0,0 +1,29 @@
+# guess_logo
+猜测网址的logo链接
+Detection of logo url from any website.
+
+# How to install.
+```
+pip3 install guess_logo
+```
+
+# How to use.
+
+```
+from guess_logo import GuessLogo
+
+
+url = 'http://www.txdkj.com/'
+
+
+logos = GuessLogo.guess(url)
+print(logos)
+
+# 调整logo大小
+from guess_logo import Logo
+height = 256
+width = 512
+source = 'demo/ifengLogo.png'
+target = 'demo/ifengLogo_{}_{}.png'.format(width, height)
+Logo.resize(source, target, width, height, bgcolor='transparent')
+```
---
+++ packages/guess-logo/guess_logo-0.0.6.tar.gz/guess_logo-0.0.6/guess_logo/guess_logo.py
@@ -0,0 +1,69 @@
+import time
+from selenium import webdriver
+from collections import Counter
+
+
+
+class GuessLogo:
+
+    option = webdriver.ChromeOptions()
+    option.add_argument("headless")
+
+    @staticmethod
+    def guess_logo_by_favicon(browser):
+        try:
+            item = browser.find_element_by_xpath("//link[contains(@href, 'favicon')]")
+            return item.get_attribute('href')
+        except Exception:
+            return None
+        
+
+    @staticmethod
+    def guess_logo_by_img_url(browser):
+        try:
+            patterns = [
+                "contains(@*, 'logo')",
+                "contains(@class, 'logo')",
+                "contains(@*, 'Logo')",
+                "contains(@class, 'Logo')",
+            ]
+            xpath = "//img[%s]" % " or ".join(patterns)
+            item = browser.find_element_by_xpath(xpath)
+            return item.get_attribute('src')
+        except Exception:
+            return None
+
+    @staticmethod
+    def guess_logo_by_parent(browser):
+        try:
+            patterns = [
+                "contains(@*, 'logo')",
+                "contains(@class, 'logo')",
+                "contains(@*, 'Logo')",
+                "contains(@class, 'Logo')",
+            ]
+            xpath = "//a|//div[%s]" % " or ".join(patterns)
+            item = browser.find_element_by_xpath(xpath)
+            return item.find_element_by_xpath('//img').get_attribute('src')
+        except Exception:
+            return None
+
+    @classmethod
+    def guess(cls, url):
+        if not url.startswith("http"):
+            url = 'http://' + url
+
+        browser = webdriver.Chrome(chrome_options=cls.option)
+
+        browser.get(url)
+        time.sleep(3)
+
+        logos = []
+
+        logos.append(cls.guess_logo_by_favicon(browser))
+        logos.append(cls.guess_logo_by_img_url(browser))
+        logos.append(cls.guess_logo_by_parent(browser))
+
+        logos = list(filter(lambda item: item, logos))
+        browser.close()
+        return dict(Counter(logos))
