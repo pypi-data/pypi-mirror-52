213e963c0b1bb24b80e6395c57b4aa3daafca997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:43 +0000

    Add package ErfanIO/0.0.5/ErfanIO-0.0.5.tar.gz

---
+++ packages/ErfanIO/ErfanIO-0.0.5.tar.gz/ErfanIO-0.0.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="ErfanIO",
+    version="0.0.5",
+    packages=find_packages(),
+    url="https://gitlab.com/Azadeh-Afzar/Utility-Development/ErfanIO",
+    license="AGPL-3.0",
+    author="Mohammad Mahdi Baghbani Pourvahid",
+    author_email="MahdiBaghbani@protonmail.com",
+    description="Simple and painless IO library",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "License :: OSI Approved :: zlib/libpng License",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+    ]
+)
