1d37640a75fdb254875ab9c4c48f168920912236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:44 +0000

    Add package samplechintan/0.1.0/samplechintan-0.1.0.tar.gz

---
+++ packages/samplechintan/samplechintan-0.1.0.tar.gz/samplechintan-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='samplechintan',
+    version='0.1.0',
+    url="https://github.com/MarziyaFatema/demo",
+    author="Marziya",
+    author_email="shaikhmarzi05@gmail.com",
+    description="An example of python package.",
+    packages=['samplechintan'],
+    package_data={'samplechintan': ['*.*']},
+)
