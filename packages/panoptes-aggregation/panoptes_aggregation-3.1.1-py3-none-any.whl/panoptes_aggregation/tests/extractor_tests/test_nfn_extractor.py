dc1242b682a495c936e3e6578e1efa758c3a1a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:50 +0000

    Add package panoptes-aggregation/3.1.1/panoptes_aggregation-3.1.1-py3-none-any.whl

---
+++ packages/panoptes-aggregation/panoptes_aggregation-3.1.1-py3-none-any.whl/panoptes_aggregation/tests/extractor_tests/test_nfn_extractor.py
@@ -0,0 +1,123 @@
+from panoptes_aggregation import extractors
+from .base_test_class import ExtractorTest
+
+classification = {
+    "annotations": [
+        {
+            "task": "T10",
+            "value": [
+                {
+                    "value": 1982,
+                    "option": True
+                }
+            ]
+        },
+        {
+            "task": "T1",
+            "value": [
+                {
+                    "value": "United States",
+                    "option": True
+                }
+            ]
+        },
+        {
+            "task": "T3",
+            "value": 5
+        }
+    ],
+    "subject": {
+        "metadata": {}
+    },
+    "metadata": {
+        "utc_offset": "21600",
+    },
+    "created_at": "2019-01-14T19:28:13.667Z",
+}
+
+expected = {
+    "workflow": "herbarium",
+    "decade": "80s",
+    "time": "nightowl",
+    "country": "United States"
+}
+
+TestNfN = ExtractorTest(
+    extractors.nfn_extractor,
+    classification,
+    expected,
+    'Test NfN with year and country tasks specified done at night',
+    kwargs={'year': 'T10', 'workflow': 'herbarium', 'country': "T1"}
+)
+
+classification_0 = {
+    "annotations": [{
+        "task": "T99",
+        "value": [
+            {
+                "task": "T11",
+                "value": [
+                    {
+                        "value": 2001,
+                        "option": True
+                    }
+                ]
+            }
+        ]
+    }],
+    "metadata": {
+        "utc_offset": "21600",
+    },
+    "subject": {
+        "metadata": {
+            "country": "United States",
+        }
+    },
+    "created_at": "2019-04-22T06:28:13.667Z",
+}
+
+expected_0 = {
+    "workflow": "herbarium",
+    "decade": "00s",
+    "time": "lunchbreak",
+    "earth_day": True,
+    "country": "United States"
+}
+
+TestNfNTwo = ExtractorTest(
+    extractors.nfn_extractor,
+    classification_0,
+    expected_0,
+    'Test NfN on Earth Day with year as nested task and country from metadata. At lunchtime, local time.',
+    kwargs={'year': 'T11', 'workflow': 'herbarium', 'country': 'metadata'}
+)
+
+classification_1 = {
+    "annotations": [
+        {
+            "task": "T10",
+            "value": 1976
+        }
+    ],
+    "metadata": {
+        "utc_offset": "21600",
+    },
+    "subject": {
+        "metadata": {}
+    },
+    "created_at": "2019-05-22T06:28:13.667Z",
+}
+
+expected_1 = {
+    "workflow": "herbarium",
+    "decade": "70s",
+    "time": "lunchbreak"
+}
+
+TestNfNThree = ExtractorTest(
+    extractors.nfn_extractor,
+    classification_1,
+    expected_1,
+    'Test NfN bare integer year annotation at lunchtime.',
+    kwargs={'year': 'T10', 'workflow': 'herbarium'}
+)
