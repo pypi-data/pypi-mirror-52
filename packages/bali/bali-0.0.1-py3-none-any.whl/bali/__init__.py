3775549cdf2c657d0fe17f3a2870db493fc040aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:36 +0000

    Add package bali/0.0.1/bali-0.0.1-py3-none-any.whl

---
+++ packages/bali/bali-0.0.1-py3-none-any.whl/bali/__init__.py
@@ -0,0 +1,7 @@
+from .to_string import to_string
+from .do_string import do_string
+
+__all__ = [
+    'do_string',
+    'nana'
+]
---
+++ packages/bali/bali-0.0.1-py3-none-any.whl/bali/do_string.py
@@ -0,0 +1,36 @@
+
+def do_string(original_class):
+
+    def is_primitive(obj):
+        return not hasattr(obj, '__dict__')
+
+    def __str__(self):
+        builder = []
+
+        for att in dir(self):
+            if not att.startswith("_"):
+                if isinstance(self.__getattribute__(att), list) or isinstance(self.__getattribute__(att), tuple):
+                    list_builder = []
+                    for list_item in self.__getattribute__(att):
+                        if not is_primitive(list_item):
+                            list_builder.append(__str__(list_item))
+                        else:
+                            list_builder.append(str(list_item))
+                    builder.append('{' + str(att) + ' = [' + ', '.join(list_builder) + ']}')
+                elif isinstance(self.__getattribute__(att), dict):
+
+                    dict_builder = []
+                    for key, value in self.__getattribute__(att).items():
+                        if not is_primitive(value):
+                            dict_builder.append(__str__(key) + ' : ' + __str__(value))
+                        else:
+                            dict_builder.append(str(key) + ' : ' + str(value))
+                    builder.append('{' + str(att) + ' = ' + ', '.join(dict_builder) + '}')
+                elif not is_primitive(self.__getattribute__(att)):
+                    builder.append('{' + str(att) + ' = ' + __str__(self.__getattribute__(att)) + '}')
+                else:
+                    builder.append('{' + str(att) + ' = ' + str(self.__getattribute__(att)) + '}')
+        return ', '.join(builder)
+
+    original_class.__str__ = __str__
+    return original_class
