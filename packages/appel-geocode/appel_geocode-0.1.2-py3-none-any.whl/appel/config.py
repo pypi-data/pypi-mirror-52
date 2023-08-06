0bc929130e0fd65f31256af243aa9c7e240def96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:36 +0000

    Add package appel-geocode/0.1.2/appel_geocode-0.1.2-py3-none-any.whl

---
+++ packages/appel-geocode/appel_geocode-0.1.2-py3-none-any.whl/appel/config.py
@@ -0,0 +1,23 @@
+from typing import Dict
+
+import numpy
+from typing_extensions import Final
+
+RESOURCES_PATH: Final[str] = "appel/resources/"
+DOTS_PER_CITY_TYPE = Dict[int, int]
+
+DATASETS_TESTS_PATH: Final[str] = RESOURCES_PATH + "/test_points/"
+
+TREE_PATH: Final[str] = 'brazil.bin'
+CITIES_INFO_PATH: Final[str] = RESOURCES_PATH + "cidades_info_2016.csv.xz"
+POLYGONS_PATHS: Final[Dict[str, str]] = {
+    "STATES": RESOURCES_PATH + "estados",
+    "CITIES": RESOURCES_PATH + "cidades",
+    "MESOS": RESOURCES_PATH + "mesos",
+    "MICROS": RESOURCES_PATH + "micros",
+    "CITIES_ATTRS": RESOURCES_PATH + "cities_attrs.bin",
+}
+MAX_STATES: numpy.uint8 = numpy.uint8(27)
+# Simplification values on each polygon
+
+
