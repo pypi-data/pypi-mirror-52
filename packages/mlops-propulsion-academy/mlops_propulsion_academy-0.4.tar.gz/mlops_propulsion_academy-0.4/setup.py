836deb1a72db9cd812b729370e0f5838e2d44308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:55 +0000

    Add package mlops-propulsion-academy/0.4/mlops_propulsion_academy-0.4.tar.gz

---
+++ packages/mlops-propulsion-academy/mlops_propulsion_academy-0.4.tar.gz/mlops_propulsion_academy-0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='mlops_propulsion_academy',
+      version='0.4',
+      description='mlops_propulsion_academy',
+      author='Daniele Roncaglioni',
+      author_email='danieler@propulsionacademy.com',
+      license='MIT',
+      packages=['mlops_propulsion_academy'],
+      zip_safe=False,
+      entry_points={
+          'console_scripts': ['mlops_install=mlops_propulsion_academy.command_line:install',
+                              'mlops_update=mlops_propulsion_academy.command_line:update',
+                              'mlops_start=mlops_propulsion_academy.command_line:start'],
+      }
+      )
