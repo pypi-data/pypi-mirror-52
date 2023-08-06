133d0c1bf874888f94c591c8596c3a249f2f4334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:24 +0000

    Add package snyp/0.1.3/snyp-0.1.3.tar.gz

---
+++ packages/snyp/snyp-0.1.3.tar.gz/snyp-0.1.3/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='snyp',
+    version='0.1.3',
+    author='Bujar Murati',
+    author_email = 'bmurati95@gmail.com',
+    description='Snyp is a command line utility that streamlines the process of creating text based documentation and programming tutorials in Markdown.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    url = 'https://github.com/BujarMurati/snyp',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    py_modules=['snyp'],
+    install_requires=[
+        'Click',
+        'configparser'
+    ],
+    include_package_data=True,
+    package_data = {
+        '':['snyp.ini']
+    },
+    entry_points='''
+        [console_scripts]
+        snyp=snyp:snyp
+    ''',
+)
