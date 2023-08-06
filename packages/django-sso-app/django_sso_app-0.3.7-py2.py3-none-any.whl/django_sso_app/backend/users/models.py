ce1ffe687493410dbb2b57ae28bbc6b8f86f8e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:00 +0000

    Add package django-sso-app/0.3.7/django_sso_app-0.3.7-py2.py3-none-any.whl

---
+++ packages/django-sso-app/django_sso_app-0.3.7-py2.py3-none-any.whl/django_sso_app/backend/users/models.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.urls import reverse
+
+
+if settings.DJANGO_SSO_BACKEND_ENABLED:
+    from django_sso_app.core.apps.users.models import AbstractUserModel
+    from django_sso_app.core.models import UpdatableModel, DeactivableModel
+
+    class User(AbstractUserModel, UpdatableModel, DeactivableModel):
+        pass
+
+else:
+    from django.contrib.auth.models import AbstractUser
+
+    class User(AbstractUser):
+        def get_absolute_url(self):
+            return reverse("users:detail", kwargs={"username": self.username})
