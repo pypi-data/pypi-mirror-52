920205002335f25865b26c0896cf050a5b36e792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:40 +0000

    Add package django-shopify-abandoned-checkout/0.0.4/django-shopify-abandoned-checkout-0.0.4.tar.gz

---
+++ packages/django-shopify-abandoned-checkout/django-shopify-abandoned-checkout-0.0.4.tar.gz/django-shopify-abandoned-checkout-0.0.4/shopify_abandoned_checkout/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import AbandonedCheckout
+
+@admin.register(AbandonedCheckout)
+class AbandonedCheckoutAdmin(admin.ModelAdmin):
+    fields = ('id', 'checkout_id', 'checkout_url', 'user_email', 'email_sent', 'email_errors', 'created_at', 'modified_at')
+    readonly_fields = ('id', 'checkout_id', 'checkout_url', 'user_email', 'email_errors', 'created_at', 'modified_at')
+    list_display = ('checkout_id', 'user_email', 'email_sent')
