6aa1cfe6c3216751204d6f769fed260cf7407ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:39 +0000

    Add package iotedge-compose/0.1.1/iotedge-compose-0.1.1.tar.gz

---
+++ packages/iotedge-compose/iotedge-compose-0.1.1.tar.gz/iotedge-compose-0.1.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="iotedge-compose",
+    version="0.1.1",
+    author="iotedge",
+    author_email="vsciet@outlook.com",
+    description="Convert Docker Compose project to Azure IoT Edge deployment manifest",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/VSChina/compose-to-manifest",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6.0',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    entry_points={
+        "console_scripts": ["iotedge-compose=convertor.convertor:main"],
+    },
+    install_requires=[
+        "docker-compose==1.24.0",
+    ],
+)
