835d5d6d360d16771dffd12e5f0920c0fa5c1eec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:22 +0000

    Add package gym-attitudecontrol/0.0.1/gym_attitudecontrol-0.0.1.tar.gz

---
+++ packages/gym-attitudecontrol/gym_attitudecontrol-0.0.1.tar.gz/gym_attitudecontrol-0.0.1/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='gym_attitudecontrol',
+      version='0.0.1',
+      install_requires=['gym', 'pybullet', 'time', 'numpy']  # And any other dependencies foo needs
+)
