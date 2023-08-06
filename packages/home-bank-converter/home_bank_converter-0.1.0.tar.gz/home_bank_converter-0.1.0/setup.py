56ae9ff0bc75e551fe710257b2c6c45e5d3ec0e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:45 +0000

    Add package home-bank-converter/0.1.0/home_bank_converter-0.1.0.tar.gz

---
+++ packages/home-bank-converter/home_bank_converter-0.1.0.tar.gz/home_bank_converter-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='home_bank_converter',
+    version='0.1.0',
+    packages=['home_bank_converter'],
+    url='',
+    license='',
+    author='Fabian Reister',
+    author_email='',
+    description=''
+)
