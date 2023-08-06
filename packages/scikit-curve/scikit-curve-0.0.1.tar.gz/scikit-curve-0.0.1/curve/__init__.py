443f2c40fa0b6fa1d87a8df02da93d97a9b40e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:56 +0000

    Add package scikit-curve/0.0.1/scikit-curve-0.0.1.tar.gz

---
+++ packages/scikit-curve/scikit-curve-0.0.1.tar.gz/scikit-curve-0.0.1/curve/__init__.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+from ._version import __version__  # noqa
+
+from curve._base import Axis, Point, CurvePoint, Curve
+from curve._distance import known_metrics, get_metric
+from curve._diffgeom import DifferentialGeometryWarning
+from curve._interpolate import (
+    InterpolationError,
+    InterpolationWarning,
+    InterpolationGrid,
+    UniformInterpolationGrid,
+    UniformExtrapolationGrid,
+    PreservedSpeedInterpolationGrid,
+    InterpolatorBase,
+    interp_methods,
+    get_interpolator,
+    register_interpolator,
+)
+from curve._numeric import isequal, allequal, dot1d
+
+__all__ = [
+    # base
+    'Axis',
+    'Point',
+    'CurvePoint',
+    'Curve',
+
+    # distance
+    'known_metrics',
+    'get_metric',
+
+    # diffgeom
+    'DifferentialGeometryWarning',
+
+    # interpolate
+    'InterpolationError',
+    'InterpolationWarning',
+    'InterpolationGrid',
+    'UniformInterpolationGrid',
+    'UniformExtrapolationGrid',
+    'PreservedSpeedInterpolationGrid',
+    'InterpolatorBase',
+    'interp_methods',
+    'get_interpolator',
+    'register_interpolator',
+
+    # numeric
+    'isequal',
+    'allequal',
+    'dot1d',
+]
