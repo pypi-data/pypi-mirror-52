59f4d163086704237281367f6dc0ced552380253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:30 +0000

    Add package autoyaml/2019.9.20.1207/autoyaml-2019.9.20.1207.tar.gz

---
+++ packages/autoyaml/autoyaml-2019.9.20.1207.tar.gz/autoyaml-2019.9.20.1207/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+from setuptools import setup, find_packages
+
+PWD = pathlib.Path(__file__).parent
+README = ( PWD / "README.md").read_text()
+VERSION = "2019.9.20.1207"
+
+
+
+
+
+setup(
+    name='autoyaml',
+    test_suite="tests",
+    version=VERSION,
+    description="Load and save config files for python projects",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/martyni/autoyaml",
+    author="martyni",
+    author_email="martynjamespratt@gmail.com",
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(exclude=["tests"],),
+    package_data = {'':['*.txt', '*.md']},
+    include_package_data=True,
+    install_requires=["pyaml"],
+)
