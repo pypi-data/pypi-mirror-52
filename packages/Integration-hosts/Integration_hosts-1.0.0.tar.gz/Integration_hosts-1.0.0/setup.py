ff4615627504f77bfd78ea58db72311eb50d4cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:26 +0000

    Add package Integration-hosts/1.0.0/Integration_hosts-1.0.0.tar.gz

---
+++ packages/Integration-hosts/Integration_hosts-1.0.0.tar.gz/Integration_hosts-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Integration_hosts',
+    install_requires=["requests"],
+    version='1.0.0',
+    packages=['hostsfile'],
+    url='',
+    license='BSD',
+    author='entrp',
+    author_email='',
+    description=''
+)
