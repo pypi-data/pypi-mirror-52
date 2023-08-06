4c1a08b1e9f03e76b912448ec23bf664fdee071b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:38 +0000

    Add package global-benchmark-database-tool/2.2.5/global_benchmark_database_tool-2.2.5-py37-none-any.whl

---
+++ packages/global-benchmark-database-tool/global_benchmark_database_tool-2.2.5-py37-none-any.whl/gbd_tool/http_client.py
@@ -0,0 +1,37 @@
+from urllib.parse import urlencode, urlparse
+from urllib.request import Request, urlopen
+
+from flask import json
+
+USER_AGENT_CLI: str = 'gbd_tool-cli'
+
+# Get url, and dictionaries of parameters and headers with which then form a get request
+def get_request(url, params, headers):
+    params = urlencode(params).encode()
+    parsed_url = urlparse(url)
+    if parsed_url.scheme != 'https' or parsed_url.scheme != 'http':
+        url = 'http://{}?{}'.format(url, params)
+    request = Request(url, data=None, headers=headers, method='GET')
+    return json.loads(urlopen(request).read().decode())
+
+
+# Get url and dictionaries of parameters and headers with which then form a post request
+def post_request(url, params, headers):
+    parsed_url = urlparse(url)
+    if parsed_url.scheme != 'https' or parsed_url.scheme != 'http':
+        url = 'http://{}'.format(url)
+    request = Request(url, data=urlencode(params).encode(), headers=headers, method='POST')
+    return json.loads(urlopen(request).read().decode())
+
+
+# return true if given string represents a valid URL
+def is_url(url):
+    try:
+        tuple = urlparse(url)
+        return len(tuple[0]) > 0 and len(tuple[1]) > 0
+    except ValueError:
+        return False
+
+
+def get_user_agent():
+    return ''.join(USER_AGENT_CLI)
