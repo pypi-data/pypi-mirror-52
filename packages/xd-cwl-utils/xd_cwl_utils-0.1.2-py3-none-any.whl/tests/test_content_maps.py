20b257ef2ac5f24e2ce8dccef839725a97bd00a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:13 +0000

    Add package xd-cwl-utils/0.1.2/xd_cwl_utils-0.1.2-py3-none-any.whl

---
+++ packages/xd-cwl-utils/xd_cwl_utils-0.1.2-py3-none-any.whl/tests/test_content_maps.py
@@ -0,0 +1,23 @@
+import os
+from tests.test_base import TestBase
+from xd_cwl_utils.config import config
+from xd_cwl_utils.content_maps import make_tools_map, make_script_map, make_script_maps
+
+class TestToolMaps(TestBase):
+
+    def test_make_tools_map(self):
+        tools_dir = config[os.environ.get('CONFIG_KEY')]['cwl_tool_dir']
+        outfile_path = config[os.environ.get('CONFIG_KEY')]['content_maps_dir'] / 'tool-maps.yaml'
+        make_tools_map(tools_dir, outfile_path)
+        return
+
+
+    def test_make_script_map(self):
+        group_name = 'ENCODE-DCC'
+        project = 'atac-seq-pipeline'
+        version = 'v1.1'
+        make_script_map(group_name, project, version)
+
+    def test_make_script_maps(self):
+        make_script_maps()
+        return
