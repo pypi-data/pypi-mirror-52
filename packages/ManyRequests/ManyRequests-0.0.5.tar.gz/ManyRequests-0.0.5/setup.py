ec1d9b9284b35dad816638da120ff733b40c26f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:51 +0000

    Add package ManyRequests/0.0.5/ManyRequests-0.0.5.tar.gz

---
+++ packages/ManyRequests/ManyRequests-0.0.5.tar.gz/ManyRequests-0.0.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+reqs = [
+    'requests >= 2.5.1'
+]
+
+setuptools.setup(
+    name="ManyRequests",
+    version="0.0.5",
+    author="Kyle Beauregard",
+    author_email="kylembeauregard@gmail.com",
+    description="A library to make many requests concurrently.",
+    url='https://github.com/kbeauregard/ManyRequests',
+    packages=setuptools.find_packages(
+        include=[
+            'ManyRequests*',
+        ]
+    ),
+    py_modules=[
+        'ManyRequests.__init__',
+    ],
+    install_requires=reqs,
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Operating System :: OS Independent",
+    ],
+)
