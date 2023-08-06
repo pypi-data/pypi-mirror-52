324824d0d931a6b7fa1a75c0272fefdc669c39b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:50 +0000

    Add package deep-orderbook/0.1.0/deep_orderbook-0.1.0.tar.gz

---
+++ packages/deep-orderbook/deep_orderbook-0.1.0.tar.gz/deep_orderbook-0.1.0/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+import setuptools
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+requirements = ["tqdm", "python_binance"]
+
+packages = setuptools.find_packages()
+print("packages:", packages)
+setuptools.setup(
+    name="deep_orderbook",
+    version="0.1.0",
+    description="Transforms orders books in temporally and spatially local-correlated images",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gQuantCoder/deep_orderbook",
+    author="gQuantCoder",
+    author_email="gquantcoder@gmail.com",
+    license="MIT",
+    classifiers=[
+        # "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=packages,
+    include_package_data=True,
+    install_requires=requirements,
+    entry_points={"console_scripts": ["deepbook=deep_orderbook.__main__:main"]},
+)
+
