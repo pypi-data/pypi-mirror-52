f041d8250df950a9632e77f5f931a02df2cb7055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:06 +0000

    Add package SIK/2.1.5/SIK-2.1.5.tar.gz

---
+++ packages/SIK/SIK-2.1.5.tar.gz/SIK-2.1.5/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: LiangjunFeng
+# Mail: zhumavip@163.com
+# Created Time:  2018-4-16 19:17:34
+#############################################
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "SIK",
+    version = "2.1.5",
+    # keywords = ("pip", "SICA","featureextraction"),
+    description = "An feature extraction algorithm",
+    long_description = "An feature extraction algorithm, improve the FastICA",
+    license = "MIT Licence",
+
+    # url = "https://github.com/LiangjunFeng/SICA",
+    author = "jnddd",
+    author_email = "122296743@qq.com",
+
+    # packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["numpy"]
+)
