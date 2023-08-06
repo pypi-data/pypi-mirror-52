3fa79ebc7816fec7bf6b2cd2d84850f99aba96b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:33 +0000

    Add package cpimgs-yzhang250/0.0.2/cpimgs-yzhang250-0.0.2.tar.gz

---
+++ packages/cpimgs-yzhang250/cpimgs-yzhang250-0.0.2.tar.gz/cpimgs-yzhang250-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cpimgs-yzhang250",
+    version="0.0.2",
+    author="Ye Zhang",
+    author_email="yzhang250@gmail.com",
+    description="A package to get DMSO plates and wells coordinates, get CPD plates and wells coordinates, get images of " +
+                "the cells treated by the CPD and nearby DMSO.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yzhang250",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
