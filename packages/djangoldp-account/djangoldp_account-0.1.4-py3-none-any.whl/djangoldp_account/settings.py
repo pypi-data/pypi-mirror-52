8a59b66cbcfadebf467bdbf4e9ef419702c7e2c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:32 +0000

    Add package djangoldp-account/0.1.4/djangoldp_account-0.1.4-py3-none-any.whl

---
+++ packages/djangoldp-account/djangoldp_account-0.1.4-py3-none-any.whl/djangoldp_account/settings.py
@@ -0,0 +1,19 @@
+import validators
+from django.conf import settings
+from django.urls import reverse_lazy
+from django.views.decorators.csrf import csrf_exempt
+
+from djangoldp_account.models import webid
+
+
+def userinfo(claims, user):
+    # Populate claims dict.
+    claims['name'] = '{0} {1}'.format(user.first_name, user.last_name)
+    claims['email'] = user.email
+    claims['website'] = user.webid()
+    claims['webid'] = user.webid()
+    return claims
+
+
+def sub_generator(user):
+    return webid(user)
