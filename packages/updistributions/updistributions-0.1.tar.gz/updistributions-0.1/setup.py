edffe121a12ffb1094be1e2a818d3b9f4011ba3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:03 +0000

    Add package updistributions/0.1/updistributions-0.1.tar.gz

---
+++ packages/updistributions/updistributions-0.1.tar.gz/updistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='updistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['updistributions'],
+      zip_safe=False)
