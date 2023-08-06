3ee8f39a4a196271d76e09a7396081c8ed52ef2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:39 +0000

    Add package dl-ext/0.9.6/dl_ext-0.9.6-py3-none-any.whl

---
+++ packages/dl-ext/dl_ext-0.9.6-py3-none-any.whl/dl_ext/vision_ext/datasets/kitti/load/label_3.py
@@ -0,0 +1,42 @@
+import os
+from multipledispatch import dispatch
+from ..kitti_object_3d import KITTIObject3D, KITTIObjectClass
+
+
+@dispatch(str)
+def load_label_3(absolute_path):
+    with open(absolute_path) as f:
+        lines = f.read().splitlines()
+    labels = []
+    for l in lines:
+        items = l.split()
+        cls = items[0]
+        truncated, occluded, alpha, x1, y1, x2, y2, h, w, l, x, y, z, ry = map(float, items[1:])
+        label = KITTIObject3D(KITTIObjectClass[cls], truncated, occluded, alpha,
+                              x1, y1, x2, y2, h, w, l, x, y, z, ry)
+        labels.append(label)
+    return labels
+
+
+@dispatch(str, str)
+def load_label_3(label_3_dir, imgid):
+    absolute_path = os.path.join(label_3_dir, imgid + '.txt')
+    return load_label_3(absolute_path)
+
+
+@dispatch(str, int)
+def load_label_3(label_3_dir, imgid):
+    imgid = '%06d' % imgid
+    return load_label_3(label_3_dir, imgid)
+
+
+@dispatch(str, str, str)
+def load_label_3(kitti_root, split, imgid):
+    calib_dir = os.path.join(kitti_root, 'object', split, 'label_3')
+    return load_label_3(calib_dir, imgid)
+
+
+@dispatch(str, str, int)
+def load_label_3(kitti_root, split, imgid):
+    imgid = '%06d' % imgid
+    return load_label_3(kitti_root, split, imgid)
