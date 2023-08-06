d832482dabf94ab3a9fde4800b57caec9aa53894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:13 +0000

    Add package thoth-storages/0.19.4/thoth_storages-0.19.4-py3-none-any.whl

---
+++ packages/thoth-storages/thoth_storages-0.19.4-py3-none-any.whl/thoth/storages/inspections.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# thoth-storages
+# Copyright(C) 2018, 2019 Fridolin Pokorny
+#
+# This program is free software: you can redistribute it and / or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Adapter for persisting Amun inspection results."""
+
+from .result_base import ResultStorageBase
+from .inspection_schema import INSPECTION_SCHEMA
+
+
+class InspectionResultsStore(ResultStorageBase):
+    """Adapter for persisting Amun inspection results."""
+
+    RESULT_TYPE = "inspection"
+    SCHEMA = INSPECTION_SCHEMA
+
+    @classmethod
+    def get_document_id(cls, document: dict) -> str:
+        """Get id under which the given document will be stored."""
+        return document["inspection_id"]
