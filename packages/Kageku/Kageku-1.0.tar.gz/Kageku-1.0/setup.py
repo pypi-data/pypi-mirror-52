fa21d84f1bf8b8860e04c9cc746a683eb56bc1e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:06 +0000

    Add package Kageku/1.0/Kageku-1.0.tar.gz

---
+++ packages/Kageku/Kageku-1.0.tar.gz/Kageku-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open('README.md') as f:
+  long_description = f.read()
+
+setup(
+  name='Kageku',
+  version='1.0',
+  url='https://github.com/lucaspellegrinelli/kageku',
+  license='MIT License',
+  author='Lucas Pellegrinelli',
+  author_email='lucas.pellegrinelli@hotmail.com',
+  description='Implementation of a chess based game created by me and some friends for an RPG we play',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  packages=['kageku']
+)
