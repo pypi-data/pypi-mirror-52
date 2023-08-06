7b40d0a7a18726c94f7a12f76715fa2cd472c719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:38 +0000

    Add package egproject/0.0.29/egproject-0.0.29-py2-none-any.whl

---
+++ packages/egproject/egproject-0.0.29-py2-none-any.whl/egproject/_main.py
@@ -0,0 +1,62 @@
+try:
+		import numpy
+except:
+		print("Please install the moudle named numpy")
+
+try:
+	from openpyxl import Workbook
+except:
+		print("Please install the moudle named openpyxl")
+
+from openpyxl import load_workbook
+try:
+	import codecs
+except:
+		print("Please install the moudle named codecs")
+
+try:
+
+	from ast import literal_eval
+except:
+		print("Please install the moudle named ast")
+
+try:
+	import delny
+except:
+		print("Please install the moudle named delny")
+
+import itertools
+import re
+import xlrd
+import time as wtime
+from datetime import datetime,time
+from openpyxl.styles import Font,Fill
+from openpyxl.styles.colors import RED
+from openpyxl.styles import PatternFill,Border,Side,Alignment,Protection,Font
+from openpyxl.styles import colors
+from openpyxl.styles.colors import BLUE
+#import pygame
+from Tkinter import *
+import Tkinter
+import tkMessageBox
+from PIL import ImageTk, Image
+import random
+import numpy as np
+#from pygame.locals import *
+import xlsxwriter
+import pandas as pd
+from shutil import copyfile
+#import delny
+
+
+	
+
+
+
+def main():
+	for i in range(10):
+		print(i)
+		
+if __name__=="__main__":
+
+		main()
