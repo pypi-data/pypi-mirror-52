7f116dfa69aa1b387f63e0cb5dabfae2211b9bc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:07 +0000

    Add package nvshim/0.1.0/nvshim-0.1.0.tar.gz

---
+++ packages/nvshim/nvshim-0.1.0.tar.gz/nvshim-0.1.0/src/nvshim/utils/process.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+import sys
+from typing import Dict
+
+from .environment import EnvironmentVariable, EnvDict, process_env
+
+
+def _with_venv(env: EnvDict):
+    path_key = "PATH"
+    env_path = env.get(path_key, "")
+    return dict(env, **{path_key: f"venv/bin/:{env_path}"})
+
+
+def run(*args, **kwargs) -> subprocess.CompletedProcess:
+    env_vars = _with_venv(os.environ)
+    env_vars[EnvironmentVariable.AUTO_INSTALL.value] = "false"
+
+    try:
+        with process_env(env_vars):
+            return subprocess.run(args, **kwargs, check=True)
+    except subprocess.CalledProcessError as error:
+        sys.exit(error.returncode)
