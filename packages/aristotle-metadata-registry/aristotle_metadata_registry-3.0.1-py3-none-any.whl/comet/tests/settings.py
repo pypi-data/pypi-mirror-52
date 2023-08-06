2e7f0b7f47b2b047f82eb3ff327eeca9982a9b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:49 +0000

    Add package aristotle-metadata-registry/3.0.1/aristotle_metadata_registry-3.0.1-py3-none-any.whl

---
+++ packages/aristotle-metadata-registry/aristotle_metadata_registry-3.0.1-py3-none-any.whl/comet/tests/settings.py
@@ -0,0 +1,13 @@
+from aristotle_mdr.tests.settings.settings import *
+from aristotle_mdr.required_settings import INSTALLED_APPS
+
+INSTALLED_APPS = (
+    #The good stuff
+    'comet',
+    'aristotle_dse',
+    'aristotle_mdr.contrib.links',
+) + INSTALLED_APPS
+
+ARISTOTLE_SETTINGS['CONTENT_EXTENSIONS'] = ['comet']
+
+ROOT_URLCONF = 'comet.tests.urls'
