1bd896b18e040696db837d91bc5858c706cea7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:00 +0000

    Add package vpolo/0.2.0/vpolo-0.2.0.tar.gz

---
+++ packages/vpolo/vpolo-0.2.0.tar.gz/vpolo-0.2.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vpolo",
+    version="0.2.0",
+    author="Avi Srivastava",
+    author_email="asrivastava@cs.stonybrook.edu",
+    description="Support packages for Alevin tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/k3yavi/vpolo",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+       'pandas',
+       'scipy'
+    ]
+)
