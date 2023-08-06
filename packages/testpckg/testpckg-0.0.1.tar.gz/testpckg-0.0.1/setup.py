25289e659ab8a6490485a1e58d0dfd3420106730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:59 +0000

    Add package testpckg/0.0.1/testpckg-0.0.1.tar.gz

---
+++ packages/testpckg/testpckg-0.0.1.tar.gz/testpckg-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testpckg",
+    version="0.0.1",
+    author="neha",
+    author_email="neha.r.garde@gma.com",
+    description="A small test example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nehagarde/testingpckg1",
+    packages=setuptools.find_packages(),
+)
---
+++ packages/testpckg/testpckg-0.0.1.tar.gz/testpckg-0.0.1/testpckg/testingpckg.py
@@ -0,0 +1,4 @@
+class TestingPckg:
+    def testingpckg(self):
+        print("testing")
+
