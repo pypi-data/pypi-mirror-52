b9fe10310e0e549272cf378d7927b3f6f5ab4857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:00 +0000

    Add package Salacia/0.0.1/Salacia-0.0.1.tar.gz

---
+++ packages/Salacia/Salacia-0.0.1.tar.gz/Salacia-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name = 'Salacia',
+    version = '0.0.1',
+    author = 'Matthew P. Humphreys',
+    author_email = 'm.p.humphreys@icloud.com',
+    packages = setuptools.find_packages(),
+    classifiers = [
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Intended Audience :: Science/Research',
+    ],
+)
