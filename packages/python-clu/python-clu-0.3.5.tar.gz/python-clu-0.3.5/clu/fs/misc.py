73b2f1252177eb9ddcc329d411caf4a490ebf588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:58 +0000

    Add package python-clu/0.3.5/python-clu-0.3.5.tar.gz

---
+++ packages/python-clu/python-clu-0.3.5.tar.gz/python-clu-0.3.5/clu/fs/misc.py
@@ -0,0 +1,249 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import os
+import re
+import sys
+
+from clu.constants.consts import ENCODING
+from clu.constants.polyfills import lru_cache
+from clu.typology import string_types
+from clu.exporting import Exporter
+
+exporter = Exporter(path=__file__)
+export = exporter.decorator()
+
+# OS UTILITIES: get the current users’ home directory
+
+gethomedir = lambda: os.path.expanduser("~")
+
+@export
+def wrap_value(value):
+    """ Get a “lazified” copy of a value, wrapped in a lamba """
+    return lambda *args, **kwargs: value
+
+none_function = wrap_value(None)
+true_function = wrap_value(True)
+
+@export
+def stringify(instance, fields, *, try_callables=True):
+    """ Stringify an object instance, using an iterable field list to
+        extract and render its values, and printing them along with the 
+        typename of the instance and its memory address -- yielding a
+        repr-style string of the format:
+        
+            TypeName(fieldname="val", otherfieldname="otherval") @ 0x0FE
+        
+        The `stringify(…)` function is of use in `__str__()` and `__repr__()`
+        definitions, e.g. something like:
+        
+            def __repr__(self):
+                return stringify(self, type(self).__slots__)
+        
+        Callable fields, by default, will be called with no arguments
+        to obtain their value. To supress this behavior – if you wish
+        to represent callable fields that require arguments – you can
+        pass the keyword-only “try_callables” flag as False:
+            
+            def __repr__(self):
+                return stringify(self,
+                            type(self).__slots__,
+                            try_callables=False)
+    """
+    field_dict = {}
+    for field in fields:
+        field_value = getattr(instance, field, "")
+        if try_callables:
+            field_value = callable(field_value) and field_value() or field_value
+        if field_value:
+            field_dict.update({ u8str(field) : field_value })
+    field_dict_items = []
+    for k, v in field_dict.items():
+        field_dict_items.append(f'''{k}="{v}"''')
+    typename = type(instance).__name__
+    field_dict_string = ", ".join(field_dict_items)
+    hex_id = hex(id(instance))
+    return f"{typename}({field_dict_string}) @ {hex_id}"
+
+ex = os.path.extsep
+dolla = '$'
+
+@lru_cache(maxsize=32)
+def re_matcher(string):
+    """ Return a boolean function that will search for the given
+        regular-expression within any strings with which it is called,
+        returning True when the regex matches from the beginning of the
+        string, and False when it doesn’t.
+    """
+    if not string:
+        return true_function
+    match_function = re.compile(string, re.IGNORECASE).match
+    return lambda searching: bool(match_function(searching))
+
+@lru_cache(maxsize=32)
+def re_searcher(string):
+    """ Return a boolean function that will search for the given
+        regular-expression within any strings with which it is called,
+        returning True when the regex matches and False when it doesn’t.
+        
+        Useful in filter(…) calls and comprehensions, e.g.:
+        
+        >>> plists = filter(re_searcher(r'.plist$'), os.listdir())
+        >>> mmsuffix = suffix_searcher(r'.mm$')
+        >>> objcpp = (f for f in os.listdir() where mmsuffix(f))
+    """
+    if not string:
+        return true_function
+    search_function = re.compile(string, re.IGNORECASE).search
+    return lambda searching: bool(search_function(searching))
+
+@export
+def re_suffix(string):
+    """ Remove any “os.path.extsep” prefixing a string and ensure
+        it ends with a “$” – to indicate a regular expression suffix.
+    """
+    if string is None:
+        return None
+    return rf"{string.lower().lstrip(ex).rstrip(dolla)}$"
+
+@export
+def suffix_searcher(string):
+    """ Return a boolean function that will search for the given
+        string within any strings with which it is called, returning
+        True when they are found and False when they aren’t.
+        
+        Useful in filter(…) calls and comprehensions, e.g.:
+        
+        >>> plists = filter(suffix_searcher('plist'), os.listdir())
+        >>> mmsuffix = suffix_searcher('mm')
+        >>> objcpp = (f for f in os.listdir() where mmsuffix(f))
+    """
+    return re_searcher(re_suffix(string))
+
+@export
+def swapext(path, new_extension):
+    """ Swap the file extension of the path with a newly specified one –
+        if no extension is present, the newly specified extension will be
+        amended to the path; the new extension can provide or omit its
+        leading extension-separator (or a “period” in most human usage).
+        
+        Like E.G.:
+            
+            >>> swapext('/yo/dogg.obj', 'odb')
+            '/yo/dogg.odb'
+            >>> swapext('/yo/dogg.obj', '.odb')
+            '/yo/dogg.odb'
+            >>> swapext('/yo/dogg', 'odb')
+            '/yo/dogg.odb'
+    """
+    bulk = os.path.splitext(path)[0]
+    if new_extension is None:
+        return bulk
+    return ex.join((bulk, new_extension.lstrip(ex)))
+
+@export
+def filesize(path):
+    """ Return the on-disk size (in bytes) of the file located at a path """
+    if not os.path.exists(path):
+        return -1
+    return os.lstat(path).st_size
+
+@export
+def samesize(path0, path1):
+    """ Compare the on-disk file sizes (in bytes) of two files by their paths """
+    from clu.constants.exceptions import FilesystemError
+    if any(p is None for p in (path0, path1)):
+        raise FilesystemError('paths must both be non-None')
+    if os.path.samefile(path0, path1):
+        return True
+    return filesize(path0) == filesize(path1)
+
+@export
+def u8encode(source):
+    """ Encode a source as bytes using the UTF-8 codec """
+    return bytes(source, encoding=ENCODING)
+
+@export
+def u8bytes(source):
+    """ Encode a source as bytes using the UTF-8 codec, guaranteeing
+        a proper return value without raising an error
+    """
+    if type(source) is bytes:
+        return source
+    elif type(source) is bytearray:
+        return bytes(source)
+    elif isinstance(source, string_types):
+        return u8encode(source)
+    elif isinstance(source, (int, float)):
+        return u8encode(str(source))
+    elif type(source) is bool:
+        return source and b'True' or b'False'
+    elif source is None:
+        return b'None'
+    return bytes(source)
+
+@export
+def u8str(source):
+    """ Encode a source as a Python string, guaranteeing a proper return
+        value without raising an error
+    """
+    return type(source) is str and source \
+                        or u8bytes(source).decode(ENCODING)
+
+# OS UTILITIES: make a path on windows into a “long path”: (…?)
+
+@export
+def win32_longpath(path):
+    """ Helper function to add the long path prefix for Windows, so that shutil.copytree
+        won't fail while working with paths with 255+ chars.
+        
+        Vendored in from pytest-datadir – q.v. https://git.io/fjMWl supra.
+    """
+    if sys.platform == 'win32':
+        # The use of os.path.normpath here is necessary since "the "\\?\" prefix to a path string
+        # tells the Windows APIs to disable all string parsing and to send the string that follows
+        # it straight to the file system".
+        # (See https://docs.microsoft.com/pt-br/windows/desktop/FileIO/naming-a-file)
+        return '\\\\?\\' + os.path.normpath(path)
+    else:
+        return path
+
+# OS UTILITIES: deal with the umask value
+
+octalize = lambda integer: "0o%04o" % integer
+
+@export
+@lru_cache(maxsize=1)
+def current_umask():
+    """ Get the current umask value (cached on Python 3 and up). """
+    mask = os.umask(0)
+    os.umask(mask)
+    return mask
+
+@export
+def masked_permissions(perms=0o666):
+    """ Compute the permission bitfield, using the current umask value
+        and a given permission octal number.
+    """
+    return perms & ~current_umask()
+
+@export
+def masked_chmod(path, perms=0o666):
+    """ Perform the `os.chmod(…)` operation, respecting the current
+        umask value (q.v. `current_umask()` supra.)
+    """
+    masked_perms = masked_permissions(perms=perms)
+    os.chmod(path, mode=masked_perms)
+    return octalize(masked_perms)
+
+# MODULE EXPORTS:
+export(re_matcher,              name='re_matcher')
+export(re_searcher,             name='re_searcher')
+
+export(gethomedir,              name='gethomedir',          doc="gethomedir() → Return the current user’s home directory")
+export(none_function,           name='none_function',       doc="none_function() → A function that always returns None")
+export(true_function,           name='true_function',       doc="true_function() → A function that always returns True")
+export(octalize,                name='octalize',            doc="octalize(integer) → Format an integer value as an octal number")
+
+# Assign the modules’ `__all__` and `__dir__` using the exporter:
+__all__, __dir__ = exporter.all_and_dir()
---
+++ packages/python-clu/python-clu-0.3.5.tar.gz/python-clu-0.3.5/tests/test_dicts.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+class TestDicts(object):
+    
+    """ Run the tests for merging dicts (the clu.dicts module), and
+        namespaces (the clu.typespace and clu.typespace.namespace modules).
+    """
+    
+    def test_ordered_mapping_views(self, dirname):
+        """ The ordered mapping views are returned from “clu.fs.filesystem.Directory”
+            mapping-view methods: “directory.items()”, “directory.keys()”, and
+            “directory.values()”.
+        """
+        from clu.dicts import OrderedItemsView, OrderedKeysView, OrderedValuesView
+        from clu.fs.filesystem import Directory
+        from clu.typology import ismapping, issequence
+        
+        # READ-ONLY:
+        data = dirname.subdirectory('data')
+        
+        # Ensure type:
+        assert type(dirname) is Directory
+        assert type(data) is Directory
+        assert type(data.items()) is OrderedItemsView
+        assert type(data.keys()) is OrderedKeysView
+        assert type(data.values()) is OrderedValuesView
+        
+        # Ensure it’s a mapping:
+        assert ismapping(dirname)
+        assert ismapping(data)
+        
+        # Ensure it’s a sequence:
+        assert issequence(data.items())
+        assert issequence(data.keys())
+        assert issequence(data.values())
+        
+        # Ensure repr:
+        assert repr(dirname).startswith('Directory')
+        assert repr(data).startswith('Directory')
+        assert repr(data.items()).startswith('OrderedItemsView')
+        assert repr(data.keys()).startswith('OrderedKeysView')
+        assert repr(data.values()).startswith('OrderedValuesView')
+        
+        # Redundant, but correct:
+        assert len(data.items()) == len(data.keys()) == len(data.values())
+        assert len(list(data.items())) == len(list(data.keys())) == len(list(data.values()))
+        assert len(data.items()) == len(list(data.items()))
+        assert len(data.keys()) == len(list(data.keys()))
+        assert len(data.values()) == len(list(data.values()))
+        
+        # Nonexistent subdirectory:
+        assert len(data.subdirectory('wat').items()) == 0
+        assert len(data.subdirectory('wat').keys()) == 0
+        assert len(data.subdirectory('wat').values()) == 0
+        
+        # Real subdirectory with at least one item:
+        assert len(data.subdirectory('yodogg').items()) > 0
+        assert len(data.subdirectory('yodogg').keys()) > 0
+        assert len(data.subdirectory('yodogg').values()) > 0
+        
+        # Set properties:
+        k1 = data.subdirectory('yodogg').keys()
+        k2 = data.subdirectory('yodata').keys()
+        kU = k1 | k2
+        assert type(kU) is set
+        assert len(kU) == len(k1) + len(k2)
+        assert kU.issuperset(k1)
+        assert kU.issuperset(k2)
+        
+        # Sequence properties:
+        keys = data.keys()
+        for idx in range(len(keys)):
+            assert keys[idx] == list(keys)[idx]
+            assert keys[idx] in data
+    
+    def test_dict_and_namespace_merge(self):
+        from clu.dicts import merge
+        from clu.typespace import Namespace
+        
+        dict_one = { 'compress_level' : 9,
+                           'optimize' : True,
+                             'format' : 'png' }
+        
+        dict_two = { 'yo' : 'dogg' }
+        
+        dict_three = { 'compress_level' : 10,
+                             'optimize' : True,
+                               'format' : 'jpg' }
+        
+        merged = merge(dict_one, dict_two, dict_three, yo='DOGG')
+        
+        # print("» Checking “merge(•) …”")
+        # print()
+        
+        assert merged == { 'compress_level' : 9,
+                                 'optimize' : True,
+                                   'format' : 'png',
+                                       'yo' : 'DOGG' }
+        
+        # print_separator()
+        # pprint(merged)
+        # print_separator()
+        # print()
+        
+        # print("» Checking “Namespace.operator+(•) …”")
+        # print()
+        
+        ns1 = Namespace(dict_one)
+        ns2 = Namespace(dict_two)
+        
+        merged = ns1 + ns2 + dict_three + Namespace(yo='DOGG')
+        
+        assert merged == { 'compress_level' : 9,
+                                 'optimize' : True,
+                                   'format' : 'png',
+                                       'yo' : 'dogg' }
+        
+        # print_separator()
+        # pprint(merged)
+        # print_separator()
+        # print()
+        
+        # print("» Checking “Namespace.operator+=(•) …”")
+        # print()
+        
+        merged = Namespace(dict_one)
+        ns2 = Namespace(dict_two)
+        
+        merged += ns2
+        merged += dict_three
+        merged += Namespace(yo='DOGG')
+        
+        assert merged == { 'compress_level' : 10,
+                                 'optimize' : True,
+                                   'format' : 'jpg',
+                                       'yo' : 'DOGG' }
+        
+        # print_separator()
+        # pprint(merged)
+        # print_separator()
+        # print()
+    
