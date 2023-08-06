3096c84e19863b16b4117a4166bfe641d277b384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:45 +0000

    Add package phonon-dos/0.0.0/phonon_dos-0.0.0.tar.gz

---
+++ packages/phonon-dos/phonon_dos-0.0.0.tar.gz/phonon_dos-0.0.0/data_pos/data_R0.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 13 14:12:07 2019
+
+@author: gc4217
+"""
+import numpy as np
+import os
+
+def avg_vs_t(time,quantity):
+    avg = []
+    for i in range(1,len(time)):
+        dt = time[i]-time[i-1]
+        y = quantity[0:i]
+        integral = np.sum(dt*y,axis=0)
+        avg.append(1/(i*dt)*integral)
+    avg = np.array(avg)
+    return avg
+
+N1, N2, N3 = sys.argv[1], sys.argv[2], sys.argv[3]
+N1N2N3 = N1*N2*N3
+N = N1*N2*N3*5
+
+with open('Cartesian_M0','r') as f:
+    A = f.readlines()[0:N+1]
+del A[0]
+
+
+with open('pos','w') as g:
+    g.writelines(A)
+    
+R0 = np.loadtxt('pos',usecols=(1,2,3))
+np.savetxt('R0',R0)
+
+os.remove('pos')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
