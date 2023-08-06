e656a6586d829cf289d3bbc11a8e5836916743ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:30 +0000

    Add package text-classification-prova-alessandro-artoni/0.0.3/text-classification-prova-alessandro-artoni-0.0.3.tar.gz

---
+++ packages/text-classification-prova-alessandro-artoni/text-classification-prova-alessandro-artoni-0.0.3.tar.gz/text-classification-prova-alessandro-artoni-0.0.3/hello_world/hello.py
@@ -0,0 +1,10 @@
+class Hello:
+    def __init__(self):
+        print("Hello class was instatiated")
+
+    def hello(self):
+        print("Say hello to me!")
+
+
+def hello():
+    print("Bye bye mate")
