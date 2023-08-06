043318b1b14902d057ac3363c90abd6df1cbd7b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:46 +0000

    Add package zlgp/1.3.5/zlgp-1.3.5.zip

---
+++ packages/zlgp/zlgp-1.3.5.zip/zlgp-1.3.5/zlgp/__init__.py
@@ -0,0 +1,34 @@
+# from zlgp.src import add_quyu,restart_quyu
+
+# from zlgp.dst import add_quyu_greenplum,restart_quyu_greenplum 
+
+from zlgp.api import add_quyu_src,restart_quyu_src
+from zlgp.api import add_quyu_greenplum,restart_quyu_greenplum
+from zlgp.api import add_quyu_union,restart_quyu_union
+
+from zlgp.api import f_all,f_zlshenpi
+
+# add_quyu_src=add_quyu
+
+# restart_quyu_src=restart_quyu
+
+gp_settings={
+    "kunming":{
+        "conp_cfg":['postgres','since2015','192.168.169.89','postgres','public'],
+
+        "conp_gp":['developer','zhulong!123','192.168.169.91:5433','base_db','public'],
+
+        "conp_pg_zlmine":['postgres','since2015','192.168.169.89','zlmine','t_bd']
+    }
+    ,
+
+    "aliyun":{
+        "conp_cfg":['postgres','since2015','192.168.4.201','postgres','public'],
+
+        "conp_gp":['developer','zhulong!123','192.168.4.183:5433','base_db','public'],
+
+        "conp_pg_zlmine":['postgres','since2015','192.168.4.201','zlmine','t_bd']
+    }
+
+}
+
