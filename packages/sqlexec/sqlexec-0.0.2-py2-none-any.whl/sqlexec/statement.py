8465c71934b6f80e2f98377061605f04e237df76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:34 +0000

    Add package sqlexec/0.0.2/sqlexec-0.0.2-py2-none-any.whl

---
+++ packages/sqlexec/sqlexec-0.0.2-py2-none-any.whl/sqlexec/statement.py
@@ -0,0 +1,63 @@
+#coding=utf8
+
+class e(str):
+    pass
+
+
+def escape(value):
+    if isinstance(value, long):
+        return repr(value)[:-1]
+    elif isinstance(value, e):
+        return value
+
+    return repr(value)
+
+
+def formatValues(values):
+    return ', '.join(escape(v) for v in values)
+
+
+def formatSlots(n):
+    return ', '.join(['%s'] * n)
+
+
+def formatColumns(values):
+    if not values:
+        return '*'
+    elif isinstance(values, basestring):
+        return values
+    else:
+        return ','.join('`{}`'.format(v) for v in values)
+
+def formatSetKV(kv):
+    segments = []
+    for k, v in kv.items():
+        segments.append('{}={}'.format(k, escape(v)))
+
+    return ', '.join(segments)
+
+
+def formatWhereKV(kv):
+    segments = []
+    for k, v in kv.items():
+        if isinstance(v, list):
+            segments.append('id in ({})'.format(formatValues(v)))
+        else:
+            segments.append('{}={}'.format(k, escape(v)))
+
+    return ' and '.join(segments)
+
+
+def formatWhere(idOrWhere):
+    if not idOrWhere:
+        return ''
+    if isinstance(idOrWhere, (int, long)):
+        return 'WHERE ' + str(idOrWhere)
+    elif isinstance(idOrWhere, basestring):
+        return 'WHERE ' + idOrWhere
+    elif isinstance(idOrWhere, list):
+        return 'WHERE ' + 'id in ({})'.format(formatValues(idOrWhere))
+    elif isinstance(idOrWhere, dict):
+        return 'WHERE ' + formatWhereKV(idOrWhere)
+    else:
+        return None
