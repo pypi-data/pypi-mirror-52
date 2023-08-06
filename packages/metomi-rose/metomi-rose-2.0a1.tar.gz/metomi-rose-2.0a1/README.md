077162df9018fd5cb11616af321fe011805fb5e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:20 +0000

    Add package metomi-rose/2.0a1/metomi-rose-2.0a1.tar.gz

---
+++ packages/metomi-rose/metomi-rose-2.0a1.tar.gz/metomi-rose-2.0a1/README.md
@@ -0,0 +1,49 @@
+# Rose
+
+[![Build Status](https://travis-ci.org/metomi/rose.svg?branch=master)](https://travis-ci.org/metomi/rose)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/ad021a33e7a64b398f792305dd901795)](https://www.codacy.com/app/metomi/rose?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=metomi/rose&amp;utm_campaign=Badge_Grade)
+[![DOI](https://zenodo.org/badge/6223866.svg)](https://zenodo.org/badge/latestdoi/6223866)
+[![codecov](https://codecov.io/gh/metomi/rose/branch/master/graph/badge.svg)](https://codecov.io/gh/metomi/rose)
+
+Rose: a framework for managing and running meteorological suites.
+
+### Python 2 or Python 3 ?
+
+Currently in the source code repository:
+ - **master branch:** Python 3, **no GUI** - **Rose-2/Cylc-8 Work In Progress**
+ - **2019.01.x branch:** Python 2, PyGTK GUI - **Rose-1/Cylc-7 Maintenance**
+
+The first official Rose-2/Cylc-8 release (with a new web UI) is not expected
+until late 2019. Until then we recommend the latest rose-2019.01.x/cylc-7.8.x
+release for production use.
+
+[Installation](http://metomi.github.io/rose/doc/html/installation.html) |
+[User Guide](http://metomi.github.io/rose/) |
+[How to Contribute](https://github.com/metomi/rose/blob/master/CONTRIBUTING.md)
+
+## Copyright and Terms of Use
+
+Copyright (C) 2012-2019 British Crown (Met Office) &amp; Contributors
+
+Rose is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Rose is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Rose. If not, see <http://www.gnu.org/licenses/>.
+
+Rose documentation is licensed under the British Open Government
+Licence. See doc/rose-terms-of-use.html and
+<http://www.nationalarchives.gov.uk/doc/open-government-licence/>
+
+See <http://metomi.github.io/rose/doc/html/terms.html>.
+
+## Acknowledgement for non-Rose Work
+
+See [Acknowledgement for Non-Rose Work](https://github.com/metomi/rose/blob/master/ACKNOWLEDGEMENT.md).
---
+++ packages/metomi-rose/metomi-rose-2.0a1.tar.gz/metomi-rose-2.0a1/setup.py
@@ -0,0 +1,91 @@
+#!/usr/ bin/env python3
+# coding=utf-8
+
+# Copyright (C) 2012-2019 British Crown (Met Office) & Contributors.
+#
+# This file is part of Rose, a framework for meteorological suites.
+#
+# Rose is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Rose is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Rose. If not, see <http://www.gnu.org/licenses/>.
+
+import codecs
+
+from glob import glob
+from os.path import join, abspath, dirname
+import re
+
+
+from setuptools import setup, find_namespace_packages
+
+here = abspath(dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(join(here, *parts), "r") as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+INSTALL_REQUIRES = [
+    "jinja2>=2.10.1, <2.11.0",
+    "cylc-flow==8.0a1",
+    "aiofiles",
+    "tornado",
+    "sqlalchemy",
+    "metomi-isodatetime",
+    "requests",
+    "ldap3",
+]
+EXTRAS_REQUIRE = {
+    'docs': [
+        'sphinx',
+        'sphinx_rtd_theme',
+        'sphinxcontrib-httpdomain',
+        'hieroglyph',
+        'sphinxcontrib-svg2pdfconverter',
+        'cylc-sphinx-extensions[all]',
+    ]
+}
+EXTRAS_REQUIRE['all'] = list({y for x in EXTRAS_REQUIRE.values() for y in x})
+
+
+setup(
+    # Metadata
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=find_version("metomi", "rose", "__init__.py"),
+
+    # Options
+    scripts=glob(join("bin", "*"))
+    + glob(join("sbin", "*"))
+    + glob(join("lib", "bash", "*")),
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
+    package_data={
+        "metomi.rose": ["etc/.*"],
+        "metomi.rosie": ["lib/*"]
+    },
+    packages=find_namespace_packages(include=["metomi.*"]),
+)
