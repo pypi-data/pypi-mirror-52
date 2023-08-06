41473455d6532493d6ad33a03e968daee1d3f374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:30 +0000

    Add package colour-printing/0.2.3/colour-printing-0.2.3.tar.gz

---
+++ packages/colour-printing/colour-printing-0.2.3.tar.gz/colour-printing-0.2.3/test/test1.py
@@ -0,0 +1,15 @@
+from colour_printing.custom import ColourPrint, Back, Fore, Mode
+from colour_printing import cprint
+
+
+class MyColour(ColourPrint):
+    def custom(self):
+        self.test = self.Markers('test')\
+            .flag_style(fore=Fore.CYAN)\
+            .time_style(mode=Mode.UNDERLINE)\
+            .message_style(fore=Fore.YELLOW)
+
+
+echo = MyColour()
+cprint(echo, fore=Fore.BLUE, mode=Mode.BOLD)
+echo.test('hello world!')
