2937688e450815cd69a7fb8dfa591c34119a944e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:18 +0000

    Add package pysoa/0.68.0/pysoa-0.68.0.tar.gz

---
+++ packages/pysoa/pysoa-0.68.0.tar.gz/pysoa-0.68.0/pysoa/server/action/base.py
@@ -0,0 +1,144 @@
+from __future__ import (
+    absolute_import,
+    unicode_literals,
+)
+
+import abc
+from typing import (  # noqa: F401 TODO Python 3
+    Any,
+    Callable,
+    Dict,
+    Optional,
+    Type,
+    Union,
+)
+
+import six
+
+from pysoa.common.types import (
+    ActionResponse,
+    Error,
+)
+from pysoa.server.errors import (
+    ActionError,
+    ResponseValidationError,
+)
+from pysoa.server.settings import ServerSettings
+from pysoa.server.types import EnrichedActionRequest
+
+
+__all__ = (
+    'Action',
+    'ActionType',
+)
+
+
+@six.add_metaclass(abc.ABCMeta)
+class Action(object):
+    """
+    Base class from which all SOA service actions inherit.
+
+    Contains the basic framework for implementing an action:
+
+    - Subclass and override `run()` with the body of your code
+    - Optionally provide a `description` attribute, which should be a unicode string and is used to display
+      introspection for the action.
+    - Optionally provide `request_schema` and/or `response_schema` attributes. These should be Conformity fields.
+    - Optionally provide a `validate()` method to do custom validation on the request.
+    """
+
+    request_schema = None
+    response_schema = None
+
+    def __init__(self, settings=None):  # type: (Optional[ServerSettings]) -> None
+        """
+        Construct a new action. Concrete classes can override this and define a different interface, but they must
+        still pass the server settings to this base constructor by calling `super`.
+
+        :param settings: The server settings object
+        :type settings: dict
+        """
+        self.settings = settings
+
+    @abc.abstractmethod
+    def run(self, request):  # type: (EnrichedActionRequest) -> Dict[str, Any]
+        """
+        Override this to perform your business logic, and either return a value abiding by the `response_schema` or
+        raise an `ActionError`.
+
+        :param request: The request object
+        :type request: EnrichedActionRequest
+
+        :return: The response
+        :rtype: dict
+
+        :raise: ActionError
+        """
+        raise NotImplementedError()
+
+    def validate(self, request):  # type: (EnrichedActionRequest) -> None
+        """
+        Override this to perform custom validation logic before the `run()` method is run. Raise `ActionError` if you
+        find issues, otherwise return (the return value is ignored). If this method raises an error, `run()` will not
+        be called. You do not have to override this method if you don't want to perform custom validation or prefer to
+        perform it in `run()`.
+
+        :param request: The request object
+        :type request: EnrichedActionRequest
+
+        :raise: ActionError
+        """
+        pass
+
+    def __call__(self, action_request):  # type: (EnrichedActionRequest) -> ActionResponse
+        """
+        Main entry point for actions from the `Server` (or potentially from tests). Validates that the request matches
+        the `request_schema`, then calls `validate()`, then calls `run()` if `validate()` raised no errors, and then
+        validates that the return value from `run()` matches the `response_schema` before returning it in an
+        `ActionResponse`.
+
+        :param action_request: The request object
+        :type action_request: EnrichedActionRequest
+
+        :return: The response object
+        :rtype: ActionResponse
+
+        :raise: ActionError, ResponseValidationError
+        """
+        # Validate the request
+        if self.request_schema:
+            errors = [
+                Error(
+                    code=error.code,
+                    message=error.message,
+                    field=error.pointer,
+                )
+                for error in (self.request_schema.errors(action_request.body) or [])
+            ]
+            if errors:
+                raise ActionError(errors=errors)
+        # Run any custom validation
+        self.validate(action_request)
+        # Run the body of the action
+        response_body = self.run(action_request)
+        # Validate the response body. Errors in a response are the problem of
+        # the service, and so we just raise a Python exception and let error
+        # middleware catch it. The server will return a SERVER_ERROR response.
+        if self.response_schema:
+            errors = self.response_schema.errors(response_body)
+            if errors:
+                raise ResponseValidationError(action=action_request.action, errors=errors)
+        # Make an ActionResponse and return it
+        if response_body is not None:
+            return ActionResponse(
+                action=action_request.action,
+                body=response_body,
+            )
+        else:
+            return ActionResponse(action=action_request.action)
+
+
+ActionType = Union[
+    Type[Action],
+    Callable[[ServerSettings], Callable[[EnrichedActionRequest], ActionResponse]],
+]
---
+++ packages/pysoa/pysoa-0.68.0.tar.gz/pysoa-0.68.0/setup.py
@@ -0,0 +1,97 @@
+from __future__ import (
+    absolute_import,
+    unicode_literals,
+)
+
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+from pysoa import __version__
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+base_requirements = [
+    'attrs>=17.4,<20',
+    'conformity~=1.26',
+    'currint>=1.6,<3',
+    'enum34;python_version<"3.4"',
+    'msgpack-python~=0.5,>=0.5.2',
+    'pytz>=2019.1',
+    'redis~=2.10',
+    'six~=1.10',
+    'typing~=3.7.4;python_version<"3.5"',
+
+    # For context, see the comment in pysoa.common.compatibility. Due to the peculiarities of the patching detailed
+    # there, we pin these dependencies to hard versions, or else things might break when they update. When new versions
+    # come out, we'll bump and adjust our patching or, hopefully, relax and remove our patching.
+    'contextvars==2.4;python_version>"3.4" and python_version<"3.7"',
+    'aiocontextvars==0.2.2;python_version>"3.4" and python_version<"3.7"',
+]
+
+test_helper_requirements = [
+    'mock>=2.0;python_version<"3.3"',
+]
+
+test_plan_requirements = test_helper_requirements + [
+    'pyparsing~=2.2',
+    'pytest>=3.1,<6,!=4.2.0',  # 4.2.0 has a regression breaking our test plans, fixed in 4.2.1
+    'pytest-asyncio;python_version>"3.4"',
+]
+
+test_requirements = [
+    'coverage~=4.5',
+    'factory_boy~=2.11.1',
+    'freezegun~=0.3',
+    'lunatic-python-universal~=2.1',
+    'mockredispy~=2.9',
+    'mypy;python_version>"3.4"',
+] + test_plan_requirements
+
+
+setup(
+    name='pysoa',
+    version=__version__,
+    author='Eventbrite, Inc.',
+    author_email='opensource@eventbrite.com',
+    description='A Python library for writing (micro)services and their clients',
+    long_description=readme(),
+    url='http://github.com/eventbrite/pysoa',
+    packages=list(map(str, find_packages(exclude=['*.tests', '*.tests.*', 'tests.*', 'tests']))),
+    include_package_data=True,
+    install_requires=base_requirements,
+    tests_require=test_requirements,
+    setup_requires=['pytest-runner'],
+    test_suite='tests',
+    extras_require={
+        'testing': test_requirements,
+        'test_helpers': test_helper_requirements,
+        'test_plans': test_plan_requirements,
+    },
+    entry_points={
+        'pytest11': [
+            'pysoa_test_plan=pysoa.test.plugins.pytest.plans',
+            'pysoa_test_fixtures=pysoa.test.plugins.pytest.fixtures',
+        ]
+    },
+    license='Apache 2.0',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development',
+    ],
+)
