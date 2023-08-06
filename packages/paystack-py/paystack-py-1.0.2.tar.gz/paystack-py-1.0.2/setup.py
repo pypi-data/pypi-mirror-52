25d007f7771608cfe3d1e5370e36a5f4a47247ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:42 +0000

    Add package paystack-py/1.0.2/paystack-py-1.0.2.tar.gz

---
+++ packages/paystack-py/paystack-py-1.0.2.tar.gz/paystack-py-1.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.rst", encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='paystack-py',
+    version='1.0.2',
+    packages=['paystack_py'],
+    url='https://github.com/iambenkay/paystack-py',
+    license='Apache 2.0',
+    author='benkay',
+    author_email='benjamincath@gmail.com',
+    description='For completing, initializing and verifying paystack transactions',
+    install_requires=[
+        'requests',
+    ],
+    zip_safe=False,
+    long_description=long_description,
+)
