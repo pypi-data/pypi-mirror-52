eb6da017f8e573e7883664a018306d906dbc5a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:00 +0000

    Add package zuper-nodes-python2/2.1.5/zuper-nodes-python2-2.1.5.tar.gz

---
+++ packages/zuper-nodes-python2/zuper-nodes-python2-2.1.5.tar.gz/zuper-nodes-python2-2.1.5/src/zuper_nodes_python2/__init__.py
@@ -0,0 +1,12 @@
+import logging
+
+__version__ = '2.1.5'
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+logging.info('zn-p2 %s' % __version__)
+
+from .imp import *
+from .outside import *
