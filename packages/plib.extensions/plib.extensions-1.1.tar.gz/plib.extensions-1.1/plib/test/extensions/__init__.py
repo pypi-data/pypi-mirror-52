2228d36a9694a77b5b1a8d191c66dd0bd2168851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:50 +0000

    Add package plib.extensions/1.1/plib.extensions-1.1.tar.gz

---
+++ packages/plib.extensions/plib.extensions-1.1.tar.gz/plib.extensions-1.1/plib/test/extensions/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+Sub-Package TEST.EXTENSIONS of Package PLIB
+Copyright (C) 2008-2019 by Peter A. Donis
+
+Released under the GNU General Public License, Version 2
+See the LICENSE and README files for more information
+
+This sub-package contains the PLIB.EXTENSIONS test suite.
+"""
---
+++ packages/plib.extensions/plib.extensions-1.1.tar.gz/plib.extensions-1.1/plib/test/extensions/__main__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""
+Module MAIN
+Sub-Package TEST.EXTENSIONS of Package PLIB
+Copyright (C) 2008-2019 by Peter A. Donis
+
+Released under the GNU General Public License, Version 2
+See the LICENSE and README files for more information
+
+This is the test-running script for the PLIB.EXTENSIONS test suite.
+"""
+
+
+if __name__ == '__main__':
+    from plib.test.support import run_tests
+    
+    run_tests(__name__)
