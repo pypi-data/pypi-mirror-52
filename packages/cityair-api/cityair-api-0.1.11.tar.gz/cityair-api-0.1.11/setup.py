19de4d37caf65c4857e355cc582d6d5e38869eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:47 +0000

    Add package cityair-api/0.1.11/cityair-api-0.1.11.tar.gz

---
+++ packages/cityair-api/cityair-api-0.1.11.tar.gz/cityair-api-0.1.11/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='cityair-api',
+    version='0.1.11',
+    packages=['cityair_api'],
+    url='https://github.com/cityairltd/cityair_python_api',
+    license='Apache 2.0',
+    author='Egor Korovin',
+    author_email='ek@cityair.ru',
+    description='A python api to access data from cityair.io'
+)
