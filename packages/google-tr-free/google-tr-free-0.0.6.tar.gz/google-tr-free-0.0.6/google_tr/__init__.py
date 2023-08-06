6fc9466a14daece1e14c981be2ad5ec978846aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:41 +0000

    Add package google-tr-free/0.0.6/google-tr-free-0.0.6.tar.gz

---
+++ packages/google-tr-free/google-tr-free-0.0.6.tar.gz/google-tr-free-0.0.6/google_tr/__init__.py
@@ -0,0 +1,12 @@
+from .google_tr import google_tr
+
+# __version__ = '0.0.2'
+# __date__ = '2019.7.23'
+# __version__ = '0.0.4'
+# __date__ = '2019.7.28'
+# __version__ = '0.0.5'
+# __date__ = '2019.9.12'
+
+__version__ = '0.0.6'  # remove f-string
+__date__ = '2019.9.19'
+VERSION = __version__
