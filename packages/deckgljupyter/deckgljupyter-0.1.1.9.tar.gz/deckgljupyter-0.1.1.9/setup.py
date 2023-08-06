73cb853171b10f77692ef717a985d4eebb3d6430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:57 +0000

    Add package deckgljupyter/0.1.1.9/deckgljupyter-0.1.1.9.tar.gz

---
+++ packages/deckgljupyter/deckgljupyter-0.1.1.9.tar.gz/deckgljupyter-0.1.1.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name                = 'deckgljupyter',
+    version             = '0.1.1.9',
+    license             = 'MIT',
+    description         = 'Let deck.gl be created in jupyter notebook.',
+    long_description    = open('readme.md').read(),
+    long_description_content_type = 'text/markdown',
+    author              = 'heumsi',
+    author_email        = 'heumsi@naver.com',
+    url                 = 'https://github.com/heumsi/deckgl-jupyter',
+    download_url        = 'https://github.com/heumsi/deckgl-jupyter/archive/master.zip',
+    install_requires    =  ['mapboxgl', 'jinja2', ],
+    packages            = find_packages(exclude = []),
+    include_package_data = True,
+    keywords            = ['deckgl', 'mapboxgl', 'location data', 'visualization'],
+    python_requires     = '>=3',
+    #package_data        = {},
+    zip_safe            = False,
+    classifiers         = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
+    ],
+)
