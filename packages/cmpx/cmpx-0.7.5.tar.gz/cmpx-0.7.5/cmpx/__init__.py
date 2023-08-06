3a178cd99dd75b9c6d32aec4c427a9ae7bff75d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:27 +0000

    Add package cmpx/0.7.5/cmpx-0.7.5.tar.gz

---
+++ packages/cmpx/cmpx-0.7.5.tar.gz/cmpx-0.7.5/cmpx/__init__.py
@@ -0,0 +1,12 @@
+from .Complex import Complex
+from .error import print_err
+
+__title__      = 'PythonComplex'
+__author__     = 'Omar Belghaouti'
+__maintainer__ = 'Omar Belghaouti'
+__email__      = 'bel_omar18@yahoo.com'
+__version__    = '0.7.5'
+__license__    = 'MIT'
+__all__ = [
+    'Complex'
+]
