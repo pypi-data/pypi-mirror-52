9f1b8385814abf8f2dfe44f5460437f31a77144f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:00 +0000

    Add package edx-oauth2-provider/1.3.1/edx-oauth2-provider-1.3.1.tar.gz

---
+++ packages/edx-oauth2-provider/edx-oauth2-provider-1.3.1.tar.gz/edx-oauth2-provider-1.3.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+
+from setuptools import find_packages, setup
+
+import edx_oauth2_provider
+
+setup(
+    name='edx-oauth2-provider',
+    version=edx_oauth2_provider.__version__,
+    description='Provide OAuth2 access to edX installations',
+    author='edX',
+    url='https://github.com/edx/edx-oauth2-provider',
+    license='AGPL',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django',
+    ],
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'django>=1.8.7,<2.0',
+        'edx-django-oauth2-provider>=1.2.1,<2.0.0',
+        'PyJWT>=1.4.0,<2.0.0'
+    ]
+)
