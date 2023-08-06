7422cc2dfe494bad64b86b18b2b271d600c94bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:28 +0000

    Add package postmodernize/0.0.1/postmodernize-0.0.1.tar.gz

---
+++ packages/postmodernize/postmodernize-0.0.1.tar.gz/postmodernize-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='postmodernize',
+    author='Michael J. Sullivan',
+    author_email='sully@msully.net',
+    version='0.0.1',
+    url='https://github.com/msullivan/postmodernize',
+    description='A hack on top of 2to3 for converting straddling (2 and 3) Python '
+                'code to pure Python 3. Just squatting on the name for now...',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ]
+)
