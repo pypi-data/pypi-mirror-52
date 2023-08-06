c95332b44bb9b0081a44cc4748c04741aa65f0b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:55 +0000

    Add package cornac/1.1.2/cornac-1.1.2.tar.gz

---
+++ packages/cornac/cornac-1.1.2.tar.gz/cornac-1.1.2/cornac/models/__init__.py
@@ -0,0 +1,45 @@
+# Copyright 2018 The Cornac Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+from .baseline_only import BaselineOnly
+from .bpr import BPR
+from .cdl import CDL
+from .c2pf import C2PF
+from .hpf import HPF
+from .pcrl import PCRL
+from .pmf import PMF
+from .nmf import NMF
+from .mcf import MCF
+from .vmf import VMF
+from .ibpr import IBPR
+from .coe import COE
+from .skm import SKMeans
+from .vbpr import VBPR
+from .online_ibpr import OnlineIBPR
+from .mf import MF
+from .svd import SVD
+from .conv_mf import ConvMF
+from .cdr import CDR
+from .sbpr import SBPR
+from .sorec import SoRec
+from .cvae import CVAE
+from .wmf import WMF
+from .vaecf import VAECF
+from .ctr import CTR
+from .hft import HFT
+from .ncf import GMF
+from .ncf import MLP
+from .ncf import NeuMF
+from .efm import EFM
