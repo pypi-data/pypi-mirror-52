577328c1f9f21443fce9c0c3790ab5b51127a5d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:10 +0000

    Add package mailbomb/0.0.1.3/mailbomb-0.0.1.3.tar.gz

---
+++ packages/mailbomb/mailbomb-0.0.1.3.tar.gz/mailbomb-0.0.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mailbomb',
+    version='0.0.1.3',
+    packages=find_packages(),
+    license='GPLv3',
+    description='A python package to send corporate amounts of email quickly and easily',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/D3PSI/mailbomb',
+    author='D3PSI',
+    author_email='d3psigames@gmail.com',
+    install_requires=[
+      ],
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+         "Operating System :: OS Independent",
+     ],
+)
