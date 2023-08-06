4ab8eadc31dde8e3da0674dcbc5d03feef4a9a3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:36 +0000

    Add package panda3d-blend2bam/0.9/panda3d_blend2bam-0.9-py2.py3-none-any.whl

---
+++ packages/panda3d-blend2bam/panda3d_blend2bam-0.9-py2.py3-none-any.whl/blend2bam/gltf2bam/panda3d-gltf/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+__version__ = ''
+#pylint: disable=exec-used
+exec(open('gltf/version.py').read())
+
+setup(
+    version=__version__,
+    keywords='panda3d gltf',
+    packages=['gltf'],
+    install_requires=[
+        'panda3d',
+    ],
+    setup_requires=[
+        'pytest-runner'
+    ],
+    tests_require=[
+        'pytest',
+        'pylint',
+        'pytest-pylint',
+    ],
+    entry_points={
+        'console_scripts': [
+            'gltf2bam=gltf.cli:main'
+        ],
+        'gui_scripts': [
+            'gltf-viewer=gltf.viewer:main'
+        ],
+        'panda3d.loaders': [
+            'gltf=gltf.loader:GltfLoader',
+            'glb=gltf.loader:GltfLoader',
+        ],
+    },
+)
