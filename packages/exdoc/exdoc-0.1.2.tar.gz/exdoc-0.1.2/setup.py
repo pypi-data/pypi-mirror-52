78403805138be3e94d9f5812ef7aa4c0ca9b88d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:29 +0000

    Add package exdoc/0.1.2/exdoc-0.1.2.tar.gz

---
+++ packages/exdoc/exdoc-0.1.2.tar.gz/exdoc-0.1.2/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+""" Documentation extractor. """
+
+import sys
+if sys.version_info < (3, 4, 0):
+    raise ImportError('Only Python 3.4+ is supported')
+
+from setuptools import setup, find_packages
+
+setup(
+    name='exdoc',
+    version='0.1.2',
+    author='Mark Vartanyan',
+    author_email='kolypto@gmail.com',
+
+    url='https://github.com/kolypto/py-exdoc',
+    license='BSD',
+    description=__doc__,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords=['documentation'],
+
+    packages=find_packages(),
+    scripts=[],
+    entry_points={},
+
+    install_requires=[],
+    extras_require={},
+    include_package_data=True,
+    test_suite='nose.collector',
+
+    platforms='any',
+    classifiers=[
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
