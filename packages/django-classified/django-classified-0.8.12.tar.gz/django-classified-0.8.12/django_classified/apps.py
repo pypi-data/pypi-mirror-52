a15193ef3848486a3d873858f7b0329c5b09586a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:51 +0000

    Add package django-classified/0.8.12/django-classified-0.8.12.tar.gz

---
+++ packages/django-classified/django-classified-0.8.12.tar.gz/django-classified-0.8.12/django_classified/apps.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+
+from django.apps import AppConfig
+
+
+class DjangoClassifiedConfig(AppConfig):
+    name = 'django_classified'
+    verbose_name = "Django Classified Ads"
---
+++ packages/django-classified/django-classified-0.8.12.tar.gz/django-classified-0.8.12/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import setuptools
+
+from django_classified import get_version  # noqa isort:skip
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    author='Sergey Lyapustin',
+    name="django-classified",
+    version=get_version().replace(' ', '-'),
+    author_email="inoks@mail.ru",
+    description="Django classified ads",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/inoks/django-classified",
+    keywords='django classified ads',
+    packages=setuptools.find_packages(exclude=('demo',)),
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=(
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        "django-bootstrap-form",
+        "django-filter",
+        "Django>=1.11,<2.3",
+        "Pillow>=4.0",
+        "sorl-thumbnail>=12",
+        "unidecode",
+        # Babel is used for currency formatting
+        'Babel>=1.0,<3.0',
+    ],
+)
