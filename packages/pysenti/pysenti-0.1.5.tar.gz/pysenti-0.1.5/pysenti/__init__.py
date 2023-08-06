cc51d1a93de5b923596bfd47bc74e42f3657e601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:01 +0000

    Add package pysenti/0.1.5/pysenti-0.1.5.tar.gz

---
+++ packages/pysenti/pysenti-0.1.5.tar.gz/pysenti-0.1.5/pysenti/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing（xuming624@qq.com)
+@description: 
+"""
+
+from . import config
+from .compat import strdecode
+from .model_classifier import ModelClassifier
+from .rule_classfier import RuleClassifier
+
+__version__ = '0.1.5'
+
+rule_classifier = RuleClassifier()
+classify = rule_classifier.classify
+
+model_classifier = ModelClassifier(config.sentiment_model_path)
---
+++ packages/pysenti/pysenti-0.1.5.tar.gz/pysenti-0.1.5/pysenti/data/process.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing（xuming624@qq.com)
+@description: 
+"""
+from codecs import open
+
+r = set()
+with open('pos_dict.txt', 'r', encoding='utf-8')as f:
+    for line in f:
+        line = line.strip()
+        r.add(line)
+
+sentiments = set()
+c = 0
+with open('sentiment_dict.txt', 'r', encoding='utf-8') as f:
+    for line in f:
+        line = line.strip().split()
+        w = line[0]
+        sentiments.add(w)
+
+with open('pos', 'w', encoding='utf-8')as f:
+    for i in r:
+        if i not in sentiments:
+            f.write(i + ' 2' + '\n')
