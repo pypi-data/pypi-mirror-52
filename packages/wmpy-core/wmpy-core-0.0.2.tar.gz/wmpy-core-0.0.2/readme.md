392d7a9c413af1f84e2230fde7b5d65072e39ebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:24 +0000

    Add package wmpy-core/0.0.2/wmpy-core-0.0.2.tar.gz

---
+++ packages/wmpy-core/wmpy-core-0.0.2.tar.gz/wmpy-core-0.0.2/readme.md
@@ -0,0 +1,27 @@
+#usage
+add to python path
+export PYTHONPATH=$PYTHONPATH:${A path to wmpy_util}
+eg. /Users/zhangweiwang/workspace/python/python_lib
+
+
+#IN PYCHARM
+open preferences/project structure
+then choose add content root
+and mark parent directory of wmpy_util as SOURCE ROOT
+
+#upload to pypi
+##项目打包
+python setup.py sdist build
+
+
+##项目上传 需要账号密码,创建文件：~/.pypirc
+python setup.py sdist upload
+
+##.pypirc格式如下
+[distutils]
+index-servers=pypi
+
+[pypi]
+repository = https://upload.pypi.org/legacy/
+username = <username>
+password = <password>
---
+++ packages/wmpy-core/wmpy-core-0.0.2.tar.gz/wmpy-core-0.0.2/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+setup(
+    name='wmpy-core',
+    version='0.0.2',
+    author='zhangweiwang',
+    author_email='zhangweiwang@pwrd.com',
+    url="https://x.wanmei.com",
+    description=u'common util of pwrd python service',
+    packages=['wmpy_util'],
+    install_requires=[
+        'Django',
+        'tensorflow',
+        'schedule',
+        'pypinyin',
+        'numpy',
+        'opencv-contrib-python'
+    ],
+    entry_points={
+        'console_scripts': [
+        ]
+    }
+)
