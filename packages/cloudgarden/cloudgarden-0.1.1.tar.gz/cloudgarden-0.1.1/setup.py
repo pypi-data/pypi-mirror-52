aeedc4b1c9c83225b80c75f32d5e1d0c92266b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:52 +0000

    Add package cloudgarden/0.1.1/cloudgarden-0.1.1.tar.gz

---
+++ packages/cloudgarden/cloudgarden-0.1.1.tar.gz/cloudgarden-0.1.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+modules = \
+['cloudgarden']
+install_requires = \
+['boto3>=1.9.232,<2.0.0', 'marshmallow>=3.2.0,<4.0.0']
+
+setup_kwargs = {
+    'name': 'cloudgarden',
+    'version': '0.1.1',
+    'description': 'Cloudgarden basic library',
+    'long_description': None,
+    'author': 'Elendir',
+    'author_email': 'mich.spicka.ja+pypi@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
