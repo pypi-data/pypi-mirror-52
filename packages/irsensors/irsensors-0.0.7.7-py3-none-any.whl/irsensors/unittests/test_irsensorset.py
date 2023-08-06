7aa532d0828c05c98f3b402e9d424f5a3885c7e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:51 +0000

    Add package irsensors/0.0.7.7/irsensors-0.0.7.7-py3-none-any.whl

---
+++ packages/irsensors/irsensors-0.0.7.7-py3-none-any.whl/irsensors/unittests/test_irsensorset.py
@@ -0,0 +1,23 @@
+﻿#!/usr/bin/env python3
+# -*-coding:utf-8 -*
+
+
+from unittest import TestCase, main
+
+from irsensors.irsensorset import IRSensorSet
+
+
+class IRSensorSetTest(TestCase):
+	"""This class test the IRSensorSet methods"""
+	pass
+
+
+if __name__ == "__main__":
+	try:
+		main()
+
+	except KeyboardInterrupt:
+		pass
+
+	finally:
+		pass
