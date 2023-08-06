05be72c038e46d0afbf95f9c429f906ff10640b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:28 +0000

    Add package transformer-model/1.5.1/transformer_model-1.5.1.tar.gz

---
+++ packages/transformer-model/transformer_model-1.5.1.tar.gz/transformer_model-1.5.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='transformer_model',
+    version='1.5.1',
+    packages=find_packages(exclude=['env']),
+    url='https://github.com/zbloss/TransformerModel',
+    download_url='https://github.com/zbloss/TransformerModel/archive/1.5.1.tar.gz',
+    keywords=['Transformer', 'seq2seq', 'SelfAttention'],
+    license='mit',
+    author='Zachary Bloss',
+    author_email='zacharybloss@gmail.com',
+    description='A Transformer model implementation in TensorFlow 2.0',
+    python_requires='>=3.5',
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'tensorflow-datasets>=1.1.0',
+        'tensorflow>=2.0.0b1'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+      ],
+)
