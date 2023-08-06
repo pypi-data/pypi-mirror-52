d1b3ef9bcfb08ae8496a23e24990ad13f953b844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:17 +0000

    Add package etepcl/2.4/etepcl-2.4.tar.gz

---
+++ packages/etepcl/etepcl-2.4.tar.gz/etepcl-2.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name = 'etepcl',
+    packages = ['etepcl'],
+    entry_points = {
+            "console_scripts": ['etepcl = etepcl.cli:main']
+        },
+    version = '2.4',
+    description = 'Command line end to end processing',
+    author = 'Anmol Warman',
+    url = 'https://github.com/anmolwarman/etepcl',
+    download_url = 'https://github.com/hugorut/etepcl/tarball/1.3', 
+    keywords = ['machine learning', 'ai', 'neural network'],
+    setup_requires=["numpy"],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'scikit-learn',
+        'pyparsing',
+        'matplotlib',
+        'click',        
+        'tensorflow',
+        'keras'
+    ],
+)
