f91dcc6532e247ca52a8237cbbae9be27e56d262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:07 +0000

    Add package pyrankvote/0.1.4/pyrankvote-0.1.4-py3-none-any.whl

---
+++ packages/pyrankvote/pyrankvote-0.1.4-py3-none-any.whl/pyrankvote/__init__.py
@@ -0,0 +1,8 @@
+from pyrankvote import single_seat_ranking_methods, multiple_seat_ranking_methods
+
+__version__ = '0.1.4'
+
+__all__ = [
+    'single_seat_ranking_methods',
+    'multiple_seat_ranking_methods',
+]
