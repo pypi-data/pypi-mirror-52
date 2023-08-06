22590cc6865d450062b8135ef1984589fef7f4c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:45 +0000

    Add package weather-santhu/0.0.2/weather%20santhu-0.0.2.tar.gz

---
+++ packages/weather-santhu/weather%20santhu-0.0.2.tar.gz/weather santhu-0.0.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+  
+setuptools.setup(
+    #Here is the module name.
+    name="weather santhu",
+ 
+    #version of the module
+    version="0.0.2",
+ 
+    #Name of Author
+    author="Santhosh R",
+ 
+    #your Email address
+    author_email="santhoshgowda.ec@gmail.com",
+ 
+    #Small Description about module
+    description="Web scrapping for weather.com",
+
+ 
+    #Any link to reach this module, if you have any webpage or github profile
+    url="https://github.com/Santhu195/",
+    install_requires = ['pandas','requests', 'bs4'],
+    #classifiers like program is suitable for python3, just leave as it is.
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
