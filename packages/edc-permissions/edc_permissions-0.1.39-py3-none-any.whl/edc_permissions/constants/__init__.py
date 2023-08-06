d4d38adb8ad4455a40b3f084a552e8020f797b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:54 +0000

    Add package edc-permissions/0.1.39/edc_permissions-0.1.39-py3-none-any.whl

---
+++ packages/edc-permissions/edc_permissions-0.1.39-py3-none-any.whl/edc_permissions/constants/__init__.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+
+from .dashboard_codenames import DASHBOARD_CODENAMES
+from .default_codenames import DEFAULT_CODENAMES
+from .navbar_codenames import NAVBAR_CODENAMES
+from .group_names import (
+    ACCOUNT_MANAGER,
+    ADMINISTRATION,
+    AE_REVIEW,
+    PII_VIEW,
+    EVERYONE,
+    AUDITOR,
+    CELERY_MANAGER,
+    CLINIC,
+    LAB,
+    LAB_VIEW,
+    PHARMACY,
+    PII,
+    EXPORT,
+    DATA_MANAGER,
+    DATA_QUERY,
+    RANDO,
+    REVIEW,
+    DEFAULT_GROUP_NAMES,
+    SITE_DATA_MANAGER,
+    SCREENING,
+    UNBLINDING_REVIEWERS,
+    UNBLINDING_REQUESTORS,
+)
