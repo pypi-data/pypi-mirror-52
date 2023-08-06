c86861881e42a517c69778095451599bb3c81a71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:05 +0000

    Add package pydantic-jsonapi/0.4.0/pydantic_jsonapi-0.4.0.tar.gz

---
+++ packages/pydantic-jsonapi/pydantic_jsonapi-0.4.0.tar.gz/pydantic_jsonapi-0.4.0/pydantic_jsonapi/__init__.py
@@ -0,0 +1,9 @@
+from .request import JsonApiRequest
+from .response import JsonApiResponse
+from .factory import JsonApiModel
+
+__all__ = [
+    'JsonApiModel',
+    'JsonApiRequest',
+    'JsonApiResponse',
+]
