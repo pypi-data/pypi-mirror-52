ef49a9c71e9ba915944b522f354df3956506e9c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:56 +0000

    Add package TakeSpellChecker/0.0.5/TakeSpellChecker-0.0.5.tar.gz

---
+++ packages/TakeSpellChecker/TakeSpellChecker-0.0.5.tar.gz/TakeSpellChecker-0.0.5/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()	
+	
+setuptools.setup(
+    name="TakeSpellChecker",
+    version="0.0.5",
+    author="Karina Tiemi Kato",
+    author_email="karinatkato@gmail.com",
+    description="Machine learning spell check package that combines word's context with characters similarity.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/karinatk/TakeSpellChecker",
+    packages=setuptools.find_packages(),
+	install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
