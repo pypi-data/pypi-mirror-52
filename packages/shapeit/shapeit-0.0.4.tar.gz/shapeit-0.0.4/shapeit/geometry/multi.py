9c542bb9cb6d03cf24c3996ee356f821ab86fb25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:41 +0000

    Add package shapeit/0.0.4/shapeit-0.0.4.tar.gz

---
+++ packages/shapeit/shapeit-0.0.4.tar.gz/shapeit-0.0.4/shapeit/geometry/multi.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Created on 9/11/19 by Pat Daburu
+"""
+.. currentmodule:: shapeit.geometry.multi
+.. moduleauthor:: Pat Daburu <pat@daburu.net>
+
+This module contains multipart geometry objects.
+"""
+from typing import Mapping, Union
+from shapely.geometry import MultiLineString, MultiPolygon
+from .base import SrGeometry1D, SrGeometry2D
+from ..sr import Sr, WGS_84
+
+
+class SrMultiPolygon(SrGeometry2D):
+    """
+    A spatially referenced multipolygon geometry.
+    """
+    def __init__(
+            self,
+            base_geometry: Union[MultiPolygon, Mapping],
+            sr_: Sr = WGS_84
+    ):
+        super().__init__(base_geometry=base_geometry, sr_=sr_)
+
+    @property
+    def multipolygon(self) -> MultiPolygon:
+        """
+        Get the base geometry as a `shapely.geometry.MultiPolygon`.
+        """
+        return self._base_geometry
+
+
+class SrMultiPolyline(SrGeometry1D):
+    """
+    A spatially referenced polyline geometry.
+    """
+    def __init__(
+            self,
+            base_geometry: Union[MultiLineString, Mapping],
+            sr_: Sr = WGS_84
+    ):
+        super().__init__(base_geometry=base_geometry, sr_=sr_)
+
+    @property
+    def multilinestring(self) -> MultiLineString:
+        """
+        Get the base geometry as a `shapely.geometry.Polygon`.
+        """
+        return self._base_geometry
+
+
+SrMultiLinestring = (
+    SrMultiPolyline
+)  #: This is an alias for :py:class:`SrMultiPolyline`
