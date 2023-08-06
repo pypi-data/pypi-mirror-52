25633c0907303064b9c49be83726c323a88c25ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:39 +0000

    Add package tencentserverless/0.1.7/tencentserverless-0.1.7.tar.gz

---
+++ packages/tencentserverless/tencentserverless-0.1.7.tar.gz/tencentserverless-0.1.7/README.rst
@@ -0,0 +1,17 @@
+============================
+The call Function of Serverless Cloud Function
+# This is a local tools for SCF.
+## Quick Start
+
+First, install the library:
+
+```
+$ pip install tencent-serverless-python
+```
+
+or download source code from github and install:
+```
+$ git clone https://github.com/tencentyun/tencent-serverless-python.git
+$ cd tencent-serverless-python
+$ python setup.py -e .
+```
---
+++ packages/tencentserverless/tencentserverless-0.1.7.tar.gz/tencentserverless-0.1.7/setup.py
@@ -0,0 +1,37 @@
+import os
+import io
+import setuptools
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', os.linesep)
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+def read_requirements(req='requirements.txt'):
+    content = read(os.path.join(req))
+    return [line for line in content.split(os.linesep)
+            if not line.strip().startswith('#')]
+
+setuptools.setup(
+    name='tencentserverless',
+    version='0.1.7',
+    keywords='scf',
+    description='This is callFunction for SCF.',
+    long_description_content_type='text/markdown',
+    long_description=io.open(
+        os.path.join(
+            os.path.dirname(__file__),
+            'README.md'
+        ), encoding='utf-8'
+    ).read(),
+    author='Tencent Cloud',
+    author_email='qcloud_middleware@qq.com',
+    url='https://github.com/tencentyun/tencent-serverless-python',
+    packages=setuptools.find_packages(),
+    install_requires=read_requirements('requirements.txt'),
+    license='Apache License 2.0'
+)
