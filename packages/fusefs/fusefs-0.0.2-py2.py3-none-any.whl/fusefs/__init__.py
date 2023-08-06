ec8f896337f283385f6449c1eda4c367bce26aba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:06 +0000

    Add package fusefs/0.0.2/fusefs-0.0.2-py2.py3-none-any.whl

---
+++ packages/fusefs/fusefs-0.0.2-py2.py3-none-any.whl/fusefs/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for FuseFS."""
+
+__author__ = """Andrew Phillips"""
+__email__ = 'skeledrew@gmail.com'
+__version__ = '0.0.2'
+
+
+from .fusefs import mount_fs
