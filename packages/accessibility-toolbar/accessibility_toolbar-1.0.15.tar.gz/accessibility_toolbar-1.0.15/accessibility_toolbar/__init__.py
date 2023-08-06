45108652613aef786900c7c21d4575dcfdb610bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:16 +0000

    Add package accessibility-toolbar/1.0.15/accessibility_toolbar-1.0.15.tar.gz

---
+++ packages/accessibility-toolbar/accessibility_toolbar-1.0.15.tar.gz/accessibility_toolbar-1.0.15/accessibility_toolbar/__init__.py
@@ -0,0 +1,10 @@
+import pkg_resources
+
+__version__ = pkg_resources.require("accessibility_toolbar")[0].version
+version_info = pkg_resources.parse_version(__version__)
+
+def _jupyter_nbextension_paths():
+    return [dict(section="notebook",
+                 src=".",
+                 dest="accessibility_toolbar",
+                 require="accessibility_toolbar/main")]
