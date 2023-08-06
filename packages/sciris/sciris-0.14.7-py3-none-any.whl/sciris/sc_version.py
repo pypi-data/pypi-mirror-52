10dceb80b91029b6796b23347abe28f2eb47fa6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:02 +0000

    Add package sciris/0.14.7/sciris-0.14.7-py3-none-any.whl

---
+++ packages/sciris/sciris-0.14.7-py3-none-any.whl/sciris/sc_version.py
@@ -0,0 +1,5 @@
+__all__ = ['__version__', '__versiondate__', '__license__']
+
+__version__      = '0.14.7'
+__versiondate__  = '2019-09-19'
+__license__      = 'Sciris %s (%s) -- (c) Sciris.org' % (__version__, __versiondate__)
