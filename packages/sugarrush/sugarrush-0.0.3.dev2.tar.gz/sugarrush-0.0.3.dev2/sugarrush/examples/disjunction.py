98a0f2a6f55f04735f0444e23107e059cf611bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:12 +0000

    Add package sugarrush/0.0.3.dev2/sugarrush-0.0.3.dev2.tar.gz

---
+++ packages/sugarrush/sugarrush-0.0.3.dev2.tar.gz/sugarrush-0.0.3.dev2/sugarrush/examples/disjunction.py
@@ -0,0 +1,93 @@
+from utils import power_set
+from solver import SugarRush
+from examples.langford import langford, print_langford_solution
+from examples.parity_board import parity_board
+
+def power_set_literals(lits):
+    for subset in power_set(lits):
+        yield [lit if lit in subset else -lit for lit in lits]
+        #yield [1 if lit in subset else 0 for lit in lits]
+
+def enumeration_test(solver, variables):
+    satisfying_assignments = []
+    unsatisfying_assignments = []
+    for lits in power_set_literals(variables):
+        res = solver.solve(assumptions=lits)
+        bin_lits = [1 if lit > 0 else 0 for lit in lits]
+        if res:
+            satisfying_assignments.append(tuple(bin_lits))
+        else:
+            unsatisfying_assignments.append(tuple(bin_lits))
+
+    print("Satisfying assignments:")
+    #[print(lits) for lits in sorted(satisfying_assignments)]
+    print(set([sum(lits) for lits in satisfying_assignments]))
+    print()
+    #print("False positives:")
+    #[print(lits) for lits in sorted(satisfying_assignments) if sum(lits[:3]) < 2]
+    
+    print("Unsatisfying assignments:")
+    print(set([sum(lits) for lits in unsatisfying_assignments]))
+    #[print(lits) for lits in sorted(unsatisfying_assignments)]
+
+def langford_test(n):
+    with SugarRush() as solver:
+        X = langford(solver, n)
+
+        print("n:", n)
+        solver.print_stats()
+
+        satisfiable = solver.solve()
+        print("Satisfiable:", satisfiable)
+        if not satisfiable:
+            return
+
+        print_langford_solution(solver, X)
+
+def negate_test():
+    n = 3
+    solver = SugarRush()
+    X = [solver.var() for _ in range(n)]
+    bound_X = solver.atmost(X, bound=2)
+    bound_X_neg = solver.negate(bound_X)
+    solver.add(bound_X_neg)
+    #enumeration_test(solver, X)
+    enumeration_test(solver, list(sorted(solver.lits - set([0]))))
+    
+def disjunction_test():
+    n = 10
+    solver = SugarRush()
+    X = [solver.var() for _ in range(n)]
+
+    bounds_even = [solver.equals(X, k) for k in range(0, n+1, 2)]
+    bound = solver.disjunction(bounds_even)
+
+    solver.add(bound)
+    enumeration_test(solver, X)
+
+    ''' successful test
+    bound_X_1 = solver.equals(X, bound=0)
+    bound_X_2 = solver.equals(X, bound=3)
+    bound_X_1or2 = solver.disjunction([bound_X_1, bound_X_2])
+    print(bound_X_1)
+    print()
+    print(bound_X_2)
+    print()
+    print(bound_X_1or2)
+    print()
+    solver.add(bound_X_1or2)
+
+    enumeration_test(solver, X)
+    '''
+
+def main():
+    #langford_test(20)
+
+    #negate_test()
+
+    #disjunction_test()
+
+    parity_board()
+
+if __name__ == '__main__':
+    main()
---
+++ packages/sugarrush/sugarrush-0.0.3.dev2.tar.gz/sugarrush-0.0.3.dev2/sugarrush/examples/langford.py
@@ -0,0 +1,57 @@
+from collections import defaultdict
+
+def langford(solver, n):
+    X = [[solver.var() for _ in range(2*n - k - 2)] for k in range(n)]
+
+    for row in X:
+        solver.add(solver.equals(row))
+        #solver.equals(row)
+
+    position2covering = defaultdict(list)
+    for k, row in enumerate(X):
+        for i, var in enumerate(row):
+            position2covering[i].append(var)
+            position2covering[i + k + 2].append(var)
+
+    for lits in position2covering.values():
+        solver.add(solver.equals(lits))
+        #solver.equals(lits)
+
+    return X
+
+def print_langford_solution(solver, X):
+    position2num = {}
+    for k, row in enumerate(X):
+        row_solve = [solver.solution_value(var) for var in row]
+        #print(row_solve)
+        idx0 = row_solve.index(True)
+        idx1 = idx0 + k + 2
+
+        if idx0 in position2num:            
+            print("{} already taken by {}".format(idx0, position2num[idx0]))
+        else:
+            position2num[idx0] = k + 1
+
+        if idx1 in position2num:            
+            print("{} already taken by {}".format(idx1, position2num[idx1]))
+        else:
+            position2num[idx1] = k + 1
+
+    _, nums = zip(*sorted(position2num.items()))
+    langford_str = ", ".join(map(str, nums))
+    print(langford_str)
+
+def run_langford(n=4):
+    from sugarrush.solver import SugarRush
+    with SugarRush() as solver:
+        X = langford(solver, n)
+
+        print("n:", n)
+        solver.print_stats()
+
+        satisfiable = solver.solve()
+        print("Satisfiable:", satisfiable)
+        if not satisfiable:
+            return
+
+        print_langford_solution(solver, X)
