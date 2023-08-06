205e4486f71b2613758eb14587a30e09bddf9854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:30 +0000

    Add package ebt/2.0.90/ebt-2.0.90.tar.gz

---
+++ packages/ebt/ebt-2.0.90.tar.gz/ebt-2.0.90/ebt_files/dd.py
@@ -0,0 +1,26 @@
+from ebt_system import popen as __popen
+import logging
+import sys
+from multiprocessing import cpu_count
+
+
+log = logging.getLogger('__main__')
+
+
+def create(source, dest, bs='8M', compress_level=0, compress_threads=cpu_count(), passwd=None):
+    assert isinstance(source, str), '{1}.{2}: variable "{0}" has wrong type.' \
+        .format('source', __name__, sys._getframe().f_code.co_name)
+    assert isinstance(dest, str), '{1}.{2}: variable "{0}" has wrong type.' \
+        .format('dest', __name__, sys._getframe().f_code.co_name)
+    assert isinstance(bs, str), '{1}.{2}: variable "{0}" has wrong type.' \
+        .format('bs', __name__, sys._getframe().f_code.co_name)
+    if compress_level > 0:
+        command = 'dd if={0} bs={2} |pigz -c -{3} -p {4} |dd of={1} bs={2}'.format(source, dest, bs, compress_level,
+                                                                                   compress_threads)
+    else:
+        command = 'dd if={0} of={1} bs={2}'.format(source, dest, bs)
+    if passwd is not None:
+        command = '{0} |openssl enc -aes-256-cbc -k "{1}"'.format(command, passwd)
+
+    __popen(command, shell=True)
+    log.info('Successful create copy of {0} to {1}'.format(source, dest))
