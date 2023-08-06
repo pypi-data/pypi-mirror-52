16e21fac7ce22eef759e4a3d209697546e3ed495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:16 +0000

    Add package func-argparse/1.0.0/func_argparse-1.0.0.tar.gz

---
+++ packages/func-argparse/func_argparse-1.0.0.tar.gz/func_argparse-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="func_argparse",
+    version="1.0.0",
+    description="Generate CLI ArgumentParser from a function signature.",
+    author="gwenzek",
+    packages=["func_argparse"],
+    install_requires=[],
+)
