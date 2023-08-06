b178ecbb8b0abafb8be118dfc6a3cb0daf945d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:07 +0000

    Add package flask-sqlalchemy-raw/0.0.1/flask_sqlalchemy_raw-0.0.1.tar.gz

---
+++ packages/flask-sqlalchemy-raw/flask_sqlalchemy_raw-0.0.1.tar.gz/flask_sqlalchemy_raw-0.0.1/flask_sqlalchemy_raw/__init__.py
@@ -0,0 +1,29 @@
+"""
+usage:
+flask_sqlalchemy execute raw sql.
+
+sample:
+from functools import partial
+fetchone = partial(fetchone, db)
+"""
+
+from typing import Tuple, List, Dict
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.engine import ResultProxy
+
+row_proxy_2_dict = lambda row_proxy: dict(row_proxy.items())
+
+
+def fetchone(sql_str: str, db: SQLAlchemy) -> Tuple:
+    return db.engine.execute(sql_str).fetchone()
+
+
+def fetchone_dict(sql_str: str, db: SQLAlchemy) -> Dict:
+    _records: ResultProxy = db.engine.execute(sql_str)
+    return row_proxy_2_dict(_records)
+
+
+def fetchall_dict(sql_str: str, db: SQLAlchemy) -> List[Dict]:
+    _records: ResultProxy = db.engine.execute(sql_str)
+    return list(map(row_proxy_2_dict, _records))
