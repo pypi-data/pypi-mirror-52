4f3392c897d1eaff438aab58e085cfe27d74ae5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:02 +0000

    Add package jungle1/1.0.0/jungle1-1.0.0.tar.gz

---
+++ packages/jungle1/jungle1-1.0.0.tar.gz/jungle1-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name="jungle1",
+    version="1.0.0",
+    py_modules=["jungle1"],
+    author="lgno",
+    author_email="giserwangziheng@163.com",
+    description="A simple printer of jungle1 lists"
+
+)
