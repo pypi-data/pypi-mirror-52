4bae09d08f7cbdda18aa6d37e2d6e4577e96aa3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:51 +0000

    Add package puzzlewheel/1.0/puzzlewheel-1.0.tar.gz

---
+++ packages/puzzlewheel/puzzlewheel-1.0.tar.gz/puzzlewheel-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'puzzlewheel',
+    package =['puzzlewheel','puzzle'],
+    description = 'python wheel to help with the puzzel.',
+    version = '1.0',
+    url = '',
+    author = 'alice bom'
+)
