b60217b6f5695cb0af1a6696aeb0825e90fec61e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:19 +0000

    Add package mdu-pytools/0.1.1/mdu-pytools-0.1.1.tar.gz

---
+++ packages/mdu-pytools/mdu-pytools-0.1.1.tar.gz/mdu-pytools-0.1.1/README.md
@@ -0,0 +1,59 @@
+# MDU Python Tools
+
+[![CircleCI](https://circleci.com/gh/MDU-PHL/mdu-pytools.svg?style=svg)](https://circleci.com/gh/MDU-PHL/mdu-pytools) ![PyPI - Python Version](https://img.shields.io/pypi/pyversions/mdu-pytools) ![PyPI](https://img.shields.io/pypi/v/mdu-pytools) ![PyPI - License](https://img.shields.io/pypi/l/mdu-pytools)
+
+- [MDU Python Tools](#mdu-python-tools)
+  - [Background](#background)
+  - [Tools](#tools)
+    - [mdu-merge-ngs-lanes](#mdu-merge-ngs-lanes)
+  - [Development](#development)
+    - [Development environment](#development-environment)
+
+## Background
+
+Some simple tools in python for MDU
+
+## Tools
+
+### mdu-merge-ngs-lanes
+
+Use it to correctly merge lanes from an Illumina run into the a single FASTQ.
+
+Get help:
+
+```bash
+mdu-merge-ngs-lanes --help
+```
+
+Basic usage:
+
+```bash
+mdu-merge-ngs-lanes -i /path/to/fastq_folder -o /path/to/output > cmd.sh
+```
+
+Advanced usage:
+
+You can split the output to muliple subfolders of the output folder by adding `--subfolder`
+to the command line. The option can be used multiple times, and takes two space separated values as input:
+`path` `regex`. The `path` gives a name of the subfolder in the output folder, and the `regex` expression
+determines which samples go in that subfolder.
+
+For instance, the command below will split samples starting the NTC in to a subfolder called `ntc`,
+while all other samples will be added to a subfolder called `data`.
+
+```bash
+mdu-merge-ngs-lanes -i /path/to/fastq -o /path/to/output --subfolder 'data' '(?!NTC).*' --subfolder 'ntc' '(?<=NTC).*' > cmd.sh
+```
+
+## Development
+
+### Development environment
+
+To develop with the same environment use `vagrant` and `virtualbox`:
+
+```bash
+vagrant up
+vagrant ssh
+```
+
+Once logged in to the VM, the shared folder is in `/vagrant`.
---
+++ packages/mdu-pytools/mdu-pytools-0.1.1.tar.gz/mdu-pytools-0.1.1/setup.py
@@ -0,0 +1,54 @@
+"""
+Setup mdu-pytools
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+
+# Load version string from package
+from mdu_pytools import __version__ as version
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="mdu-pytools",  # Required
+    version=version,  # Required
+    description="Small tools/scripts written in Python for MDU",
+    long_description=long_description,  # Optional
+    long_description_content_type="text/markdown",  # Optional
+    url="https://github.com/MDU-PHL/mdu-pytools",  # Optional
+    author="MDU Bioinformatics",  # Optional
+    author_email="andersgs@gmail.com",  # Optional
+    classifiers=[  # Optional
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="bioinformatics microbial-genomics",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),  # Required
+    python_requires=">=3.6, <4",
+    install_requires=["pandas", "loguru", "click", "tqdm"],  # Optional
+    extras_require={  # Optional
+        "dev": ["pre-commit", "pipenv", "bumpversion", "invoke"],
+        "test": ["pytest", "pytest-cov"],
+    },
+    #    package_data={"package_name": ["data/example_data.csv"]},  # Optional
+    entry_points={
+        "console_scripts": ["mdu-merge-ngs-lanes=mdu_pytools.merge_ngs_lanes:main"]
+    },  # Optional
+    project_urls={  # Optional
+        "Bug Reports": "https://github.com/MDU-PHL/mdu-pytools/issues",
+        # "Funding": "https://donate.pypi.org",
+        # "Say Thanks!": "http://saythanks.io/to/example",
+        "Source": "https://github.com/MDU-PHL/mdu-pytools/",
+    },
+)
