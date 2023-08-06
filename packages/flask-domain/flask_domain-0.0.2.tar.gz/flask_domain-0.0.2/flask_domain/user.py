f5580a2a7be43996d9702fa9106bc94a12f263f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:59 +0000

    Add package flask-domain/0.0.2/flask_domain-0.0.2.tar.gz

---
+++ packages/flask-domain/flask_domain-0.0.2.tar.gz/flask_domain-0.0.2/flask_domain/user.py
@@ -0,0 +1,23 @@
+"""
+interface api;
+"""
+from typing import Optional, Mapping, List
+
+
+class User:
+
+    def __init__(self, stored):
+        self.stored = stored
+
+    def has_permission(self, user: str, block: str, action: Optional[str] = None) -> bool:
+        """
+        check the user has the action permission for the block.
+        :param user:
+        :param block:
+        :param action:
+        :return:
+        """
+        return self.stored.has_permission(user, block, action)
+
+    def filter(self, condition: Mapping) -> List:
+        return self.stored.filter(condition)
