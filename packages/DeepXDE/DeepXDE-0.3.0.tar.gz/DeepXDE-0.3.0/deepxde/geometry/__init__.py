a6e13b29ebb68fec1eaad95fc61967820b22ec08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:57 +0000

    Add package DeepXDE/0.3.0/DeepXDE-0.3.0.tar.gz

---
+++ packages/DeepXDE/DeepXDE-0.3.0.tar.gz/DeepXDE-0.3.0/deepxde/geometry/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+from .csg import CSGDifference
+from .csg import CSGIntersection
+from .csg import CSGUnion
+from .geometry_1d import Interval
+from .geometry_2d import Disk
+from .geometry_2d import Polygon
+from .geometry_2d import Rectangle
+from .geometry_2d import Triangle
+from .geometry_nd import Hypercube
+from .geometry_nd import Hypersphere
+from .timedomain import GeometryXTime
+from .timedomain import TimeDomain
