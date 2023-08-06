fde5addb7070cda898cfd54cbbe9123fa5331329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:54 +0000

    Add package dialect/0.1.13/dialect-0.1.13.tar.gz

---
+++ packages/dialect/dialect-0.1.13.tar.gz/dialect-0.1.13/dialect/context.py
@@ -0,0 +1,34 @@
+from .macro import Macro
+
+class Context:
+    def __init__(self, parent=None, __path__=None):
+        self.parent = parent
+        self._context = {}
+        self.ret_val = None
+        self.__path__ = __path__ 
+        self.macro = Macro()
+
+    def get(self, key):
+        if key in self._context:
+            return self._context[key]
+        elif self.parent is not None:
+            return self.parent.get(key)
+        else:
+            return ('error', 'No key %s' % key)
+
+    def set(self, key, value):
+        self._context[key[1]] = value
+
+    def flag_return(self, val):
+        self.ret_val = val
+
+    def check(self, identifier):
+        if identifier[1] in self._context or (self.parent is not None and self.parent.check(identifier)):
+            return ('bool', True)
+        return ('bool', False)
+
+    def delete(self, identifier):
+        if identifier[1] in self._context:
+            del self._context[identifier[1]]
+        elif self.parent is not None and self.parent.check(identifier):
+            self.parent.delete(identifier)
