c5a2f122c790fa2e176ebad98c2d379d6bfffacc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:25 +0000

    Add package shipmnts-utils/0.0.37/shipmnts-utils-0.0.37.tar.gz

---
+++ packages/shipmnts-utils/shipmnts-utils-0.0.37.tar.gz/shipmnts-utils-0.0.37/shipmnts_utils/sql_executor.py
@@ -0,0 +1,30 @@
+from django.db import connection
+
+
+def execute_select_query(query, output_format='tuple', fetch=None):
+    tuple_data, json_data = None, None
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        if fetch and fetch == "one":
+            tuple_data = cursor.fetchone()
+        else:
+            tuple_data = cursor.fetchall()
+    finally:
+        cursor.close()
+        if output_format=='json':
+            if tuple_data:
+                json_data = list()
+                for row in tuple_data:
+                    for key in cursor.description:
+                        json_data.append({key[0]: value for value in row})
+            return json_data
+        return tuple_data
+
+def execute_update_query(query):
+    c = connection.cursor()
+    try:
+        c.execute(query)
+    finally:
+        c.close()
+
