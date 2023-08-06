e25056bbccd848422c31a1222f802c1402d5bc75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:00 +0000

    Add package testepip/1.0.3/testepip-1.0.3.tar.gz

---
+++ packages/testepip/testepip-1.0.3.tar.gz/testepip-1.0.3/pythonpip/teste.py
@@ -0,0 +1,13 @@
+import json
+
+def teste():
+ print('Hello Word')
+ ler = open('testepip/data/dados.json','r')
+ dic = json.load(ler)
+ cont = 0
+ for i in dic['Entrevistas']:
+    print(f'Hello {dic["Entrevistas"][cont]["nome"]}')
+    cont += 1
+
+def main():
+ teste()
