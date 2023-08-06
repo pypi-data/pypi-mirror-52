7da05bd6f3982da756ba6ef50cd797c59330e900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:30 +0000

    Add package enaml/0.10.4/enaml-0.10.4-cp27-none-win32.whl

---
+++ packages/enaml/enaml-0.10.4-cp27-none-win32.whl/enaml/layout/linear_symbolic.py
@@ -0,0 +1,23 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2013, Nucleic Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#------------------------------------------------------------------------------
+from abc import ABCMeta
+
+
+from enaml.compat import with_metaclass
+import kiwisolver as kiwi
+
+
+class LinearSymbolic(with_metaclass(ABCMeta, object)):
+    """ An abstract base class for testing linear symbolic interfaces.
+
+    """
+
+
+LinearSymbolic.register(kiwi.Variable)
+LinearSymbolic.register(kiwi.Term)
+LinearSymbolic.register(kiwi.Expression)
