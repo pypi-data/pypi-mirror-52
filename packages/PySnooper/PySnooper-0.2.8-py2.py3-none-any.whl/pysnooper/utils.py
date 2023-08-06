0eba7d2ab3b27cd9ffa6546aade88876040ca743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:04 +0000

    Add package PySnooper/0.2.8/PySnooper-0.2.8-py2.py3-none-any.whl

---
+++ packages/PySnooper/PySnooper-0.2.8-py2.py3-none-any.whl/pysnooper/utils.py
@@ -0,0 +1,87 @@
+# Copyright 2019 Ram Rachum and collaborators.
+# This program is distributed under the MIT license.
+
+import abc
+
+import sys
+from .pycompat import ABC, string_types, collections_abc
+
+def _check_methods(C, *methods):
+    mro = C.__mro__
+    for method in methods:
+        for B in mro:
+            if method in B.__dict__:
+                if B.__dict__[method] is None:
+                    return NotImplemented
+                break
+        else:
+            return NotImplemented
+    return True
+
+
+class WritableStream(ABC):
+    @abc.abstractmethod
+    def write(self, s):
+        pass
+
+    @classmethod
+    def __subclasshook__(cls, C):
+        if cls is WritableStream:
+            return _check_methods(C, 'write')
+        return NotImplemented
+
+
+
+file_reading_errors = (
+    IOError,
+    OSError,
+    ValueError # IronPython weirdness.
+)
+
+
+
+def shitcode(s):
+    return ''.join(
+        (c if (0 < ord(c) < 256) else '?') for c in s
+    )
+
+
+def get_repr_function(item, custom_repr):
+    for condition, action in custom_repr:
+        if isinstance(condition, type):
+            condition = lambda x, y=condition: isinstance(x, y)
+        if condition(item):
+            return action
+    return repr
+
+
+def get_shortish_repr(item, custom_repr=(), max_length=None):
+    repr_function = get_repr_function(item, custom_repr)
+    try:
+        r = repr_function(item)
+    except Exception:
+        r = 'REPR FAILED'
+    r = r.replace('\r', '').replace('\n', '')
+    if max_length:
+        r = truncate(r, max_length)
+    return r
+
+
+def truncate(string, max_length):
+    if (max_length is None) or (len(string) <= max_length):
+        return string
+    else:
+        left = (max_length - 3) // 2
+        right = max_length - 3 - left
+        return u'{}...{}'.format(string[:left], string[-right:])
+
+
+def ensure_tuple(x):
+    if isinstance(x, collections_abc.Iterable) and \
+                                               not isinstance(x, string_types):
+        return tuple(x)
+    else:
+        return (x,)
+
+
+
