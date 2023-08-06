3696a6ca05178dab71552d8f124819a33c87c7bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:22 +0000

    Add package LIBTwinSVM/0.3.0/LIBTwinSVM-0.3.0.tar.gz

---
+++ packages/LIBTwinSVM/LIBTwinSVM-0.3.0.tar.gz/LIBTwinSVM-0.3.0/README.md
@@ -0,0 +1,186 @@
+# LIBTwinSVM
+<a href="https://opensource.org/licenses/GPL-3.0"><img src="https://img.shields.io/badge/License-GPL%20v3-blue.svg" alt="License"></a>
+<a href="https://travis-ci.org/mir-am/LIBTwinSVM"><img src="https://api.travis-ci.org/mir-am/LIBTwinSVM.svg?branch=master" alt="Travis-CI"></a>
+[![Documentation Status](https://readthedocs.org/projects/libtwinsvm/badge/?version=latest)](https://libtwinsvm.readthedocs.io/en/latest/?badge=latest)
+[![PyPI version](https://badge.fury.io/py/LIBTwinSVM.svg)](https://badge.fury.io/py/LIBTwinSVM)
+
+
+# Contents
+1. [Core Features](#core-features)
+2. [Installation Guide](#installation-guide)
+3. [Quick Start](#quick-start)
+4. [Documentation](#documentation)
+5. [Development](#development) 
+6. [License](#license)
+
+## Core Features
+LIBTwinSVM is an easy-to-use implementation of Twin Support Vector Machine.  It is licensed under the terms of GNU GPL v3. This application comes with a user interface which makes using the algorithm so handy for Data Scientists, Machine Learning Researchers and whoever else that is interested in Machine Learning.
+<br>
+
+- A **simple** and **user-friendly Graphical User Interface** (GUI).
+- Supports both **standard TwinSVM** and **Least Squares TwinSVM** classifiers.
+- Easy to import data in **CSV** & **LIBSVM** format.
+- A dataset can be loaded with **shuffling** and **normalization**.
+- A **fast optimizer** (clipDCD) is improved and implemented in C++ to solve optimization problems of TwinSVMs.
+- Supports **Linear**, **RBF** and **Rectangular** kernel.
+- Supports **Binary** and **Multi-class** classification (One-vs-All & One-vs-One).
+- The OVA and OVO estimators are **compatible with scikit-learn** tools such as GridSearchCV, cross_val_score, etc.
+- A classifier can be evaluated using either **K-fold cross-validation** or **Training/Test split**.
+- Supports **grid search** over estimators' hyper-parameters.
+- The detailed classification results can be saved in an **Excel-format spreadsheet** file.
+- The classification results can be logged during the grid search process to not lose results in case of power failure.
+- A **feature-rich visualization tool** to show decision boundaries and geometrical interpretation of TwinSVMs.
+- The best-fitted classifier can be saved on the disk after the grid search process.
+- The pre-trained models can be loaded and evaluated on the test samples.
+
+
+## Installation Guide
+### Dependencies
+
+LIBTwinSVM depends on the following packages.
+
+|   Package                                      |                      Description                               |       License         |
+| ---------------------------------------------- | -------------------------------------------------------------- | --------------------- |
+| [Cython](https://cython.org/)                  |  To use C++ code in Python.                                    | Apache License 2.0    |
+| [NumPy](https://www.numpy.org/)                |  Fast linear algebra operations.                               | BSD 3-Clause          | 
+| [Matplotlib](https://matplotlib.org/)          |  Visualization and geometrical representation of classifiers.  | [Matplotlib License](https://matplotlib.org/users/license.html)                    |
+| [PyQt5](https://www.riverbankcomputing.com/software/pyqt/intro)  | To create a GUI for using the LIBTwinSVM's features.| GPL |
+| [Scikit-learn](https://scikit-learn.org/)      | For TwinSVM-based models evaluation and selection.             | BSD 3-Clause |
+| [Pandas](https://pandas.pydata.org/)           | For reading and processing datasets.                           | BSD 3-Clause |
+| [XlsxWriter](https://xlsxwriter.readthedocs.io/) | For saving classification results in an Excel file.          | BSD 3-Clause |
+| [Joblib](https://joblib.readthedocs.io)   | For saving and loading TwinSVM-based models.                   | BSD 3-Clause |
+| [numpydoc](https://numpydoc.readthedocs.io/en/latest/) | API code documentation.                                | BSD License  |
+
+
+### Quick Installation
+
+For Installing LIBTwinSVM you can choose one of the following commands based on your Operating System and your Python version. Please note that for installing the latest bugfixes and features, we recommend you to install the library from the source. 
+
+* Linux & Mac OS:
+```
+pip3 install libtwinsvm
+```
+
+* Windows:
+```python
+pip install libtwinsvm
+```
+
+### Installation from the source
+
+**1.  Downloading LIBTwinSVM**
+
+First, make sure that [Git](https://git-scm.com/) is installed as it is required for getting the source code. Then open Git in any arbitrary path and enter the following command:
+
+```
+git clone --recursive https://github.com/mir-am/LIBTwinSVM
+```
+
+**2. Downloading Requirements**
+
+Before installing any packages, we recommend you to upgrade pip:
+
+* Linux & Mac OS:
+```
+pip3 install -U pip
+```
+
+* Windows:
+```
+pip install -U pip
+```
+
+For LIBTwinSVM installation, *Numpy* and *Cython* must be installed. You can install them by entering the following commands in your terminal. If you already have *Cython* and *Numpy* installed, you can skip to the next section.
+
+* Linux & Mac OS: For installing Numpy & Cython on your computer, you should enter the following command in the terminal.
+```
+pip3 install numpy cython
+```
+
+**Note for Linux users:** You should also install the following packages for your distribution:
+
+Debian-based Linux distro:
+```
+sudo apt-get install python3-tk liblapack-dev libblas-dev
+```
+
+RPM-based Linux distro:
+```
+sudo yum install python3-tkinter lapack-devel blas-devel
+```
+
+
+* Windows: for installing the requirements on windows, you should enter the following command in the command prompt. 
+```
+pip install numpy cython
+```
+
+**3. Installing LIBTwinSVM**
+
+Go to LIBTwinSVM folder where you have downloaded the source code in step 2. Then enter the following command in the terminal:
+
+* Linux & Mac OS:
+```python
+pip3 install .
+```
+* Windows:
+```python
+pip install .
+```
+
+### Uninstall LIBTwinSVM
+
+For uninstalling LIBTwinSVM, enter the following command in the terminal:
+
+* Linux & Mac OS:
+```python
+pip3 uninstall libtsvm
+```
+* Windows:
+```python
+pip uninstall libtsvm
+```
+
+## Quick start
+
+After LIBTwinSVM was installed, for running the GUI application, open the terminal and enter the following code:
+* Linux & Mac OS:
+```python
+python3 -m libtsvm
+```
+* Windows:
+```python
+python -m libtsvm
+```
+## Dataset Format 
+
+LIBTwinSVM supports data files with the following formats:
+- **CSV** files with '*.csv*' extention.
+- **LIBSVM** files with '*.libsvm*' extention.
+
+Note that for importing data as a CSV file, the dataset has to comply the following terms: 
+1. The first column has to be the dataset labels. Moreover, labels of positive and negative samples should be 1 and -1, respectively.
+2. The first row can be dataset's headernames. (Optional)
+3. All the values in dataset except headernames should be numerical. Nominal values are not allowed. <br/>
+
+To help you prepare your dataset and test the program, three datasets are included [here](https://github.com/mir-am/LIBTwinSVM/tree/master/dataset).
+
+## Documentation
+Usage examples and API reference can be found on the project's [Read the Docs page](https://libtwinsvm.readthedocs.io/en/latest/). 
+
+## Development
+### Tests
+After installing the library, you can run unit tests to ensure that your installation is valid. To do so, you need to install pytest package with the following command:
+
+```
+pip install pytest
+```
+
+Next, execute the following command in the root of the project directory:
+
+```
+pytest tests
+```
+
+## License
+LIBTwinSVM library is licensed under the terms of GNU General Public License v3. This library can be used for both academic and commercial purposes. For more information, check out the [LICENSE](https://github.com/mir-am/LIBTwinSVM/blob/master/LICENSE.txt) file.
---
+++ packages/LIBTwinSVM/LIBTwinSVM-0.3.0.tar.gz/LIBTwinSVM-0.3.0/setup.py
@@ -0,0 +1,322 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# LIBTwinSVM: A Library for Twin Support Vector Machines
+# Developers: Mir, A. and Mahdi Rahbar
+# License: GNU General Public License v3.0
+
+"""
+Installation of LIBTwinSVM library.
+"""
+
+from libtsvm import __version__
+from pkg_resources import parse_version
+from shutil import copy
+# from setuptools import find_packages, setup, Command
+import os
+import subprocess
+import sys
+import traceback
+
+
+# Package meta-data.
+NAME = "LIBTwinSVM"
+DESCRIPTION = "LIBTwinSVM: A Library for Twin Support Vector Machines."
+URL = 'https://github.com/mir-am/LIBTwinSVM'
+EMAIL = "mir-am@hotmail.com, mahdirahbar@gmail.com"
+AUTHOR = "Mir, A. and Mahdi Rahbar"
+REQUIRES_PYTHON = '>=3.5'
+VERSION = '%s' % __version__
+REQ_PACKAGES = ["cython", "numpy", "matplotlib", "pyQt5", "sklearn", "pandas",
+                "xlsxwriter", "joblib", "numpydoc==0.7.0"]
+PKG_DATA = {}
+DATA_FILES = []
+
+NUMPY_MIN_VERSION = '1.14.0'
+CYTHON_MIN_VERSION = '0.28'
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+LONG_DESCRIPTION = ("Please check out the project's `GitHub page "
+                    "<https://github.com/mir-am/LIBTwinSVM>`_ for installation"
+                    " guide and documentation.")
+
+# using setuptool features
+SETUPTOOLS_COMMANDS = set([
+    'develop', 'release', 'install', 'bdist_egg', 'bdist_rpm',
+    'bdist_wininst', 'install_egg_info', 'build_sphinx',
+    'egg_info', 'easy_install', 'upload', 'bdist_wheel',
+    '--single-version-externally-managed',
+])
+if SETUPTOOLS_COMMANDS.intersection(sys.argv):
+    import setuptools
+
+    class BinaryDistribution(setuptools.Distribution):
+        def has_ext_modules(foo):
+            return True
+
+    if 'bdist_wheel' in sys.argv or 'install' in sys.argv:
+
+        if sys.platform == 'win32':
+
+            DATA_FILES = [(os.path.join('libtsvm', 'optimizer'),
+                         [os.path.join('libtsvm', 'optimizer',
+                                       'lapack_win64_MT.dll'),
+             os.path.join('libtsvm', 'optimizer', 'blas_win64_MT.dll')])]
+
+            print("Added LAPACK and BLAS DLLs to the wheel for Windows "
+                  "platform.")
+
+    extra_setuptools_args = dict(
+        zip_safe=False,  # the package can run out of an .egg file
+        include_package_data=True,
+        distclass=BinaryDistribution,
+        data_files=DATA_FILES,
+        extras_require={
+            'alldeps': (
+                'numpy >= {0}'.format(NUMPY_MIN_VERSION),
+                'cython >= {0}'.format(CYTHON_MIN_VERSION),
+            ),
+        },
+    )
+
+else:
+    extra_setuptools_args = dict()
+
+
+### Utility functinos for installation #######################################    
+def cp_libs_win():
+    """
+    Copies external libraries for installing package on Windows.
+    """
+
+    src_1 = os.path.join('libtsvm', 'optimizer', 'armadillo-code', 'lib_win64',
+                         'lapack_win64_MT.dll')
+    src_2 = os.path.join('libtsvm', 'optimizer', 'armadillo-code', 'lib_win64',
+                         'blas_win64_MT.dll')
+
+    dst = os.path.join('libtsvm', 'optimizer')
+
+    copy(src_1, dst)
+    copy(src_2, dst)
+
+    print("Copied the LAPACK and BLAS libraries to source distribution...")
+
+
+def get_numpy_status():
+    """
+    It checks whether numpy is installed. If so, checks whether it is
+    up-to-date.
+    """
+
+    np_status = {}
+
+    try:
+
+        import numpy
+
+        np_status['version'] = numpy.__version__
+        np_status['up_to_date'] = parse_version(np_status['version']) >=  \
+        parse_version(NUMPY_MIN_VERSION)
+
+    except ImportError:
+
+        traceback.print_exc()
+
+        np_status['version'] = ""
+        np_status['up_to_date'] = False
+
+    return np_status
+
+
+def get_cython_status():
+    """
+    It checks whether Cython is installed. If so, checks whether it is
+    up-to-date.
+    """
+
+    cy_status = {}
+
+    try:
+
+        import cython
+
+        cy_status['version'] = cython.__version__
+        cy_status['up_to_date'] = parse_version(cy_status['version']) >=  \
+        parse_version(CYTHON_MIN_VERSION)
+
+    except ImportError:
+
+        traceback.print_exc()
+
+        cy_status['version'] = ""
+        cy_status['up_to_date'] = False
+
+    return cy_status
+
+
+def check_install_updated(pkg_name, pkg_status, req_str, help_instr):
+    """
+    It checks whether a package is installed and up-to-date.
+    """
+
+    if pkg_status['version']:
+
+        if pkg_status['up_to_date'] is False:
+
+            raise ImportError("Your current version of %s %s is out-of-date. %s %s" %  \
+                              (pkg_name, pkg_status['version'], req_str,
+                               help_instr))
+
+        else:
+
+            print("Found %s %s" % (pkg_name, pkg_status['version']))
+
+    else:
+
+        raise ImportError("%s is not installed. %s %s "
+                          % (pkg_name, req_str, help_instr))
+
+
+# This function is written with inspiration from NumPy's setup.py
+def check_arma_submodule():
+    """
+    Verify that Armadillo submodule is initialized and checked out.
+    """
+
+    if not os.path.exists('.git'):
+        return  # Not a git reposiroty
+
+    help_instr = ("Please use the following command to initialize Armadillo"
+                  " submodule:\ngit submodule update --init")
+
+    # Check submodule is not missing
+    with open('.gitmodules') as f:
+        for l in f:
+            if 'path' in l:
+                p = l.split('=')[-1].strip()
+                if not os.path.exists(p):
+                    raise ValueError('Submodule %s missing.\n%s' % (p,
+                                     help_instr))
+
+    # Check submodule is clean
+    proc = subprocess.Popen(['git', 'submodule', 'status'],
+                            stdout=subprocess.PIPE)
+    status, _ = proc.communicate()
+    status = status.decode("ascii", "replace")
+    for line in status.splitlines():
+        if line.startswith('-') or line.startswith('+'):
+            raise ValueError('Submodule not clean: %s\n%s' % (line,
+                                                              help_instr))
+
+    print("Found Armadillo submodule.")
+
+
+def detect_anaconda_dist():
+    """
+    It detects Anaconda distribution.
+    """
+
+    return True if 'anaconda' in sys.version.lower() else False
+
+
+def configuration(parent_package='', top_path=None):
+
+    from numpy.distutils.misc_util import Configuration
+
+    config = Configuration(None, parent_package, top_path)
+
+    config.set_options(ignore_setup_xxx_py=True,
+                       assume_default_configuration=True,
+                       delegate_options_to_subpackages=True)
+                       # quiet=True)
+
+    config.add_subpackage('libtsvm')
+
+    return config
+
+
+def setup_package():
+
+    global PKG_DATA
+
+    # Check dependencies and requirements ####################################
+    help_instr = ("Please check out the installation guide of the LIBTwinSVM:\n"
+                  "https://libtwinsvm.readthedocs.io/en/latest/")
+    np_req_str = "LIBTwinSVM requires NumPy >= %s.\n" % NUMPY_MIN_VERSION
+    cy_req_str = ("Please install cython with a version >= %s in order to"
+                  " build the LIBTwinSVM library." % CYTHON_MIN_VERSION)
+
+    check_arma_submodule()
+
+    # Check numpy status and its version on users' system
+    np_status = get_numpy_status()
+    cy_status = get_cython_status()
+
+    check_install_updated('Numerical Python (NumPy)', np_status, np_req_str,
+                          help_instr)
+    check_install_updated('Cython', cy_status, cy_req_str, help_instr)
+    ##########################################################################
+
+    # Platform-dependent options
+    if sys.platform == 'win32':
+
+        cp_libs_win()
+
+        PKG_DATA = {'libtsvm': [os.path.join('libtsvm', 'optimizer', '*.dll')]}
+
+    # Remove PyQT5 from dependencies for Anaconda distribution
+    if detect_anaconda_dist():
+        # TODO: Check whether PyQT is installed.
+        print("Detected Anaconda distribution...")
+        REQ_PACKAGES.remove("pyQt5")
+
+    metadata = dict(
+        name=NAME,
+        version=VERSION,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type='text/markdown',
+        author=AUTHOR,
+        author_email=EMAIL,
+        python_requires=REQUIRES_PYTHON,
+        url=URL,
+        package_data=PKG_DATA,
+        #packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+        # If your package is a single module, use this instead of 'packages':
+        # py_modules=['mypackage'],
+
+        # entry_points={
+        #     'console_scripts': ['mycli=mymodule:cli'],
+        # },
+        install_requires=REQ_PACKAGES,
+        license='GNU General Public License v3.0',
+        classifiers=[
+            # Trove classifiers
+            # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Programming Language :: C++',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: Implementation :: CPython',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: POSIX',
+            'Operating System :: Unix',
+            'Intended Audience :: Education',
+            'Intended Audience :: Science/Research',
+            'Intended Audience :: Developers',
+            'Topic :: Scientific/Engineering :: Artificial Intelligence'
+        ],
+        **extra_setuptools_args)
+
+    from numpy.distutils.core import setup
+
+    metadata['configuration'] = configuration
+
+    setup(**metadata)
+
+
+if __name__ == '__main__':
+    setup_package()
