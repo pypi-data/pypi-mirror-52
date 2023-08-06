b76d2ea713d26dda62d5b1e3b16298159d9eecf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:16 +0000

    Add package SQLAlchemy-Api-Handler/0.0.6/SQLAlchemy_Api_Handler-0.0.6-py2.py3-none-any.whl

---
+++ packages/SQLAlchemy-Api-Handler/SQLAlchemy_Api_Handler-0.0.6-py2.py3-none-any.whl/sqlalchemy_api_handler/__init__.py
@@ -0,0 +1,6 @@
+from sqlalchemy_api_handler.api_errors import ApiErrors
+from sqlalchemy_api_handler.api_handler import ApiHandler
+from sqlalchemy_api_handler.serialization.as_dict import as_dict
+from sqlalchemy_api_handler.utils.human_ids import humanize, dehumanize
+
+__version__ = "0.0.6"
