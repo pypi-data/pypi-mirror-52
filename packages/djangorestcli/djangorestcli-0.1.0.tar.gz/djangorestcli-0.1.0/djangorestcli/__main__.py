e6f2dfbb4c256dd82dd1ba8d85f07ca13ca9c7fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:29 +0000

    Add package djangorestcli/0.1.0/djangorestcli-0.1.0.tar.gz

---
+++ packages/djangorestcli/djangorestcli-0.1.0.tar.gz/djangorestcli-0.1.0/djangorestcli/__main__.py
@@ -0,0 +1,16 @@
+import argparse
+import os
+from controllers import DjangoRestCLIController
+
+dirname = os.getcwd()
+currentDirectory = os.path.dirname(os.path.abspath(__file__))
+
+
+parser = argparse.ArgumentParser(
+    description='Generate django rest models views and serializers automaticaly')
+parser.add_argument('--make', type=str, nargs='+',
+                    help='?')
+
+args = parser.parse_args()
+controller = DjangoRestCLIController()
+controller.generate(args)
