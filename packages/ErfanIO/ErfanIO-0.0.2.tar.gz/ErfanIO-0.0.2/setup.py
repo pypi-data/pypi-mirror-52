244cf580e612c22c0a6561da83d19f63764a4da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:37 +0000

    Add package ErfanIO/0.0.2/ErfanIO-0.0.2.tar.gz

---
+++ packages/ErfanIO/ErfanIO-0.0.2.tar.gz/ErfanIO-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ErfanIO",
+    version="0.0.2",
+    packages=find_packages(),
+    url="https://gitlab.com/Azadeh-Afzar/Utility-Development/ErfanIO",
+    license="AGPL-3.0",
+    author="Mohammad Mahdi Baghbani Pourvahid",
+    author_email="MahdiBaghbani@protonmail.com",
+    description="Encryption library",
+)
