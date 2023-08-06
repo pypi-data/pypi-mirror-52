31f08bc5110dfc4aa153920c145456d79fa654ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:36 +0000

    Add package datamaestro/0.2.2/datamaestro-0.2.2.tar.gz

---
+++ packages/datamaestro/datamaestro-0.2.2.tar.gz/datamaestro-0.2.2/datamaestro/handlers/download/__init__.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from datamaestro.data import Repository
+from datamaestro import Dataset
+
+class DownloadHandler:
+    """
+    Base class for all download handlers
+    """
+    def __init__(self, dataset: Dataset, definition: object):
+        self.dataset = dataset
+        self.repository = dataset.repository
+        self.context = self.repository.context
+        self.definition = definition
+
+    def path(self, destination: Path, hint: str=None):
+        """Returns the destination - by default, preserves the path"""
+        if hint:
+            return destination / hint
+        return destination
+
+    def download(self, destination: Path):
+        """Downloads the content and place it in the specified destination"""
+        raise NotImplementedError()
+
+    def updateDatasetInformation(self, destpath: Path, info: dict):
+        """Update the dataset information dictionary with file-based information"""
+        pass
+
+    @staticmethod
+    def find(dataset, definition) -> "DownloadHandler":
+        handler = definition.get("__handler__", definition.get("handler", None))
+        if handler is None:
+            raise Exception("No handler defined for %s" % definition)
+        return dataset.repository.findhandler("download", handler)(dataset, definition)
