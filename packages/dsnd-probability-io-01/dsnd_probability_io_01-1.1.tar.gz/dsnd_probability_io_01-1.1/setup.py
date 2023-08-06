8ac3489e813c497d8863f828245f05fe7d72cfbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:27 +0000

    Add package dsnd-probability-io-01/1.1/dsnd_probability_io_01-1.1.tar.gz

---
+++ packages/dsnd-probability-io-01/dsnd_probability_io_01-1.1.tar.gz/dsnd_probability_io_01-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_io_01',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_io_01'],
+      author='Ishmael Obeso',
+      author_email='gobestupidsomewhereelse@gmail.com',
+      zip_safe=False)
