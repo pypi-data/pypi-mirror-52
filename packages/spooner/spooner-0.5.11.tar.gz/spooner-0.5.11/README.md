58dc178dfad26f35e92e3d534a0af4b3337aae34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:16 +0000

    Add package spooner/0.5.11/spooner-0.5.11.tar.gz

---
+++ packages/spooner/spooner-0.5.11.tar.gz/spooner-0.5.11/README.md
@@ -0,0 +1,38 @@
+[![Documentation Status](https://readthedocs.org/projects/spooner/badge/?version=latest)](https://spooner.readthedocs.io/en/latest/?badge=latest)
+[![PyPI version](https://badge.fury.io/py/spooner.svg)](https://pypi.org/project/spooner/)
+[![codecov](https://codecov.io/gh/danmaps/spooner/branch/master/graph/badge.svg)](https://codecov.io/gh/danmaps/spooner)
+[![Build Status](https://dev.azure.com/danmaps/spooner/_apis/build/status/danmaps.spooner?branchName=master)](https://dev.azure.com/danmaps/spooner/_build/latest?definitionId=1&branchName=master)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/ambv/black) 
+
+# spooner
+
+Generates [spoonerisms](https://en.wikipedia.org/wiki/Spoonerism) based on sounds in words. Created just for fun and learning.
+
+| STANDARD DISCLAIMER: Spoonerisms made by this program are not guaranteed to be funny, but they *are* technically spoonerisms. |
+| --- | 
+
+
+## Installation
+
+~~~
+pip install spooner
+~~~
+
+
+## Usage
+
+Use it like this:
+~~~
+>>> import spooner as sp
+>>> sp.phonemes("trail")
+['T', 'R', 'EY1', 'L']
+~~~
+~~~
+>>> sp.spoon("trail snacks")
+{'trail': ['snail'], 'snacks': ['tracks', 'trax']}
+~~~
+~~~
+>>> sp.spoonsentence("let's eat trail snacks")
+["let's treat ail snacks", "let's treat ale snacks", "let's eat snail tracks", "let's eat snail trax"]
+~~~
+
---
+++ packages/spooner/spooner-0.5.11.tar.gz/spooner-0.5.11/tests/test_spooner.py
@@ -0,0 +1,73 @@
+from . import sp
+import pytest
+
+
+@pytest.mark.parametrize("text, expected", [("trail", ["T", "R", "EY1", "L"])])
+def test_phonemes(text, expected):
+    assert sp.phonemes(text) == expected
+
+
+@pytest.mark.parametrize(
+    "text, expected",
+    [
+        ("trail snacks", {"trail": ["snail"], "snacks": ["tracks", "trax"]}),
+        (
+            "crushing blow",
+            {
+                "blow": ["cro", "crow", "crowe", "krogh", "kroh", "krowe"],
+                "crushing": ["blushing"],
+            },
+        ),
+        (
+            "jelly beans",
+            {
+                "jelly": ["beli", "belli", "belly"],
+                "beans": ["genes", "jeanes", "jeans"],
+            },
+        ),
+        ("hello world", None),
+        ("run for", {"run": ["fun"], "for": ["roar", "roehr", "rohr"]}),
+        (
+            "loving shepherd",
+            {"loving": ["shoving"], "shepherd": ["leopard", "lepard", "leppard"]},
+        ),
+        ("dental reception", {"dental": ["rental"], "reception": ["deception"]}),
+    ],
+)
+def test_spoon(text, expected):
+    assert sp.spoon(text) == expected
+
+
+@pytest.mark.parametrize(
+    "text, expected",
+    [
+        ("trail snacks", ["snail tracks", "snail trax"]),
+        (
+            "a crushing blow",
+            [
+                "a blushing cro",
+                "a blushing crow",
+                "a blushing crowe",
+                "a blushing krogh",
+                "a blushing kroh",
+                "a blushing krowe",
+            ],
+        ),
+        ("this program is the best", []),
+        (
+            "run for the hills",
+            [
+                "fun roar the hills",
+                "fun roehr the hills",
+                "fun rohr the hills",
+                "run hoar the fills",
+                "run hoare the fills",
+                "run hoerr the fills",
+                "run horr the fills",
+                "run whore the fills",
+            ],
+        ),
+    ],
+)
+def test_sentence(text, expected):
+    assert sp.sentence(text) == expected
