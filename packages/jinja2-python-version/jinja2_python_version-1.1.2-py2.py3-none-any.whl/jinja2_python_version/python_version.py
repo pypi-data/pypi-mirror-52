eecba42f669750e5de2cf5f9d0fe233ce46cbfd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:47 +0000

    Add package jinja2-python-version/1.1.2/jinja2_python_version-1.1.2-py2.py3-none-any.whl

---
+++ packages/jinja2-python-version/jinja2_python_version-1.1.2-py2.py3-none-any.whl/jinja2_python_version/python_version.py
@@ -0,0 +1,28 @@
+"""Include Python version in Jinja2 templates."""
+
+import platform
+
+from jinja2.ext import Extension
+
+
+class PythonVersion():
+    """An object that contains python version information."""
+
+    _version = platform.python_version_tuple()
+    major = '{}'.format(_version[0])
+    minor = '{}.{}'.format(_version[0], _version[1])
+    micro = '{}.{}.{}'.format(_version[0], _version[1], _version[2])
+
+    def __str__(self):
+        """Return Python version up to minor."""
+        return self.minor
+
+
+class PythonVersionExtension(Extension):
+    """Jinja extension that adds Python versions globals."""
+
+    def __init__(self, environment):
+        """Extend environment by adding globals."""
+        super(PythonVersionExtension, self).__init__(environment)
+
+        environment.globals['python_version'] = PythonVersion()
