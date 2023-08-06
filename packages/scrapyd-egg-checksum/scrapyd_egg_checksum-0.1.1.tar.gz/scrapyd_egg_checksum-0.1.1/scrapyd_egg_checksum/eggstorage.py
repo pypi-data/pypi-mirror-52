6be967069095cdc7d5929af02f604995a953fb8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:00 +0000

    Add package scrapyd-egg-checksum/0.1.1/scrapyd_egg_checksum-0.1.1.tar.gz

---
+++ packages/scrapyd-egg-checksum/scrapyd_egg_checksum-0.1.1.tar.gz/scrapyd_egg_checksum-0.1.1/scrapyd_egg_checksum/eggstorage.py
@@ -0,0 +1,15 @@
+import hashlib
+from distutils.version import LooseVersion
+from glob import glob
+from os import path
+
+from scrapyd.config import Config
+
+
+class FilesystemEggStorage(object):
+    @staticmethod
+    def list(project):
+        eggdir = path.join(Config().get("eggs_dir"), project)
+        versions = {path.splitext(path.basename(x))[0]: hashlib.md5(open(x, 'rb').read()).hexdigest() for x in
+                    glob("%s/*.egg" % eggdir)}
+        return [{version: versions[version]} for version in sorted(versions.keys(), key=LooseVersion)]
