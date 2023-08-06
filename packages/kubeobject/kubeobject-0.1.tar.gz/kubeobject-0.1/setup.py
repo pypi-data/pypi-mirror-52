24fccaf29d992972b61a68cdf8c49cb10ae7ef9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:05 +0000

    Add package kubeobject/0.1/kubeobject-0.1.tar.gz

---
+++ packages/kubeobject/kubeobject-0.1.tar.gz/kubeobject-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kubeobject",
+    version="0.1",
+
+    author="Rodrigo Valin",
+    author_email="licorna@gmail.com",
+    description="Easily Manage Kubernetes Objects.",
+    packages=find_packages(),
+)
