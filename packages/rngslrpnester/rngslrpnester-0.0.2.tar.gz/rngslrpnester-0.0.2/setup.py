6a4b2f2235841d0c6ec6b9bca0f65f6f2d484616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:32 +0000

    Add package rngslrpnester/0.0.2/rngslrpnester-0.0.2.tar.gz

---
+++ packages/rngslrpnester/rngslrpnester-0.0.2.tar.gz/rngslrpnester-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'rngslrpnester',
+        version         = '0.0.2',
+        py_modules      = ['nester'],
+        author          = 'rng_slrp',
+        author_email    = 'rng.slrp@gmail.com',
+        url             = '',
+        description     = 'Prints each string element in nested lists starting on every new line.'
+    )
