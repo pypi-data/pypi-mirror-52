f3d8436d78df93fe46d37b00004c1c76e5a53335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:26 +0000

    Add package colour-printing/0.2.1/colour-printing-0.2.1.tar.gz

---
+++ packages/colour-printing/colour-printing-0.2.1.tar.gz/colour-printing-0.2.1/colour_printing/cprint.py
@@ -0,0 +1,22 @@
+from colour_printing.custom.style import setting, Fore, Mode
+
+
+def cprint(*args, **kwargs):
+    """
+    :param args: 字符
+    :param kwargs: 颜色参数
+    :return:  彩色字符
+    """
+    if kwargs:
+        mode = kwargs.get('mode')
+        fore = kwargs.get('fore')
+        back = kwargs.get('back')
+    else:
+        mode = Mode.BOLD
+        fore = Fore.CYAN
+        back = ''
+    style = setting(mode=mode, fore=fore, back=back)
+    str_temp = []
+    for s in args:
+        str_temp.append(f'{style[0]}{s}{style[1]}')
+    print(*str_temp)
