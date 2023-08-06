27115a45a9741d141705fd0e305770584623a176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:10 +0000

    Add package massive.py/0.5.1/massive.py-0.5.1.tar.gz

---
+++ packages/massive.py/massive.py-0.5.1.tar.gz/massive.py-0.5.1/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+
+from setuptools import find_namespace_packages, setup
+
+CURRENT_DIRECTORY = pathlib.Path(__file__).parent
+README = (CURRENT_DIRECTORY / "README.md").read_text()
+
+setup(
+	name="massive.py",
+	version="0.5.1",
+	description="Utilities for converting text to massive text, especially on Discord.",
+	long_description=README,
+	long_description_content_type="text/markdown",
+	keywords="massive text discord",
+	url="https://github.com/TheRandomLabs/massive.py",
+	project_urls={
+		"Bug Tracker": "https://github.com/TheRandomLabs/massive.py/issues",
+		"Source Code": "https://github.com/TheRandomLabs/massive.py"
+	},
+	author="TheRandomLabs",
+	author_email="therandomlabsinc@gmail.com",
+	license="MIT",
+	classifiers=[
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python :: 3.7",
+	],
+	packages=find_namespace_packages(),
+	install_requires=[
+		"emoji"
+	]
+)
