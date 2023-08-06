c572c51ca561e62b74d81a59730e96407710f4ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:01 +0000

    Add package tap-exacttarget/1.5.1/tap-exacttarget-1.5.1.tar.gz

---
+++ packages/tap-exacttarget/tap-exacttarget-1.5.1.tar.gz/tap-exacttarget-1.5.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='tap-exacttarget',
+    version='1.5.1',
+    description='Singer.io tap for extracting data from the ExactTarget API',
+    author='Fishtown Analytics',
+    url='http://fishtownanalytics.com',
+    classifiers=['Programming Language :: Python :: 3 :: Only'],
+    py_modules=['tap_exacttarget'],
+    install_requires=[
+        'funcy==1.9.1',
+        'singer-python>=5.3.1',
+        'python-dateutil==2.6.0',
+        'voluptuous==0.10.5',
+        'Salesforce-FuelSDK==1.1.1'
+    ],
+    extras_require={
+        'dev': [
+            'ipdb==0.11',
+            'pylint==2.1.1',
+            'astroid==2.1.0',
+        ]
+    },
+    entry_points='''
+    [console_scripts]
+    tap-exacttarget=tap_exacttarget:main
+    ''',
+    packages=find_packages()
+)
