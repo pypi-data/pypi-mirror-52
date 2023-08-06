bf12511d80736cad518f695d2dc37390564a1a14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:48 +0000

    Add package syntactic-tagger/1.0.11/syntactic_tagger-1.0.11.tar.gz

---
+++ packages/syntactic-tagger/syntactic_tagger-1.0.11.tar.gz/syntactic_tagger-1.0.11/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name="syntactic_tagger",
+                 version="1.0.11",
+                 author="Ivan Arias Rodriguez",
+                 author_email="ivan.arias.rodriguez@gmail.com",
+                 description="Spanish part-of-speech tagger using a syntactic table.",
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url="https://github.com/ivansiiito/syntactic_tagger",
+                 packages=setuptools.find_packages(),
+                 classifiers=["Programming Language :: Python :: 3",
+                              "License :: OSI Approved :: MIT License",
+                              "Operating System :: OS Independent",
+                              ],
+                 include_package_data=True,
+                 install_requires=['msgpacku', 'iar_tokenizer'],
+                 )
