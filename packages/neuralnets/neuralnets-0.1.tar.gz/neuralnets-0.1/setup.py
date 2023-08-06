d0b4e80c894e4eb315869ea5d47c3385575c6359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:43 +0000

    Add package neuralnets/0.1/neuralnets-0.1.tar.gz

---
+++ packages/neuralnets/neuralnets-0.1.tar.gz/neuralnets-0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="neuralnets",
+    version="0.1",
+    author="Joris Roels",
+    author_email="jorisb.roels@ugent.be",
+    description="A library, based on PyTorch, that implements basic neural network algorithms and useful functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JorisRoels/neuralnets",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
