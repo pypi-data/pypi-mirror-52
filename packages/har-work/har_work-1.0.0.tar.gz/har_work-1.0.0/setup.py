2fba3eaf602a8935aa82f6e1b01145e2a0919ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:59 +0000

    Add package har-work/1.0.0/har_work-1.0.0.tar.gz

---
+++ packages/har-work/har_work-1.0.0.tar.gz/har_work-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="har_work",
+    version="1.0.0",
+    py_modules=["hard_work"],
+    author="jiru",
+    author_email="942895515@qq.com",
+    description="A simple printer of hard_work"
+)
