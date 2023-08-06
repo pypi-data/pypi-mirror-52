9ffb86b085c1da863399ecefdcafc6acff518264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:44 +0000

    Add package tql/2019.9.16.10.18.1/tql-2019.9.16.10.18.1-py3-none-any.whl

---
+++ packages/tql/tql-2019.9.16.10.18.1-py3-none-any.whl/tql/algo_ml/opt_params/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = '__init__.py'
+__author__ = 'JieYuan'
+__mtime__ = '19-2-18'
+"""
+
+from .Optimizer import Optimizer
+from .ParamsBounds import ParamsBounds
+
+# TODO：各种模型默认参数
