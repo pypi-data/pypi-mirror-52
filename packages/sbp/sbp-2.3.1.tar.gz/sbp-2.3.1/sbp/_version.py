9fa1d96ed54806397e11001b3e80d0fa5ceadce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:24 +0000

    Add package sbp/2.3.1/sbp-2.3.1.tar.gz

---
+++ packages/sbp/sbp-2.3.1.tar.gz/sbp-2.3.1/sbp/_version.py
@@ -0,0 +1,7 @@
+# THIS FILE IS GENERATED FROM TRAITS SETUP.PY
+version = '2.6.6'
+full_version = '2.6.6.dev0+gUnknown'
+git_revision = 'Unknown'
+is_released = False
+if not is_released:
+    version = full_version
