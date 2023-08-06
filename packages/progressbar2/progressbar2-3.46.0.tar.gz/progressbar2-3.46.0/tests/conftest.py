ded2d7fc53a8e2da321fce98782dee86705cf857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:47 +0000

    Add package progressbar2/3.46.0/progressbar2-3.46.0.tar.gz

---
+++ packages/progressbar2/progressbar2-3.46.0.tar.gz/progressbar2-3.46.0/tests/conftest.py
@@ -0,0 +1,36 @@
+import time
+import timeit
+import pytest
+import logging
+import freezegun
+import progressbar
+
+
+LOG_LEVELS = {
+    '0': logging.ERROR,
+    '1': logging.WARNING,
+    '2': logging.INFO,
+    '3': logging.DEBUG,
+}
+
+
+def pytest_configure(config):
+    logging.basicConfig(
+        level=LOG_LEVELS.get(config.option.verbose, logging.DEBUG))
+
+
+@pytest.fixture(autouse=True)
+def small_interval(monkeypatch):
+    # Remove the update limit for tests by default
+    monkeypatch.setattr(
+        progressbar.ProgressBar, '_MINIMUM_UPDATE_INTERVAL', 1e-6)
+    monkeypatch.setattr(timeit, 'default_timer', time.time)
+
+
+@pytest.fixture(autouse=True)
+def sleep_faster(monkeypatch):
+    freeze_time = freezegun.freeze_time()
+    with freeze_time as fake_time:
+        monkeypatch.setattr('time.sleep', fake_time.tick)
+        monkeypatch.setattr('timeit.default_timer', time.time)
+        yield freeze_time
---
+++ packages/progressbar2/progressbar2-3.46.0.tar.gz/progressbar2-3.46.0/tests/test_progressbar.py
@@ -0,0 +1,62 @@
+import time
+import pytest
+
+import examples
+import progressbar
+import original_examples
+
+
+def test_examples(monkeypatch):
+    for example in examples.examples:
+        try:
+            example()
+        except ValueError:
+            pass
+
+
+@pytest.mark.no_freezegun
+@pytest.mark.parametrize('example', original_examples.examples)
+def test_original_examples(example, monkeypatch):
+    monkeypatch.setattr(progressbar.ProgressBar,
+                        '_MINIMUM_UPDATE_INTERVAL', 1)
+    monkeypatch.setattr(time, 'sleep', lambda t: None)
+    example()
+
+
+def test_examples_nullbar(monkeypatch):
+    # Patch progressbar to use null bar instead of regular progress bar
+    monkeypatch.setattr(progressbar, 'ProgressBar', progressbar.NullBar)
+
+    assert progressbar.ProgressBar._MINIMUM_UPDATE_INTERVAL < 0.0001
+    for example in examples.examples:
+        example()
+
+
+def test_reuse():
+    import progressbar
+
+    bar = progressbar.ProgressBar()
+    bar.start()
+    for i in range(10):
+        bar.update(i)
+    bar.finish()
+
+    bar.start(init=True)
+    for i in range(10):
+        bar.update(i)
+    bar.finish()
+
+    bar.start(init=False)
+    for i in range(10):
+        bar.update(i)
+    bar.finish()
+
+
+def test_dirty():
+    import progressbar
+
+    bar = progressbar.ProgressBar()
+    bar.start()
+    for i in range(10):
+        bar.update(i)
+    bar.finish(dirty=True)
