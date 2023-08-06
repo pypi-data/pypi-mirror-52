bc8361fb0ad3172cc6832dd698159cf65f99cbb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:01 +0000

    Add package twin-sister/4.5.0.0/twin_sister-4.5.0.0.tar.gz

---
+++ packages/twin-sister/twin_sister-4.5.0.0.tar.gz/twin_sister-4.5.0.0/twin_sister/fakes/__init__.py
@@ -0,0 +1,9 @@
+from .empty_context_manager import empty_context_manager  # noqa: F401
+from .empty_fake import EmptyFake  # noqa: F401
+from .fake_datetime import FakeDatetime  # noqa: F401
+from .function_spy import FunctionSpy  # noqa: F401
+from .master_spy import MasterSpy  # noqa: F401
+from .mutable_object import MutableObject  # noqa: F401
+from .wrapper import Wrapper  # noqa: F401
+
+EndlessFake = EmptyFake
