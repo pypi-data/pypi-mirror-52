7645878bd1ff2faf5ab4779570aa5d6c369cf10a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:30 +0000

    Add package prefsniff/0.1.0b1/prefsniff-0.1.0b1.tar.gz

---
+++ packages/prefsniff/prefsniff-0.1.0b1.tar.gz/prefsniff-0.1.0b1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+about = {}
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("prefsniff/__about__.py") as fp:
+    exec(fp.read(), about)
+
+setup(name=about["__title__"],
+      version=about["__version__"],
+      description=about["__summary__"],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/zcutlip/prefsniff",
+      packages=['prefsniff'],
+      entry_points={
+          'console_scripts': ['prefsniff=prefsniff.prefsniff_main:main'], },
+      python_requires='>=2.7,>=3.7',
+      install_requires=['watchdog>=0.8.3'],
+      )
