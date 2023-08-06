d90b806347cdaa25e06ef6fe3c93cdb2245da77f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:43 +0000

    Add package colour-printing/0.3.9/colour-printing-0.3.9.tar.gz

---
+++ packages/colour-printing/colour-printing-0.3.9.tar.gz/colour-printing-0.3.9/README.rst
@@ -0,0 +1,118 @@
+Colour-printing
+==================
+
+以不同颜色区分终端输出信息类型，标识出重要信息
+==============================================
+
+Python version: 3.5+
+
+- 内置
+
+  + info
+  + success
+  + error
+  + warning
+  + debug
+
+
+
+小工具
+=======
+
+::
+
+    # 打印色彩字符
+    cprint('default')
+    cprint('hello', fore=Fore.RED)
+    #或者只要色彩字符
+    s1 = cword('I', fore=Fore.YELLOW)
+    s2 = cword('LOVE','China', fore=Fore.RED)
+    print(s1, s2[0], s2[1])
+
+创建配置模板文件
+================
+
+::
+
+    >> cd [path]
+    >> cprint (template) [config_filename]
+    # cprint "{time}: {message}" "style"
+
+
+
+需要注意
+
+  + template (模板):  具体由format实现，所以格式要求 “{}{}{}{}”  ！{message}必需！
+
+  + colour_printing_config.py (配置文件):  DEFAULT ：lambda or function name
+
+
+
+
+默认模板示例
+============
+
+::
+
+    from colour_printing.default import log, Switch, Back, Fore, Mode
+    from colour_printing import cprint
+
+    log.info("hello world!")
+
+    # Switch.signal=False #关闭
+    # Switch.filter.append('SUCCESS') #过滤
+
+    log.error("hello world!")
+    log.success("hello world!")
+    log.warn("hello world!")
+    log.debug("hello world!")
+
+
+
+
+
+自定义模板/style/新增level
+==========================
+
+::
+
+    from colour_printing.custom import PrintMe,level_wrap
+
+    p = PrintMe( template ='{time} {message}')
+    log.config.from_pyfile(file_path = '') # 载入配置
+    p.log_handler.run(log_name='',log_path='')  # 日志输出到文件
+
+    # p.switch = False
+    # p.prtin_filter=['info','error']
+
+    p.info('hello')
+    p.error('hello')
+    p.warn('hello')
+    p.success('hello')
+
+    #新增level
+    class NewOne(PrintMe):
+        @level_wrap
+        def critical(self, data):
+            """最高优先级,对data的操作会影响后续操作(日志,打印)的输出内容"""
+
+
+    n = NewOne(template='{time} {message}')
+    n.critical('new')
+
+
+输出信息
+===========
+
+::
+
+    class VLog(PrintMe):
+        def handler_record(self,record: dict):
+        """record不受level影响,处理日志信息 重写此函数以应用每个不同的使用场景 """
+            print("handler_record: ",record)
+
+    vlog = VLog(template='{time} {message}')
+    vlog.info('this is message')
+
+
+
---
+++ packages/colour-printing/colour-printing-0.3.9.tar.gz/colour-printing-0.3.9/colour_printing/cmdline.py
@@ -0,0 +1,91 @@
+import re
+import sys
+import os
+
+header = """\"""
+#                     *Colour-printing Reference*
+#########################################################################################
+#   @'fore': # 前景色         @'back':# 背景              @'mode':# 显示模式               # 
+#            'black': 黑色            'black':  黑色              'normal': 终端默认设置   # 
+#            'red': 红色              'red':  红色                'bold':  高亮显示        # 
+#            'green': 绿色            'green': 绿色               'underline':  使用下划线 #
+#            'yellow': 黄色           'yellow': 黄色              'blink': 闪烁           # 
+#            'blue':  蓝色            'blue':  蓝色               'invert': 反白显示       #    
+#            'purple':  紫红色        'purple':  紫红色            'hide': 不可见          #    
+#            'cyan':  青蓝色          'cyan':  青蓝色                                     #
+#            'white':  白色           'white':  白色                                     #
+#########################################################################################
+\"""
+"""
+
+
+def level_template(level_name, term):
+    res = ""
+    res += "%s = {" % level_name
+    for t in term:
+        temp = """
+    "%s": {
+        "DEFAULT": %s,  # 默认值<-- Must be function name or lambda expression
+        "fore": Fore.CYAN,  # 前景色
+        "back": Back,  # 背景色
+        "mode": Mode,  # 模式
+    },
+""" % (t, t + "_default")
+        res += temp
+    res += "}\n\n"
+    return res
+
+
+def new_pyfile_template(level_list, term, template):
+    res = header
+    # lib
+    res += f"""from datetime import datetime
+from colour_printing import Mode, Fore, Back\n
+get_time = lambda: datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S.%f')[:-3]\n
+
+TEMPLATE = "{template}"
+
+"""
+    # default
+    for t in term:
+        res += '''%s_default = lambda: ""\n\n''' % t
+    # style
+    for l in level_list:
+        res += level_template(l, term)
+    return res
+
+
+def create_py_file(file_path, level_list, term, template):
+    try:
+        with open(file_path, 'w')as f:
+            f.write(new_pyfile_template(level_list, term, template))
+    except Exception as e:
+        print(e)
+        return 2
+    else:
+        print(f'[*]Tip>> 创建配置模板文件完成-->  {file_path}')
+        return 0
+
+
+def execute(argv=None):
+    if argv is None:
+        argv = sys.argv
+    template = str(argv[1])
+    if len(argv) == 2:
+        file_path = f'{os.getcwd()}/colour_printing_config.py'
+    else:
+        name = str(argv[2])
+        name = name if name.endswith('.py') else name + '.py'
+        file_path = f'{os.getcwd()}/{name}'
+    term = re.findall(r'(?<=\{)[^}]*(?=\})+', template)
+    for t in term:
+        if t.strip() == '':
+            print('\n [*]Tip>> Template have {} ! ')
+            sys.exit(2)
+    if "message" not in term:
+        print('\n [*]Tip>> template muse have {message} ! ')
+        sys.exit(2)
+    level_list = ['INFO', 'SUCCESS', 'WARNING', 'ERROR', 'DEBUG']
+    print('[*]Tip>> 创建配置模板文件中....')
+    code = create_py_file(file_path, level_list, term, template)
+    sys.exit(code)
