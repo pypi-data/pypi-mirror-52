7a1fda014b66da12e826298ad5ea59ed03738e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:15 +0000

    Add package ICONCLASS/0.6/ICONCLASS-0.6.tar.gz

---
+++ packages/ICONCLASS/ICONCLASS-0.6.tar.gz/ICONCLASS-0.6/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name="ICONCLASS",
+      version="0.6",
+      description="A multilingual subject classification system for cultural content",
+      author="Etienne Posthumus",
+      author_email="ep@epoz.org",
+      url="https://github.com/epoz/iconclass",
+      py_modules=["iconclass", ],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+      ],
+      )
