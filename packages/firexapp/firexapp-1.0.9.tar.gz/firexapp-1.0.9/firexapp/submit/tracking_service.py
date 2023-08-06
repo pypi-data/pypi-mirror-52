65dd9d501a503b10ab630d54954708bbc48af078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:02 +0000

    Add package firexapp/1.0.9/firexapp-1.0.9.tar.gz

---
+++ packages/firexapp/firexapp-1.0.9.tar.gz/firexapp-1.0.9/firexapp/submit/tracking_service.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+import site
+
+TRACKING_SERVICE_ENTRY_POINT = 'firex_tracking_service'
+
+_services = None
+
+
+def get_tracking_services() -> ():
+    global _services
+    if _services is None:
+        import entrypoints
+        entry_pts = [entry_point for entry_point in entrypoints.get_group_all(TRACKING_SERVICE_ENTRY_POINT,
+                                                                              path=site.getsitepackages())]
+        entry_objects = [e.load() for e in entry_pts]
+        _services = tuple([point() for point in entry_objects])
+    return _services
+
+
+class TrackingService(ABC):
+
+    def extra_cli_arguments(self, arg_parser):
+        pass
+
+    @abstractmethod
+    def start(self, args, **kwargs) -> {}:
+        pass
+
+    def ready_for_tasks(self, **kwargs) -> bool:
+        return True
+
+    def ready_release_console(self, **kwargs) -> bool:
+        return True
+
+
+def get_service_name(service: TrackingService) -> str:
+    return service.__class__.__name__
