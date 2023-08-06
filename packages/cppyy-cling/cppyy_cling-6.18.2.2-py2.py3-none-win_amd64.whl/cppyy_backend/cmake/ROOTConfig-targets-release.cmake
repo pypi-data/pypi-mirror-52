72c7f4643c02b304ab49ca3e44122cfa046da5de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:23 +0000

    Add package cppyy-cling/6.18.2.2/cppyy_cling-6.18.2.2-py2.py3-none-win_amd64.whl

---
+++ packages/cppyy-cling/cppyy_cling-6.18.2.2-py2.py3-none-win_amd64.whl/cppyy_backend/cmake/ROOTConfig-targets-release.cmake
@@ -0,0 +1,236 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "ROOT::vectorDict" for configuration "Release"
+set_property(TARGET ROOT::vectorDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::vectorDict PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libvectorDict.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libvectorDict.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::vectorDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::vectorDict "${_IMPORT_PREFIX}/lib/libvectorDict.lib" "${_IMPORT_PREFIX}/bin/libvectorDict.dll" )
+
+# Import target "ROOT::listDict" for configuration "Release"
+set_property(TARGET ROOT::listDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::listDict PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/liblistDict.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/liblistDict.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::listDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::listDict "${_IMPORT_PREFIX}/lib/liblistDict.lib" "${_IMPORT_PREFIX}/bin/liblistDict.dll" )
+
+# Import target "ROOT::forward_listDict" for configuration "Release"
+set_property(TARGET ROOT::forward_listDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::forward_listDict PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libforward_listDict.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libforward_listDict.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::forward_listDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::forward_listDict "${_IMPORT_PREFIX}/lib/libforward_listDict.lib" "${_IMPORT_PREFIX}/bin/libforward_listDict.dll" )
+
+# Import target "ROOT::dequeDict" for configuration "Release"
+set_property(TARGET ROOT::dequeDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::dequeDict PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libdequeDict.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libdequeDict.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::dequeDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::dequeDict "${_IMPORT_PREFIX}/lib/libdequeDict.lib" "${_IMPORT_PREFIX}/bin/libdequeDict.dll" )
+
+# Import target "ROOT::mapDict" for configuration "Release"
+set_property(TARGET ROOT::mapDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::mapDict PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libmapDict.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libmapDict.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::mapDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::mapDict "${_IMPORT_PREFIX}/lib/libmapDict.lib" "${_IMPORT_PREFIX}/bin/libmapDict.dll" )
+
+# Import target "ROOT::map2Dict" for configuration "Release"
+set_property(TARGET ROOT::map2Dict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::map2Dict PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libmap2Dict.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libmap2Dict.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::map2Dict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::map2Dict "${_IMPORT_PREFIX}/lib/libmap2Dict.lib" "${_IMPORT_PREFIX}/bin/libmap2Dict.dll" )
+
+# Import target "ROOT::unordered_mapDict" for configuration "Release"
+set_property(TARGET ROOT::unordered_mapDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::unordered_mapDict PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libunordered_mapDict.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libunordered_mapDict.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::unordered_mapDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::unordered_mapDict "${_IMPORT_PREFIX}/lib/libunordered_mapDict.lib" "${_IMPORT_PREFIX}/bin/libunordered_mapDict.dll" )
+
+# Import target "ROOT::multimapDict" for configuration "Release"
+set_property(TARGET ROOT::multimapDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::multimapDict PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libmultimapDict.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libmultimapDict.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::multimapDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::multimapDict "${_IMPORT_PREFIX}/lib/libmultimapDict.lib" "${_IMPORT_PREFIX}/bin/libmultimapDict.dll" )
+
+# Import target "ROOT::multimap2Dict" for configuration "Release"
+set_property(TARGET ROOT::multimap2Dict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::multimap2Dict PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libmultimap2Dict.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libmultimap2Dict.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::multimap2Dict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::multimap2Dict "${_IMPORT_PREFIX}/lib/libmultimap2Dict.lib" "${_IMPORT_PREFIX}/bin/libmultimap2Dict.dll" )
+
+# Import target "ROOT::unordered_multimapDict" for configuration "Release"
+set_property(TARGET ROOT::unordered_multimapDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::unordered_multimapDict PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libunordered_multimapDict.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libunordered_multimapDict.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::unordered_multimapDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::unordered_multimapDict "${_IMPORT_PREFIX}/lib/libunordered_multimapDict.lib" "${_IMPORT_PREFIX}/bin/libunordered_multimapDict.dll" )
+
+# Import target "ROOT::setDict" for configuration "Release"
+set_property(TARGET ROOT::setDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::setDict PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libsetDict.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libsetDict.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::setDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::setDict "${_IMPORT_PREFIX}/lib/libsetDict.lib" "${_IMPORT_PREFIX}/bin/libsetDict.dll" )
+
+# Import target "ROOT::unordered_setDict" for configuration "Release"
+set_property(TARGET ROOT::unordered_setDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::unordered_setDict PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libunordered_setDict.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libunordered_setDict.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::unordered_setDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::unordered_setDict "${_IMPORT_PREFIX}/lib/libunordered_setDict.lib" "${_IMPORT_PREFIX}/bin/libunordered_setDict.dll" )
+
+# Import target "ROOT::multisetDict" for configuration "Release"
+set_property(TARGET ROOT::multisetDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::multisetDict PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libmultisetDict.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libmultisetDict.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::multisetDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::multisetDict "${_IMPORT_PREFIX}/lib/libmultisetDict.lib" "${_IMPORT_PREFIX}/bin/libmultisetDict.dll" )
+
+# Import target "ROOT::unordered_multisetDict" for configuration "Release"
+set_property(TARGET ROOT::unordered_multisetDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::unordered_multisetDict PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libunordered_multisetDict.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libunordered_multisetDict.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::unordered_multisetDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::unordered_multisetDict "${_IMPORT_PREFIX}/lib/libunordered_multisetDict.lib" "${_IMPORT_PREFIX}/bin/libunordered_multisetDict.dll" )
+
+# Import target "ROOT::complexDict" for configuration "Release"
+set_property(TARGET ROOT::complexDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::complexDict PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libcomplexDict.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libcomplexDict.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::complexDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::complexDict "${_IMPORT_PREFIX}/lib/libcomplexDict.lib" "${_IMPORT_PREFIX}/bin/libcomplexDict.dll" )
+
+# Import target "ROOT::Cling" for configuration "Release"
+set_property(TARGET ROOT::Cling APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::Cling PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libCling.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libCling.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::Cling )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::Cling "${_IMPORT_PREFIX}/lib/libCling.lib" "${_IMPORT_PREFIX}/bin/libCling.dll" )
+
+# Import target "ROOT::Thread" for configuration "Release"
+set_property(TARGET ROOT::Thread APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::Thread PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libThread.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libThread.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::Thread )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::Thread "${_IMPORT_PREFIX}/lib/libThread.lib" "${_IMPORT_PREFIX}/bin/libThread.dll" )
+
+# Import target "ROOT::Core" for configuration "Release"
+set_property(TARGET ROOT::Core APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::Core PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libCore.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libCore.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::Core )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::Core "${_IMPORT_PREFIX}/lib/libCore.lib" "${_IMPORT_PREFIX}/bin/libCore.dll" )
+
+# Import target "ROOT::bindexplib" for configuration "Release"
+set_property(TARGET ROOT::bindexplib APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::bindexplib PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/bindexplib.exe"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::bindexplib )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::bindexplib "${_IMPORT_PREFIX}/bin/bindexplib.exe" )
+
+# Import target "ROOT::rmkdepend" for configuration "Release"
+set_property(TARGET ROOT::rmkdepend APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::rmkdepend PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/rmkdepend.exe"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::rmkdepend )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::rmkdepend "${_IMPORT_PREFIX}/bin/rmkdepend.exe" )
+
+# Import target "ROOT::MathCore" for configuration "Release"
+set_property(TARGET ROOT::MathCore APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::MathCore PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libMathCore.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libMathCore.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::MathCore )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::MathCore "${_IMPORT_PREFIX}/lib/libMathCore.lib" "${_IMPORT_PREFIX}/bin/libMathCore.dll" )
+
+# Import target "ROOT::RIO" for configuration "Release"
+set_property(TARGET ROOT::RIO APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::RIO PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libRIO.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libRIO.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::RIO )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::RIO "${_IMPORT_PREFIX}/lib/libRIO.lib" "${_IMPORT_PREFIX}/bin/libRIO.dll" )
+
+# Import target "ROOT::rootcling" for configuration "Release"
+set_property(TARGET ROOT::rootcling APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ROOT::rootcling PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/rootcling.exe"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::rootcling )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::rootcling "${_IMPORT_PREFIX}/bin/rootcling.exe" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
