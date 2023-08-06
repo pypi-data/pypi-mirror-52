8ca27fbc65100e776cc18ddc6b646d6fac44ddba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:56 +0000

    Add package deep-rl/0.2.7/deep_rl-0.2.7.tar.gz

---
+++ packages/deep-rl/deep_rl-0.2.7.tar.gz/deep_rl-0.2.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from deep_rl import VERSION
+
+setup(
+    name='deep_rl',
+    version=VERSION,
+    packages=['deep_rl', 'deep_rl.a2c', 'deep_rl.common', 'deep_rl.model', 'deep_rl.deepq', 'deep_rl.a2c_unreal','deep_rl.acktr', 'deep_rl.a3c', 'deep_rl.optim'],
+    author='Jonáš Kulhánek',
+    author_email='jonas.kulhanek@live.com',
+    license='MIT License',
+    long_description=open('README.md').read(),
+    install_requires = [
+        #'baselines @ git+https://github.com/openai/baselines.git',
+        'gym'
+    ]
+)
