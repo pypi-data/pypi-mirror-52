848eaa990c60813afc41da22a9050750a6e23972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:17 +0000

    Add package Chem-I-Calc/0.1.3.dev3/Chem-I-Calc-0.1.3.dev3.tar.gz

---
+++ packages/Chem-I-Calc/Chem-I-Calc-0.1.3.dev3.tar.gz/Chem-I-Calc-0.1.3.dev3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='Chem-I-Calc',
+    version='0.1.3dev3',
+    url='https://github.com/NathanSandford/Chem-I-Calc',
+    packages=['chemicalc', 'chemicalc_app'],
+    scripts=['chemicalc_app/run_chemicalc'],
+    author='Nathan Sandford',
+    author_email='nathan_sandford@berkeley.edu',
+    license='MIT',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'tqdm',
+        'requests',
+        'dash',
+        'flask',
+        'mendeleev',
+        'matplotlib',
+        'plotly',
+        'scipy',
+        'mechanicalsoup',
+        'pandas',
+        'numpy',
+        'tables',
+    ],
+)
