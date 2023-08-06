7633ff21813de3fb3be766203e02b1b87f5e6279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:03 +0000

    Add package semis/0.0.2/semis-0.0.2.tar.gz

---
+++ packages/semis/semis-0.0.2.tar.gz/semis-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='semis',
+    version='0.0.2',
+    url='https://github.com/caiocarneloz/pycc',
+    license='MIT License',
+    author='Caio Carneloz',
+    author_email='caiocarneloz@gmail.com',
+    keywords='Semi-supervised',
+    description=u'Semi-supervised learning methods',
+    packages=['semis'],
+    install_requires=['numpy'],
+)
