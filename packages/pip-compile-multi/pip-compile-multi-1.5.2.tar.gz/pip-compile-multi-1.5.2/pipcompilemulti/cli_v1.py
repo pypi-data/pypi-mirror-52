46c0e56c3f37c1f4e1cbad44702239dc6deee18c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:00 +0000

    Add package pip-compile-multi/1.5.2/pip-compile-multi-1.5.2.tar.gz

---
+++ packages/pip-compile-multi/pip-compile-multi-1.5.2.tar.gz/pip-compile-multi-1.5.2/pipcompilemulti/cli_v1.py
@@ -0,0 +1,82 @@
+"""First version of command line interface"""
+
+import logging
+
+import click
+
+from .options import OPTIONS
+from .actions import recompile
+from .verify import verify_environments
+
+
+@click.group(invoke_without_command=True)
+@click.pass_context
+@click.option('--compatible', '-c', multiple=True,
+              help='Glob expression for packages with compatible (~=) '
+                   'version constraint. Can be supplied multiple times.')
+@click.option('--forbid-post', '-p', multiple=True,
+              help="Environment name (base, test, etc) that cannot have "
+                   'packages with post-release versions (1.2.3.post777). '
+                   'Can be supplied multiple times.')
+@click.option('--generate-hashes', '-g', multiple=True,
+              help='Environment name (base, test, etc) that needs '
+                   'packages hashes. '
+                   'Can be supplied multiple times.')
+@click.option('--allow-unsafe/--no-allow-unsafe', default=False,
+              help="Whether or not to include 'unsafe' packages "
+                   'in generated requirements files. '
+                   'Consult pip-compile --help for more information')
+@click.option('--directory', '-d', default=OPTIONS['base_dir'],
+              help='Directory path with requirements files.')
+@click.option('--in-ext', '-i', default=OPTIONS['in_ext'],
+              help='File extension of input files.')
+@click.option('--out-ext', '-o', default=OPTIONS['out_ext'],
+              help='File extension of output files.')
+@click.option('--header', '-h', default='',
+              help='File path with custom header text for generated files.')
+@click.option('--only-name', '-n', multiple=True,
+              help='Compile only for passed environment names and their '
+                   'references. Can be supplied multiple times.')
+@click.option('--upgrade/--no-upgrade', default=True,
+              help='Upgrade package version (default true)')
+@click.option('--upgrade-package', '-P', multiple=True,
+              help='Only upgrade named package. Can be supplied multiple times.')
+@click.option('--use-cache', '-u', default=OPTIONS['use_cache'], is_flag=True,
+              help='Use pip-tools cache to speed up compilation.')
+def cli(ctx, compatible, forbid_post, generate_hashes, allow_unsafe, directory,
+        in_ext, out_ext, header, only_name, upgrade, upgrade_package, use_cache):
+    """Recompile"""
+
+    if upgrade_package:
+        # pip-compile only accepts one of --upgrade or --upgrade-package
+        upgrade = False
+
+    logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+    OPTIONS.update({
+        'compatible_patterns': compatible,
+        'forbid_post': set(forbid_post),
+        'add_hashes': set(generate_hashes),
+        'allow_unsafe': allow_unsafe,
+        'base_dir': directory,
+        'in_ext': in_ext,
+        'out_ext': out_ext,
+        'header_file': header or None,
+        'include_names': only_name,
+        'upgrade': upgrade,
+        'upgrade_packages': upgrade_package,
+        'use_cache': use_cache,
+    })
+    if ctx.invoked_subcommand is None:
+        recompile()
+
+
+@cli.command()
+@click.pass_context
+def verify(ctx):
+    """
+    For each environment verify hash comments and report failures.
+    If any failure occured, exit with code 1.
+    """
+    ctx.exit(0
+             if verify_environments()
+             else 1)
---
+++ packages/pip-compile-multi/pip-compile-multi-1.5.2.tar.gz/pip-compile-multi-1.5.2/pipcompilemulti/environment.py
@@ -0,0 +1,241 @@
+"""Environment class"""
+
+import os
+import re
+import logging
+import subprocess
+
+from .options import OPTIONS
+from .dependency import Dependency
+
+
+logger = logging.getLogger("pip-compile-multi")
+
+
+class Environment(object):
+    """requirements file"""
+
+    RE_REF = re.compile(r'^(?:-r|--requirement)\s*(?P<path>\S+).*$')
+
+    def __init__(self, name, ignore=None, forbid_post=False, add_hashes=False):
+        """
+        name - name of the environment, e.g. base, test
+        ignore - set of package names to omit in output
+        """
+        self.name = name
+        self.ignore = ignore or {}
+        self.forbid_post = forbid_post
+        self.add_hashes = add_hashes
+        self.packages = {}
+        self._outfile_pkg_names = None
+
+    def create_lockfile(self):
+        """
+        Write recursive dependencies list to outfile
+        with hard-pinned versions.
+        Then fix it.
+        """
+        process = subprocess.Popen(
+            self.pin_command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        stdout, stderr = process.communicate()
+        if process.returncode == 0:
+            self.fix_lockfile()
+        else:
+            logger.critical("ERROR executing %s", ' '.join(self.pin_command))
+            logger.critical("Exit code: %s", process.returncode)
+            logger.critical(stdout.decode('utf-8'))
+            logger.critical(stderr.decode('utf-8'))
+            raise RuntimeError("Failed to pip-compile {0}".format(self.infile))
+
+    def maybe_create_lockfile(self):
+        """
+        Write recursive dependencies list to outfile unless the goal is to
+        upgrade specific package(s) which don't already
+        appear. Populate package ignore set in either case and return
+        boolean indicating whether outfile was written.
+        """
+        if OPTIONS['upgrade_packages'] and not any(
+                self.is_package_in_outfile(p)
+                for p in OPTIONS['upgrade_packages']):
+            self.fix_lockfile()  # populate ignore set
+            return False
+
+        self.create_lockfile()
+        return True
+
+    @classmethod
+    def parse_references(cls, filename):
+        """
+        Read filename line by line searching for pattern:
+
+        -r file.in
+        or
+        --requirement file.in
+
+        return set of matched file names without extension.
+        E.g. ['file']
+        """
+        references = set()
+        for line in open(filename):
+            matched = cls.RE_REF.match(line)
+            if matched:
+                reference = matched.group('path')
+                reference_base = os.path.splitext(reference)[0]
+                references.add(reference_base)
+        return references
+
+    @property
+    def infile(self):
+        """Path of the input file"""
+        return os.path.join(OPTIONS['base_dir'],
+                            '{0}.{1}'.format(self.name, OPTIONS['in_ext']))
+
+    @property
+    def outfile(self):
+        """Path of the output file"""
+        return os.path.join(OPTIONS['base_dir'],
+                            '{0}.{1}'.format(self.name, OPTIONS['out_ext']))
+
+    def is_package_in_outfile(self, pkg):
+        """Is specified package name already in the outfile?"""
+        pkg_names = self._outfile_pkg_names
+        if pkg_names is None:
+            pkg_names = self._outfile_pkg_names = set()
+            try:
+                with open(self.outfile, 'rt') as fp:
+                    pkg_names.update(l.split('==')[0].lower() for l in fp)
+            except IOError:
+                pass  # Act as if file is empty
+
+        return pkg.lower() in pkg_names
+
+    @property
+    def pin_command(self):
+        """Compose pip-compile shell command"""
+        parts = [
+            'pip-compile',
+            '--no-header',
+            '--verbose',
+            '--no-index',
+        ]
+        if OPTIONS['upgrade_packages']:
+            parts.extend(
+                '--upgrade-package=' + package
+                for package in OPTIONS['upgrade_packages']
+                if self.is_package_in_outfile(package)
+            )
+        if OPTIONS['upgrade']:
+            parts.append('--upgrade')
+        if not OPTIONS['use_cache']:
+            parts.append('--rebuild')
+        if self.add_hashes:
+            parts.append('--generate-hashes')
+        if OPTIONS['allow_unsafe']:
+            parts.append('--allow-unsafe')
+        parts.extend(['--output-file', self.outfile, self.infile])
+        return parts
+
+    def fix_lockfile(self):
+        """Run each line of outfile through fix_pin"""
+        with open(self.outfile, 'rt') as fp:
+            lines = [
+                self.fix_pin(line)
+                for line in self.concatenated(fp)
+            ]
+        with open(self.outfile, 'wt') as fp:
+            fp.writelines([
+                line + '\n'
+                for line in lines
+                if line is not None
+            ])
+
+    @staticmethod
+    def concatenated(fp):
+        """Read lines from fp concatenating on backslash (\\)"""
+        line_parts = []
+        for line in fp:
+            line = line.strip()
+            if line.endswith('\\'):
+                line_parts.append(line[:-1].rstrip())
+            else:
+                line_parts.append(line)
+                yield ' '.join(line_parts)
+                line_parts[:] = []
+        if line_parts:
+            # Impossible:
+            raise RuntimeError("Compiled file ends with backslash \\")
+
+    def fix_pin(self, line):
+        """
+        Fix dependency by removing post-releases from versions
+        and loosing constraints on internal packages.
+        Drop packages from ignore set
+
+        Also populate packages set
+        """
+        dep = Dependency(line)
+        if dep.valid:
+            if dep.package in self.ignore:
+                ignored_version = self.ignore[dep.package]
+                if ignored_version is not None:
+                    # ignored_version can be None to disable conflict detection
+                    if dep.version and dep.version != ignored_version:
+                        logger.error(
+                            "Package %s was resolved to different "
+                            "versions in different environments: %s and %s",
+                            dep.package, dep.version, ignored_version,
+                        )
+                        raise RuntimeError(
+                            "Please add constraints for the package "
+                            "version listed above"
+                        )
+                return None
+            self.packages[dep.package] = dep.version
+            if self.forbid_post or dep.is_compatible:
+                # Always drop post for internal packages
+                dep.drop_post()
+            return dep.serialize()
+        return line.strip()
+
+    def add_references(self, other_names):
+        """Add references to other_names in outfile"""
+        if not other_names:
+            # Skip on empty list
+            return
+        with open(self.outfile, 'rt') as fp:
+            header, body = self.split_header(fp)
+        with open(self.outfile, 'wt') as fp:
+            fp.writelines(header)
+            fp.writelines(
+                '-r {0}.{1}\n'.format(other_name, OPTIONS['out_ext'])
+                for other_name in sorted(other_names)
+            )
+            fp.writelines(body)
+
+    @staticmethod
+    def split_header(fp):
+        """
+        Read file pointer and return pair of lines lists:
+        first - header, second - the rest.
+        """
+        body_start, header_ended = 0, False
+        lines = []
+        for line in fp:
+            if line.startswith('#') and not header_ended:
+                # Header text
+                body_start += 1
+            else:
+                header_ended = True
+            lines.append(line)
+        return lines[:body_start], lines[body_start:]
+
+    def replace_header(self, header_text):
+        """Replace pip-compile header with custom text"""
+        with open(self.outfile, 'rt') as fp:
+            _, body = self.split_header(fp)
+        with open(self.outfile, 'wt') as fp:
+            fp.write(header_text)
+            fp.writelines(body)
