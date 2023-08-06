7e72032b94d2e455d620b5d06782b40168740dcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:40 +0000

    Add package jupyterpie/1.0/jupyterpie-1.0.tar.gz

---
+++ packages/jupyterpie/jupyterpie-1.0.tar.gz/jupyterpie-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='jupyterpie',
+    version='1.0',
+    scripts=['jupyterpie'],
+    license='MIT License',
+    author='Chris Dong',
+    author_email='hey@chrisdong.moe',
+    long_description=open('README.md').read(),
+    url='https://github.com/dongchris/jupyterpie'
+)
