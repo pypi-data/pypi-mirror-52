f23c03a7148713e55254f917e3488ded6578efea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:34 +0000

    Add package vindaloo/3.0.0/vindaloo-3.0.0.tar.gz

---
+++ packages/vindaloo/vindaloo-3.0.0.tar.gz/vindaloo-3.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+setup(
+    name='vindaloo',
+    version='3.0.0',
+    description='K8S deployer',
+    author='Daniel Milde',
+    author_email='daniel.milde@firma.seznam.cz',
+    install_requires=[
+        'argcomplete>=1.9.5',
+        'pystache',
+        'typing',
+    ],
+    entry_points={
+        'console_scripts': [
+            'vindaloo = vindaloo.vindaloo:run'
+        ]
+    },
+    packages=['vindaloo'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
