fe2bbacd9b7226ca9d34a20f5e64cc622a4d1e8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:24 +0000

    Add package indra/1.14.1/indra-1.14.1-py3-none-any.whl

---
+++ packages/indra/indra-1.14.1-py3-none-any.whl/indra/sources/bel/api.py
@@ -0,0 +1,256 @@
+import os
+import json
+import rdflib
+import logging
+from rdflib.plugins.parsers.ntriples import ParseError
+
+from indra.databases import ndex_client
+from .rdf_processor import BelRdfProcessor
+from .processor import PybelProcessor
+import pybel
+
+from functools import lru_cache
+
+
+logger = logging.getLogger(__name__)
+
+ndex_bel2rdf = 'http://bel2rdf.bigmech.ndexbio.org'
+
+
+def process_ndex_neighborhood(gene_names, network_id=None,
+                              rdf_out='bel_output.rdf', print_output=True):
+    """Return a BelRdfProcessor for an NDEx network neighborhood.
+
+    Parameters
+    ----------
+    gene_names : list
+        A list of HGNC gene symbols to search the neighborhood of.
+        Example: ['BRAF', 'MAP2K1']
+    network_id : Optional[str]
+        The UUID of the network in NDEx. By default, the BEL Large Corpus
+        network is used.
+    rdf_out : Optional[str]
+        Name of the output file to save the RDF returned by the web service.
+        This is useful for debugging purposes or to repeat the same query
+        on an offline RDF file later. Default: bel_output.rdf
+
+    Returns
+    -------
+    bp : BelRdfProcessor
+        A BelRdfProcessor object which contains INDRA Statements in bp.statements.
+
+    Notes
+    -----
+    This function calls process_belrdf to the returned RDF string from the
+    webservice.
+    """
+    logger.warning('This method is deprecated and the results are not '
+                   'guaranteed to be correct. Please use '
+                   'process_pybel_neighborhood instead.')
+    if network_id is None:
+        network_id = '9ea3c170-01ad-11e5-ac0f-000c29cb28fb'
+    url = ndex_bel2rdf + '/network/%s/asBELRDF/query' % network_id
+    params = {'searchString': ' '.join(gene_names)}
+    # The ndex_client returns the rdf as the content of a json dict
+    res_json = ndex_client.send_request(url, params, is_json=True)
+    if not res_json:
+        logger.error('No response for NDEx neighborhood query.')
+        return None
+    if res_json.get('error'):
+        error_msg = res_json.get('message')
+        logger.error('BEL/RDF response contains error: %s' % error_msg)
+        return None
+    rdf = res_json.get('content')
+    if not rdf:
+        logger.error('BEL/RDF response is empty.')
+        return None
+
+    with open(rdf_out, 'wb') as fh:
+        fh.write(rdf.encode('utf-8'))
+    bp = process_belrdf(rdf, print_output=print_output)
+    return bp
+
+
+def process_pybel_neighborhood(gene_names, network_file=None,
+                               network_type='belscript', **kwargs):
+    """Return PybelProcessor around neighborhood of given genes in a network.
+
+    This function processes the given network file and filters the returned
+    Statements to ones that contain genes in the given list.
+
+    Parameters
+    ----------
+    network_file : Optional[str]
+        Path to the network file to process. If not given, by default, the
+        BEL Large Corpus is used.
+    network_type : Optional[str]
+        This function allows processing both BEL Script files and JSON files.
+        This argument controls which type is assumed to be processed, and the
+        value can be either 'belscript' or 'json'. Default: bel_script
+
+    Returns
+    -------
+    bp : PybelProcessor
+        A PybelProcessor object which contains INDRA Statements in
+        bp.statements.
+    """
+    if network_file is None:
+        # Use large corpus as base network
+        network_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                    os.path.pardir, os.path.pardir,
+                                    os.path.pardir, 'data', 'large_corpus.bel')
+    if network_type == 'belscript':
+        bp = process_belscript(network_file, **kwargs)
+    elif network_type == 'json':
+        bp = process_json_file(network_file)
+
+    filtered_stmts = []
+    for stmt in bp.statements:
+        found = False
+        for agent in stmt.agent_list():
+            if agent is not None:
+                if agent.name in gene_names:
+                    found = True
+        if found:
+            filtered_stmts.append(stmt)
+
+    bp.statements = filtered_stmts
+
+    return bp
+
+
+def process_belrdf(rdf_str, print_output=True):
+    """Return a BelRdfProcessor for a BEL/RDF string.
+
+    Parameters
+    ----------
+    rdf_str : str
+        A BEL/RDF string to be processed. This will usually come from reading
+        a .rdf file.
+
+    Returns
+    -------
+    bp : BelRdfProcessor
+        A BelRdfProcessor object which contains INDRA Statements in
+        bp.statements.
+
+    Notes
+    -----
+    This function calls all the specific get_type_of_mechanism()
+    functions of the newly constructed BelRdfProcessor to extract
+    INDRA Statements.
+    """
+    g = rdflib.Graph()
+    try:
+        g.parse(data=rdf_str, format='nt')
+    except ParseError as e:
+        logger.error('Could not parse rdf: %s' % e)
+        return None
+    # Build INDRA statements from RDF
+    bp = BelRdfProcessor(g)
+    bp.get_complexes()
+    bp.get_activating_subs()
+    bp.get_modifications()
+    bp.get_activating_mods()
+    bp.get_transcription()
+    bp.get_activation()
+    bp.get_conversions()
+
+    # Print some output about the process
+    if print_output:
+        bp.print_statement_coverage()
+        bp.print_statements()
+    return bp
+
+
+@lru_cache(maxsize=100)
+def process_pybel_graph(graph):
+    """Return a PybelProcessor by processing a PyBEL graph.
+
+    Parameters
+    ----------
+    graph : pybel.struct.BELGraph
+        A PyBEL graph to process
+
+    Returns
+    -------
+    bp : PybelProcessor
+        A PybelProcessor object which contains INDRA Statements in
+        bp.statements.
+    """
+    bp = PybelProcessor(graph)
+    bp.get_statements()
+    if bp.annot_manager.failures:
+        logger.warning('missing %d annotation pairs',
+                       sum(len(v)
+                           for v in bp.annot_manager.failures.values()))
+    return bp
+
+
+def process_belscript(file_name, **kwargs):
+    """Return a PybelProcessor by processing a BEL script file.
+
+    Key word arguments are passed directly to pybel.from_path,
+    for further information, see
+    pybel.readthedocs.io/en/latest/io.html#pybel.from_path
+    Some keyword arguments we use here differ from the defaults
+    of PyBEL, namely we set `citation_clearing` to False
+    and `no_identifier_validation` to True.
+
+    Parameters
+    ----------
+    file_name : str
+        The path to a BEL script file.
+
+    Returns
+    -------
+    bp : PybelProcessor
+        A PybelProcessor object which contains INDRA Statements in
+        bp.statements.
+    """
+    if 'citation_clearing' not in kwargs:
+        kwargs['citation_clearing'] = False
+    if 'no_identifier_validation' not in kwargs:
+        kwargs['no_identifier_validation'] = True
+    pybel_graph = pybel.from_path(file_name, **kwargs)
+    return process_pybel_graph(pybel_graph)
+
+
+def process_json_file(file_name):
+    """Return a PybelProcessor by processing a Node-Link JSON file.
+
+    For more information on this format, see:
+    http://pybel.readthedocs.io/en/latest/io.html#node-link-json
+
+    Parameters
+    ----------
+    file_name : str
+        The path to a Node-Link JSON file.
+
+    Returns
+    -------
+    bp : PybelProcessor
+        A PybelProcessor object which contains INDRA Statements in
+        bp.statements.
+    """
+    with open(file_name, 'rt') as fh:
+        pybel_graph = pybel.from_json_file(fh, False)
+    return process_pybel_graph(pybel_graph)
+
+
+def process_cbn_jgif_file(file_name):
+    """Return a PybelProcessor by processing a CBN JGIF JSON file.
+
+    Parameters
+    ----------
+    file_name : str
+        The path to a CBN JGIF JSON file.
+
+    Returns
+    -------
+    bp : PybelProcessor
+        A PybelProcessor object which contains INDRA Statements in
+        bp.statements.
+    """
+    with open(file_name, 'r') as jgf:
+        return process_pybel_graph(pybel.from_cbn_jgif(json.load(jgf)))
---
+++ packages/indra/indra-1.14.1-py3-none-any.whl/indra/sources/indra_db_rest/processor.py
@@ -0,0 +1,312 @@
+from __future__ import absolute_import, unicode_literals
+from builtins import dict, str
+
+__all__ = ['IndraDBRestProcessor']
+
+import logging
+from copy import deepcopy
+from threading import Thread
+from datetime import datetime
+from collections import OrderedDict, defaultdict
+
+from indra.statements import stmts_from_json, get_statement_by_name, \
+    get_all_descendants
+
+from indra.sources.indra_db_rest.util import submit_query_request
+from indra.sources.indra_db_rest.exceptions import IndraDBRestResponseError
+
+logger = logging.getLogger(__name__)
+
+
+class IndraDBRestProcessor(object):
+    """The packaging for query responses.
+
+    Parameters
+    ----------
+    subject/object : str
+        Optionally specify the subject and/or object of the statements in
+        you wish to get from the database. By default, the namespace is assumed
+        to be HGNC gene names, however you may specify another namespace by
+        including `@<namespace>` at the end of the name string. For example, if
+        you want to specify an agent by chebi, you could use `CHEBI:6801@CHEBI`,
+        or if you wanted to use the HGNC id, you could use `6871@HGNC`.
+    agents : list[str]
+        A list of agents, specified in the same manner as subject and object,
+        but without specifying their grammatical position.
+    stmt_type : str
+        Specify the types of interactions you are interested in, as indicated
+        by the sub-classes of INDRA's Statements. This argument is *not* case
+        sensitive. If the statement class given has sub-classes
+        (e.g. RegulateAmount has IncreaseAmount and DecreaseAmount), then both
+        the class itself, and its subclasses, will be queried, by default. If
+        you do not want this behavior, set use_exact_type=True. Note that if
+        max_stmts is set, it is possible only the exact statement type will
+        be returned, as this is the first searched. The processor then cycles
+        through the types, getting a page of results for each type and adding it
+        to the quota, until the max number of statements is reached.
+    use_exact_type : bool
+        If stmt_type is given, and you only want to search for that specific
+        statement type, set this to True. Default is False.
+    persist : bool
+        Default is True. When False, if a query comes back limited (not all
+        results returned), just give up and pass along what was returned.
+        Otherwise, make further queries to get the rest of the data (which may
+        take some time).
+    timeout : positive int or None
+        If an int, block until the work is done and statements are retrieved, or
+        until the timeout has expired, in which case the results so far will be
+        returned in the response object, and further results will be added in
+        a separate thread as they become available. If simple_response is True,
+        all statements available will be returned. Otherwise (if None), block
+        indefinitely until all statements are retrieved. Default is None.
+    ev_limit : int or None
+        Limit the amount of evidence returned per Statement. Default is 10.
+    best_first : bool
+        If True, the preassembled statements will be sorted by the amount of
+        evidence they have, and those with the most evidence will be
+        prioritized. When using `max_stmts`, this means you will get the "best"
+        statements. If False, statements will be queried in arbitrary order.
+    tries : int > 0
+        Set the number of times to try the query. The database often caches
+        results, so if a query times out the first time, trying again after a
+        timeout will often succeed fast enough to avoid a timeout. This can also
+        help gracefully handle an unreliable connection, if you're willing to
+        wait. Default is 2.
+    max_stmts : int or None
+        Select the maximum number of statements to return. When set less than
+        1000 the effect is much the same as setting persist to false, and will
+        guarantee a faster response. Default is None.
+
+    Attributes
+    ----------
+    statements : list[:py:class:`indra.statements.Statement`]
+        A list of INDRA Statements that will be filled once all queries have
+        been completed.
+    statements_sample : list[:py:class:`indra.statements.Statement`]
+        A list of the INDRA Statements received from the first query. In
+        general these will be the "best" (currently this means they have the
+        most evidence) Statements available.
+    """
+    def __init__(self, subject=None, object=None, agents=None, stmt_type=None,
+                 use_exact_type=False, persist=True, timeout=None, ev_limit=10,
+                 best_first=True, tries=2, max_stmts=None):
+        self.statements = []
+        self.statements_sample = None
+        self.__statement_jsons = {}
+        self.__done_dict = defaultdict(lambda: False)
+        self.__evidence_counts = {}
+        self.__source_counts = {}
+        self.__started = False
+        self.__page_dict = defaultdict(lambda: 0)
+        self.__th = None
+        self.__quota = max_stmts
+
+        # Make sure we got at least SOME agents (the remote API will error if
+        # we proceed with no arguments).
+        if subject is None and object is None and not agents:
+            raise ValueError("At least one agent must be specified, or else "
+                             "the scope will be too large.")
+
+        # Formulate inputs for the agents..
+        agent_strs = [] if agents is None else ['agent%d=%s' % (i, ag)
+                                                for i, ag in enumerate(agents)]
+        key_val_list = [('subject', subject), ('object', object)]
+        params = {param_key: param_val for param_key, param_val in key_val_list
+                  if param_val is not None}
+        params['best_first'] = best_first
+        params['ev_limit'] = ev_limit
+        params['tries'] = tries
+
+        # Handle the type(s).
+        stmt_types = [stmt_type] if stmt_type else []
+        if stmt_type is not None and not use_exact_type:
+            stmt_class = get_statement_by_name(stmt_type)
+            descendant_classes = get_all_descendants(stmt_class)
+            stmt_types += [cls.__name__ for cls in descendant_classes]
+
+        # Handle the content if we were limited.
+        args = [agent_strs, stmt_types, params, persist]
+        logger.debug("The remainder of the query will be performed in a "
+                     "thread...")
+        self.__th = Thread(target=self._run_queries, args=args)
+        self.__th.start()
+
+        if timeout is None:
+            logger.debug("Waiting for thread to complete...")
+            self.__th.join()
+        elif timeout:  # is not 0
+            logger.debug("Waiting at most %d seconds for thread to complete..."
+                         % timeout)
+            self.__th.join(timeout)
+        return
+
+    def is_working(self):
+        """Check if the thread is running."""
+        if not self.__th:
+            return False
+        return self.__th.is_alive()
+
+    def get_ev_count(self, stmt):
+        """Get the total evidence count for a statement."""
+        return self.get_ev_count_by_hash(stmt.get_hash(shallow=True))
+
+    def get_ev_count_by_hash(self, stmt_hash):
+        """Get the total evidence count for a statement hash."""
+        return self.__evidence_counts.get(str(stmt_hash))
+
+    def get_source_counts(self):
+        """Get the source counts as a dict per statement hash."""
+        return deepcopy(self.__source_counts)
+
+    def get_source_count(self, stmt):
+        """Get the source counts for a given statement."""
+        return self.get_source_count_by_hash(stmt.get_hash(shallow=True))
+
+    def get_source_count_by_hash(self, stmt_hash):
+        """Get the source counts for a given statement."""
+        return self.__source_counts.get(stmt_hash)
+
+    def get_ev_counts(self):
+        """Get a dictionary of evidence counts."""
+        return self.__evidence_counts.copy()
+
+    def get_hash_statements_dict(self):
+        """Return a dict of Statements keyed by hashes."""
+        res = {stmt_hash: stmts_from_json([stmt])[0]
+               for stmt_hash, stmt in self.__statement_jsons.items()}
+        return res
+
+    def merge_results(self, other_processor):
+        """Merge the results of this processor with those of another."""
+        if not isinstance(other_processor, self.__class__):
+            raise ValueError("Can only extend with another %s instance."
+                             % self.__class__.__name__)
+        self.statements.extend(other_processor.statements)
+        if other_processor.statements_sample is not None:
+            if self.statements_sample is None:
+                self.statements_sample = other_processor.statements_sample
+            else:
+                self.statements_sample.extend(other_processor.statements_sample)
+
+        self._merge_json(other_processor.__statement_jsons,
+                         other_processor.__evidence_counts,
+                         other_processor.__source_counts)
+        return
+
+    def wait_until_done(self, timeout=None):
+        """Wait for the background load to complete."""
+        start = datetime.now()
+        if not self.__th:
+            raise IndraDBRestResponseError("There is no thread waiting to "
+                                           "complete.")
+        self.__th.join(timeout)
+        now = datetime.now()
+        dt = now - start
+        if self.__th.is_alive():
+            logger.warning("Timed out after %0.3f seconds waiting for "
+                           "statement load to complete." % dt.total_seconds())
+            ret = False
+        else:
+            logger.info("Waited %0.3f seconds for statements to finish"
+                        "loading." % dt.total_seconds())
+            ret = True
+        return ret
+
+    def _merge_json(self, stmt_json, ev_counts, source_counts):
+        """Merge these statement jsons with new jsons."""
+        # Where there is overlap, there _should_ be agreement.
+        self.__evidence_counts.update(ev_counts)
+        # We turn source counts into an int-keyed dict and update it that way
+        self.__source_counts.update({int(k): v for k,
+                                     v in source_counts.items()})
+
+        for k, sj in stmt_json.items():
+            if k not in self.__statement_jsons:
+                self.__statement_jsons[k] = sj  # This should be most of them
+            else:
+                # This should only happen rarely.
+                for evj in sj['evidence']:
+                    self.__statement_jsons[k]['evidence'].append(evj)
+
+        if not self.__started:
+            self.statements_sample = stmts_from_json(
+                self.__statement_jsons.values())
+            self.__started = True
+        return
+
+    def _compile_statements(self):
+        """Generate statements from the jsons."""
+        self.statements = stmts_from_json(self.__statement_jsons.values())
+
+    def _all_done(self):
+        every_type_done = (len(self.__done_dict) > 0
+                           and all(self.__done_dict.values()))
+        quota_done = (self.__quota is not None and self.__quota <= 0)
+        return every_type_done or quota_done
+
+    def _query_and_extract(self, agent_strs, params, stmt_type=None):
+        assert not self._all_done(), "Tried to run query but I'm done!"
+        params['offset'] = self.__page_dict[stmt_type]
+        params['max_stmts'] = self.__quota
+        if stmt_type is not None:
+            params['type'] = stmt_type
+        resp = submit_query_request('from_agents', *agent_strs, **params)
+        resp_dict = resp.json(object_pairs_hook=OrderedDict)
+        stmts_json = resp_dict['statements']
+        ev_totals = resp_dict['evidence_totals']
+        source_counts = resp_dict['source_counts']
+        page_step = resp_dict['statement_limit']
+        num_returned = len(stmts_json)
+
+        # Update the result
+        self._merge_json(stmts_json, ev_totals, source_counts)
+
+        # NOTE: this is technically not a direct conclusion, and could be
+        # wrong, resulting in a single unnecessary extra query, but that
+        # should almost never happen, and if it does, it isn't the end of
+        # the world.
+        self.__done_dict[stmt_type] = num_returned < page_step
+
+        # Update the quota
+        if self.__quota is not None:
+            self.__quota -= num_returned
+
+        # Increment the page
+        self.__page_dict[stmt_type] += page_step
+
+        return
+
+    def _query_over_statement_types(self, agent_strs, stmt_types, params):
+        if not stmt_types:
+            self._query_and_extract(agent_strs, params.copy())
+        else:
+            for stmt_type in stmt_types:
+                if self.__done_dict[stmt_type]:
+                    continue
+                self._query_and_extract(agent_strs, params.copy(), stmt_type)
+
+                # Check the quota
+                if self.__quota is not None and self.__quota <= 0:
+                    break
+        return
+
+    def _run_queries(self, agent_strs, stmt_types, params, persist):
+        """Use paging to get all statements requested."""
+        self._query_over_statement_types(agent_strs, stmt_types, params)
+
+        assert len(self.__done_dict) == len(stmt_types) \
+            or None in self.__done_dict.keys(), \
+            "Done dict was not initiated for all stmt_type's."
+
+        # Check if we want to keep going.
+        if not persist:
+            self._compile_statements()
+            return
+
+        # Get the rest of the content.
+        while not self._all_done():
+            self._query_over_statement_types(agent_strs, stmt_types, params)
+
+        # Create the actual statements.
+        self._compile_statements()
+        return
