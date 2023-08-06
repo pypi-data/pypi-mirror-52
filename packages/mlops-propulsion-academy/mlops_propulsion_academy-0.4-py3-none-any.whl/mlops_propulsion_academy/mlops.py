8d1609a797b55b35558e86a4ba0490835eeaab3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:55 +0000

    Add package mlops-propulsion-academy/0.4/mlops_propulsion_academy-0.4-py3-none-any.whl

---
+++ packages/mlops-propulsion-academy/mlops_propulsion_academy-0.4-py3-none-any.whl/mlops_propulsion_academy/mlops.py
@@ -0,0 +1,20 @@
+import os
+
+
+def install():
+    os.system('ls')
+    os.system('pwd')
+    os.system('chmod +x ./mlops_propulsion_academy/scripts/install.sh')
+    os.system('sh ./mlops_propulsion_academy/scripts/install.sh')
+
+
+def update():
+    # os.system('chmod +x ./mlops_propulsion_academy/scripts/update.sh')
+    # os.system('sh ./mlops_propulsion_academy/scripts/update.sh')
+    pass
+
+
+def start():
+    print('Starting')
+    os.system('chmod +x ./mlops_propulsion_academy/scripts/start.sh')
+    os.system('sh ./mlops_propulsion_academy/scripts/start.sh')
