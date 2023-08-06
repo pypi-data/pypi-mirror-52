7c593bdd7470650f2709dd57b011c8f154345950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:20 +0000

    Add package AsyncLine/1.5.9.4/AsyncLine-1.5.9.4-py3-none-any.whl

---
+++ packages/AsyncLine/AsyncLine-1.5.9.4-py3-none-any.whl/AsyncLine/__init__.py
@@ -0,0 +1,22 @@
+from .lib.Gen.ttypes import *
+from .lib.Gen.liff import FLiffServiceClient as LiffClient
+from .lib.Gen.liff.ttypes import *
+from .connections import Connection
+from .main import Client
+from .proto import *
+from .filters import *
+from .database import *
+from .timeline import *
+from .log import *
+from .talk import *
+from .shop import *
+from .poll import *
+from .auth import *
+from .models import *
+from .handler import *
+from .call import Call
+__author__ = "Dyseo"
+__from_source__ = "Anysz"
+__base__ = "Anysz"
+__fixing__ = "Dyseo"
+__version__ = "1.5.9"
---
+++ packages/AsyncLine/AsyncLine-1.5.9.4-py3-none-any.whl/AsyncLine/http_client.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+import async_timeout, asyncio, aiohttp
+from aiohttp.client import ClientSession
+
+from thrift.transport.TTransport import TTransportBase
+from thrift.transport.TTransport import TMemoryBuffer
+from thrift.transport.TTransport import TTransportException
+
+from frugal.aio.transport import FTransportBase
+from frugal.context import FContext
+from frugal.aio.transport.http_transport import FHttpTransport
+from frugal.exceptions import TTransportExceptionType
+
+class HttpClient(FHttpTransport):
+	def __init__(self, url, timeout=5000, loop=None):
+		super().__init__(0)
+		self._url = url
+		self.loop = loop if loop else asyncio.get_event_loop()
+		self.setTimeout(timeout) 
+		self._headers = {
+			'Content-Type': 'application/x-thrift',
+			'Accept': 'application/x-thrift',
+			'User-Agent': 'Python/Frugal Thrift',
+		}
+		
+	def setTimeout(self, timeout):
+		self._timeout = timeout
+
+	async def request(self, context: FContext, payload) -> TTransportBase:
+		payload = payload[4:] 
+		self._payload = payload
+		self._preflight_request_check(payload)
+		status, text = await self._make_request(context, self._payload)
+		if status == 400: 
+			raise TTransportException(
+				type=400, 
+				message='Bad request: '+str(text) + ' :: '+ str(payload))
+		elif status == 403:
+			raise TTransportException(
+				type=403, 
+				message='Forbidden: '+str(text))
+		elif status == 404:
+			raise TTransportException(
+				type=404,
+				message='Not Found: '+str(text))
+		elif status == 410:
+			pass
+		elif status == 500:
+			raise TTransportException(
+				type=500,
+				message='Backend Error: '+str(text))
+		elif status >= 300:
+			raise TTransportException(
+				type=TTransportExceptionType.UNKNOWN,
+				message='request errored with {0} and message {1}'.format(
+					status, str(text)
+					))
+		return TMemoryBuffer(text)
+		
+	async def _make_request(self, context:FContext, payload):
+		sem = asyncio.Semaphore(200)
+		conn = aiohttp.TCPConnector(use_dns_cache=True, loop=self.loop, limit=0)
+		async with sem:
+			async with ClientSession(connector=conn) as session:
+				try:
+					if self._timeout > 0:
+						with async_timeout.timeout(self._timeout / 1000):
+							async with session.post(self._url, 
+												data=payload,
+												headers=self._headers) \
+								as response:
+								return response.status, await response.content.read()
+					else:
+						async with session.post(self._url,data=payload,headers=self._headers) as response:
+							return response.status, await response.content.read()
+				except asyncio.TimeoutError:
+					raise TTransportException(
+						type=TTransportExceptionType.TIMED_OUT,
+						message='request timed out'
+						)
