a5c525f60101728eb864022b2d822991fbf0308b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:53 +0000

    Add package roleml/0.1.10/roleml-0.1.10.tar.gz

---
+++ packages/roleml/roleml-0.1.10.tar.gz/roleml-0.1.10/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+setup(
+  name = 'roleml',
+  packages = find_packages(),
+  package_data={
+    '': ['*.sav'],
+  },
+  version = '0.1.10',
+  description = 'ML classifier for role in Riot API LoL matches',
+  author = 'Canisback',
+  author_email = 'canisback@gmail.com',
+  url = 'https://github.com/Canisback/roleML',
+  keywords = ['Riot API', 'python', 'machine learning','role','Leaue of Legends', ' classifier'],
+  classifiers = [],
+  install_requires=[
+    "scikit-learn",
+      "numpy >= 1.16.2",
+      "matplotlib",
+      "pandas"
+  ],
+)
