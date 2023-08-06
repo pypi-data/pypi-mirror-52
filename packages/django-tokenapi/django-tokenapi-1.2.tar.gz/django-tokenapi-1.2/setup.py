d8fa1edb14d76cc441b4ce982e4ccc0503c4dd83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:52 +0000

    Add package django-tokenapi/1.2/django-tokenapi-1.2.tar.gz

---
+++ packages/django-tokenapi/django-tokenapi-1.2.tar.gz/django-tokenapi-1.2/setup.py
@@ -0,0 +1,26 @@
+# Run 'setup.py sdist register upload' to upload new version
+
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='django-tokenapi',
+    version='1.2',
+    description='Add an API to your Django app using token-based authentication.',
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    author='Julian Pulgarin',
+    author_email='julian@pulgarin.co',
+    url='https://github.com/jpulgarin/django-tokenapi',
+    packages=['tokenapi'],
+    license='Apache License, Version 2.0',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+)
