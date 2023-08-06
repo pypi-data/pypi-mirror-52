eef3508fb1dcfb1c4a19de9821c70b5cbef419f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:53 +0000

    Add package skimage-encoder-v2/0.0.1/skimage-encoder-v2-0.0.1.tar.gz

---
+++ packages/skimage-encoder-v2/skimage-encoder-v2-0.0.1.tar.gz/skimage-encoder-v2-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="skimage-encoder-v2",
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    scripts=["skimage-encoder-v2"],
+    python_requires=">=3.6",
+)
