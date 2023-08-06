a9faf80a3ca42a5aba1a63a0b067e8965e2afad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:49 +0000

    Add package outplan/1.2.2/outplan-1.2.2.tar.gz

---
+++ packages/outplan/outplan-1.2.2.tar.gz/outplan-1.2.2/outplan/const.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+
+class GroupResultType(object):
+    """Type of group item result"""
+    group = 1   # return result directly
+    layer = 2   # namespace nested
+
+
+ONE_MINUTE = 60
+ONE_HOUR = ONE_MINUTE * 60
