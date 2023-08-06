a1591c31f4ca9695915d9a8e624a8f228357e1d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:22 +0000

    Add package distributions-metric/1.0/distributions_metric-1.0.tar.gz

---
+++ packages/distributions-metric/distributions_metric-1.0.tar.gz/distributions_metric-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_metric',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_metric'],
+      author='Yu Zheng',
+      zip_safe=False)
