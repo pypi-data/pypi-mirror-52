01d43eea0ece44888cc50a37c8e47a4f7be895dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:36 +0000

    Add package sparkworksrest/1.0.1/sparkworksrest-1.0.1.tar.gz

---
+++ packages/sparkworksrest/sparkworksrest-1.0.1.tar.gz/sparkworksrest-1.0.1/sparkworks_client/api/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from sparkworks_client.api.command_api_api import CommandAPIApi
+from sparkworks_client.api.data_api_api import DataAPIApi
+from sparkworks_client.api.gateway_api_api import GatewayAPIApi
+from sparkworks_client.api.group_api_api import GroupAPIApi
+from sparkworks_client.api.participatory_sensing_api_api import ParticipatorySensingAPIApi
+from sparkworks_client.api.phenomenon_api_api import PhenomenonAPIApi
+from sparkworks_client.api.registry_api_api import RegistryAPIApi
+from sparkworks_client.api.resource_api_api import ResourceAPIApi
+from sparkworks_client.api.unit_api_api import UnitAPIApi
