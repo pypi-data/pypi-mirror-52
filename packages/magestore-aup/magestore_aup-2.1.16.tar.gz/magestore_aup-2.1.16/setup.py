d157b16a0285cb2f632628e808dd0459bbd49fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:46 +0000

    Add package magestore-aup/2.1.16/magestore_aup-2.1.16.tar.gz

---
+++ packages/magestore-aup/magestore_aup-2.1.16.tar.gz/magestore_aup-2.1.16/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+package_name = 'magestore_aup'
+packages = setuptools.find_packages(
+    include=[package_name, "{}.*".format(package_name)]
+)
+
+setuptools.setup(
+    name=package_name,
+    version="2.1.16",
+    author="Mars",
+    author_email="mars@trueplus.vn",
+    description="Don't need to show error when install lib",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://magestore.com",
+    packages=packages,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3',
+    install_requires=[
+        'cryptography>=2.5',
+        'fabric',
+        'requests'
+    ],
+    include_package_data=True
+)
