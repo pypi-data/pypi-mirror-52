9dabe5e61d82d29897e43789786f46280c288f7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:46 +0000

    Add package adhesive/0.7.9/adhesive-0.7.9-py3-none-any.whl

---
+++ packages/adhesive/adhesive-0.7.9-py3-none-any.whl/adhesive/execution/ExecutionTask.py
@@ -0,0 +1,44 @@
+from typing import Callable, Optional
+
+from adhesive.logredirect.LogRedirect import redirect_stdout
+from adhesive.model.ActiveEvent import ActiveEvent
+from adhesive.execution import token_utils
+from adhesive.execution.ExecutionBaseTask import ExecutionBaseTask
+from .ExecutionToken import ExecutionToken
+
+
+class ExecutionTask(ExecutionBaseTask):
+    """
+    A task implementation.
+    """
+    def __init__(self,
+                 code: Callable,
+                 *expressions: str,
+                 loop: Optional[str] = None,
+                 when: Optional[str] = None,
+                 lane: Optional[str] = None) -> None:
+        """
+        Create a new adhesive task. The `loop`, `when` and `lane` are only
+        available when doing a programmatic API.
+        :param code:
+        :param expressions:
+        :param loop:
+        :param when:
+        """
+        super(ExecutionTask, self).__init__(code, *expressions)
+
+        self.loop = loop
+        self.when = when
+        self.lane = lane
+
+    def invoke(
+            self,
+            event: ActiveEvent) -> ExecutionToken:
+        with redirect_stdout(event):
+            params = token_utils.matches(self.re_expressions,
+                                         event.context.task_name)
+
+            self.code(event.context, *params)  # type: ignore
+
+            return event.context
+
