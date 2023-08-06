e3d5e928784a0e1a1238cb1c1a8d16aebdba4f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:33 +0000

    Add package easy-icm-runner/1.2.1/easy-icm-runner-1.2.1.tar.gz

---
+++ packages/easy-icm-runner/easy-icm-runner-1.2.1.tar.gz/easy-icm-runner-1.2.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+setup(
+  name = 'easy-icm-runner',
+  packages = ['easy-icm-runner',],
+  version = '1.2.1',
+  license='MIT',
+  description = 'A wrapper for IBM ICMs Scheduler API Calls',
+  author = 'Bachir El Koussa and Elliott Cordo',
+  author_email = 'bgkoussa@gmail.com',
+  url = 'https://github.com/equinoxfitness/easy-icm-runner',
+  download_url = 'https://github.com/equinoxfitness/easy-icm-runner/archive/v_1.1.0.tar.gz',
+  keywords = ['ICM', 'API', 'SCHEDULER', 'RUNNER', 'IBM'],
+  install_requires=[
+          'requests',
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+)
