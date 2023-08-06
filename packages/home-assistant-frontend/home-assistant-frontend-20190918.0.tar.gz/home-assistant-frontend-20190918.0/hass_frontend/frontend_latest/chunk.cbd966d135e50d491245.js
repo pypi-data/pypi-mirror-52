a03ce3c9e0411b82982795668c6827c58e75151f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:06 +0000

    Add package home-assistant-frontend/20190918.0/home-assistant-frontend-20190918.0.tar.gz

---
+++ packages/home-assistant-frontend/home-assistant-frontend-20190918.0.tar.gz/home-assistant-frontend-20190918.0/hass_frontend/frontend_latest/chunk.cbd966d135e50d491245.js
@@ -0,0 +1,2 @@
+(self.webpackJsonp=self.webpackJsonp||[]).push([[67],{136:function(e,s,n){"use strict";n.r(s);var a=n(30),b=n(5),l=n(4);b.b.Element=a.a,b.b.html=l.a,window.Polymer=b.b}}]);
+//# sourceMappingURL=chunk.cbd966d135e50d491245.js.map
