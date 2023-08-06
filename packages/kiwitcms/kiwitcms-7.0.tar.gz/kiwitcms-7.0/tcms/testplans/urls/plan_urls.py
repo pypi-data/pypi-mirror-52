9db530b5b18996875df02a04fd0191bb0537e9d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:13 +0000

    Add package kiwitcms/7.0/kiwitcms-7.0.tar.gz

---
+++ packages/kiwitcms/kiwitcms-7.0.tar.gz/kiwitcms-7.0/tcms/testplans/urls/plan_urls.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import url
+
+from tcms.testplans import views
+
+urlpatterns = [
+    url(r'^(?P<plan_id>\d+)/$', views.get, name='test_plan_url_short'),
+    # human friendly url
+    url(r'^(?P<plan_id>\d+)/(?P<slug>[-\w\d]+)$', views.get, name='test_plan_url'),
+
+    url(r'^(?P<plan_id>\d+)/edit/$', views.edit, name='plan-edit'),
+
+    url(r'^(?P<plan_id>\d+)/reorder-cases/$', views.ReorderCasesView.as_view(),
+        name='plan-reorder-cases'),
+    url(r'^(?P<plan_id>\d+)/link-cases/$', views.LinkCasesView.as_view(),
+        name='plan-link-cases'),
+    url(r'^(?P<plan_id>\d+)/link-cases/search/$', views.LinkCasesSearchView.as_view(),
+        name='plan-search-cases-for-link'),
+    url(r'^(?P<plan_id>\d+)/delete-cases/$', views.DeleteCasesView.as_view(),
+        name='plan-delete-cases'),
+
+    url(r'^update-parent/$', views.UpdateParentView.as_view()),
+]
