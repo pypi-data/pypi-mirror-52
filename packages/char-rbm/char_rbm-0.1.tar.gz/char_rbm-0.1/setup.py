3aa98806421b598fa591d65ff1ad45073d5942dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:15 +0000

    Add package char-rbm/0.1/char_rbm-0.1.tar.gz

---
+++ packages/char-rbm/char_rbm-0.1.tar.gz/char_rbm-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='char_rbm',
+    version='0.1',
+    packages=['char_rbm'],
+    url='',
+    license='',
+    install_requires=["scikit-learn", "colorama"],
+    author='jarbasai',
+    author_email='jarbasai@mailfence.com',
+    description='simple interface to train and sample char-rbms'
+)
