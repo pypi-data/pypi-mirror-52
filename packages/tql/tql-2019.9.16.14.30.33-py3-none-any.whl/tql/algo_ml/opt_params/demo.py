aad49bcb70326908e18ca5e300a8c95721fef666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:29 +0000

    Add package tql/2019.9.16.14.30.33/tql-2019.9.16.14.30.33-py3-none-any.whl

---
+++ packages/tql/tql-2019.9.16.14.30.33-py3-none-any.whl/tql/algo_ml/opt_params/demo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : tql-Python.
+# @File         : demo
+# @Time         : 2019-09-16 14:17
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
+from tql.algo_ml.opt_params import LGBMOptimizer
+
+print(LGBMOptimizer.LGBMOptimizer(..., ...).params_bounds)
