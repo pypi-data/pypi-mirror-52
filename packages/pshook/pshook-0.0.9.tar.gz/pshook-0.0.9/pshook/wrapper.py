0f9c83580125614677a3090bf79e3b5c7678a64a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:41 +0000

    Add package pshook/0.0.9/pshook-0.0.9.tar.gz

---
+++ packages/pshook/pshook-0.0.9.tar.gz/pshook-0.0.9/pshook/wrapper.py
@@ -0,0 +1,48 @@
+#!venv/bin/python
+import os
+import psutil
+import sys
+import time
+import json
+from subprocess import Popen
+
+pickup_stats = ['username', 'cpu_num', 'cpu_percent', 'cwd']
+
+
+def proc_probe(ps_proc, log_entry):
+    try:
+        update_probe = ps_proc.as_dict(attrs=pickup_stats)
+        log_entry['log_time'].append(time.time())
+        for attr in pickup_stats:
+            log_entry[attr].append(update_probe[attr])
+    except psutil.NoSuchProcess as e:
+        return False
+    return True
+
+
+def execute_command(cmd):
+    proc = Popen(cmd)
+    ps_proc = psutil.Process(proc.pid)
+    init_probe = ps_proc.as_dict(attrs=['pid', 'name', 'username', 'cpu_num', 'cpu_percent', 'create_time', 'cwd', 'cmdline'])
+    log_entry = dict(init_probe)
+    log_entry['log_time'] = [time.time()]
+    for attr in pickup_stats:
+        log_entry[attr] = [log_entry[attr]]
+    while proc_probe(ps_proc, log_entry):
+        try:
+            proc.wait(timeout=.1)
+        except:
+            pass
+    log_entry['duration'] = log_entry['log_time'][-1:][0] - log_entry['log_time'][0]
+
+    try:
+        open('/tmp/psmon.log', 'a').write(json.dumps(log_entry))
+    except:
+        pass
+
+    return proc.returncode
+
+
+if __name__ == '__main__':
+    sys.exit(execute_command(sys.argv[1:]))
+
---
+++ packages/pshook/pshook-0.0.9.tar.gz/pshook-0.0.9/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pshook",
+    version="0.0.9",
+    author="Jakob Simon-Gaarde",
+    author_email="jakobsg@simon-gaarde.dk",
+    description="A small tool that enables monitoring of executable files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/jakobsg/ladon",
+    packages=setuptools.find_packages(),
+    scripts=['bin/pshook'],
+    install_requires = [
+        'psutil==5.6.3'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.6',
+)
