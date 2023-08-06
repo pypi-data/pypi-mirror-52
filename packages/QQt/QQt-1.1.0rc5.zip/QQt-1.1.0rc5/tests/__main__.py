f09df0966439a0a17346ae92e252430044fe13c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:00 +0000

    Add package QQt/1.1.0rc5/QQt-1.1.0rc5.zip

---
+++ packages/QQt/QQt-1.1.0rc5.zip/QQt-1.1.0rc5/tests/__main__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2012-2019 Adam Karpierz
+# Licensed under the zlib/libpng License
+# http://opensource.org/licenses/zlib/
+
+import unittest, sys
+from . import test_dir, top_dir
+tests = unittest.defaultTestLoader.discover(start_dir=test_dir,
+                                            top_level_dir=top_dir)
+result = unittest.TextTestRunner(verbosity=1).run(tests)
+sys.exit(0 if result.wasSuccessful() else 1)
