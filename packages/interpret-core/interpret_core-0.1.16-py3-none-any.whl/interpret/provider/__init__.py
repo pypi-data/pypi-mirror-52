f6411fabd7c9df2d4df000715c747629f77fe64e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:50 +0000

    Add package interpret-core/0.1.16/interpret_core-0.1.16-py3-none-any.whl

---
+++ packages/interpret-core/interpret_core-0.1.16-py3-none-any.whl/interpret/provider/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2019 Microsoft Corporation
+# Distributed under the MIT software license
+
+from .compute import ComputeProvider, JobLibProvider  # noqa: F401
+from .visualize import (  # noqa: F401
+    VisualizeProvider,
+    DashProvider,
+    AutoVisualizeProvider,
+    PreserveProvider,
+)
