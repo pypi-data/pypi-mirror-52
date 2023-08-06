a699717277222f906827b6dea716138248750f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:39 +0000

    Add package solarpy/0.1.3/solarpy-0.1.3.tar.gz

---
+++ packages/solarpy/solarpy-0.1.3.tar.gz/solarpy-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="solarpy",
+    author='aqreed',
+    description='Solar radiation model based on Duffie & Beckman\
+                "Solar energy thermal processes" (1974)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version="0.1.3",
+    url='https://github.com/aqreed/solarpy',
+    packages=['solarpy'],
+    install_requires=['numpy', 'matplotlib'],
+    tests_requires=['pytest']
+    )
