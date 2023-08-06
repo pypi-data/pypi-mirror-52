054481fe0da11876d4b07d12f781d48ced9e9c01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:47 +0000

    Add package rioxarray/0.0.11/rioxarray-0.0.11.tar.gz

---
+++ packages/rioxarray/rioxarray-0.0.11.tar.gz/rioxarray-0.0.11/rioxarray/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for rioxarray."""
+
+__author__ = """rioxarray Contributors"""
+__email__ = "alansnow21@gmail.com"
+
+import rioxarray.rioxarray  # noqa
+from rioxarray._io import open_rasterio  # noqa
+from rioxarray._version import __version__  # noqa
