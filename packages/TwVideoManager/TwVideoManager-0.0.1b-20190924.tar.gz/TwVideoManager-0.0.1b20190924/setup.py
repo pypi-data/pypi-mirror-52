0647e75e61c193de307cc8de60b2c7bc7facfa6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:55 +0000

    Add package TwVideoManager/0.0.1b20190924/TwVideoManager-0.0.1b-20190924.tar.gz

---
+++ packages/TwVideoManager/TwVideoManager-0.0.1b-20190924.tar.gz/TwVideoManager-0.0.1b20190924/setup.py
@@ -0,0 +1,34 @@
+import re
+from setuptools import setup
+
+
+# Reading version
+# Reffered https://github.com/Rapptz/discord.py/blob/master/setup.py#L8-L30
+with open("TwVideoManager/__init__.py") as f:
+    data = f.read()
+    version = re.search(r'^__VERSION__\s=\s[\'"]([^\'"]*)[\'"]', data, re.MULTILINE).group(1)
+
+    if version.endswith(("a", "b", "rp")):
+        version += "-" + re.search(r'^__DATE__\s=\s[\'"](\d+)[\'"]', data, re.MULTILINE).group(1)
+
+# Reading long description
+long_description = open("README.md", encoding="UTF-8").read()
+
+setup(
+    name='TwVideoManager',
+    version=version,
+    packages=['TwVideoManager'],
+    url='https://github.com/R3turn-Dev/TwVideoManager/',
+    license='LGPL-3.0',
+    author='Eunhak Lee(return0927)',
+    author_email='admin@return0927.xyz',
+    description='Unified Twitch Video manager via Local and Google Drive',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    )
