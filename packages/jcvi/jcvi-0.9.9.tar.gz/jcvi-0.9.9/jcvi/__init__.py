6f58df6aad6f5cc26abb18d36b4652beb1f87af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:16 +0000

    Add package jcvi/0.9.9/jcvi-0.9.9.tar.gz

---
+++ packages/jcvi/jcvi-0.9.9.tar.gz/jcvi-0.9.9/jcvi/__init__.py
@@ -0,0 +1,6 @@
+__author__ = ("Haibao Tang", "Vivek Krishnakumar", "Jingping Li")
+__copyright__ = "Copyright (c) 2010-2019, Haibao Tang"
+__email__ = "tanghaibao@gmail.com"
+__license__ = "BSD"
+__status__ = "Development"
+__version__ = "0.9.9"
