406657b02b92900a24afbf2fb116c846624ef4c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:41 +0000

    Add package ax/0.34.0/ax-0.34.0-py3-none-any.whl

---
+++ packages/ax/ax-0.34.0-py3-none-any.whl/ax/backend/migration.py
@@ -0,0 +1,432 @@
+"""
+Module for database migration using Alembic
+It checks if database is accesible.
+Creates sqlite database if needed.
+Checks if database have SqlAlchemy tables. If not - it creates tables.
+    When tables are created, they are filled with default values.
+    Such as AxFieldType are filled with field types.
+Checks that current Database schema fits SqlAlchemy schema. If it is not,
+    it tryes to upgrade it by running alembic migration scripts.
+
+"""
+import os
+import sys
+from loguru import logger
+from alembic.config import Config
+from alembic.migration import MigrationContext
+from alembic.autogenerate import compare_metadata
+from alembic import command
+import backend.model as ax_model
+import backend.misc as ax_misc
+from backend.model import AxAlembicVersion, AxFieldType
+
+this = sys.modules[__name__]
+alembic_cfg = None
+
+
+def init_alembic_config() -> None:
+    """Create Alembic config"""
+    try:
+        ini_path = ax_misc.path("alembic.ini")
+        this.alembic_cfg = Config(ini_path)
+        alembic_folder = ax_misc.path("backend/alembic")
+        this.alembic_cfg.set_main_option(
+            'script_location', str(alembic_folder))
+    except Exception:
+        logger.exception('Error initating alembic config.')
+        raise
+
+
+def tables_exist() -> bool:
+    """Checks if database tables are created"""
+    try:
+        tables_created = ax_model.engine.dialect.has_table(
+            ax_model.engine, '_ax_alembic_version')
+        if tables_created:
+            return True
+        else:
+            return False
+    except Exception:
+        logger.exception('Error in detecting if database tables are created')
+        raise
+
+
+def create_tables() -> None:
+    """Create database and create baseline version in Alembic"""
+    with ax_model.scoped_session("migration -> create_tables") as db_session:
+        if os.environ.get('AX_DB_REVISION') is None:
+            msg = 'Cant find AX_DB_REVISION in enviroment variables or app.yaml'
+            logger.error(msg)
+            raise Exception(msg)
+
+        # ax_model.Base.query = db_session.query_property()
+        ax_model.Base.metadata.create_all(ax_model.engine)
+        first_version = AxAlembicVersion()
+        first_version.version_num = os.environ.get('AX_DB_REVISION')
+        db_session.add(first_version)
+
+        logger.info('Ax tables not found. Creating database tables.')
+        return True
+
+
+def create_field_types() -> None:
+    """Fill AxFieldType with field types"""
+    try:
+        with ax_model.scoped_session() as db_session:
+            # Identification
+            db_session.add(AxFieldType(
+                tag='group-id',
+                is_group=True,
+                position=1,
+                icon="key"))
+            db_session.add(AxFieldType(
+                tag="AxGuid",
+                parent="group-id",
+                default_db_name="rowGuid",
+                position=1,
+                is_virtual=True,
+                virtual_source='guid',
+                comparator="",
+                icon="key"))
+            db_session.add(AxFieldType(
+                tag='AxNum',
+                parent="group-id",
+                default_db_name="axNum",
+                value_type="VARCHAR(255)",
+                position=2,
+                is_readonly=True,
+                is_backend_available=True,
+                icon="sort-numeric-up"))
+
+            # Process controll
+            db_session.add(AxFieldType(
+                tag='group-process',
+                is_group=True,
+                position=2,
+                icon="code-branch"))
+            db_session.add(AxFieldType(
+                tag="AxState",
+                parent="group-process",
+                default_db_name="rowState",
+                position=1,
+                is_virtual=True,
+                virtual_source='axState',
+                icon="code-branch"))
+            db_session.add(AxFieldType(
+                tag="AxChangelog",
+                parent="group-process",
+                position=4,
+                default_db_name="changelog",
+                is_columnn_avalible=True,
+                value_type="JSON",
+                is_backend_available=True,
+                is_readonly=True,
+                is_updated_always=True,
+                is_always_whole_row=True,
+                icon="code-branch"))
+
+            # String
+            db_session.add(AxFieldType(
+                tag='group-string',
+                is_group=True,
+                position=3,
+                icon="font"))
+            db_session.add(AxFieldType(
+                tag="AxString",
+                parent="group-string",
+                default_db_name="string",
+                position=1,
+                value_type="VARCHAR(255)",
+                is_inline_editable=True,
+                icon="font"))
+            db_session.add(AxFieldType(
+                tag="AxEmail",
+                parent="group-string",
+                default_db_name="email",
+                position=2,
+                value_type="VARCHAR(255)",
+                is_inline_editable=True,
+                icon="font"))
+            db_session.add(AxFieldType(
+                tag="AxTelephone",
+                parent="group-string",
+                default_db_name="telephone",
+                position=3,
+                value_type="VARCHAR(255)",
+                is_columnn_avalible=True,
+                is_inline_editable=True,
+                icon="font"))
+
+            # Decoration
+            db_session.add(AxFieldType(
+                tag='group-decoration',
+                is_group=True,
+                position=4,
+                icon="code"))
+            db_session.add(AxFieldType(
+                tag="AxHtml",
+                parent="group-decoration",
+                default_db_name="html",
+                position=1,
+                is_virtual=True,
+                virtual_source='guid',
+                is_display_backend_avalible=True,
+                icon="code"))
+
+            # Text
+            db_session.add(AxFieldType(
+                tag='group-text',
+                name="types.text",
+                is_group=True,
+                position=5,
+                icon="align-left"))
+            db_session.add(AxFieldType(
+                tag="AxText",
+                parent="group-text",
+                default_db_name="text",
+                position=1,
+                value_type="TEXT",
+                is_inline_editable=True,
+                icon="align-left"))
+            db_session.add(AxFieldType(
+                tag="AxMarkdown",
+                parent="group-text",
+                default_db_name="markdown",
+                position=1,
+                value_type="TEXT",
+                is_inline_editable=True,
+                icon="align-left"))
+
+            # Number
+            db_session.add(AxFieldType(
+                tag='group-number',
+                is_group=True,
+                position=6,
+                icon="hashtag"))
+            db_session.add(AxFieldType(
+                tag="AxInteger",
+                parent="group-number",
+                default_db_name="integer",
+                position=1,
+                value_type="INT",
+                comparator="number",
+                is_inline_editable=True,
+                is_columnn_avalible=True,
+                icon="hashtag"))
+            db_session.add(AxFieldType(
+                tag="AxDecimal",
+                parent="group-number",
+                default_db_name="decimal",
+                position=2,
+                value_type="DECIMAL(65,2)",
+                comparator="number",
+                is_columnn_avalible=True,
+                is_inline_editable=True,
+                icon="hashtag"))
+            db_session.add(AxFieldType(
+                tag="AxIntSlider",
+                parent="group-number",
+                default_db_name="intSlider",
+                position=1,
+                value_type="INT",
+                comparator="number",
+                is_inline_editable=True,
+                icon="hashtag"))
+
+            # Boolean
+            db_session.add(AxFieldType(
+                tag='group-boolean',
+                is_group=True,
+                position=7,
+                icon="toggle-on"))
+            db_session.add(AxFieldType(
+                tag="AxCheckbox",
+                parent="group-boolean",
+                default_db_name="checkbox",
+                position=1,
+                value_type="BOOL",
+                is_inline_editable=True,
+                is_columnn_avalible=True,
+                icon="toggle-on"))
+
+            # Relationship
+            db_session.add(AxFieldType(
+                tag='group-relationship',
+                is_group=True,
+                position=8,
+                icon="link"))
+            db_session.add(AxFieldType(
+                tag="Ax1to1",
+                parent="group-relationship",
+                default_name="types.ax-1to1-default",
+                default_db_name="toOne",
+                position=1,
+                value_type="GUID",
+                is_inline_editable=False,
+                is_columnn_avalible=True,
+                icon="link"))
+            db_session.add(AxFieldType(
+                tag="Ax1tom",
+                parent="group-relationship",
+                default_name="types.ax-1tom-default",
+                default_db_name="toMany",
+                position=2,
+                value_type="JSON",
+                is_backend_available=True,
+                is_columnn_avalible=True,
+                is_inline_editable=False,
+                icon="link"))
+            db_session.add(AxFieldType(
+                tag="Ax1tomTable",
+                parent="group-relationship",
+                default_name="types.ax-1tom-table-default",
+                default_db_name="toManyTable",
+                position=3,
+                value_type="JSON",
+                is_backend_available=True,
+                is_columnn_avalible=True,
+                is_inline_editable=True,
+                icon="link"))
+
+            #Date and Time
+            db_session.add(AxFieldType(
+                tag='group-date',
+                is_group=True,
+                position=9,
+                icon="calendar"))
+            db_session.add(AxFieldType(
+                tag="AxDate",
+                parent="group-date",
+                default_db_name="date",
+                position=1,
+                value_type="TIMESTAMP",
+                comparator="date",
+                is_inline_editable=True,
+                icon="calendar"))
+
+            # List
+            db_session.add(AxFieldType(
+                tag='group-list',
+                is_group=True,
+                position=10,
+                icon="list"))
+            db_session.add(AxFieldType(
+                tag="AxChoise",
+                parent="group-list",
+                default_db_name="choise",
+                position=1,
+                value_type="VARCHAR(255)",
+                is_inline_editable=True,
+                icon="list"))
+            db_session.add(AxFieldType(
+                tag="AxRadio",
+                parent="group-list",
+                default_db_name="radio",
+                position=1,
+                value_type="VARCHAR(255)",
+                is_inline_editable=True,
+                icon="list"))
+
+            # Images
+            db_session.add(AxFieldType(
+                tag='group-images',
+                is_group=True,
+                position=11,
+                icon="image"))
+            db_session.add(AxFieldType(
+                tag="AxImageCropDb",
+                parent="group-images",
+                position=4,
+                default_db_name="cropImage",
+                is_backend_available=True,
+                value_type="VARCHAR(255)",
+                icon="image"))
+
+            # Files
+            db_session.add(AxFieldType(
+                tag='group-files',
+                is_group=True,
+                position=12,
+                icon="file"))
+            db_session.add(AxFieldType(
+                tag="AxFiles",
+                parent="group-files",
+                position=1,
+                default_db_name="files",
+                value_type="JSON",
+                is_backend_available=True,
+                icon="file"))
+
+            # Users
+            db_session.add(AxFieldType(
+                tag='group-users',
+                is_group=True,
+                position=13,
+                icon="user"))
+            db_session.add(AxFieldType(
+                tag="AxUsers",
+                parent="group-users",
+                position=1,
+                default_db_name="users",
+                is_backend_available=True,
+                value_type="JSON",
+                icon="user"))
+
+            # Communication
+            db_session.add(AxFieldType(
+                tag='group-communication',
+                is_group=True,
+                position=14,
+                icon="comments"))
+            db_session.add(AxFieldType(
+                tag="AxComments",
+                parent="group-communication",
+                position=4,
+                default_db_name="comments",
+                value_type="JSON",
+                is_always_whole_row=True,
+                is_backend_available=True,
+                icon="comments"))
+
+            db_session.commit()
+    except Exception:
+        logger.exception('Failed creating default AxFieldTypes')
+        raise
+
+
+def database_fits_metadata() -> None:
+    """Compare metada and database"""
+    try:
+        with ax_model.engine.connect() as active_connection:
+            context = MigrationContext.configure(active_connection)
+            db_session = ax_model.Session()
+            ax_model.Base.query = db_session.query_property()
+            metadata = ax_model.Base.metadata
+            diff = compare_metadata(context, metadata)
+            return diff == []
+    except Exception:
+        logger.exception('Error in comparing metadata and database.')
+        raise
+
+
+def upgrade_database():
+    """Run Alembic migration script and update db version"""
+    try:
+        logger.info('Database does not fit metadata. Upgrading database.')
+        command.upgrade(this.alembic_cfg, "head")
+        command.stamp(this.alembic_cfg, "head")
+    except Exception:
+        logger.exception('Error upgrading database with Alembic')
+        raise
+
+
+def init_migration():
+    """Initiate migration module"""
+    init_alembic_config()
+    if tables_exist() is False:
+        create_tables()
+        create_field_types()
+    else:
+        pass
+        # if database_fits_metadata() is False:
+        #     upgrade_database()
---
+++ packages/ax/ax-0.34.0-py3-none-any.whl/ax/backend/schema.py
@@ -0,0 +1,311 @@
+"""Ax GraphQL schema
+1. Combines all mutations, queryes and subscribtions from schemas folder.
+    Creates static part of schema from them.
+2. Creates dynamic part of schema. For each AxGrid the dynamic schema is created
+    Schema contains all fields of form. Rows are filtered using AxGris server
+    filter. The name of schema is FormName+GridName. If grid is default, the
+    additional schema is created, named FormName.
+"""
+import sys
+import graphene
+from loguru import logger
+from sqlalchemy import literal
+from graphene_sqlalchemy import SQLAlchemyConnectionField
+from dotmap import DotMap
+import ujson as json
+
+from backend.schemas.users_schema import UsersQuery
+from backend.schemas.users_schema import UsersMutations, UsersSubscription
+from backend.schemas.home_schema import HomeQuery, HomeMutations
+from backend.schemas.form_schema import FormQuery, FormMutations
+from backend.schemas.workflow_schema import WorkflowMutations
+from backend.schemas.action_schema import ActionQuery, ActionMutations, \
+    ActionSubscription
+from backend.schemas.grids_schema import GridsQuery, GridsMutations
+from backend.schemas.fields_schema import FieldsQuery, FieldsMutations
+from backend.schemas.types import Form, FieldType, Field, Grid, \
+    Column, User, Role, State, Action, RoleFieldPermission, Group2Users, \
+    Action2Role, State2Role, Role2Users, PositionInput
+
+from backend.model import AxForm
+import backend.model as ax_model
+import backend.dialects as ax_dialects
+
+
+this = sys.modules[__name__]
+schema = None
+
+#  Dict used to determin what graphene type to use for AxFieldType.value_type
+type_dictionary = {
+    'text': graphene.String,
+    'TEXT': graphene.String,
+    'VARCHAR(255)': graphene.String,
+    'INT': graphene.Int,
+    'DECIMAL(65,2)': graphene.Float,
+    'BOOL': graphene.Boolean,
+    'GUID': graphene.String,
+    'JSON': graphene.String,
+    'TIMESTAMP': graphene.Int,
+    'BLOB': graphene.String
+}
+
+# Static GQL types. Are same as SqlAlchemy model.
+gql_types = [
+    Form,
+    FieldType,
+    Field,
+    Grid,
+    Column,
+    User,
+    Role,
+    State,
+    Action,
+    RoleFieldPermission,
+    Group2Users,
+    Action2Role,
+    State2Role,
+    Role2Users,
+    PositionInput,
+]
+
+
+class Query(HomeQuery, FormQuery, UsersQuery, GridsQuery,
+            ActionQuery, FieldsQuery, graphene.ObjectType):
+    """Combines all schemas queryes"""
+    forms = SQLAlchemyConnectionField(Form)
+    field_types = SQLAlchemyConnectionField(FieldType)
+    fields = SQLAlchemyConnectionField(Field)
+    grids = SQLAlchemyConnectionField(Grid)
+    columns = SQLAlchemyConnectionField(Column)
+    users = SQLAlchemyConnectionField(User)
+    roles = SQLAlchemyConnectionField(Role)
+    states = SQLAlchemyConnectionField(State)
+    actions = SQLAlchemyConnectionField(Action)
+    role_field_permissions = SQLAlchemyConnectionField(RoleFieldPermission)
+    group_to_users = SQLAlchemyConnectionField(Group2Users)
+
+
+class Mutations(HomeMutations, FormMutations, UsersMutations, GridsMutations,
+                WorkflowMutations, FieldsMutations, ActionMutations,
+                graphene.ObjectType):
+    """Combines all schemas mutations"""
+
+
+class Subscription(UsersSubscription, ActionSubscription, graphene.ObjectType):
+    """Combines all schemas subscription"""
+
+
+def exec_grid_code(form, grid, arguments=None):
+    """ Execute python code to get SQL query """
+    localz = dict()
+    ax = DotMap()  # javascript style dicts item['guid'] == item.guid
+    ax.row.guid = form.row_guid
+    ax.arguments = arguments
+    ax.form = form
+    ax.grid = grid
+    ax.sql = ax_dialects.dialect.custom_query
+    localz['ax'] = ax
+
+    try:
+        exec(str(grid.code), globals(), localz)   # pylint: disable=exec-used
+        ret_query = None
+        ret_ax = localz['ax']
+        if ret_ax and ret_ax.query:
+            ret_query = str(ret_ax.query)
+        return ret_query
+    except SyntaxError as err:
+        logger.exception(
+            f'Error in query for [grid.name] - SyntaxError - {err}')
+        return None
+    except Exception as err:    # pylint: disable=broad-except
+        logger.exception(
+            f'Error in query for [grid.name] - SyntaxError - {err}')
+        return None
+
+
+def make_resolver(db_name, type_class):
+    """ Dynamicly create resolver for GrapQL query based on
+        db_name - db_name of AxForm + db_name of AxGrid.
+        or only db_name of AxForm for default view grid
+
+        Example:
+        db_name=MyUberFormGridOne
+        MyUberForm is form name
+        GridOne is grid name
+
+    """
+
+    async def resolver(
+            self, info, arguments=None, update_time=None, quicksearch=None,
+            guids=None):
+        del update_time, self
+        err = f"Schema -> Resolver for {db_name}"
+        with ax_model.try_catch(info.context['session'], err) as db_session:
+            # Find AxForm with name that db_name is started with
+            ax_form = None
+            found_forms = db_session.query(AxForm).filter(
+                literal(db_name).startswith(AxForm.db_name)
+            ).all()
+
+            ax_grid = {}
+            default_grid = {}
+
+            for form in found_forms:
+                # iterate all grids to know, what grid to use
+                for grid in form.grids:
+                    if form.db_name + grid.db_name == db_name:
+                        ax_grid = grid
+                        ax_form = form
+                    if grid.is_default_view and form.db_name == db_name:
+                        default_grid = grid
+                        ax_form = form
+
+            grid_to_use = ax_grid or default_grid
+            if not grid_to_use:
+                return None
+
+            grid_options = json.loads(grid_to_use.options_json)
+
+            # TODO add permission checks
+            allowed_fields = []
+            for field in ax_form.db_fields:
+                allowed_fields.append(field.db_name)
+
+            results = None
+            # select * from table
+            # TODO Add paging
+            if quicksearch or guids:
+                results = await ax_dialects.dialect.select_all(
+                    db_session=db_session,
+                    ax_form=ax_form,
+                    quicksearch=quicksearch,
+                    guids=guids)
+            else:
+                arguments_dict = None
+                if arguments:
+                    try:
+                        arguments_dict = json.loads(arguments)
+                    except Exception as err:
+                        logger.exception(
+                            f'Error reading arguments for grid query - {err}')
+                        raise
+
+                sql = exec_grid_code(
+                    form=ax_form, grid=grid_to_use, arguments=arguments_dict)
+
+                results = await ax_dialects.dialect.select_custom_query(
+                    db_session=db_session,
+                    ax_form=ax_form,
+                    sql=sql,
+                    arguments=arguments_dict)
+
+            if not results:
+                return None
+
+            result_items = []
+            for row in results:
+                kwargs = {}
+                for key, value in row.items():
+                    kwargs[key] = value
+
+                result_items.append(type_class(**kwargs))
+
+            return result_items
+
+    resolver.__name__ = 'resolve_%s' % db_name
+    return resolver
+
+
+def init_schema(db_session):
+    """Initiate GQL schema. Create dynamic part of schema and combine it with
+    static part take from schemas folder"""
+    error_msg = "Schema -> init_schema. Error initiating GraphQL shcema."
+    with ax_model.try_catch(db_session, error_msg) as db_session:
+        # Create typeClass based on each AxForm
+        this.schema = None
+        type_classes = {}
+        all_types = gql_types.copy()  # Add dynamic types to GQL schema
+
+        ax_forms = db_session.query(AxForm).all()
+        for form in ax_forms:
+            for grid in form.grids:
+                # For each grid we create Graphene field with name FormGrid
+                capital_form_db_name = form.db_name[0].upper(
+                ) + form.db_name[1:]
+                class_name = capital_form_db_name + grid.db_name
+                class_fields = {}
+                class_fields['guid'] = graphene.String()
+                class_fields['axNum'] = graphene.Int()
+                class_fields['axState'] = graphene.String()
+                class_fields['axLabel'] = graphene.String()
+                class_fields['axIcon'] = graphene.String()
+
+                # Add fields for each field of AxForm
+                for field in form.db_fields:
+                    field_type = (
+                        type_dictionary[field.field_type.value_type])
+                    # TODO maybe add label as description?
+                    class_fields[field.db_name] = field_type()
+
+                # Create graphene class and append class dict
+                graph_class = type(
+                    class_name,
+                    (graphene.ObjectType,),
+                    class_fields,
+                    name=class_name,
+                    description=form.name
+                )
+                type_classes[class_name] = graph_class
+                all_types.append(graph_class)
+
+                # if grid is default view we add enother class with name
+                # Form without Grid name
+                if grid.is_default_view is True:
+                    default_class_name = form.db_name[0].upper(
+                    ) + form.db_name[1:]
+
+                    default_graph_class = type(
+                        default_class_name,
+                        (graphene.ObjectType,),
+                        class_fields,
+                        name=default_class_name,
+                        description=form.name
+                    )
+                    type_classes[default_class_name] = graph_class
+                    all_types.append(default_graph_class)
+
+        # Dynamicly crate resolvers for each typeClass
+        # Iterate throw created classes and create resolver for each
+        dynamic_fields = {}
+        for key, type_class in type_classes.items():
+            dynamic_fields[key] = graphene.List(
+                type_class,
+                update_time=graphene.Argument(
+                    type=graphene.String, required=False),
+                quicksearch=graphene.Argument(
+                    type=graphene.String, required=False),
+                guids=graphene.Argument(
+                    type=graphene.String, required=False),
+                arguments=graphene.Argument(
+                    type=graphene.String, required=False)
+            )
+
+            dynamic_fields['resolve_%s' % key] = make_resolver(
+                key, type_class)
+
+        # Combine static schema query and dynamic query
+        DynamicQuery = type('DynamicQuery', (Query,), dynamic_fields)
+
+        this.schema = graphene.Schema(
+            query=DynamicQuery,
+            mutation=Mutations,
+            types=all_types,
+            subscription=Subscription
+        )
+
+
+def init_schema_standalone():
+    """ Initiate GQL schema without db_session """
+    error_msg = "Schema -> init_schema. Error initiating GraphQL shcema."
+    with ax_model.scoped_session(error_msg) as db_session:
+        init_schema(db_session)
