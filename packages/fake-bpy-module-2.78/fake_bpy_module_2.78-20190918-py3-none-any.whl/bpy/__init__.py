a141a91d50ce06eb7818bdda965efb495e7f7749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:46 +0000

    Add package fake-bpy-module-2.78/20190918/fake_bpy_module_2.78-20190918-py3-none-any.whl

---
+++ packages/fake-bpy-module-2.78/fake_bpy_module_2.78-20190918-py3-none-any.whl/bpy/__init__.py
@@ -0,0 +1,14 @@
+import sys
+import typing
+from . import types
+from . import ops
+from . import path
+from . import context
+from . import utils
+from . import app
+from . import props
+
+context: 'types.Context' = None
+
+data: 'types.BlendData' = None
+'''Access to Blenders internal data '''
