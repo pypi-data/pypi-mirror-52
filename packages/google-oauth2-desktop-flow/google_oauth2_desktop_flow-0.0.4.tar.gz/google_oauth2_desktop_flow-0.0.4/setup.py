0948103c74c8eaccf8ee01fa4fe54876dcb07c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:05 +0000

    Add package google-oauth2-desktop-flow/0.0.4/google_oauth2_desktop_flow-0.0.4.tar.gz

---
+++ packages/google-oauth2-desktop-flow/google_oauth2_desktop_flow-0.0.4.tar.gz/google_oauth2_desktop_flow-0.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='google_oauth2_desktop_flow',
+    version='0.0.4',
+    author='Paulo Rosário',
+    author_email='paulo.filip3@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'flask',
+        'google-auth-oauthlib',
+        'click'
+    ],
+    entry_points={
+        'console_scripts': [
+            'google-oauth-tokens = google_oauth2_desktop_flow.cli:main'
+        ]
+    }
+)
