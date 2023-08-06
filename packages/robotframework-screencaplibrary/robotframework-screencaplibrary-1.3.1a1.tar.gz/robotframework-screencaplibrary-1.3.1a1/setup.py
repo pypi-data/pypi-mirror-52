a5d8de78a1cb1bc7e0bfbd2194ed84d39c452036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:03 +0000

    Add package robotframework-screencaplibrary/1.3.1a1/robotframework-screencaplibrary-1.3.1a1.tar.gz

---
+++ packages/robotframework-screencaplibrary/robotframework-screencaplibrary-1.3.1a1.tar.gz/robotframework-screencaplibrary-1.3.1a1/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import re
+import sys
+from os.path import abspath, dirname, join
+from setuptools import setup
+
+CURDIR = dirname(abspath(__file__))
+REQUIREMENTS = ['robotframework >= 3.0', 'mss >= 3.2.1', 'pillow >= 5.2.0', 'opencv-python >= 4.0.0.21']
+with open(join(CURDIR, 'src', 'ScreenCapLibrary', 'version.py')) as f:
+    VERSION = re.search("\nVERSION = '(.*)'", f.read()).group(1)
+with open(join(CURDIR, 'README.rst')) as f:
+    DESCRIPTION = f.read()
+if sys.version_info[0] < 3:
+    REQUIREMENTS.append('futures >= 3.2.0')
+if sys.version_info[0] > 3:
+    REQUIREMENTS.append('futures3 >= 1.0.0')
+CLASSIFIERS = '''
+Development Status :: 5 - Production/Stable
+License :: OSI Approved :: Apache Software License
+Operating System :: OS Independent
+Programming Language :: Python
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Topic :: Software Development :: Testing
+Framework :: Robot Framework
+Framework :: Robot Framework :: Library
+'''.strip().splitlines()
+
+setup(
+    name='robotframework-screencaplibrary',
+    version=VERSION,
+    description='Robot Framework test library for taking screenshots',
+    long_description=DESCRIPTION,
+    author='Robot Framework Developers',
+    author_email='robotframework@gmail.com',
+    url='https://github.com/robotframework/ScreenCapLibrary',
+    license='Apache License 2.0',
+    keywords='robotframework testing testautomation screenshot screencap',
+    platforms='any',
+    classifiers=CLASSIFIERS,
+    install_requires=REQUIREMENTS,
+    package_dir={'': 'src'},
+    packages=['ScreenCapLibrary']
+)
