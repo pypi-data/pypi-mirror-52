e744c4757744f66c9e2b297380f573510d13be86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:24 +0000

    Add package blackfynn-collection-downloader/0.0.1/blackfynn-collection-downloader-0.0.1.tar.gz

---
+++ packages/blackfynn-collection-downloader/blackfynn-collection-downloader-0.0.1.tar.gz/blackfynn-collection-downloader-0.0.1/blackfynn-collection-downloader/__init__.py
@@ -0,0 +1,6 @@
+from blackfynn_collection_downloader.ui import DetailsInput
+from blackfynn_collection_downloader.config import API_TOKEN, API_SECRET
+from blackfynn_collection_downloader.main import *
+
+
+
