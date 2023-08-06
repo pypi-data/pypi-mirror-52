79c16f82e3d8de5fdd8141898b986f9fd57294f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:37 +0000

    Add package pybandwidth-v2/0.0.3/pybandwidth_v2-0.0.3.tar.gz

---
+++ packages/pybandwidth-v2/pybandwidth_v2-0.0.3.tar.gz/pybandwidth_v2-0.0.3/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+  name='pybandwidth_v2',
+  packages=setuptools.find_packages(),
+  version='0.0.3',
+  license='MIT',
+  description='Bandwidth API for v2 messaging',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author='Reef Technologies',
+  author_email='anuj.agarwal@reef.pl',
+  url='https://github.com/reef-technologies/pybandwidth_v2',
+  download_url='https://github.com/reef-technologies/pybandwidth_v2/archive/master.zip',
+  keywords=['python', 'bandwidth', 'v2', 'api', 'client'],   # Keywords that define your package best
+  install_requires=[
+      'requests',
+  ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of package
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+  python_requires='>=3.6',
+  project_urls={
+    'Source': 'https://github.com/reef-technologies/pybandwidth_v2',
+    'Tracker': 'https://github.com/reef-technologies/pybandwidth_v2/issues',
+  },
+)
