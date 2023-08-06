c93dd305390f9bdee5c7ec24bce8678b122b5a7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:33 +0000

    Add package mlops-propulsion-academy/1.1/mlops_propulsion_academy-1.1-py3-none-any.whl

---
+++ packages/mlops-propulsion-academy/mlops_propulsion_academy-1.1-py3-none-any.whl/mlops_propulsion_academy-1.1.data/scripts/update.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+ssh-keyscan gitlab.propulsion-home.ch >>/root/.ssh/known_hosts
+
+FILE=/mlops
+if [ -f "$FILE" ]
+then
+    cd /mlops
+    git pull
+else
+    mkdir -p /mlops
+    git clone git@gitlab.propulsion-home.ch:DOPE/mlops-cluster.git .
+fi
+
+
+# Build conda env
+conda deactivate
+cd /mlops/backend
+
+FILE=/root/anaconda3/envs/mlops
+if [ -f "$FILE" ]
+then
+    conda env update -f ./requirements.yml
+else
+    conda env create -f ./requirements.yml
+fi
+
+
+conda activate mlops
+
+
+
+
+
