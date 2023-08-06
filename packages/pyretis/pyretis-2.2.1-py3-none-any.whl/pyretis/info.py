73d4e80989ae31081df100d7b1ed49b59619bd15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:47 +0000

    Add package pyretis/2.2.1/pyretis-2.2.1-py3-none-any.whl

---
+++ packages/pyretis/pyretis-2.2.1-py3-none-any.whl/pyretis/info.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, PyRETIS Development Team.
+# Distributed under the LGPLv2.1+ License. See LICENSE for more info.
+"""This module just contains some info for PyRETIS.
+
+Here we define the name of the program and some other
+relevant info.
+"""
+PROGRAM_NAME = 'PyRETIS'
+URL = 'http://www.pyretis.org'
+GIT_URL = 'https://gitlab.com/pyretis/pyretis'
+CITE = """
+[1] A. Lervik, E. Riccardi and T. S. van Erp, J. Comput. Chem., 2017
+    doi: http://dx.doi.org/10.1002/jcc.24900
+"""
+LOGO = r"""
+  _______                                     ___________
+ /   ___ \         ___  ____  ____________   / __     __/
+/_/\ \_/ /_  __   / _ \/ __/ /_  _/ / ___/  /_/ / // /
+   /  __  / / /  / /_)  _/    / // /\ \        / // /
+  / /   \ \/ /  / _  \ /__   / // /__\ \    __/ // /__
+ /_/    _\  /  /_/ |_|___/  /_//_/_____/   /_________/
+       /___/
+"""
