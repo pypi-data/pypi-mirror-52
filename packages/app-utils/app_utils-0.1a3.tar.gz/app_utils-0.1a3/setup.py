01f17edbbcdc20196511f75902d510b417f0f26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:56 +0000

    Add package app-utils/0.1a3/app_utils-0.1a3.tar.gz

---
+++ packages/app-utils/app_utils-0.1a3.tar.gz/app_utils-0.1a3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="app_utils",
+    version="0.1a3",
+    author="Patrick Shinn",
+    author_email="shinn5112@gmail.com",
+    description="A collection of reusable utility functions to aid in rapid application development.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://gitlab.com/shinn5112/app_utils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 1 - Planning",
+    ],
+    python_requires='>=3.5',
+)
