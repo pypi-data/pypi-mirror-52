e876270fe16ac29348c5f2ef99afdde52a9c7efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:34 +0000

    Add package database-manager/0.0.1/database-manager-0.0.1.tar.gz

---
+++ packages/database-manager/database-manager-0.0.1.tar.gz/database-manager-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="database-manager",
+    version="0.0.1",
+    author="Shane Byers",
+    author_email="shane@shane-byers.com",
+    description="Database Manager for MySQL",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ShaneBByers/DatabaseManager",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
