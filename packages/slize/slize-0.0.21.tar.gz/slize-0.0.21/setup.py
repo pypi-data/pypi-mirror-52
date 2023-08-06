abf98cb0867624219204eaba6845fe8bea1752f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:45 +0000

    Add package slize/0.0.21/slize-0.0.21.tar.gz

---
+++ packages/slize/slize-0.0.21.tar.gz/slize-0.0.21/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="slize",
+    version="0.0.21",
+    author="Zeio Nara",
+    author_email="zeionara@gmail.com",
+    description="Reusable Intent and Slot-filling tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zeionara/slize",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
