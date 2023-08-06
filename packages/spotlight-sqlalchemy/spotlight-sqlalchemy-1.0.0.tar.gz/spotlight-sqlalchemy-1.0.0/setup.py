3e33259f8719cd726dcc299e71a9d8fa00e4f954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:29 +0000

    Add package spotlight-sqlalchemy/1.0.0/spotlight-sqlalchemy-1.0.0.tar.gz

---
+++ packages/spotlight-sqlalchemy/spotlight-sqlalchemy-1.0.0.tar.gz/spotlight-sqlalchemy-1.0.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+from collections import OrderedDict
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="spotlight-sqlalchemy",
+    version="1.0.0",
+    author="Michiel Doesburg",
+    author_email="michiel@moddix.com",
+    description="SQLAlchemy plugin for Spotlight.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="spotlight sqlalchemy validation validate",
+    url="https://github.com/mdoesburg/spotlight-sqlalchemy",
+    project_urls=OrderedDict(
+        (
+            ("Documentation", "https://github.com/mdoesburg/spotlight-sqlalchemy"),
+            ("Code", "https://github.com/mdoesburg/spotlight-sqlalchemy"),
+        )
+    ),
+    license="MIT",
+    packages=setuptools.find_packages(),
+    install_requires=["spotlight"],
+)
