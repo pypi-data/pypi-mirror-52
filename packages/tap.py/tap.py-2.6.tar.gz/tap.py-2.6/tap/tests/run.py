c27470beaa2a514bf28305ae6554cd22d0be22ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:55 +0000

    Add package tap.py/2.6/tap.py-2.6.tar.gz

---
+++ packages/tap.py/tap.py-2.6.tar.gz/tap.py-2.6/tap/tests/run.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2019, Matt Layman and contributors
+
+import os
+import unittest
+
+from tap import TAPTestRunner
+
+if __name__ == "__main__":
+    tests_dir = os.path.dirname(os.path.abspath(__file__))
+    loader = unittest.TestLoader()
+    tests = loader.discover(tests_dir)
+    runner = TAPTestRunner()
+    runner.set_outdir("testout")
+    runner.set_format("Hi: {method_name} - {short_description}")
+    runner.run(tests)
