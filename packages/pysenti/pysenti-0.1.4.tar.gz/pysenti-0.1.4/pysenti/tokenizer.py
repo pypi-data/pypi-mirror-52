ff917cc0e148e9f2cc0971e93f3cb8db406b7fe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:58 +0000

    Add package pysenti/0.1.4/pysenti-0.1.4.tar.gz

---
+++ packages/pysenti/pysenti-0.1.4.tar.gz/pysenti-0.1.4/pysenti/tokenizer.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing（xuming624@qq.com)
+@description: 配置切词器
+"""
+import logging
+
+import jieba
+from jieba import posseg
+
+from .compat import strdecode
+
+jieba.default_logger.setLevel(logging.ERROR)
+
+
+def segment(sentence, cut_type='word', pos=False):
+    """
+    切词
+    :param sentence:
+    :param cut_type: 'word' use jieba.lcut; 'char' use list(sentence)
+    :param pos: enable POS
+    :return: list
+    """
+    sentence = strdecode(sentence)
+    if pos:
+        if cut_type == 'word':
+            return posseg.lcut(sentence)
+        elif cut_type == 'char':
+            word_seq = list(sentence)
+            pos_seq = []
+            for w in word_seq:
+                w_p = posseg.lcut(w)
+                pos_seq.append(w_p[0].flag)
+            return word_seq, pos_seq
+    else:
+        if cut_type == 'word':
+            return jieba.lcut(sentence)
+        elif cut_type == 'char':
+            return list(sentence)
---
+++ packages/pysenti/pysenti-0.1.4.tar.gz/pysenti-0.1.4/pysenti/utils.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing（xuming624@qq.com)
+@description: 
+"""
+import os
+import pickle
+import re
+from codecs import open
+
+from pysenti import config
+
+
+def load_set(path):
+    words = set()
+    with open(path, 'r', 'utf-8') as f:
+        for line in f:
+            words.add(line.strip())
+    return words
+
+
+re_zh = re.compile('([\u4E00-\u9FA5]+)')
+
+stopwords = load_set(config.stopwords_path)
+
+
+def filter_stop(words):
+    return list(filter(lambda x: x not in stopwords, words))
+
+
+def load_pkl(pkl_path):
+    """
+    加载词典文件
+    :param pkl_path:
+    :return:
+    """
+    with open(pkl_path, 'rb') as f:
+        result = pickle.load(f)
+    return result
+
+
+def dump_pkl(vocab, pkl_path, overwrite=True):
+    """
+    存储文件
+    :param pkl_path:
+    :param overwrite:
+    :return:
+    """
+    if pkl_path and os.path.exists(pkl_path) and not overwrite:
+        return
+    if pkl_path:
+        with open(pkl_path, 'wb') as f:
+            pickle.dump(vocab, f, protocol=0)
+        print("save %s ok." % pkl_path)
+    else:
+        raise IOError("no file: %s" % pkl_path)
+
+
+def split_sentence(sentence):
+    pattern = re.compile(u"[，。%、！!？?,；～~.… ]+")
+    clauses = [i for i in pattern.split(sentence.strip()) if i]
+    return clauses
+
+
+if __name__ == '__main__':
+    sent = "nihao,我是警察，你站起来。我要问你话！好不。"
+    k = split_sentence(sent)
+    print(k)
