4534726f4949e9ccb37e0c7d33ba1af4389ab702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:32 +0000

    Add package powershell_kernel/0.1.0/powershell_kernel-0.1.0.tar.gz

---
+++ packages/powershell_kernel/powershell_kernel-0.1.0.tar.gz/powershell_kernel-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'powershell_kernel',
+  packages = ['powershell_kernel'],
+  version = '0.1.0',
+  description = 'PowerShell language kernel for Jupyter',
+  author = 'Sergei Vorobev',
+  author_email = 'xvorsx@gmail.com',
+  url = 'https://github.com/vors/jupyter-powershell',
+  download_url = 'https://github.com/vors/jupyter-powershell/archive/0.1.0.tar.gz',
+  keywords = ['ikernel', 'jupyter', 'powershell', 'pwsh'],
+  classifiers = [],
+)
