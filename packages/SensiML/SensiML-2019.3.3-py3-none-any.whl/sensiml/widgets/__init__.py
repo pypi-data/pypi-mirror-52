1d37936c9ae3cac998c76fa476ba6714c90b4682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:28 +0000

    Add package SensiML/2019.3.3/SensiML-2019.3.3-py3-none-any.whl

---
+++ packages/SensiML/SensiML-2019.3.3-py3-none-any.whl/sensiml/widgets/__init__.py
@@ -0,0 +1,13 @@
+from sensiml.widgets.auto_sense import AutoSenseWidget
+from sensiml.widgets.download_widget import DownloadWidget
+from sensiml.widgets.query_widget import QueryWidget
+from sensiml.widgets.flash_widget import FlashWidget
+from sensiml.widgets.model_visualization_widget import ModelVisualizationWidget
+from sensiml.widgets.model_explore_widget import ModelExploreWidget
+from sensiml.widgets.dashboard_widget import DashBoard
+from sensiml.widgets.pipeline_builder_widget import PipelineBuilderWidget
+
+
+__all__ = ['AutoSenseWidget', 'DownloadWidget', 'QueryWidget', 'FlashWidget',
+           "ModelVisualizationWidget", "DashBoard", "ModelExploreWidget", "PipelineBuilderWidget",
+           ]
