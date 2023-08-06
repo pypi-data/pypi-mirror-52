c5105dbff766f268f28bb7adb9db59aa8bd4eade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:25 +0000

    Add package mlkatas/0.0.5/mlkatas-0.0.5-py3-none-any.whl

---
+++ packages/mlkatas/mlkatas-0.0.5-py3-none-any.whl/mlkatas/__init__.py
@@ -0,0 +1,15 @@
+"""Utility functions for Machine Learning"""
+
+from .plot import (
+    plot_planar_data,
+    plot_decision_boundary,
+    plot_multiclass,
+    plot_multiclass_decision_boundary,
+    plot_loss_acc,
+    plot_loss_mae
+)
+
+from .utils import (
+    make_multiclass,
+    vectorize_sequences
+)
