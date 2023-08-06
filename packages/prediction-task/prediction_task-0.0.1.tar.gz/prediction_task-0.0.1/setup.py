ff7094d1a4987469f6ad5db82f8bba01cc87b696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:05 +0000

    Add package prediction-task/0.0.1/prediction_task-0.0.1.tar.gz

---
+++ packages/prediction-task/prediction_task-0.0.1.tar.gz/prediction_task-0.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+  
+setuptools.setup(
+    #Here is the module name.
+    name="prediction_task",
+ 
+    #version of the module
+    version="0.0.1",
+ 
+    #Name of Author
+    author="Santhosh R",
+ 
+    #your Email address
+    author_email="santhoshgowda.ec@gmail.com",
+ 
+    #Small Description about module
+    description="Web scrapping for weather.com",
+
+ 
+    #Any link to reach this module, if you have any webpage or github profile
+    url="https://github.com/Santhu195/",
+    packages=setuptools.find_packages() ,
+    #classifiers like program is suitable for python3, just leave as it is.
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
