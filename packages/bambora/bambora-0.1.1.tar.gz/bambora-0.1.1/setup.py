6f9b0b2dce410dfda047e6e00b2bfbf60470c067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:19 +0000

    Add package bambora/0.1.1/bambora-0.1.1.tar.gz

---
+++ packages/bambora/bambora-0.1.1.tar.gz/bambora-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+with open('requirements.txt') as fin:
+    required = fin.read().splitlines()
+
+setup(
+    name="bambora",
+    version="0.1.1",
+    description="Bambora Client API",
+    license="MIT",
+    author="Loïc Faure-Lacroix",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=required,
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
