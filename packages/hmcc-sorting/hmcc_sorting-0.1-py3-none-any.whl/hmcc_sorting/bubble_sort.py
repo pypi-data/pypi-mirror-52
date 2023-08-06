54b4d4473b32d0e09ebe414c192f4c37fb52a868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:30 +0000

    Add package hmcc-sorting/0.1/hmcc_sorting-0.1-py3-none-any.whl

---
+++ packages/hmcc-sorting/hmcc_sorting-0.1-py3-none-any.whl/hmcc_sorting/bubble_sort.py
@@ -0,0 +1,9 @@
+def bubble_sort(alist):
+    for passnum in range(len(alist) - 1, 0, -1):
+        for i in range(passnum):
+            if alist[i] > alist[i + 1]:
+                temp = alist[i]
+                alist[i] = alist[i + 1]
+                alist[i + 1] = temp
+    return alist
+
