3eadf8be0c86631c71115b6f941622f032fb3640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:22 +0000

    Add package openImageDatasetSDK/1.0.4/openImageDatasetSDK-1.0.4-py3-none-any.whl

---
+++ packages/openImageDatasetSDK/openImageDatasetSDK-1.0.4-py3-none-any.whl/openimagedataset/_utils/__init__.py
@@ -0,0 +1,5 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+"""Init file for openimagedataset_interface/_utils."""
