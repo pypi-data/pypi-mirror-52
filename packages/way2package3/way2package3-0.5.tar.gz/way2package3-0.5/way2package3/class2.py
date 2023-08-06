c27e51a1a2b1b83cf44fd532947b89cc3f67c0d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:06 +0000

    Add package way2package3/0.5/way2package3-0.5.tar.gz

---
+++ packages/way2package3/way2package3-0.5.tar.gz/way2package3-0.5/way2package3/class2.py
@@ -0,0 +1,15 @@
+from way2package3.colors import colors
+class Myclass:
+    def __init__(self,username):
+        if(not isinstance(username,str)): return "Sorry!..username must be string."
+        self.name = username
+    def sayHai(self,color):
+        print(colors.fg.blue, colors.bold,colors.underline,"woww asdf Amartya",colors.reset)
+        print("\033[96m Hai '{}'.\033[00m".format(self.name))
+        return "Hai '{}'.".format(self.name)
+    def sayBye(self):
+        print("Bye..miss you '{}'".format(self.name))
+        return "Bye..miss you '{}'".format(self.name)
+
+
+# Myclass("eee").sayHai("red")
