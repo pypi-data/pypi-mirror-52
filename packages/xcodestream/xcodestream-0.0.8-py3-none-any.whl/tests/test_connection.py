6589c63918a13f9a2706bc8a54e1b5b6b2b15886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:57 +0000

    Add package xcodestream/0.0.8/xcodestream-0.0.8-py3-none-any.whl

---
+++ packages/xcodestream/xcodestream-0.0.8-py3-none-any.whl/tests/test_connection.py
@@ -0,0 +1,39 @@
+import unittest
+
+from xcodestream.connection import Connection
+
+
+class TestConnection(unittest.TestCase):
+    def test_matched_url(self):
+        url = "https://test.com:5609?username=test&password=pass"
+        matched, conn = Connection.from_url(url)
+        self.assertTrue(matched)
+        self.assertTrue(isinstance(conn, Connection))
+        self.assertEqual(str(conn), url)
+
+    def test_matched_url_without_port(self):
+        url = "http://test.com?username=test&password=pass"
+        matched, conn = Connection.from_url(url)
+        self.assertTrue(matched)
+        self.assertTrue(isinstance(conn, Connection))
+        self.assertEqual(str(conn), url)
+
+    def test_not_matched_username_url(self):
+        url = "https://test.com:5609?usename=test&password=pass"
+        matched, conn = Connection.from_url(url)
+        self.assertFalse(matched)
+
+    # def test_parse(self):
+    #     self.assertTrue('FOO'.isupper())
+    #     self.assertFalse('Foo'.isupper())
+
+    # def test_split(self):
+    #     s = 'hello world'
+    #     self.assertEqual(s.split(), ['hello', 'world'])
+    #     # check that s.split fails when the separator is not a string
+    #     with self.assertRaises(TypeError):
+    #         s.split(2)
+
+
+if __name__ == "__main__":
+    unittest.main()
