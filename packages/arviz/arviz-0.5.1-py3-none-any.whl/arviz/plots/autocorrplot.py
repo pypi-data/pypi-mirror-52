b19dbf4544a5a5afad6d7a5a3835c0ebbc590a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:26 +0000

    Add package arviz/0.5.1/arviz-0.5.1-py3-none-any.whl

---
+++ packages/arviz/arviz-0.5.1-py3-none-any.whl/arviz/plots/autocorrplot.py
@@ -0,0 +1,126 @@
+"""Autocorrelation plot of data."""
+import numpy as np
+
+from ..data import convert_to_dataset
+from ..stats import autocorr
+from .plot_utils import (
+    _scale_fig_size,
+    default_grid,
+    make_label,
+    xarray_var_iter,
+    _create_axes_grid,
+    filter_plotters_list,
+)
+from ..utils import _var_names
+
+
+def plot_autocorr(
+    data, var_names=None, max_lag=None, combined=False, figsize=None, textsize=None, ax=None
+):
+    """Bar plot of the autocorrelation function for a sequence of data.
+
+    Useful in particular for posteriors from MCMC samples which may display correlation.
+
+    Parameters
+    ----------
+    data : obj
+        Any object that can be converted to an az.InferenceData object
+        Refer to documentation of az.convert_to_dataset for details
+    var_names : list of variable names, optional
+        Variables to be plotted, if None all variable are plotted.
+        Vector-value stochastics are handled automatically.
+    max_lag : int, optional
+        Maximum lag to calculate autocorrelation. Defaults to 100 or num draws, whichever is smaller
+    combined : bool
+        Flag for combining multiple chains into a single chain. If False (default), chains will be
+        plotted separately.
+    figsize : tuple
+        Figure size. If None it will be defined automatically.
+        Note this is not used if ax is supplied.
+    textsize: float
+        Text size scaling factor for labels, titles and lines. If None it will be autoscaled based
+        on figsize.
+    ax: axes
+        Matplotlib axes
+
+    Returns
+    -------
+    axes : matplotlib axes
+
+    Examples
+    --------
+    Plot default autocorrelation
+
+    .. plot::
+        :context: close-figs
+
+        >>> import arviz as az
+        >>> data = az.load_arviz_data('centered_eight')
+        >>> az.plot_autocorr(data)
+
+    Plot subset variables by specifying variable name exactly
+
+    .. plot::
+        :context: close-figs
+
+        >>> az.plot_autocorr(data, var_names=['mu', 'tau'] )
+
+
+    Combine chains collapsing by variable
+
+    .. plot::
+        :context: close-figs
+
+        >>> az.plot_autocorr(data, var_names=['mu', 'tau'], combined=True)
+
+
+    Specify maximum lag (x axis bound)
+
+    .. plot::
+        :context: close-figs
+
+        >>> az.plot_autocorr(data, var_names=['mu', 'tau'], max_lag=200, combined=True)
+    """
+    data = convert_to_dataset(data, group="posterior")
+    var_names = _var_names(var_names, data)
+
+    # Default max lag to 100 or max length of chain
+    if max_lag is None:
+        max_lag = min(100, data["draw"].shape[0])
+
+    plotters = filter_plotters_list(
+        list(xarray_var_iter(data, var_names, combined)), "plot_autocorr"
+    )
+    length_plotters = len(plotters)
+    rows, cols = default_grid(length_plotters)
+
+    figsize, _, titlesize, xt_labelsize, linewidth, _ = _scale_fig_size(
+        figsize, textsize, rows, cols
+    )
+
+    if ax is None:
+        _, axes = _create_axes_grid(
+            length_plotters, rows, cols, figsize=figsize, squeeze=False, sharex=True, sharey=True
+        )
+    else:
+        axes = ax
+
+    axes = np.atleast_2d(axes)  # in case of only 1 plot
+    for (var_name, selection, x), ax_ in zip(plotters, axes.flatten()):
+        x_prime = x
+
+        if combined:
+            x_prime = x.flatten()
+
+        y = autocorr(x_prime)
+
+        ax_.vlines(x=np.arange(0, max_lag), ymin=0, ymax=y[0:max_lag], lw=linewidth)
+        ax_.hlines(0, 0, max_lag, "steelblue")
+        ax_.set_title(make_label(var_name, selection), fontsize=titlesize, wrap=True)
+        ax_.tick_params(labelsize=xt_labelsize)
+
+    if axes.size > 0:
+        axes[0, 0].set_xlim(0, max_lag)
+        axes[0, 0].set_ylim(-1, 1)
+
+    return axes
---
+++ packages/arviz/arviz-0.5.1-py3-none-any.whl/arviz/plots/parallelplot.py
@@ -0,0 +1,139 @@
+"""Parallel coordinates plot showing posterior points with and without divergences marked."""
+import matplotlib.pyplot as plt
+import numpy as np
+
+from scipy.stats.mstats import rankdata
+from ..data import convert_to_dataset
+from .plot_utils import _scale_fig_size, xarray_to_ndarray, get_coords
+from ..utils import _var_names, _numba_var
+from ..stats.stats_utils import stats_variance_2d as svar
+
+
+def plot_parallel(
+    data,
+    var_names=None,
+    coords=None,
+    figsize=None,
+    textsize=None,
+    legend=True,
+    colornd="k",
+    colord="C1",
+    shadend=0.025,
+    ax=None,
+    norm_method=None,
+):
+    """
+    Plot parallel coordinates plot showing posterior points with and without divergences.
+
+    Described by https://arxiv.org/abs/1709.01449, suggested by Ari Hartikainen
+
+    Parameters
+    ----------
+    data : obj
+        Any object that can be converted to an az.InferenceData object
+        Refer to documentation of az.convert_to_dataset for details
+    var_names : list of variable names
+        Variables to be plotted, if None all variable are plotted. Can be used to change the order
+        of the plotted variables
+    coords : mapping, optional
+        Coordinates of var_names to be plotted. Passed to `Dataset.sel`
+    figsize : tuple
+        Figure size. If None it will be defined automatically.
+    textsize: float
+        Text size scaling factor for labels, titles and lines. If None it will be autoscaled based
+        on figsize.
+    legend : bool
+        Flag for plotting legend (defaults to True)
+    colornd : valid matplotlib color
+        color for non-divergent points. Defaults to 'k'
+    colord : valid matplotlib color
+        color for divergent points. Defaults to 'C1'
+    shadend : float
+        Alpha blending value for non-divergent points, between 0 (invisible) and 1 (opaque).
+        Defaults to .025
+    ax : axes
+        Matplotlib axes.
+    norm_method : str
+        Method for normalizing the data. Methods include normal, minmax and rank.
+        Defaults to none.
+
+    Returns
+    -------
+    ax : matplotlib axes
+
+    Examples
+    --------
+    Plot default parallel plot
+
+    .. plot::
+        :context: close-figs
+
+        >>> import arviz as az
+        >>> data = az.load_arviz_data('centered_eight')
+        >>> az.plot_parallel(data, var_names=["mu", "tau"])
+
+
+    Plot parallel plot with normalization
+
+    .. plot::
+        :context: close-figs
+
+        >>> az.plot_parallel(data, var_names=["mu", "tau"], norm_method='normal')
+
+    """
+    if coords is None:
+        coords = {}
+
+    # Get diverging draws and combine chains
+    divergent_data = convert_to_dataset(data, group="sample_stats")
+    _, diverging_mask = xarray_to_ndarray(divergent_data, var_names=("diverging",), combined=True)
+    diverging_mask = np.squeeze(diverging_mask)
+
+    # Get posterior draws and combine chains
+    posterior_data = convert_to_dataset(data, group="posterior")
+    var_names = _var_names(var_names, posterior_data)
+    var_names, _posterior = xarray_to_ndarray(
+        get_coords(posterior_data, coords), var_names=var_names, combined=True
+    )
+    if len(var_names) < 2:
+        raise ValueError("This plot needs at least two variables")
+    if norm_method is not None:
+        if norm_method == "normal":
+            mean = np.mean(_posterior, axis=1)
+            if _posterior.ndim <= 2:
+                standard_deviation = np.sqrt(_numba_var(svar, np.var, _posterior, axis=1))
+            else:
+                standard_deviation = np.std(_posterior, axis=1)
+            for i in range(0, np.shape(mean)[0]):
+                _posterior[i, :] = (_posterior[i, :] - mean[i]) / standard_deviation[i]
+        elif norm_method == "minmax":
+            min_elem = np.min(_posterior, axis=1)
+            max_elem = np.max(_posterior, axis=1)
+            for i in range(0, np.shape(min_elem)[0]):
+                _posterior[i, :] = ((_posterior[i, :]) - min_elem[i]) / (max_elem[i] - min_elem[i])
+        elif norm_method == "rank":
+            _posterior = rankdata(_posterior, axis=1)
+        else:
+            raise ValueError("{} is not supported. Use normal, minmax or rank.".format(norm_method))
+
+    figsize, _, _, xt_labelsize, _, _ = _scale_fig_size(figsize, textsize, 1, 1)
+
+    if ax is None:
+        _, ax = plt.subplots(figsize=figsize, constrained_layout=True)
+
+    ax.plot(_posterior[:, ~diverging_mask], color=colornd, alpha=shadend)
+
+    if np.any(diverging_mask):
+        ax.plot(_posterior[:, diverging_mask], color=colord, lw=1)
+
+    ax.tick_params(labelsize=textsize)
+    ax.set_xticks(range(len(var_names)))
+    ax.set_xticklabels(var_names)
+
+    if legend:
+        ax.plot([], color=colornd, label="non-divergent")
+        if np.any(diverging_mask):
+            ax.plot([], color=colord, label="divergent")
+        ax.legend(fontsize=xt_labelsize)
+
+    return ax
