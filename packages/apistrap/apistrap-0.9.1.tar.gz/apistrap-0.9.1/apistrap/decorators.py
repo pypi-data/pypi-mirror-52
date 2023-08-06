0a42609c34166b15b9c53eee90c1d582d7f6fee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:27 +0000

    Add package apistrap/0.9.1/apistrap-0.9.1.tar.gz

---
+++ packages/apistrap/apistrap-0.9.1.tar.gz/apistrap-0.9.1/apistrap/decorators.py
@@ -0,0 +1,79 @@
+from dataclasses import dataclass
+from typing import Optional, Sequence, Type, Union
+
+from schematics import Model
+
+from apistrap.tags import TagData
+
+
+class IgnoreDecorator:
+    """
+    Marks an endpoint as ignored so that Apistrap doesn't include it in the specification.
+    """
+
+
+@dataclass(frozen=True)
+class IgnoreParamsDecorator:
+    """
+    Marks specified function parameters as ignored for the purposes of generating a specification
+    """
+
+    ignored_params: Sequence[str]
+
+
+@dataclass(frozen=True)
+class RespondsWithDecorator:
+    """
+    Specifies the format of the response. The response is automatically validated by Apistrap.
+    """
+
+    response_class: Type[Model]
+    code: int = 200
+    description: Optional[str] = None
+    mimetype: Optional[str] = None
+
+
+@dataclass(frozen=True)
+class AcceptsDecorator:
+    """
+    Specifies the format of the request body and injects it as an argument to the view handler.
+    The destination parameter must be annotated with a corresponding type.
+    """
+
+    request_class: Type[Model]
+
+
+@dataclass(frozen=True)
+class AcceptsFileDecorator:
+    """
+    Declares that an endpoint accepts a file upload as the request body.
+    """
+
+    mime_type: str
+
+
+@dataclass(frozen=True)
+class AcceptsQueryStringDecorator:
+    """
+    Declares that an endpoint accepts query string parameters.
+    """
+
+    parameter_names: Sequence[str]
+
+
+@dataclass(frozen=True)
+class TagsDecorator:
+    """
+    Adds tags to the OpenAPI specification of the decorated view function.
+    """
+
+    tags: Sequence[Union[str, TagData]]
+
+
+@dataclass(frozen=True)
+class SecurityDecorator:
+    """
+    Enforces user authentication and authorization.
+    """
+
+    scopes: Sequence[str]
