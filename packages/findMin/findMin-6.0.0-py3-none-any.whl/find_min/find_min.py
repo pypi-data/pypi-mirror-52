af920debc363dde7bf90c0e6bd974291c2d68497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:17 +0000

    Add package findMin/6.0.0/findMin-6.0.0-py3-none-any.whl

---
+++ packages/findMin/findMin-6.0.0-py3-none-any.whl/find_min/find_min.py
@@ -0,0 +1,42 @@
+#
+# The core class to find min in an array
+#
+from find_min.sample.sample_file import SampleFile 
+
+
+class FindMin:
+
+
+    def printSample(self):
+        sf = SampleFile()
+        samples = sf.load()
+
+        fm = FindMin()
+        fm.printMin(samples)
+
+
+    def findMin(self, nums):
+        i = 1
+        size = len(nums)
+        minimum = None
+        
+        if (size == 1):
+            return nums[0]
+        
+        while i < size:
+            minimum = nums[i]
+            if (nums[i-1] < minimum):
+                minimum = nums[i-1]
+                break
+
+            i += 1
+        
+        return minimum
+
+
+    def printMin(self, samples):
+        for sample in samples.values():
+            min = self.findMin(sample["sample"])
+            print("Min is ({0}) in {1}".format(min, sample["sample"]))
+
+
