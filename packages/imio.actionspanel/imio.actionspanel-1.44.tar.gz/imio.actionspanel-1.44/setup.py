2852bb7e1bb1e91e81ea499a8bfa72a975bb108d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:13 +0000

    Add package imio.actionspanel/1.44/imio.actionspanel-1.44.tar.gz

---
+++ packages/imio.actionspanel/imio.actionspanel-1.44.tar.gz/imio.actionspanel-1.44/setup.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""Installer for the imio.actionspanel package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+long_description = (
+    open('README.rst').read() + '\n' + 'Contributors\n============\n' + '\n' +
+    open('CONTRIBUTORS.rst').read() + '\n' + open('CHANGES.rst').read() + '\n')
+
+setup(
+    name='imio.actionspanel',
+    version='1.44',
+    description="Actions panel",
+    long_description=long_description,
+    # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='Plone Python IMIO',
+    author='IMIO',
+    author_email='dev@imio.be',
+    url='http://pypi.python.org/pypi/imio.actionspanel',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['imio', ],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Plone',
+        'setuptools',
+        'appy',
+        'collective.fingerpointing',
+        'imio.history>=1.17',
+        'plone.api',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
