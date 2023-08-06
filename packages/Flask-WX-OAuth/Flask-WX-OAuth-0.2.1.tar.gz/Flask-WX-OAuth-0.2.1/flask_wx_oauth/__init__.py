fb48560bf35e1d0c2b90c013e2df3214fbf06b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:58 +0000

    Add package Flask-WX-OAuth/0.2.1/Flask-WX-OAuth-0.2.1.tar.gz

---
+++ packages/Flask-WX-OAuth/Flask-WX-OAuth-0.2.1.tar.gz/Flask-WX-OAuth-0.2.1/flask_wx_oauth/__init__.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+# from rauth.compat import is_basestring, parse_qsl, urlencode
+# from rauth.service import Service
+# from rauth.session import OAUTH2_DEFAULT_TIMEOUT, RauthSession
+from urllib.parse import urlencode
+
+import requests
+
+DEFAULT_TIMEOUT = 10
+__version__ = '0.2.1'
+
+
+class WXOAuth:
+    """网站应用微信OAuth2.0登录服务"""
+    def __init__(self, app=None):
+        self.base_url = 'https://api.weixin.qq.com/sns'
+        self.authorize_url = 'https://open.weixin.qq.com/connect/oauth2/authorize'
+
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        self.appid = app.config['WX_APPID']
+        self.secret = app.config['WX_SECRET']
+
+    def get_authorize_url(self, redirect_uri, scope='snsapi_base',
+                          **params):
+        """Returns a formatted authorize URL."""
+        assert redirect_uri
+        params.update({
+            'appid': self.appid,
+            'response_type': 'code',
+            'scope': scope,
+            'redirect_uri': redirect_uri
+        })
+
+        query = urlencode(sorted(params.items()))
+        return '{0}?{1}#wechat_redirect'.format(self.authorize_url, query)
+
+    def get_access_token(self, code):
+        """通过code获取access_token的接口。"""
+        url = self.base_url + '/oauth2/access_token'
+        params = {
+            'appid': self.appid,
+            'secret': self.secret,
+            'code': code,
+            'grant_type': 'authorization_code',
+        }
+        r = requests.get(url, params=params)
+        return r.json()
+
+    def refresh_token(self, refresh_token):
+        """access_token是调用授权关系接口的调用凭证，由于access_token有效期（目前为2个小时）较短，当access_token超时后，可以使用refresh_token进行刷新，access_token刷新结果有两种：
+
+        1. 若access_token已超时，那么进行refresh_token会获取一个新的access_token，新的超时时间；
+
+        2. 若access_token未超时，那么进行refresh_token不会改变access_token，但超时时间会刷新，相当于续期access_token。
+
+        refresh_token拥有较长的有效期（30天），当refresh_token失效的后，需要用户重新授权，所以，请开发者在refresh_token即将过期时（如第29天时），进行定时的自动刷新并保存好它。
+        """
+        url = self.base_url + '/oauth2/refresh_token'
+        params = {
+            'appid': self.appid,
+            'grant_type': 'refresh_token',
+            'refresh_token': refresh_token,
+        }
+        r = requests.get(url, params=params)
+        return r.json()
+
+    def check_access_token(self, access_token, openid):
+        """检验授权凭证（access_token）是否有效"""
+        url = self.base_url + '/auth'
+        params = {
+            'access_token': access_token,
+            'openid': openid,
+        }
+        r = requests.get(url, params=params)
+        return r.json()
+
+    def get_userinfo(self, access_token, openid):
+        """此接口用于获取用户个人信息。开发者可通过OpenID来获取用户基本信息。特别需要注意的是，如果开发者拥有多个移动应用、网站应用和公众帐号，可通过获取用户基本信息中的unionid来区分用户的唯一性，因为只要是同一个微信开放平台帐号下的移动应用、网站应用和公众帐号，用户的unionid是唯一的。换句话说，同一用户，对同一个微信开放平台下的不同应用，unionid是相同的。
+        请注意，在用户修改微信头像后，旧的微信头像URL将会失效，因此开发者应该自己在获取用户信息后，将头像图片保存下来，避免微信头像URL失效后的异常情况。"""
+        url = self.base_url + '/userinfo'
+        params = {
+            'access_token': access_token,
+            'openid': openid,
+        }
+        r = requests.get(url, params=params)
+        if r.encoding == 'ISO-8859-1':
+            r.encoding = 'UTF-8'
+        return r.json()
