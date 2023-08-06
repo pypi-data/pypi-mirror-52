cb5687132fd806fd4d145867f1fe4b5f3d6b88f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:56 +0000

    Add package recoflow/0.0.4/recoflow-0.0.4-py3-none-any.whl

---
+++ packages/recoflow/recoflow-0.0.4-py3-none-any.whl/recoflow/__init__.py
@@ -0,0 +1,13 @@
+#from . import datasets
+#from . import utils
+
+from . import preprocessing
+from . import recommend
+from . import models
+from . import metrics
+from . import vis
+
+__version__ = '0.0.4'
+
+if __name__ == "__main__":
+    print("Welcome to RecoFlow")
