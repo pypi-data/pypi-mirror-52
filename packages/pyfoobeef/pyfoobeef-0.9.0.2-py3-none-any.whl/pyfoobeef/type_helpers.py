79429a54423db7860dfc0b3dcaf68a666390b423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:03 +0000

    Add package pyfoobeef/0.9.0.2/pyfoobeef-0.9.0.2-py3-none-any.whl

---
+++ packages/pyfoobeef/pyfoobeef-0.9.0.2-py3-none-any.whl/pyfoobeef/type_helpers.py
@@ -0,0 +1,9 @@
+from typing import List, Tuple, Union, Set, Dict
+
+PlaylistRef = Union[str, int, "PlaylistInfo"]
+Paths = Union[
+    List[Union[str, "FileSystemEntry"]],
+    Tuple[List[Union[str, "FileSystemEntry"]]],
+]
+ItemIndices = Union[List[int], Tuple[int]]
+ColumnsMap = Union[Dict[str, str], Set[str], List[str], Tuple[str]]
