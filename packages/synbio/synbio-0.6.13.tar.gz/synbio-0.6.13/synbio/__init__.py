809fd45a9ea15a19b2a404101386e9ce92ce1663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:10 +0000

    Add package synbio/0.6.13/synbio-0.6.13.tar.gz

---
+++ packages/synbio/synbio-0.6.13.tar.gz/synbio-0.6.13/synbio/__init__.py
@@ -0,0 +1,7 @@
+"""PlasmidLibrary for synthetic biology design and build."""
+
+from .mix import Mix
+from .primers import Primers
+from .protocol import Protocol
+from .reagents import Reagent
+from .species import Species
