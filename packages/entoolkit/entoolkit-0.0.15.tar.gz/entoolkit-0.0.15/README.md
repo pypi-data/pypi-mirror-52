b6884efe2476faea8cb4473438b7f8b88e1e630a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:59 +0000

    Add package entoolkit/0.0.15/entoolkit-0.0.15.tar.gz

---
+++ packages/entoolkit/entoolkit-0.0.15.tar.gz/entoolkit-0.0.15/README.md
@@ -0,0 +1,21 @@
+# Epanet Toolkit Wrapper for Python
+https://www.epa.gov/water-research/epanet
+
+# Source
+
+Adapted from https://github.com/USEPA/WNTR and others
+
+## Install
+
+```console
+> pip install entoolkit
+```
+## Load module
+
+```python
+from entoolkit import toolkit as et
+```
+
+## Help
+https://www.epa.gov/water-research/epanet
+
---
+++ packages/entoolkit/entoolkit-0.0.15.tar.gz/entoolkit-0.0.15/setup.py
@@ -0,0 +1,20 @@
+﻿# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='entoolkit',
+    version='0.0.15',
+    description='Epanet Toolkit Wrapper for Python',
+    long_description=open('./README.md', encoding="utf8").read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/andresgciamtez/entoolkit',
+    author='Andrés García Martínez',
+    author_email='ppnoptimizer@gmail.com',   
+    packages=['entoolkit'],
+    include_package_data=True,             
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent"
+		]
+	)
