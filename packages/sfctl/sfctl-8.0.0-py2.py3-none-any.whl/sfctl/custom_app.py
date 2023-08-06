d91d9596018f473422b2e1f2c29900e9ddd5221e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:40 +0000

    Add package sfctl/8.0.0/sfctl-8.0.0-py2.py3-none-any.whl

---
+++ packages/sfctl/sfctl-8.0.0-py2.py3-none-any.whl/sfctl/custom_app.py
@@ -0,0 +1,380 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# -----------------------------------------------------------------------------
+
+"""Custom application related commands"""
+
+from __future__ import print_function
+
+import os
+from multiprocessing import Process
+from time import time
+import sys
+import shutil
+from knack.util import CLIError
+from sfctl.custom_exceptions import SFCTLInternalException
+
+def validate_app_path(app_path):
+    """Validate and return application package as absolute path"""
+
+    abspath = os.path.abspath(app_path)
+    if os.path.isdir(abspath):
+        return abspath
+
+    raise ValueError(
+        'Invalid path to application directory: {0}'.format(abspath)
+    )
+
+def print_progress(current, total, rel_file_path, show_progress, time_left=None):
+    """Display progress for uploading"""
+    if show_progress:
+        print(
+            '[{}/{}] files, {}'.format(current, total, rel_file_path),
+            file=sys.stderr
+        )
+        if time_left is not None:
+            print('Time left: {} seconds'.format(time_left))
+
+def path_from_imagestore_string(imagestore_connstr):
+    """
+    Parse the file share path from the image store connection string
+    """
+    if imagestore_connstr and 'file:' in imagestore_connstr:
+        conn_str_list = imagestore_connstr.split("file:")
+        return conn_str_list[1]
+    return False
+
+def upload_to_fileshare(source, dest, show_progress):
+    """
+    Copies the package from source folder to dest folder
+    """
+    total_files_count = 0
+    current_files_count = 0
+    for root, _, files in os.walk(source):
+        total_files_count += len(files)
+
+    for root, _, files in os.walk(source):
+        for single_file in files:
+            rel_path = root.replace(source, '').lstrip(os.sep)
+            dest_path = os.path.join(dest, rel_path)
+            if not os.path.isdir(dest_path):
+                os.makedirs(dest_path)
+
+            shutil.copyfile(
+                os.path.join(root, single_file), os.path.join(dest_path, single_file)
+            )
+            current_files_count += 1
+            print_progress(current_files_count, total_files_count,
+                           os.path.normpath(os.path.join(rel_path, single_file)),
+                           show_progress)
+
+    if show_progress:
+        print('Complete', file=sys.stderr)
+
+def get_timeout_left(target_timeout):
+    """
+    Return the number of seconds until timeout is reached, given a target_timeout which represents
+      the time at which the timer should stop. If the time left is less than 0, return 0
+    :param target_timeout: time measured as from epoch in seconds
+    :return: int
+    """
+    current_time = int(time())  # time from epoch in seconds
+    time_left = target_timeout - current_time
+
+    if time_left <= 0:
+        return 0
+    return time_left
+
+def get_lesser(num_a, num_b):
+    """
+    Return the lesser of int num_a and int num_b. If the lesser number is less than 0, return 0
+    :param num_a: (int)
+    :param num_b: (int)
+    :return: Return the smaller of num_a or num_b.
+    """
+
+    return max(0, min(num_a, num_b))
+
+def upload_to_native_imagestore(sesh, endpoint, abspath, basename, #pylint: disable=too-many-locals,too-many-arguments
+                                show_progress, timeout):
+    """
+    Upload the application package to cluster
+
+    :param sesh: A requests (module) session object.
+    """
+
+    try:
+        from urllib.parse import urlparse, urlencode, urlunparse
+    except ImportError:
+        from urllib import urlencode
+        from urlparse import urlparse, urlunparse  # pylint: disable=import-error
+    total_files_count = 0
+    current_files_count = 0
+    for root, _, files in os.walk(abspath):
+        # Number of uploads is number of files plus number of directories
+        total_files_count += (len(files) + 1)
+
+    target_timeout = int(time()) + timeout
+
+    # Note: while we are raising some exceptions regarding upload timeout, we are leaving the
+    # timeouts raised by the requests library as is since it contains enough information
+    for root, _, files in os.walk(abspath):
+        rel_path = os.path.normpath(os.path.relpath(root, abspath))
+        for single_file in files:
+
+            current_time_left = get_timeout_left(target_timeout)   # an int representing seconds
+
+            if current_time_left == 0:
+                raise SFCTLInternalException('Upload has timed out. Consider passing a longer '
+                                             'timeout duration.')
+
+            url_path = (
+                os.path.normpath(os.path.join('ImageStore', basename,
+                                              rel_path, single_file))
+            ).replace('\\', '/')
+            fp_norm = os.path.normpath(os.path.join(root, single_file))
+            with open(fp_norm, 'rb') as file_opened:
+                url_parsed = list(urlparse(endpoint))
+                url_parsed[2] = url_path
+                url_parsed[4] = urlencode(
+                    {'api-version': '6.1',
+                     'timeout': current_time_left})
+                url = urlunparse(url_parsed)
+
+                # timeout is (connect_timeout, read_timeout)
+                res = sesh.put(url, data=file_opened,
+                               timeout=(get_lesser(60, current_time_left), current_time_left))
+
+                res.raise_for_status()
+                current_files_count += 1
+                print_progress(current_files_count, total_files_count,
+                               os.path.normpath(os.path.join(rel_path, single_file)),
+                               show_progress, get_timeout_left(target_timeout))
+
+        current_time_left = get_timeout_left(target_timeout)
+
+        if current_time_left == 0:
+            raise SFCTLInternalException('Upload has timed out. Consider passing a longer '
+                                         'timeout duration.')
+
+        url_path = (
+            os.path.normpath(os.path.join('ImageStore', basename,
+                                          rel_path, '_.dir'))
+        ).replace('\\', '/')
+        url_parsed = list(urlparse(endpoint))
+        url_parsed[2] = url_path
+        url_parsed[4] = urlencode({'api-version': '6.1',
+                                   'timeout': current_time_left})
+        url = urlunparse(url_parsed)
+
+        res = sesh.put(url,
+                       timeout=(get_lesser(60, current_time_left), current_time_left))
+        res.raise_for_status()
+        current_files_count += 1
+        print_progress(current_files_count, total_files_count,
+                       os.path.normpath(os.path.join(rel_path, '_.dir')),
+                       show_progress, get_timeout_left(target_timeout))
+    if show_progress:
+        print('Complete', file=sys.stderr)
+
+def upload(path, imagestore_string='fabric:ImageStore', show_progress=False, timeout=300):  # pylint: disable=too-many-locals,missing-docstring
+
+    from sfctl.config import (client_endpoint, no_verify_setting, ca_cert_info,
+                              cert_info)
+    import requests
+
+    abspath = validate_app_path(path)
+    basename = os.path.basename(abspath)
+
+    endpoint = client_endpoint()
+    cert = cert_info()
+    ca_cert = True
+    if no_verify_setting():
+        ca_cert = False
+    elif ca_cert_info():
+        ca_cert = ca_cert_info()
+
+    if all([no_verify_setting(), ca_cert_info()]):
+        raise CLIError('Cannot specify both CA cert info and no verify')
+
+    # Note: pressing ctrl + C during upload does not end the current upload in progress, but only
+    # stops the next one from occurring. This will be fixed in the future.
+
+    # Upload to either to a folder, or native image store only
+    if 'file:' in imagestore_string:
+        dest_path = path_from_imagestore_string(imagestore_string)
+
+        process = Process(target=upload_to_fileshare,
+                          args=(abspath, os.path.join(dest_path, basename), show_progress))
+
+        process.start()
+        process.join(timeout)  # If timeout is None then there is no timeout.
+
+        if process.is_alive():
+            process.terminate()  # This will leave any children of process orphaned.
+            raise SFCTLInternalException('Upload has timed out. Consider passing a longer '
+                                         'timeout duration.')
+
+    elif imagestore_string == 'fabric:ImageStore':
+
+        with requests.Session() as sesh:
+            sesh.verify = ca_cert
+            sesh.cert = cert
+
+            # There is no need for a new process here since
+            upload_to_native_imagestore(sesh, endpoint, abspath, basename, show_progress, timeout)
+
+    else:
+        raise CLIError('Unsupported image store connection string')
+
+def parse_app_params(formatted_params):
+    """Parse application parameters from string"""
+    from azure.servicefabric.models import ApplicationParameter
+
+    if formatted_params is None:
+        return None
+
+    res = []
+    for k in formatted_params:
+        param = ApplicationParameter(key=k, value=formatted_params[k])
+        res.append(param)
+
+    return res
+
+def parse_app_metrics(formatted_metrics):
+    """Parse application metrics description from string"""
+    from azure.servicefabric.models import ApplicationMetricDescription
+
+    if formatted_metrics is None:
+        return None
+
+    res = []
+
+    for metric in formatted_metrics:
+        metric_name = metric.get('name', None)
+
+        if not metric_name:
+            raise CLIError('Could not find required application metric name')
+
+        maximum_capacity = metric.get('maximum_capacity', None)
+        reservation_capacity = metric.get('reservation_capacity', None)
+        total_application_capacity = metric.get('total_application_capacity', None)
+
+        res.append(ApplicationMetricDescription(
+            name=metric_name,
+            maximum_capacity=maximum_capacity,
+            reservation_capacity=reservation_capacity,
+            total_application_capacity=total_application_capacity))
+
+    return res
+
+def create(client,  # pylint: disable=too-many-locals,too-many-arguments
+           app_name, app_type, app_version, parameters=None,
+           min_node_count=0, max_node_count=0, metrics=None,
+           timeout=60):
+    """
+    Creates a Service Fabric application using the specified description.
+    :param str app_name: The name of the application, including the 'fabric:'
+    URI scheme.
+    :param str app_type: The application type name found in the application
+    manifest.
+    :param str app_version: The version of the application type as defined in
+    the application manifest.
+    :param str parameters: A JSON encoded list of application parameter
+    overrides to be applied when creating the application.
+    :param int min_node_count: The minimum number of nodes where Service
+    Fabric will reserve capacity for this application. Note that this does not
+    mean that the services of this application will be placed on all of those
+    nodes.
+    :param int max_node_count: The maximum number of nodes where Service
+    Fabric will reserve capacity for this application. Note that this does not
+    mean that the services of this application will be placed on all of those
+    nodes.
+    :param str metrics: A JSON encoded list of application capacity metric
+    descriptions. A metric is defined as a name, associated with a set of
+    capacities for each node that the application exists on.
+    """
+    from azure.servicefabric.models import  ApplicationDescription, ApplicationCapacityDescription
+
+    if (any([min_node_count, max_node_count]) and
+            not all([min_node_count, max_node_count])):
+        raise CLIError('Must specify both maximum and minimum node count')
+
+    if (all([min_node_count, max_node_count]) and
+            min_node_count > max_node_count):
+        raise CLIError('The minimum node reserve capacity count cannot '
+                       'be greater than the maximum node count')
+
+    app_params = parse_app_params(parameters)
+
+    app_metrics = parse_app_metrics(metrics)
+
+    app_cap_desc = ApplicationCapacityDescription(minimum_nodes=min_node_count,
+                                                  maximum_nodes=max_node_count,
+                                                  application_metrics=app_metrics)
+
+    app_desc = ApplicationDescription(name=app_name,
+                                      type_name=app_type,
+                                      type_version=app_version,
+                                      parameter_list=app_params,
+                                      application_capacity=app_cap_desc)
+
+    client.create_application(app_desc, timeout)
+
+def upgrade(  # pylint: disable=too-many-arguments,too-many-locals,missing-docstring
+        client, application_id, application_version, parameters,
+        mode="UnmonitoredAuto", replica_set_check_timeout=None,
+        force_restart=None, failure_action=None,
+        health_check_wait_duration="0",
+        health_check_stable_duration="PT0H2M0S",
+        health_check_retry_timeout="PT0H10M0S",
+        upgrade_timeout="P10675199DT02H48M05.4775807S",
+        upgrade_domain_timeout="P10675199DT02H48M05.4775807S",
+        warning_as_error=False,
+        max_unhealthy_apps=0, default_service_health_policy=None,
+        service_health_policy=None, timeout=60):
+    from azure.servicefabric.models import (ApplicationUpgradeDescription,
+                                            MonitoringPolicyDescription,
+                                            ApplicationHealthPolicy)
+
+    from sfctl.custom_health import (parse_service_health_policy_map,
+                                     parse_service_health_policy)
+
+    monitoring_policy = MonitoringPolicyDescription(
+        failure_action=failure_action,
+        health_check_wait_duration_in_milliseconds=health_check_wait_duration,
+        health_check_stable_duration_in_milliseconds=health_check_stable_duration,
+        health_check_retry_timeout_in_milliseconds=health_check_retry_timeout,
+        upgrade_timeout_in_milliseconds=upgrade_timeout,
+        upgrade_domain_timeout_in_milliseconds=upgrade_domain_timeout
+    )
+
+    # Must always have empty list
+    app_params = parse_app_params(parameters)
+    if app_params is None:
+        app_params = []
+
+    def_shp = parse_service_health_policy(default_service_health_policy)
+
+    map_shp = parse_service_health_policy_map(service_health_policy)
+
+    app_health_policy = ApplicationHealthPolicy(
+        consider_warning_as_error=warning_as_error,
+        max_percent_unhealthy_deployed_applications=max_unhealthy_apps,
+        default_service_type_health_policy=def_shp,
+        service_type_health_policy_map=map_shp)
+
+    desc = ApplicationUpgradeDescription(
+        name='fabric:/' + application_id,
+        target_application_type_version=application_version,
+        parameters=app_params,
+        upgrade_kind='Rolling',
+        rolling_upgrade_mode=mode,
+        upgrade_replica_set_check_timeout_in_seconds=replica_set_check_timeout,
+        force_restart=force_restart,
+        monitoring_policy=monitoring_policy,
+        application_health_policy=app_health_policy)
+
+    client.start_application_upgrade(application_id, desc, timeout)
---
+++ packages/sfctl/sfctl-8.0.0-py2.py3-none-any.whl/sfctl/helps/app.py
@@ -0,0 +1,146 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# -----------------------------------------------------------------------------
+
+"""Help documentation for Service Fabric application and compose commands."""
+
+from knack.help_files import helps
+
+# If the parameter name doesn't match the actual parameter name,
+# no information will be provided in the help page
+
+# To keep newlines in the help documentation, follow this format:
+# long-summary: |
+#    Following are the ...
+#    1. text
+#    2. text
+
+helps['compose create'] = """
+    type: command
+    short-summary: Creates a Service Fabric compose deployment
+    long-summary: Compose is a file format that describes multi-container applications.
+        This API allows deploying container based applications defined in
+        compose format in a Service Fabric cluster. Once the deployment is
+        created, its status can be tracked via the `GetComposeDeploymentStatus`
+        API.
+    parameters:
+        - name: --repo-pass
+          type: string
+          short-summary: Encrypted contain repository password
+"""
+
+helps['application upload'] = """
+    type: command
+    short-summary: Copy a Service Fabric application package to the image store
+    long-summary: Optionally display upload progress for each file in the
+      package. Upload progress is sent to `stderr`
+    parameters:
+        - name: --path
+          type: string
+          short-summary: Path to local application package
+        - name: --show-progress
+          type: bool
+          short-summary: Show file upload progress for large packages
+        - name: --imagestore-string
+          type: string
+          short-summary: Destination image store to upload the application
+            package to
+          long-summary: To upload to a file location, start this parameter with 'file:'.
+            Otherwise the value should be the image store connection string, such as the default
+            value.
+"""
+
+helps['application upgrade'] = """
+    type: command
+    short-summary: Starts upgrading an application in the Service Fabric
+      cluster
+    long-summary: Validates the supplied application upgrade parameters and
+      starts upgrading the application if the parameters are valid. Note
+      that upgrade description replaces the existing application description.
+      This means that if the parameters are not specified, the existing
+      parameters on the applications will be overwritten with the empty
+      parameters list. This would result in the application using the default
+      value of the parameters from the application manifest.
+    parameters:
+        - name: --application-id
+          type: string
+          name: --application-id
+          type: string
+          short-summary: The identity of the application.
+          long-summary: This is typically the full name of the application without the 
+            'fabric:' URI scheme. Starting from version 6.0, hierarchical names are delimited 
+            with the "~" character. For example, if the application name is "fabric:/myapp/app1", 
+            the application identity would be "myapp~app1" in 6.0+ and 
+            "myapp/app1" in previous versions.
+        - name: --application-version
+          type: string
+          short-summary: The target application type version 
+            (found in the application manifest) for the application upgrade.
+        - name: --parameters
+          type: string
+          short-summary: A JSON encoded list of application parameter overrides
+            to be applied when upgrading the application
+        - name: --mode
+          type: string
+          short-summary: The mode used to monitor health during a rolling
+            upgrade
+        - name: --replica-set-check-timeout
+          type: int
+          short-summary: The maximum amount of time to block processing of an
+            upgrade domain and prevent loss of availability when there are
+            unexpected issues. Measured in seconds.
+        - name: --force-restart
+          type: bool
+          short-summary: Forcefully restart processes during upgrade even
+            when the code version has not changed
+        - name: --failure-action
+          type: string
+          short-summary: The action to perform when a Monitored upgrade
+            encounters monitoring policy or health policy violations
+        - name: --health-check-wait-duration
+          type: string
+          short-summary: The length of time to wait after completing an upgrade domain 
+            before starting the health checks process.
+        - name: --health-check-stable-duration
+          type: string
+          short-summary: The amount of time that the application or cluster must remain healthy 
+            before the upgrade proceeds to the next upgrade domain.
+          long-summary: It is first interpreted as a string representing an ISO 8601 duration. 
+            If that fails, then it is interpreted as a number representing the total number 
+            of milliseconds.
+        - name: --health-check-retry-timeout
+          type: string
+          short-summary: The length of time between attempts to perform health checks if 
+            the application or cluster is not healthy.
+        - name: --upgrade-timeout
+          type: string
+          short-summary: The amount of time the overall upgrade has to complete before 
+            FailureAction is executed. 
+          long-summary: It is first interpreted as a string representing an 
+            ISO 8601 duration. If that fails, then it is interpreted as a number 
+            representing the total number of milliseconds.
+        - name: --upgrade-domain-timeout
+          type: string
+          short-summary: The amount of time each upgrade domain has to complete before 
+            FailureAction is executed.
+          long-summary: It is first interpreted as a string representing an 
+            ISO 8601 duration. If that fails, then it is interpreted as a number 
+            representing the total number of milliseconds.
+        - name: --warning-as-error
+          type: bool
+          short-summary: Indicates whether warnings are treated with the same severity as errors.
+        - name: --max-unhealthy-apps
+          type: int
+          short-summary: The maximum allowed percentage of unhealthy deployed
+            applications. Represented as a number between 0 and 100.
+        - name: --default-service-health-policy
+          type: string
+          short-summary: JSON encoded specification of the health policy used
+            by default to evaluate the health of a service type
+        - name: --service-health-policy
+          type: string
+          short-summary: JSON encoded map with service type health
+            policy per service type name. The map is empty be default.
+"""
