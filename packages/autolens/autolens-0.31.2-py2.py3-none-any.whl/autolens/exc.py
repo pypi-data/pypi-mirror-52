5a2c31710368d14e58af38b96f4b4cbed8c89513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:56 +0000

    Add package autolens/0.31.2/autolens-0.31.2-py2.py3-none-any.whl

---
+++ packages/autolens/autolens-0.31.2-py2.py3-none-any.whl/autolens/exc.py
@@ -0,0 +1,61 @@
+import autofit as af
+
+
+class ScaledArrayException(Exception):
+    pass
+
+
+class GridException(Exception):
+    pass
+
+
+class MappingException(Exception):
+    pass
+
+
+class DataException(Exception):
+    pass
+
+
+class ConvolutionException(Exception):
+    pass
+
+
+class MaskException(Exception):
+    pass
+
+
+class CosmologyException(Exception):
+    pass
+
+
+class GalaxyException(Exception):
+    pass
+
+
+class RayTracingException(af.exc.FitException):
+    pass
+
+
+class PixelizationException(af.exc.FitException):
+    pass
+
+
+class InversionException(af.exc.FitException):
+    pass
+
+
+class FittingException(Exception):
+    pass
+
+
+class PlottingException(Exception):
+    pass
+
+
+class PhaseException(Exception):
+    pass
+
+
+class UnitsException(Exception):
+    pass
