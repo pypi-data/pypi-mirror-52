4f69192ce011f0154510eac1f1f91ca72270ddab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:20 +0000

    Add package asyncprogressor/0.1.6/asyncprogressor-0.1.6.tar.gz

---
+++ packages/asyncprogressor/asyncprogressor-0.1.6.tar.gz/asyncprogressor-0.1.6/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name="asyncprogressor",
+    version="0.1.6",
+    author="Vlad Nadzuga",
+    author_email="nadzuga0vlad@gmail.com    ",
+    description="Package for showing progress on your function based on last run",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Spr00t/asyncprogressor",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
