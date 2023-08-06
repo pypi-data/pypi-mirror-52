25c4680eaa47c9f9f49853b7dccc6a8cfc6a8632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:38 +0000

    Add package bert-etl/0.4.18/bert-etl-0.4.18.tar.gz

---
+++ packages/bert-etl/bert-etl-0.4.18.tar.gz/bert-etl-0.4.18/bert/exceptions.py
@@ -0,0 +1,21 @@
+class BertException(Exception):
+    pass
+
+class SchemaRequired(BertException):
+    pass
+
+class EncoderLoadError(BertException):
+    pass
+
+class BertEncoderError(BertException):
+    pass
+
+class BertDecoderError(BertException):
+    pass
+
+class BertConfigError(BertException):
+    pass
+
+class AWSError(BertException):
+    pass
+
