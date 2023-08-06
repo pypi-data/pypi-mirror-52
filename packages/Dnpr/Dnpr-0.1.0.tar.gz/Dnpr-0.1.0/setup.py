4cfa753d1959263e15a266de398003b502889a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:54 +0000

    Add package Dnpr/0.1.0/Dnpr-0.1.0.tar.gz

---
+++ packages/Dnpr/Dnpr-0.1.0.tar.gz/Dnpr-0.1.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Dnpr",
+    version="0.1.0",
+    author="Satyaki De",
+    author_email="satyaki.de@gmail.com",
+    description="Useful Data manipulation functions for JSON. Fixed partition by bug.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SatyakiDe2019/Dnpr",
+    packages=setuptools.find_packages(),
+	install_requires=[
+          'markdown','pandas','regex',
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
