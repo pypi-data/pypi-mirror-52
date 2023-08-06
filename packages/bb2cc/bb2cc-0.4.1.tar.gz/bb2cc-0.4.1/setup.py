02c6988c5306489a9f6679a43be655936eb6003a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:30 +0000

    Add package bb2cc/0.4.1/bb2cc-0.4.1.tar.gz

---
+++ packages/bb2cc/bb2cc-0.4.1.tar.gz/bb2cc-0.4.1/setup.py
@@ -0,0 +1,19 @@
+"""Package definition for bb2cc."""
+
+from distutils.core import setup
+
+setup(
+    name='bb2cc',
+    version='0.4.1',
+    url='https://bitbucket.org/dtao/bb2cc',
+    license='MIT',
+    author='Dan Tao',
+    author_email='dtao@atlassian.com',
+    description='Bitbucket to Confluence Cloud',
+    py_modules=['bb2cc', 'cc', 'mm2cc'],
+    scripts=['bin/bb2cc'],
+    install_requires=[
+        'requests==2.22.0',
+        'mistune==0.8.4'
+    ],
+)
