7dd153778c932e8866dcbe0a09cce8eb2706f587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:00 +0000

    Add package pyautoit-win64/1.0.3/pyautoit-win64-1.0.3.tar.gz

---
+++ packages/pyautoit-win64/pyautoit-win64-1.0.3.tar.gz/pyautoit-win64-1.0.3/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import autoit
+import os
+
+setup(
+    name='pyautoit-win64',
+    version="1.0.3",
+    packages=['autoit'],
+    package_data={'': ['lib\\*.dll']},
+    url='https://github.com/xuepl/pyautoit-64.git',
+    license='MIT',
+    author='xuepl',
+    author_email='xuepl@guoyasoft.com',
+    description='Python binding for AutoItX3.dll 支持64位dll',
+    platforms="python"
+)
+
