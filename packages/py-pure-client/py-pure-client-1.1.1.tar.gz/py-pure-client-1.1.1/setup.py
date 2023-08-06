3d5fbf30e5d15451f6090aa2b92f61565a61a8fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:53 +0000

    Add package py-pure-client/1.1.1/py-pure-client-1.1.1.tar.gz

---
+++ packages/py-pure-client/py-pure-client-1.1.1.tar.gz/py-pure-client-1.1.1/setup.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+# flake8: noqa
+
+'''
+Pure Storage Python clients for FlashArray, FlashBlade, and Pure1 APIs
+'''
+
+
+from setuptools import setup, find_packages  # noqa: H301
+
+NAME = 'py-pure-client'
+VERSION = '1.1.1'
+
+REQUIRES = ['urllib3 >= 1.15', 'six >= 1.10', 'certifi', 'python-dateutil', 'paramiko', 'PyJWT', 'requests']
+
+readme = open('README.md', 'r')
+README_TEXT = readme.read()
+readme.close()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description='Pure Storage Python clients for FlashArray, FlashBlade, and Pure1 APIs',
+    author='Pure Storage',
+    author_email='tvilcu@purestorage.com',
+    url='https://github.com/PureStorage-OpenConnect/py-pure-client',
+    download_url='https://github.com/PureStorage-OpenConnect/py-pure-client/archive/1.tar.gz',
+    keywords=['Swagger', 'Pure Storage', 'Python', 'clients', 'REST', 'API', 'FlashArray', 'FlashBlade', 'Pure1'],
+    license='BSD 2-Clause',
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=README_TEXT,
+    long_description_content_type='text/markdown'
+)
