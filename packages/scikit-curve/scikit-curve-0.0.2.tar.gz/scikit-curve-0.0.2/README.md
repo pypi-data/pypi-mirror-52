d65ffd0ca4280e4e19bcc922bd95f72f3bc654e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:58 +0000

    Add package scikit-curve/0.0.2/scikit-curve-0.0.2.tar.gz

---
+++ packages/scikit-curve/scikit-curve-0.0.2.tar.gz/scikit-curve-0.0.2/README.md
@@ -0,0 +1,29 @@
+# scikit-curve
+
+[![PyPI version](https://img.shields.io/pypi/v/scikit-curve.svg)](https://pypi.python.org/pypi/scikit-curve)
+[![Build status](https://travis-ci.org/espdev/scikit-curve.svg?branch=master)](https://travis-ci.org/espdev/scikit-curve)
+[![Docs status](https://readthedocs.org/projects/scikit-curve/badge/)](https://scikit-curve.readthedocs.io/en/latest/)
+[![BSD License](https://img.shields.io/pypi/l/scikit-curve.svg)](https://github.com/espdev/scikit-curve/blob/master/LICENSE)
+
+:warning: :construction: **UNDER DEVELOPMENT** :construction:  :warning:
+
+A toolkit to manipulate n-dimensional geometric curves in Python.
+
+```python
+import matplotlib.pyplot as plt
+
+from curve.curves import lissajous
+from curve.plot import curveplot
+from curve import PreservedSpeedInterpolationGrid
+
+curve = lissajous(p_count=51)
+
+grid = PreservedSpeedInterpolationGrid(301)
+curve_i = curve.interpolate(grid, method='hermite')
+
+curveplot(curve_i, param='speed', normals=True, marker='.')
+
+plt.show()
+```
+
+![lissajous](assets/lissajous_plot.png)
---
+++ packages/scikit-curve/scikit-curve-0.0.2.tar.gz/scikit-curve-0.0.2/curve/_numeric.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+"""
+The module provides numeric routines
+
+"""
+
+import typing as t
+import numpy as np
+
+
+def isequal(obj1: np.ndarray, obj2: np.ndarray, **kwargs) -> np.ndarray:
+    """Returns a boolean array where two arrays are element-wise equal
+
+    Notes
+    -----
+    int/float dtype independent equal check
+
+    Parameters
+    ----------
+    obj1 : np.ndarray
+        The first object
+    obj2 : np.ndarray
+        The second object
+    kwargs : dict
+        Additional arguments for equal function
+
+    Returns
+    -------
+    res : np.ndarray
+        Result array
+
+    """
+
+    if np.issubdtype(obj1.dtype, np.integer) and np.issubdtype(obj2.dtype, np.integer):
+        cmp = np.equal
+    else:
+        cmp = np.isclose
+
+    return cmp(obj1, obj2, **kwargs)
+
+
+def allequal(obj1: np.ndarray, obj2: np.ndarray, axis: t.Optional[int] = None, **kwargs) -> np.ndarray:
+    """Test whether all array elements along a given axis evaluate to True.
+
+    Parameters
+    ----------
+    obj1 : np.ndarray
+        The first object
+    obj2 : np.ndarray
+        The second object
+    axis : int, None
+        Axis for test equal. By default None
+    kwargs : dict
+        Additional arguments for equal function
+
+    Returns
+    -------
+    res : np.ndarray
+        The result array
+
+    """
+
+    return np.all(isequal(obj1, obj2, **kwargs), axis=axis)
+
+
+def dot1d(data1: np.ndarray, data2: np.ndarray) -> np.ndarray:
+    """Computes row-wise dot product of two MxN arrays
+
+    Parameters
+    ----------
+    data1 : np.ndarray
+        The first MxN array
+    data2 : np.ndarray
+        The second MxN array
+
+    Returns
+    -------
+    res : np.ndarray
+        The array 1xM with row-wise dot product result
+
+    """
+
+    return np.einsum('ij,ij->i', data1, data2)
