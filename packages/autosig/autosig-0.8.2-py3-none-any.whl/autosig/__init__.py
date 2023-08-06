5c65190ecb05569a2c02edc592ffac358d8e8091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:12 +0000

    Add package autosig/0.8.2/autosig-0.8.2-py3-none-any.whl

---
+++ packages/autosig/autosig-0.8.2-py3-none-any.whl/autosig/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""Top-level package for autosig."""
+from .autosig import (
+    Signature,
+    autosig,
+    check,
+    param,
+)
+__all__ = [
+    "Signature",
+    "autosig",
+    "check",
+    "param",
+]
+__author__ = """Antonio Piccolboni"""
+__email__ = 'autosig@piccolboni.info'
+__version__ = '0.8.2'
