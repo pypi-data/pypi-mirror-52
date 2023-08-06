62478a762be7c22d86886ea6ed83aea5957707c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:07 +0000

    Add package visens/0.0.8/visens-0.0.8-py3-none-any.whl

---
+++ packages/visens/visens-0.0.8-py3-none-any.whl/visens/image.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from .load import load
+
+
+def image(filename, colormap="viridis", vmin=None, vmax=None, log=False,
+          save=None, **kwargs):
+    """
+    Make a 2D image of the detector counts
+    """
+
+    data = load(filename, ids=True, **kwargs)
+
+    z, edges = np.histogram(data.ids,
+                            bins=np.arange(-0.5, data.nx * data.ny + 0.5))
+    z = z.reshape(data.nx, data.ny)
+    clab = "Counts"
+    if log:
+        with np.errstate(divide="ignore", invalid="ignore"):
+            z = np.log10(z)
+        clab = "log({})".format(clab)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    im = ax.imshow(z, origin="lower", aspect="equal", interpolation="none",
+                   vmin=vmin, vmax=vmax, extent=[data.x[0, 0], data.x[0, -1],
+                                                 data.y[0, 0], data.y[-1, 0]])
+    cb = plt.colorbar(im, ax=ax)
+    cb.ax.set_ylabel(clab)
+    ax.set_xlabel("x position [m]")
+    ax.set_ylabel("y position [m]")
+    ax.set_title(filename.split("/")[-1])
+    if save is not None:
+        fig.savefig(save, bbox_inches="tight")
+    else:
+        plt.show()
