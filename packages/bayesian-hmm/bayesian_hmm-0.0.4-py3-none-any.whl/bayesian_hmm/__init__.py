ba41b4452351b69c63bf34dcaa0a6d75d3edc69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:30 +0000

    Add package bayesian-hmm/0.0.4/bayesian_hmm-0.0.4-py3-none-any.whl

---
+++ packages/bayesian-hmm/bayesian_hmm-0.0.4-py3-none-any.whl/bayesian_hmm/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Non-parametric Bayesian hidden Markov models. Hidden Markov models are generative
+time series models. This package uses a non-parametric Bayesian estimation process
+that uses dynamic numbers of latent states to avoid having to specify this number in
+advance.
+"""
+
+from .hdphmm import HDPHMM
+from .chain import Chain
+import warnings
+
+warnings.warn(
+    "bayesian_hmm is in beta testing and future versions may behave differently"
+)
