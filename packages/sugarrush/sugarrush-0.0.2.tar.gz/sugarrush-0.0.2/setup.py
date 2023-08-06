e008716256b8c90f55949fe0be8b04513894e81f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:03 +0000

    Add package sugarrush/0.0.2/sugarrush-0.0.2.tar.gz

---
+++ packages/sugarrush/sugarrush-0.0.2.tar.gz/sugarrush-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sugarrush",
+    version="0.0.2",
+    author="Jonatan Westholm",
+    author_email="jonatanwestholm@gmail.com",
+    description="Quality-of-life and extra features for python-sat",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jonatanwestholm/sugarrush",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
