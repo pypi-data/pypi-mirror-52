8088134b4b8c6e4df9c40c96525b7540d76a4784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:59 +0000

    Add package outsystems-pipeline/0.2.19/outsystems-pipeline-0.2.19.tar.gz

---
+++ packages/outsystems-pipeline/outsystems-pipeline-0.2.19.tar.gz/outsystems-pipeline-0.2.19/setup.py
@@ -0,0 +1,50 @@
+from distutils.core import setup
+import os
+
+NAME = 'outsystems-pipeline'
+DESCRIPTION = 'Python pipeline to enable continuous testing using OutSystems.'
+AUTHOR = u'João Furtado, Miguel Afonso, Rui Mendes, Mário Pires'
+EMAIL = u'joao.furtado@outsystems.com, miguel.afonso@outsystems.com, rui.mendes@outsystems.com, mario.pires@outsystems.com'
+URL = 'https://github.com/OutSystems/outsystems-pipeline'
+KEYWORDS = [
+    '',
+]
+
+REQUIREMENTS = [
+    'python-dateutil==2.7.5',
+    'requests==2.20.1',
+    'unittest-xml-reporting==2.2.1',
+    'xunitparser==1.3.3',
+    'pytest==4.3.0'
+]
+
+PACKAGES = [
+    'outsystems',
+    'outsystems.bdd_framework',
+    'outsystems.cicd_probe',
+    'outsystems.exceptions',
+    'outsystems.file_helpers',
+    'outsystems.lifetime',
+    'outsystems.pipeline',
+    'outsystems.vars'
+]
+
+if __name__ == '__main__':  # Do not run setup() when we import this module.
+    if os.path.isfile("VERSION"):
+        with open("VERSION", 'r') as version_file:
+            version = version_file.read().replace('\n', '')
+    else:
+        # dummy version
+        version = '1.0.0'
+
+    setup(
+        name=NAME,
+        version='0.2.19',
+        description=DESCRIPTION,
+        keywords=' '.join(KEYWORDS),
+        author=AUTHOR,
+        author_email=EMAIL,
+        url=URL,
+        packages=PACKAGES,
+        install_requires=REQUIREMENTS
+    )
