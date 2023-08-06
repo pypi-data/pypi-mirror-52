6db84b9281d78e1f121a5a2d771945203081f169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:20 +0000

    Add package instawow/1.4.2/instawow-1.4.2.tar.gz

---
+++ packages/instawow/instawow-1.4.2.tar.gz/instawow-1.4.2/setup.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+from setuptools import find_packages, setup
+
+if os.environ.get('INSTAWOW_DEV'):
+    import fastentrypoints
+
+
+setup(name='instawow',
+      use_scm_version={'write_to': 'instawow/_version.py',
+                       'write_to_template': "__version__ = '{version}'\n",},
+      description='A CLI for managing World of Warcraft add-ons.',
+      url='http://github.com/layday/instawow',
+      author='layday',
+      author_email='layday@protonmail.com',
+      license='GPL-3.0-or-later',
+      long_description=Path('README.rst').read_text(),
+      python_requires='~=3.7',
+      packages=find_packages(),
+      setup_requires=['wheel', 'setuptools_scm'],
+      install_requires=Path('requirements.txt').read_text(),
+      entry_points={'console_scripts': ['instawow = instawow.cli:main']},)
