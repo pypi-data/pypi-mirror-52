eabb54f2dba1295e868b2d9e0c5e8b8ff3661a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:22 +0000

    Add package pyzpaq/0.1/pyzpaq-0.1.tar.gz

---
+++ packages/pyzpaq/pyzpaq-0.1.tar.gz/pyzpaq-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, Extension
+
+zpaq = Extension('zpaq',
+                  sources = ['pyzpaq.cpp', 'zpaq/libzpaq.cpp'],
+                  libraries = ['advapi32'])
+
+setup (name = 'pyzpaq',
+       version = '0.1',
+       description = 'Python zpaq bindings',
+       url='https://github.com/CPB9/pyzpaq.git',
+       ext_modules = [zpaq])
