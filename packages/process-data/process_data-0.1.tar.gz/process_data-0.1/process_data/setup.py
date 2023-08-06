9e69ff4ebb13c70113f6496721913e167047e60e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:33 +0000

    Add package process-data/0.1/process_data-0.1.tar.gz

---
+++ packages/process-data/process_data-0.1.tar.gz/process_data-0.1/process_data/setup.py
@@ -0,0 +1,11 @@
+from setuptools
+
+setuptools.setup(name='process_data',
+      version='0.1',
+      description='Data processing for the SEAT mobility advisor project',
+      url='',
+      author='bauer2488',
+      author_email='',
+      license='MIT',
+      packages=['data_process'],
+      zip_safe=False)
---
+++ packages/process-data/process_data-0.1.tar.gz/process_data-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='process_data',
+      version='0.1',
+      description='Data processing for the SEAT mobility advisor project',
+      url='',
+      author='bauer2488',
+      author_email='',
+      license='MIT',
+      packages=['process_data'],
+      zip_safe=False)
