a13dda0c2a7eb6a7b6783ce811056e9bf94c1f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:52 +0000

    Add package mpx-api/0.1.14/mpx_api-0.1.14-py2.py3-none-any.whl

---
+++ packages/mpx-api/mpx_api-0.1.14-py2.py3-none-any.whl/mpxapi/__init__.py
@@ -0,0 +1,6 @@
+from .http import MPXApi
+from .api_base import get_guid_based_id
+
+
+__version__ = "0.1.14"
+__title__ = "mpxapi"
---
+++ packages/mpx-api/mpx_api-0.1.14-py2.py3-none-any.whl/mpxapi/storefront/order_summary.py
@@ -0,0 +1,10 @@
+from mpxapi.api_base import ApiBase
+
+
+class OrderSummary(ApiBase):
+    def __init__(self, api):
+        ApiBase.__init__(self, api)
+
+        self.schema = "3.4.0"
+        self.service = "Admin Storefront Service"
+        self.path = "/data/OrderSummary"
