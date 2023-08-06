0193a479d2fc4ab8081d13853fbbb5b666957990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:05 +0000

    Add package cmpx/0.7.4/cmpx-0.7.4-py3-none-any.whl

---
+++ packages/cmpx/cmpx-0.7.4-py3-none-any.whl/cmpx/__init__.py
@@ -0,0 +1,11 @@
+from .Complex import Complex
+
+__title__      = 'PythonComplex'
+__author__     = 'Omar Belghaouti'
+__maintainer__ = 'Omar Belghaouti'
+__email__      = 'bel_omar18@yahoo.com'
+__version__    = '0.7.4'
+__license__    = 'MIT'
+__all__ = [
+    'Complex'
+]
