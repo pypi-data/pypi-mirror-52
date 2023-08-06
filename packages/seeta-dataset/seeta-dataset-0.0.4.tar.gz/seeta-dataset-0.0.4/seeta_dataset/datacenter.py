23a4499b8df31889a1b83744108e2a2e99a1eb9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:03 +0000

    Add package seeta-dataset/0.0.4/seeta-dataset-0.0.4.tar.gz

---
+++ packages/seeta-dataset/seeta-dataset-0.0.4.tar.gz/seeta-dataset-0.0.4/seeta_dataset/datacenter.py
@@ -0,0 +1,16 @@
+import uuid
+from seeta_dataset.readonly_dataset import ReadOnlyDataset
+from seeta_dataset.writeonly_dataset import WriteOnlyDataset
+
+
+class DataCenter(object):
+    def __init__(self, root):
+        self._root = root
+
+    def load_dataset(self, dataset_id):
+        return ReadOnlyDataset(self._root, dataset_id)
+
+    def create_dataset(self, record_type, dataset_id=None):
+        if dataset_id is None:
+            dataset_id = str(uuid.uuid1())
+        return WriteOnlyDataset(self._root, dataset_id, record_type)
