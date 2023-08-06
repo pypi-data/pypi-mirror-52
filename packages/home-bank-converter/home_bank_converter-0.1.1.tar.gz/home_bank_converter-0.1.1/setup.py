9473bc0aa5d84f7494da73b62c88033ecf7ec8ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:47 +0000

    Add package home-bank-converter/0.1.1/home_bank_converter-0.1.1.tar.gz

---
+++ packages/home-bank-converter/home_bank_converter-0.1.1.tar.gz/home_bank_converter-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='home_bank_converter',
+    version='0.1.1',
+    packages=['home_bank_converter'],
+    url='https://github.com/FabianReister/home_bank_converter',
+    license='GPL-3.0',
+    author='Fabian Reister',
+    author_email='',
+    description='Converter for various online banking transaction files to be able to import them to HomeBank (http://homebank.free.fr)'
+)
