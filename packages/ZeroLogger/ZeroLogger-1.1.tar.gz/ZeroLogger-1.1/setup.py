5558754b018f3d7082a5b5d0675cae4c7e88c5f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:56 +0000

    Add package ZeroLogger/1.1/ZeroLogger-1.1.tar.gz

---
+++ packages/ZeroLogger/ZeroLogger-1.1.tar.gz/ZeroLogger-1.1/setup.py
@@ -0,0 +1,44 @@
+import setuptools  
+ 
+
+setuptools.setup(  
+    name="ZeroLogger",  
+    version="1.1",  
+    author="ZéroTwó#5019",  
+    author_email="wolf.sf75@googlemail.com",  
+    description="My Own Logger Package to throw done/warn/error/critical(error + exit) messages",    
+    long_description="""# ZeroLogger
+
+ZeroLogger is a simple logger i made for my projects its simple and helps me alot keeping the output clean and smooth.
+
+u can use:
+  - done_task("Lorem") 
+        - to show something worked fine 
+
+  - warn("Ipsum") 
+        - to show a warning 
+
+  - error("Dorem") 
+        - to show a non critical error 
+
+  - critical("FCK") 
+        - Shows a Critical error and quit the "programm" 
+
+# How to use:
+
+  - import using:
+  ```python
+            from ZeroLogger.ZeroLogger import *
+  ```
+  
+  - now u can just use:
+  ```python
+  done_task("Finished")
+  warn("oh oh something is wrong")
+  error("Okay i cant handle this, but i can go on...")
+  critical("Icant handle it EXIT! NOW!")
+  ```
+
+""",
+    long_description_content_type='text/markdown'
+    )
