202f89e934158cc4ad7ec03e40953edf5cedd9c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:41 +0000

    Add package 2wf90-assignment/1.0.2/2wf90-assignment-1.0.2.tar.gz

---
+++ packages/2wf90-assignment/2wf90-assignment-1.0.2.tar.gz/2wf90-assignment-1.0.2/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['2wf90_assignment']
+
+package_data = \
+{'': ['*'], '2wf90_assignment': ['unused/*']}
+
+entry_points = \
+{'console_scripts': ['2wf90 = entry:main']}
+
+setup_kwargs = {
+    'name': '2wf90-assignment',
+    'version': '1.0.2',
+    'description': 'Our attempt at the first software assignment.',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
