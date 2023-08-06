2310b50882b39c68d36171deccc294aaaae9694e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:16 +0000

    Add package gluon2pytorch/0.0.5/gluon2pytorch-0.0.5.tar.gz

---
+++ packages/gluon2pytorch/gluon2pytorch-0.0.5.tar.gz/gluon2pytorch-0.0.5/gluon2pytorch/pytorch_model_template.py
@@ -0,0 +1,19 @@
+pytorch_model_template = """import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class {module_name}(nn.Module):
+    def __init__(self):
+        super({module_name}, self).__init__()
+{inits}
+
+    def forward(self, {inputs}):
+{calls}
+        return {outputs}
+
+if __name__ == '__main__':
+    net = {module_name}()
+    net.load_state_dict(torch.load('{module_name_lower}.pt'))
+    net.eval()
+    print(net(torch.ones(1, 3, 224, 224)))
+"""
