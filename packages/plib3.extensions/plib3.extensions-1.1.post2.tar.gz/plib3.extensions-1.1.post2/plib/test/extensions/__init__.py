60086faf4796f5b27ead0aab68349c62a9f77413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:42 +0000

    Add package plib3.extensions/1.1.post2/plib3.extensions-1.1.post2.tar.gz

---
+++ packages/plib3.extensions/plib3.extensions-1.1.post2.tar.gz/plib3.extensions-1.1.post2/plib/test/extensions/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Sub-Package TEST.EXTENSIONS of Package PLIB3
+Copyright (C) 2008-2019 by Peter A. Donis
+
+Released under the GNU General Public License, Version 2
+See the LICENSE and README files for more information
+
+This sub-package contains the PLIB3.EXTENSIONS test suite.
+"""
