200b0b83d71d9789ffbbcb3209b436cfc64796f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:46 +0000

    Add package tachyon3/3.3.0/tachyon3-3.3.0.tar.gz

---
+++ packages/tachyon3/tachyon3-3.3.0.tar.gz/tachyon3-3.3.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+from tachyon.__version__ import __version__
+
+
+setup(
+    name='tachyon3',
+    url="https://github.com/delvelabs/tachyon",
+    version=__version__,
+    packages=find_packages(),
+    package_data={'tachyon': ['data/*.json']},
+    entry_points={
+        'console_scripts': [
+            'tachyon = tachyon.__main__:main'
+        ]
+    },
+    install_requires=[
+        'hammertime-http>=0.7.0,<0.8',
+        'easyinject==0.3',
+        'click>=6.7,<7'
+    ],
+)
