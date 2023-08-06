4ed1bc2da3e0bde5c0c7d314dd892a274f92058e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:40 +0000

    Add package colour-printing/0.3.11/colour-printing-0.3.11.tar.gz

---
+++ packages/colour-printing/colour-printing-0.3.11.tar.gz/colour-printing-0.3.11/test/test.py
@@ -0,0 +1,16 @@
+class Mode:
+    NORMAL = 'normal'
+    BOLD = 'bold'
+    UNDERLINE = 'underline'
+    BLINK = 'blink'
+    INVERT = 'invert'
+    HIDE = 'hide'
+
+    def __repr__(self):
+        return '123'
+
+    def __str__(self):
+        return '123'
+
+
+print(Mode)
