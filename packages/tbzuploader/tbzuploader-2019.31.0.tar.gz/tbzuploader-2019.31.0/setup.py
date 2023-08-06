32a68179ba63effb20724560eaaf388a37f3d86d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:40 +0000

    Add package tbzuploader/2019.31.0/tbzuploader-2019.31.0.tar.gz

---
+++ packages/tbzuploader/tbzuploader-2019.31.0.tar.gz/tbzuploader-2019.31.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    name='tbzuploader',
+    version='2019.31.0',
+    license='Apache Software License 2.0',
+    long_description=open('README.rst').read(),
+    url='https://github.com/tbz-pariv/tbzuploader',
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'requests[security] ; python_version<="2.7.9"',
+        'requests ; python_version>"2.7.9"',
+        'future',
+    ],
+
+    include_package_data=True,
+
+    author='TBZ-PARIV GmbH',
+    author_email='info@tbz-pariv.de',
+
+    entry_points={
+        'console_scripts': [
+            'tbzuploader=tbzuploader.console_scripts:main',
+        ],
+    }
+)
