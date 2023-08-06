1072527ecd70e6857f703aa36f270cb202b900d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:43 +0000

    Add package Companies-classification-first-nanodegree-program-upload/0.1/Companies%20classification%20-%20first%20nanodegree%20program%20upload-0.1.tar.gz

---
+++ packages/Companies-classification-first-nanodegree-program-upload/Companies%20classification%20-%20first%20nanodegree%20program%20upload-0.1.tar.gz/Companies classification - first nanodegree program upload-0.1/setup.py
@@ -0,0 +1,15 @@
+# TODO: Fill out this file with information about your package
+
+# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+
+# HINT: Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
+
+from setuptools import setup
+
+setup(name='Companies classification - first nanodegree program upload',
+      version='0.1',
+      author='vlamine',
+      description='object-oriented companies classification',
+      package=['Company_nanodegree'],
+      zip_safe=True)
