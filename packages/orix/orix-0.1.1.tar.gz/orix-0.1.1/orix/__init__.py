74e0f48b14395aa4f1a873b1af9e6877a12b5568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:16 +0000

    Add package orix/0.1.1/orix-0.1.1.tar.gz

---
+++ packages/orix/orix-0.1.1.tar.gz/orix-0.1.1/orix/__init__.py
@@ -0,0 +1,19 @@
+"""Handling of quaternion, vector, and other 3d object data in Python.
+
+Submodules
+==========
+
+.. autosummary::
+    :toctree: _autosummary
+
+    scalar
+    vector
+    quaternion
+
+"""
+
+__name__ = "orix"
+__version__ = "0.1.1"
+__author__ = "Ben Martineau"
+__author_email__ = "pyxem.team@gmail.com"
+__description__ = "Orientation, rotation, quaternion, and crystal symmetry handling in Python."
