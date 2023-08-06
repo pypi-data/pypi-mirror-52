ef34b83171587551f9c47499b67ae4826b50daac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:57 +0000

    Add package mlflowrate/0.0.1/mlflowrate-0.0.1.tar.gz

---
+++ packages/mlflowrate/mlflowrate-0.0.1.tar.gz/mlflowrate-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mlflowrate',  # Name of package, required
+    version='0.0.1',  # Version number, required
+    packages=['mlflowrate'],  # directories to install, required
+    # One-line description or tagline of what your project does
+    description='A data exploration package for oil well flow rate prediction.',  # Optional
+    url='https://github.com/kkf18/acse-9-independent-research-project-kkf18',  # Optional
+    author='Kevin Fung',  # Optional
+    author_email='kkf18@imperial.ac.uk',  # Optional
+    license='MIT',
+)
