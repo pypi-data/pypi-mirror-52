582c4d31cac24a81008a53a121abc34a955dd655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:40 +0000

    Add package genepattern-notebook/19.9/genepattern-notebook-19.9.tar.gz

---
+++ packages/genepattern-notebook/genepattern-notebook-19.9.tar.gz/genepattern-notebook-19.9/genepattern/__init__.py
@@ -0,0 +1,28 @@
+"""
+GenePattern Notebook extension for Jupyter
+
+Copyright 2015-2019 Regents of the University of California & Broad Institute
+"""
+
+from .remote_widgets import GPAuthWidget, GPTaskWidget, GPJobWidget, session, register_session, get_session, display
+from nbtools import UIBuilder as GPUIBuilder, UIOutput as GPUIOutput, build_ui, open
+from .local_widgets import GPModuleWidget
+from .jupyter_extensions import (GenePatternMagic, load_ipython_extension, load_jupyter_server_extension,
+                                 _jupyter_server_extension_paths, _jupyter_nbextension_paths)
+
+
+# try:  # Import subpackages, if available
+#     import genepattern.client
+# except Exception:  # Ignore if subpackages are unavailable
+#     pass
+#
+# try:
+#     import genepattern.utils
+# except Exception:
+#     pass
+
+__author__ = 'Thorin Tabor'
+__copyright__ = 'Copyright 2014-2019, Regents of the University of California & Broad Institute'
+__version__ = '19.09'
+__status__ = 'Production/Stable'
+__license__ = 'BSD-3-Clause'
