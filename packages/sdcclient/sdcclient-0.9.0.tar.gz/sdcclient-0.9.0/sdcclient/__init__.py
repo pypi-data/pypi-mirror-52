f0ae93cbc8c0116849bc14b70b2d32fe4022ee7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:09 +0000

    Add package sdcclient/0.9.0/sdcclient-0.9.0.tar.gz

---
+++ packages/sdcclient/sdcclient-0.9.0.tar.gz/sdcclient-0.9.0/sdcclient/__init__.py
@@ -0,0 +1,6 @@
+from sdcclient._monitor import SdcClient
+from sdcclient._monitor import SdMonitorClient
+from sdcclient._monitor_v1 import SdMonitorClientV1
+from sdcclient._secure import SdSecureClient
+from sdcclient._scanning import SdScanningClient
+from sdcclient.ibm_auth_helper import IbmAuthHelper
---
+++ packages/sdcclient/sdcclient-0.9.0.tar.gz/sdcclient-0.9.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='sdcclient',
+      version='0.9.0',
+      description='Python client for Sysdig Cloud',
+      url='http://github.com/draios/python-sdc-client',
+      author='sysdig Inc.',
+      author_email='info@sysdig.com',
+      license='MIT',
+      packages=['sdcclient'],
+      install_requires=['requests', 'pyaml'],
+      zip_safe=False)
