ed0930fdd1526448ffa4e22c8ddb3ef229a20025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:04 +0000

    Add package setuputils3/1.0.3/setuputils3-1.0.3.tar.gz

---
+++ packages/setuputils3/setuputils3-1.0.3.tar.gz/setuputils3-1.0.3/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3 -u
+"""
+Setup script for setuputils
+Copyright (C) 2012-2013 by Peter A. Donis
+
+Released under the Python Software Foundation License.
+"""
+
+
+name = "setuputils3"
+version = "1.0.3"
+description = "A utility to automate away boilerplate in Python 3 setup scripts."
+startline = 3
+
+author = "Peter A. Donis"
+author_email = "peterdonis@alum.mit.edu"
+
+license = "PSF"
+
+dev_status = "Production"
+
+classifiers = """
+Environment :: Console
+Intended Audience :: Developers
+Intended Audience :: System Administrators
+Operating System :: OS Independent
+Topic :: Software Development :: Libraries :: Python Modules
+Topic :: System :: Systems Administration
+Topic :: Utilities
+"""
+
+py_modules = ["setuputils"]
+
+
+if __name__ == '__main__':
+    import sys
+    import os
+    from subprocess import call
+    from distutils.core import setup
+    from setuputils import convert_md_to_rst, setup_vars, long_description as make_long_description
+    
+    if os.path.isfile("README.md"):
+        long_description = make_long_description(globals(), filename="README.md")
+    if "sdist" in sys.argv:
+        convert_md_to_rst()
+        call(['sed', '-i', 's/gitlab.com\/pdonis/pypi.org\/project/', 'README'])
+    setup(**setup_vars(globals()))
