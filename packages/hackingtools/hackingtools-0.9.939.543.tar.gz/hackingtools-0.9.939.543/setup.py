537d364b7d074c5585d2ab295c89971d1b12b46a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:39 +0000

    Add package hackingtools/0.9.939.543/hackingtools-0.9.939.543.tar.gz

---
+++ packages/hackingtools/hackingtools-0.9.939.543.tar.gz/hackingtools-0.9.939.543/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+requirements = ''
+try:
+    with open('requirements.txt') as f:
+        requirements = f.read().splitlines()
+except:
+    print('You should install on Python 3.7+')
+
+setup(name='hackingtools',
+        version='0.9.939.543',
+        description='All Hacking Tools in this Python with Manually Created Modules',
+        url='http://github.com/ConnorXploit/hackingtools-py',
+        author='Connor',
+        author_email='cgehminecraft@gmail.com',
+        license='MIT',
+        packages=find_packages(include=['hackingtools*']),
+        include_package_data = True,
+        package_data = {
+            'hackingtools/core': ['config.json'],
+            '': ['requirements.txt']
+        },
+        classifiers=[
+            "Programming Language :: Python :: 3.7",
+            "License :: OSI Approved :: MIT License"
+        ],
+        install_requires=requirements,
+        zip_safe=False)
