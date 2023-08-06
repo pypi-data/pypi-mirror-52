b49853139e7b65bce1ee5bc98569036e18bfcb10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:30 +0000

    Add package pyminitouch/0.3.2/pyminitouch-0.3.2.tar.gz

---
+++ packages/pyminitouch/pyminitouch-0.3.2.tar.gz/pyminitouch-0.3.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pyminitouch',
+    version='0.3.2',
+    description='python wrapper of minitouch, for better experience',
+    author='williamfzc',
+    author_email='fengzc@vip.qq.com',
+    url='https://github.com/williamfzc/pyminitouch',
+    packages=find_packages(),
+    install_requires=[
+        'loguru',
+        'requests',
+    ]
+)
