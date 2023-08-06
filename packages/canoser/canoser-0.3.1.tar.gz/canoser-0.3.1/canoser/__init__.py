7e779122933fb35cd10654e409ddf384f3ab1150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:25 +0000

    Add package canoser/0.3.1/canoser-0.3.1.tar.gz

---
+++ packages/canoser/canoser-0.3.1.tar.gz/canoser-0.3.1/canoser/__init__.py
@@ -0,0 +1,8 @@
+from canoser.types import (  # noqa: F401
+    StrT, BytesT, BoolT, ArrayT, MapT, TupleT, OptionalT, OptionalObj,
+    Uint8, Uint16, Uint32, Uint64, Int8, Int16, Int32, Int64
+)
+from canoser.cursor import Cursor  # noqa: F401
+from canoser.struct import Struct  # noqa: F401
+from canoser.rust_enum import RustEnum  # noqa: F401
+
