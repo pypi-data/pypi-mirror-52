3b9c73309fca0b11934aa5ae3acae82b3a7761b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:27 +0000

    Add package hits/0.0.7/hits-0.0.7.tar.gz

---
+++ packages/hits/hits-0.0.7.tar.gz/hits-0.0.7/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, Extension
+
+setup(
+    name='hits',
+    version='0.0.7',
+
+    author='Jeff Hussmann',
+    author_email='jeff.hussmann@gmail.com',
+    url='https://github.com/jeffhussmann/hits',
+    description='utilities for processing high-throughput sequencing experiments',
+
+    setup_requires=['cython'],
+
+    ext_package='hits',
+    ext_modules=[
+        Extension('adapters_cython', ['hits/adapters_cython.pyx']),
+        Extension('fastq_cython', ['hits/fastq_cython.pyx']),
+        Extension('sw_cython', ['hits/sw_cython.pyx']),
+    ],
+
+    packages=[
+        'hits',
+        'hits/visualize',
+        'hits/visualize/interactive',
+    ],
+    package_data={
+        'hits/visualize/interactive': [
+            '*.coffee',
+            'example_df.txt',
+            '*.json',
+        ],
+    },
+
+    python_requires='>=3.6',
+
+    install_requires=[
+        'bokeh',
+        'biopython',
+        'matplotlib',
+        'numpy',
+        'ipython',
+        'ipywidgets',
+        'pandas',
+        'pillow',
+        'pysam',
+        'PyYAML',
+        'scipy',
+    ],
+)
