7861f7b14131568e1af036497c0ce1064b7c3385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:33 +0000

    Add package wrattler-data-store/0.4/wrattler_data_store-0.4-py3-none-any.whl

---
+++ packages/wrattler-data-store/wrattler_data_store-0.4-py3-none-any.whl/config.py
@@ -0,0 +1,10 @@
+"""
+Template for configuration module holding credentials for backend storage.
+Copy this file to 'config.py' and fill in your credentials as necessary.
+"""
+
+class AzureConfig:
+    protocol="https"
+    account_name = "wrattlerdatastoretest"
+    account_key = "NNmBHlWcq0T7yl3jIWf+C4vL7UxpIOer7ei1Dg7ksMqxKVe6GY/VaAcmYTJ3CFqom/7by4tx89T+OOZPiN944Q=="
+    container_name = "data"
