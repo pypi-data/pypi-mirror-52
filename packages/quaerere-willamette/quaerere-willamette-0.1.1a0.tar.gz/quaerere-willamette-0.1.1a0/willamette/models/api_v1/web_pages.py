0e8bf84b4566b11ce479c9f2870c7958592c0b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:43 +0000

    Add package quaerere-willamette/0.1.1a0/quaerere-willamette-0.1.1a0.tar.gz

---
+++ packages/quaerere-willamette/quaerere-willamette-0.1.1a0.tar.gz/quaerere-willamette-0.1.1a0/willamette/models/api_v1/web_pages.py
@@ -0,0 +1,20 @@
+__all__ = ['WebPageModel']
+import logging
+
+
+from arango_orm import Collection
+from arango_orm.references import relationship
+from willamette_common.schemas.api_v1.mixins import WebPageFieldsMixin
+
+from .web_sites import WebSiteModel
+
+LOGGER = logging.getLogger(__name__)
+
+
+class WebPageModel(WebPageFieldsMixin, Collection):
+    __collection__ = 'WebPages'
+    _index = [
+        {'type': 'hash', 'fields': ['url'], 'unique': True}
+    ]
+
+    web_site = relationship(WebSiteModel, 'web_site_key')
