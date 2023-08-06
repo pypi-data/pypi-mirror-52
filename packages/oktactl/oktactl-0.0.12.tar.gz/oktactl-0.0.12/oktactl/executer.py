d8612d06aa710076eb432dad86abf8bf298756fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:26 +0000

    Add package oktactl/0.0.12/oktactl-0.0.12.tar.gz

---
+++ packages/oktactl/oktactl-0.0.12.tar.gz/oktactl-0.0.12/oktactl/executer.py
@@ -0,0 +1,19 @@
+import click
+import logging
+from requests.exceptions import ConnectionError
+import traceback
+
+
+def executer(func):
+    def execute(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except ConnectionError:
+            click.secho(
+                "Invalid credentials provided. \nUse `oktactl configure` command to configure credentials", fg='red')
+        except FileNotFoundError:
+            click.secho("Incorrect file path provided", fg='red')
+        except Exception:
+            click.secho("Exception occured:\n {}".format(traceback.format_exc()), fg='red')
+    
+    return execute
---
+++ packages/oktactl/oktactl-0.0.12.tar.gz/oktactl-0.0.12/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+setup(name='oktactl',
+      version='0.0.12',
+      description='Sample Okta CLI tool in python',
+      license='MIT',
+      author='Atulya Kumar Pandey',
+      author_email='atul3015@gmail.com',
+      url='https://github.com/atul3015kr/oktactl',
+      packages=find_packages(),
+      install_requires=[
+          'Click',
+          'requests',
+          'tqdm'
+      ],
+      entry_points='''
+            [console_scripts]
+            oktactl=oktactl.commands:okta
+        ''',
+      zip_safe=True,
+      include_package_data=True,
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+      ],
+      )
