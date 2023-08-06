c2d3d719b21bbe33d446c3d9162d9b39abc00e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:43 +0000

    Add package hzpromise/1.0/hzpromise-1.0.tar.gz

---
+++ packages/hzpromise/hzpromise-1.0.tar.gz/hzpromise-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="hzpromise",
+     author="Huzheng",
+     author_email= "backbye@163.com",
+     description= "An implementation of Promises/A+ for Python",
+     version="1.0",
+     py_modules=["hzpromise", "thenable"],
+     )
+
