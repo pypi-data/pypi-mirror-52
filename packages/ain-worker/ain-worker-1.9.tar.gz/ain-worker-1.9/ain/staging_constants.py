ef4693bdae8b293b3cfd61aab336b462d7dda532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:45 +0000

    Add package ain-worker/1.9/ain-worker-1.9.tar.gz

---
+++ packages/ain-worker/ain-worker-1.9.tar.gz/ain-worker-1.9/ain/staging_constants.py
@@ -0,0 +1,6 @@
+import os 
+
+SHARED_PATH = os.path.dirname(os.path.abspath(__file__)) + "/share"
+ENV_FILE_PATH = SHARED_PATH + "/worker.env"
+IMAGE = "ainblockchain/ain-worker-staging:latest"
+VERSION="1.1"
