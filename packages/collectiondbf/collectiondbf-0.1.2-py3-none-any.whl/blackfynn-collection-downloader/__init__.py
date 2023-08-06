e492df121052e54671fe870fe12f46206d06e542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:04 +0000

    Add package collectiondbf/0.1.2/collectiondbf-0.1.2-py3-none-any.whl

---
+++ packages/collectiondbf/collectiondbf-0.1.2-py3-none-any.whl/blackfynn-collection-downloader/__init__.py
@@ -0,0 +1,6 @@
+from blackfynn-collection-downloader.ui import DetailsInput
+from blackfynn-collection-downloader.config import API_TOKEN, API_SECRET
+from blackfynn-collection-downloader.main import *
+
+
+
