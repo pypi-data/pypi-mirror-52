724018e65fd39417034ff8c37a21402ef6b75a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:48 +0000

    Add package bash.py/0.4.2/bash.py-0.4.2.tar.gz

---
+++ packages/bash.py/bash.py-0.4.2.tar.gz/bash.py-0.4.2/setup.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+pip setup file
+
+Note: To use the 'upload' functionality of this file, you must:
+  $ pip install twine
+"""
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = "bash.py"
+DESCRIPTION = "An inline Bash script runner, for Python."
+URL = "https://github.com/jpetrucciani/bash.py"
+EMAIL = "jacobi@mimirhq.com"
+AUTHOR = "Kenneth Reitz, Jacobi Petrucciani"
+REQUIRES_PYTHON = ">=3.6.0"
+VERSION = "0.4.2"
+
+# What packages are required for this module to be executed?
+REQUIRED = ["delegator.py"]
+
+# What packages are optional?
+EXTRAS = {}  # type: ignore
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+CURRENT_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(
+        os.path.join(CURRENT_DIRECTORY, "README.md"), encoding="utf-8"
+    ) as f:
+        LONG_DESCRIPTION = "\n" + f.read()
+except FileNotFoundError:
+    LONG_DESCRIPTION = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+ABOUT = {}
+ABOUT["__version__"] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []  # type: ignore
+
+    @staticmethod
+    def status(text: str) -> None:
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(text))
+
+    def initialize_options(self) -> None:
+        pass
+
+    def finalize_options(self) -> None:
+        pass
+
+    def run(self) -> None:
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(CURRENT_DIRECTORY, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system(
+            "{0} setup.py sdist bdist_wheel --universal".format(sys.executable)
+        )
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(ABOUT["__version__"]))
+        os.system("git push --tags")
+
+        sys.exit()
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=ABOUT["__version__"],
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    py_modules=['bash'],
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license="MIT",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    # $ setup.py publish support.
+    cmdclass={"upload": UploadCommand},
+)
