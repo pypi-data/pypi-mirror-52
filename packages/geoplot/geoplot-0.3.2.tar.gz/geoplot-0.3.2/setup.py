2fef30c034329e9f989e0dd4808bb783e48b5c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:04 +0000

    Add package geoplot/0.3.2/geoplot-0.3.2.tar.gz

---
+++ packages/geoplot/geoplot-0.3.2.tar.gz/geoplot-0.3.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+setup(
+    name='geoplot',
+    packages=['geoplot'], # this must be the same as the name above
+    install_requires=[
+        'matplotlib', 'seaborn', 'pandas', 'geopandas', 'cartopy', 'descartes', 'mapclassify',
+        'contextily>=1.0rc2'
+    ],
+    extras_require={'develop': ['pytest', 'pytest-mpl', 'scipy']},
+    py_modules=['geoplot', 'crs', 'utils', 'ops'],
+    version='0.3.2',
+    python_requires='>=3.6.0',
+    description='High-level geospatial plotting for Python.',
+    author='Aleksey Bilogur',
+    author_email='aleksey.bilogur@gmail.com',
+    url='https://github.com/ResidentMario/geoplot',
+    download_url='https://github.com/ResidentMario/geoplot/tarball/0.3.0',
+    keywords=[
+        'data', 'data visualization', 'data analysis', 'data science', 'pandas', 'geospatial data',
+        'geospatial analytics'
+    ],
+    classifiers=[],
+)
