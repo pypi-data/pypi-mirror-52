1e08b9644a38837e2387e56ab0411c9688f88836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:03 +0000

    Add package jupytergitcommit/1.2/jupytergitcommit-1.2.tar.gz

---
+++ packages/jupytergitcommit/jupytergitcommit-1.2.tar.gz/jupytergitcommit-1.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="jupytergitcommit",
+    version='1.2',
+    url="https://github.com/newellp2019/jupytergitcommit",
+    author="Peter Newell",
+    description="Jupyter extension to enable user push notebooks to a git repo",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'psutil',
+        'notebook',
+        'pygithub'
+    ],
+    package_data={'jupytergitcommit': ['static/*']},
+    include_package_data=True,
+)
