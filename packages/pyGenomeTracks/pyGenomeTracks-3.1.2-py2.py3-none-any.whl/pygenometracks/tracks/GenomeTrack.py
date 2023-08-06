c7af11673b3fe20330d13e821a06ef5c782208ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:27 +0000

    Add package pyGenomeTracks/3.1.2/pyGenomeTracks-3.1.2-py2.py3-none-any.whl

---
+++ packages/pyGenomeTracks/pyGenomeTracks-3.1.2-py2.py3-none-any.whl/pygenometracks/tracks/GenomeTrack.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+
+from .. utilities import to_string, to_bytes
+import logging
+import numpy as np
+
+
+class GenomeTrack(object):
+    """
+    The GenomeTrack object is a holder for all tracks that are to be plotted.
+    For example, to plot a bedgraph file a new class that extends GenomeTrack
+    should be created.
+
+    It is expected that all GenomeTrack objects have a plot method.
+
+    """
+    SUPORTED_ENDINGS = []
+    TRACK_TYPE = None
+    OPTIONS_TXT = """
+# title of track (plotted on the right side)
+title =
+# height of track in cm (ignored if the track is overlay on top the previous track)
+height = 2
+# if you want to plot the track upside-down:
+# orientation = inverted
+# if you want to plot the track on top of the previous track. Options are 'yes' or 'share-y'. For the 'share-y'
+# option the y axis values is shared between this plot and the overlay plot. Otherwise, each plot use its own scale
+#overlay previous = yes
+"""
+
+    def __init__(self, properties_dict):
+        self.properties = properties_dict
+        self.file_type = 'test'
+
+        FORMAT = "[%(levelname)s:%(filename)s:%(lineno)s - %(funcName)20s()] %(message)s"
+        logging.basicConfig(format=FORMAT)
+        log = logging.getLogger(__name__)
+        log.setLevel(logging.DEBUG)
+        self.log = log
+
+    def plot_y_axis(self, ax, plot_axis):
+        """
+        Plot the scale of the y axis with respect to the plot_axis
+        Args:
+            ax: axis to use to plot the scale
+            plot_axis: the reference axis to get the max and min.
+
+        Returns:
+
+        """
+        if 'show data range' in self.properties and self.properties['show data range'] == 'no':
+            return
+
+        def value_to_str(value):
+            # given a numeric value, returns a
+            # string that removes unneeded decimal places
+            if value % 1 == 0:
+                str_value = str(int(value))
+            else:
+                str_value = "{:.1f}".format(value)
+            return str_value
+
+        ymin, ymax = plot_axis.get_ylim()
+
+        ymax_str = value_to_str(ymax)
+        ymin_str = value_to_str(ymin)
+        # plot something that looks like this:
+        # ymax ┐
+        #      │
+        #      │
+        # ymin ┘
+
+        # the coordinate system used is the ax.transAxes (lower left corner (0,0), upper right corner (1,1)
+        # this way is easier to adjust the positions such that the lines are plotted complete
+        # and not only half of the width of the line.
+        x_pos = [0, 0.5, 0.5, 0]
+        y_pos = [0.01, 0.01, 0.99, 0.99]
+        ax.plot(x_pos, y_pos, color='black', linewidth=1, transform=ax.transAxes)
+        ax.text(-0.2, -0.01, ymin_str, verticalalignment='bottom', horizontalalignment='right', transform=ax.transAxes)
+        ax.text(-0.2, 1, ymax_str, verticalalignment='top', horizontalalignment='right', transform=ax.transAxes)
+        ax.patch.set_visible(False)
+
+    def plot_label(self, label_ax):
+        label_ax.text(0.05, 0.5, self.properties['title'], horizontalalignment='left',
+                      size='large', verticalalignment='center', transform=label_ax.transAxes)
+
+    @staticmethod
+    def change_chrom_names(chrom):
+        """
+        Changes UCSC chromosome names to ensembl chromosome names
+        and vice versa.
+        """
+        # TODO: mapping from chromosome names like mithocondria is missing
+        if chrom.startswith('chr'):
+            # remove the chr part from chromosome name
+            chrom = chrom[3:]
+        else:
+            # prefix with 'chr' the chromosome name
+            chrom = 'chr' + chrom
+
+        return chrom
+
+    @staticmethod
+    def check_chrom_str_bytes(iteratable_obj, p_obj):
+        # determine type
+        if isinstance(p_obj, list) and len(p_obj) > 0:
+            type_ = type(p_obj[0])
+        else:
+            type_ = type(p_obj)
+        if not isinstance(type(next(iter(iteratable_obj))), type_):
+            if type(next(iter(iteratable_obj))) is str:
+                p_obj = to_string(p_obj)
+            elif type(next(iter(iteratable_obj))) in [bytes, np.bytes_]:
+                p_obj = to_bytes(p_obj)
+        return p_obj
