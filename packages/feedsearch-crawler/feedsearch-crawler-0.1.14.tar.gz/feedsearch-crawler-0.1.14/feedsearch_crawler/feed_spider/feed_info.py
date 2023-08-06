dbc5088a66d84da11d03f2f0b8e986114dc314ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:38 +0000

    Add package feedsearch-crawler/0.1.14/feedsearch-crawler-0.1.14.tar.gz

---
+++ packages/feedsearch-crawler/feedsearch-crawler-0.1.14.tar.gz/feedsearch-crawler-0.1.14/feedsearch_crawler/feed_spider/feed_info.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+from typing import List
+
+from yarl import URL
+
+from feedsearch_crawler.crawler import Item, to_string
+
+
+class FeedInfo(Item):
+    score: int = 0
+    url: URL = ""
+    content_type: str = ""
+    version: str = ""
+    title: str = ""
+    hubs: List[str] = []
+    description: str = ""
+    is_push: bool = False
+    self_url: str = ""
+    bozo: int = 0
+    favicon: URL = ""
+    site_url: URL = ""
+    site_name: str = ""
+    favicon_data_uri: str = ""
+    content_length: int = 0
+    last_updated: datetime = None
+
+    def serialize(self):
+        last_updated = self.last_updated.isoformat() if self.last_updated else ""
+
+        return dict(
+            url=to_string(self.url),
+            title=self.title,
+            version=self.version,
+            score=self.score,
+            hubs=self.hubs,
+            description=self.description,
+            is_push=self.is_push,
+            self_url=self.self_url,
+            favicon=to_string(self.favicon),
+            content_type=self.content_type,
+            bozo=self.bozo,
+            site_url=to_string(self.site_url),
+            site_name=self.site_name,
+            favicon_data_uri=self.favicon_data_uri,
+            content_length=self.content_length,
+            last_updated=last_updated,
+        )
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.url == other.url
+
+    def __hash__(self):
+        return hash(self.url)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({str(self.url)})"
