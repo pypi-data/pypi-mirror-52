cd95bebc94bcfb408a321424c42d81055aaf1f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:50 +0000

    Add package pycosmos/0.1/pycosmos-0.1.tar.gz

---
+++ packages/pycosmos/pycosmos-0.1.tar.gz/pycosmos-0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+long_description = "A python judging client library for Cosmos front-ends"
+
+setuptools.setup(
+    name='pycosmos',
+    version='0.1',
+    author="Chinmay Phulse",
+    author_email="phulsechinmay@gmail.com",
+    packages=['pycosmos'],
+    description="A python judging client library for Cosmos front-ends",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/phulsechinmay/pycosmos",
+    download_url = 'https://github.com/phulsechinmay/pycosmos/archive/v_01.tar.gz',
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
