dfbf3f3b22199509192151d3c771632f3d89c610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:08 +0000

    Add package imageboard-dl/1.0.63/imageboard-dl-1.0.63.tar.gz

---
+++ packages/imageboard-dl/imageboard-dl-1.0.63.tar.gz/imageboard-dl-1.0.63/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name="imageboard-dl",
+	version="1.0.63",
+	description="Image downloader/scraper for various imageboards and image albums written in Python.",
+	url="https://github.com/sixem/imageboard-dl",
+	author="emy",
+	author_email="admin@eyy.co",
+	license='MIT',
+	packages=["imageboard-dl"],
+	install_requires=['cfscrape','beautifulsoup4',],
+	scripts=["bin/imageboard-dl"],
+	zip_safe=False)
