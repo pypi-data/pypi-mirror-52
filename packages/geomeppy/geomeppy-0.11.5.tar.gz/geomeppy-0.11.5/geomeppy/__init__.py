b2f31b537dc1eef684b23179f20bf517c97b503a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:21 +0000

    Add package geomeppy/0.11.5/geomeppy-0.11.5.tar.gz

---
+++ packages/geomeppy/geomeppy-0.11.5.tar.gz/geomeppy-0.11.5/geomeppy/__init__.py
@@ -0,0 +1,5 @@
+from geomeppy.idf import IDF
+
+__version__ = "0.11.5"
+
+__all__ = ["IDF"]
---
+++ packages/geomeppy/geomeppy-0.11.5.tar.gz/geomeppy-0.11.5/tests/test_obj.py
@@ -0,0 +1,10 @@
+import os
+from geomeppy.io.obj import export_to_obj
+
+
+def test_export_to_obj(base_idf):
+    export_to_obj(base_idf, "test.obj")
+    assert os.path.isfile("test.obj")
+    assert os.path.isfile("test.mtl")
+    os.remove("test.obj")
+    os.remove("test.mtl")
