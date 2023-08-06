036b722e47fa91e8e2ca0476cbaf485036818eec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:38 +0000

    Add package specsheet/0.1.0/specsheet-0.1.0-py3-none-any.whl

---
+++ packages/specsheet/specsheet-0.1.0-py3-none-any.whl/specsheet/__init__.py
@@ -0,0 +1,12 @@
+"""
+Spec Sheet
+~~~~~~
+Command to produce navigable documentation from Gherkin feature files.
+
+:copyright: Copyright 2019 Edward Armitage.
+:license: MIT, see LICENSE for details.
+"""
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
