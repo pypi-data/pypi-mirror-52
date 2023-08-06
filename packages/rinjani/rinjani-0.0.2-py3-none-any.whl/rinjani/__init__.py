1c28bc344f34ce05ea45c3eb4c229012cd0c8bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:11 +0000

    Add package rinjani/0.0.2/rinjani-0.0.2-py3-none-any.whl

---
+++ packages/rinjani/rinjani-0.0.2-py3-none-any.whl/rinjani/__init__.py
@@ -0,0 +1,19 @@
+
+
+# python package to run machine learning tasks
+
+__version__ = "0.0.1"
+__author__ = "Connor Beard"
+__all__ = ['yeet']
+
+
+
+
+def yeet():
+    return('yeet indeed')
+        
+
+
+
+
+
