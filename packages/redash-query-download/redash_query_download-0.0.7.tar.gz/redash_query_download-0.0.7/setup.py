f3384a4642dac89ad042db76a07223846cef9a43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:41 +0000

    Add package redash-query-download/0.0.7/redash_query_download-0.0.7.tar.gz

---
+++ packages/redash-query-download/redash_query_download-0.0.7.tar.gz/redash_query_download-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="redash_query_download",
+    version="0.0.7",
+    author="Michihito Shigemura",
+    author_email="i.am.shige@gmail.com",
+    description="redash query download client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shigemk2/redash_query_download",
+    scripts=['bin/rqd'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
