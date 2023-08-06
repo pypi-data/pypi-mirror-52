43c4c270d3756f2c526e08ebaac02d5fea202848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:11 +0000

    Add package fings/1.6.7/fings-1.6.7.tar.gz

---
+++ packages/fings/fings-1.6.7.tar.gz/fings-1.6.7/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fings",
+    version="1.6.7",
+    author="Christopher Wardell",
+    author_email="github@cpwardell.com",
+    description="Filters for Next Generation Sequencing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cpwardell/FiNGS",
+    packages=['fings'],
+    include_package_data=True,
+    install_requires=['pyvcf',
+                      'pysam',
+                      'numpy',
+                      'scipy',
+                      'pandas',
+                      'joblib',
+                      'seaborn',
+                      'statsmodels',
+                      'editdistance'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
