b373324843290051eb5dc670c3feac51effdd878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:53 +0000

    Add package putio-downloader/3.1.3/putio-downloader-3.1.3.tar.gz

---
+++ packages/putio-downloader/putio-downloader-3.1.3.tar.gz/putio-downloader-3.1.3/tests/test_crc.py
@@ -0,0 +1,8 @@
+"""Test CRC calculation."""
+from putio_downloader import PutioAria2cDownloader
+
+
+def test_crc():
+    config_path = 'config.ini.example'
+    test_instance = PutioAria2cDownloader(rpc_url='http://localhost:6800/rpc')
+    assert test_instance.crc(config_path) == '5C4A2283'
