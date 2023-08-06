8503a326fa0e34b04ed2b5324f69351aed00f7bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:14 +0000

    Add package casymda/0.1.2/casymda-0.1.2.tar.gz

---
+++ packages/casymda/casymda-0.1.2.tar.gz/casymda-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='casymda',
+    url='',
+    author='FFC',
+    author_email='fladdi.mir@gmx.de',
+    packages=['casymda.BPMN', 'casymda.CoreBlocks',
+              'casymda.Visualization', 'casymda.WIP'],
+    install_requires=['simpy', 'xmltodict', 'Pillow'],
+    version='0.1.2',
+    zip_safe=False,
+    license='MIT',
+    description='A simple DES modeling and simulation environment based on simpy: create models with help of Camunda Modeler; run and debug using a 2D animation based on tkinter and PIL.',
+    include_package_data=True,
+    package_data={
+        'casymda.Visualizer': ['state_icons/*.png']
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+)
