a9b41bbf107162ceda06f8807efea930de5334bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:40 +0000

    Add package rfd/0.2.0/rfd-0.2.0.tar.gz

---
+++ packages/rfd/rfd-0.2.0.tar.gz/rfd-0.2.0/rfd/api.py
@@ -0,0 +1,203 @@
+"""RFD API."""
+
+try:
+    from json.decoder import JSONDecodeError
+except ImportError:
+    JSONDecodeError = ValueError
+import logging
+from math import ceil
+import requests
+from bs4 import BeautifulSoup
+from rfd.constants import API_BASE_URL
+
+try:
+    from urllib.parse import urlparse  # python 3
+except ImportError:
+    from urlparse import urlparse  # python 2
+
+
+def build_web_path(slug):
+    return "{}{}".format(API_BASE_URL, slug)
+
+
+def extract_post_id(url):
+    return url.split("/")[3].split("-")[-1]
+
+
+def is_int(number):
+    try:
+        int(number)
+        return True
+    except ValueError:
+        return False
+
+
+def calculate_score(post):
+    """Calculate either topic or post score. If votes cannot be retrieved, the score is 0.
+
+    Arguments:
+        post {dict} -- pass in the topic/post object
+
+    Returns:
+        int -- score
+    """
+    score = 0
+    try:
+        score = int(post.get("votes").get("total_up")) - int(
+            post.get("votes").get("total_down")
+        )
+    except AttributeError:
+        pass
+
+    return score
+
+
+def get_safe_per_page(limit):
+    if limit < 5:
+        return 5
+    if limit > 40:
+        return 40
+    return limit
+
+
+def users_to_dict(users):
+    users_dict = {}
+    for user in users:
+        users_dict[user.get("user_id")] = user.get("username")
+    return users_dict
+
+
+def strip_html(text):
+    return BeautifulSoup(text, "html.parser").get_text()
+
+
+def is_valid_url(url):
+    result = urlparse(url)
+    return all([result.scheme, result.netloc, result.path])
+
+
+def get_threads(forum_id, limit):
+    """Get threads from rfd api
+
+    Arguments:
+        forum_id {int} -- forum id
+        limit {[type]} -- limit number of threads returned
+
+    Returns:
+        dict -- api response
+    """
+    try:
+        response = requests.get(
+            "{}/api/topics?forum_id={}&per_page={}".format(
+                API_BASE_URL, forum_id, get_safe_per_page(limit)
+            )
+        )
+        if response.status_code == 200:
+            return response.json()
+        logging.error("Unable to retrieve threads. %s", response.text)
+    except JSONDecodeError as err:
+        logging.error("Unable to retrieve threads. %s", err)
+    return None
+
+
+def parse_threads(api_response, limit):
+    """parse topics list api response into digestible list.
+
+    Arguments:
+        api_response {dict} -- topics response from rfd api
+        limit {int} -- limit number of threads returned
+
+    Returns:
+        list(dict) -- digestible list of threads
+    """
+    threads = []
+    if api_response is None:
+        return threads
+    for topic in api_response.get("topics"):
+        threads.append(
+            {
+                "title": topic.get("title"),
+                "dealer_name": topic["offer"].get("dealer_name"),
+                "score": calculate_score(topic),
+                "url": build_web_path(topic.get("web_path")),
+            }
+        )
+    return threads[:limit]
+
+
+def get_posts(post, count=5, tail=False, per_page=40):
+    """Retrieve posts from a thread.
+
+    Args:
+        post (str): either post id or full url
+        count (int, optional): Description
+
+    Yields:
+        list(dict): body, score, and user
+    """
+    if is_valid_url(post):
+        post_id = extract_post_id(post)
+    elif is_int(post):
+        post_id = post
+    else:
+        raise ValueError()
+
+    response = requests.get(
+        "{}/api/topics/{}/posts?per_page=40&page=1".format(API_BASE_URL, post_id)
+    )
+    total_posts = response.json().get("pager").get("total")
+    total_pages = response.json().get("pager").get("total_pages")
+
+    if count == 0:
+        pages = total_pages
+    if count > per_page:
+        if count > total_posts:
+            count = total_posts
+        pages = ceil(count / per_page)
+    else:
+        if tail:
+            pages = total_pages
+        else:
+            pages = 1
+
+    if tail:
+        start_page = ceil((total_posts + 1 - count) / per_page)
+        start_post = (total_posts + 1 - count) % per_page
+        if start_post == 0:
+            start_post = per_page
+    else:
+        start_page, start_post = 0, 0
+
+    # Go through as many pages as necessary
+    for page in range(start_page, pages + 1):
+        response = requests.get(
+            "{}/api/topics/{}/posts?per_page={}&page={}".format(
+                API_BASE_URL, post_id, get_safe_per_page(per_page), page
+            )
+        )
+
+        users = users_to_dict(response.json().get("users"))
+
+        _posts = response.json().get("posts")
+
+        # Determine which post to start with (for --tail)
+        if page == start_page and start_post != 0:
+            if tail:
+                _posts = _posts[start_post - 1 :]
+            else:
+                _posts = _posts[:start_post]
+
+        for _post in _posts:
+            count -= 1
+            if count < 0:
+                return
+            # Sometimes votes is null
+            if _post.get("votes") is not None:
+                calculated_score = calculate_score(_post)
+            else:
+                calculated_score = 0
+            yield {
+                "body": strip_html(_post.get("body")),
+                "score": calculated_score,
+                "user": users[_post.get("author_id")],
+            }
---
+++ packages/rfd/rfd-0.2.0.tar.gz/rfd-0.2.0/rfd/rfd_cli.py
@@ -0,0 +1,143 @@
+from __future__ import unicode_literals
+
+
+import logging
+import os
+import sys
+import click
+from colorama import init, Fore, Style
+from rfd.api import parse_threads, get_threads, get_posts
+from rfd.__version__ import version as current_version
+
+init()
+print()
+
+logging.getLogger()
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler())
+
+
+def get_version():
+    return "rfd " + current_version
+
+
+def get_terminal_width():
+    _, columns = os.popen("stty size", "r").read().split()
+    return int(columns)
+
+
+def get_vote_color(score):
+    if score > 0:
+        return Fore.GREEN + " [+" + str(score) + "] "
+    if score < 0:
+        return Fore.RED + " [" + str(score) + "] "
+    return Fore.BLUE + " [" + str(score) + "] "
+
+
+@click.group(invoke_without_command=True)
+@click.option("--version/--no-version", default=False)
+@click.pass_context
+def cli(ctx, version):
+    """Welcome to the RFD CLI. (RedFlagDeals.com)"""
+    if version:
+        click.echo(get_version())
+    elif not ctx.invoked_subcommand:
+        click.echo(ctx.get_help())
+
+
+@cli.command("version")
+def display_version():
+    click.echo(get_version())
+
+
+@cli.command(short_help="Displays posts in a specific thread.")
+@click.option(
+    "--head", default=0, help="Number of topics. Default is 0, for all topics"
+)
+@click.option(
+    "--tail",
+    default=0,
+    help="Number of topics. Default is disabled. This will override head.",
+)
+@click.argument("post_id")
+def posts(post_id, head, tail):
+    """Displays posts in a specific thread.
+
+    post_id can be a full url or post id only
+
+    Example:
+
+    \b
+    url: https://forums.redflagdeals.com/koodo-targeted-public-mobile-12-120-koodo-5gb-40-no-referrals-2173603
+    post_id: 2173603
+    """
+    if head < 0:
+        click.echo("Invalid head.")
+        sys.exit(1)
+
+    if tail < 0:
+        click.echo("Invalid tail.")
+        sys.exit(1)
+
+    # Tail overrides head
+    if tail > 0:
+        count = tail
+    else:
+        count = head
+
+    try:
+        click.echo("-" * get_terminal_width())
+        for post in get_posts(post=post_id, count=count, tail=tail > 0):
+            click.echo(
+                " -"
+                + get_vote_color(post.get("score"))
+                + Fore.RESET
+                + post.get("body")
+                + Fore.YELLOW
+                + " ({})".format(post.get("user"))
+            )
+            click.echo(Style.RESET_ALL)
+            click.echo("-" * get_terminal_width())
+    except ValueError:
+        click.echo("Invalid post id.")
+        sys.exit(1)
+    except AttributeError:
+        click.echo("AttributeError: RFD API did not return expected data.")
+
+
+@cli.command(short_help="Displays threads in the specified forum.")
+@click.option("--limit", default=10, help="Number of topics.")
+@click.argument("forum_id", default=9)
+def threads(limit, forum_id):
+    """Displays threads in the specified forum id. Defaults to 9.
+
+    Popular forum ids:
+
+    \b
+    9 \t hot deals
+    14 \t computer and electronics
+    15 \t offtopic
+    17 \t entertainment
+    18 \t food and drink
+    40 \t automotive
+    53 \t home and garden
+    67 \t fashion and apparel
+    74 \t shopping discussion
+    88 \t cell phones
+    """
+    _threads = parse_threads(get_threads(forum_id, limit), limit)
+    for i, thread in enumerate(_threads, 1):
+        click.echo(
+            " "
+            + str(i)
+            + "."
+            + get_vote_color(thread.get("score"))
+            + Fore.RESET
+            + "[%s] %s" % (thread.get("dealer_name"), thread.get("title"))
+        )
+        click.echo(Fore.BLUE + " {}".format(thread.get("url")))
+        click.echo(Style.RESET_ALL)
+
+
+if __name__ == "__main__":
+    cli()
