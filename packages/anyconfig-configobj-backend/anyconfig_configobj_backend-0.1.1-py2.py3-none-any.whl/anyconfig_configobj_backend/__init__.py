f98de790cd9784da255dcbe16caf1e9b8c65a56d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:05 +0000

    Add package anyconfig-configobj-backend/0.1.1/anyconfig_configobj_backend-0.1.1-py2.py3-none-any.whl

---
+++ packages/anyconfig-configobj-backend/anyconfig_configobj_backend-0.1.1-py2.py3-none-any.whl/anyconfig_configobj_backend/__init__.py
@@ -0,0 +1,10 @@
+"""anyconfig configobj backend module.
+"""
+from __future__ import absolute_import
+from .configobj import Parser
+
+__author__ = "Satoru SATOH"
+__version__ = "0.1.1"
+__all__ = ["Parser", ]
+
+# vim:sw=4:ts=4:et:
