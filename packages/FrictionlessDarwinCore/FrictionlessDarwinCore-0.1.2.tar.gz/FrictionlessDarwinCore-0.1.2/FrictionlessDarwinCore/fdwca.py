e9ac5d52aa6b2c028e561c6f336b78f12dd3ba05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:58 +0000

    Add package FrictionlessDarwinCore/0.1.2/FrictionlessDarwinCore-0.1.2.tar.gz

---
+++ packages/FrictionlessDarwinCore/FrictionlessDarwinCore-0.1.2.tar.gz/FrictionlessDarwinCore-0.1.2/FrictionlessDarwinCore/fdwca.py
@@ -0,0 +1,20 @@
+import click
+
+from FrictionlessDarwinCore import DwCArchive
+
+@click.command()
+@click.argument('dwca', type=str, required=True)
+@click.argument('outpath', type=click.Path(writable=True),required=True)
+@click.option('-f','--format', help='Output format', type=click.Choice(['json', 'md']))
+def cli(dwca, outpath, format):
+        da = DwCArchive(dwca)
+        da.infer()
+        if  format is None:
+            da.save(outpath)
+        if format == 'json':
+            da.to_json(outpath)
+        if format == 'md':
+            da.to_markdown(outpath)
+
+if __name__ == '__main__':
+    cli()
