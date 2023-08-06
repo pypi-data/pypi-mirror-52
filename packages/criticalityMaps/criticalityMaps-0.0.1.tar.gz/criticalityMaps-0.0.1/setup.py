c3aeab7a19d4b3bb09ce9bdbf5ea849b8f8b07b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:45 +0000

    Add package criticalityMaps/0.0.1/criticalityMaps-0.0.1.tar.gz

---
+++ packages/criticalityMaps/criticalityMaps-0.0.1.tar.gz/criticalityMaps-0.0.1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+from distutils.core import Extension
+
+DISTNAME = 'criticalityMaps'
+VERSION = '0.0.1'
+PACKAGES = find_packages()
+EXTENSIONS = []
+DESCRIPTION = 'WNTR utility for generating criticality maps'
+LONG_DESCRIPTION = open('README.md').read()
+AUTHOR = 'Patrick Hassett'
+MAINTAINER_EMAIL = 'pshassett@humboldt.edu'
+LICENSE = 'MIT'
+URL = 'https://github.com/pshassett/WNTRCriticalityMaps'
+INSTALL_REQUIRES = ['networkx',
+                    'pandas',
+                    'numpy',
+                    'scipy',
+                    'matplotlib',
+                    'plotly',
+                    'wntr',
+                    'pyyaml',
+                    'utm',
+                    'jinja2',
+                    'six']
+
+setuptools_kwargs = {
+    'zip_safe': False,
+    'install_requires': INSTALL_REQUIRES,
+    'scripts': [],
+    'include_package_data': True
+}
+
+setup(name=DISTNAME,
+      version=VERSION,
+      packages=PACKAGES,
+      ext_modules=EXTENSIONS,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      author=AUTHOR,
+      maintainer_email=MAINTAINER_EMAIL,
+      license=LICENSE,
+      url=URL,
+      **setuptools_kwargs)
+
