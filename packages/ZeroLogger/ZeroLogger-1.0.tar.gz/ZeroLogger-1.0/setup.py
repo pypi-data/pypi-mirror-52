99b849ad9360d26f8c6fccceaa3c08111fec258d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:10 +0000

    Add package ZeroLogger/1.0/ZeroLogger-1.0.tar.gz

---
+++ packages/ZeroLogger/ZeroLogger-1.0.tar.gz/ZeroLogger-1.0/setup.py
@@ -0,0 +1,8 @@
+import setuptools  
+ 
+setuptools.setup(  
+    name="ZeroLogger",  
+    version="1.0",  
+    author="ZéroTwó#5019",  
+    author_email="wolf.sf75@googlemail.com",  
+    description="My Own Logger Package to throw done/warn/error/critical(error + exit) messages")
