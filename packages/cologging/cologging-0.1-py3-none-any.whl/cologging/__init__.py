111a70afc6f190d9f58cd8258b7a4b2805155fcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:19 +0000

    Add package cologging/0.1/cologging-0.1-py3-none-any.whl

---
+++ packages/cologging/cologging-0.1-py3-none-any.whl/cologging/__init__.py
@@ -0,0 +1,16 @@
+from logging import Formatter, DEBUG, INFO, WARNING, ERROR
+from incolor import Color, incolor
+
+
+class ColorFormatter(Formatter):
+    level_colors = {
+        DEBUG: Color.blue,
+        INFO: Color.green,
+        WARNING: Color.yellow,
+        ERROR: Color.red
+    }
+
+    def format(self, record):
+        s = super().format(record)
+        color = ColorFormatter.level_colors.get(record.levelno, Color.white)
+        return incolor(color, s)
