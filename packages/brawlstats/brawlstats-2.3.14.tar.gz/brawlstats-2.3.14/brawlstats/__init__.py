d7ce6486cd89a56d92770acb90d39259a278aa80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:00 +0000

    Add package brawlstats/2.3.14/brawlstats-2.3.14.tar.gz

---
+++ packages/brawlstats/brawlstats-2.3.14.tar.gz/brawlstats-2.3.14/brawlstats/__init__.py
@@ -0,0 +1,15 @@
+from .core import Client
+from .errors import *
+from .models import *
+
+
+############
+# METADATA #
+############
+
+
+__version__ = 'v2.3.14'
+__title__ = 'brawlstats'
+__license__ = 'MIT'
+__author__ = 'SharpBit'
+__github__ = 'https://github.com/SharpBit/brawlstats'
