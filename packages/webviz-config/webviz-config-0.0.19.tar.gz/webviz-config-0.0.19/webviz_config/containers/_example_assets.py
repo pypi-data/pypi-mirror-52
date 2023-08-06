a98c4e2cdcf4392b8226aeee1bf6a5dc909e5280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:28 +0000

    Add package webviz-config/0.0.19/webviz-config-0.0.19.tar.gz

---
+++ packages/webviz-config/webviz-config-0.0.19.tar.gz/webviz-config-0.0.19/webviz_config/containers/_example_assets.py
@@ -0,0 +1,14 @@
+import dash_html_components as html
+from pathlib import Path
+from . import WebvizContainer
+from ..webviz_assets import webviz_assets
+
+
+class ExampleAssets(WebvizContainer):
+
+    def __init__(self, app, picture_path: Path):
+        self.asset_url = webviz_assets.add(picture_path)
+
+    @property
+    def layout(self):
+        return html.Img(src=self.asset_url)
