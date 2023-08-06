758fc9c189f7474c10f53958897ffc834e9841e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:55 +0000

    Add package pytypewriter/0.0.1/pytypewriter-0.0.1.tar.gz

---
+++ packages/pytypewriter/pytypewriter-0.0.1.tar.gz/pytypewriter-0.0.1/pytypewriter/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+.. currentmodule:: pytypewriter
+.. moduleauthor:: Pat Daburu <pat@daburu.net>
+
+This is a simple library to help you serialize Python type names and call them
+up again when you need them.
+"""
+from .types import InvalidTypeException, pycls, pyfqn
+from .version import __version__, __release__
