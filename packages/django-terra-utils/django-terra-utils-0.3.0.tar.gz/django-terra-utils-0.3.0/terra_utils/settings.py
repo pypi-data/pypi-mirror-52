d32397379db805256313e0b74a04d4579fa838b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:11 +0000

    Add package django-terra-utils/0.3.0/django-terra-utils-0.3.0.tar.gz

---
+++ packages/django-terra-utils/django-terra-utils-0.3.0.tar.gz/django-terra-utils-0.3.0/terra_utils/settings.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from .helpers import Choices
+
+
+STATES = Choices(
+    ('DRAFT', 100, 'Draft'),
+    ('SUBMITTED', 200, 'Submitted'),
+    ('ACCEPTED', 300, 'Accepted'),
+    ('REFUSED', -1, 'Refused'),
+    ('CANCELLED', -100, 'Cancelled'),
+    ('MISSING', 0, 'Missing'),
+)
+STATES.add_subset('MANUAL', (
+    'DRAFT',
+    'SUBMITTED',
+    'ACCEPTED',
+    'REFUSED',
+    'CANCELLED',
+))
+
+STATES = getattr(settings, 'STATES', STATES)
+
+TERRA_APPLIANCE_SETTINGS = getattr(settings, 'TERRA_APPLIANCE_SETTINGS', {})
