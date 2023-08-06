4d6ae5825594d3e3cf9c9e7596852c1ba9b51e55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:16 +0000

    Add package OpenVisus/1.3.68/OpenVisus-1.3.68-cp36-none-win_amd64.whl

---
+++ packages/OpenVisus/OpenVisus-1.3.68-cp36-none-win_amd64.whl/OpenVisus/Samples/python/Viewer.py
@@ -0,0 +1,114 @@
+
+import sys, os
+
+from OpenVisus       import *
+
+# IMPORTANT for WIndows
+# Mixing C++ Qt5 and PyQt5 won't work in Windows/DEBUG mode
+# because forcing the use of PyQt5 means to use only release libraries (example: Qt5Core.dll)
+# but I'm in need of the missing debug version (example: Qt5Cored.dll)
+# as you know, python (release) does not work with debugging versions, unless you recompile all from scratch
+
+# on windows rememeber to INSTALL and CONFIGURE
+
+
+from VisusGuiPy      import *
+from VisusGuiNodesPy import *
+from VisusAppKitPy   import *
+
+import PyQt5
+from   PyQt5.QtCore    import *
+from   PyQt5.QtWidgets import *
+from   PyQt5.QtGui     import *
+import PyQt5.sip
+
+# ///////////////////////////////////////////////////////////
+class MyWidget(QWidget):
+    
+	# __init__
+	def __init__(self):
+		QWidget.__init__(self)
+		self.checkbox = QCheckBox('Show title', self)
+		self.checkbox.move(20, 20)
+		self.checkbox.toggle()
+		self.setGeometry(300, 300, 250, 150)
+		self.setWindowTitle('QCheckBox')
+		self.show()
+
+
+# ///////////////////////////////////////////////////////////
+class MyPythonNode(PythonNode):
+
+	# __init__
+	def __init__(self):
+		PythonNode.__init__(self)
+		self.setName("MyPythonNode")
+		self.addInputPort("data")
+    
+	# getOsDependentTypeName
+	def getOsDependentTypeName(self):
+		return "MyPythonNode"
+
+	# glRender
+	def glRender(self, gl):
+		gl.pushFrustum()
+		gl.setHud()
+		gl.pushDepthTest(False)
+		gl.pushBlend(True)
+		
+		GL_QUADS=0x0007
+		mesh=GLMesh()
+		mesh.begin(GL_QUADS);
+		mesh.vertex( 0, 0)
+		mesh.vertex(50, 0)
+		mesh.vertex(50,50)
+		mesh.vertex( 0,50)
+		mesh.end();
+
+		obj=GLPhongObject()
+		obj.color=Color(255,255,0)
+		obj.mesh=mesh
+		obj.glRender(gl)
+		
+		gl.popBlend()
+		gl.popDepthTest()
+		gl.popFrustum()
+
+	# processInput
+	def processInput(self):
+		return PythonNode.processInput(self)
+
+  
+# ///////////////////////////////////////////////////////////
+if __name__ == '__main__':
+
+	SetCommandLine("__main__")
+	GuiModule.createApplication()
+	AppKitModule.attach()  
+
+	viewer=Viewer()
+	viewer.openFile("http://atlantis.sci.utah.edu/mod_visus?dataset=2kbit1") 
+
+	# example of adding a PyQt5 widget to C++ Qt
+	mywidget=MyWidget()
+	viewer.addDockWidget("MyWidget",ToCppQtWidget(PyQt5.sip.unwrapinstance(mywidget)))
+
+	# example of adding a python node to the dataflow
+	root=viewer.getRoot()
+	world_box=viewer.getWorldBounds()
+	
+	VISUS_REGISTER_NODE_CLASS("MyPythonNode")
+	pynode=MyPythonNode()
+	pynode.glSetRenderQueue(999)
+	pynode.setNodeBounds(Position(world_box))
+	viewer.addNode(root,pynode)
+
+	# pynode will get the data from the query
+	query_node=viewer.findNodeByName("Volume 1")
+	viewer.connectPorts(query_node,"data","data",pynode)
+	 
+	GuiModule.execApplication()
+	viewer=None  
+	AppKitModule.detach()
+	print("All done")
+	sys.exit(0)
---
+++ packages/OpenVisus/OpenVisus-1.3.68-cp36-none-win_amd64.whl/OpenVisus/include/XIdx/Visus/xidx_element.h
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) 2017 University of Utah
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef XIDX_PARSABLE_INTERFACE_H_
+#define XIDX_PARSABLE_INTERFACE_H_
+
+namespace Visus{
+
+class VISUS_XIDX_API Domain;
+class VISUS_XIDX_API DataSource;
+
+VISUS_XIDX_API String XIdxFormatString(const String fmt_str, ...);
+
+/////////////////////////////////////////////////////////////////////
+class VISUS_XIDX_API XIdxElement 
+{
+public:
+
+  //VISUS_XIDX_CLASS(XIdxElement)
+  VISUS_CLASS(XIdxElement)
+
+  virtual String getTypeName() {
+    return "XIdxElement";
+  }
+
+  String name;
+
+  //constructor
+  XIdxElement(String name_ = "") : name(name_) {
+  }
+
+  //destructor
+  virtual ~XIdxElement() {
+  }
+
+  //getParent
+  virtual XIdxElement* getParent() const {
+    return parent;
+  }
+
+  //hasChild
+  bool hasChild(XIdxElement* child) const {
+    return std::find(childs.begin(), childs.end(), child) != childs.end();
+  }
+
+  //addEdge
+  static void addEdge(XIdxElement* parent, XIdxElement* child) {
+    VisusAssert(parent && child && !parent->hasChild(child) && child->parent == nullptr);
+    parent->childs.push_back(child);
+    child->parent = parent;
+  }
+
+  //removeEdge
+  static void removeEdge(XIdxElement* parent, XIdxElement* child) {
+    VisusAssert(parent && child && parent->hasChild(child) && child->parent == parent);
+    Utils::remove(parent->childs, child);
+    child->parent = nullptr;
+  }
+
+  //getXPathPrefix
+  virtual String getXPathPrefix() {
+    return StringUtils::format()<<(getParent() ? getParent()->getXPathPrefix() : "/") << "/" << getTypeName();
+  }
+
+  //findChildWithName
+  XIdxElement* findChildWithName(String name)
+  {
+    for (auto child : this->childs) {
+      if (child->getTypeName() == name)
+        return child;
+    }
+    return nullptr;
+  }
+
+public:
+
+  //writeToObjectStream
+  virtual void writeToObjectStream(ObjectStream& ostream) {
+    if(name.size())
+      ostream.writeInline("Name", name);
+  }
+
+  //readFromObjectStream
+  virtual void readFromObjectStream(ObjectStream& istream) {
+    name = istream.readInline("Name", name);
+  }
+
+  //writeChilds
+  template <typename T>
+  void writeChild(ObjectStream& ostream,String name, T* child) {
+    if (!child) return;
+    ostream.pushContext(name);
+    child->writeToObjectStream(ostream);
+    ostream.popContext(name);
+  }
+
+  //readChilds
+  template <typename T>
+  VISUS_NEWOBJECT(T*) readChild(ObjectStream& istream, String name) {
+    
+    if (!istream.pushContext(name))
+      return nullptr;
+
+    auto ret = new T();
+    ret->readFromObjectStream(istream);
+    istream.popContext(name);
+    return ret;
+  }
+
+private:
+
+  XIdxElement*              parent = nullptr;
+  std::vector<XIdxElement*> childs;
+
+};
+
+} //namespace
+
+#endif //XIDX_PARSABLE_INTERFACE_H_
+
+
