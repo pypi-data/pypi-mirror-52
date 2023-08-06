dd3cbbbfd1ff93865cdc4dd40d6d386c4600cecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:22 +0000

    Add package scout-apm/2.5.0/scout_apm-2.5.0-cp34-cp34m-manylinux1_i686.whl

---
+++ packages/scout-apm/scout_apm-2.5.0-cp34-cp34m-manylinux1_i686.whl/scout_apm/sqlalchemy.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from sqlalchemy import event
+
+from scout_apm.core.tracked_request import TrackedRequest
+
+
+def before_cursor_execute(conn, cursor, statement, parameters, context, executemany):
+    tr = TrackedRequest.instance()
+    span = tr.start_span(operation="SQL/Query")
+    span.tag("db.statement", statement)
+
+
+def after_cursor_execute(conn, cursor, statement, parameters, context, executemany):
+    tr = TrackedRequest.instance()
+    span = tr.current_span()
+    if span is not None:
+        tr.callset.update(statement, 1, span.duration())
+        if tr.callset.should_capture_backtrace(statement):
+            span.capture_backtrace()
+    tr.stop_span()
+
+
+def instrument_sqlalchemy(engine):
+    if getattr(engine, "_scout_instrumented", False):
+        return
+    event.listen(engine, "before_cursor_execute", before_cursor_execute)
+    event.listen(engine, "after_cursor_execute", after_cursor_execute)
+    engine._scout_instrumented = True
