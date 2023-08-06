96296bb54bb0777fce659bf7c0d13b435134ab7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:54 +0000

    Add package python-consul2/0.0.3.dev0/python-consul2-0.0.3.dev0.tar.gz

---
+++ packages/python-consul2/python-consul2-0.0.3.dev0.tar.gz/python-consul2-0.0.3.dev0/consul/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.0.3-dev'
+
+from consul.base import ACLDisabled
+from consul.base import ACLPermissionDenied
+from consul.base import Check
+from consul.base import ConsulException
+from consul.base import NotFound
+from consul.base import Timeout
+from consul.std import Consul
