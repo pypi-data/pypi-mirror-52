0eec7a307c78861041cae1bba067b6c3aab27b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:24 +0000

    Add package lighttable/0.2.1/lighttable-0.2.1.tar.gz

---
+++ packages/lighttable/lighttable-0.2.1.tar.gz/lighttable-0.2.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup, Extension
+import pathlib
+# The directory containing this file
+
+setup(name='lighttable',
+      version='0.2.1',
+      description='light pivot feature engineer',
+      url='http://github.com/fe/lightpivot2',
+      author='fefly',
+      author_email='fefly@gmail.com',
+      license='MIT',
+      include_package_data=True,
+      packages=['lightpivot'],
+      zip_safe=False)
