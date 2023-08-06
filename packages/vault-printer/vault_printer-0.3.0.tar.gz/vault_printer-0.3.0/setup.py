a92b7e84ab268b68fff63fae28026eb942bad6b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:41 +0000

    Add package vault-printer/0.3.0/vault_printer-0.3.0.tar.gz

---
+++ packages/vault-printer/vault_printer-0.3.0.tar.gz/vault_printer-0.3.0/setup.py
@@ -0,0 +1,34 @@
+"""
+the setup to install vault_printer
+"""
+import setuptools
+
+from vault_printer import description, name, url, version
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name=name,
+    version=version,
+    author="Philip Molares",
+    author_email="philipmolares@yahoo.de",
+    description=description,
+    entry_points={
+        'console_scripts': [
+            "vault_printer = vault_printer.main:main"
+        ]
+    },
+    install_requires=["hvac", "hvac[parser]", "jinja2"],
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url=url,
+    packages=setuptools.find_packages(),
+    python_requires='>=3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ packages/vault-printer/vault_printer-0.3.0.tar.gz/vault_printer-0.3.0/vault_printer/__init__.py
@@ -0,0 +1,8 @@
+"""
+vault_printer module
+"""
+
+description = "A program to get all secrets from a vault servers kv_store for printing"  # pylint: disable=invalid-name
+name = "vault_printer"  # pylint: disable=invalid-name
+url = "https://github.com/DerMolly/vault_printer"  # pylint: disable=invalid-name
+version = "0.3.0"  # pylint: disable=invalid-name
