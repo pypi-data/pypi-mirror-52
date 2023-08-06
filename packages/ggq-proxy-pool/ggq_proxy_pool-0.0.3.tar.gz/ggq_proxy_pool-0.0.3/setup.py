974e29e59a66917c1f2d36e0d84340f2fb65de39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:41 +0000

    Add package ggq-proxy-pool/0.0.3/ggq_proxy_pool-0.0.3.tar.gz

---
+++ packages/ggq-proxy-pool/ggq_proxy_pool-0.0.3.tar.gz/ggq_proxy_pool-0.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r",encoding="utf8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ggq_proxy_pool",
+    version="0.0.3",
+    author="ggq",
+    author_email="942490944@qq.com",
+    description="爬虫代理IP池",
+    long_description=long_description,
+    long_description_context_type="text/markdown",
+    url="https://github.com/ggqshr/proxy_pool",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
