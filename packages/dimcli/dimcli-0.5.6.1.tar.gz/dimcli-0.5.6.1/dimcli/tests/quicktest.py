6d8fc3e0f48f7f7ba907419675b88cc7f55cf055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:01 +0000

    Add package dimcli/0.5.6.1/dimcli-0.5.6.1.tar.gz

---
+++ packages/dimcli/dimcli-0.5.6.1.tar.gz/dimcli-0.5.6.1/dimcli/tests/quicktest.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+"""
+simple test queries [for DEVELOPMENT  / not part of official tests]
+
+$ pip instal -e .
+$ dimcli_quicktest 1
+
+"""
+
+import click 
+from .. import *
+from ..shortcuts import *
+
+import requests
+
+@click.command()
+@click.argument('test_number', nargs=1)
+def main(test_number=1):
+    
+    
+    test_number = int(test_number)
+
+    if test_number == 1:
+        res = dslquery("search publications where year=2018 return icecreams")
+        print("NOW a BAD query: search publications where year=2018 return icecreams")
+        print("Query TOT results: ", res.total_count)
+        print("Query errors: ", res.errors_string)
+
+    elif test_number == 2:
+        res = dslquery("""search publications where journal.title="nature medicine" return publications[doi+FOR] limit 1000""")
+        print("Query results for standard query: ")
+        print(" ==> res['stats']: ", res['stats'])
+        print(" ==> len(res['publications']): ", len(res['publications']))
+        print(" ==> len([x for x in res.publications if 'FOR' in x]): ", len([x for x in res.publications if 'FOR' in x]))
+        print("Now Normalizing the FOR key...")
+        normalize_key("FOR", res.publications)
+        print(" ==> len([x for x in res.publications if 'FOR' in x]): ", len([x for x in res.publications if 'FOR' in x]))       
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
