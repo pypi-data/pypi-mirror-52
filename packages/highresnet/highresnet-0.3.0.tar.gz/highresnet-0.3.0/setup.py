74dcbde9e54cbf48076f567e8c1d217e41264f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:02 +0000

    Add package highresnet/0.3.0/highresnet-0.3.0.tar.gz

---
+++ packages/highresnet/highresnet-0.3.0.tar.gz/highresnet-0.3.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+setup(
+    name='highresnet',
+    version='0.3.0',
+    author='Fernando Perez-Garcia',
+    author_email='fernando.perezgarcia.17@ucl.ac.uk',
+    description='PyTorch implementation of HighResNet',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=requirements,
+    url='https://github.com/fepegar/highresnet',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+    ],
+    entry_points={
+        'console_scripts': [
+            'deepgif=highresnet.cli:main',
+        ],
+    },
+)
