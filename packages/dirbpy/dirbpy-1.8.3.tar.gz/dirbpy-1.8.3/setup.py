2d5ea5d93820284364904d690844930d0023da38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:30 +0000

    Add package dirbpy/1.8.3/dirbpy-1.8.3.tar.gz

---
+++ packages/dirbpy/dirbpy-1.8.3.tar.gz/dirbpy-1.8.3/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+from src._dirbpy import __version__
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='dirbpy',
+    version=__version__,
+    license="MIT license",
+    author='Marc-Olivier Bouchard',
+    author_email='mo.bouchard1997@gmail.com',
+    url='https://github.com/marcolivierbouch/dirbpy',
+    description='This is the new version of dirb in python.',
+    platforms=["unix", "linux", "osx"],
+    scripts=["bin/dirbpy"],
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    packages=[
+        "_dirbpy",
+    ],
+    package_dir={"": "src"},
+    install_requires=[
+        "argparse",
+        "requests",
+        "JSON-log-formatter",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    py_modules=["dirbpy"],
+    zip_safe=False,
+)
+
