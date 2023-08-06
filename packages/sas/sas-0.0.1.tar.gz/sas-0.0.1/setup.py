af5c6036dd832377d4a0dd0f2a75c647b21020a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:23 +0000

    Add package sas/0.0.1/sas-0.0.1.tar.gz

---
+++ packages/sas/sas-0.0.1.tar.gz/sas-0.0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(name='sas',
+      version='0.0.1',
+      description='Python split annotations library',
+      author='Shoumik Palkar',
+      packages=setuptools.find_packages(),
+      author_email='shoumik@cs.stanford.edu',
+      url='https://www.github.com/weld-project/split-annotations',
+      python_requires='>=3.6',
+     )
