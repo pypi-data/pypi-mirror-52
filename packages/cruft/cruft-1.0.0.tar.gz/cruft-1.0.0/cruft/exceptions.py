9f307c86c63d679e15fbab6025b012fb06c8ebb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:57 +0000

    Add package cruft/1.0.0/cruft-1.0.0.tar.gz

---
+++ packages/cruft/cruft-1.0.0.tar.gz/cruft-1.0.0/cruft/exceptions.py
@@ -0,0 +1,29 @@
+"""Contains all custom exceptions raised by cruft"""
+
+
+class CruftError(Exception):
+    """The base exception for any error originating from the cruft project"""
+
+
+class UnableToFindCookiecutterTemplate(CruftError):
+    """Raised when Cruft is unable to find a cookiecutter template"""
+
+    def __init__(self, directory: str):
+        super().__init__(self, f"Was unable to locate a Cookiecutter template in `{directory}` !")
+        self.directory = directory
+
+
+class NoCruftFound(CruftError):
+    """Raised when no .cruft.json state is found in the current directory"""
+
+    def __init__(self, directory: str):
+        super().__init__(
+            self, f"Was unable to locate a `.cruft.json` state file in `{directory}` !"
+        )
+        self.directory = directory
+
+
+class InvalidCookiecutterRepository(CruftError):
+    """Raised when an invalid cookiecutter repository is provided"""
+
+    pass
