603fd26cbedaa28c95ed66ec7d36f0c5ce24de7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:06 +0000

    Add package autohooks-plugin-pylint/1.1.1/autohooks-plugin-pylint-1.1.1.tar.gz

---
+++ packages/autohooks-plugin-pylint/autohooks-plugin-pylint-1.1.1.tar.gz/autohooks-plugin-pylint-1.1.1/CHANGELOG.md
@@ -0,0 +1,22 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+### Fixed
+
+* Don't run pylint if not files have to validates [#4](https://github.com/greenbone/autohooks-plugin-pylint/pull/4)
+
+## [1.1.0] - 2019-03-28
+
+### Changed
+
+* Changed git repository location to https://github.com/greenbone/autohooks-plugin-pylint
+* Allow to configure files to be linted [#1](https://github.com/greenbone/autohooks-plugin-pylint/pull/1)
+* Use new autohooks status API to print messages [#1](https://github.com/greenbone/autohooks-plugin-pylint/pull/1)
+
+[Unreleased]: https://github.com/greenbone/autohooks-plugin-pylint/compare/v1.1.0...HEAD
+[1.1.0]: https://github.com/greenbone/autohooks-plugin-pylint/compare/v1.0.0...v1.1.0
---
+++ packages/autohooks-plugin-pylint/autohooks-plugin-pylint-1.1.1.tar.gz/autohooks-plugin-pylint-1.1.1/autohooks/plugins/pylint/pylint.py
@@ -0,0 +1,78 @@
+# Copyright (C) 2019 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import subprocess
+
+from autohooks.api import out, ok, fail
+from autohooks.api.path import match
+from autohooks.api.git import get_staged_status, stash_unstaged_changes
+
+DEFAULT_INCLUDE = ('*.py',)
+
+
+def check_pylint_installed():
+    try:
+        import pylint
+    except ImportError:
+        raise Exception(
+            'Could not find pylint. Please add pylint to your python '
+            'environment'
+        )
+
+
+def get_pylint_config(config):
+    return config.get('tool').get('autohooks').get('plugins').get('pylint')
+
+
+def get_include_from_config(config):
+    if not config:
+        return DEFAULT_INCLUDE
+
+    pylint_config = get_pylint_config(config)
+    include = pylint_config.get_value('include', DEFAULT_INCLUDE)
+
+    if isinstance(include, str):
+        return [include]
+
+    return include
+
+
+def precommit(config=None, **kwargs):
+    out('Running pylint pre-commit hook')
+
+    check_pylint_installed()
+
+    include = get_include_from_config(config)
+    files = [f for f in get_staged_status() if match(f.path, include)]
+
+    if not files:
+        ok('No files to lint')
+        return 0
+
+    with stash_unstaged_changes(files):
+        args = ['pylint']
+        args.extend([str(f.absolute_path()) for f in files])
+
+        status = subprocess.call(args)
+        str_files = ', '.join([str(f.path) for f in files])
+
+        if status:
+            fail('Linting error(s) found in {}'.format(str_files))
+        else:
+            ok('Linting {} was successful'.format(str_files))
+
+        return status
