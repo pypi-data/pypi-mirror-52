1e93058ea4c00b125ccd57764fcb61c09fad9435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:43 +0000

    Add package secure-graphene/0.1.2/secure-graphene-0.1.2.tar.gz

---
+++ packages/secure-graphene/secure-graphene-0.1.2.tar.gz/secure-graphene-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, Extension
+  
+setup(
+  name = 'secure-graphene',         
+  packages = ['secure_graphene'],  
+  version = '0.1.2',      
+  license='MIT',        
+  description = 'Python library that assists you in securing your GraphQL API against malicious queries',   
+  author = 'Zachary Manesiotis',        
+  author_email = 'zack.manesiotis@gmail.com',    
+  url = 'https://github.com/manesioz/secure-graphene',   
+  download_url = 'https://github.com/manesioz/secure-graphene/archive/v0.1.2.tar.gz',    
+  keywords = ['graphql', 'python', 'security', 'graphene', 'api'],   
+  install_requires=[
+          'graphql-core',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
