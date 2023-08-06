2afccda936e738371f2d8858df3a56413da15399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:22 +0000

    Add package LDB-Django-Common/0.0.11/LDB_Django_Common-0.0.11.tar.gz

---
+++ packages/LDB-Django-Common/LDB_Django_Common-0.0.11.tar.gz/LDB_Django_Common-0.0.11/ldb/django/contrib/auth/views.py
@@ -0,0 +1,10 @@
+from ldb.django.contrib.auth.mixins import AuthorizedMixin
+
+from django.views.generic import (ListView, DetailView, CreateView, UpdateView,
+                                  DeleteView)
+
+class AuthorizedListView(AuthorizedMixin, ListView): pass
+class AuthorizedDetailView(AuthorizedMixin, DetailView): pass
+class AuthorizedCreateView(AuthorizedMixin, CreateView): pass
+class AuthorizedUpdateView(AuthorizedMixin, UpdateView): pass
+class AuthorizedDeleteView(AuthorizedMixin, DeleteView): pass
