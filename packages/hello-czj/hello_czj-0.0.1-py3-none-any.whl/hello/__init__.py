d088f8e2bc61c004c299b35ba2d23f5d9b754fa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:59 +0000

    Add package hello-czj/0.0.1/hello_czj-0.0.1-py3-none-any.whl

---
+++ packages/hello-czj/hello_czj-0.0.1-py3-none-any.whl/hello/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Chen Zhaojin
+# @Date  : 19-9-18
+# @Desc  : Life is Short, I use Python!
+# ----------------------------------------------------------------------------------------------------------------------
+
+from hello.hello import hi
+
---
+++ packages/hello-czj/hello_czj-0.0.1-py3-none-any.whl/hello/hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Chen Zhaojin
+# @Date  : 19-9-18
+# @Desc  : Life is Short, I use Python!
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def hi():
+    return "hi, setuptool!"
+
