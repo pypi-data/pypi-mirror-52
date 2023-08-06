3c9975e4fd0db2b030b08c91d1c6f22341337ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:23 +0000

    Add package aws-oidc-login/0.1.0/aws-oidc-login-0.1.0.tar.gz

---
+++ packages/aws-oidc-login/aws-oidc-login-0.1.0.tar.gz/aws-oidc-login-0.1.0/setup.py
@@ -0,0 +1,37 @@
+# coding=utf8
+
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(name='aws-oidc-login',
+      version='0.1.0',
+      description='CLI login to AWS using OpenID Connect',
+      long_description=README,
+      long_description_content_type="text/markdown",
+      url='http://github.com/NitorCreations/aws-oidc-login',
+      download_url='https://github.com/NitorCreations/aws-oidc-login',
+      author='Mika Majakorpi',
+      author_email='mika.majakorpi@nitor.com',
+      license='Apache 2.0',
+      packages=['login'],
+      include_package_data=True,
+      scripts=[],
+      entry_points={
+          'console_scripts': ['aol=login.aws_oidc_login:aws_oidc_login'],
+      },
+      setup_requires=['pytest-runner', 'wheel'],
+      install_requires=[
+          'boto3==1.9.208',
+          'requests==2.22.0',
+          'argparse==1.4.0'],
+      tests_require=[
+          'pytest==4.6.5',
+          'pytest-mock==1.10.4',
+          'pytest-cov==2.7.1',
+          'requests-mock==1.6.0',
+          'pytest-runner'
+      ],
+      test_suite='tests')
