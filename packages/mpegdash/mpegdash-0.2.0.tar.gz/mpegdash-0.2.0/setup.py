7b39d336115a04d623a324c731d6393b024de928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:31 +0000

    Add package mpegdash/0.2.0/mpegdash-0.2.0.tar.gz

---
+++ packages/mpegdash/mpegdash-0.2.0.tar.gz/mpegdash-0.2.0/setup.py
@@ -0,0 +1,33 @@
+from os.path import dirname, abspath, join, exists
+from setuptools import setup
+
+long_description = None
+if exists("README.md"):
+    long_description = open("README.md").read()
+
+setup(
+  name="mpegdash",
+  packages=["mpegdash"],
+  description="MPEG-DASH MPD(Media Presentation Description) Parser",
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author="sangwonl",
+  author_email="gamzabaw@gmail.com",
+  version="0.2.0",
+  license="MIT",
+  zip_safe=False,
+  include_package_data=True,
+  install_requires=["future"],
+  url="https://github.com/sangwonl/python-mpegdash",
+  tests_require=["unittest2"],
+  test_suite="tests.my_module_suite",
+  classifiers = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Environment :: Other Environment",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+  ],
+)
