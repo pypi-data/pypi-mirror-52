b7efae440199895394f999fc2ca0e310fe577678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:59 +0000

    Add package quetzal-client/0.3.1/quetzal-client-0.3.1.tar.gz

---
+++ packages/quetzal-client/quetzal-client-0.3.1.tar.gz/quetzal-client-0.3.1/quetzal/client/config.py
@@ -0,0 +1,17 @@
+import os
+
+import quetzal.openapi_client.configuration
+
+
+class Configuration(quetzal.openapi_client.configuration.Configuration,
+                    metaclass=quetzal.openapi_client.configuration.TypeWithDefault):
+    # Use this later for particular extensions/modifications on the API
+    # configuration object
+
+    def __init__(self):
+        super().__init__()
+        if 'QUETZAL_URL' in os.environ:
+            self.host = os.getenv('QUETZAL_URL', '')
+        self.username = os.getenv('QUETZAL_USER', '')
+        self.password = os.getenv('QUETZAL_PASSWORD', '')
+
