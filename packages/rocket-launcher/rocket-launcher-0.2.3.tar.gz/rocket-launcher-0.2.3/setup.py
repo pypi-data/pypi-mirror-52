3e805e4891fe1b4cfad3e26f0a39fe6c5a5e7b5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:39 +0000

    Add package rocket-launcher/0.2.3/rocket-launcher-0.2.3.tar.gz

---
+++ packages/rocket-launcher/rocket-launcher-0.2.3.tar.gz/rocket-launcher-0.2.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+setup(
+    name='rocket-launcher',
+    version='0.2.3',
+    author='who',
+    author_email='who@whooami.me',
+    scripts=[
+        'rocket-launcher',
+        'rockets/bin/configure-file-browser',
+        'rockets/bin/configure-multiplexer',
+        'rockets/bin/configure-version-controller',
+        'rockets/bin/configure-secure_shell',
+        'rockets/bin/set-terminal-fish',
+        'rockets/bin/set-text-editor',
+    ],
+    packages=['rockets'],
+)
