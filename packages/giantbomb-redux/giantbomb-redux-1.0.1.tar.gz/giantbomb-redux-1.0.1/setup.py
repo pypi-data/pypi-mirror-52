ef9b7bac7cc309a37ec47eeb349b65914ebdfdf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:15 +0000

    Add package giantbomb-redux/1.0.1/giantbomb-redux-1.0.1.tar.gz

---
+++ packages/giantbomb-redux/giantbomb-redux-1.0.1.tar.gz/giantbomb-redux-1.0.1/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os, sys
+from setuptools import setup
+from giantbomb import giantbomb
+
+def read(fname):
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name = "giantbomb-redux",
+    version = giantbomb.__version__,
+    author = giantbomb.__author__,
+    author_email = "hidde@hiddejansen.com",
+    description = ("A Python wrapper for the Giantbomb API."),
+    license = "MIT",
+    keywords = "giantbomb api wrapper",
+    url = "https://github.com/jansenh/GiantBomb-redux",
+    packages=['giantbomb'],
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=['requests>=2.22.0', 'dacite>=1.0.2'],
+    python_requires=">=3.7"
+)
