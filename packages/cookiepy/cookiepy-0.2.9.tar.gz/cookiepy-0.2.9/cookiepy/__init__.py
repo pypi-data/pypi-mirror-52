6f7a11d6127f67789f8a008fcaeae392903ed33e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:08 +0000

    Add package cookiepy/0.2.9/cookiepy-0.2.9.tar.gz

---
+++ packages/cookiepy/cookiepy-0.2.9.tar.gz/cookiepy-0.2.9/cookiepy/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from .foo import *
+
+"""Top-level package for cookiepy."""
+
+__author__ = """Nikos Koumbakis"""
+__email__ = 'n.koumbakis@gmail.com'
+__version__ = '0.1.0'
