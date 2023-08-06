177818df8fc54b48776ec01cf6c72a1a5a15d4f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:06 +0000

    Add package AIMaker/1.0.12/AIMaker-1.0.12.tar.gz

---
+++ packages/AIMaker/AIMaker-1.0.12.tar.gz/AIMaker-1.0.12/setup.py
@@ -0,0 +1,18 @@
+# coding:utf-8
+
+from setuptools import setup
+
+version_file = open("VERSION", "r")
+VERSION = version_file.readline()
+version_file.close()
+
+setup(
+    name='AIMaker',
+    version='1.0.12',
+    description='Return training result to api server',
+    author='chris',
+    author_email='chris5_lin@asus.com',
+    packages=['AIMaker'],
+    license='MIT',
+    zip_safe=False
+)
