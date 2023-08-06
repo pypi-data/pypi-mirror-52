8c204a3e9b14e55bbdd53388f54a1c471ad706a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:17 +0000

    Add package cppyy-cling/6.18.2.2/cppyy_cling-6.18.2.2-py2.py3-none-macosx_10_11_x86_64.whl

---
+++ packages/cppyy-cling/cppyy_cling-6.18.2.2-py2.py3-none-macosx_10_11_x86_64.whl/cppyy_backend/cmake/ROOTConfig-targets-relwithdebinfo.cmake
@@ -0,0 +1,237 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "RelWithDebInfo".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "ROOT::vectorDict" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::vectorDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::vectorDict PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libvectorDict.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libvectorDict.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::vectorDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::vectorDict "${_IMPORT_PREFIX}/lib/libvectorDict.so" )
+
+# Import target "ROOT::listDict" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::listDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::listDict PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/liblistDict.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/liblistDict.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::listDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::listDict "${_IMPORT_PREFIX}/lib/liblistDict.so" )
+
+# Import target "ROOT::forward_listDict" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::forward_listDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::forward_listDict PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libforward_listDict.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libforward_listDict.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::forward_listDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::forward_listDict "${_IMPORT_PREFIX}/lib/libforward_listDict.so" )
+
+# Import target "ROOT::dequeDict" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::dequeDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::dequeDict PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libdequeDict.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libdequeDict.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::dequeDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::dequeDict "${_IMPORT_PREFIX}/lib/libdequeDict.so" )
+
+# Import target "ROOT::mapDict" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::mapDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::mapDict PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libmapDict.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libmapDict.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::mapDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::mapDict "${_IMPORT_PREFIX}/lib/libmapDict.so" )
+
+# Import target "ROOT::map2Dict" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::map2Dict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::map2Dict PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libmap2Dict.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libmap2Dict.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::map2Dict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::map2Dict "${_IMPORT_PREFIX}/lib/libmap2Dict.so" )
+
+# Import target "ROOT::unordered_mapDict" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::unordered_mapDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::unordered_mapDict PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libunordered_mapDict.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libunordered_mapDict.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::unordered_mapDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::unordered_mapDict "${_IMPORT_PREFIX}/lib/libunordered_mapDict.so" )
+
+# Import target "ROOT::multimapDict" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::multimapDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::multimapDict PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libmultimapDict.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libmultimapDict.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::multimapDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::multimapDict "${_IMPORT_PREFIX}/lib/libmultimapDict.so" )
+
+# Import target "ROOT::multimap2Dict" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::multimap2Dict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::multimap2Dict PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libmultimap2Dict.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libmultimap2Dict.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::multimap2Dict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::multimap2Dict "${_IMPORT_PREFIX}/lib/libmultimap2Dict.so" )
+
+# Import target "ROOT::unordered_multimapDict" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::unordered_multimapDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::unordered_multimapDict PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libunordered_multimapDict.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libunordered_multimapDict.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::unordered_multimapDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::unordered_multimapDict "${_IMPORT_PREFIX}/lib/libunordered_multimapDict.so" )
+
+# Import target "ROOT::setDict" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::setDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::setDict PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libsetDict.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libsetDict.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::setDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::setDict "${_IMPORT_PREFIX}/lib/libsetDict.so" )
+
+# Import target "ROOT::unordered_setDict" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::unordered_setDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::unordered_setDict PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libunordered_setDict.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libunordered_setDict.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::unordered_setDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::unordered_setDict "${_IMPORT_PREFIX}/lib/libunordered_setDict.so" )
+
+# Import target "ROOT::multisetDict" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::multisetDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::multisetDict PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libmultisetDict.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libmultisetDict.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::multisetDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::multisetDict "${_IMPORT_PREFIX}/lib/libmultisetDict.so" )
+
+# Import target "ROOT::unordered_multisetDict" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::unordered_multisetDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::unordered_multisetDict PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libunordered_multisetDict.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libunordered_multisetDict.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::unordered_multisetDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::unordered_multisetDict "${_IMPORT_PREFIX}/lib/libunordered_multisetDict.so" )
+
+# Import target "ROOT::complexDict" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::complexDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::complexDict PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libcomplexDict.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libcomplexDict.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::complexDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::complexDict "${_IMPORT_PREFIX}/lib/libcomplexDict.so" )
+
+# Import target "ROOT::valarrayDict" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::valarrayDict APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::valarrayDict PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libvalarrayDict.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libvalarrayDict.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::valarrayDict )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::valarrayDict "${_IMPORT_PREFIX}/lib/libvalarrayDict.so" )
+
+# Import target "ROOT::Cling" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::Cling APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::Cling PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libCling.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libCling.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::Cling )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::Cling "${_IMPORT_PREFIX}/lib/libCling.so" )
+
+# Import target "ROOT::Thread" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::Thread APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::Thread PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libThread.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libThread.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::Thread )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::Thread "${_IMPORT_PREFIX}/lib/libThread.so" )
+
+# Import target "ROOT::Core" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::Core APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::Core PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libCore.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libCore.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::Core )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::Core "${_IMPORT_PREFIX}/lib/libCore.so" )
+
+# Import target "ROOT::rmkdepend" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::rmkdepend APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::rmkdepend PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/bin/rmkdepend"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::rmkdepend )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::rmkdepend "${_IMPORT_PREFIX}/bin/rmkdepend" )
+
+# Import target "ROOT::MathCore" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::MathCore APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::MathCore PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libMathCore.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libMathCore.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::MathCore )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::MathCore "${_IMPORT_PREFIX}/lib/libMathCore.so" )
+
+# Import target "ROOT::RIO" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::RIO APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::RIO PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libRIO.so"
+  IMPORTED_SONAME_RELWITHDEBINFO "@rpath/libRIO.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::RIO )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::RIO "${_IMPORT_PREFIX}/lib/libRIO.so" )
+
+# Import target "ROOT::rootcling" for configuration "RelWithDebInfo"
+set_property(TARGET ROOT::rootcling APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(ROOT::rootcling PROPERTIES
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/bin/rootcling"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ROOT::rootcling )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ROOT::rootcling "${_IMPORT_PREFIX}/bin/rootcling" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
