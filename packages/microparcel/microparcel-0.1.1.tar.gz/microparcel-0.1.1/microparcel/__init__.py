1d2cc3a3ef0de67c541de4faf0ae1b28baae8da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:59 +0000

    Add package microparcel/0.1.1/microparcel-0.1.1.tar.gz

---
+++ packages/microparcel/microparcel-0.1.1.tar.gz/microparcel-0.1.1/microparcel/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for microparcel."""
+
+__author__ = """Vivien Henry"""
+__email__ = 'vivien.henry@outlook.fr'
+__version__ = '0.1.1'
+
+from .microparcel import Message, Frame, make_parser_cls
