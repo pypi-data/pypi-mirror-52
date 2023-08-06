86b227e3a9b4b9cf4f0d4c8bb1579c0eeada7ebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:48 +0000

    Add package aetherling/0.0.1/aetherling-0.0.1.tar.gz

---
+++ packages/aetherling/aetherling-0.0.1.tar.gz/aetherling-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+DESCRIPTION = """\
+A Python package for testing hardware (part of the magma ecosystem)\
+"""
+
+setup(
+    name='aetherling',
+    version='0.0.1',
+    description='Aetherling: a DSL for compiling data-parallel programs to hardware accelerators.',
+    packages=["aetherling"],
+    install_requires=[
+        "magma-lang>=1.0.18",
+        "coreir",
+        "fault",
+        "mantle",
+        "numpy",
+        "Pillow",
+        "bitarray",
+        "colorlog",
+        "pytest"
+    ],
+    license='BSD License',
+    url='https://github.com/David-Durst/aetherling',
+    author='David Durst',
+    python_requires='>=3.7',
+    author_email='davidbdurst@gmail.com',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown"
+)
