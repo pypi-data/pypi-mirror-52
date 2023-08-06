30447a2c62512fc03c40851b8eba24063497a88e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:16 +0000

    Add package accessibility-toolbar/1.0.16/accessibility_toolbar-1.0.16.tar.gz

---
+++ packages/accessibility-toolbar/accessibility_toolbar-1.0.16.tar.gz/accessibility_toolbar-1.0.16/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README-Pypi.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="accessibility_toolbar",
+    version="1.0.16",
+    author="MSJUPYTER",
+    author_email="joshua.zeltser@gmail.com",
+    description="An Accessibility Toolbar for Jupyter Notebooks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/uclixnjupyternbaccessibility/accessibility_toolbar",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+)
