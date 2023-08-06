bf7e1eab813e9367a1776dce708a5a353fcfd5a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:52 +0000

    Add package gems/0.4.0/gems-0.4.0.tar.gz

---
+++ packages/gems/gems-0.4.0.tar.gz/gems-0.4.0/tests/test_metaclasses.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#
+# Datatype testing
+#
+# ------------------------------------------------
+
+
+# imports
+# -------
+import unittest
+from gems import DocRequire
+import six
+
+
+# tests
+# -----
+class TestDocRequire(unittest.TestCase):
+
+    def test_docrequire(self):
+        # raises AssertionError when evaled
+        with self.assertRaises(AssertionError):
+
+            @six.add_metaclass(DocRequire)
+            class A(object):
+                def method(self):
+                    return
+
+        # shouldn't raise an AssertionError
+        with self.assertRaises(TypeError):
+
+            @six.add_metaclass(DocRequire)  ## noqa
+            class A(object):
+                def method(self):
+                    """ okay """
+                    return
+
+            raise TypeError
+        return
