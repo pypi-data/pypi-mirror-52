14f5ce605ee098c25e1390cb1395d98eb752032a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:58 +0000

    Add package puzzelegg/2.0/puzzelegg-2.0.tar.gz

---
+++ packages/puzzelegg/puzzelegg-2.0.tar.gz/puzzelegg-2.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'puzzelegg',
+    description = 'python wheel to help with the puzzel.',
+    version = '2.0',
+    url = '',
+    author = 'alice bom'
+)
