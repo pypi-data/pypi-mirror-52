ffff44ea95d584af4c199b19df4aa1e20458bc87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:13 +0000

    Add package nester_2833/1.2.0/nester-2833-1.2.0.tar.gz

---
+++ packages/nester_2833/nester-2833-1.2.0.tar.gz/nester-2833-1.2.0/nester.py
@@ -0,0 +1,9 @@
+def print_lol (the_list,indent = False, the_level=0):
+    for each_item in the_list:
+        if isinstance(the_list,list):
+            print_lol(each_item,indent,the_level+1)
+        else:
+        		if indent:
+        			for tab in range():
+        				print("\t",end = '')
+            print(each_item)
