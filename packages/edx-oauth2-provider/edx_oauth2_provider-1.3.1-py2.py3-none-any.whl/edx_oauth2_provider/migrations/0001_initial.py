362bd24392f8fd3174ebd53502632688a801d547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:00 +0000

    Add package edx-oauth2-provider/1.3.1/edx_oauth2_provider-1.3.1-py2.py3-none-any.whl

---
+++ packages/edx-oauth2-provider/edx_oauth2_provider-1.3.1-py2.py3-none-any.whl/edx_oauth2_provider/migrations/0001_initial.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from __future__ import absolute_import
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('oauth2', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='TrustedClient',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('client', models.ForeignKey(to='oauth2.Client')),
+            ],
+            options={
+                'db_table': 'oauth2_provider_trustedclient',
+            },
+        ),
+    ]
