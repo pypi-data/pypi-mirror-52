a69faca86c5b54336647161f45cd94ac82bb67ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:53 +0000

    Add package ankilist/0.1.4/ankilist-0.1.4.tar.gz

---
+++ packages/ankilist/ankilist-0.1.4.tar.gz/ankilist-0.1.4/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+modules = \
+['ankilist']
+entry_points = \
+{'console_scripts': ['ankilist = ankilist:main']}
+
+setup_kwargs = {
+    'name': 'ankilist',
+    'version': '0.1.4',
+    'description': 'Lite google translate console client and anki list generator',
+    'long_description': None,
+    'author': 'VladislavNekto',
+    'author_email': 'vladislav.nekto.dev@gmail.com',
+    'url': None,
+    'py_modules': modules,
+    'entry_points': entry_points,
+    'python_requires': '==3.7.4',
+}
+
+
+setup(**setup_kwargs)
