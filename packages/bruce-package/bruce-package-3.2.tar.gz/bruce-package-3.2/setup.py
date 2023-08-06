42be476c7e28d6e53ebbb25da9e06ca1c8bdcf82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:43 +0000

    Add package bruce-package/3.2/bruce-package-3.2.tar.gz

---
+++ packages/bruce-package/bruce-package-3.2.tar.gz/bruce-package-3.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, Extension
+
+setup(
+    name = 'bruce-package',
+    version = '3.2',
+    description = 'GPU-accelerated binary star model',
+    url = None,
+    author = 'Samuel Gill et al',
+    author_email = 'samuel.gill@warwick.ac.uk',
+    license = 'GNU',
+    packages=['bruce','bruce/binarystar'],
+    scripts=['Utils/ngtsfit/ngtsfit',
+             'Utils/lcbin/lcbin',
+             'Utils/tessget/tessget',
+             'Utils/tessget/tessquery',
+             'Utils/tls/tls',
+             'Utils/tls/findtransit',
+             'Utils/tls/PAOtls',
+             'Utils/prewhiten/prewhiten',
+             'Utils/mbls/mbls',
+             'Utils/predict/predict',
+             'Utils/predict/predictedges',
+             'Utils/analyse/aperturesearch',
+             'Utils/analyse/templatematch',
+             'Utils/analyse/analyse',
+             'Utils/analyse/plot_nights',
+             'Utils/massfunc/massfunc',
+             'Utils/modulation/modulation',
+             'Utils/monofind/monofind',
+             'Utils/quickphot/quickphot',
+             'Utils/tic2wasp/tic2wasp',
+             'Utils/fitpipe/fitpipe'],
+
+    install_requires=['numba']
+)
