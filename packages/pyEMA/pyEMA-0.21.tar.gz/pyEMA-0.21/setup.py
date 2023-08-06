abaf998b2ea5926e8ea884b439aa73a9a2e51080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:59 +0000

    Add package pyEMA/0.21/pyEMA-0.21.tar.gz

---
+++ packages/pyEMA/pyEMA-0.21.tar.gz/pyEMA-0.21/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+desc = """\
+Experimental and operational modal analysis
+===========================================
+This module supports experimental and operational modal analysis. 
+
+For the showcase see: `https://github.com/ladisk/pyEMA/ <https://github.com/ladisk/pyEMA/blob/master/pyEMA%20Showcase.ipynb>`_
+
+Documentation is located here: `https://pyema.readthedocs.io/ <https://pyema.readthedocs.io/en/latest/>`_
+"""
+
+from setuptools import setup
+setup(name='pyEMA',
+      version='0.21',
+      author='Klemen Zaletelj, Tomaž Bregar, Domen Gorjup, Janko Slavič',
+      author_email='janko.slavic@fs.uni-lj.si, ladisk@gmail.com',
+      description='Experimental and operational modal analysis.',
+      url='https://github.com/ladisk/pyEMA',
+      packages=['pyEMA'],
+      long_description=desc,
+      install_requires=['numpy>=1.15.4', 'tqdm', 'scipy>=1.1.0', 'matplotlib>=3.0.0']
+      )
