3d85bfb0913a2637db998416debc7a0d12e70aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:42 +0000

    Add package pyglottolog/2.2.1/pyglottolog-2.2.1.tar.gz

---
+++ packages/pyglottolog/pyglottolog-2.2.1.tar.gz/pyglottolog-2.2.1/src/pyglottolog/__init__.py
@@ -0,0 +1,7 @@
+# pyglottolog
+
+from .api import Glottolog
+
+__all__ = ['Glottolog']
+
+__version__ = '2.2.1'
---
+++ packages/pyglottolog/pyglottolog-2.2.1.tar.gz/pyglottolog-2.2.1/tests/test_monster.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+
+
+from pyglottolog import monster
+
+
+def test_main(capsys, api_copy):
+    monster.compile(api_copy)
+    out, _ = capsys.readouterr()
+    assert len(out.splitlines()) == 50
+    assert '2 splitted' in out
+    assert '2 merged' in out
