7e0d28acefb71dc84cb00eb6881b4c434cd01fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:28 +0000

    Add package mymathpkg/0.1/mymathpkg-0.1.tar.gz

---
+++ packages/mymathpkg/mymathpkg-0.1.tar.gz/mymathpkg-0.1/mymathpkg/adv/sqrt.py
@@ -0,0 +1,5 @@
+import math
+
+
+def squareroot(n):
+    return math.sqrt(n)
---
+++ packages/mymathpkg/mymathpkg-0.1.tar.gz/mymathpkg-0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='mymathpkg',
+    version='0.1',
+    description='A math package',
+    author='Marziya',
+    packages=['mymathpkg', 'mymathpkg.adv'],
+    author_email="shaikhmarzi05@gmail.com",
+    url="https://github.com/MarziyaFatema/demo",
+)
+
+
