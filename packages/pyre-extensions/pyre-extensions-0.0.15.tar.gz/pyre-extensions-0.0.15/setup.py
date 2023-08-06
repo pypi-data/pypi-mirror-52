138f77c4788f6e81ea590c15fa5ff4d170dc4402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:05 +0000

    Add package pyre-extensions/0.0.15/pyre-extensions-0.0.15.tar.gz

---
+++ packages/pyre-extensions/pyre-extensions-0.0.15.tar.gz/pyre-extensions-0.0.15/setup.py
@@ -0,0 +1,27 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyre-extensions",
+    version="0.0.15",
+    author="Facebook",
+    author_email="pyre@fb.com",
+    description="Type system extensions for use with the pyre type checker",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        "Operating System :: OS Independent",
+    ],
+)
+        
