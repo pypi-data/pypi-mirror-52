1466142b9f83031b73b711fd6b941ec22667c287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:47 +0000

    Add package imgur-scraper/0.1.12/imgur_scraper-0.1.12.tar.gz

---
+++ packages/imgur-scraper/imgur_scraper-0.1.12.tar.gz/imgur_scraper-0.1.12/README.md
@@ -0,0 +1,30 @@
+# Imgur Scraper
+[![Downloads](https://pepy.tech/badge/imgur-scraper)](https://pepy.tech/project/imgur-scraper) 
+
+Retrieve Imgur's viral posts from a specific date. No authentication required. Implemented using their frontend API.
+
+# Usage
+
+[![asciicast](https://asciinema.org/a/268419.svg)](https://asciinema.org/a/268419)
+
+```
+$ imgur-scraper [COMMAND]
+
+Retrieve Imgur's Viral Posts
+
+COMMAND:
+  -h, --help   show this help message and exit
+  --date       Format YYYY-MM-DD (required)
+  --end_date   Format YYYY-MM-DD (optional)
+  --csv        Format to Save Data In (defaults to False)
+  --path       Path where in to the file csv file, i.e. ../somefoler/.
+               (Defaults to the path from where the script is called.)
+
+```
+
+# Installation
+```
+$ pip install imgur-scraper
+```
+
+Python 3.6 is required
---
+++ packages/imgur-scraper/imgur_scraper-0.1.12.tar.gz/imgur_scraper-0.1.12/setup.py
@@ -0,0 +1,48 @@
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+
+NAME = "imgur_scraper"
+DESCRIPTION = "Scrape years of Imgur's data without any authentication."
+URL = "https://github.com/saadmanrafat/imgur-scraper"
+EMAIL = "saadmanhere@gmail.com"
+AUTHOR = "Saadman Rafat"
+REQUIRES_PYTHON = ">=3.6.0"
+VERSION = "0.1.12"
+REQUIRED = ["requests-html", "requests"]
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name=NAME,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=["imgur_scraper"],
+    entry_points={
+        "console_scripts": ["imgur-scraper=imgur_scraper.imgur_scraper:main"]
+    },
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+)
