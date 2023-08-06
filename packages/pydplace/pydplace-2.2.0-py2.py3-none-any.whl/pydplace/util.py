d2f44a13780dfb91f50e1e89be318503ae95acff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:21 +0000

    Add package pydplace/2.2.0/pydplace-2.2.0-py2.py3-none-any.whl

---
+++ packages/pydplace/pydplace-2.2.0-py2.py3-none-any.whl/pydplace/util.py
@@ -0,0 +1,32 @@
+from functools import partial
+import types
+import shutil
+from pathlib import Path
+
+from clldutils.text import split_text
+
+__all__ = ['comma_split', 'semicolon_split', 'remove_subdirs', 'comma_join']
+
+
+comma_split = partial(split_text, separators=',', strip=True, brackets={})
+semicolon_split = partial(split_text, separators=';', strip=True, brackets={})
+
+
+def _join(sep, *args):
+    if len(args) == 1 and isinstance(args[0], (list, tuple, set, types.GeneratorType)):
+        args = args[0]
+    return sep.join(args)
+
+
+comma_join = partial(_join, ', ')
+semicolon_join = partial(_join, '; ')
+
+
+def format_float(f):
+    return format(float(f), '.6f').rstrip('0').rstrip('.')
+
+
+def remove_subdirs(d, pattern='*'):
+    for sd in Path(d).glob(pattern):
+        if sd.is_dir():
+            shutil.rmtree(str(sd))
