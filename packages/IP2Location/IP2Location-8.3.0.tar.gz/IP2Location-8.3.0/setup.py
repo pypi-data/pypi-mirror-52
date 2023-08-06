fea5ebb30340b3482766a1cd51a8936731e32ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:47 +0000

    Add package IP2Location/8.3.0/IP2Location-8.3.0.tar.gz

---
+++ packages/IP2Location/IP2Location-8.3.0.tar.gz/IP2Location-8.3.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="IP2Location",
+	version="8.3.0", 
+	author="IP2Location",
+	author_email="support@ip2location.com",
+	description="Python API for IP2Location database.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	py_modules=['IP2Location'],
+	url="https://github.com/ip2location/ip2location-python",
+	packages=setuptools.find_packages(),
+	classifiers=(
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	),
+)
