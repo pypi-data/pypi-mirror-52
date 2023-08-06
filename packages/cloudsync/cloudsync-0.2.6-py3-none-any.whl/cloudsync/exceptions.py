3084c25e754045259d2a68d5ee398e37a7a46391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:09 +0000

    Add package cloudsync/0.2.6/cloudsync-0.2.6-py3-none-any.whl

---
+++ packages/cloudsync/cloudsync-0.2.6-py3-none-any.whl/cloudsync/exceptions.py
@@ -0,0 +1,26 @@
+class CloudException(Exception):
+    pass
+
+
+class CloudFileNotFoundError(CloudException):
+    pass
+
+
+class CloudTemporaryError(CloudException):
+    pass
+
+
+class CloudFileExistsError(CloudException):
+    pass
+
+
+class CloudTokenError(CloudException):
+    pass
+
+
+class CloudDisconnectedError(CloudException):
+    pass
+
+
+class CloudCursorError(CloudException):
+    pass
