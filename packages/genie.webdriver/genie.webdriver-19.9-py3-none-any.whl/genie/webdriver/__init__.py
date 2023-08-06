b8ff0192f1491e3452ddf43463c28c43c9339d8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:32 +0000

    Add package genie.webdriver/19.9/genie.webdriver-19.9-py3-none-any.whl

---
+++ packages/genie.webdriver/genie.webdriver-19.9-py3-none-any.whl/genie/webdriver/__init__.py
@@ -0,0 +1,25 @@
+# metadata
+__version__ = '19.9'
+__author__ = 'Cisco Systems Inc.'
+__contact__ = ['pyats-support@cisco.com', 'pyats-support-ext@cisco.com']
+
+from .webpage import WebPage
+
+# try to record usage statistics
+#  - only internal cisco users will have stats.CesMonitor module
+#  - below code does nothing for DevNet users -  we DO NOT track usage stats
+#    for PyPI/public/customer users
+try:
+    # new internal cisco-only pkg since devnet release
+    from pyats.cisco.stats import CesMonitor
+except Exception:
+    try:
+        # legacy pyats version, stats was inside utils module
+        from pyats.utils.stats import CesMonitor
+    except Exception:
+        CesMonitor = None
+
+finally:
+    if CesMonitor is not None:
+        # CesMonitor exists -> this is an internal cisco user
+        CesMonitor(action = __name__, application='pyATS Packages').post()
