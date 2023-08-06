b936592f867ef7f424e8b42784a03442a44128b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:10 +0000

    Add package starrydb/0.0.3/starrydb-0.0.3.tar.gz

---
+++ packages/starrydb/starrydb-0.0.3.tar.gz/starrydb-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="starrydb",
+    version="0.0.3",
+    author="Luo Xi",
+    author_email="rossi913@163.com",
+    description="A NoSQL lightweight database.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/luoxi-github/starrydb",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=["msgpack", "pylibmc"],
+)
