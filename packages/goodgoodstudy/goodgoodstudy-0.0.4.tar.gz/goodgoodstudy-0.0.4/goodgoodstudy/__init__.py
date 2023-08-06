392d1162ebc8fd0382578326a30a3a1718c94fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:46 +0000

    Add package goodgoodstudy/0.0.4/goodgoodstudy-0.0.4.tar.gz

---
+++ packages/goodgoodstudy/goodgoodstudy-0.0.4.tar.gz/goodgoodstudy-0.0.4/goodgoodstudy/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+
+def goodgoodstudy():
+	print u'好好学习'
+
+def daydayup():
+	print u'天天向上'
---
+++ packages/goodgoodstudy/goodgoodstudy-0.0.4.tar.gz/goodgoodstudy-0.0.4/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='goodgoodstudy',
+    version='0.0.4',
+    author='gyk001',
+    author_email='gyk001@gmail.com',
+    url='https://github.com/gyk001',
+    description=u'好好学习',
+    packages=['goodgoodstudy'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'goodgoodstudy=goodgoodstudy:goodgoodstudy',
+            'goodgoodstudythen=goodgoodstudy:daydayup'
+        ]
+    }
+)
