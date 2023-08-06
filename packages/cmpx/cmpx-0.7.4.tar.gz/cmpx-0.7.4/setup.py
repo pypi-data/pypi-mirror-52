39f74b418cbda3739da1b390646c9cb7f2ca3c64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:05 +0000

    Add package cmpx/0.7.4/cmpx-0.7.4.tar.gz

---
+++ packages/cmpx/cmpx-0.7.4.tar.gz/cmpx-0.7.4/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup_args = dict(
+    name='cmpx',
+    version='0.7.4',
+    description='A package for different operations on complex numbers',
+    long_description_content_type='text/markdown',
+    long_description=README,
+    license='MIT',
+    packages=find_packages(),
+    author='Omar Belghaouti',
+    maintainer='Omar Belghaouti',
+    author_email='bel_omar18@yahoo.com',
+    maintainer_email='bel_omar18@yahoo.com',
+    keywords=['Complex', 'ComplexOperations', 'ComplexNumbers'],
+    url='https://github.com/Omar-Belghaouti/PythonComplex',
+    download_url='https://pypi.org/project/cmpx/',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='~=3.3'
+)
+
+install_requires = [
+    'colorama'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
