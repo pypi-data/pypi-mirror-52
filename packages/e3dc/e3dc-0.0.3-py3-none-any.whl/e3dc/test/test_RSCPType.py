9caf64ffad0897dbb422d7ced923f656b45d0180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:18 +0000

    Add package e3dc/0.0.3/e3dc-0.0.3-py3-none-any.whl

---
+++ packages/e3dc/e3dc-0.0.3-py3-none-any.whl/e3dc/test/test_RSCPType.py
@@ -0,0 +1,16 @@
+from e3dc.rscp_type import RSCPType
+
+
+def test_rscptype_value_is_returned():
+    result = RSCPType['Nil'].value
+    assert result == 0x00
+
+
+def test_rscptype_name_is_returned():
+    result = RSCPType(0x00).name
+    assert result == 'Nil'
+
+
+def test_rscptype_mapping_is_returned():
+    result = RSCPType(0x00).mapping
+    assert result is None
