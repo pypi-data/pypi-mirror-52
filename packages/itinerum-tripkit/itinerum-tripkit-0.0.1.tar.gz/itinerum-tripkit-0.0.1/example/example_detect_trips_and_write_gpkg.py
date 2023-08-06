dc8cb42882210c024325ceb7afbe8a1c9b12bd2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:06 +0000

    Add package itinerum-tripkit/0.0.1/itinerum-tripkit-0.0.1.tar.gz

---
+++ packages/itinerum-tripkit/itinerum-tripkit-0.0.1.tar.gz/itinerum-tripkit-0.0.1/example/example_detect_trips_and_write_gpkg.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# Kyle Fitzsimmons, 2018
+
+# run from parent directory
+import os
+import sys
+
+sys.path[0] = os.path.abspath(os.path.pardir)
+os.chdir(os.path.pardir)
+# begin
+from tripkit import Itinerum
+
+import tripkit_config
+
+
+# Edit ./tripkit_config.py first!
+itinerum = Itinerum(config=tripkit_config)
+
+# -- Load user trip from database and write as GIS-compatible file
+users = itinerum.load_users(load_trips=False)
+
+parameters = {
+    'subway_entrances': itinerum.database.load_subway_entrances(),
+    'break_interval_seconds': tripkit_config.TRIP_DETECTION_BREAK_INTERVAL_SECONDS,
+    'subway_buffer_meters': tripkit_config.TRIP_DETECTION_SUBWAY_BUFFER_METERS,
+    'cold_start_distance': tripkit_config.TRIP_DETECTION_COLD_START_DISTANCE_METERS,
+    'accuracy_cutoff_meters': tripkit_config.TRIP_DETECTION_ACCURACY_CUTOFF_METERS,
+}
+for user in users:
+    user.trips = itinerum.process.trip_detection.triplab.v2.algorithm.run(user.coordinates, parameters)
+    itinerum.database.save_trips(user, user.trips)
+    itinerum.io.write_trips_geopackage(tripkit_config, fn_base=user.uuid, trips=user.trips)
