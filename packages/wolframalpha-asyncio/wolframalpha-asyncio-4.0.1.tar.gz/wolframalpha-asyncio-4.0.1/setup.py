ef34d4a7cda2da0dd198944c4b2f67f82564e0ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:57 +0000

    Add package wolframalpha-asyncio/4.0.1/wolframalpha-asyncio-4.0.1.tar.gz

---
+++ packages/wolframalpha-asyncio/wolframalpha-asyncio-4.0.1.tar.gz/wolframalpha-asyncio-4.0.1/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+# Project skeleton maintained at https://github.com/jaraco/skeleton
+
+import io
+
+import setuptools
+
+with io.open('README.rst', encoding='utf-8') as readme:
+	long_description = readme.read()
+
+name = 'wolframalpha-asyncio'
+description = 'Wolfram|Alpha 2.0 API Async client'
+nspkg_technique = 'native'
+"""
+Does this package use "native" namespace packages or
+pkg_resources "managed" namespace packages?
+"""
+
+params = dict(
+	name=name,
+	version="4.0.1",
+	author="Painor",
+	author_email="topcode.softwares@gmail.com",
+	description=description or name,
+	long_description=long_description,
+	url="https://github.com/painor/" + name,
+	packages=setuptools.find_packages(),
+	include_package_data=True,
+	namespace_packages=(
+		name.split('.')[:-1] if nspkg_technique == 'managed'
+		else []
+	),
+	python_requires='>=3.5.3',
+	install_requires=[
+		'aiohttp',
+		'xmltodict',
+	],
+	setup_requires=[
+		'setuptools_scm>=1.15.0',
+	],
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 3",
+	],
+)
+if __name__ == '__main__':
+	setuptools.setup(**params)
