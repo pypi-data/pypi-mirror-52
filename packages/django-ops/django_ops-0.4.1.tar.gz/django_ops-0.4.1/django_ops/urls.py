181201b3c23a046e8c097bf1035fde431509f580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:47 +0000

    Add package django-ops/0.4.1/django_ops-0.4.1.tar.gz

---
+++ packages/django-ops/django_ops-0.4.1.tar.gz/django_ops-0.4.1/django_ops/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.RootView.as_view(), name="root"),
+    path('ping/',  views.PingView.as_view(), name="ping"),
+    path('info/', views.InfoView.as_view(), name="info"),
+    path('migration/', views.MigrationView.as_view(), name="migration"),
+    path('request/', views.RequestView.as_view(), name="request"),
+]
