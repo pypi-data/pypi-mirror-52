2cf43639036128086862e8f2632fb0a059914b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:09 +0000

    Add package nrn/0.2.5/nrn-0.2.5.tar.gz

---
+++ packages/nrn/nrn-0.2.5.tar.gz/nrn-0.2.5/setup.py
@@ -0,0 +1,87 @@
+import os
+import sys
+import contextlib
+import tarfile
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as build_ext_orig
+
+try:
+    from urllib.request import urlretrieve
+except ImportError:
+    from urllib import urlretrieve
+
+VERSION = '0.2.5'
+
+NRN_SHORT_VERSION = '7.6'
+NRN_LONG_VERSION = '7.6.7'
+
+NRN_TARBALL_URL = \
+    'https://neuron.yale.edu/ftp/neuron/versions/v%s/%s/nrn-%s.tar.gz' % (
+        NRN_SHORT_VERSION, NRN_LONG_VERSION, NRN_LONG_VERSION)
+
+NRN_TARBALL = 'nrn-%s.tar.gz' % NRN_LONG_VERSION
+
+
+class NrnExtension(Extension, object):
+
+    def __init__(self, name):
+        super(NrnExtension, self).__init__(name, sources=[])
+
+
+class build_ext(build_ext_orig, object):
+
+    def run(self):
+        for ext in self.extensions:
+            self.build_nrn(ext)
+        super(NrnExtension, self).run()
+
+    def build_nrn(self, ext):
+        os.makedirs(self.build_temp)
+
+        configure_args = [
+            '--without-x',
+            '--prefix=%s' %
+            sys.prefix,
+            '--exec-prefix=%s' %
+            sys.prefix,
+            '--with-nrnpython=python',
+            '--disable-pysetup',
+            '--disable-rx3d']
+        make_args = ['-j', 'install']
+        setup_args = ['--prefix=%s' % sys.prefix]
+
+        with cd(self.build_temp):
+            urlretrieve(NRN_TARBALL_URL, NRN_TARBALL)
+            tarfile.open(NRN_TARBALL, 'r:gz').extractall()
+            configure_cmd = os.path.join(
+                'nrn-%s' %
+                NRN_SHORT_VERSION, 'configure')
+            self.spawn([configure_cmd] + configure_args)
+            make_cmd = os.path.join('make')
+            self.spawn([make_cmd] + make_args)
+            with cd('src/nrnpython'):
+                self.spawn(['python', 'setup.py', 'install'] + setup_args)
+
+
+@contextlib.contextmanager
+def cd(dir_name):
+    """Change directory"""
+    old_cwd = os.getcwd()
+    os.chdir(dir_name)
+    try:
+        yield
+    finally:
+        os.chdir(old_cwd)
+
+
+setup(
+    name='nrn',
+    version=VERSION,
+    packages=['nrn'],
+    install_requires=['wget'],
+    ext_modules=[NrnExtension('nrn')],
+    cmdclass={
+        'build_ext': build_ext,
+    }
+)
