fde7526ee43b0a66546ed48dc0807800b736423c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:06 +0000

    Add package tormetron/0.0.dev2/tormetron-0.0.dev2.tar.gz

---
+++ packages/tormetron/tormetron-0.0.dev2.tar.gz/tormetron-0.0.dev2/tormetron/__init__.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: cp1252 -*-
+'''
+Created on 31 ago. 2019
+
+@author: joseb
+# -*- coding: UTF8 -*-
+# -*- coding: cp1252 -*-
+'''
+#Para versionado, ver
+#    https://www.python.org/dev/peps/pep-0440/
+__version__ = '0.0.dev2'
+
+try:
+    from radares import EstacionRadar
+    from radares import ImagenRadarAEMET
+    from radares import ImagenRadarFile
+    #print(f'Import classes of radares ok from __init__.py (__name__ = {__name__}')
+except:
+    from tormetron.radares import EstacionRadar
+    from tormetron.radares import ImagenRadarAEMET
+    from tormetron.radares import ImagenRadarFile
+    print(f'Import classes of tormetron.radares ok from __init__.py (__name__ = {__name__}')
+
+def main():
+    #REMOVE: quitar los comentarios y los print(). Sustituir por pass
+    #Estos mensajes solo se muestran si se llama directamente __ini__.py
+    #Cuando el módulo se carga al importar el paquete tormetron, este mensaje no se muestra
+    print('\nSe ha llamado especificamente a /tormetron/__init__.py como main(): modo de prueba de carga de clases')
+    print('Simplemente se prueba la carga de paquetes y no se ejecuta busqueda de estación ni descarga de imágenes')
+    print('Lista de estaciones (EstacionRadar.LISTA_RADAR):')
+    for estacion_radar in EstacionRadar.LISTA_RADAR:
+        #TODO: arreglar para que muestre bien los acentos -> decode()
+        print('\t', estacion_radar['cod'], '->', estacion_radar['nombre'])
+    #REMOVE:\>
+
+if __name__ == '__main__':
+    main()
