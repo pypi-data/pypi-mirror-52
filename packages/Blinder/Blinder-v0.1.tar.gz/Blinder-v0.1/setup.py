2c28c023c225fe78fa1f01491bb53fb8fd595f3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:46 +0000

    Add package Blinder/v0.1/Blinder-v0.1.tar.gz

---
+++ packages/Blinder/Blinder-v0.1.tar.gz/Blinder-v0.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'Blinder',
+  packages = ['Blinder'],
+  version = 'v0.1',
+  license='GPL V3.0',
+  description = 'Blidner is a small python library to automate time-based blind SQL injection by using a pre defined queries as a functions to automate a rapid PoC development.',
+  author = 'Askar',
+  author_email = 'm.askar@isecur1ty.org',
+  url = 'https://github.com/user/reponame',
+  download_url = 'https://github.com/mhaskar/Blinder/archive/v0.1.tar.gz',
+  keywords = ['SQL injection', 'Pentesting', "AppSec"],
+  install_requires=[
+          'requests',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Programming Language :: Python :: 2.7'
+  ],
+)
