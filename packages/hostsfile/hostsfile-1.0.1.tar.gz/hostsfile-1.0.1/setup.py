e587c5b053bce5b4570344b8dd24b9e3e3b2e786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:03 +0000

    Add package hostsfile/1.0.1/hostsfile-1.0.1.tar.gz

---
+++ packages/hostsfile/hostsfile-1.0.1.tar.gz/hostsfile-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='hostsfile',
+    install_requires=["requests"],
+    version='1.0.1',
+    packages=['hostsfile'],
+    url='',
+    license='BSD',
+    author='entrp',
+    author_email='',
+    description=''
+)
