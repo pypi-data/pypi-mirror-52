17b05c67faf0d7d6cf09594cbc3d0e7e1dddd956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:57 +0000

    Add package rocketloghandler/0.1.1/rocketloghandler-0.1.1.tar.gz

---
+++ packages/rocketloghandler/rocketloghandler-0.1.1.tar.gz/rocketloghandler-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='rocketloghandler',
+    version='0.1.1',
+    packages=['rocketloghandler'],
+    url='https://mygit.th-deg.de/fwahl/rocketloghandler',
+    license='MIT',
+    author='Florian Wahl',
+    author_email='florian.wahl@gmail.com',
+    description='Send python logs to RocketChat server.',
+    install_requires = ["rocketchat-API>=0.6.34"],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
