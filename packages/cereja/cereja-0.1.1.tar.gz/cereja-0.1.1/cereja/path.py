4b96bcdb82fb5458d14e74c8068379beaa8a2bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:43 +0000

    Add package cereja/0.1.1/cereja-0.1.1.tar.gz

---
+++ packages/cereja/cereja-0.1.1.tar.gz/cereja-0.1.1/cereja/path.py
@@ -0,0 +1,59 @@
+import os
+import random
+
+from cereja.utils import group_items_in_batches
+
+
+def mkdir(path_dir: str):
+    """
+    Creates directory regardless of whether full cereja exists.
+    If a nonexistent cereja is entered the function will create the full cereja until it creates the requested directory.
+    e.g:
+    original structure
+    content/
+
+    after create_dir(/content/cereja/to/my/dir)
+    content/
+        --cereja/
+            --to/
+                --my/
+                    --dir/
+
+    It's a recursive function
+
+    :param path_dir: directory cereja
+    :return: None
+    """
+
+    dir_name_path = os.path.dirname(path_dir)
+    if not os.path.exists(dir_name_path):
+        mkdir(dir_name_path)
+    if not os.path.exists(path_dir):
+        os.mkdir(path_dir)
+
+
+def group_path_from_dir(dir_path: str, num_items_on_tuple: int, ext_file: str, to_random: bool = False,
+                        key_sort_function=None):
+    """
+    returns data tuples based on the number of items entered for each tuple, follows the default order
+    if no sort function is sent
+
+    :param dir_path: full path to dir
+    :param num_items_on_tuple: how much items have your tuple?
+    :param ext_file: is file extension to scan
+    :param to_random: randomize result
+    :param key_sort_function: function order items
+    :return:
+    """
+    key_sort_function = {"key": key_sort_function} if key_sort_function else {}
+    paths = sorted([os.path.join(dir_path, i) for i in os.listdir(dir_path) if ext_file in i], **key_sort_function)
+
+    batches = group_items_in_batches(items=paths, items_per_batch=num_items_on_tuple)
+
+    if to_random:
+        random.shuffle(batches)
+    return batches
+
+
+if __name__ == '__main__':
+    pass
