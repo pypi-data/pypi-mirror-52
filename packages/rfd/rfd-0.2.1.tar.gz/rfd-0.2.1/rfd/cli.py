40f6860aeacfa5e228dacae7955503226953f0d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:51 +0000

    Add package rfd/0.2.1/rfd-0.2.1.tar.gz

---
+++ packages/rfd/rfd-0.2.1.tar.gz/rfd-0.2.1/rfd/cli.py
@@ -0,0 +1,124 @@
+from __future__ import unicode_literals
+
+
+import logging
+import os
+import sys
+import click
+from colorama import init, Fore, Style
+from .api import get_threads, get_posts
+from .parsing import parse_threads
+from .__version__ import version as current_version
+
+init()
+print()
+
+logging.getLogger()
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler())
+
+
+def get_version():
+    return "rfd " + current_version
+
+
+def get_terminal_width():
+    _, columns = os.popen("stty size", "r").read().split()
+    return int(columns)
+
+
+def get_vote_color(score):
+    if score > 0:
+        return Fore.GREEN + " [+" + str(score) + "] "
+    if score < 0:
+        return Fore.RED + " [" + str(score) + "] "
+    return Fore.BLUE + " [" + str(score) + "] "
+
+
+@click.group(invoke_without_command=True)
+@click.option("--version/--no-version", default=False)
+@click.pass_context
+def cli(ctx, version):
+    """CLI for https://forums.redflagdeals.com"""
+    if version:
+        click.echo(get_version())
+    elif not ctx.invoked_subcommand:
+        click.echo(ctx.get_help())
+
+
+@cli.command("version")
+def display_version():
+    click.echo(get_version())
+
+
+@cli.command(short_help="Displays posts in a specific thread.")
+@click.argument("post_id")
+def posts(post_id):
+    """Displays posts in a specific thread.
+
+    post_id can be a full url or post id only
+
+    Example:
+
+    \b
+    url: https://forums.redflagdeals.com/koodo-targeted-public-mobile-12-120-koodo-5gb-40-no-referrals-2173603
+    post_id: 2173603
+    """
+
+    try:
+        click.echo("-" * get_terminal_width())
+        for post in get_posts(post=post_id):
+            click.echo(
+                " -"
+                + get_vote_color(post.score)
+                + Fore.RESET
+                + post.body
+                + Fore.YELLOW
+                + " ({})".format(post.user)
+            )
+            click.echo(Style.RESET_ALL)
+            click.echo("-" * get_terminal_width())
+    except ValueError:
+        click.echo("Invalid post id.")
+        sys.exit(1)
+    except AttributeError:
+        click.echo("The RFD API did not return the expected data.")
+        sys.exit(1)
+
+
+@cli.command(short_help="Displays threads in the specified forum.")
+@click.option("--limit", default=10, help="Number of topics.")
+@click.argument("forum_id", default=9)
+def threads(limit, forum_id):
+    """Displays threads in the specified forum id. Defaults to 9.
+
+    Popular forum ids:
+
+    \b
+    9 \t hot deals
+    14 \t computer and electronics
+    15 \t offtopic
+    17 \t entertainment
+    18 \t food and drink
+    40 \t automotive
+    53 \t home and garden
+    67 \t fashion and apparel
+    74 \t shopping discussion
+    88 \t cell phones
+    """
+    _threads = parse_threads(get_threads(forum_id, limit), limit)
+    for i, thread in enumerate(_threads, 1):
+        click.echo(
+            " "
+            + str(i)
+            + "."
+            + get_vote_color(thread.score)
+            + Fore.RESET
+            + "[%s] %s" % (thread.dealer_name, thread.title)
+        )
+        click.echo(Fore.BLUE + " {}".format(thread.url))
+        click.echo(Style.RESET_ALL)
+
+
+if __name__ == "__main__":
+    cli()
