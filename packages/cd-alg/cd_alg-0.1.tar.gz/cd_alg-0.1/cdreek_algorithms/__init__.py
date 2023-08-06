d3e329219eb58ecc98001f822446ddb6ea6499d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:38 +0000

    Add package cd-alg/0.1/cd_alg-0.1.tar.gz

---
+++ packages/cd-alg/cd_alg-0.1.tar.gz/cd_alg-0.1/cdreek_algorithms/__init__.py
@@ -0,0 +1,4 @@
+from .bubble import bubbleSort
+from .insertion import insertionSort
+from .selection import selectionSort
+from .quick import quick_sort
---
+++ packages/cd-alg/cd_alg-0.1.tar.gz/cd_alg-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cd_alg',
+      version='0.1',
+      description='algorithms package exercise',
+      author='cdreek',
+      author_email='cdreek@example.com',
+      license='MIT',
+      packages=['cdreek_algorithms'],
+      zip_safe=False
+)
