734f345b3863c79fa38cd3c31e38f293dfbeaeaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:05 +0000

    Add package destream/5.0.0/destream-5.0.0.tar.gz

---
+++ packages/destream/destream-5.0.0.tar.gz/destream-5.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "destream",
+    version = "5.0.0",
+    author = "Cecile Tonglet",
+    author_email = "cecile.tonglet@gmail.com",
+    description = ("A simple module to decompress streams compressed multiple "
+                   "times"),
+    license = "GPLv2",
+    keywords = "postgres database dump restore",
+    url = "https://github.com/cecton/destream",
+    packages = find_packages(),
+    scripts = ['scripts/destream'],
+    install_requires = ['python-magic>=0.4.12'],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: System :: Archiving :: Compression",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+    ],
+)
