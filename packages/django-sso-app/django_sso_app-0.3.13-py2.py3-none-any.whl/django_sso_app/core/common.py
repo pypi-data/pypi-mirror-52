5b14788e4207a65ef4b164e90436bf6fe4310f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:30 +0000

    Add package django-sso-app/0.3.13/django_sso_app-0.3.13-py2.py3-none-any.whl

---
+++ packages/django-sso-app/django_sso_app-0.3.13-py2.py3-none-any.whl/django_sso_app/core/common.py
@@ -0,0 +1,9 @@
+ROLE_CHOICES = (
+    (-1, 'Staff'),
+    (0, 'Cityzen'),
+    (1, 'Institutional'),
+    (2, 'Professionist'),
+    (4, 'Company'),
+    (6, 'Istitutional Partition Item'),
+    (7, 'App'),
+)
