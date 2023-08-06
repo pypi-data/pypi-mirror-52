31d6c857706e4a72384dc1c9e039c47258a0ae66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:07 +0000

    Add package partialsmiles/1.0/partialsmiles-1.0.tar.gz

---
+++ packages/partialsmiles/partialsmiles-1.0.tar.gz/partialsmiles-1.0/README.rst
@@ -0,0 +1,6 @@
+partialsmiles
+=============
+
+A validating SMILES parser, with support for partial SMILES (e.g. as generated character by character by a generative model).
+
+For more information see the website at https://baoilleach.github.io/partialsmiles/introduction.html
---
+++ packages/partialsmiles/partialsmiles-1.0.tar.gz/partialsmiles-1.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+        name="partialsmiles",
+        version="1.0",
+        author="Noel M. O'Boyle",
+        author_email="baoilleach@gmail.com",
+        url="https://github.com/baoilleach/partialsmiles",
+        packages=['partialsmiles',],
+        license="MIT License",
+        description="A validating SMILES parser with support for partial SMILES",
+   )
+
