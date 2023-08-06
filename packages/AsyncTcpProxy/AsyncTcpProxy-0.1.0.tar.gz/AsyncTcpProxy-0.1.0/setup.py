a50f28a27da64b656956a35b20cbf36c96e485d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:08 +0000

    Add package AsyncTcpProxy/0.1.0/AsyncTcpProxy-0.1.0.tar.gz

---
+++ packages/AsyncTcpProxy/AsyncTcpProxy-0.1.0.tar.gz/AsyncTcpProxy-0.1.0/setup.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='AsyncTcpProxy',
+      version='0.1.0',
+      description='My Python Project Setup',
+      author='Eric Tang',
+      author_email='tangcongyuan@gmail.com',
+      url='',
+      packages=[
+          'proxy',
+          'server',
+      ],
+     )
+
