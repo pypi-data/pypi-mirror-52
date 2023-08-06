33c29695ee51336b7043c1a5030221ff25590baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:58 +0000

    Add package pymoondb/0.1rc1/pymoondb-0.1rc1-py3-none-any.whl

---
+++ packages/pymoondb/pymoondb-0.1rc1-py3-none-any.whl/moondb/__init__.py
@@ -0,0 +1,24 @@
+"""
+A Sphinx example and cheat sheet.
+ 
+Comments
+########
+ 
+.. comment This is a comment!
+ 
+.. code-block:: python
+ 
+    .. comment This is a comment!
+ 
+Basic Syntax
+############
+ 
+*italic*
+ 
+**bold**
+ 
+``verbatim with special characters such as *, also useful for inline code examples``
+"""
+
+from .core import *
+from .helpers import *
