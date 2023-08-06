e263ca43f8a5220a76322fd298acaf54ff888f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:50 +0000

    Add package sphinx-csharp/0.1.8/sphinx-csharp-0.1.8.tar.gz

---
+++ packages/sphinx-csharp/sphinx-csharp-0.1.8.tar.gz/sphinx-csharp-0.1.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name = 'sphinx-csharp',
+    version = '0.1.8',
+    author = 'djungelorm',
+    author_email = 'djungelorm@users.noreply.github.com',
+    packages = ['sphinx_csharp'],
+    url = 'https://github.com/djungelorm/sphinx-csharp',
+    license = 'MIT',
+    description = 'C# domain for Sphinx',
+    install_requires = ['Sphinx>=1.6'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: OS Independent',
+        'Topic :: Documentation :: Sphinx'
+    ]
+)
