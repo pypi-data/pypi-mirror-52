47063960658b706b5ff4378098f7fcb77f603c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:43 +0000

    Add package rapydo-utils/0.6.7/rapydo-utils-0.6.7.tar.gz

---
+++ packages/rapydo-utils/rapydo-utils-0.6.7.tar.gz/rapydo-utils-0.6.7/utilities/__init__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+__version__ = '0.6.7'
+__authors__ = ["Paolo D'Onorio De Meo <p.donorio.demeo@gmail.com>", "Mattia D'Antonio"]
+
+###############
+FRAMEWORK_NAME = 'RAPyDo'
+
+# TOOLS = [
+#     'utils',
+#     'core',
+#     'frontend',
+#     'do',
+#     'develop',
+#     'build-templates',
+#     'http-api',
+# ]
+# ORDER: utils > core > do > develop > builds > http-api > frontend
+
+###############
+PROJECTS_DEFAULTS_FILE = 'projects_defaults'
+PROJECT_CONF_FILENAME = 'project_configuration'
+PROJECT_DIR = 'projects'
+SWAGGER_DIR = 'swagger'
+MODELS_DIR = 'models'
+SWAGGER_MODELS_FILE = 'models'
+CONF_PATH = 'confs'
+EXTENDED_PROJECT_DISABLED = "no_extended_project"
+
+GITREPOS_TEAM = FRAMEWORK_NAME.lower()
+CONTAINERS_YAML_DIRNAME = CONF_PATH
+UTILS_PKGNAME = __package__.split('.')[::-1][0]
+
+BACKEND_DIR = 'backend'  # directory outside docker
+MAIN_PACKAGE = 'restapi'
+BACKEND_PACKAGE = MAIN_PACKAGE  # package inside rapydo-http
+CUSTOM_PACKAGE = os.environ.get('VANILLA_PACKAGE', 'custom')
+EXTENDED_PACKAGE = os.environ.get('EXTENDED_PACKAGE', None)
+CORE_CONFIG_PATH = os.path.join(BACKEND_PACKAGE, CONF_PATH)
+ENDPOINTS_CODE_DIR = 'apis'
