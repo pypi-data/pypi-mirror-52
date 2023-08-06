52dee172d662a5e94951f2b0f5939a52a7628987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:35 +0000

    Add package micropy-cli/2.1.1/micropy-cli-2.1.1.tar.gz

---
+++ packages/micropy-cli/micropy-cli-2.1.1.tar.gz/micropy-cli-2.1.1/setup.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+import os
+
+from setuptools import find_packages, setup
+
+# Package meta-data.
+NAME = 'micropy-cli'
+DESCRIPTION = ('Micropython Project Management Tool for VSCode with Linting,'
+               ' Intellisense, Dependency Management, and more!')
+URL = 'https://github.com/BradenM/micropy-cli'
+AUTHOR = 'Braden Mars'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = '2.1.1'  # Update via bump2version
+
+# Required Packages
+REQUIRED = [
+    'click>=7',
+    'questionary',
+    'rshell',
+    'colorama ; platform_system=="Windows"',
+    'jsonschema',
+    'jinja2',
+    'requests',
+    'tqdm',
+    'requirements-parser',
+    'packaging'
+]
+
+EXTRAS = {
+    'create_stubs': ['pyminifier==2.1']
+}
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Description
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(
+        exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+
+    entry_points={
+        'console_scripts': ['micropy=micropy.cli:cli'],
+    },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='GNU GPLv3',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: MicroPython',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Software Development :: Embedded Systems'
+    ],
+)
