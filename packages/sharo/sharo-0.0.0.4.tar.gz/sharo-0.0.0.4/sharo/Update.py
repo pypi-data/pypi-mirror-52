28c6cfd9e54714a846ef52260e4364caf3049173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:41 +0000

    Add package sharo/0.0.0.4/sharo-0.0.0.4.tar.gz

---
+++ packages/sharo/sharo-0.0.0.4.tar.gz/sharo-0.0.0.4/sharo/Update.py
@@ -0,0 +1,17 @@
+class Update:
+    def __init__(self,node):
+        self.node = node
+        self.blackboard = node.new_pub("/blackboard","json")
+        self.whiteboard = node.new_pub("/whiteboard","json")
+
+    def blackboard(self, social_primtive,input_name, intensity, robot = "pepper"):
+        data = {"primitive":social_primtive, "input":{input_name:intensity}, "robot":robot}
+        self.blackboard.publish(data)
+
+    def whiteboard(self, sensory_primtive,value,robot = "pepper"):
+        data = {"primitive":sensory_primtive, "input":value, "robot":robot}
+        self.blackboard.publish(data)
+
+
+
+
---
+++ packages/sharo/sharo-0.0.0.4.tar.gz/sharo-0.0.0.4/sharo/__init__.py
@@ -0,0 +1,4 @@
+
+from .Update import*
+
+
