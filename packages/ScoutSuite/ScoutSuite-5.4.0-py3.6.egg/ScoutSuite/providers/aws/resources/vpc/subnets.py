56ce1a629cb0559aa44e47957749857ff723982e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:59 +0000

    Add package ScoutSuite/5.4.0/ScoutSuite-5.4.0-py3.6.egg

---
+++ packages/ScoutSuite/ScoutSuite-5.4.0-py3.6.egg/ScoutSuite/providers/aws/resources/vpc/subnets.py
@@ -0,0 +1,24 @@
+from ScoutSuite.providers.aws.facade.base import AWSFacade
+from ScoutSuite.providers.aws.resources.base import AWSResources
+from ScoutSuite.providers.aws.utils import get_name
+
+
+class Subnets(AWSResources):
+    def __init__(self, facade: AWSFacade, region: str, vpc: str):
+        self.region = region
+        self.vpc = vpc
+
+        super(Subnets, self).__init__(facade)
+
+    async def fetch_all(self):
+        raw_subnets = await self.facade.ec2.get_subnets(self.region, self.vpc)
+        for raw_subnet in raw_subnets:
+            id, subnet = self._parse_subnet(raw_subnet)
+            self[id] = subnet
+
+    def _parse_subnet(self, raw_subnet):
+        raw_subnet['id'] = raw_subnet['SubnetId']
+        get_name(raw_subnet, raw_subnet, 'SubnetId')
+        raw_subnet.pop('SubnetId')
+
+        return raw_subnet['id'], raw_subnet
