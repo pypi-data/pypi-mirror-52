a89fac847e0e5ef38b11e8aa65de39b0e7139c85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:33 +0000

    Add package YTSAPI/0.1.1/YTSAPI-0.1.1-py3-none-any.whl

---
+++ packages/YTSAPI/YTSAPI-0.1.1-py3-none-any.whl/ytsapi/__main__.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    description=(
+                'YTSAPI is a python API which allows you to get the transcript/subtitle for a given YouTube video.\n'
+                'It also works for automatically generated subtitles. This API is a part of The Abuse Project\n\n'
+                'Usage:\n'
+                'import ytsapi\n'
+                'ytsapi.YTSAPI.get_transcript("Youtube Video ID")\n\n'
+            )
+    print(description)
