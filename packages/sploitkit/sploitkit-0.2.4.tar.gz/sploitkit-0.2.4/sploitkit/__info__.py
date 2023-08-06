6f63ddea70975e3c3181aec7b3060b8274872b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:30 +0000

    Add package sploitkit/0.2.4/sploitkit-0.2.4.tar.gz

---
+++ packages/sploitkit/sploitkit-0.2.4.tar.gz/sploitkit-0.2.4/sploitkit/__info__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""Sploitkit package information.
+
+"""
+
+__author__    = "Alexandre D'Hondt"
+__version__   = "0.2.4"
+__copyright__ = "© 2019 A. D'Hondt"
+__license__   = "AGPLv3 (http://www.gnu.org/licenses/agpl.html)"
