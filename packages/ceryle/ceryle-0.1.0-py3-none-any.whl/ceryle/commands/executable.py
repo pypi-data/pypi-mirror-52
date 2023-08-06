759217e4fa0215ee72bdc75af65a2302b794f745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:59 +0000

    Add package ceryle/0.1.0/ceryle-0.1.0-py3-none-any.whl

---
+++ packages/ceryle/ceryle-0.1.0-py3-none-any.whl/ceryle/commands/executable.py
@@ -0,0 +1,61 @@
+import abc
+
+import ceryle.util as util
+
+
+class Executable(abc.ABC):
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def execute(self, *args, context=None, **kwargs):
+        pass
+
+
+class ExecutionResult:
+    def __init__(self, return_code, stdout=[], stderr=[]):
+        self._return_code = return_code
+        self._stdout = [util.assert_type(l, str) for l in stdout]
+        self._stderr = [util.assert_type(l, str) for l in stderr]
+
+    @property
+    def return_code(self):
+        return self._return_code
+
+    @property
+    def stdout(self):
+        return [*self._stdout]
+
+    @property
+    def stderr(self):
+        return [*self._stderr]
+
+
+class ExecutableWrapper(Executable):
+    def __init__(self, func, *args, **kwargs):
+        self._func = func
+        self._args = list(args)
+        self._kwargs = dict(kwargs)
+
+    def execute(self, **kwargs):
+        func_code = self._func.__code__
+        extra_keys = func_code.co_varnames[len(self._args):func_code.co_argcount]
+        kwdefaults = dict(zip(extra_keys, self._func.__defaults__ or ()))
+        exact_kwargs = {}
+        for k in [*self._kwargs.keys(), *kwdefaults.keys()]:
+            exact_kwargs[k] = kwargs.get(k, self._kwargs.get(k, kwdefaults.get(k)))
+
+        res = self._func(*self._args, **exact_kwargs)
+        if res is None or isinstance(res, int):
+            return ExecutionResult(res or 0)
+
+        if not isinstance(res, ExecutionResult):
+            raise RuntimeError(f'ExecutionResult was not returned by {self._func.__name__}')
+        return res
+
+
+def executable(func):
+    def wrapper(*args, **kwargs):
+        return ExecutableWrapper(func, *args, **kwargs)
+
+    return wrapper
---
+++ packages/ceryle/ceryle-0.1.0-py3-none-any.whl/ceryle/tasks/runner.py
@@ -0,0 +1,44 @@
+import ceryle.util as util
+
+from ceryle.tasks import TaskDefinitionError, TaskIOError
+from ceryle.tasks.resolver import DependencyResolver
+from ceryle.tasks.task import TaskGroup
+
+
+class TaskRunner:
+    def __init__(self, task_groups):
+        util.assert_type(task_groups, list)
+        for g in task_groups:
+            util.assert_type(g, TaskGroup)
+
+        resolver = DependencyResolver(dict([(g.name, [*g.dependencies]) for g in task_groups]))
+        resolver.validate()
+        self._deps_chain_map = resolver.deps_chain_map()
+        self._groups = dict([(g.name, g) for g in task_groups])
+
+    def run(self, task_group, dry_run=False):
+        chain = self._deps_chain_map.get(task_group)
+        if chain is None:
+            raise TaskDefinitionError(f'task {task_group} is not defined')
+        return self._run(chain, dry_run=dry_run)[0]
+
+    def _run(self, chain, dry_run=False, register={}):
+        r = dict(register)
+        for c in chain.deps:
+            res, r = self._run(c, dry_run=dry_run, register=r)
+            if not res:
+                return False, r
+        return self._run_group(self._groups[chain.task_name], dry_run=dry_run, register=r)
+
+    def _run_group(self, tg, dry_run=False, register={}):
+        r = dict(register)
+        for t in tg.tasks:
+            if t.input_key and t.input_key not in r:
+                raise TaskIOError(f'{t.input_key} is required by a task in {tg.name}, but not registered')
+            if not t.run(dry_run=dry_run, inputs=r.get(t.input_key, [])):
+                return False, r
+            if t.stdout_key:
+                r.update({t.stdout_key: t.stdout()})
+            if t.stderr_key:
+                r.update({t.stderr_key: t.stderr()})
+        return True, r
