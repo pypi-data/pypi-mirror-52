3e6422a2f53eda573160c3faf1bf3c8d76d5dabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:24 +0000

    Add package beams/0.1.0/beams-0.1.0-py3-none-any.whl

---
+++ packages/beams/beams-0.1.0-py3-none-any.whl/beams/__init__.py
@@ -0,0 +1,5 @@
+"""`beams.__init__.py`"""
+
+from beams._version import __version__
+from beams.beam import Beam, PointLoad, DistributedLoad
+from beams.shapes import Rectangle, Circle, CrossSection, XAxis, YAxis
