c41c721e1c698e4ddc1946e9cdfd96bc80a2334c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:18 +0000

    Add package archives/0.8/archives-0.8.tar.gz

---
+++ packages/archives/archives-0.8.tar.gz/archives-0.8/archives/utils/state.py
@@ -0,0 +1,38 @@
+"""
+@author jacobi petrucciani
+@desc click state related handling
+"""
+import click
+from typing import List
+
+
+class State:
+    """
+    @desc state object for click
+    """
+
+    def __init__(self) -> None:
+        """
+        @cc 1
+        @desc state constructor
+        """
+        self.verbose = False
+        self.quiet = False
+        self.disable_list: List[str] = []
+        self.format = "flake8"
+        self.module_rules: List = []
+        self.class_rules: List = []
+        self.function_rules: List = []
+
+
+def get_state() -> State:
+    """
+    @cc 2
+    @desc gets the current state of the click app
+    @ret the current click state object
+    """
+    try:
+        return click.get_current_context().ensure_object(State)
+    except RuntimeError:
+        # this allows us to use archives programmatically
+        return State()
