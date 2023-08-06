0af70ffc42ca6463948e8819d36b6cd0a27dfde3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:00 +0000

    Add package pythx/1.2.6/pythx-1.2.6.tar.gz

---
+++ packages/pythx/pythx-1.2.6.tar.gz/pythx-1.2.6/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+"""The setup script."""
+
+from setuptools import find_packages, setup
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.rst") as history_file:
+    history = history_file.read()
+
+with open("requirements.txt", "r") as f:
+    requirements = list(map(str.strip, f.read().split("\n")))[:-1]
+
+setup_requirements = ["pytest-runner"]
+
+test_requirements = ["pytest"]
+
+setup(
+    author="Dominik Muhs",
+    author_email="dominik.muhs@consensys.net",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    description="A Python library for the MythX platform",
+    entry_points={"console_scripts": ["pythx=pythx.cli.main:cli"]},
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + "\n\n" + history,
+    include_package_data=True,
+    keywords="pythx",
+    name="pythx",
+    packages=find_packages(),
+    setup_requires=setup_requirements,
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/dmuhs/pythx",
+    version="1.2.6",
+    zip_safe=False,
+)
