5bf262e467f629dea1c5665cb93dede52226b9ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:49 +0000

    Add package crispy-forms-propeller/0.0.2a9/crispy_forms_propeller-0.0.2a9-py2.py3-none-any.whl

---
+++ packages/crispy-forms-propeller/crispy_forms_propeller-0.0.2a9-py2.py3-none-any.whl/crispy_forms_propeller/layout/__init__.py
@@ -0,0 +1,28 @@
+"""
+Layout items for Foundation components.
+
+Inherits from the default **crispy_forms** layout objects to force templates on
+the right ``TEMPLATE_PACK`` (defined from ``settings.CRISPY_TEMPLATE_PACK``)
+and implements Foundation components.
+"""
+from __future__ import absolute_import
+
+from .base import (PrependedAppendedText, AppendedText, PrependedText, FormActions,
+                   InlineCheckboxes, InlineRadios, FieldWithButtons, StrictButton, Container,
+                   ContainerHolder, Tab, TabHolder, AccordionGroup,
+                   Accordion, Alert, InlineFieldUneditableField)
+
+
+__all__ = [
+    'PrependedAppendedText', 'AppendedText', 'PrependedText',
+
+    'FormActions', 'InlineCheckboxes', 'InlineRadios',
+    'InlineFieldUneditableField',
+
+    'FieldWithButtons', 'StrictButton',
+
+    'Container', 'ContainerHolder', 'Tab', 'TabHolder',
+    'Accordion', 'AccordionGroup',
+
+    'Alert'
+]
