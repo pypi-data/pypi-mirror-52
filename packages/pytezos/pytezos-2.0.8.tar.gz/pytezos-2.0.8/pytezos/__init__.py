6a7267d1b595a211cc2eb930bc6ddf949b757961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:00 +0000

    Add package pytezos/2.0.8/pytezos-2.0.8.tar.gz

---
+++ packages/pytezos/pytezos-2.0.8.tar.gz/pytezos-2.0.8/pytezos/__init__.py
@@ -0,0 +1,23 @@
+"""
+Welcome to PyTezos!
+
+To start playing with the Tezos blockchain you need to get a PyTezosClient instance.
+Just type:
+
+>>> from pytezos import pytezos
+>>> pytezos
+
+And follow the interactive documentation.
+"""
+
+from pytezos.rpc import RpcProvider, localhost, mainnet, alphanet, zeronet
+from pytezos.crypto import Key
+from pytezos.proto import Proto
+from pytezos.michelson.contract import Contract
+from pytezos.michelson.formatter import format_timestamp
+from pytezos.client import PyTezosClient
+from pytezos.operation.group import OperationGroup
+from pytezos.michelson.interface import ContractInterface, MichelsonRuntimeError
+from pytezos.standards.non_fungible_token import NonFungibleTokenImpl
+
+pytezos = PyTezosClient()
