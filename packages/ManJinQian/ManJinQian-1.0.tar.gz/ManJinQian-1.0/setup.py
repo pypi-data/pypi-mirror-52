5bbc29dd91b31eafcb2afcbe19c743cafc419ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:45 +0000

    Add package ManJinQian/1.0/ManJinQian-1.0.tar.gz

---
+++ packages/ManJinQian/ManJinQian-1.0.tar.gz/ManJinQian-1.0/setup.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+from distutils.core import setup
+
+setup(
+    name='ManJinQian',
+    version='1.0',
+    description='这是第一个测试版本',
+    author='AHICUE',
+    author_email='ahicue@outlook.com',
+)
