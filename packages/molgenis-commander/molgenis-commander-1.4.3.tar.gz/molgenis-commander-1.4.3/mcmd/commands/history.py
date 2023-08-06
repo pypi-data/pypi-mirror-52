484f11e2d0cbdf93cf1ba13235697327b8f4b407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:11 +0000

    Add package molgenis-commander/1.4.3/molgenis-commander-1.4.3.tar.gz

---
+++ packages/molgenis-commander/molgenis-commander-1.4.3.tar.gz/molgenis-commander-1.4.3/mcmd/commands/history.py
@@ -0,0 +1,53 @@
+from mcmd.core import history as hist
+from mcmd.io import io
+from mcmd.core.command import command
+from mcmd.commands._registry import arguments
+from mcmd.io.logging import get_logger
+
+
+# =========
+# Arguments
+# =========
+
+@arguments('history')
+def add_arguments(subparsers):
+    p_history = subparsers.add_parser('history',
+                                      help="Shows the history of commands that were run. Commands from scripts are"
+                                           " also included.")
+    p_history.set_defaults(func=history,
+                           write_to_history=False)
+    p_history.add_argument('--number', '-n',
+                           type=int,
+                           default=10,
+                           help='Number of lines of history to show. Default: 10')
+    p_history.add_argument('--clear', '-c',
+                           action='store_true',
+                           help='Clears the history.')
+
+
+# =======
+# Globals
+# =======
+
+log = get_logger()
+
+
+# =======
+# Methods
+# =======
+
+@command
+def history(args):
+    if args.clear:
+        io.start('Clearing history')
+        hist.clear()
+    else:
+        lines = hist.read(args.number, include_fails=True)
+        if len(lines) == 0:
+            log.info('History is empty.')
+        for line in lines:
+            io.start(line[1])
+            if line[0]:
+                io.succeed()
+            else:
+                io.error(None)
