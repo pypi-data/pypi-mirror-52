179e843d1fee00644741bf7b87676e7cf6560aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:02 +0000

    Add package yandex-checkout/1.4.5/yandex_checkout-1.4.5.tar.gz

---
+++ packages/yandex-checkout/yandex_checkout-1.4.5.tar.gz/yandex_checkout-1.4.5/yandex_checkout/domain/models/currency.py
@@ -0,0 +1,11 @@
+class Currency(object):
+    """
+    Class representing currency enum
+    """
+    RUB = "RUB"
+    USD = "USD"
+    EUR = "EUR"
+    BYN = "BYN"
+    CNY = "CNY"
+    KZT = "KZT"
+    UAH = "UAH"
