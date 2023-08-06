2922505d41321dba6118610dd0e7acce4f4f09e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:01 +0000

    Add package winpwn/0.0.1/winpwn-0.0.1.tar.gz

---
+++ packages/winpwn/winpwn-0.0.1.tar.gz/winpwn-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import *
+
+setup(
+    name='winpwn',
+    version='0.0.1',
+    description='mini pwntools for windows',
+    author='byzero512',
+    author_email='byzero512@gamil.com',
+    license='MIT',
+    packages=find_packages()
+)
