e1308fa3b79ed42562de6aae8694c9c38b183f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:43 +0000

    Add package dist-prob-dst/1.2/dist_prob_dst-1.2.tar.gz

---
+++ packages/dist-prob-dst/dist_prob_dst-1.2.tar.gz/dist_prob_dst-1.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dist_prob_dst',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['dist_prob_dst'], author = 'Megha Raveendran',
+      zip_safe=False)
