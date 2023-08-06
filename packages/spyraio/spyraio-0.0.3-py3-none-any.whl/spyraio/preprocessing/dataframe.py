cd6e5a56fdd5fa9dc0203cc3542ae74ac7bc3f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:18 +0000

    Add package spyraio/0.0.3/spyraio-0.0.3-py3-none-any.whl

---
+++ packages/spyraio/spyraio-0.0.3-py3-none-any.whl/spyraio/preprocessing/dataframe.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""Preprocessing functions.
+"""
+import os
+import pandas as pd
+
+
+def load_dataset(path, files):
+    """Load dataframe.
+    
+    # Arguments
+        `path`: a string containing the file directory
+        `files`: a list containing one or more file names
+    # Returns
+        Return a concatenated dataframe from files
+    """
+    
+    dfs = [pd.read_csv(os.path.join(path, file)) for file in files]
+    df = pd.concat(dfs, sort=False)
+    
+    return df
+
+
+
+if __name__ == "__main__":
+    df = load_dataset('./data/private', ['august.csv'])
+    print(df)
+    
