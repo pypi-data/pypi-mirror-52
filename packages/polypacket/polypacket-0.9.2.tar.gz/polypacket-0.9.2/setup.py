40438fca42d3f72427a523fd14e954ed33ca4547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:00 +0000

    Add package polypacket/0.9.2/polypacket-0.9.2.tar.gz

---
+++ packages/polypacket/polypacket-0.9.2.tar.gz/polypacket-0.9.2/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='polypacket',
+     version='0.9.02',
+     author="Jason Berger",
+     author_email="JBerger@up-rev.com",
+     description="A tool for building protocol services",
+     long_description=long_description,
+     scripts=['polypacket/poly-packet'],
+     long_description_content_type="text/markdown",
+     url="http://www.up-rev.com/",
+     packages=setuptools.find_packages(),
+     package_data={
+     'polypacket':['templates/*'],
+     'polypacket':['examples/*'],
+     },
+     install_requires=[
+        'markdown',
+        'mako',
+        'prompt_toolkit',
+        'pyyaml'
+     ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
