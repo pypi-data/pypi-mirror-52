751a6913433975d36807bb7d6cb6ab9deb2e6576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:36 +0000

    Add package odooup/0.1.0/odooup-0.1.0.tar.gz

---
+++ packages/odooup/odooup-0.1.0.tar.gz/odooup-0.1.0/CHANGES.rst
@@ -0,0 +1,32 @@
+Changes
+~~~~~~~
+
+.. Future (?)
+.. ----------
+.. -
+
+0.1.0 (2019-05-14)
+------------------
+- Be forgifing on non parsable manifests for dep whitelisting
+
+[...]
+-----
+
+0.0.4 (2019-05-14)
+------------------
+Fixed
+^^^^^
+- Fix shallow submodules for any version of Odoo
+
+0.0.3 (2019-05-02)
+------------------
+Fixed
+^^^^^
+- Fix duplicated tag "{BACKPORT_FLAG}-{TAG}"
+- Fix error passing candidate as a tuple backporting a specific commit
+
+0.0.1 (2019-04-18)
+------------------
+- Carve out functionality from dockery-odoo-scaffold hack
+- init: helper to initialize an odoo project
+- repo: toolset to manage your own odoo-dev / patches
---
+++ packages/odooup/odooup-0.1.0.tar.gz/odooup-0.1.0/setup.py
@@ -0,0 +1,48 @@
+# Copyright 2019 XOE Labs (<https://xoe.solutions>)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+import os
+
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name="odooup",
+    description="Odoo Project puppet master",
+    long_description="\n".join(
+        (
+            open(os.path.join(here, "README.rst")).read(),
+            open(os.path.join(here, "CHANGES.rst")).read(),
+        )
+    ),
+    use_scm_version=True,
+    packages=find_packages(),
+    setup_requires=["setuptools-scm"],
+    install_requires=["click>=7.0", "click-plugins", "future", "appdirs", "networkx"],
+    license="LGPLv3+",
+    author="XOE Labs",
+    author_email="info@xoe.solutions",
+    url="http://github.com/xoe-labs/odooup",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: "
+        "GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Framework :: Odoo",
+    ],
+    entry_points="""
+        [console_scripts]
+        odooup=odooup.cli:main
+        [core_package.cli_plugins]
+        version=odooup.cli:version
+        init=odooup.init:init
+        patches=odooup.patches:patches
+        whitelist=odooup.whitelist:whitelist
+        clone=odooup.clone:clone
+    """,
+)
