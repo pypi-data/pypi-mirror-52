2ccec3e03184397739721f273a1e9b1626b30fcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:25 +0000

    Add package pltCIreg/0.0.4/pltCIreg-0.0.4.tar.gz

---
+++ packages/pltCIreg/pltCIreg-0.0.4.tar.gz/pltCIreg-0.0.4/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup
+
+setup(
+    name='pltCIreg',
+    version='0.0.4',
+    author='Aldrich_1',
+    author_email='zhengz3000@gmail.com',
+    url='https://zhuanlan.zhihu.com/p/26159930',
+    description=u'吃枣药丸 提供画对比折线图，且附带置信区间的接口',
+    packages=['pltCIreg'],
+    install_requires=[],
+)
