05cdd5e33aebd707e3ed87a278969ea6da33bcf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:46 +0000

    Add package panopto-cli/0.6.1/panopto-cli-0.6.1.tar.gz

---
+++ packages/panopto-cli/panopto-cli-0.6.1.tar.gz/panopto-cli-0.6.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+install_requires = [
+    "Click",
+    "requests",
+    "zeep"
+]
+
+setup(
+    name="panopto-cli",
+    version="0.6.1",
+    py_modules=['panopto'],
+    install_requires=install_requires,
+    licence='Apache 2.0',
+    author='Jay Luker',
+    url='https://github.com/harvard-dce/panopto-cli',
+    entry_points='''
+        [console_scripts]
+        panopto=panopto:cli 
+    ''',
+)
