6b2fd40e8a5d5159ba76b1c444ff180ce224c51e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:24 +0000

    Add package sauronmodels/0.1/sauronmodels-0.1.tar.gz

---
+++ packages/sauronmodels/sauronmodels-0.1.tar.gz/sauronmodels-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'sauronmodels',
+  packages = ['sauronmodels'], # this must be the same as the name above
+  version = '0.1',
+  description = 'sauron models',
+  author = 'Jhon Velasquez',
+  author_email = 'jhon.velasquez@gdx.travel',
+  url = 'https://jhon_velasquez@bitbucket.org/gdxbackoffice/models-sauron.git', # use the URL to the github repo
+  download_url = 'https://jhon_velasquez@bitbucket.org/gdxbackoffice/models-sauron.git',
+  keywords = ['testing', 'logging', 'example'],
+  classifiers = [],
+)
