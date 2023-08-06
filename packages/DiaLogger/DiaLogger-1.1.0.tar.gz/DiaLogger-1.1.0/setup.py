fc88761ea85b2086933533701d448f8f53f4b751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:03 +0000

    Add package DiaLogger/1.1.0/DiaLogger-1.1.0.tar.gz

---
+++ packages/DiaLogger/DiaLogger-1.1.0.tar.gz/DiaLogger-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'DiaLogger',
+    version = '1.1.0',
+    py_modules = ['DiaLogger'],
+    author = 'Ceekayshen',
+    author_email = 'ceekayshen@foxmail.com',
+    url = 'https://github.com/shenliuming/yys',
+    description = 'None',
+)
