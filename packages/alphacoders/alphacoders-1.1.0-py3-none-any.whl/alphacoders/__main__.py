e1c5d8fc910701b27a40dd454c03464af396d6a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:52 +0000

    Add package alphacoders/1.1.0/alphacoders-1.1.0-py3-none-any.whl

---
+++ packages/alphacoders/alphacoders-1.1.0-py3-none-any.whl/alphacoders/__main__.py
@@ -0,0 +1,34 @@
+#
+
+from aiohttp import ClientSession
+from alphacoders import SingleTask, execute_single_task
+import asyncio
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description='Download wallpapers from Alpha Coders.')
+parser.add_argument('keywords', metavar='keyword', help='searching keywords', nargs='+')
+parser.add_argument('--limit',
+                    type=int,
+                    help='max download wallpaper',
+                    default=None)
+args = parser.parse_args()
+
+manager_list = [
+    SingleTask(keyword, limit=args.limit)
+    for keyword in args.keywords
+]
+
+
+async def main():
+    async with ClientSession() as client:
+        task_list = [execute_single_task(manager, client) for manager in manager_list]
+        [await task for task in task_list]
+
+
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    pass
+finally:
+    for manager in manager_list:
+        print(f'[{manager.keyword}] download {manager.complete_count} images')
