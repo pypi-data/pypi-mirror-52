3cf039736f8ea2042b6e7ec2bb9f1721543c7341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:32 +0000

    Add package blockapi/0.0.10/blockapi-0.0.10-py3-none-any.whl

---
+++ packages/blockapi/blockapi-0.0.10-py3-none-any.whl/blockapi/api/cryptoid.py
@@ -0,0 +1,12 @@
+from blockapi.services import (
+    BlockchainAPI
+)
+
+
+class CryptoIDAPI(BlockchainAPI):
+    """
+    Multi coins
+    API docs: https://chainz.cryptoid.info/api.dws
+    Explorer: https://chainz.cryptoid.info
+    """
+    pass
